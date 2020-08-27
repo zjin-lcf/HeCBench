b023bb937d3b5e7e324867d1bd06c9d3e94b75e0
Zheming Jin <zheming@jlselogin2.ftm.alcf.anl.gov>
2020-08-27 02:29:50 +0000

    Add memory copy from host to device benchmark

---
+++ memcpy-cuda/main.cu
@@ -0,0 +1,74 @@
+/*
+   Copyright (c) 2015-2016 Advanced Micro Devices, Inc. All rights reserved.
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <cuda.h>
+
+#define NUM_SIZE 16
+#define NUM_ITER (1 << 16)
+
+void setup(size_t *size) {
+  for (int i = 0; i < NUM_SIZE; i++) {
+    size[i] = 1 << (i + 6);  // start at 8 bytes
+  }
+}
+
+void valSet(int* A, int val, size_t size) {
+  size_t len = size / sizeof(int);
+  for (int i = 0; i < len; i++) {
+    A[i] = val;
+  }
+}
+
+int main() {
+  int *A, *Ad;
+  size_t size[NUM_SIZE];
+  cudaError_t err;
+
+  setup(size);
+  for (int i = 0; i < NUM_SIZE; i++) {
+    A = (int*)malloc(size[i]);
+    if (A == nullptr) {
+      std::cerr << "Host memory allocation failed\n";
+      return -1;
+    }	
+    valSet(A, 1, size[i]);
+    err = cudaMalloc((void**)&Ad, size[i]);
+    if (err != cudaSuccess) {
+      std::cerr << "Device memory allocation failed\n";
+      free(A);
+      return -1;
+    }
+    clock_t start, end;
+    start = clock();
+    for (int j = 0; j < NUM_ITER; j++) {
+      cudaMemcpy(Ad, A, size[i], cudaMemcpyHostToDevice);
+    }
+    //cudaDeviceSynchronize();
+    end = clock();
+    double uS = (double)(end - start) * 1000 / (NUM_ITER * CLOCKS_PER_SEC);
+    std::cout << "Copy " << size[i] << " btyes from host to device takes " 
+      << uS <<  " us" << std::endl;
+    cudaFree(Ad);
+    free(A);
+  }
+  return 0;
+}
---
+++ memcpy-sycl/main.cpp
@@ -0,0 +1,81 @@
+/*
+   Copyright (c) 2015-2016 Advanced Micro Devices, Inc. All rights reserved.
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+   */
+
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include "common.h"
+
+#define NUM_SIZE 16
+#define NUM_ITER (1 << 16)
+
+void setup(size_t *size) {
+  for (int i = 0; i < NUM_SIZE; i++) {
+    size[i] = 1 << (i + 6);  // start at 8 bytes
+  }
+}
+
+void valSet(int* A, int val, size_t size) {
+  size_t len = size / sizeof(int);
+  for (int i = 0; i < len; i++) {
+    A[i] = val;
+  }
+}
+
+int main() {
+#ifdef USE_GPU 
+    gpu_selector dev_sel;
+#else
+    cpu_selector dev_sel;
+#endif
+    queue q(dev_sel);
+
+  int *A;
+  size_t size[NUM_SIZE];
+
+  setup(size);
+  for (int i = 0; i < NUM_SIZE; i++) {
+    A = (int*)malloc(size[i]);
+    if (A == nullptr) {
+      std::cerr << "Host memory allocation failed\n";
+      return -1;
+    }	
+    valSet(A, 1, size[i]);
+
+
+    size_t len = size[i] / sizeof(int);
+    buffer<int, 1> Ad (len);
+    clock_t start, end;
+    start = clock();
+
+    for (int j = 0; j < NUM_ITER; j++) {
+      q.submit([&](handler &h) {
+        auto Ad_acc = Ad.get_access<sycl_write>(h);
+        h.copy(A, Ad_acc);
+      });
+      q.wait();
+    }
+    end = clock();
+    double uS = (double)(end - start) * 1000 / (NUM_ITER * CLOCKS_PER_SEC);
+    std::cout << "Copy " << size[i] << " btyes from host to device takes " 
+      << uS <<  " us" << std::endl;
+    free(A);
+  }
+  return 0;
+}
