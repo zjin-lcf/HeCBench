4f432b9ba22eb973a9c7ca4b6c3891a201481710
Chameleon Cloud User <cc@lp-xeon-test.novalocal>
2020-08-27 21:09:01 +0000

    In-order queue can reduce the host timing, but increase the device timing

--- reverse-sycl/main.cpp
+++ reverse-sycl/main.cpp
@@ -13,13 +13,13 @@ int main() {
 #else
   cpu_selector dev_sel;
 #endif
-  queue q(dev_sel);
+  queue q(dev_sel, {property::queue::in_order()});
 
   buffer<int, 1> dd (d, len);
   for (int i = 0; i <= iteration; i++) {
     q.submit([&](handler &h) {
       accessor <int, 1, sycl_read_write, access::target::local> s (len, h);
-      auto d = dd.get_access<sycl_read_write>(h);
+      auto d = dd.get_access<sycl_discard_read_write>(h);
       h.parallel_for(nd_range<1>(range<1>(len), range<1>(len)), [=](nd_item<1> item) {
         int t = item.get_local_id(0);
         int tr = len-t-1;
