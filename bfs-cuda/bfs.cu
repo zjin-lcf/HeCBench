c5d67bcde583ae8026317412c35e40470fb2750a
Zheming Jin <zheming@firestone.ftm.alcf.anl.gov>
2020-09-15 13:46:49 +0000

    Update the BFS in CUDA and add the BFS in OpenMP

--- bfs-cuda/bfs.cu
+++ bfs-cuda/bfs.cu
@@ -1,11 +1,10 @@
-//--by Jianbin Fang
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <cuda.h>
 
-#define __CL_ENABLE_EXCEPTIONS
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <cstdio>
+#define MAX_THREADS_PER_BLOCK 512
 
 #ifdef  PROFILING
 #include "timer.h"
@@ -13,23 +12,45 @@
 
 #include "util.h"
 
-#define MAX_THREADS_PER_BLOCK 256
-
-
 //Structure to hold a node information
 struct Node
 {
-  int starting;
-  int no_of_edges;
+	int starting;
+	int no_of_edges;
 };
 
+__global__ void
+Kernel( const Node* d_graph_nodes, const int* d_graph_edges, char* d_graph_mask, char* d_updatind_graph_mask, const char *d_graph_visited, int* d_cost, const int no_of_nodes) 
+{
+	int tid = blockIdx.x*blockDim.x + threadIdx.x;
+	if( tid<no_of_nodes && d_graph_mask[tid])
+	{
+		d_graph_mask[tid]=0;
+		for(int i=d_graph_nodes[tid].starting; i<(d_graph_nodes[tid].no_of_edges + d_graph_nodes[tid].starting); i++)
+			{
+			int id = d_graph_edges[i];
+			if(!d_graph_visited[id])
+				{
+				d_cost[id]=d_cost[tid]+1;
+				d_updatind_graph_mask[id]=1;
+				}
+			}
+	}
+}
 
-//----------------------------------------------------------
-//--bfs on cpu
-//--programmer:  jianbin
-//--date:  26/01/2011
-//--note: width is changed to the new_width
-//----------------------------------------------------------
+__global__ void
+Kernel2( char* d_graph_mask, char *d_updatind_graph_mask, char* d_graph_visited, char *d_over, const int no_of_nodes)
+{
+	int tid = blockIdx.x*blockDim.x + threadIdx.x;
+	if( tid<no_of_nodes && d_updatind_graph_mask[tid])
+	{
+		d_graph_mask[tid]=1;
+		d_graph_visited[tid]=1;
+		*d_over=1;
+		d_updatind_graph_mask[tid]=0;
+	}
+}
+
 void run_bfs_cpu(int no_of_nodes, Node *h_graph_nodes, int edge_list_size, \
     int *h_graph_edges, char *h_graph_mask, char *h_updating_graph_mask, \
     char *h_graph_visited, int *h_cost_ref){
@@ -65,78 +86,86 @@ void run_bfs_cpu(int no_of_nodes, Node *h_graph_nodes, int edge_list_size, \
   }
   while(stop);
 }
-//----------------------------------------------------------
-//--breadth first search on GPUs
-//----------------------------------------------------------
-void run_bfs_gpu(int no_of_nodes, Node *d_graph_nodes, int edge_list_size, \
-    int *d_graph_edges, char *d_graph_mask, char *d_updating_graph_mask, \
-    char *d_graph_visited, int *d_cost) throw(std::string)
-{
-  char d_over[1];
-#ifdef  PROFILING
-  timer kernel_timer;
-  double kernel_time = 0.0;    
-  kernel_timer.reset();
-  kernel_timer.start();
-#endif
 
-#pragma omp target data map(to: d_graph_nodes[0:no_of_nodes], \
-                                d_graph_edges[0:edge_list_size], \
-                                d_graph_visited[0:no_of_nodes], \
-                                d_graph_mask[0:no_of_nodes], \
-                                d_updating_graph_mask[0:no_of_nodes]) \
-                        map(alloc: d_over[0:1])\
-                        map(tofrom: d_cost[0:no_of_nodes])
-{
+////////////////////////////////////////////////////////////////////////////////
+// Main Program
+////////////////////////////////////////////////////////////////////////////////
 
-    do {
-      d_over[0] = 0;
-#pragma omp target update to (d_over[0:1])
-
-#pragma omp target teams distribute parallel for thread_limit(MAX_THREADS_PER_BLOCK)
-      for (int tid = 0; tid < no_of_nodes; tid++) {
-        if(d_graph_mask[tid]){
-          d_graph_mask[tid]=0;
-          for(int i=d_graph_nodes[tid].starting; 
-              i<(d_graph_nodes[tid].no_of_edges + d_graph_nodes[tid].starting); i++){
-            int id = d_graph_edges[i];
-            if(!d_graph_visited[id]){
-              d_cost[id]=d_cost[tid]+1;
-              d_updating_graph_mask[id]=1;
-            }
-          }
-        }  
-      }
-
-#pragma omp target teams distribute parallel for thread_limit(MAX_THREADS_PER_BLOCK) 
-      for (int tid = 0; tid < no_of_nodes; tid++) {
-        if(d_updating_graph_mask[tid]){
-          d_graph_mask[tid]=1;
-          d_graph_visited[tid]=1;
-          d_over[0]=1;
-          d_updating_graph_mask[tid]=0;
-        }
-      }
-
-#pragma omp target update from (d_over[0:1])
-  } while (d_over[0]);
-
-  //--statistics
-#ifdef  PROFILING
-  kernel_timer.stop();
-  kernel_time = kernel_timer.getTimeInSeconds();
-  std::cout<<"kernel time(s):"<<kernel_time<<std::endl;    
-#endif
-}
 void Usage(int argc, char**argv){
 
-  fprintf(stderr,"Usage: %s <input_file>\n", argv[0]);
+fprintf(stderr,"Usage: %s <input_file>\n", argv[0]);
 
 }
+////////////////////////////////////////////////////////////////////////////////
+//Apply BFS on a Graph using CUDA
+////////////////////////////////////////////////////////////////////////////////
+void run_bfs_gpu(int no_of_nodes, Node *h_graph_nodes, int edge_list_size, \
+    int *h_graph_edges, char *h_graph_mask, char *h_updating_graph_mask, \
+    char *h_graph_visited, int *h_cost) throw(std::string)
+{
+
+	Node* d_graph_nodes;
+	cudaMalloc( (void**) &d_graph_nodes, sizeof(Node)*no_of_nodes) ;
+	cudaMemcpy( d_graph_nodes, h_graph_nodes, sizeof(Node)*no_of_nodes, cudaMemcpyHostToDevice) ;
+
+	int* d_graph_edges;
+	cudaMalloc( (void**) &d_graph_edges, sizeof(int)*edge_list_size) ;
+	cudaMemcpy( d_graph_edges, h_graph_edges, sizeof(int)*edge_list_size, cudaMemcpyHostToDevice) ;
+
+	char* d_graph_mask;
+	cudaMalloc( (void**) &d_graph_mask, sizeof(char)*no_of_nodes) ;
+	cudaMemcpy( d_graph_mask, h_graph_mask, sizeof(char)*no_of_nodes, cudaMemcpyHostToDevice) ;
+
+	char* d_updating_graph_mask;
+	cudaMalloc( (void**) &d_updating_graph_mask, sizeof(char)*no_of_nodes) ;
+	cudaMemcpy( d_updating_graph_mask, h_updating_graph_mask, sizeof(char)*no_of_nodes, cudaMemcpyHostToDevice) ;
+
+	char* d_graph_visited;
+	cudaMalloc( (void**) &d_graph_visited, sizeof(char)*no_of_nodes) ;
+	cudaMemcpy( d_graph_visited, h_graph_visited, sizeof(char)*no_of_nodes, cudaMemcpyHostToDevice) ;
+
+	int* d_cost;
+	cudaMalloc( (void**) &d_cost, sizeof(int)*no_of_nodes);
+	cudaMemcpy( d_cost, h_cost, sizeof(int)*no_of_nodes, cudaMemcpyHostToDevice) ;
+
+  char h_over;
+	char *d_over;
+	cudaMalloc( (void**) &d_over, sizeof(char));
+
+	// setup execution parameters
+	dim3  grid( (no_of_nodes + MAX_THREADS_PER_BLOCK - 1) / MAX_THREADS_PER_BLOCK);
+	dim3  threads(MAX_THREADS_PER_BLOCK);
+
+  do {
+    h_over = 0;
+		cudaMemcpy( d_over, &h_over, sizeof(char), cudaMemcpyHostToDevice) ;
+		Kernel<<< grid, threads >>>( d_graph_nodes, d_graph_edges, d_graph_mask, d_updating_graph_mask, d_graph_visited, d_cost, no_of_nodes);
+		Kernel2<<< grid, threads >>>( d_graph_mask, d_updating_graph_mask, d_graph_visited, d_over, no_of_nodes);
+		cudaMemcpy( &h_over, d_over, sizeof(char), cudaMemcpyDeviceToHost) ;
+	} while(h_over);
+
+
+	// copy result from device to host
+	cudaMemcpy( h_cost, d_cost, sizeof(int)*no_of_nodes, cudaMemcpyDeviceToHost) ;
+
+	cudaFree(d_graph_nodes);
+	cudaFree(d_graph_edges);
+	cudaFree(d_graph_mask);
+	cudaFree(d_updating_graph_mask);
+	cudaFree(d_graph_visited);
+	cudaFree(d_cost);
+
+#ifdef	PROFILING
+  kernel_timer.stop();
+  kernel_time = kernel_timer.getTimeInSeconds();
+  std::cout<<"kernel time(s):"<<kernel_time<<std::endl;		
+#endif
+}
+
 //----------------------------------------------------------
-//--cambine:  main function
-//--author:    created by Jianbin Fang
-//--date:    25/01/2011
+//--cambine:	main function
+//--author:		created by Jianbin Fang
+//--date:		25/01/2011
 //----------------------------------------------------------
 int main(int argc, char * argv[])
 {
@@ -199,18 +228,18 @@ int main(int argc, char * argv[])
     if(fp)
       fclose(fp);    
     // allocate mem for the result on host side
-    int  *h_cost = (int*) malloc(sizeof(int)*no_of_nodes);
+    int	*h_cost = (int*) malloc(sizeof(int)*no_of_nodes);
     int *h_cost_ref = (int*)malloc(sizeof(int)*no_of_nodes);
     for(int i=0;i<no_of_nodes;i++){
       h_cost[i]=-1;
       h_cost_ref[i] = -1;
     }
     h_cost[source]=0;
-    h_cost_ref[source]=0;    
+    h_cost_ref[source]=0;		
     //---------------------------------------------------------
     printf("run bfs (#nodes = %d) on device\n", no_of_nodes);
     run_bfs_gpu(no_of_nodes,h_graph_nodes,edge_list_size,h_graph_edges, 
-        h_graph_mask, h_updating_graph_mask, h_graph_visited, h_cost);  
+                h_graph_mask, h_updating_graph_mask, h_graph_visited, h_cost);	
     //---------------------------------------------------------
     //
     printf("run bfs (#nodes = %d) on host (cpu) \n", no_of_nodes);
@@ -225,11 +254,11 @@ int main(int argc, char * argv[])
     h_graph_mask[source]=1;
     h_graph_visited[source]=1;
     run_bfs_cpu(no_of_nodes,h_graph_nodes,edge_list_size,h_graph_edges, 
-        h_graph_mask, h_updating_graph_mask, h_graph_visited, h_cost_ref);
+                h_graph_mask, h_updating_graph_mask, h_graph_visited, h_cost_ref);
     //---------------------------------------------------------
     //--result varification
     compare_results<int>(h_cost_ref, h_cost, no_of_nodes);
-    //release host memory    
+    //release host memory		
     free(h_graph_nodes);
     free(h_graph_mask);
     free(h_updating_graph_mask);
@@ -242,7 +271,7 @@ int main(int argc, char * argv[])
     free(h_graph_nodes);
     free(h_graph_mask);
     free(h_updating_graph_mask);
-    free(h_graph_visited);    
+    free(h_graph_visited);		
   }
 
   return 0;
