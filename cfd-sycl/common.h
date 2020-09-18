4db9331f01631ef4157e04df1883c7ea93259339
Chameleon Cloud User <cc@lp-xeon-test.novalocal>
2020-09-18 23:09:29 +0000

    Add the SYCL and OMP versions of cfd

---
+++ cfd-sycl/common.h
@@ -0,0 +1,10 @@
+#include <CL/sycl.hpp>
+
+using namespace cl::sycl;
+constexpr access::mode sycl_read       = access::mode::read;
+constexpr access::mode sycl_write      = access::mode::write;
+constexpr access::mode sycl_read_write = access::mode::read_write;
+constexpr access::mode sycl_discard_read_write = access::mode::discard_read_write;
+constexpr access::mode sycl_discard_write = access::mode::discard_write;
+constexpr access::target sycl_global_buffer = access::target::global_buffer;
+
