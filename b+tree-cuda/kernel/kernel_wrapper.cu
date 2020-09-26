286b385b2d443737ae1fde56d80c298934e41c9d
Zheming Jin <zheming@iris14.ftm.alcf.anl.gov>
2020-09-26 04:24:32 +0000

    Add the modified CUDA version

---
+++ b+tree-cuda/kernel/kernel_wrapper.cu
@@ -0,0 +1,145 @@
+#include <cuda.h>
+#include <stdio.h>
+#include "../common.h"
+#include "../util/timer/timer.h"
+#include "./kernel.cu"
+#include "./kernel_wrapper.h"				// (in current directory)
+
+void 
+kernel_wrapper(record *records,
+						long records_mem,
+						knode *knodes,
+						long knodes_elem,
+						long knodes_mem,
+
+						int order,
+						long maxheight,
+						int count,
+
+						long *currKnode,
+						long *offset,
+						int *keys,
+						record *ans)
+{
+
+	long long offload_start = get_time();
+
+	int numBlocks;
+	numBlocks = count;									// max # of blocks can be 65,535
+	int threadsPerBlock;
+	threadsPerBlock = order < 256 ? order : 256;
+
+	printf("# of blocks = %d, # of threads/block = %d (ensure that device can handle)\n", numBlocks, threadsPerBlock);
+
+	//==================================================50
+	//	recordsD
+	//==================================================50
+
+	record *recordsD;
+	cudaMalloc((void**)&recordsD, records_mem);
+
+	//==================================================50
+	//	knodesD
+	//==================================================50
+
+	knode *knodesD;
+	cudaMalloc((void**)&knodesD, knodes_mem);
+
+	//==================================================50
+	//	currKnodeD
+	//==================================================50
+
+	long *currKnodeD;
+	cudaMalloc((void**)&currKnodeD, count*sizeof(long));
+
+	//==================================================50
+	//	offsetD
+	//==================================================50
+
+	long *offsetD;
+	cudaMalloc((void**)&offsetD, count*sizeof(long));
+
+	//==================================================50
+	//	keysD
+	//==================================================50
+
+	int *keysD;
+	cudaMalloc((void**)&keysD, count*sizeof(int));
+
+	//==================================================50
+	//	ansD
+	//==================================================50
+
+	record *ansD;
+	cudaMalloc((void**)&ansD, count*sizeof(record));
+
+	//==================================================50
+	//	recordsD
+	//==================================================50
+
+	cudaMemcpyAsync(recordsD, records, records_mem, cudaMemcpyHostToDevice, 0);
+
+	//==================================================50
+	//	knodesD
+	//==================================================50
+
+	cudaMemcpyAsync(knodesD, knodes, knodes_mem, cudaMemcpyHostToDevice, 0);
+
+	//==================================================50
+	//	currKnodeD
+	//==================================================50
+
+	cudaMemcpyAsync(currKnodeD, currKnode, count*sizeof(long), cudaMemcpyHostToDevice, 0);
+
+	//==================================================50
+	//	offsetD
+	//==================================================50
+
+	cudaMemcpyAsync(offsetD, offset, count*sizeof(long), cudaMemcpyHostToDevice, 0);
+
+	//==================================================50
+	//	keysD
+	//==================================================50
+
+	cudaMemcpyAsync(keysD, keys, count*sizeof(int), cudaMemcpyHostToDevice, 0);
+
+	//==================================================50
+	//	ansD
+	//==================================================50
+
+	cudaMemcpyAsync(ansD, ans, count*sizeof(record), cudaMemcpyHostToDevice, 0);
+
+	//======================================================================================================================================================150
+	// findK kernel
+	//======================================================================================================================================================150
+
+	findK<<<numBlocks, threadsPerBlock>>>(	maxheight,
+
+											knodesD,
+											knodes_elem,
+
+											recordsD,
+
+											currKnodeD,
+											offsetD,
+											keysD,
+											ansD);
+
+	//==================================================50
+	//	ansD
+	//==================================================50
+
+	cudaMemcpy(ans, ansD, count*sizeof(record), cudaMemcpyDeviceToHost);
+
+	cudaFree(recordsD);
+	cudaFree(knodesD);
+	cudaFree(currKnodeD);
+	cudaFree(offsetD);
+	cudaFree(keysD);
+	cudaFree(ansD);
+
+	long long offload_end = get_time();
+
+	printf("Total time:\n");
+  printf("%.12f s\n", (float) (offload_end-offload_start) / 1000000); 
+}
