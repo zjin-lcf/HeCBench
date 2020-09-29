3360e44ab722cf4b2e462f93ada799b824f2d592
Chameleon Cloud User <cc@lp-xeon-test.novalocal>
2020-09-29 04:59:52 +0000

    Add the updated myocyte: 1) add the missing master function call to compute the final values for the 7th step of a total of 13 steps (the cause of the missing function call is unknown)  2) isinf returns -1 when the argument is -inf in master.c

---
+++ myocyte-sycl/common.h
@@ -0,0 +1,35 @@
+// #ifdef __cplusplus
+// extern "C" {
+// #endif
+
+//========================================================================================================================================================================================================200
+//	DEFINE
+//========================================================================================================================================================================================================200
+#ifndef __COMMON
+#define __COMMON
+
+#define fp float
+
+#define NUMBER_THREADS 2
+
+#define EQUATIONS 91
+#define PARAMETERS 18
+
+#include <CL/sycl.hpp>
+
+using namespace cl::sycl;
+constexpr access::mode sycl_read       = access::mode::read;
+constexpr access::mode sycl_write      = access::mode::write;
+constexpr access::mode sycl_read_write = access::mode::read_write;
+constexpr access::mode sycl_discard_read_write = access::mode::discard_read_write;
+constexpr access::mode sycl_discard_write = access::mode::discard_write;
+constexpr access::target sycl_global_buffer = access::target::global_buffer;
+
+//========================================================================================================================================================================================================200
+//	END
+//========================================================================================================================================================================================================200
+#endif
+
+// #ifdef __cplusplus
+// }
+// #endif
---
+++ myocyte-sycl/kernel/kernel_wrapper.h
@@ -0,0 +1,8 @@
+
+int 
+kernel_wrapper(	int xmax,
+		int workload,
+		fp*** y,
+		fp** x,
+		fp** params,
+		fp* com);
