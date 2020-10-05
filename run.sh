a18033717d4597b3969b868501d792e109496ded
Chameleon Cloud User <cc@lp-xeon-test.novalocal>
2020-10-05 04:54:58 +0000

    Format the script to remove tabs

--- run.sh
+++ run.sh
@@ -3,16 +3,16 @@ export SYCL_BE=PI_OPENCL
 
 for dir in `find . -mindepth 1 -maxdepth 1 -type d | grep -Ev '.\.git|include|cuda|omp'`
 do
-	cd ${dir}
-	make clean
-	echo "${dir} results:"
+  cd ${dir}
+  make clean
+  echo "${dir} results:"
   for (( i = 0; i < 10; i = i + 1 ))
   do
     cliloader -q -h -d make run &> report${i}.txt
   done
   grep "Total" report*.txt
   make clean
-	cd ..
+  cd ..
 done
 
 # For OMP target offloading
@@ -20,14 +20,14 @@ export LIBOMPTARGET_PLUGIN=OPENCL
 
 for dir in `find . -mindepth 1 -maxdepth 1 -type d | grep -Ev '.\.git|include|sycl|dpct'`
 do
-	cd ${dir}
-	make clean
-	echo "${dir} results:"
+  cd ${dir}
+  make clean
+  echo "${dir} results:"
   for (( i = 0; i < 10; i = i + 1 ))
   do
     cliloader -q -h -d make run &> report${i}.txt
   done
   grep "Total" report*.txt
   make clean
-	cd ..
+  cd ..
 done
