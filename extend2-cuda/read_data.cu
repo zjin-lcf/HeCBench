4a668337fa8b2acbeae609b48013029635fe2345
Chameleon Cloud User <cc@lp-xeon-test.novalocal>
2020-10-23 03:53:05 +0000

    Update Makefiles to maintain a single file for reading data

--- extend2-cuda/read_data.cu
+++ extend2-cuda/read_data.cu
@@ -1,6 +1,6 @@
 /*
-  written by Kaz Yoshii <ky@anl.gov>
- */
+   written by Kaz Yoshii <ky@anl.gov>
+   */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -14,60 +14,60 @@
 /* return non-zero on success */
 static int read_check(int fd, void *buf, size_t sz)
 {
-	if(read(fd, buf, sz) != sz)
-		return 0;
-	return 1;
+  if(read(fd, buf, sz) != sz)
+    return 0;
+  return 1;
 }
 
 /* return non-zero on success */
 int read_data(const char *fn, struct extend2_dat *d)
 {
-	int fd;
 
-	if (!d)
-		return 0;
+  if (!d)
+    return 0;
 
-	fd = open(fn, O_RDONLY);
-	if (fd < 0) {
-    printf("Failed to open file %s\n", fn);
-		return 0;
-	}
+  int fd = open(fn, O_RDONLY);
+  if (fd < 0) {
+    perror("Cannot open output file\n");
+    return 0;
+  }
 
-	/* input */
-	assert( read_check(fd, (void*)&d->qlen, sizeof(int)) );
-	d->query = (uint8_t*)malloc(d->qlen);
-	assert( read_check(fd, d->query, d->qlen) );
+  /* input */
+  assert( read_check(fd, (void*)&d->qlen, sizeof(int)) );
+  d->query = (uint8_t*)malloc(d->qlen);
+  assert( read_check(fd, d->query, d->qlen) );
 
-	assert( read_check(fd, (void*)&d->tlen, sizeof(int)) );
-	d->target = (uint8_t*)malloc(d->tlen);
-	assert( read_check(fd, d->target, d->tlen) );
+  assert( read_check(fd, (void*)&d->tlen, sizeof(int)) );
+  d->target = (uint8_t*)malloc(d->tlen);
+  assert( read_check(fd, d->target, d->tlen) );
 
-	assert( read_check(fd, (void*)&d->m, sizeof(int)) );
-	assert( read_check(fd, (void*)d->mat, 25) );
+  assert( read_check(fd, (void*)&d->m, sizeof(int)) );
+  assert( read_check(fd, (void*)d->mat, 25) );
 
-	assert( read_check(fd, (void*)&d->o_del, sizeof(int)) );
-	assert( read_check(fd, (void*)&d->e_del, sizeof(int)) );
-	assert( read_check(fd, (void*)&d->o_ins, sizeof(int)) );
-	assert( read_check(fd, (void*)&d->e_ins, sizeof(int)) );
-	assert( read_check(fd, (void*)&d->w, sizeof(int)) );
-	assert( read_check(fd, (void*)&d->end_bonus, sizeof(int)) );
-	assert( read_check(fd, (void*)&d->zdrop, sizeof(int)) );
-	assert( read_check(fd, (void*)&d->h0, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->o_del, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->e_del, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->o_ins, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->e_ins, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->w, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->end_bonus, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->zdrop, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->h0, sizeof(int)) );
 
-	/* results */
-	assert( read_check(fd, (void*)&d->qle, sizeof(int)) );
-	assert( read_check(fd, (void*)&d->tle, sizeof(int)) );
-	assert( read_check(fd, (void*)&d->gtle, sizeof(int)) );
-	assert( read_check(fd, (void*)&d->gscore, sizeof(int)) );
-	assert( read_check(fd, (void*)&d->max_off, sizeof(int)) );
+  /* results */
+  assert( read_check(fd, (void*)&d->qle, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->tle, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->gtle, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->gscore, sizeof(int)) );
+  assert( read_check(fd, (void*)&d->max_off, sizeof(int)) );
 
-	/* return */
-	assert( read_check(fd, (void*)&d->score, sizeof(int)) );
+  /* return */
+  assert( read_check(fd, (void*)&d->score, sizeof(int)) );
 
-	/* time-to-solution in cycle */
-	assert( read_check(fd, (void*)&d->tsc, sizeof(uint64_t)) );
-	assert( read_check(fd, (void*)&d->sec, sizeof(double)) );
+  /* time-to-solution in cycle */
+  assert( read_check(fd, (void*)&d->tsc, sizeof(uint64_t)) );
+  assert( read_check(fd, (void*)&d->sec, sizeof(double)) );
 
-	return 1;
+  close(fd);
+  return 1;
 }
 
