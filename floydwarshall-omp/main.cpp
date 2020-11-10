0b66369a1ad0ad73dfd6a00128637a493b7dfe8b
Chameleon Cloud User <cc@lp-xeon-test.novalocal>
2020-11-10 23:50:56 +0000

    Add the Floyd Warshall examples

---
+++ floydwarshall-omp/main.cpp
@@ -0,0 +1,255 @@
+/*
+   Copyright (c) 2015-2016 Advanced Micro Devices, Inc. All rights reserved.
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+   */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#define MAXDISTANCE    (200)
+
+/**
+ * Returns the lesser of the two unsigned integers a and b
+ */
+unsigned int minimum(unsigned int a, unsigned int b) 
+{
+  return (b < a) ? b : a;
+}
+
+/**
+ * Reference CPU implementation of FloydWarshall PathFinding
+ * for performance comparison
+ * @param pathDistanceMatrix Distance between nodes of a graph
+ * @param intermediate node between two nodes of a graph
+ * @param number of nodes in the graph
+ */
+void floydWarshallCPUReference(unsigned int * pathDistanceMatrix,
+    unsigned int * pathMatrix, unsigned int numNodes)
+{
+  unsigned int distanceYtoX, distanceYtoK, distanceKtoX, indirectDistance;
+
+  /*
+   * pathDistanceMatrix is the adjacency matrix(square) with
+   * the dimension equal to the number of nodes in the graph.
+   */
+  unsigned int width = numNodes;
+  unsigned int yXwidth;
+
+  /*
+   * for each intermediate node k in the graph find the shortest distance between
+   * the nodes i and j and update as
+   *
+   * ShortestPath(i,j,k) = min(ShortestPath(i,j,k-1), ShortestPath(i,k,k-1) + ShortestPath(k,j,k-1))
+   */
+
+  for(unsigned int k = 0; k < numNodes; ++k)
+  {
+    for(unsigned int y = 0; y < numNodes; ++y)
+    {
+      yXwidth =  y*numNodes;
+      for(unsigned int x = 0; x < numNodes; ++x)
+      {
+        distanceYtoX = pathDistanceMatrix[yXwidth + x];
+        distanceYtoK = pathDistanceMatrix[yXwidth + k];
+        distanceKtoX = pathDistanceMatrix[k * width + x];
+
+        indirectDistance = distanceYtoK + distanceKtoX;
+
+        if(indirectDistance < distanceYtoX)
+        {
+          pathDistanceMatrix[yXwidth + x] = indirectDistance;
+          pathMatrix[yXwidth + x]         = k;
+        }
+      }
+    }
+  }
+}
+
+
+/*!
+ * The floyd Warshall algorithm is a multipass algorithm
+ * that calculates the shortest path between each pair of
+ * nodes represented by pathDistanceBuffer.
+ *
+ * In each pass a node k is introduced and the pathDistanceBuffer
+ * which has the shortest distance between each pair of nodes
+ * considering the (k-1) nodes (that are introduced in the previous
+ * passes) is updated such that
+ *
+ * ShortestPath(x,y,k) = min(ShortestPath(x,y,k-1), ShortestPath(x,k,k-1) + ShortestPath(k,y,k-1))
+ * where x and y are the pair of nodes between which the shortest distance
+ * is being calculated.
+ *
+ * pathBuffer stores the intermediate nodes through which the shortest
+ * path goes for each pair of nodes.
+ *
+ * numNodes is the number of nodes in the graph.
+ *
+ * for more detailed explaination of the algorithm kindly refer to the document
+ * provided with the sample
+ */
+
+
+int main(int argc, char** argv) {
+  // There are three required command-line arguments
+  unsigned int numNodes = atoi(argv[1]);
+  unsigned int iterations = atoi(argv[2]);
+  unsigned int blockSize = atoi(argv[3]);
+
+  // allocate and init memory used by host
+  unsigned int* pathMatrix = NULL;
+  unsigned int* pathDistanceMatrix = NULL;
+  unsigned int* verificationPathDistanceMatrix = NULL;
+  unsigned int* verificationPathMatrix = NULL;
+  unsigned int matrixSize;
+  unsigned int matrixSizeBytes;
+
+  matrixSize = numNodes * numNodes;
+  matrixSizeBytes = numNodes * numNodes * sizeof(unsigned int);
+  pathDistanceMatrix = (unsigned int *) malloc(matrixSizeBytes);
+  assert (pathDistanceMatrix != NULL) ;
+
+  pathMatrix = (unsigned int *) malloc(matrixSizeBytes);
+  assert (pathMatrix != NULL) ;
+
+  // input must be initialized; otherwise host and device results may be different
+  srand(2);
+  for(unsigned int i = 0; i < numNodes; i++)
+    for(unsigned int j = 0; j < numNodes; j++)
+    {
+      int index = i*numNodes + j;
+      pathDistanceMatrix[index] = rand() % (MAXDISTANCE + 1);
+    }
+  for(unsigned int i = 0; i < numNodes; ++i)
+  {
+    unsigned int iXWidth = i * numNodes;
+    pathDistanceMatrix[iXWidth + i] = 0;
+  }
+
+  /*
+   * pathMatrix is the intermediate node from which the path passes
+   * pathMatrix(i,j) = k means the shortest path from i to j
+   * passes through an intermediate node k
+   * Initialized such that pathMatrix(i,j) = i
+   */
+  for(unsigned int i = 0; i < numNodes; ++i)
+  {
+    for(unsigned int j = 0; j < i; ++j)
+    {
+      pathMatrix[i * numNodes + j] = i;
+      pathMatrix[j * numNodes + i] = j;
+    }
+    pathMatrix[i * numNodes + i] = i;
+  }
+
+  verificationPathDistanceMatrix = (unsigned int *) malloc(matrixSizeBytes);
+  assert (verificationPathDistanceMatrix != NULL);
+
+  verificationPathMatrix = (unsigned int *) malloc(matrixSizeBytes);
+  assert(verificationPathMatrix != NULL);
+
+  memcpy(verificationPathDistanceMatrix, pathDistanceMatrix, matrixSizeBytes);
+  memcpy(verificationPathMatrix, pathMatrix, matrixSizeBytes);
+
+  unsigned int numPasses = numNodes;
+
+#pragma omp target data map(alloc: pathDistanceMatrix[0:matrixSize], \
+                                   pathMatrix[0:matrixSize])
+  {
+    for (unsigned int n = 0; n < iterations; n++) {
+      /*
+       * The floyd Warshall algorithm is a multipass algorithm
+       * that calculates the shortest path between each pair of
+       * nodes represented by pathDistanceBuffer.
+       *
+       * In each pass a node k is introduced and the pathDistanceBuffer
+       * which has the shortest distance between each pair of nodes
+       * considering the (k-1) nodes (that are introduced in the previous
+       * passes) is updated such that
+       *
+       * ShortestPath(x,y,k) = min(ShortestPath(x,y,k-1), ShortestPath(x,k,k-1) + ShortestPath(k,y,k-1))
+       * where x and y are the pair of nodes between which the shortest distance
+       * is being calculated.
+       *
+       * pathBuffer stores the intermediate nodes through which the shortest
+       * path goes for each pair of nodes.
+       */
+
+#pragma omp target update to (pathDistanceMatrix[0:matrixSize]) 
+
+      for(unsigned int k = 0; k < numPasses; k++)
+      {
+#pragma omp target teams distribute parallel for collapse(2) thread_limit (blockSize*blockSize)
+        for(unsigned int y = 0; y < numNodes; ++y)
+        {
+          for(unsigned int x = 0; x < numNodes; ++x)
+          {
+            unsigned int distanceYtoX = pathDistanceMatrix[y*numNodes + x];
+            unsigned int distanceYtoK = pathDistanceMatrix[y*numNodes + k];
+            unsigned int distanceKtoX = pathDistanceMatrix[k*numNodes + x];
+            unsigned int indirectDistance = distanceYtoK + distanceKtoX;
+
+            if(indirectDistance < distanceYtoX)
+            {
+              pathDistanceMatrix[y*numNodes + x] = indirectDistance;
+              pathMatrix[y*numNodes + x]         = k;
+            }
+          }
+        }
+      }
+    }
+#pragma omp target update from (pathDistanceMatrix[0:matrixSize]) 
+  }
+
+
+  // verify
+  floydWarshallCPUReference(verificationPathDistanceMatrix,
+      verificationPathMatrix, numNodes);
+  if(memcmp(pathDistanceMatrix, verificationPathDistanceMatrix,
+        numNodes*numNodes*sizeof(unsigned int)) == 0)
+  {
+    printf("Pass\n");
+  }
+  else
+  {
+    printf("Fail\n");
+    if (numNodes <= 8) 
+    {
+      for (unsigned int i = 0; i < numNodes; i++) {
+        for (unsigned int j = 0; j < numNodes; j++)
+          printf("host: %u ", verificationPathDistanceMatrix[i*numNodes+j]);
+        printf("\n");
+      }
+      for (unsigned int i = 0; i < numNodes; i++) {
+        for (unsigned int j = 0; j < numNodes; j++)
+          printf("device: %u ", pathDistanceMatrix[i*numNodes+j]);
+        printf("\n");
+      }
+    }
+  }
+
+  free(pathDistanceMatrix);
+  free(pathMatrix);
+  free(verificationPathDistanceMatrix);
+  free(verificationPathMatrix);
+  return 0;
+}
