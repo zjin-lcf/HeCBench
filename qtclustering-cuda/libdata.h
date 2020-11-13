25881cfd69fa03892f8d017d80e0f4cc3143b0c6
Jin Z <5zj@leconte.ftpn.ornl.gov>
2020-11-13 13:42:26 -0500

    Add the CUDA version. We use compact storage only and generate synthetic data.

---
+++ qtclustering-cuda/libdata.h
@@ -0,0 +1,7 @@
+#ifndef _LIBDATA_H_
+#define _LIBDATA_H_
+#include <math.h>
+
+float *generate_synthetic_data(float **rslt_mtrx, int **indr_mtrx, int *max_degree, float threshold, int N, int type);
+
+#endif
