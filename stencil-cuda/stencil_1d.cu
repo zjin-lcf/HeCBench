013c637ad9fbc878764ae11c10905f8b78e9646c
Jin Z <5zj@radeon.ftpn.ornl.gov>
2020-11-21 09:12:24 -0500

    add the hip codes and no memory leak when verification fails

--- stencil-cuda/stencil_1d.cu
+++ stencil-cuda/stencil_1d.cu
@@ -4,8 +4,8 @@
 
    The C model is added to verify the stencil result on a GPU
 
-   Developer: Zheming Jin
-*/
+Developer: Zheming Jin
+ */
 
 #define LENGTH 1024
 #define THREADS_PER_BLOCK 256
@@ -17,87 +17,90 @@
 #include <cuda.h>
 
 __global__ void stencil_1d(int *in, int *out) {
-    __shared__ int temp[BLOCK_SIZE + 2 * RADIUS];
-    int gindex = threadIdx.x + blockIdx.x * blockDim.x;
-    int lindex = threadIdx.x + RADIUS;
+  __shared__ int temp[BLOCK_SIZE + 2 * RADIUS];
+  int gindex = threadIdx.x + blockIdx.x * blockDim.x;
+  int lindex = threadIdx.x + RADIUS;
 
-    // Read input elements into shared memory
-    temp[lindex] = in[gindex];
+  // Read input elements into shared memory
+  temp[lindex] = in[gindex];
 
-    // At both end of a block, the sliding window moves beyond the block boundary.
-    if (threadIdx.x < RADIUS) {
-       temp[lindex - RADIUS] = (gindex < RADIUS) ? 0 : in[gindex - RADIUS];
-       temp[lindex + BLOCK_SIZE] = in[gindex + BLOCK_SIZE];
-    }
+  // At both end of a block, the sliding window moves beyond the block boundary.
+  if (threadIdx.x < RADIUS) {
+    temp[lindex - RADIUS] = (gindex < RADIUS) ? 0 : in[gindex - RADIUS];
+    temp[lindex + BLOCK_SIZE] = in[gindex + BLOCK_SIZE];
+  }
 
-    // Synchronize (ensure all the threads will be completed before continue)
-    __syncthreads();
+  // Synchronize (ensure all the threads will be completed before continue)
+  __syncthreads();
 
-    // Apply the 1D stencil
-    int result = 0;
-    for (int offset = -RADIUS ; offset <= RADIUS ; offset++)
-       result += temp[lindex + offset];
+  // Apply the 1D stencil
+  int result = 0;
+  for (int offset = -RADIUS ; offset <= RADIUS ; offset++)
+    result += temp[lindex + offset];
 
-    // Store the result
-    out[gindex] = result; 
+  // Store the result
+  out[gindex] = result; 
 }
 
 
 
 int main(void) {
-   int size = LENGTH * sizeof(int);
-   int pad_size = (LENGTH + RADIUS) * sizeof(int);
+  int size = LENGTH * sizeof(int);
+  int pad_size = (LENGTH + RADIUS) * sizeof(int);
 
-   int *a, *b;
-   // Alloc space for host copies of a, b, c and setup input values
-   a = (int *)malloc(pad_size); 
-   b = (int *)malloc(size);
+  int *a, *b;
+  // Alloc space for host copies of a, b, c and setup input values
+  a = (int *)malloc(pad_size); 
+  b = (int *)malloc(size);
 
-   for (int i = 0; i < LENGTH+RADIUS; i++) a[i] = i;
+  for (int i = 0; i < LENGTH+RADIUS; i++) a[i] = i;
 
-   int *d_a, *d_b;
-   // Alloc space for device copies of a, b, c
-   cudaMalloc((void **)&d_a, pad_size);
-   cudaMalloc((void **)&d_b, size);
+  int *d_a, *d_b;
+  // Alloc space for device copies of a, b, c
+  cudaMalloc((void **)&d_a, pad_size);
+  cudaMalloc((void **)&d_b, size);
 
-   // Copy inputs to device
-   cudaMemcpy(d_a, a, pad_size, cudaMemcpyHostToDevice);
+  // Copy inputs to device
+  cudaMemcpy(d_a, a, pad_size, cudaMemcpyHostToDevice);
 
-   // Launch add() kernel on GPU
-   stencil_1d <<< dim3(LENGTH/THREADS_PER_BLOCK), dim3(THREADS_PER_BLOCK) >>> (d_a, d_b);
+  // Launch add() kernel on GPU
+  stencil_1d <<< dim3(LENGTH/THREADS_PER_BLOCK), dim3(THREADS_PER_BLOCK) >>> (d_a, d_b);
 
-   // Copy result back to host
-   cudaMemcpy(b, d_b, size, cudaMemcpyDeviceToHost);
+  // Copy result back to host
+  cudaMemcpy(b, d_b, size, cudaMemcpyDeviceToHost);
 
-   // verification
-   for (int i = 0; i < 2*RADIUS; i++) {
-	   int s = 0;
-	   for (int j = i; j <= i+2*RADIUS; j++) {
-		   s += j < RADIUS ? 0 : (a[j] - RADIUS);
-	   }
-	   if (s != b[i]) {
-	   	printf("FAILED at %d: %d (cpu) != %d (gpu)\n", i, s, b[i]);
-		return 1;
-	   }
-   }
+  // verification
+  bool error = false;
+  for (int i = 0; i < 2*RADIUS; i++) {
+    int s = 0;
+    for (int j = i; j <= i+2*RADIUS; j++) {
+      s += j < RADIUS ? 0 : (a[j] - RADIUS);
+    }
+    if (s != b[i]) {
+      printf("FAILED at %d: %d (cpu) != %d (gpu)\n", i, s, b[i]);
+      error = true;
+      break;
+    }
+  }
 
-   for (int i = 2*RADIUS; i < LENGTH; i++) {
-	   int s = 0;
-	   for (int j = i-RADIUS; j <= i+RADIUS; j++) {
-		   s += a[j];
-	   }
-	   if (s != b[i]) {
-	   	printf("FAILED at %d: %d (cpu) != %d (gpu)\n", i, s, b[i]);
-		return 1;
-	   }
-   }
+  for (int i = 2*RADIUS; i < LENGTH; i++) {
+    int s = 0;
+    for (int j = i-RADIUS; j <= i+RADIUS; j++) {
+      s += a[j];
+    }
+    if (s != b[i]) {
+      printf("FAILED at %d: %d (cpu) != %d (gpu)\n", i, s, b[i]);
+      error = true;
+      break;
+    }
+  }
 
-   // Cleanup
-   free(a);
-   free(b); 
-   cudaFree(d_a); 
-   cudaFree(d_b); 
+  // Cleanup
+  free(a);
+  free(b); 
+  cudaFree(d_a); 
+  cudaFree(d_b); 
 
-   printf("PASSED\n");
-   return 0;
+  if (!error) printf("PASSED\n");
+  return 0;
 }
--- stencil-omp/stencil_1d.cpp
+++ stencil-omp/stencil_1d.cpp
@@ -4,7 +4,7 @@
 
    The C model is added to verify the stencil result on a GPU
 
-   Developer: Zheming Jin
+Developer: Zheming Jin
 */
 
 #define LENGTH 134217728
@@ -19,64 +19,67 @@
 
 
 int main(void) {
-   int size = LENGTH;
-   int pad_size = (LENGTH + RADIUS);
+  int size = LENGTH;
+  int pad_size = (LENGTH + RADIUS);
 
-   // Alloc space for host copies of a, b, c and setup input values
-   int* a = (int *)malloc(pad_size*sizeof(int)); 
-   int* b = (int *)malloc(size*sizeof(int));
+  // Alloc space for host copies of a, b, c and setup input values
+  int* a = (int *)malloc(pad_size*sizeof(int)); 
+  int* b = (int *)malloc(size*sizeof(int));
 
-   for (int i = 0; i < LENGTH+RADIUS; i++) a[i] = i;
+  for (int i = 0; i < LENGTH+RADIUS; i++) a[i] = i;
 
 #pragma omp target teams distribute map(to: a[0:pad_size]) map(from:b[0:size]) 
-   for (int i = 0; i < LENGTH; i = i + THREADS_PER_BLOCK) {
-     int temp[BLOCK_SIZE + 2 * RADIUS];
-     #pragma omp parallel for schedule(static,1)
-     for (int j = 0; j < THREADS_PER_BLOCK; j++) {
-       int gindex = i+j;
-       temp[j+RADIUS] = a[gindex]; 
-       if (j < RADIUS) {
-          temp[j] = (gindex < RADIUS) ? 0 : a[gindex - RADIUS];
-          temp[j + RADIUS + BLOCK_SIZE] = a[gindex + BLOCK_SIZE];
-       }
-     }
+  for (int i = 0; i < LENGTH; i = i + THREADS_PER_BLOCK) {
+    int temp[BLOCK_SIZE + 2 * RADIUS];
+#pragma omp parallel for schedule(static,1)
+    for (int j = 0; j < THREADS_PER_BLOCK; j++) {
+      int gindex = i+j;
+      temp[j+RADIUS] = a[gindex]; 
+      if (j < RADIUS) {
+        temp[j] = (gindex < RADIUS) ? 0 : a[gindex - RADIUS];
+        temp[j + RADIUS + BLOCK_SIZE] = a[gindex + BLOCK_SIZE];
+      }
+    }
 
-     #pragma omp parallel for schedule(static,1)
-     for (int j = 0; j < THREADS_PER_BLOCK; j++) {
-       int result = 0;
-       for (int offset = -RADIUS ; offset <= RADIUS ; offset++)
-          result += temp[j+RADIUS+offset];
-       b[i+j] = result; 
-     }
-   }
+#pragma omp parallel for schedule(static,1)
+    for (int j = 0; j < THREADS_PER_BLOCK; j++) {
+      int result = 0;
+      for (int offset = -RADIUS ; offset <= RADIUS ; offset++)
+        result += temp[j+RADIUS+offset];
+      b[i+j] = result; 
+    }
+  }
 
 
-   // verification
-   for (int i = 0; i < 2*RADIUS; i++) {
-	   int s = 0;
-	   for (int j = i; j <= i+2*RADIUS; j++) {
-		   s += j < RADIUS ? 0 : (a[j] - RADIUS);
-	   }
-	   if (s != b[i]) {
-	   	printf("FAILED at %d: %d (cpu) != %d (gpu)\n", i, s, b[i]);
-		return 1;
-	   }
-   }
+  // verification
+  bool error = false;
+  for (int i = 0; i < 2*RADIUS; i++) {
+    int s = 0;
+    for (int j = i; j <= i+2*RADIUS; j++) {
+      s += j < RADIUS ? 0 : (a[j] - RADIUS);
+    }
+    if (s != b[i]) {
+      printf("FAILED at %d: %d (cpu) != %d (gpu)\n", i, s, b[i]);
+      error = true;
+      break;
+    }
+  }
 
-   for (int i = 2*RADIUS; i < LENGTH; i++) {
-	   int s = 0;
-	   for (int j = i-RADIUS; j <= i+RADIUS; j++) {
-		   s += a[j];
-	   }
-	   if (s != b[i]) {
-	   	printf("FAILED at %d: %d (cpu) != %d (gpu)\n", i, s, b[i]);
-		return 1;
-	   }
-   }
+  for (int i = 2*RADIUS; i < LENGTH; i++) {
+    int s = 0;
+    for (int j = i-RADIUS; j <= i+RADIUS; j++) {
+      s += a[j];
+    }
+    if (s != b[i]) {
+      printf("FAILED at %d: %d (cpu) != %d (gpu)\n", i, s, b[i]);
+      error = true;
+      break;
+    }
+  }
 
-   // Cleanup
-   free(a);
-   free(b); 
-   printf("PASSED\n");
-   return 0;
+  // Cleanup
+  free(a);
+  free(b); 
+  if (!error) printf("PASSED\n");
+  return 0;
 }
