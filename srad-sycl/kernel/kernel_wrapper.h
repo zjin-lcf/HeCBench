82559d2674c3f0da715f6edcff4df6912bd3bda4
Jin Z <5zj@kold02.ftpn.ornl.gov>
2020-11-23 09:57:13 -0500

    the macro fp is replaced with FP due to a naming conflict using HipSYCL

--- srad-sycl/kernel/kernel_wrapper.h
+++ srad-sycl/kernel/kernel_wrapper.h
@@ -1,10 +1,10 @@
 void 
-kernel_wrapper(	fp* image,											// input image
+kernel_wrapper(	FP* image,											// input image
 		int Nr,												// IMAGE nbr of rows
 		int Nc,												// IMAGE nbr of cols
 		long Ne,											// IMAGE nbr of elem
 		int niter,											// nbr of iterations
-		fp lambda,											// update step size
+		FP lambda,											// update step size
 		long NeROI,											// ROI nbr of elements
 		int* iN,
 		int* iS,
