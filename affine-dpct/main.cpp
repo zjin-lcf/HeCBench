e6cd62d84d093a09561c6fd1d0422bee1f4cadc5
Jin Z <5zj@kold02.ftpn.ornl.gov>
2020-11-27 17:02:33 -0500

    Verify the kernel using a reference implementation. The max error should be at most 1

--- affine-dpct/main.cpp
+++ affine-dpct/main.cpp
@@ -35,6 +35,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <cstring>
+#include <cmath>
 #include <iostream>
 
 #define X_SIZE 512
@@ -130,6 +131,91 @@ void affine (const unsigned short *src, unsigned short *dst,
   dst[(y * X_SIZE)+x] = output_buffer[x];
 }
 
+// reference implementation for verification
+void affine_reference(const unsigned short *src, unsigned short *dst) 
+{
+  for (int y = 0; y < 512; y++) {
+    for (int x = 0; x < 512; x++) {
+      const float    lx_rot   = 30.0f;
+      const float    ly_rot   = 0.0f; 
+      const float    lx_expan = 0.5f;
+      const float    ly_expan = 0.5f; 
+      int      lx_move  = 0;
+      int      ly_move  = 0;
+      float    affine[2][2];   // coefficients
+      float    i_affine[2][2];
+      float    beta[2];
+      float    i_beta[2];
+      float    det;
+      float    x_new, y_new;
+      float    x_frac, y_frac;
+      float    gray_new;
+      int      m, n;
+
+      unsigned short    output_buffer[X_SIZE];
+
+
+      // forward affine transformation 
+      affine[0][0] = lx_expan * std::cosf((float)(lx_rot*PI/180.0f));
+      affine[0][1] = ly_expan * std::sinf((float)(ly_rot*PI/180.0f));
+      affine[1][0] = lx_expan * std::sinf((float)(lx_rot*PI/180.0f));
+      affine[1][1] = ly_expan * std::cosf((float)(ly_rot*PI/180.0f));
+      beta[0]      = lx_move;
+      beta[1]      = ly_move;
+
+      // determination of inverse affine transformation
+      det = (affine[0][0] * affine[1][1]) - (affine[0][1] * affine[1][0]);
+      if (det == 0.0f)
+      {
+        i_affine[0][0]   = 1.0f;
+        i_affine[0][1]   = 0.0f;
+        i_affine[1][0]   = 0.0f;
+        i_affine[1][1]   = 1.0f;
+        i_beta[0]        = -beta[0];
+        i_beta[1]        = -beta[1];
+      } 
+      else 
+      {
+        i_affine[0][0]   =  affine[1][1]/det;
+        i_affine[0][1]   = -affine[0][1]/det;
+        i_affine[1][0]   = -affine[1][0]/det;
+        i_affine[1][1]   =  affine[0][0]/det;
+        i_beta[0]        = -i_affine[0][0]*beta[0]-i_affine[0][1]*beta[1];
+        i_beta[1]        = -i_affine[1][0]*beta[0]-i_affine[1][1]*beta[1];
+      }
+
+      // Output image generation by inverse affine transformation and bilinear transformation
+
+      x_new    = i_beta[0] + i_affine[0][0]*(x-X_SIZE/2.0f) + i_affine[0][1]*(y-Y_SIZE/2.0f) + X_SIZE/2.0f;
+      y_new    = i_beta[1] + i_affine[1][0]*(x-X_SIZE/2.0f) + i_affine[1][1]*(y-Y_SIZE/2.0f) + Y_SIZE/2.0f;
+
+      m        = (int)std::floorf(x_new);
+      n        = (int)std::floorf(y_new);
+
+      x_frac   = x_new - m;
+      y_frac   = y_new - n;
+
+      if ((m >= 0) && (m + 1 < X_SIZE) && (n >= 0) && (n+1 < Y_SIZE))
+      {
+        gray_new = (1.0f - y_frac) * ((1.0f - x_frac) * (src[(n * X_SIZE) + m])       + x_frac * (src[(n * X_SIZE) + m + 1])) + 
+          y_frac  * ((1.0f - x_frac) * (src[((n + 1) * X_SIZE) + m]) + x_frac * (src[((n + 1) * X_SIZE) + m + 1]));
+
+        output_buffer[x] = (unsigned short)gray_new;
+      } 
+      else if (((m + 1 == X_SIZE) && (n >= 0) && (n < Y_SIZE)) || ((n + 1 == Y_SIZE) && (m >= 0) && (m < X_SIZE))) 
+      {
+        output_buffer[x] = src[(n * X_SIZE) + m];
+      } 
+      else 
+      {
+        output_buffer[x] = WHITE;
+      }
+
+      dst[(y * X_SIZE)+x] = output_buffer[x];
+    }
+  }
+}
+
 int main(int argc, char** argv)
 {
   dpct::device_ext &dev_ct1 = dpct::get_current_device();
@@ -143,6 +229,7 @@ int main(int argc, char** argv)
 
   unsigned short    input_image[Y_SIZE*X_SIZE] __attribute__((aligned(1024)));
   unsigned short    output_image[Y_SIZE*X_SIZE] __attribute__((aligned(1024)));
+  unsigned short    output_image_ref[Y_SIZE*X_SIZE] __attribute__((aligned(1024)));
 
   // Read the bit map file into memory and allocate memory for the final image
   std::cout << "Reading input image...\n";
@@ -198,6 +285,17 @@ int main(int argc, char** argv)
       .wait();
   sycl::free(d_input_image, q_ct1);
   sycl::free(d_output_image, q_ct1);
+
+  // verify
+  affine_reference(input_image, output_image_ref);
+  int max_error = 0;
+  for (int y = 0; y < 512; y++) {
+    for (int x = 0; x < 512; x++) {
+      max_error = std::max(max_error, std::abs(output_image[y*512+x] - output_image_ref[y*512+x]));
+    }
+  }
+  printf("   Max output error is %d\n\n", max_error);
+
 
   printf("   Writing RAW Image\n");
   const char *outputImageFilename = argv[2];
