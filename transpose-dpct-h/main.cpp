6833548b822e19e90855817aa17e897f61d08a08
u38534 <u38534@s001-n141.aidevcloud>
2020-11-30 05:49:35 -0800

    Free memory when verification fails

--- transpose-dpct-h/main.cpp
+++ transpose-dpct-h/main.cpp
@@ -1,6 +1,7 @@
 #define DPCT_USM_LEVEL_NONE
 #include <CL/sycl.hpp>
 #include <dpct/dpct.hpp>
+#include <cstdio>
 
 #define TILE_SIZE 5900
 #define NTHREADS 256
@@ -22,26 +23,27 @@ void verify(double *input, double *output) {
   int input_offset  = 2 + d1 * (2 + d2 * (2 + d3 * (2 + d4 * (0 + 2 * d5))));
   int output_offset = 2 + d2 * (2 + d3 * (2 + d4 * (2 + d6 * (2 + 0 * d1))));
   for (size_t i = 0; i < d5; i++) {
-    if (input[input_offset + i * d1 * d2 * d3 * d4] != output[output_offset + i * d2 * d3 * d4 * d6 * d1]) {
-      printf("Failed!\n");
-      exit(-1);
+    if (input[input_offset + i * d1 * d2 * d3 * d4] != 
+        output[output_offset + i * d2 * d3 * d4 * d6 * d1]) {
+      printf("Failed\n");
+      break;
     }
   }
 }
 
-  
-void tensor_transpose(int dim_input, 
-    int dim_output, 
-    int nblocks, 
-    int tile_size,
-    int *shape_input, 
-    int *shape_output, 
-    float *shape_input_r, 
-    float *shape_output_r, 
-    int *stride_input,
-    int *stride_output_local, 
-    int *stride_output_global,
-    double *input, 
+void tensor_transpose(
+    const int dim_input, 
+    const int dim_output, 
+    const int nblocks, 
+    const int tile_size,
+    const int *shape_input, 
+    const int *shape_output, 
+    const float *shape_input_r, 
+    const float *shape_output_r, 
+    const int *stride_input,
+    const int *stride_output_local, 
+    const int *stride_output_global,
+    const double *input, 
     double *output,
     sycl::nd_item<3> item_ct1,
     double *tile) 
@@ -51,7 +53,7 @@ void tensor_transpose(int dim_input,
        block_idx += item_ct1.get_group_range(2)) {
     int it = block_idx, im = 0, offset1 = 0;
     for (int i = 0; i < dim_input; i++) {
-      im = it * shape_input_r[i];
+      im = it * shape_input_r[i];  // replace division with multiplication
       offset1 += stride_input[i] * (it - im * shape_input[i]);
       it = im;
     }
@@ -68,7 +70,7 @@ void tensor_transpose(int dim_input,
       it = i;
       int offset2 = 0, local_offset = 0;
       for (int j = 0; j < dim_output; j++) {
-        im = it * shape_output_r[j];
+        im = it * shape_output_r[j];  // replace division with multiplication
         int tmp = it - im * shape_output[j];
         offset2 += stride_output_global[j] * tmp;
         local_offset += stride_output_local[j] * tmp;
@@ -97,86 +99,80 @@ int main(int argv, char **argc) {
   const int tile_size = d1 * d2 * d3;
   const int dim_output = 3;
   const int dim_input = 3;
-  double *device_output, *device_input;
-  int *device_shape_input, *device_shape_output;
-  float *device_shape_input_r, *device_shape_output_r;
-  int *device_stride_output_local, *device_stride_output_global;
-  int *device_stride_input;
+  double *d_output, *d_input;
+  int *d_shape_input, *d_shape_output;
+  float *d_shape_input_r, *d_shape_output_r;
+  int *d_stride_output_local, *d_stride_output_global;
+  int *d_stride_input;
 
-  dpct::dpct_malloc(&device_output, data_size * sizeof(double));
-  dpct::dpct_malloc(&device_input, data_size * sizeof(double));
-  dpct::dpct_malloc(&device_shape_input, dim_input * sizeof(int));
-  dpct::dpct_malloc(&device_shape_input_r, dim_input * sizeof(float));
-  dpct::dpct_malloc(&device_shape_output, dim_output * sizeof(int));
-  dpct::dpct_malloc(&device_shape_output_r, dim_output * sizeof(float));
-  dpct::dpct_malloc(&device_stride_input, dim_input * sizeof(int));
-  dpct::dpct_malloc(&device_stride_output_local, dim_output * sizeof(int));
-  dpct::dpct_malloc(&device_stride_output_global, dim_output * sizeof(int));
+  dpct::dpct_malloc(&d_output, data_size * sizeof(double));
+  dpct::dpct_malloc(&d_input, data_size * sizeof(double));
+  dpct::dpct_malloc(&d_shape_input, dim_input * sizeof(int));
+  dpct::dpct_malloc(&d_shape_input_r, dim_input * sizeof(float));
+  dpct::dpct_malloc(&d_shape_output, dim_output * sizeof(int));
+  dpct::dpct_malloc(&d_shape_output_r, dim_output * sizeof(float));
+  dpct::dpct_malloc(&d_stride_input, dim_input * sizeof(int));
+  dpct::dpct_malloc(&d_stride_output_local, dim_output * sizeof(int));
+  dpct::dpct_malloc(&d_stride_output_global, dim_output * sizeof(int));
 
-  dpct::dpct_memcpy(device_input, input, data_size * sizeof(double),
+  dpct::dpct_memcpy(d_input, input, data_size * sizeof(double),
                     dpct::host_to_device);
-  dpct::dpct_memcpy(device_shape_input, shape_input, dim_input * sizeof(int),
+  dpct::dpct_memcpy(d_shape_input, shape_input, dim_input * sizeof(int),
                     dpct::host_to_device);
-  dpct::dpct_memcpy(device_shape_input_r, shape_input_r,
-                    dim_input * sizeof(float), dpct::host_to_device);
-  dpct::dpct_memcpy(device_shape_output, shape_output, dim_output * sizeof(int),
+  dpct::dpct_memcpy(d_shape_input_r, shape_input_r, dim_input * sizeof(float),
                     dpct::host_to_device);
-  dpct::dpct_memcpy(device_shape_output_r, shape_output_r,
+  dpct::dpct_memcpy(d_shape_output, shape_output, dim_output * sizeof(int),
+                    dpct::host_to_device);
+  dpct::dpct_memcpy(d_shape_output_r, shape_output_r,
                     dim_output * sizeof(float), dpct::host_to_device);
-  dpct::dpct_memcpy(device_stride_input, stride_input, dim_input * sizeof(int),
+  dpct::dpct_memcpy(d_stride_input, stride_input, dim_input * sizeof(int),
                     dpct::host_to_device);
-  dpct::dpct_memcpy(device_stride_output_local, stride_output_local,
+  dpct::dpct_memcpy(d_stride_output_local, stride_output_local,
                     dim_output * sizeof(int), dpct::host_to_device);
-  dpct::dpct_memcpy(device_stride_output_global, stride_output_global,
+  dpct::dpct_memcpy(d_stride_output_global, stride_output_global,
                     dim_output * sizeof(int), dpct::host_to_device);
 
   for (size_t i = 0; i < ITER; ++i) {
-    dpct::buffer_t device_shape_input_buf_ct4 =
-        dpct::get_buffer(device_shape_input);
-    dpct::buffer_t device_shape_output_buf_ct5 =
-        dpct::get_buffer(device_shape_output);
-    dpct::buffer_t device_shape_input_r_buf_ct6 =
-        dpct::get_buffer(device_shape_input_r);
-    dpct::buffer_t device_shape_output_r_buf_ct7 =
-        dpct::get_buffer(device_shape_output_r);
-    dpct::buffer_t device_stride_input_buf_ct8 =
-        dpct::get_buffer(device_stride_input);
-    dpct::buffer_t device_stride_output_local_buf_ct9 =
-        dpct::get_buffer(device_stride_output_local);
-    dpct::buffer_t device_stride_output_global_buf_ct10 =
-        dpct::get_buffer(device_stride_output_global);
-    dpct::buffer_t device_input_buf_ct11 = dpct::get_buffer(device_input);
-    dpct::buffer_t device_output_buf_ct12 = dpct::get_buffer(device_output);
+    dpct::buffer_t d_shape_input_buf_ct4 = dpct::get_buffer(d_shape_input);
+    dpct::buffer_t d_shape_output_buf_ct5 = dpct::get_buffer(d_shape_output);
+    dpct::buffer_t d_shape_input_r_buf_ct6 = dpct::get_buffer(d_shape_input_r);
+    dpct::buffer_t d_shape_output_r_buf_ct7 =
+        dpct::get_buffer(d_shape_output_r);
+    dpct::buffer_t d_stride_input_buf_ct8 = dpct::get_buffer(d_stride_input);
+    dpct::buffer_t d_stride_output_local_buf_ct9 =
+        dpct::get_buffer(d_stride_output_local);
+    dpct::buffer_t d_stride_output_global_buf_ct10 =
+        dpct::get_buffer(d_stride_output_global);
+    dpct::buffer_t d_input_buf_ct11 = dpct::get_buffer(d_input);
+    dpct::buffer_t d_output_buf_ct12 = dpct::get_buffer(d_output);
     dpct::get_default_queue().submit([&](sycl::handler &cgh) {
       sycl::accessor<double, 1, sycl::access::mode::read_write,
                      sycl::access::target::local>
           tile_acc_ct1(sycl::range<1>(5900 /*TILE_SIZE*/), cgh);
-      auto device_shape_input_acc_ct4 =
-          device_shape_input_buf_ct4.get_access<sycl::access::mode::read_write>(
+      auto d_shape_input_acc_ct4 =
+          d_shape_input_buf_ct4.get_access<sycl::access::mode::read_write>(cgh);
+      auto d_shape_output_acc_ct5 =
+          d_shape_output_buf_ct5.get_access<sycl::access::mode::read_write>(
               cgh);
-      auto device_shape_output_acc_ct5 =
-          device_shape_output_buf_ct5
-              .get_access<sycl::access::mode::read_write>(cgh);
-      auto device_shape_input_r_acc_ct6 =
-          device_shape_input_r_buf_ct6
-              .get_access<sycl::access::mode::read_write>(cgh);
-      auto device_shape_output_r_acc_ct7 =
-          device_shape_output_r_buf_ct7
-              .get_access<sycl::access::mode::read_write>(cgh);
-      auto device_stride_input_acc_ct8 =
-          device_stride_input_buf_ct8
-              .get_access<sycl::access::mode::read_write>(cgh);
-      auto device_stride_output_local_acc_ct9 =
-          device_stride_output_local_buf_ct9
-              .get_access<sycl::access::mode::read_write>(cgh);
-      auto device_stride_output_global_acc_ct10 =
-          device_stride_output_global_buf_ct10
-              .get_access<sycl::access::mode::read_write>(cgh);
-      auto device_input_acc_ct11 =
-          device_input_buf_ct11.get_access<sycl::access::mode::read_write>(cgh);
-      auto device_output_acc_ct12 =
-          device_output_buf_ct12.get_access<sycl::access::mode::read_write>(
+      auto d_shape_input_r_acc_ct6 =
+          d_shape_input_r_buf_ct6.get_access<sycl::access::mode::read_write>(
               cgh);
+      auto d_shape_output_r_acc_ct7 =
+          d_shape_output_r_buf_ct7.get_access<sycl::access::mode::read_write>(
+              cgh);
+      auto d_stride_input_acc_ct8 =
+          d_stride_input_buf_ct8.get_access<sycl::access::mode::read_write>(
+              cgh);
+      auto d_stride_output_local_acc_ct9 =
+          d_stride_output_local_buf_ct9
+              .get_access<sycl::access::mode::read_write>(cgh);
+      auto d_stride_output_global_acc_ct10 =
+          d_stride_output_global_buf_ct10
+              .get_access<sycl::access::mode::read_write>(cgh);
+      auto d_input_acc_ct11 =
+          d_input_buf_ct11.get_access<sycl::access::mode::read_write>(cgh);
+      auto d_output_acc_ct12 =
+          d_output_buf_ct12.get_access<sycl::access::mode::read_write>(cgh);
 
       cgh.parallel_for(sycl::nd_range<3>(sycl::range<3>(1, 1, nblocks) *
                                              sycl::range<3>(1, 1, NTHREADS),
@@ -184,37 +180,35 @@ int main(int argv, char **argc) {
                        [=](sycl::nd_item<3> item_ct1) {
                          tensor_transpose(
                              dim_input, dim_output, nblocks, tile_size,
-                             (int *)(&device_shape_input_acc_ct4[0]),
-                             (int *)(&device_shape_output_acc_ct5[0]),
-                             (float *)(&device_shape_input_r_acc_ct6[0]),
-                             (float *)(&device_shape_output_r_acc_ct7[0]),
-                             (int *)(&device_stride_input_acc_ct8[0]),
-                             (int *)(&device_stride_output_local_acc_ct9[0]),
-                             (int *)(&device_stride_output_global_acc_ct10[0]),
-                             (double *)(&device_input_acc_ct11[0]),
-                             (double *)(&device_output_acc_ct12[0]), item_ct1,
+                             (const int *)(&d_shape_input_acc_ct4[0]),
+                             (const int *)(&d_shape_output_acc_ct5[0]),
+                             (const float *)(&d_shape_input_r_acc_ct6[0]),
+                             (const float *)(&d_shape_output_r_acc_ct7[0]),
+                             (const int *)(&d_stride_input_acc_ct8[0]),
+                             (const int *)(&d_stride_output_local_acc_ct9[0]),
+                             (const int *)(&d_stride_output_global_acc_ct10[0]),
+                             (const double *)(&d_input_acc_ct11[0]),
+                             (double *)(&d_output_acc_ct12[0]), item_ct1,
                              tile_acc_ct1.get_pointer());
                        });
     });
   }
 
-  dpct::dpct_memcpy(output, device_output, data_size * sizeof(double),
+  dpct::dpct_memcpy(output, d_output, data_size * sizeof(double),
                     dpct::device_to_host);
-
-  dpct::dpct_free(device_output);
-  dpct::dpct_free(device_input);
-  dpct::dpct_free(device_shape_input);
-  dpct::dpct_free(device_shape_input_r);
-  dpct::dpct_free(device_shape_output);
-  dpct::dpct_free(device_shape_output_r);
-  dpct::dpct_free(device_stride_input);
-  dpct::dpct_free(device_stride_output_local);
-  dpct::dpct_free(device_stride_output_global);
+  dpct::dpct_free(d_output);
+  dpct::dpct_free(d_input);
+  dpct::dpct_free(d_shape_input);
+  dpct::dpct_free(d_shape_input_r);
+  dpct::dpct_free(d_shape_output);
+  dpct::dpct_free(d_shape_output_r);
+  dpct::dpct_free(d_stride_input);
+  dpct::dpct_free(d_stride_output_local);
+  dpct::dpct_free(d_stride_output_global);
 
   verify(input, output);
 
   delete [] input;
   delete [] output;
-
   return 0;
 }
