0f7acd40952540c896fbbffec1784bd9151b8d52
Chameleon Cloud User <cc@lp-xeon-test.novalocal>
2020-12-30 20:20:57 +0000

    Add the dpct and openmp codes

---
+++ extrema-omp/main.cpp
@@ -0,0 +1,271 @@
+// Copyright (c) 2019-2020, NVIDIA CORPORATION.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma omp declare target
+inline void clip_plus( const bool &clip, const int &n, int &plus ) {
+  if ( clip ) {
+    if ( plus >= n ) {
+      plus = n - 1;
+    }
+  } else {
+    if ( plus >= n ) {
+      plus -= n;
+    }
+  }
+}
+#pragma omp end declare target
+
+#pragma omp declare target
+inline void clip_minus( const bool &clip, const int &n, int &minus ) {
+  if ( clip ) {
+    if ( minus < 0 ) {
+      minus = 0;
+    }
+  } else {
+    if ( minus < 0 ) {
+      minus += n;
+    }
+  }
+}
+#pragma omp end declare target
+
+///////////////////////////////////////////////////////////////////////////////
+//                          BOOLRELEXTREMA 1D                                //
+///////////////////////////////////////////////////////////////////////////////
+
+  template<typename T>
+void cpu_relextrema_1D( const int  n,
+    const int  order,
+    const bool clip,
+    const T * inp,
+    bool * results)
+{
+
+  for ( int tid = 0; tid < n; tid++ ) {
+
+    const T data = inp[tid];
+    bool    temp = true;
+
+    for ( int o = 1; o < ( order + 1 ); o++ ) {
+      int plus = tid + o;
+      int minus = tid - o;
+
+      clip_plus( clip, n, plus );
+      clip_minus( clip, n, minus );
+
+      temp &= data > inp[plus];
+      temp &= data >= inp[minus];
+    }
+    results[tid] = temp;
+  }
+}
+
+
+
+  template<typename T>
+void cpu_relextrema_2D( const int  in_x,
+    const int  in_y,
+    const int  order,
+    const bool clip,
+    const int  axis,
+    const T * inp,
+    bool * results) 
+{
+  for (int tx = 0; tx < in_y; tx++)
+    for (int ty = 0; ty < in_x; ty++) {
+
+      int tid = tx * in_x + ty ;
+
+      const T data = inp[tid] ;
+      bool    temp = true ;
+
+      for ( int o = 1; o < ( order + 1 ); o++ ) {
+
+        int plus;
+        int minus;
+
+        if ( axis == 0 ) {
+          plus  = tx + o;
+          minus = tx - o;
+
+          clip_plus( clip, in_y, plus );
+          clip_minus( clip, in_y, minus );
+
+          plus  = plus * in_x + ty;
+          minus = minus * in_x + ty;
+        } else {
+          plus  = ty + o;
+          minus = ty - o;
+
+          clip_plus( clip, in_x, plus );
+          clip_minus( clip, in_x, minus );
+
+          plus  = tx * in_x + plus;
+          minus = tx * in_x + minus;
+        }
+
+        temp &= data > inp[plus] ;
+        temp &= data >= inp[minus] ;
+      }
+      results[tid] = temp;
+    }
+}
+
+  template <typename T>
+void test_1D (const int length, 
+    const int order, const bool clip)
+{
+  T* inp = (T*) malloc (sizeof(T)*length);
+  for (int i = 0; i < length; i++)
+    inp[i] = rand() % length;
+
+  bool* cpu_r = (bool*) malloc (sizeof(bool)*length);
+  bool* gpu_r = (bool*) malloc (sizeof(bool)*length);
+
+#pragma omp target data map(to: inp[0:length]) map(from: gpu_r[0:length])
+  {
+
+    for (int n = 0; n < 100; n++) {
+#pragma omp target teams distribute parallel for thread_limit(256)
+      for (int tid = 0; tid < length; tid++) {
+        const T data = inp[tid];
+        bool    temp = true;
+
+        for ( int o = 1; o < ( order + 1 ); o++ ) {
+          int plus = tid + o;
+          int minus = tid - o;
+
+          clip_plus( clip, length, plus );
+          clip_minus( clip, length, minus );
+
+          temp &= data > inp[plus];
+          temp &= data >= inp[minus];
+        }
+        gpu_r[tid] = temp;
+      }
+    }
+  }
+
+  cpu_relextrema_1D<T>(length, order, clip, inp, cpu_r);
+
+  int error = 0;
+  for (int i = 0; i < length; i++)
+    if (cpu_r[i] != gpu_r[i]) {
+      error = 1; 
+      break;
+    }
+
+  free(inp);
+  free(cpu_r);
+  free(gpu_r);
+  if (error) printf("1D test: FAILED\n");
+}
+
+// length_x is the number of columns
+// length_y is the number of rows
+  template <typename T>
+void test_2D (const int length_x, const int length_y, 
+    const int order, const bool clip, const int axis) 
+{
+  const int length = length_x * length_y;
+  T* inp = (T*) malloc (sizeof(T)*length);
+  for (int i = 0; i < length; i++)
+    inp[i] = rand() % length;
+
+  bool* cpu_r = (bool*) malloc (sizeof(bool)*length);
+  bool* gpu_r = (bool*) malloc (sizeof(bool)*length);
+
+#pragma omp target data map(to: inp[0:length]) map(from: gpu_r[0:length])
+  {
+    for (int n = 0; n < 100; n++)  {
+#pragma omp target teams distribute parallel for collapse(2) thread_limit(256)
+      for (int tx = 0; tx < length_y; tx++)
+        for (int ty = 0; ty < length_x; ty++) {
+          int tid = tx * length_x + ty ;
+          const T data = inp[tid] ;
+          bool    temp = true ;
+
+          for ( int o = 1; o < ( order + 1 ); o++ ) {
+            int plus;
+            int minus;
+            if ( axis == 0 ) {
+              plus  = tx + o;
+              minus = tx - o;
+
+              clip_plus( clip, length_y, plus );
+              clip_minus( clip, length_y, minus );
+
+              plus  = plus * length_x + ty;
+              minus = minus * length_x + ty;
+            } else {
+              plus  = ty + o;
+              minus = ty - o;
+
+              clip_plus( clip, length_x, plus );
+              clip_minus( clip, length_x, minus );
+
+              plus  = tx * length_x + plus;
+              minus = tx * length_x + minus;
+            }
+            temp &= data > inp[plus] ;
+            temp &= data >= inp[minus] ;
+          }
+          gpu_r[tid] = temp;
+        }
+    }
+  }
+
+  cpu_relextrema_2D(length_x, length_y, order, clip, axis, inp, cpu_r);
+
+  int error = 0;
+  for (int i = 0; i < length; i++)
+    if (cpu_r[i] != gpu_r[i]) {
+      error = 1; 
+      break;
+    }
+
+  free(inp);
+  free(cpu_r);
+  free(gpu_r);
+  if (error) printf("2D test: FAILED\n");
+}
+
+int main() {
+
+  for (int order = 1; order <= 128; order = order * 2) {
+    test_1D<int>(1000000, order, true);
+    test_1D<long>(1000000, order, true);
+    test_1D<float>(1000000, order, true);
+    test_1D<double>(1000000, order, true);
+  }
+
+  for (int order = 1; order <= 128; order = order * 2) {
+    test_2D<int>(1000, 1000, order, true, 1);
+    test_2D<long>(1000, 1000, order, true, 1);
+    test_2D<float>(1000, 1000, order, true, 1);
+    test_2D<double>(1000, 1000, order, true, 1);
+  }
+
+  for (int order = 1; order <= 128; order = order * 2) {
+    test_2D<int>(1000, 1000, order, true, 0);
+    test_2D<long>(1000, 1000, order, true, 0);
+    test_2D<float>(1000, 1000, order, true, 0);
+    test_2D<double>(1000, 1000, order, true, 0);
+  }
+
+  return 0;
+}
+
