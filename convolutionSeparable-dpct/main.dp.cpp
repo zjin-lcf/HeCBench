91f01717d398e24b0fbd56a6c37eadbe2885ea81
u58480 <u58480@login-2.aidevcloud>
2021-03-08 04:43:02 -0800

    Add the DPCT versions using Gold. Beta8 DPCT does not generate codes completely. Update the pointer arithmetic in the OMP version

---
+++ convolutionSeparable-dpct/main.dp.cpp
@@ -0,0 +1,126 @@
+/*
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+#include <CL/sycl.hpp>
+#include <dpct/dpct.hpp>
+#include <stdio.h>
+#include <math.h>
+#include "conv.h"
+
+int main(int argc, char **argv)
+{
+  dpct::device_ext &dev_ct1 = dpct::get_current_device();
+  sycl::queue &q_ct1 = dev_ct1.default_queue();
+  const unsigned int imageW = 3072;
+  const unsigned int imageH = 3072;
+
+  float* h_Kernel    = (float*)malloc(KERNEL_LENGTH * sizeof(float));
+  float* h_Input     = (float*)malloc(imageW * imageH * sizeof(float));
+  float* h_Buffer    = (float*)malloc(imageW * imageH * sizeof(float));
+  float* h_OutputCPU = (float*)malloc(imageW * imageH * sizeof(float));
+  float* h_OutputGPU = (float*)malloc(imageW * imageH * sizeof(float));
+
+  srand(2009);
+  for(unsigned int i = 0; i < KERNEL_LENGTH; i++)
+    h_Kernel[i] = (float)(rand() % 16);
+
+  for(unsigned int i = 0; i < imageW * imageH; i++)
+    h_Input[i] = (float)(rand() % 16);
+
+
+  float* d_Kernel;
+  d_Kernel = sycl::malloc_device<float>(KERNEL_LENGTH, q_ct1);
+  q_ct1.memcpy(d_Kernel, h_Kernel, sizeof(float) * KERNEL_LENGTH).wait();
+
+  float* d_Input;
+  d_Input =
+      (float *)sycl::malloc_device(sizeof(float) * imageW * imageH, q_ct1);
+  q_ct1.memcpy(d_Input, h_Input, sizeof(float) * imageW * imageH).wait();
+
+  float* d_Buffer;
+  d_Buffer =
+      (float *)sycl::malloc_device(sizeof(float) * imageW * imageH, q_ct1);
+
+  float* d_Output;
+  d_Output =
+      (float *)sycl::malloc_device(sizeof(float) * imageW * imageH, q_ct1);
+
+  //Just a single run or a warmup iteration
+  convolutionRows(
+      d_Buffer,
+      d_Input,
+      d_Kernel,
+      imageW,
+      imageH,
+      imageW
+           );
+
+  convolutionColumns(
+      d_Output,
+      d_Buffer,
+      d_Kernel,
+      imageW,
+      imageH,
+      imageW
+      );
+
+  const int numIterations = 100;
+
+  for(int iter = 0; iter < numIterations; iter++){
+    convolutionRows(
+        d_Buffer,
+        d_Input,
+        d_Kernel,
+        imageW,
+        imageH,
+        imageW
+             );
+
+    convolutionColumns(
+        d_Output,
+        d_Buffer,
+        d_Kernel,
+        imageW,
+        imageH,
+        imageW
+        );
+  }
+
+  q_ct1.memcpy(h_OutputGPU, d_Output, sizeof(float) * imageW * imageH).wait();
+
+  printf("Comparing against Host/C++ computation...\n"); 
+  convolutionRowHost(h_Buffer, h_Input, h_Kernel, imageW, imageH, KERNEL_RADIUS);
+  convolutionColumnHost(h_OutputCPU, h_Buffer, h_Kernel, imageW, imageH, KERNEL_RADIUS);
+  double sum = 0, delta = 0;
+  double L2norm;
+  for(unsigned int i = 0; i < imageW * imageH; i++){
+    delta += (h_OutputCPU[i] - h_OutputGPU[i]) * (h_OutputCPU[i] - h_OutputGPU[i]);
+    sum += h_OutputCPU[i] * h_OutputCPU[i];
+  }
+  L2norm = sqrt(delta / sum);
+  printf("Relative L2 norm: %.3e\n\n", L2norm);
+
+  free(h_OutputGPU);
+  free(h_OutputCPU);
+  free(h_Buffer);
+  free(h_Input);
+  free(h_Kernel);
+  sycl::free(d_Kernel, q_ct1);
+  sycl::free(d_Input, q_ct1);
+  sycl::free(d_Buffer, q_ct1);
+  sycl::free(d_Output, q_ct1);
+
+  if (L2norm < 1e-6)
+    printf("PASS\n"); 
+  else
+    printf("FAIL\n");
+
+  return 0;
+}
