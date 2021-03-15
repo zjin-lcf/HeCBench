df56ea988de3190657312c9700493cbf8b4697cc
u58480 <u58480@s001-n183.aidevcloud>
2021-03-15 06:48:14 -0700

    remove the CUDA arch definitions

--- fdtd3d-omp/shrUtils.h
+++ fdtd3d-omp/shrUtils.h
@@ -48,41 +48,6 @@
 // Un-comment the following #define to enable profiling code in SDK apps
 //#define GPU_PROFILING
 
-// Beginning of GPU Architecture definitions
-inline int ConvertSMVer2Cores(int major, int minor)
-{
-	// Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
-	typedef struct {
-		int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
-		int Cores;
-	} sSMtoCores;
-
-	sSMtoCores nGpuArchCoresPerSM[] = 
-	{ { 0x10,  8 }, // Tesla Generation (SM 1.0) G80 class
-	  { 0x11,  8 }, // Tesla Generation (SM 1.1) G8x class
-	  { 0x12,  8 }, // Tesla Generation (SM 1.2) G9x class
-	  { 0x13,  8 }, // Tesla Generation (SM 1.3) GT200 class
-	  { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
-	  { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
-	  { 0x30, 192}, // Kepler Generation (SM 3.0) GK10x class
-	  { 0x35, 192}, // Kepler Generation (SM 3.5) GK11x class
-	  {   -1, -1 }
-	};
-
-	int index = 0;
-	while (nGpuArchCoresPerSM[index].SM != -1) {
-		if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor) ) {
-			return nGpuArchCoresPerSM[index].Cores;
-		}
-		index++;
-	}
-    // If we don't find the values, we default use the previous one to run properly
-    printf("MapSMtoCores for SM %d.%d is undefined.  Default to use %d Cores/SM\n", major, minor, nGpuArchCoresPerSM[7].Cores);
-    return nGpuArchCoresPerSM[7].Cores;
-}
-// end of GPU Architecture definitions
-
-
 // Defines and enum for use with logging functions
 // *********************************************************************
 #define DEFAULTLOGFILE "SdkConsoleLog.txt"
