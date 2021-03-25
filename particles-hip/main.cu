cf50dc4b87deee2fa7b46a005dc433da5ac8f50c
Jin Z <5zj@leconte.ftpn.ornl.gov>
2021-03-25 10:23:33 -0400

    Update the HIP version

---
+++ particles-hip/main.cu
@@ -0,0 +1,234 @@
+/*
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+#define MAX_EPSILON_ERROR 5.00f
+#define THRESHOLD         0.30f
+#define GRID_SIZE         64
+#define NUM_PARTICLES     16384
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <hip/hip_runtime.h>
+#include "particles.h"
+
+// Simulation parameters
+const float timestep = 0.5f;              // Time slice for re-computation iteration
+//const float gravity = 0.0005f;            // Strength of gravity
+//const float damping = 1.0f;
+const float fParticleRadius = 0.023f;     // Radius of individual particles
+const float fColliderRadius = 0.17f;      // Radius of collider for interacting with particles in 'm' mode
+//const float collideSpring = 0.4f;         // Elastic spring constant for impact between particles
+//const float collideDamping = 0.025f;      // Inelastic loss component for impact between particles
+//const float collideShear = 0.12f;         // Friction constant for particles in contact
+//const float collideAttraction = 0.0012f;  // Attraction between particles (~static or Van der Waals) 
+
+
+inline float frand(void){
+  return (float)rand() / (float)RAND_MAX;
+}
+
+void initGrid(float *hPos, float *hVel, float particleRadius, float spacing, 
+    unsigned int numParticles)
+{
+  float jitter = particleRadius * 0.01f;
+  unsigned int s = (int) ceilf(powf((float) numParticles, 1.0f / 3.0f));
+  unsigned int gridSize[3];
+  gridSize[0] = gridSize[1] = gridSize[2] = s;
+
+  srand(1973);
+  for(unsigned int z=0; z<gridSize[2]; z++) 
+  {
+    for(unsigned int y=0; y<gridSize[1]; y++) 
+    {
+      for(unsigned int x=0; x<gridSize[0]; x++) 
+      {
+        unsigned int i = (z * gridSize[0] * gridSize[1]) + (y * gridSize[1]) + x;
+        if (i < numParticles) 
+        {
+          hPos[i * 4] = (spacing * x) + particleRadius - 1.0f + (frand() * 2.0f - 1.0f) * jitter;
+          hPos[i * 4 + 1] = (spacing * y) + particleRadius - 1.0f + (frand() * 2.0f - 1.0f) * jitter;
+          hPos[i * 4 + 2] = (spacing * z) + particleRadius - 1.0f + (frand() * 2.0f - 1.0f) * jitter;
+          hPos[i * 4 + 3] = 1.0f;
+          hVel[i * 4] = 0.0f;
+          hVel[i * 4 + 1] = 0.0f;
+          hVel[i * 4 + 2] = 0.0f;
+          hVel[i * 4 + 3] = 0.0f;
+        }
+      }
+    }
+  }
+}
+
+
+
+// Main program
+//*****************************************************************************
+int main(int argc, char** argv) 
+{
+  const int iterations = atoi(argv[1]);               // Number of iterations
+  unsigned int numParticles = NUM_PARTICLES;
+  unsigned int gridDim = GRID_SIZE;
+
+  // Set and log grid size and particle count, after checking optional command-line inputs
+  uint3 gridSize;
+  gridSize.x = gridSize.y = gridSize.z = gridDim;
+
+  unsigned int numGridCells = gridSize.x * gridSize.y * gridSize.z;
+  //float3 worldSize = {2.0f, 2.0f, 2.0f};
+
+  // set simulation parameters
+  simParams_t params;
+  params.gridSize = gridSize;
+  params.numCells = numGridCells;
+  params.numBodies = numParticles;
+  params.particleRadius = fParticleRadius; 
+  params.colliderPos = {1.2f, -0.8f, 0.8f};
+  params.colliderRadius = fColliderRadius;
+
+  params.worldOrigin = {1.0f, -1.0f, -1.0f};
+  float cellSize = params.particleRadius * 2.0f;  // cell size equal to particle diameter
+  params.cellSize = {cellSize, cellSize, cellSize};
+
+  params.spring = 0.5f;
+  params.damping = 0.02f;
+  params.shear = 0.1f;
+  params.attraction = 0.0f;
+  params.boundaryDamping = -0.5f;
+
+  params.gravity = {0.0f, -0.0003f, 0.0f};
+  params.globalDamping = 1.0f;
+
+  printf(" grid: %d x %d x %d = %d cells\n", gridSize.x, gridSize.y, gridSize.z, numGridCells);
+  printf(" particles: %d\n\n", numParticles);
+
+  float* hPos          = (float*)malloc(numParticles * 4 * sizeof(float));
+  float* hVel          = (float*)malloc(numParticles * 4 * sizeof(float));
+  float* hReorderedPos = (float*)malloc(numParticles * 4 * sizeof(float));
+  float* hReorderedVel = (float*)malloc(numParticles * 4 * sizeof(float));
+  unsigned int* hHash      = (unsigned int*)malloc(numParticles * sizeof(unsigned int));
+  unsigned int* hIndex     = (unsigned int*)malloc(numParticles * sizeof(unsigned int));
+  unsigned int* hCellStart = (unsigned int*)malloc(numGridCells * sizeof(unsigned int));
+  unsigned int* hCellEnd   = (unsigned int*)malloc(numGridCells * sizeof(unsigned int));
+
+  // configure grid 
+  initGrid(hPos, hVel, params.particleRadius, params.particleRadius * 2.0f, numParticles);
+
+  float4* dPos;
+  hipMalloc((void**)&dPos, numParticles * sizeof(float4));
+  hipMemcpy(dPos, (float4*)hPos, numParticles * sizeof(float4), hipMemcpyHostToDevice);
+
+  float4* dVel;
+  hipMalloc((void**)&dVel, numParticles * sizeof(float4));
+  hipMemcpy(dVel, (float4*)hVel, numParticles * sizeof(float4), hipMemcpyHostToDevice);
+
+  float4* dReorderedPos; 
+  hipMalloc((void**)&dReorderedPos, numParticles * sizeof(float4));
+  hipMemcpy(dReorderedPos, (float4*)hReorderedPos, 
+      numParticles * sizeof(float4), hipMemcpyHostToDevice);
+
+  float4* dReorderedVel;
+  hipMalloc((void**)&dReorderedVel, numParticles * sizeof(float4));
+  hipMemcpy(dReorderedVel, (float4*)hReorderedVel, 
+      numParticles * sizeof(float4), hipMemcpyHostToDevice);
+
+  unsigned int* dHash;
+  hipMalloc((void**)&dHash, numParticles * sizeof(unsigned int));
+  unsigned int* dIndex;
+  hipMalloc((void**)&dIndex, numParticles * sizeof(unsigned int));
+
+  unsigned int* dCellStart;
+  hipMalloc((void**)&dCellStart, numGridCells * sizeof(unsigned int));
+
+  unsigned int* dCellEnd;
+  hipMalloc((void**)&dCellEnd, numGridCells * sizeof(unsigned int));
+
+  for (int i = 0; i < iterations; i++)
+  {
+    integrateSystem(
+        dPos,
+        dVel,
+        params,
+        timestep,
+        numParticles
+             );
+
+    calcHash(
+        dHash,
+        dIndex,
+        dPos,
+        params,
+        numParticles
+      );
+
+    bitonicSort(dHash, dIndex, dHash, dIndex, 1, numParticles, 0);
+
+    //Find start and end of each cell and
+    //Reorder particle data for better cache coherency
+    findCellBoundsAndReorder(
+        dCellStart,
+        dCellEnd,
+        dReorderedPos,
+        dReorderedVel,
+        dHash,
+        dIndex,
+        dPos,
+        dVel,
+        numParticles,
+        numGridCells
+        );
+
+    collide(
+        dVel,
+        dReorderedPos,
+        dReorderedVel,
+        dIndex,
+        dCellStart,
+        dCellEnd,
+        params,
+        numParticles,
+        numGridCells
+           );
+  }
+
+  hipDeviceSynchronize();
+
+#ifdef DEBUG
+  hipMemcpy((float4*)hVel, dVel, numParticles * sizeof(float4), hipMemcpyDeviceToHost);
+  hipMemcpy((float4*)hPos, dPos, numParticles * sizeof(float4), hipMemcpyDeviceToHost);
+  for (unsigned int i = 0; i < numParticles; i++) {
+    printf("%d: ", i);
+    printf("pos: (%.4f, %.4f, %.4f, %.4f)\n",
+        hPos[4*i], hPos[4*i+1], hPos[4*i+2], hPos[4*i+3]);
+    printf("vel: (%.4f, %.4f, %.4f, %.4f)\n",
+        hVel[4*i], hVel[4*i+1], hVel[4*i+2], hVel[4*i+3]);
+  }
+#endif
+
+  free(hPos         );
+  free(hVel         );
+  free(hReorderedPos);
+  free(hReorderedVel);
+  free(hHash        );
+  free(hIndex       );
+  free(hCellStart   );
+  free(hCellEnd     );
+  hipFree(dPos);
+  hipFree(dVel);
+  hipFree(dReorderedPos); 
+  hipFree(dReorderedVel);
+  hipFree(dHash);
+  hipFree(dIndex);
+  hipFree(dCellStart);
+  hipFree(dCellEnd);
+
+  return EXIT_SUCCESS;
+}
---
+++ particles-hip/particles.cu
@@ -0,0 +1,117 @@
+/*
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+//Standard utilities and systems includes
+#include <stdio.h>
+#include <hip/hip_runtime.h>
+#include "particles.h"
+#include "particles_kernels.cu"
+
+//Simulation parameters
+
+static const size_t wgSize = 64;
+
+
+static size_t uSnap(size_t a, size_t b){
+  return ((a % b) == 0) ? a : (a - (a % b) + b);
+}
+
+void integrateSystem(
+    float4* d_Pos,
+    float4* d_Vel,
+    const simParams_t &params,
+    const float deltaTime,
+    const unsigned int numParticles)
+{
+  size_t globalWorkSize = uSnap(numParticles, wgSize);
+  dim3 gws (globalWorkSize/wgSize);
+  dim3 lws (wgSize);
+
+  hipLaunchKernelGGL(integrateSystemK, dim3(gws), dim3(lws), 0, 0, d_Pos, d_Vel, params, deltaTime, numParticles);
+}
+
+void calcHash(
+    unsigned int *d_Hash,
+    unsigned int *d_Index,
+    float4 *d_Pos,
+    const simParams_t &params,
+    const int numParticles)
+{
+  size_t globalWorkSize = uSnap(numParticles, wgSize);
+  dim3 gws (globalWorkSize/wgSize);
+  dim3 lws (wgSize);
+
+  hipLaunchKernelGGL(calcHashK, dim3(gws), dim3(lws), 0, 0, d_Hash, d_Index, d_Pos, params, numParticles);
+}
+
+void memSet(
+    unsigned int* d_Data,
+    unsigned int val,
+    unsigned int N)
+{
+  size_t globalWorkSize = uSnap(N, wgSize);
+
+  dim3 gws (globalWorkSize/wgSize);
+  dim3 lws (wgSize);
+
+  hipLaunchKernelGGL(memSetK, dim3(gws), dim3(lws), 0, 0, d_Data, val, N);
+}
+
+void findCellBoundsAndReorder(
+    unsigned int* d_CellStart,
+    unsigned int* d_CellEnd,
+    float4 *d_ReorderedPos,
+    float4 *d_ReorderedVel,
+    unsigned int *d_Hash,
+    unsigned int *d_Index,
+    float4 *d_Pos,
+    float4 *d_Vel,
+    const unsigned int numParticles,
+    const unsigned int numCells)
+{
+  memSet(d_CellStart, 0xFFFFFFFFU, numCells);
+
+  size_t globalWorkSize = uSnap(numParticles, wgSize);
+  dim3 gws (globalWorkSize/wgSize);
+  dim3 lws (wgSize);
+
+  hipLaunchKernelGGL(findCellBoundsAndReorderK, dim3(gws), dim3(lws), (wgSize+1)*sizeof(unsigned int), 0, 
+      d_CellStart,
+      d_CellEnd, 
+      d_ReorderedPos,
+      d_ReorderedVel, 
+      d_Hash, 
+      d_Index, 
+      d_Pos, 
+      d_Vel,
+      numParticles);
+}
+
+void collide(
+    float4 *d_Vel,
+    float4 *d_ReorderedPos,
+    float4 *d_ReorderedVel,
+    unsigned int *d_Index,
+    unsigned int *d_CellStart,
+    unsigned int *d_CellEnd,
+    const simParams_t &params,
+    const unsigned int   numParticles,
+    const unsigned int   numCells)
+{
+  size_t globalWorkSize = uSnap(numParticles, wgSize);
+
+  dim3 gws (globalWorkSize/wgSize);
+  dim3 lws (wgSize);
+
+  hipLaunchKernelGGL(collideK, dim3(gws), dim3(lws), 0, 0, d_Vel, d_ReorderedPos, d_ReorderedVel, 
+      d_Index, d_CellStart, d_CellEnd, 
+      params, numParticles);
+}
