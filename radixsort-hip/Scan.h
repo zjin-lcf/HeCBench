bc43385f2ba333a16d6028f53ea3ef7bfea4aa6e
Jin Z <5zj@radeon.ftpn.ornl.gov>
2021-04-13 17:19:44 -0400

    Add the radix sort in HIP

---
+++ radixsort-hip/Scan.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+#ifndef _SCAN_H_
+#define _SCAN_H_
+
+#include <hip/hip_runtime.h>
+#include <assert.h>
+#include <stdio.h>
+
+#define MAX_WORKGROUP_INCLUSIVE_SCAN_SIZE 1024
+#define MAX_LOCAL_GROUP_SIZE 256
+static const int WORKGROUP_SIZE = 256;
+static const unsigned int   MAX_BATCH_ELEMENTS = 64 * 1048576;
+static const unsigned int MIN_SHORT_ARRAY_SIZE = 4;
+static const unsigned int MAX_SHORT_ARRAY_SIZE = 4 * WORKGROUP_SIZE;
+static const unsigned int MIN_LARGE_ARRAY_SIZE = 8 * WORKGROUP_SIZE;
+static const unsigned int MAX_LARGE_ARRAY_SIZE = 4 * WORKGROUP_SIZE * WORKGROUP_SIZE;
+
+unsigned int factorRadix2(unsigned int& log2L, unsigned int L);
+
+void scanExclusiveLarge(
+    unsigned int* d_Dst,
+    unsigned int* d_Src,
+    unsigned int* d_Buf,
+    const unsigned int batchSize,
+    const unsigned int arrayLength,
+    const unsigned int numElements);
+
+inline __device__ uint4 make_uint4(uint s)
+{
+    return make_uint4(s, s, s, s);
+}
+
+#endif
