1c7a1d7931923518cd06d0ba355dd76b88e79a72
Jin Z <5zj@leconte.ftpn.ornl.gov>
2021-04-15 09:08:22 -0400

    Remove the { } to fix the nvc error : : a for statement must follow this OMP pragma

--- affine-omp/main.cpp
+++ affine-omp/main.cpp
@@ -159,94 +159,92 @@ int main(int argc, char** argv)
   printf("   Bytes read = %d\n\n", (int)(items_read * sizeof(input_image)));
 
 
-#pragma omp target data map(to: input_image[0:X_SIZE*Y_SIZE]) map(from:output_image[0:X_SIZE*Y_SIZE])
+  #pragma omp target data map(to: input_image[0:X_SIZE*Y_SIZE]) map(from:output_image[0:X_SIZE*Y_SIZE])
   {
     for (int i = 0; i < 100; i++) {
       #pragma omp target teams distribute parallel for collapse(2) thread_limit(256)
-      {
-        for (int y = 0; y < Y_SIZE; y++)
-          for (int x = 0; x < X_SIZE; x++) {
+      for (int y = 0; y < Y_SIZE; y++)
+        for (int x = 0; x < X_SIZE; x++) {
 
-            const float    lx_rot   = 30.0f;
-            const float    ly_rot   = 0.0f; 
-            const float    lx_expan = 0.5f;
-            const float    ly_expan = 0.5f; 
-            int      lx_move  = 0;
-            int      ly_move  = 0;
-            float    affine[2][2];   // coefficients
-            float    i_affine[2][2];
-            float    beta[2];
-            float    i_beta[2];
-            float    det;
-            float    x_new, y_new;
-            float    x_frac, y_frac;
-            float    gray_new;
-            int      m, n;
+          const float    lx_rot   = 30.0f;
+          const float    ly_rot   = 0.0f; 
+          const float    lx_expan = 0.5f;
+          const float    ly_expan = 0.5f; 
+          int      lx_move  = 0;
+          int      ly_move  = 0;
+          float    affine[2][2];   // coefficients
+          float    i_affine[2][2];
+          float    beta[2];
+          float    i_beta[2];
+          float    det;
+          float    x_new, y_new;
+          float    x_frac, y_frac;
+          float    gray_new;
+          int      m, n;
 
-            unsigned short    output_buffer[X_SIZE];
+          unsigned short    output_buffer[X_SIZE];
 
 
-            // forward affine transformation 
-            affine[0][0] = lx_expan * cosf((float)(lx_rot*PI/180.0f));
-            affine[0][1] = ly_expan * sinf((float)(ly_rot*PI/180.0f));
-            affine[1][0] = lx_expan * sinf((float)(lx_rot*PI/180.0f));
-            affine[1][1] = ly_expan * cosf((float)(ly_rot*PI/180.0f));
-            beta[0]      = lx_move;
-            beta[1]      = ly_move;
+          // forward affine transformation 
+          affine[0][0] = lx_expan * cosf((float)(lx_rot*PI/180.0f));
+          affine[0][1] = ly_expan * sinf((float)(ly_rot*PI/180.0f));
+          affine[1][0] = lx_expan * sinf((float)(lx_rot*PI/180.0f));
+          affine[1][1] = ly_expan * cosf((float)(ly_rot*PI/180.0f));
+          beta[0]      = lx_move;
+          beta[1]      = ly_move;
 
-            // determination of inverse affine transformation
-            det = (affine[0][0] * affine[1][1]) - (affine[0][1] * affine[1][0]);
-            if (det == 0.0f)
-            {
-              i_affine[0][0]   = 1.0f;
-              i_affine[0][1]   = 0.0f;
-              i_affine[1][0]   = 0.0f;
-              i_affine[1][1]   = 1.0f;
-              i_beta[0]        = -beta[0];
-              i_beta[1]        = -beta[1];
-            } 
-            else 
-            {
-              i_affine[0][0]   =  affine[1][1]/det;
-              i_affine[0][1]   = -affine[0][1]/det;
-              i_affine[1][0]   = -affine[1][0]/det;
-              i_affine[1][1]   =  affine[0][0]/det;
-              i_beta[0]        = -i_affine[0][0]*beta[0]-i_affine[0][1]*beta[1];
-              i_beta[1]        = -i_affine[1][0]*beta[0]-i_affine[1][1]*beta[1];
-            }
-
-            // Output image generation by inverse affine transformation and bilinear transformation
-
-            x_new    = i_beta[0] + i_affine[0][0]*(x-X_SIZE/2.0f) + i_affine[0][1]*(y-Y_SIZE/2.0f) + X_SIZE/2.0f;
-            y_new    = i_beta[1] + i_affine[1][0]*(x-X_SIZE/2.0f) + i_affine[1][1]*(y-Y_SIZE/2.0f) + Y_SIZE/2.0f;
-
-            m        = (int)floorf(x_new);
-            n        = (int)floorf(y_new);
-
-            x_frac   = x_new - m;
-            y_frac   = y_new - n;
-
-            if ((m >= 0) && (m + 1 < X_SIZE) && (n >= 0) && (n+1 < Y_SIZE))
-            {
-              gray_new = (1.0f - y_frac) * ((1.0f - x_frac) * (input_image[(n * X_SIZE) + m])  + 
-                  x_frac * (input_image[(n * X_SIZE) + m + 1])) + 
-                y_frac  * ((1.0f - x_frac) * (input_image[((n + 1) * X_SIZE) + m]) + 
-                    x_frac * (input_image[((n + 1) * X_SIZE) + m + 1]));
-
-              output_buffer[x] = (unsigned short)gray_new;
-            } 
-            else if (((m + 1 == X_SIZE) && (n >= 0) && (n < Y_SIZE)) || ((n + 1 == Y_SIZE) && (m >= 0) && (m < X_SIZE))) 
-            {
-              output_buffer[x] = input_image[(n * X_SIZE) + m];
-            } 
-            else 
-            {
-              output_buffer[x] = WHITE;
-            }
-
-            output_image[(y * X_SIZE)+x] = output_buffer[x];
+          // determination of inverse affine transformation
+          det = (affine[0][0] * affine[1][1]) - (affine[0][1] * affine[1][0]);
+          if (det == 0.0f)
+          {
+            i_affine[0][0]   = 1.0f;
+            i_affine[0][1]   = 0.0f;
+            i_affine[1][0]   = 0.0f;
+            i_affine[1][1]   = 1.0f;
+            i_beta[0]        = -beta[0];
+            i_beta[1]        = -beta[1];
+          } 
+          else 
+          {
+            i_affine[0][0]   =  affine[1][1]/det;
+            i_affine[0][1]   = -affine[0][1]/det;
+            i_affine[1][0]   = -affine[1][0]/det;
+            i_affine[1][1]   =  affine[0][0]/det;
+            i_beta[0]        = -i_affine[0][0]*beta[0]-i_affine[0][1]*beta[1];
+            i_beta[1]        = -i_affine[1][0]*beta[0]-i_affine[1][1]*beta[1];
           }
-      }
+
+          // Output image generation by inverse affine transformation and bilinear transformation
+
+          x_new    = i_beta[0] + i_affine[0][0]*(x-X_SIZE/2.0f) + i_affine[0][1]*(y-Y_SIZE/2.0f) + X_SIZE/2.0f;
+          y_new    = i_beta[1] + i_affine[1][0]*(x-X_SIZE/2.0f) + i_affine[1][1]*(y-Y_SIZE/2.0f) + Y_SIZE/2.0f;
+
+          m        = (int)floorf(x_new);
+          n        = (int)floorf(y_new);
+
+          x_frac   = x_new - m;
+          y_frac   = y_new - n;
+
+          if ((m >= 0) && (m + 1 < X_SIZE) && (n >= 0) && (n+1 < Y_SIZE))
+          {
+            gray_new = (1.0f - y_frac) * ((1.0f - x_frac) * (input_image[(n * X_SIZE) + m])  + 
+                x_frac * (input_image[(n * X_SIZE) + m + 1])) + 
+              y_frac  * ((1.0f - x_frac) * (input_image[((n + 1) * X_SIZE) + m]) + 
+                  x_frac * (input_image[((n + 1) * X_SIZE) + m + 1]));
+
+            output_buffer[x] = (unsigned short)gray_new;
+          } 
+          else if (((m + 1 == X_SIZE) && (n >= 0) && (n < Y_SIZE)) || ((n + 1 == Y_SIZE) && (m >= 0) && (m < X_SIZE))) 
+          {
+            output_buffer[x] = input_image[(n * X_SIZE) + m];
+          } 
+          else 
+          {
+            output_buffer[x] = WHITE;
+          }
+
+          output_image[(y * X_SIZE)+x] = output_buffer[x];
+        }
     }
   }
 
