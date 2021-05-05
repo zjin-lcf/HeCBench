a65de1344f6035559b02997838ed8ec40780c911
Jin Z <5zj@login.excl.ornl.gov>
2021-05-04 22:45:35 -0400

    add the CUDA and HIP versions

---
+++ deredundancy-cuda/utils.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream> // cout
+#include <fstream>  // ifstream
+#include <vector>  // vector
+#include <cstring>  // memcpy
+#include <algorithm>  // sort
+#include <cuda.h>
+//--------------------data--------------------//
+struct Option {
+  std::string inputFile;
+  std::string outputFile;
+  float threshold;
+  int wordLength;
+};
+
+struct Read {
+  std::string data;
+  std::string name;
+};
+
+//--------------------function--------------------//
+void checkOption(int argc, char **argv, Option &option);
+void readFile(std::vector<Read> &reads, Option &option);
