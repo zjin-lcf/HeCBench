fa7276437a9a3b473dcf8e57f423d19e4263654a
Jin Z <5zj@oswald01.ftpn.ornl.gov>
2021-05-24 22:41:50 -0400

    StrictlyNo double-precision floating-point operations when test is running in the float mode

--- s3d-cuda/ratt2.h
+++ s3d-cuda/ratt2.h
@@ -15,10 +15,9 @@ ratt2_kernel(const real* RESTRICT T, const real* RESTRICT RF, real* RESTRICT RB,
     real TEMP = T[threadIdx.x + (blockIdx.x * blockDim.x)]*TCONV;
     //real ALOGT = LOG(TEMP);
 
-    const register real SMALL_INV = 1e37f;
-
-    const real RU=8.31451e7;
-    const real PATM = 1.01325e6;
+    const register real SMALL_INV = (real)1e37;
+    const real RU = (real)8.31451e7;
+    const real PATM = (real)1.01325e6;
     const real PFAC = DIV (PATM, (RU*(TEMP)));
 
     register real rtemp_inv;
@@ -108,9 +107,9 @@ ratt3_kernel(const real* RESTRICT T, const real* RESTRICT RF, real* RESTRICT RB,
 
     real TEMP = T[threadIdx.x + (blockIdx.x * blockDim.x)]*TCONV;
     //real ALOGT = LOG(TEMP);
-    const register real SMALL_INV = 1e37f;
-    const real RU=8.31451e7;
-    const real PATM = 1.01325e6;
+    const register real SMALL_INV = (real)1e37;
+    const real RU=(real)8.31451e7;
+    const real PATM = (real)1.01325e6;
     const real PFAC = DIV (PATM, (RU*(TEMP)));
 
     register real rtemp_inv;
@@ -200,9 +199,9 @@ ratt4_kernel(const real* RESTRICT T, const real* RESTRICT RF, real* RESTRICT RB,
 
     real TEMP = T[threadIdx.x + (blockIdx.x * blockDim.x)]*TCONV;
     //real ALOGT = LOG(TEMP);
-    const register real SMALL_INV = 1e37f;
-    const real RU=8.31451e7;
-    const real PATM = 1.01325e6;
+    const register real SMALL_INV = (real)1e37;
+    const real RU=(real)8.31451e7;
+    const real PATM = (real)1.01325e6;
     const real PFAC = DIV (PATM, (RU*(TEMP)));
     register real rtemp_inv;
 
@@ -291,9 +290,9 @@ ratt5_kernel(const real* RESTRICT T, const real* RESTRICT RF, real* RESTRICT RB,
 
     const real TEMP = T[threadIdx.x + (blockIdx.x * blockDim.x)]*TCONV;
     //const real ALOGT = LOG(TEMP);
-    const register real SMALL_INV = 1e37f;
-    const real RU=8.31451e7;
-    const real PATM = 1.01325e6;
+    const register real SMALL_INV = (real)1e37;
+    const real RU=(real)8.31451e7;
+    const real PATM = (real)1.01325e6;
     const real PFAC = DIV (PATM, (RU*(TEMP)));
 
     register real rtemp_inv;
@@ -384,9 +383,9 @@ ratt6_kernel(const real* RESTRICT T, const real* RESTRICT RF, real* RESTRICT RB,
 
     const real TEMP = T[threadIdx.x + (blockIdx.x * blockDim.x)]*TCONV;
     //const real ALOGT = LOG(TEMP);
-    const register real SMALL_INV = 1e37f;
-    const real RU=8.31451e7;
-    const real PATM = 1.01325e6;
+    const register real SMALL_INV = (real)1e37;
+    const real RU = (real)8.31451e7;
+    const real PATM =  (real)1.01325e6;
     const real PFAC = DIV (PATM, (RU*(TEMP)));
     register real rtemp_inv;
 
@@ -474,9 +473,9 @@ ratt7_kernel(const real* RESTRICT T, const real* RESTRICT RF, real* RESTRICT RB,
 {
     const real TEMP = T[threadIdx.x + (blockIdx.x * blockDim.x)]*TCONV;
     //const real ALOGT = LOG(TEMP);
-    const register real SMALL_INV = 1e37f;
-    const real RU=8.31451e7;
-    const real PATM = 1.01325e6;
+    const register real SMALL_INV = (real)1e37;
+    const real RU= (real)8.31451e7;
+    const real PATM =  (real)1.01325e6;
     const real PFAC = DIV (PATM, (RU*(TEMP)));
     register real rtemp_inv;
 
@@ -565,9 +564,9 @@ ratt8_kernel(const real* RESTRICT T, const real* RESTRICT RF,
 
     const real TEMP = T[threadIdx.x + (blockIdx.x * blockDim.x)]*TCONV;
     //const real ALOGT = LOG(TEMP);
-    const register real SMALL_INV = 1e37f;
-    const real RU=8.31451e7;
-    const real PATM = 1.01325e6;
+    const register real SMALL_INV = (real)1e37;
+    const real RU = (real)8.31451e7;
+    const real PATM = (real)1.01325e6;
     const real PFAC = DIV (PATM, (RU*(TEMP)));
     register real rtemp_inv;
 
@@ -656,9 +655,9 @@ ratt9_kernel(const real* RESTRICT T, const real* RESTRICT RF,
 
     const real TEMP = T[threadIdx.x + (blockIdx.x * blockDim.x)]*TCONV;
     //const real ALOGT = LOG(TEMP);
-    const register real SMALL_INV = 1e37f;
-    const real RU=8.31451e7;
-    const real PATM = 1.01325e6;
+    const register real SMALL_INV = (real)1e37;
+    const real RU = (real)8.31451e7;
+    const real PATM = (real)1.01325e6;
     const real PFAC = DIV (PATM, (RU*(TEMP)));
     register real rtemp_inv;
 
@@ -765,27 +764,27 @@ ratt10_kernel(const real* RESTRICT T, real* RESTRICT RKLOW, real TCONV)
     const register real TEMP = T[threadIdx.x + (blockIdx.x * blockDim.x)]*TCONV;
     const register real ALOGT = LOG(TEMP);
 
-    RKLOW(1) = EXP(4.22794408e1 -9.e-1*ALOGT + DIV(8.55468335e2,TEMP));
-    RKLOW(2) = EXP(6.37931383e1 -3.42e0*ALOGT - DIV(4.24463259e4,TEMP));
-    RKLOW(3) = EXP(6.54619238e1 -3.74e0*ALOGT - DIV(9.74227469e2,TEMP));
-    RKLOW(4) = EXP(5.55621468e1 -2.57e0*ALOGT - DIV(7.17083751e2,TEMP));
-    RKLOW(5) = EXP(6.33329483e1 -3.14e0*ALOGT - DIV(6.18956501e2,TEMP));
-    RKLOW(6) = EXP(7.69748493e1 -5.11e0*ALOGT - DIV(3.57032226e3,TEMP));
-    RKLOW(7) = EXP(6.98660102e1 -4.8e0*ALOGT - DIV(2.79788467e3,TEMP));
-    RKLOW(8) = EXP(7.68923562e1 -4.76e0*ALOGT - DIV(1.22784867e3,TEMP));
-    RKLOW(9) = EXP(1.11312542e2 -9.588e0*ALOGT - DIV(2.566405e3,TEMP));
-    RKLOW(10) = EXP(1.15700234e2 -9.67e0*ALOGT - DIV(3.13000767e3,TEMP));
-    RKLOW(11) = EXP(3.54348644e1 -6.4e-1*ALOGT - DIV(2.50098684e4,TEMP));
-    RKLOW(12) = EXP(6.3111756e1 -3.4e0*ALOGT - DIV(1.80145126e4,TEMP));
-    RKLOW(13) = EXP(9.57409899e1 -7.64e0*ALOGT - DIV(5.98827834e3,TEMP));
-    RKLOW(14) = EXP(6.9414025e1 -3.86e0*ALOGT - DIV(1.67067934e3,TEMP));
-    RKLOW(15) = EXP(1.35001549e2 -1.194e1*ALOGT - DIV(4.9163262e3,TEMP));
-    RKLOW(16) = EXP(9.14494773e1 -7.297e0*ALOGT - DIV(2.36511834e3,TEMP));
-    RKLOW(17) = EXP(1.17075165e2 -9.31e0*ALOGT - DIV(5.02512164e4,TEMP));
-    RKLOW(18) = EXP(9.68908955e1 -7.62e0*ALOGT - DIV(3.50742017e3,TEMP));
-    RKLOW(19) = EXP(9.50941235e1 -7.08e0*ALOGT - DIV(3.36400342e3,TEMP));
-    RKLOW(20) = EXP(1.38440285e2 -1.2e1*ALOGT - DIV(3.00309643e3,TEMP));
-    RKLOW(21) = EXP(8.93324137e1 -6.66e0*ALOGT - DIV(3.52251667e3,TEMP));
+    RKLOW(1) = EXP((real)4.22794408e1  -(real)9.e-1*ALOGT   + DIV((real)8.55468335e2,TEMP));
+    RKLOW(2) = EXP((real)6.37931383e1  -(real)3.42e0*ALOGT  - DIV((real)4.24463259e4,TEMP));
+    RKLOW(3) = EXP((real)6.54619238e1  -(real)3.74e0*ALOGT  - DIV((real)9.74227469e2,TEMP));
+    RKLOW(4) = EXP((real)5.55621468e1  -(real)2.57e0*ALOGT  - DIV((real)7.17083751e2,TEMP));
+    RKLOW(5) = EXP((real)6.33329483e1  -(real)3.14e0*ALOGT  - DIV((real)6.18956501e2,TEMP));
+    RKLOW(6) = EXP((real)7.69748493e1  -(real)5.11e0*ALOGT  - DIV((real)3.57032226e3,TEMP));
+    RKLOW(7) = EXP((real)6.98660102e1  -(real)4.8e0*ALOGT   - DIV((real)2.79788467e3,TEMP));
+    RKLOW(8) = EXP((real)7.68923562e1  -(real)4.76e0*ALOGT  - DIV((real)1.22784867e3,TEMP));
+    RKLOW(9) = EXP((real)1.11312542e2  -(real)9.588e0*ALOGT - DIV((real)2.566405e3,TEMP));
+    RKLOW(10) = EXP((real)1.15700234e2 -(real)9.67e0*ALOGT  - DIV((real)3.13000767e3,TEMP));
+    RKLOW(11) = EXP((real)3.54348644e1 -(real)6.4e-1*ALOGT  - DIV((real)2.50098684e4,TEMP));
+    RKLOW(12) = EXP((real)6.3111756e1  -(real)3.4e0*ALOGT   - DIV((real)1.80145126e4,TEMP));
+    RKLOW(13) = EXP((real)9.57409899e1 -(real)7.64e0*ALOGT  - DIV((real)5.98827834e3,TEMP));
+    RKLOW(14) = EXP((real)6.9414025e1  -(real)3.86e0*ALOGT  - DIV((real)1.67067934e3,TEMP));
+    RKLOW(15) = EXP((real)1.35001549e2 -(real)1.194e1*ALOGT - DIV((real)4.9163262e3,TEMP));
+    RKLOW(16) = EXP((real)9.14494773e1 -(real)7.297e0*ALOGT - DIV((real)2.36511834e3,TEMP));
+    RKLOW(17) = EXP((real)1.17075165e2 -(real)9.31e0*ALOGT  - DIV((real)5.02512164e4,TEMP));
+    RKLOW(18) = EXP((real)9.68908955e1 -(real)7.62e0*ALOGT  - DIV((real)3.50742017e3,TEMP));
+    RKLOW(19) = EXP((real)9.50941235e1 -(real)7.08e0*ALOGT  - DIV((real)3.36400342e3,TEMP));
+    RKLOW(20) = EXP((real)1.38440285e2 -(real)1.2e1*ALOGT   - DIV((real)3.00309643e3,TEMP));
+    RKLOW(21) = EXP((real)8.93324137e1 -(real)6.66e0*ALOGT  - DIV((real)3.52251667e3,TEMP));
 }
 
 
