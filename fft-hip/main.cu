b6bd67f42a688f61c6e9aa8b2684e86edcb98057
Jin Z <5zj@oswald01.ftpn.ornl.gov>
2021-05-25 18:30:25 -0400

    update the error bounds for the cuda/hip fft benchmark

--- fft-hip/main.cu
+++ fft-hip/main.cu
@@ -14,9 +14,11 @@ using namespace std;
 #ifdef SINGLE_PRECISION
 #define T float 
 #define T2 float2
+#define EPISON 1e-4
 #else
 #define T double
 #define T2 double2
+#define EPISON 1e-6
 #endif
 
 
@@ -177,12 +179,12 @@ int main(int argc, char** argv)
   // Verification
   bool error = false;
   for (int i = 0; i < N; i++) {
-    if ( fabs((T)source[i].x - (T)reference[i].x) > 1e-6) {
+    if ( fabs((T)source[i].x - (T)reference[i].x) > EPISON) {
       //std::cout << i << " " << (T)source[i].x << " " << (T)reference[i].x << std::endl;
       error = true;
       break;
     }
-    if ( fabs((T)source[i].y - (T)reference[i].y) > 1e-6) {
+    if ( fabs((T)source[i].y - (T)reference[i].y) > EPISON) {
       //std::cout << i << " " << (T)source[i].y << " " << (T)reference[i].y << std::endl;
       error = true;
       break;
