98304b782bd6fb78878840f1b33b447140c092f8
jinz <57232910+zjin-lcf@users.noreply.github.com>
2021-05-29 22:02:38 -0400

    no double-precision operations in the kernel

--- page-rank-omp/main.cpp
+++ page-rank-omp/main.cpp
@@ -31,7 +31,7 @@
 #include <getopt.h>
 #include <chrono>
 
-#define D_FACTOR (0.85)
+#define D_FACTOR (0.85f)
 #ifndef BLOCK_SIZE
 #define BLOCK_SIZE 256
 #endif
@@ -43,178 +43,178 @@ const float threshold= 0.00001f;
 
 // generates an array of random pages and their links
 int *random_pages(int n, unsigned int *noutlinks, int divisor){
-	int i, j, k;
-	int *pages = (int*) malloc(sizeof(int)*n*n); // matrix 1 means link from j->i
+  int i, j, k;
+  int *pages = (int*) malloc(sizeof(int)*n*n); // matrix 1 means link from j->i
 
-	if (divisor <= 0) {
-		fprintf(stderr, "ERROR: Invalid divisor '%d' for random initialization, divisor should be greater or equal to 1\n", divisor);
-		exit(1);
-	}
+  if (divisor <= 0) {
+    fprintf(stderr, "ERROR: Invalid divisor '%d' for random initialization, divisor should be greater or equal to 1\n", divisor);
+    exit(1);
+  }
 
-	for(i=0; i<n; ++i){
-		noutlinks[i] = 0;
-		for(j=0; j<n; ++j){
-			if(i!=j && (abs(rand())%divisor == 0)){
-				pages[i*n+j] = 1;
-				noutlinks[i] += 1;
-			}
-		}
+  for(i=0; i<n; ++i){
+    noutlinks[i] = 0;
+    for(j=0; j<n; ++j){
+      if(i!=j && (abs(rand())%divisor == 0)){
+        pages[i*n+j] = 1;
+        noutlinks[i] += 1;
+      }
+    }
 
-		// the case with no outlinks is avoided
-		if(noutlinks[i] == 0){
-			do { k = abs(rand()) % n; } while ( k == i);
-			pages[i*n + k] = 1;
-			noutlinks[i] = 1;
-		}
-	}
-	return pages;
+    // the case with no outlinks is avoided
+    if(noutlinks[i] == 0){
+      do { k = abs(rand()) % n; } while ( k == i);
+      pages[i*n + k] = 1;
+      noutlinks[i] = 1;
+    }
+  }
+  return pages;
 }
 
 void init_array(float *a, int n, float val){
-	int i;
-	for(i=0; i<n; ++i){
-		a[i] = val;
-	}
+  int i;
+  for(i=0; i<n; ++i){
+    a[i] = val;
+  }
 }
 
 void usage(char *argv[]){
-	fprintf(stderr, "Usage: %s [-n number of pages] [-i max iterations]"
-			" [-t threshold] [-q divsor for zero density]\n", argv[0]);
+  fprintf(stderr, "Usage: %s [-n number of pages] [-i max iterations]"
+      " [-t threshold] [-q divsor for zero density]\n", argv[0]);
 }
 
 static struct option size_opts[] =
 {
-	/* name, has_tag, flag, val*/
-	{"number of pages", 1, NULL, 'n'},
-	{"max number of iterations", 1, NULL, 'i'},
-	{"minimum threshold", 1, NULL, 't'},
-	{"divisor for zero density", 1, NULL, 'q'},
-	{ 0, 0, 0}
+  /* name, has_tag, flag, val*/
+  {"number of pages", 1, NULL, 'n'},
+  {"max number of iterations", 1, NULL, 'i'},
+  {"minimum threshold", 1, NULL, 't'},
+  {"divisor for zero density", 1, NULL, 'q'},
+  { 0, 0, 0}
 };
 
 float maximum_dif(float *difs, int n){
-	int i;
-	float max = 0.0f;
-	for(i=0; i<n; ++i){
-		max = difs[i] > max ? difs[i] : max;
-	}
-	return max;
+  int i;
+  float max = 0.0f;
+  for(i=0; i<n; ++i){
+    max = difs[i] > max ? difs[i] : max;
+  }
+  return max;
 }
 int main(int argc, char *argv[]) {
-	int *pages;
-	float *maps;
-	float *page_ranks;
-	unsigned int *noutlinks;
-	int t;
-	float max_diff;
+  int *pages;
+  float *maps;
+  float *page_ranks;
+  unsigned int *noutlinks;
+  int t;
+  float max_diff;
 
-	int i = 0;
-	int j;
-	int n = 1000;
-	int iter = max_iter;
-	float thresh = threshold;
-	int divisor = 2;
-	int nb_links = 0;
+  int i = 0;
+  int j;
+  int n = 1000;
+  int iter = max_iter;
+  float thresh = threshold;
+  int divisor = 2;
+  int nb_links = 0;
 
-	int opt, opt_index = 0;
-	while((opt = getopt_long(argc, argv, "::n:i:t:q:", size_opts, &opt_index)) != -1){
-		switch(opt){
-			case 'n':
-				n = atoi(optarg);
-				break;
-			case 'i':
-				iter = atoi(optarg);
-				break;
-			case 't':
-				thresh = atof(optarg);
-				break;
-			case 'q':
-				divisor = atoi(optarg);
-				break;
-			default:
-				usage(argv);
-				exit(EXIT_FAILURE);
-		}
-	}
-	page_ranks = (float*)malloc(sizeof(float)*n);
-	maps = (float*)malloc(sizeof(float)*n*n);
-	noutlinks = (unsigned int*)malloc(sizeof(unsigned int)*n);
+  int opt, opt_index = 0;
+  while((opt = getopt_long(argc, argv, "::n:i:t:q:", size_opts, &opt_index)) != -1){
+    switch(opt){
+      case 'n':
+        n = atoi(optarg);
+        break;
+      case 'i':
+        iter = atoi(optarg);
+        break;
+      case 't':
+        thresh = atof(optarg);
+        break;
+      case 'q':
+        divisor = atoi(optarg);
+        break;
+      default:
+        usage(argv);
+        exit(EXIT_FAILURE);
+    }
+  }
+  page_ranks = (float*)malloc(sizeof(float)*n);
+  maps = (float*)malloc(sizeof(float)*n*n);
+  noutlinks = (unsigned int*)malloc(sizeof(unsigned int)*n);
 
-	max_diff=99.0f;
+  max_diff=99.0f;
 
-	for (i=0; i<n; ++i) {
-		noutlinks[i] = 0;
-	}
-	pages = random_pages(n,noutlinks,divisor);
-	init_array(page_ranks, n, 1.0f / (float) n);
+  for (i=0; i<n; ++i) {
+    noutlinks[i] = 0;
+  }
+  pages = random_pages(n,noutlinks,divisor);
+  init_array(page_ranks, n, 1.0f / (float) n);
 
-	nb_links = 0;
-	for (i=0; i<n; ++i) {
-		for (j=0; j<n; ++j) {
-			nb_links += pages[i*n+j];
-		}
-	}
+  nb_links = 0;
+  for (i=0; i<n; ++i) {
+    for (j=0; j<n; ++j) {
+      nb_links += pages[i*n+j];
+    }
+  }
 
-	float *diffs, *nzeros;
-	diffs  = (float*) malloc(sizeof(float)*n);
-	nzeros = (float*) malloc(sizeof(float)*n);
-	for(i = 0; i < n; ++i){
-		diffs[i] = 0.0f;
-		nzeros[i] = 0.0f;
-	}
+  float *diffs, *nzeros;
+  diffs  = (float*) malloc(sizeof(float)*n);
+  nzeros = (float*) malloc(sizeof(float)*n);
+  for(i = 0; i < n; ++i){
+    diffs[i] = 0.0f;
+    nzeros[i] = 0.0f;
+  }
 
-	auto start = std::chrono::high_resolution_clock::now();
-
-
-	size_t block_size  = n < BLOCK_SIZE ? n : BLOCK_SIZE;
-
-       #pragma omp target data map(to: pages[0:n*n]) \
-	                       map(to: page_ranks[0:n]) \
-                               map(to:noutlinks[0:n]) \
-	                       map(alloc: diffs[0:n]) \
-	                       map(to: nzeros[0:n]) \
-                               map(alloc: maps[0:n*n]) 
-
-	for (t=1; t<=iter && max_diff>=thresh; ++t) {
-          #pragma omp target teams distribute parallel for thread_limit(block_size) 
-          for (int i = 0; i < n; i++) {
-          	float outbound_rank = page_ranks[i]/(float)noutlinks[i];
-          	for(int j=0; j<n; ++j) maps[i*n+j] = pages[i*n+j]*outbound_rank;
-          }
-          
-           #pragma omp target teams distribute parallel for thread_limit(block_size) 
-           for (int j = 0; j < n; j++) {
-             float new_rank;
-             float old_rank;
-             	old_rank = page_ranks[j];
-             	new_rank = 0.0f;
-             	for(int i=0; i< n; ++i) new_rank += maps[i*n + j];
-             	new_rank = ((1-D_FACTOR)/n)+(D_FACTOR*new_rank);
-             	diffs[j] = fabs(new_rank - old_rank) > diffs[j] ? fabs(new_rank - old_rank) : diffs[j];
-             	page_ranks[j] = new_rank;
-          }
-          #pragma omp target update from(diffs[0:n])
-          max_diff = maximum_dif(diffs, n);
+  auto start = std::chrono::high_resolution_clock::now();
 
 
-          #pragma omp target teams distribute parallel for thread_limit(block_size) 
-		  for (int i = 0; i < n; i++)
-		    diffs[i] = nzeros[i];
-	}
+  size_t block_size  = n < BLOCK_SIZE ? n : BLOCK_SIZE;
 
-	auto end = std::chrono::high_resolution_clock::now();
-	double seconds = std::chrono::duration_cast<std::chrono::duration<double> >(end - start).count();
+#pragma omp target data map(to: pages[0:n*n]) \
+  map(to: page_ranks[0:n]) \
+  map(to:noutlinks[0:n]) \
+  map(alloc: diffs[0:n]) \
+  map(to: nzeros[0:n]) \
+  map(alloc: maps[0:n*n]) 
 
-	fprintf(stderr, "max dif %f is reached at iteration %d\n", max_diff, t);
-	printf("{ \"status\": %d, \"options\": \"-n %d -i %d -t %f\", \"time\": %f }\n", 1, n, iter, thresh, seconds);
+  for (t=1; t<=iter && max_diff>=thresh; ++t) {
+#pragma omp target teams distribute parallel for thread_limit(block_size) 
+    for (int i = 0; i < n; i++) {
+      float outbound_rank = page_ranks[i]/(float)noutlinks[i];
+      for(int j=0; j<n; ++j) maps[i*n+j] = pages[i*n+j]*outbound_rank;
+    }
 
-	free(pages);
-	free(maps);
-	free(page_ranks);
-	free(noutlinks);
-	free(nzeros);
-	free(diffs);
-	return 0;
+#pragma omp target teams distribute parallel for thread_limit(block_size) 
+    for (int j = 0; j < n; j++) {
+      float new_rank;
+      float old_rank;
+      old_rank = page_ranks[j];
+      new_rank = 0.0f;
+      for(int i=0; i< n; ++i) new_rank += maps[i*n + j];
+      new_rank = ((1.f-D_FACTOR)/n)+(D_FACTOR*new_rank);
+      diffs[j] = fabsf(new_rank - old_rank) > diffs[j] ? fabsf(new_rank - old_rank) : diffs[j];
+      page_ranks[j] = new_rank;
+    }
+#pragma omp target update from(diffs[0:n])
+    max_diff = maximum_dif(diffs, n);
+
+
+#pragma omp target teams distribute parallel for thread_limit(block_size) 
+    for (int i = 0; i < n; i++)
+      diffs[i] = nzeros[i];
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  double seconds = std::chrono::duration_cast<std::chrono::duration<double> >(end - start).count();
+
+  fprintf(stderr, "max dif %f is reached at iteration %d\n", max_diff, t);
+  printf("{ \"status\": %d, \"options\": \"-n %d -i %d -t %f\", \"time\": %f }\n", 1, n, iter, thresh, seconds);
+
+  free(pages);
+  free(maps);
+  free(page_ranks);
+  free(noutlinks);
+  free(nzeros);
+  free(diffs);
+  return 0;
 }
 
 
