b8d030bd0be8be18d43bba47d1ad07d413964fb2
Jin Z <5zj@radeon.ftpn.ornl.gov>
2021-06-01 16:57:37 -0400

    Add the HIP version

---
+++ ss-hip/StringSearch.h
@@ -0,0 +1,32 @@
+/**********************************************************************
+Copyright ©2013 Advanced Micro Devices, Inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+• Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+• Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+********************************************************************/
+
+
+#ifndef STRINGSEARCH_H_
+#define STRINGSEARCH_H_
+
+#define LOCAL_SIZE      256
+#define COMPARE(x,y)    ((toupper(x) == toupper(y)))
+#define SEARCH_BYTES_PER_WORKITEM   512
+#define TOLOWER(x) (('A' <= (x) && (x) <= 'Z') ? ((x - 'A') + 'a') : (x))
+
+
+// Enable filtering when the search pattern has more than 16 characters
+#define ENABLE_2ND_LEVEL_FILTER
+
+typedef unsigned char uchar;
+
+#endif
