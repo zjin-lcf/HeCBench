e4951ed9effafa6ee97137427b756a99c0ae391f
Jin Z <5zj@oswald01.ftpn.ornl.gov>
2021-06-03 22:13:29 -0400

    print result message clearly

--- atomicIntrinsics-sycl/main.cpp
+++ atomicIntrinsics-sycl/main.cpp
@@ -33,7 +33,7 @@
 //! @param idata      input data as provided to device
 //! @param len        number of elements in reference / idata
 ////////////////////////////////////////////////////////////////////////////////
-int
+void
 computeGold(int *gpuData, const int len)
 {
     int val = 0;
@@ -119,10 +119,9 @@ computeGold(int *gpuData, const int len)
     if (val != gpuData[6])
     {
         printf("Xor failed %d %d\n", val, gpuData[6]);
-        return false;
     }
 
-    return true;
+    printf("PASS\n");
 }
 
 ////////////////////////////////////////////////////////////////////////////////
