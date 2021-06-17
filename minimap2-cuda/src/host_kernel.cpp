d28f148a383c56ccc153878c63724ef07f8585c3
u58480 <u58480@s001-n140.aidevcloud>
2021-06-16 18:46:46 -0700

    replace 255 with -1 to remove DPCT conversion error

--- minimap2-cuda/src/host_kernel.cpp
+++ minimap2-cuda/src/host_kernel.cpp
@@ -4,7 +4,7 @@
 
 static const char LogTable256[256] = {
 #define LT(n) n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n
-    255, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+    -1, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
     LT(4), LT(5), LT(5), LT(6), LT(6), LT(6), LT(6),
     LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7)};
 
