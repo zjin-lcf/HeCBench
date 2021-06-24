6e4e47dd8804710200d67516a6931fca8b400f2c
Jin Z <5zj@leconte.ftpn.ornl.gov>
2021-06-24 17:39:27 -0400

    add the updated miniBUDE

---
+++ minibude-cuda/main.cu
@@ -0,0 +1,369 @@
+#include <cmath>
+#include <memory>
+#include <vector>
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <functional>
+#include <algorithm>
+#include "bude.h"
+
+typedef std::chrono::high_resolution_clock::time_point TimePoint;
+
+struct Params {
+
+  size_t natlig;
+  size_t natpro;
+  size_t ntypes;
+  size_t nposes;
+
+  std::vector<Atom> protein;
+  std::vector<Atom> ligand;
+  std::vector<FFParams> forcefield;
+  std::array<std::vector<float>, 6> poses;
+
+  size_t iterations;
+
+  // XXX bring this back once all SYCL implementations implement 2020 spec
+  //  size_t posesPerWI;
+  size_t wgSize;
+  std::string deckDir;
+
+  friend std::ostream &operator<<(std::ostream &os, const Params &params) {
+    os <<
+      "natlig:      " << params.natlig << "\n" <<
+      "natpro:      " << params.natpro << "\n" <<
+      "ntypes:      " << params.ntypes << "\n" <<
+      "nposes:      " << params.nposes << "\n" <<
+      "iterations:  " << params.iterations << "\n" <<
+      "posesPerWI:  " << NUM_TD_PER_THREAD << "\n" <<
+      "wgSize:      " << params.wgSize << "\n";
+    return os;
+  }
+};
+
+__global__ void fasten_main(
+    //    size_t posesPerWI,
+    const size_t ntypes,
+    const size_t nposes,
+    const size_t natlig,
+    const size_t natpro,
+    const Atom *__restrict protein_molecule,
+    const Atom *__restrict ligand_molecule,
+    const float *__restrict transforms_0,
+    const float *__restrict transforms_1,
+    const float *__restrict transforms_2,
+    const float *__restrict transforms_3,
+    const float *__restrict transforms_4,
+    const float *__restrict transforms_5,
+    const FFParams *__restrict forcefield,
+    float *__restrict etotals);
+
+
+double elapsedMillis( const TimePoint &start, const TimePoint &end){
+  auto elapsedNs = static_cast<double>(
+      std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+  return elapsedNs * 1e-6;
+}
+
+void printTimings(const Params &params, double millis) {
+
+  // Average time per iteration
+  double ms = (millis / params.iterations);
+  double runtime = ms * 1e-3;
+
+  // Compute FLOP/s
+  double ops_per_wg = NUM_TD_PER_THREAD * 27 + params.natlig * (3 + NUM_TD_PER_THREAD * 18 + 
+      params.natpro * (11 + NUM_TD_PER_THREAD * 30)) + NUM_TD_PER_THREAD;
+  double total_ops = ops_per_wg * ((double) params.nposes / NUM_TD_PER_THREAD);
+  double flops = total_ops / runtime;
+  double gflops = flops / 1e9;
+
+  double interactions = (double) params.nposes * (double) params.natlig * (double) params.natpro;
+  double interactions_per_sec = interactions / runtime;
+
+  // Print stats
+  std::cout.precision(3);
+  std::cout << std::fixed;
+  std::cout << "- Kernel time:    " << (millis) << " ms\n";
+  std::cout << "- Average time:   " << ms << " ms\n";
+  std::cout << "- Interactions/s: " << (interactions_per_sec / 1e9) << " billion\n";
+  std::cout << "- GFLOP/s:        " << gflops << "\n";
+}
+
+template<typename T>
+std::vector<T> readNStruct(const std::string &path) {
+  std::fstream s(path, std::ios::binary | std::ios::in);
+  if (!s.good()) {
+    throw std::invalid_argument("Bad file: " + path);
+  }
+  s.ignore(std::numeric_limits<std::streamsize>::max());
+  auto len = s.gcount();
+  s.clear();
+  s.seekg(0, std::ios::beg);
+  std::vector<T> xs(len / sizeof(T));
+  s.read(reinterpret_cast<char *>(xs.data()), len);
+  s.close();
+  return xs;
+}
+
+Params loadParameters(const std::vector<std::string> &args) {
+
+  Params params = {};
+
+  // Defaults
+  params.iterations = DEFAULT_ITERS;
+  params.nposes = DEFAULT_NPOSES;
+  params.wgSize = DEFAULT_WGSIZE;
+  params.deckDir = DATA_DIR;
+  //  params.posesPerWI = DEFAULT_PPWI;
+
+  const auto readParam = [&args](size_t &current,
+      const std::string &arg,
+      const std::initializer_list<std::string> &matches,
+      const std::function<void(std::string)> &handle) {
+    if (matches.size() == 0) return false;
+    if (std::find(matches.begin(), matches.end(), arg) != matches.end()) {
+      if (current + 1 < args.size()) {
+        current++;
+        handle(args[current]);
+      } else {
+        std::cerr << "[";
+        for (const auto &m : matches) std::cerr << m;
+        std::cerr << "] specified but no value was given" << std::endl;
+        std::exit(EXIT_FAILURE);
+      }
+      return true;
+    }
+    return false;
+  };
+
+  const auto bindInt = [](const std::string &param, size_t &dest, const std::string &name) {
+    try {
+      auto parsed = std::stol(param);
+      if (parsed < 0) {
+        std::cerr << "positive integer required for <" << name << ">: `" << parsed << "`" << std::endl;
+        std::exit(EXIT_FAILURE);
+      }
+      dest = parsed;
+    } catch (...) {
+      std::cerr << "malformed value, integer required for <" << name << ">: `" << param << "`" << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+  };
+
+  for (size_t i = 0; i < args.size(); ++i) {
+    using namespace std::placeholders;
+    const auto arg = args[i];
+    if (readParam(i, arg, {"--iterations", "-i"}, std::bind(bindInt, _1, std::ref(params.iterations), "iterations"))) continue;
+    if (readParam(i, arg, {"--numposes", "-n"}, std::bind(bindInt, _1, std::ref(params.nposes), "numposes"))) continue;
+    //    if (readParam(i, arg, {"--posesperwi", "-p"}, std::bind(bindInt, _1, std::ref(params.posesPerWI), "posesperwi"))) continue;
+    if (readParam(i, arg, {"--wgsize", "-w"}, std::bind(bindInt, _1, std::ref(params.wgSize), "wgsize"))) continue;
+    if (readParam(i, arg, {"--deck"}, [&](const std::string &param) { params.deckDir = param; })) continue;
+
+    if (arg == "--help" || arg == "-h") {
+      std::cout << "\n";
+      std::cout << "Usage: ./main [OPTIONS]\n\n"
+        << "Options:\n"
+        << "  -h  --help               Print this message\n"
+        << "  -i  --iterations I       Repeat kernel I times (default: " << DEFAULT_ITERS << ")\n"
+        << "  -n  --numposes   N       Compute energies for N poses (default: " << DEFAULT_NPOSES << ")\n"
+        //                << "  -p  --poserperwi PPWI    Compute PPWI poses per work-item (default: " << DEFAULT_PPWI << ")\n"
+        << "  -w  --wgsize     WGSIZE  Run with work-group size WGSIZE using nd_range, set to 0 for plain range (default: " << DEFAULT_WGSIZE << ")\n"
+        << "      --deck       DECK    Use the DECK directory as input deck (default: " << DATA_DIR << ")"
+        << std::endl;
+      std::exit(EXIT_SUCCESS);
+    }
+
+    std::cout << "Unrecognized argument '" << arg << "' (try '--help')" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  params.ligand = readNStruct<Atom>(params.deckDir + FILE_LIGAND);
+  params.natlig = params.ligand.size();
+
+  params.protein = readNStruct<Atom>(params.deckDir + FILE_PROTEIN);
+  params.natpro = params.protein.size();
+
+  params.forcefield = readNStruct<FFParams>(params.deckDir + FILE_FORCEFIELD);
+  params.ntypes = params.forcefield.size();
+
+  auto poses = readNStruct<float>(params.deckDir + FILE_POSES);
+  if (poses.size() / 6 != params.nposes) {
+    throw std::invalid_argument("Bad poses: " + std::to_string(poses.size()));
+  }
+
+  for (size_t i = 0; i < 6; ++i) {
+    params.poses[i].resize(params.nposes);
+    std::copy(
+        std::next(poses.cbegin(), i * params.nposes),
+        std::next(poses.cbegin(), i * params.nposes + params.nposes),
+        params.poses[i].begin());
+
+  }
+
+  return params;
+}
+
+std::vector<float> runKernel(Params params) {
+
+  std::vector<float> energies(params.nposes);
+
+  Atom *protein;
+  cudaMalloc((void**)&protein, params.natpro*sizeof(Atom));
+  cudaMemcpy(protein, params.protein.data(), params.natpro*sizeof(Atom), cudaMemcpyHostToDevice);
+
+  Atom *ligand;
+  cudaMalloc((void**)&ligand, params.natlig*sizeof(Atom));
+  cudaMemcpy(ligand, params.ligand.data(), params.natlig*sizeof(Atom), cudaMemcpyHostToDevice);
+
+  float *transforms_0;
+  cudaMalloc((void**)&transforms_0, params.nposes*sizeof(float));
+  cudaMemcpy(transforms_0, params.poses[0].data(), params.nposes*sizeof(float), cudaMemcpyHostToDevice);
+
+  float *transforms_1;
+  cudaMalloc((void**)&transforms_1, params.nposes*sizeof(float));
+  cudaMemcpy(transforms_1, params.poses[1].data(), params.nposes*sizeof(float), cudaMemcpyHostToDevice);
+
+  float *transforms_2;
+  cudaMalloc((void**)&transforms_2, params.nposes*sizeof(float));
+  cudaMemcpy(transforms_2, params.poses[2].data(), params.nposes*sizeof(float), cudaMemcpyHostToDevice);
+
+  float *transforms_3;
+  cudaMalloc((void**)&transforms_3, params.nposes*sizeof(float));
+  cudaMemcpy(transforms_3, params.poses[3].data(), params.nposes*sizeof(float), cudaMemcpyHostToDevice);
+
+  float *transforms_4;
+  cudaMalloc((void**)&transforms_4, params.nposes*sizeof(float));
+  cudaMemcpy(transforms_4, params.poses[4].data(), params.nposes*sizeof(float), cudaMemcpyHostToDevice);
+
+  float *transforms_5;
+  cudaMalloc((void**)&transforms_5, params.nposes*sizeof(float));
+  cudaMemcpy(transforms_5, params.poses[5].data(), params.nposes*sizeof(float), cudaMemcpyHostToDevice);
+
+  FFParams *forcefield;
+  cudaMalloc((void**)&forcefield, params.ntypes*sizeof(FFParams));
+  cudaMemcpy(forcefield, params.forcefield.data(), params.ntypes*sizeof(FFParams), cudaMemcpyHostToDevice);
+
+  float *results;
+  cudaMalloc((void**)&results, params.nposes*sizeof(float));
+
+  size_t global = ceil((params.nposes) / static_cast<double> (NUM_TD_PER_THREAD));
+  global = ceil(static_cast<double> (global) / params.wgSize);
+
+  dim3 grid (global);
+  dim3 block (params.wgSize);
+
+  // warmup
+  fasten_main<<< grid, block, params.ntypes * sizeof(FFParams) >>>(
+      params.ntypes,
+      params.nposes,
+      params.natlig,
+      params.natpro,
+      protein,
+      ligand,
+      transforms_0,
+      transforms_1,
+      transforms_2,
+      transforms_3,
+      transforms_4,
+      transforms_5,
+      forcefield,
+      results);
+
+  auto kernelStart = std::chrono::high_resolution_clock::now();
+
+  for (size_t i = 0; i < params.iterations; ++i) {
+    fasten_main<<< grid, block, params.ntypes * sizeof(FFParams) >>>(
+        params.ntypes,
+        params.nposes,
+        params.natlig,
+        params.natpro,
+        protein,
+        ligand,
+        transforms_0,
+        transforms_1,
+        transforms_2,
+        transforms_3,
+        transforms_4,
+        transforms_5,
+        forcefield,
+        results);
+  }
+
+  cudaDeviceSynchronize();
+
+  auto kernelEnd = std::chrono::high_resolution_clock::now();
+
+  cudaMemcpy(energies.data(), results, params.nposes*sizeof(float), cudaMemcpyDeviceToHost);
+
+  printTimings(params, elapsedMillis(kernelStart, kernelEnd));
+
+  cudaFree(protein);
+  cudaFree(ligand);
+  cudaFree(transforms_0);
+  cudaFree(transforms_1);
+  cudaFree(transforms_2);
+  cudaFree(transforms_3);
+  cudaFree(transforms_4);
+  cudaFree(transforms_5);
+  cudaFree(forcefield);
+  cudaFree(results);
+
+  return energies;
+}
+
+int main(int argc, char *argv[]) {
+
+  auto args = std::vector<std::string>(argv + 1, argv + argc);
+  auto params = loadParameters(args);
+
+  std::cout << "Poses     : " << params.nposes << std::endl;
+  std::cout << "Iterations: " << params.iterations << std::endl;
+  std::cout << "Ligands   : " << params.natlig << std::endl;
+  std::cout << "Proteins  : " << params.natpro << std::endl;
+  std::cout << "Deck      : " << params.deckDir << std::endl;
+  std::cout << "Types     : " << params.ntypes << std::endl;
+  std::cout << "WG        : " << params.wgSize << std::endl;
+  auto energies = runKernel(params);
+
+#ifdef DUMP
+  // Keep the output format consistent with the original version
+  FILE *output = fopen("result.out", "w+");
+
+  printf("\nEnergies\n");
+  for (size_t i = 0; i < params.nposes; i++) {
+    fprintf(output, "%7.2f\n", energies[i]);
+    if (i < 16)
+      printf("%7.2f\n", energies[i]);
+  }
+  fclose(output);
+#endif
+
+  // Validate energies
+  std::ifstream refEnergies(params.deckDir + FILE_REF_ENERGIES);
+  size_t nRefPoses = params.nposes;
+  if (params.nposes > REF_NPOSES) {
+    std::cout << "Only validating the first " << REF_NPOSES << " poses.\n";
+    nRefPoses = REF_NPOSES;
+  }
+
+  std::string line;
+  float maxdiff = 0.0f;
+  for (size_t i = 0; i < nRefPoses; i++) {
+    if (!std::getline(refEnergies, line)) {
+      throw std::logic_error("ran out of ref energies lines to verify");
+    }
+    float e = std::stof(line);
+    if (std::fabs(e) < 1.f && std::fabs(energies[i]) < 1.f) continue;
+
+    float diff = std::fabs(e - energies[i]) / e;
+    if (diff > maxdiff) maxdiff = diff;
+  }
+  std::cout << "Largest difference was " <<
+    std::setprecision(3) << (100 * maxdiff) << "%.\n\n"; 
+  // Expect numbers to be accurate to 2 decimal places
+  refEnergies.close();
+
+  return 0;
+}
---
+++ minibude-sycl/kernel.cpp
@@ -0,0 +1,182 @@
+#include <cmath>
+#include "bude.h"
+
+#define ZERO    0.0f
+#define QUARTER 0.25f
+#define HALF    0.5f
+#define ONE     1.0f
+#define TWO     2.0f
+#define FOUR    4.0f
+#define CNSTNT 45.0f
+
+// Energy evaluation parameters
+#define HBTYPE_F 70
+#define HBTYPE_E 69
+#define HARDNESS 38.0f
+#define NPNPDIST  5.5f
+#define NPPDIST   1.0f
+
+
+void fasten_main(
+    handler &h,
+    //    size_t posesPerWI,
+    size_t wgSize,
+    size_t ntypes,
+    size_t nposes,
+    size_t natlig,
+    size_t natpro,
+    accessor<Atom,  1, sycl_read, sycl_gmem> protein_molecule,
+    accessor<Atom,  1, sycl_read, sycl_gmem> ligand_molecule,
+    accessor<float, 1, sycl_read, sycl_gmem> transforms_0,
+    accessor<float, 1, sycl_read, sycl_gmem> transforms_1,
+    accessor<float, 1, sycl_read, sycl_gmem> transforms_2,
+    accessor<float, 1, sycl_read, sycl_gmem> transforms_3,
+    accessor<float, 1, sycl_read, sycl_gmem> transforms_4,
+    accessor<float, 1, sycl_read, sycl_gmem> transforms_5,
+    accessor<FFParams, 1, sycl_read, sycl_gmem> forcefield,
+    accessor<float, 1, sycl_discard_write, sycl_gmem> etotals) 
+{
+
+    constexpr const auto FloatMax = std::numeric_limits<float>::max();
+
+    size_t global =  ceil((nposes) / static_cast<double> (NUM_TD_PER_THREAD));
+    global = wgSize * ceil(static_cast<double> (global) / wgSize);
+
+    accessor<FFParams, 1, sycl_read_write, sycl_lmem> local_forcefield(range<1>(ntypes), h);
+
+    h.parallel_for<class bude_kernel>(nd_range<1>(global, wgSize), [=](nd_item<1> item) {
+
+        const size_t lid = item.get_local_id(0);
+        const size_t gid = item.get_group(0);
+        const size_t lrange = item.get_local_range(0);
+
+        float etot[NUM_TD_PER_THREAD];
+        cl::sycl::float3 lpos[NUM_TD_PER_THREAD];
+        cl::sycl::float4 transform[NUM_TD_PER_THREAD][3];
+
+        size_t ix = gid * lrange * NUM_TD_PER_THREAD + lid;
+        ix = ix < nposes ? ix : nposes - NUM_TD_PER_THREAD;
+
+        for (int i = lid; i < ntypes; i += lrange) local_forcefield[i] = forcefield[i];
+        //if (ix < ntypes) local_forcefield[ix] = forcefield[ix];
+
+        // Compute transformation matrix to private memory
+        for (size_t i = 0; i < NUM_TD_PER_THREAD; i++) {
+          size_t index = ix + i * lrange;
+
+          const float sx = cl::sycl::sin(transforms_0[index]);
+          const float cx = cl::sycl::cos(transforms_0[index]);
+          const float sy = cl::sycl::sin(transforms_1[index]);
+          const float cy = cl::sycl::cos(transforms_1[index]);
+          const float sz = cl::sycl::sin(transforms_2[index]);
+          const float cz = cl::sycl::cos(transforms_2[index]);
+
+          transform[i][0].x() = cy * cz;
+          transform[i][0].y() = sx * sy * cz - cx * sz;
+          transform[i][0].z() = cx * sy * cz + sx * sz;
+          transform[i][0].w() = transforms_3[index];
+          transform[i][1].x() = cy * sz;
+          transform[i][1].y() = sx * sy * sz + cx * cz;
+          transform[i][1].z() = cx * sy * sz - sx * cz;
+          transform[i][1].w() = transforms_4[index];
+          transform[i][2].x() = -sy;
+          transform[i][2].y() = sx * cy;
+          transform[i][2].z() = cx * cy;
+          transform[i][2].w() = transforms_5[index];
+
+          etot[i] = ZERO;
+        }
+
+        item.barrier(access::fence_space::local_space);
+
+        // Loop over ligand atoms
+        size_t il = 0;
+        do {
+          // Load ligand atom data
+          const Atom l_atom = ligand_molecule[il];
+          const FFParams l_params = local_forcefield[l_atom.type];
+          const bool lhphb_ltz = l_params.hphb < ZERO;
+          const bool lhphb_gtz = l_params.hphb > ZERO;
+
+          const cl::sycl::float4 linitpos(l_atom.x, l_atom.y, l_atom.z, ONE);
+          for (size_t i = 0; i < NUM_TD_PER_THREAD; i++) {
+            // Transform ligand atom
+            lpos[i].x() = transform[i][0].w() +
+              linitpos.x() * transform[i][0].x() +
+              linitpos.y() * transform[i][0].y() +
+              linitpos.z() * transform[i][0].z();
+            lpos[i].y() = transform[i][1].w() +
+              linitpos.x() * transform[i][1].x() +
+              linitpos.y() * transform[i][1].y() +
+              linitpos.z() * transform[i][1].z();
+            lpos[i].z() = transform[i][2].w() +
+              linitpos.x() * transform[i][2].x() +
+              linitpos.y() * transform[i][2].y() +
+              linitpos.z() * transform[i][2].z();
+          }
+
+          // Loop over protein atoms
+          size_t ip = 0;
+          do {
+            // Load protein atom data
+            const Atom p_atom = protein_molecule[ip];
+            const FFParams p_params = local_forcefield[p_atom.type];
+
+            const float radij = p_params.radius + l_params.radius;
+            const float r_radij = 1.f / (radij);
+
+            const float elcdst = (p_params.hbtype == HBTYPE_F && l_params.hbtype == HBTYPE_F) ? FOUR : TWO;
+            const float elcdst1 = (p_params.hbtype == HBTYPE_F && l_params.hbtype == HBTYPE_F) ? QUARTER : HALF;
+            const bool type_E = ((p_params.hbtype == HBTYPE_E || l_params.hbtype == HBTYPE_E));
+
+            const bool phphb_ltz = p_params.hphb < ZERO;
+            const bool phphb_gtz = p_params.hphb > ZERO;
+            const bool phphb_nz = p_params.hphb != ZERO;
+            const float p_hphb = p_params.hphb * (phphb_ltz && lhphb_gtz ? -ONE : ONE);
+            const float l_hphb = l_params.hphb * (phphb_gtz && lhphb_ltz ? -ONE : ONE);
+            const float distdslv = (phphb_ltz ? (lhphb_ltz ? NPNPDIST : NPPDIST) : (lhphb_ltz ? NPPDIST : -FloatMax));
+            const float r_distdslv = 1.f / (distdslv);
+
+            const float chrg_init = l_params.elsc * p_params.elsc;
+            const float dslv_init = p_hphb + l_hphb;
+
+            for (size_t i = 0; i < NUM_TD_PER_THREAD; i++) {
+              // Calculate distance between atoms
+              const float x = lpos[i].x() - p_atom.x;
+              const float y = lpos[i].y() - p_atom.y;
+              const float z = lpos[i].z() - p_atom.z;
+
+              const float distij = cl::sycl::sqrt(x * x + y * y + z * z);
+
+              // Calculate the sum of the sphere radii
+              const float distbb = distij - radij;
+              const bool zone1 = (distbb < ZERO);
+
+              // Calculate steric energy
+              etot[i] += (ONE - (distij * r_radij)) * (zone1 ? 2 * HARDNESS : ZERO);
+
+              // Calculate formal and dipole charge interactions
+              float chrg_e = chrg_init * ((zone1 ? 1 : (ONE - distbb * elcdst1)) * (distbb < elcdst ? 1 : ZERO));
+              const float neg_chrg_e = -cl::sycl::fabs(chrg_e);
+              chrg_e = type_E ? neg_chrg_e : chrg_e;
+              etot[i] += chrg_e * CNSTNT;
+
+              // Calculate the two cases for Nonpolar-Polar repulsive interactions
+              const float coeff = (ONE - (distbb * r_distdslv));
+              float dslv_e = dslv_init * ((distbb < distdslv && phphb_nz) ? 1 : ZERO);
+              dslv_e *= (zone1 ? 1 : coeff);
+              etot[i] += dslv_e;
+            }
+          } while (++ip < natpro); // loop over protein atoms
+        } while (++il < natlig); // loop over ligand atoms
+
+        // Write results
+        const size_t td_base = gid * lrange * NUM_TD_PER_THREAD + lid;
+
+        if (td_base < nposes) {
+          for (size_t i = 0; i < NUM_TD_PER_THREAD; i++) {
+            etotals[td_base + i * lrange] = etot[i] * HALF;
+          }
+        }
+    });
+}
