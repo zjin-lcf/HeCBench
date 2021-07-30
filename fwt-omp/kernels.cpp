af7a6cf6737072b6db7b4d76ed91c89dd8b1a1bb
Jin Z <5zj@oswald00.ftpn.ornl.gov>
2021-07-30 17:47:24 -0400

    add the fwt for debugging

---
+++ fwt-omp/kernels.cpp
@@ -0,0 +1,156 @@
+/*
+ * Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Elementary(for vectors less than elementary size) in-shared memory
+// combined radix-2 + radix-4 Fast Walsh Transform
+///////////////////////////////////////////////////////////////////////////////
+#define ELEMENTARY_LOG2SIZE 11
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Put everything together: batched Fast Walsh Transform CPU front-end
+////////////////////////////////////////////////////////////////////////////////
+void fwtBatchGPU(float *d_Data, int M, int log2N)
+{
+    int N = 1 << log2N;
+    
+    for (; log2N > ELEMENTARY_LOG2SIZE; log2N -= 2, N >>= 2, M <<= 2)
+    {
+      #pragma omp target teams distribute parallel for collapse(2) thread_limit(256)
+      for (int m = 0; m < M; m++) {
+        for (int pos = 0; pos < N; pos++) {
+          const float *d_Src = d_Data  + m * N;
+          float *d_Dst = d_Data + m * N;
+          const int stride = N/4;
+          int lo = pos & (stride - 1);
+          int i0 = ((pos - lo) << 2) + lo;
+          int i1 = i0 + stride;
+          int i2 = i1 + stride;
+          int i3 = i2 + stride;
+
+          float D0 = d_Src[i0];
+          float D1 = d_Src[i1];
+          float D2 = d_Src[i2];
+          float D3 = d_Src[i3];
+
+          float T;
+          T = D0;
+          D0        = D0 + D2;
+          D2        = T - D2;
+          T = D1;
+          D1        = D1 + D3;
+          D3        = T - D3;
+          T = D0;
+          d_Dst[i0] = D0 + D1;
+          d_Dst[i1] = T - D1;
+          T = D2;
+          d_Dst[i2] = D2 + D3;
+          d_Dst[i3] = T - D3;
+        }
+      }
+    }
+
+    #pragma omp target teams num_teams(M) thread_limit(N/4)
+    {
+      float s_data[1024];
+      #pragma omp parallel 
+      {
+        int lid = omp_get_thread_num();
+        int gid = omp_get_team_num();
+        int gsz = omp_get_num_threads(); 
+
+        // Handle to thread block group
+        const int    N = 1 << log2N;
+        const int base = gid << log2N;
+
+        const float *d_Src = d_Data  + base;
+        float *d_Dst = d_Data + base;
+
+        for (int pos = lid; pos < N; pos += gsz)
+        {
+            s_data[pos] = d_Src[pos];
+        }
+
+        //Main radix-4 stages
+        const int pos = lid;
+
+        for (int stride = N >> 2; stride > 0; stride >>= 2)
+        {
+            int lo = pos & (stride - 1);
+            int i0 = ((pos - lo) << 2) + lo;
+            int i1 = i0 + stride;
+            int i2 = i1 + stride;
+            int i3 = i2 + stride;
+
+            #pragma omp barrier
+            float D0 = s_data[i0];
+            float D1 = s_data[i1];
+            float D2 = s_data[i2];
+            float D3 = s_data[i3];
+
+            float T;
+            T = D0;
+            D0         = D0 + D2;
+            D2         = T - D2;
+            T = D1;
+            D1         = D1 + D3;
+            D3         = T - D3;
+            T = D0;
+            s_data[i0] = D0 + D1;
+            s_data[i1] = T - D1;
+            T = D2;
+            s_data[i2] = D2 + D3;
+            s_data[i3] = T - D3;
+        }
+
+        //Do single radix-2 stage for odd power of two
+        if (log2N & 1)
+        {
+            #pragma omp barrier
+
+            for (int pos = lid; pos < N / 2; pos += gsz)
+            {
+                int i0 = pos << 1;
+                int i1 = i0 + 1;
+
+                float D0 = s_data[i0];
+                float D1 = s_data[i1];
+                s_data[i0] = D0 + D1;
+                s_data[i1] = D0 - D1;
+            }
+        }
+
+        #pragma omp barrier
+
+        for (int pos = lid; pos < N; pos += gsz)
+        {
+            d_Dst[pos] = s_data[pos];
+        }
+      }
+    }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Modulate two arrays
+////////////////////////////////////////////////////////////////////////////////
+void modulateGPU(float *__restrict d_A, const float *__restrict d_B, int N)
+{
+    float     rcpN = 1.0f / (float)N;
+    #pragma omp target teams distribute parallel for num_teams(128) thread_limit(256)
+    for (int pos = 0; pos < N; pos++)
+    {
+        d_A[pos] *= d_B[pos] * rcpN;
+    }
+}
