9caf208a7c0f7439584e7cfe90a35e303a881207
u58480 <u58480@s001-n173.aidevcloud>
2021-08-06 12:10:48 -0700

    write the reference from scratch

--- sobel-sycl/reference.cpp
+++ sobel-sycl/reference.cpp
@@ -1,6 +1,41 @@
 #include "common.h"
 
-void reference (uchar *verificationOutput,
+inline uchar clamp (float f) {
+  if (f < 0.f) return 0;
+  if (f > 255.f) return 255;
+  return (uchar)f;
+}
+
+// convolution at the achor point (c) using a 3x3 kernel(k) 
+// on an image pointed by ptr. The image width is "width"
+void convoluation(float4 &g, const int k[][3], const uchar4 *ptr, const int c, const int width) {
+  g.x() =  
+    k[0][0] * ptr[c-1-width].x() + k[0][1] * ptr[c-width].x() + k[0][2] * ptr[c+1-width].x() +
+    k[1][0] * ptr[c-1].x()       + k[1][1] * ptr[c].x()       + k[1][2] * ptr[c+1].x() +
+    k[2][0] * ptr[c-1+width].x() + k[2][1] * ptr[c+width].x() + k[2][2] * ptr[c+1+width].x();
+  g.y() =  
+    k[0][0] * ptr[c-1-width].y() + k[0][1] * ptr[c-width].y() + k[0][2] * ptr[c+1-width].y() +
+    k[1][0] * ptr[c-1].y()       + k[1][1] * ptr[c].y()       + k[1][2] * ptr[c+1].y() +
+    k[2][0] * ptr[c-1+width].y() + k[2][1] * ptr[c+width].y() + k[2][2] * ptr[c+1+width].y();
+  g.z() =  
+    k[0][0] * ptr[c-1-width].z() + k[0][1] * ptr[c-width].z() + k[0][2] * ptr[c+1-width].z() +
+    k[1][0] * ptr[c-1].z()       + k[1][1] * ptr[c].z()       + k[1][2] * ptr[c+1].z() +
+    k[2][0] * ptr[c-1+width].z() + k[2][1] * ptr[c+width].z() + k[2][2] * ptr[c+1+width].z();
+  g.w() =  
+    k[0][0] * ptr[c-1-width].w() + k[0][1] * ptr[c-width].w() + k[0][2] * ptr[c+1-width].w() +
+    k[1][0] * ptr[c-1].w()       + k[1][1] * ptr[c].w()       + k[1][2] * ptr[c+1].w() +
+    k[2][0] * ptr[c-1+width].w() + k[2][1] * ptr[c+width].w() + k[2][2] * ptr[c+1+width].w();
+}
+
+// gradient magnitude
+void magnitude(uchar4 &result, float4 &gx, float4 &gy) {
+  result.x() = clamp(sqrtf(powf(gx.x(), 2.f) + powf(gy.x(), 2.f)) / 2.f);
+  result.y() = clamp(sqrtf(powf(gx.y(), 2.f) + powf(gy.y(), 2.f)) / 2.f);
+  result.z() = clamp(sqrtf(powf(gx.z(), 2.f) + powf(gy.z(), 2.f)) / 2.f);
+  result.w() = clamp(sqrtf(powf(gx.w(), 2.f) + powf(gy.w(), 2.f)) / 2.f);
+}
+
+void reference (uchar4 *verificationOutput,
                 const uchar4 *inputImageData, 
                 const int width,
                 const int height,
@@ -9,70 +44,32 @@ void reference (uchar *verificationOutput,
     // x-axis gradient mask
     const int kx[][3] =
     {
-        { 1, 2, 1},
-        { 0, 0, 0},
-        { -1,-2,-1}
-    };
-
-    // y-axis gradient mask
-    const int ky[][3] =
-    {
         { 1, 0, -1},
         { 2, 0, -2},
         { 1, 0, -1}
     };
 
-    int gx = 0;
-    int gy = 0;
-
-    // pointer to input image data
-    uchar *ptr = (uchar*)malloc(width * height * pixelSize);
-    memcpy(ptr, inputImageData, width * height * pixelSize);
-
-    // each pixel has 4 uchar components
-    int w = width * 4;
-
-    int k = 1;
+    // y-axis gradient mask
+    const int ky[][3] =
+    {
+        { 1, 2, 1},
+        { 0, 0, 0},
+        { -1,-2,-1}
+    };
 
     // apply filter on each pixel (except boundary pixels)
-    for(int i = 0; i < (int)(w * (height - 1)) ; i++)
-    {
-        if(i < (k+1)*w - 4 && i >= 4 + k*w)
-        {
-            gx =  kx[0][0] **(ptr + i - 4 - w)
-                  + kx[0][1] **(ptr + i - w)
-                  + kx[0][2] **(ptr + i + 4 - w)
-                  + kx[1][0] **(ptr + i - 4)
-                  + kx[1][1] **(ptr + i)
-                  + kx[1][2] **(ptr + i + 4)
-                  + kx[2][0] **(ptr + i - 4 + w)
-                  + kx[2][1] **(ptr + i + w)
-                  + kx[2][2] **(ptr + i + 4 + w);
+    for (uint y = 0; y < height; y++)
+      for (uint x = 0; x < width; x++) {
+        if( x >= 1 && x < (width-1) && y >= 1 && y < height - 1) {
+          int c = x + y * width;
+            float4 gx, gy;
+            convoluation(gx, kx, inputImageData, c, width);
+            convoluation(gy, ky, inputImageData, c, width);
 
+            uchar4 result;
+	    magnitude(result, gx, gy);
 
-            gy =  ky[0][0] **(ptr + i - 4 - w)
-                  + ky[0][1] **(ptr + i - w)
-                  + ky[0][2] **(ptr + i + 4 - w)
-                  + ky[1][0] **(ptr + i - 4)
-                  + ky[1][1] **(ptr + i)
-                  + ky[1][2] **(ptr + i + 4)
-                  + ky[2][0] **(ptr + i - 4 + w)
-                  + ky[2][1] **(ptr + i + w)
-                  + ky[2][2] **(ptr + i + 4 + w);
-
-            float gx2 = pow((float)gx, 2);
-            float gy2 = pow((float)gy, 2);
-
-
-            *(verificationOutput + i) = (uchar)(sqrt(gx2 + gy2) / 2.0);
-        }
-
-        // if reached at the end of its row then incr k
-        if(i == (k + 1) * w - 5)
-        {
-            k++;
+            *(verificationOutput + c) = result;  
         }
     }
-
-    free(ptr);
 }
