dcc137f8126bd842fcb906f00077ac045d09972b
Jin Z <5zj@radeon.ftpn.ornl.gov>
2021-08-06 17:12:08 -0400

    add the HIP version

---
+++ sobel-hip/sobel.h
@@ -0,0 +1,15 @@
+#ifndef SOBEL_H
+#define SOBEL_H
+
+#include <cmath>
+#include <hip/hip_runtime.h>
+
+typedef unsigned char uchar;
+
+void reference (uchar4 *verificationOutput,
+                const uchar4 *inputImageData, 
+                const uint width,
+                const uint height,
+                const int pixelSize);
+
+#endif
