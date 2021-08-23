73605eb205904bfc1f63e3e7cce59c9726a1bb68
Jin Z <5zj@explorer.ftpn.ornl.gov>
2021-08-23 16:47:42 -0400

    add the hip codes; most source files  are shared with those in the frna-cuda folder

---
+++ frna-hip/cu.h
@@ -0,0 +1,20 @@
+#ifndef CU_H
+#define CU_H
+
+#ifdef __HIPCC__
+
+#include <hip/hip_runtime.h>
+#define CU(x) if ((x) != hipSuccess) {;die("%s:%d: HIP error: %s", __FILE__, __LINE__,hipGetErrorString(x)); }
+#define DEV __device__
+#define HOST __host__
+#define GLOBAL __global__
+
+#else
+
+#define DEV
+#define HOST
+#define GLOBAL
+
+#endif /* __HIPCC__ */
+
+#endif /* CU_H */
