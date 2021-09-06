3c7b8ff2ba2fb863997543402c6417027d358a6c
Jin Z <5zj@explorer.ftpn.ornl.gov>
2021-09-05 21:31:34 -0400

    add the slightly optimized bitpacking programs

---
+++ bitpacking-cuda/kernels.cu
@@ -0,0 +1,443 @@
+#include "utils.h"
+
+// DEVICE FUNCTIONS
+
+__device__ int clamp(size_t const val, int const max)
+{
+  return static_cast<int>(min(static_cast<size_t>(max), val));
+}
+
+template <typename T>
+__device__ void readMinAndMax(
+    T const* const inMin,
+    T const* const inMax,
+    T* const minBuffer,
+    T* const maxBuffer,
+    int const blockOffset,
+    int const blockEnd)
+{
+  static_assert(
+      BLOCK_SIZE <= BLOCK_WIDTH,
+      "BLOCK_SIZE must be less than or equal to BLOCK_WIDTH");
+
+  if (threadIdx.x < blockEnd) {
+    T localMin = inMin[blockOffset + threadIdx.x];
+    T localMax = inMax[blockOffset + threadIdx.x];
+    for (int i = threadIdx.x + BLOCK_SIZE; i < BLOCK_WIDTH && i < blockEnd;
+         i += BLOCK_SIZE) {
+      int const readIdx = blockOffset + i;
+      localMin = min(inMin[readIdx], localMin);
+      localMax = max(inMax[readIdx], localMax);
+    }
+    minBuffer[threadIdx.x] = localMin;
+    maxBuffer[threadIdx.x] = localMax;
+  }
+}
+
+template <typename T>
+__device__ void
+reduceMinAndMax(T* const minBuffer, T* const maxBuffer, int const blockEnd)
+{
+  // cooperatively compute min and max
+  for (int d = BLOCK_SIZE / 2; d > 0; d >>= 1) {
+    if (threadIdx.x < BLOCK_SIZE / 2) {
+      int const idx = threadIdx.x;
+      if (idx < d && idx + d < blockEnd) {
+        minBuffer[idx] = min(minBuffer[idx], minBuffer[d + idx]);
+      }
+    } else {
+      int const idx = threadIdx.x - (BLOCK_SIZE / 2);
+      if (idx < d && idx + d < blockEnd) {
+        maxBuffer[idx] = max(maxBuffer[idx], maxBuffer[d + idx]);
+      }
+    }
+    __syncthreads();
+  }
+}
+
+
+// KERNELS
+
+template <typename LIMIT, typename INPUT>
+__global__ void bitPackConfigScanKernel(
+    LIMIT* const minValue,
+    LIMIT* const maxValue,
+    INPUT const* const in,
+    const size_t* const numDevice)
+{
+  static_assert(BLOCK_SIZE % 64 == 0, "BLOCK_SIZE must a multiple of 64");
+
+  assert(BLOCK_SIZE == blockDim.x);
+
+  const size_t num = *numDevice;
+  const int numBlocks = roundUpDiv(num, BLOCK_SIZE);
+
+  assert(num > 0);
+  assert(threadIdx.x < BLOCK_SIZE);
+
+  if (blockIdx.x < numBlocks) {
+    // each block processes it's chunks, updates min/max
+    __shared__ LIMIT minBuffer[BLOCK_SIZE];
+    __shared__ LIMIT maxBuffer[BLOCK_SIZE];
+
+    LIMIT localMin = 0;
+    LIMIT localMax = 0;
+
+    int lastThread = 0;
+    for (int block = blockIdx.x; block < numBlocks; block += gridDim.x) {
+
+      int const blockOffset = BLOCK_SIZE * block;
+      int const blockEnd = min(static_cast<int>(num) - blockOffset, BLOCK_SIZE);
+
+      lastThread = max(lastThread, blockEnd);
+
+      if (threadIdx.x < blockEnd) {
+        LIMIT const val = in[blockOffset + threadIdx.x];
+        if (block == blockIdx.x) {
+          // first iteration just set values
+          localMax = val;
+          localMin = val;
+        } else {
+          localMin = min(val, localMin);
+          localMax = max(val, localMax);
+        }
+      }
+    }
+
+    minBuffer[threadIdx.x] = localMin;
+    maxBuffer[threadIdx.x] = localMax;
+
+    __syncthreads();
+
+    // cooperatively compute min and max
+    reduceMinAndMax(minBuffer, maxBuffer, lastThread);
+
+    if (threadIdx.x == 0) {
+      minValue[blockIdx.x] = minBuffer[0];
+      maxValue[blockIdx.x] = maxBuffer[0];
+    }
+  }
+}
+
+template <typename LIMIT, typename INPUT>
+__global__ void bitPackConfigFinalizeKernel(
+    LIMIT const* const inMin,
+    LIMIT const* const inMax,
+    unsigned char* const numBitsPtr,
+    INPUT* const outMinValPtr,
+    const size_t* const numDevice)
+{
+  static_assert(
+      BLOCK_SIZE <= BLOCK_WIDTH,
+      "BLOCK_SIZE must be less than or equal to BLOCK_WIDTH");
+  static_assert(
+      BLOCK_WIDTH % BLOCK_SIZE == 0,
+      "BLOCK_WIDTH must be a multiple of BLOCK_SIZE");
+  static_assert(BLOCK_SIZE % 64 == 0, "BLOCK_SIZE must a multiple of 64");
+
+  assert(blockIdx.x == 0);
+
+  const size_t num = min(
+      roundUpDiv(*numDevice, BLOCK_SIZE), static_cast<size_t>(BLOCK_WIDTH));
+
+  assert(num > 0);
+
+  // each block processes it's chunk, updates min/max, and the calculates
+  // the bitwidth based on the last update
+  __shared__ LIMIT minBuffer[BLOCK_SIZE];
+  __shared__ LIMIT maxBuffer[BLOCK_SIZE];
+
+  // load data
+  readMinAndMax(inMin, inMax, minBuffer, maxBuffer, 0, num);
+
+  __syncthreads();
+
+  // cooperatively compute min and max
+  reduceMinAndMax(minBuffer, maxBuffer, min(BLOCK_SIZE, (int)num));
+
+  if (threadIdx.x == 0) {
+    *outMinValPtr = static_cast<INPUT>(minBuffer[0]);
+    // we need to update the number of bits
+    if (sizeof(LIMIT) > sizeof(int)) {
+      const long long int range = static_cast<uint64_t>(maxBuffer[0]) - static_cast<uint64_t>(minBuffer[0]);
+      // need 64 bit clz
+      *numBitsPtr = sizeof(long long int) * 8
+                     - __clzll(range);
+    } else {
+      const int range = static_cast<uint32_t>(maxBuffer[0]) - static_cast<uint32_t>(minBuffer[0]);
+      // can use 32 bit clz
+      *numBitsPtr = sizeof(int) * 8 - __clz(range);
+    }
+  }
+}
+
+template <typename INPUT, typename OUTPUT>
+__global__ void bitPackKernel(
+    unsigned char const* const numBitsPtr,
+    INPUT const* const valueOffsetPtr,
+    OUTPUT* const outPtr,
+    INPUT const* const in,
+    const size_t* const numDevice)
+{
+  using UINPUT = typename std::make_unsigned<INPUT>::type;
+
+  const size_t num = *numDevice;
+
+  const int numBlocks = roundUpDiv(num, BLOCK_SIZE);
+
+  OUTPUT* const out = outPtr;
+  int const numBits = *numBitsPtr;
+  INPUT const valueOffset = *valueOffsetPtr;
+
+  __shared__ UINPUT inBuffer[BLOCK_SIZE];
+
+  for (int blockId = blockIdx.x; blockId < numBlocks; blockId += gridDim.x) {
+    // The kernel works by assigning an output index to each thread.
+    // The kernel then iterates over chunks of input, filling the bits
+    // for each thread.
+    // And then writing the stored bits to the output.
+    int const outputIdx = threadIdx.x + blockId * BLOCK_SIZE;
+    assert(outputIdx >= 0);
+    assert(*numBitsPtr <= sizeof(INPUT) * 8U);
+
+    size_t const bitStart = outputIdx * sizeof(*out) * 8U;
+    size_t const bitEnd = bitStart + (sizeof(*out) * 8U);
+
+    int const startIdx = clamp(bitStart / static_cast<size_t>(numBits), num);
+    int const endIdx = clamp(roundUpDiv(bitEnd, numBits), num);
+    assert(startIdx >= 0);
+
+    size_t const blockStartBit = blockId * BLOCK_SIZE * sizeof(*out) * 8U;
+    size_t const blockEndBit = (blockId + 1) * BLOCK_SIZE * sizeof(*out) * 8U;
+    assert(blockStartBit < blockEndBit);
+
+    int const blockStartIdx = clamp(
+        roundDownTo(blockStartBit / static_cast<size_t>(numBits), BLOCK_SIZE),
+        num);
+    int const blockEndIdx
+        = clamp(roundUpTo(roundUpDiv(blockEndBit, numBits), BLOCK_SIZE), num);
+    assert(blockStartIdx >= 0);
+    assert(blockStartIdx <= blockEndIdx);
+
+    OUTPUT val = 0;
+    for (int bufferStart = blockStartIdx; bufferStart < blockEndIdx;
+         bufferStart += BLOCK_SIZE) {
+      __syncthreads();
+
+      // fill input buffer
+      int const inputIdx = bufferStart + threadIdx.x;
+      if (inputIdx < num) {
+        inBuffer[threadIdx.x] = in[inputIdx] - valueOffset;
+      }
+
+      __syncthreads();
+
+      int const currentStartIdx = max(startIdx, bufferStart);
+      int const currentEndIdx = min(endIdx, bufferStart + BLOCK_SIZE);
+
+      for (int idx = currentStartIdx; idx < currentEndIdx; ++idx) {
+        int const localIdx = idx - bufferStart;
+
+        // keep only bits we're interested in
+        OUTPUT bits = static_cast<OUTPUT>(inBuffer[localIdx]);
+        int const offset = static_cast<int>(
+            static_cast<ssize_t>(idx * numBits)
+            - static_cast<ssize_t>(bitStart));
+        assert(std::abs(offset) < sizeof(bits) * 8U);
+
+        if (offset > 0) {
+          bits <<= offset;
+        } else {
+          bits >>= -offset;
+        }
+
+        // update b
+        val |= bits;
+      }
+    }
+
+    if (startIdx < num) {
+      out[outputIdx] = val;
+    }
+  }
+}
+
+
+/**
+ * @brief Launch of all of the kernels necessary for the configuration step of
+ * bit packing.
+ *
+ * @tparam LIMIT The type used for min/max values.
+ * @tparam INPUT The type being reduced.
+ * @param minValueScratch Space used by the kernels to reduce the minimum
+ * values. Must be at least the size returned by `getReduceScratchSpaceSize()`.
+ * @param maxValueScratch Space used by the kernels to reduce the maximum
+ * values. Must be at least the size returned by `getReduceScratchSpaceSize()`.
+ * @param minValOutPtr The place to put the actual minimum value of the entire
+ * series (output).
+ * @param numBitsPtr The number of bits to compact to (output).
+ * @param in The input to be compressed.
+ * @param numDevice The number of elements on the device.
+ * @param maxNum The maximum number of elements in the input.
+ */
+template <typename LIMIT, typename INPUT>
+void bitPackConfigLaunch(
+    LIMIT* const minValueScratch,
+    LIMIT* const maxValueScratch,
+    INPUT* const minValOutPtr,
+    unsigned char* const numBitsPtr,
+    INPUT const* const in,
+    const size_t* const numDevice,
+    size_t const maxNum)
+{
+  const dim3 grid(
+      min(BLOCK_WIDTH, static_cast<int>(roundUpDiv(maxNum, BLOCK_SIZE))));
+  const dim3 block(BLOCK_SIZE);
+
+  // make sure the result will fit in a single block for the finalize kernel
+  bitPackConfigScanKernel<<<grid, block>>>(
+      minValueScratch, maxValueScratch, in, numDevice);
+#ifdef DEBUG
+  cudaError_t err;
+  err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    throw std::runtime_error(
+        "Failed to launch bitPackConfigScanKernel "
+        "kernel: "
+        + std::to_string(err));
+  }
+#endif
+
+  // determine numBits and convert min value
+  bitPackConfigFinalizeKernel<<<dim3(1), block>>>(
+      minValueScratch, maxValueScratch, numBitsPtr, minValOutPtr, numDevice);
+#ifdef DEBUG
+  err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    throw std::runtime_error(
+        "Failed to launch bitPackConfigFinalizeKernel "
+        "kernel: "
+        + std::to_string(err));
+  }
+#endif
+}
+
+template <typename INPUT, typename OUTPUT>
+void bitPackLaunch(
+    const INPUT * const minValueDevicePtr,
+    unsigned char const* const numBitsDevicePtr,
+    OUTPUT* const outPtr,
+    INPUT const* const in,
+    const size_t* const numDevice,
+    const size_t maxNum)
+{
+  static_assert(
+      BLOCK_SIZE % (sizeof(OUTPUT) * 8U) == 0,
+      "Block size must be a multiple of output word size.");
+
+  dim3 const grid(
+      std::min(4096, static_cast<int>(roundUpDiv(maxNum, BLOCK_SIZE))));
+  dim3 const block(BLOCK_SIZE);
+
+  bitPackKernel<<<grid, block>>>(
+      numBitsDevicePtr, minValueDevicePtr, outPtr, in, numDevice);
+
+#ifdef DEBUG
+  cudaError_t err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    throw std::runtime_error(
+        "Failed to launch bitPackKernel kernel: " + std::to_string(err));
+  }
+#endif
+}
+
+template <typename IN, typename OUT, typename LIMIT>
+void bitPackFixedBitAndMinInternal(
+    void const* const minValueDevicePtr,
+    unsigned char const* const numBitsDevicePtr,
+    void* const /* workspace */,
+    void* const outPtr,
+    void const* const in,
+    const size_t* const numDevice,
+    size_t const maxNum)
+{
+  // type qualifier is meaningless on cast type
+  OUT* const outputTypedPtr = reinterpret_cast<OUT*>(outPtr);
+  IN const* const inputTyped = static_cast<IN const*>(in);
+
+  bitPackLaunch(
+      reinterpret_cast<const IN*>(minValueDevicePtr),
+      numBitsDevicePtr,
+      outputTypedPtr,
+      inputTyped,
+      numDevice,
+      maxNum);
+}
+
+template <typename IN, typename OUT, typename LIMIT>
+void bitPackInternal(
+    void* const workspace,
+    void* const outPtr,
+    void const* const in,
+    const size_t* const numDevice,
+    size_t const maxNum,
+    void* const minValueDevicePtr,
+    unsigned char* const numBitsDevicePtr)
+{
+  // cast voids to known types
+  LIMIT* const maxValueTyped = static_cast<LIMIT*>(workspace);
+  LIMIT* const minValueTyped = maxValueTyped + getReduceScratchSpaceSize(maxNum);
+  IN const* const inputTyped = static_cast<IN const*>(in);
+
+  // determine min, and bit width
+  bitPackConfigLaunch(
+      minValueTyped,
+      maxValueTyped,
+      reinterpret_cast<IN*>(minValueDevicePtr),
+      numBitsDevicePtr,
+      inputTyped,
+      numDevice,
+      maxNum);
+
+  bitPackFixedBitAndMinInternal<IN, OUT, LIMIT>(
+      minValueDevicePtr,
+      numBitsDevicePtr,
+      workspace,
+      outPtr,
+      in,
+      numDevice,
+      maxNum);
+}
+
+
+void compress(
+    void* const workspace,
+    const size_t workspaceSize,
+    const nvcompType_t inType,
+    void* const outPtr,
+    const void* const in,
+    const size_t* const numPtr,
+    const size_t maxNum,
+    void* const minValueDevicePtr,
+    unsigned char* const numBitsDevicePtr)
+{
+  const size_t reqWorkSize = requiredWorkspaceSize(maxNum, inType);
+  if (workspaceSize < reqWorkSize) {
+    throw std::runtime_error(
+        "Insufficient workspace size: " + std::to_string(workspaceSize)
+        + ", need " + std::to_string(reqWorkSize));
+  }
+
+  for (int n = 0; n < 100; n++)
+  NVCOMP_TYPE_SWITCH(
+      inType,
+      bitPackInternal,
+      workspace,
+      outPtr,
+      in,
+      numPtr,
+      maxNum,
+      minValueDevicePtr,
+      numBitsDevicePtr);
+}
+
---
+++ bitpacking-hip/utils.h
@@ -0,0 +1,276 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+
+#include <cstdlib>
+#include <cstdio>
+#include <cstdint>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cassert>
+#include <climits>
+#include <chrono>
+#include <limits>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <hip/hip_runtime.h>
+
+constexpr int const BLOCK_SIZE = 256;
+
+// only used for min/max
+constexpr int const BLOCK_WIDTH = 4096;
+
+#define CUDA_RT_CALL(call) call
+
+#ifdef DEBUG
+#undef CUDA_RT_CALL
+#define CUDA_RT_CALL(call)                                                     \
+  {                                                                            \
+    hipError_t cudaStatus = call;                                             \
+    if (hipSuccess != cudaStatus) {                                           \
+      fprintf(                                                                 \
+          stderr,                                                              \
+          "ERROR: CUDA RT call \"%s\" in line %d of file %s failed with %s "   \
+          "(%d).\n",                                                           \
+          #call,                                                               \
+          __LINE__,                                                            \
+          __FILE__,                                                            \
+          hipGetErrorString(cudaStatus),                                      \
+          cudaStatus);                                                         \
+      abort();                                                                 \
+    }                                                                          \
+  }
+#endif
+
+#define NVCOMP_TYPE_SWITCH(type_var, func, ...)                                \
+  do {                                                                         \
+    switch (type_var) {                                                        \
+    case NVCOMP_TYPE_CHAR:                                                     \
+      func<char, uint32_t, char>(__VA_ARGS__);                                 \
+      break;                                                                   \
+    case NVCOMP_TYPE_UCHAR:                                                    \
+      func<unsigned char, uint32_t, unsigned char>(__VA_ARGS__);               \
+      break;                                                                   \
+    case NVCOMP_TYPE_SHORT:                                                    \
+      func<short, uint32_t, short>(__VA_ARGS__);                               \
+      break;                                                                   \
+    case NVCOMP_TYPE_USHORT:                                                   \
+      func<unsigned short, uint32_t, unsigned short>(__VA_ARGS__);             \
+      break;                                                                   \
+    case NVCOMP_TYPE_INT:                                                      \
+      func<int, uint32_t, int>(__VA_ARGS__);                                   \
+      break;                                                                   \
+    case NVCOMP_TYPE_UINT:                                                     \
+      func<unsigned int, uint32_t, unsigned int>(__VA_ARGS__);                 \
+      break;                                                                   \
+    case NVCOMP_TYPE_LONGLONG:                                                 \
+      func<long long, uint64_t, long long>(__VA_ARGS__);                       \
+      break;                                                                   \
+    case NVCOMP_TYPE_ULONGLONG:                                                \
+      func<unsigned long long, uint64_t, unsigned long long>(__VA_ARGS__);     \
+      break;                                                                   \
+    default:                                                                   \
+      throw std::runtime_error("Unknown type: " + std::to_string(type_var));   \
+    }                                                                          \
+  } while (0)
+
+
+/* Supported datatypes */
+typedef enum nvcompType_t
+{
+  NVCOMP_TYPE_CHAR = 0,      // 1B
+  NVCOMP_TYPE_UCHAR = 1,     // 1B
+  NVCOMP_TYPE_SHORT = 2,     // 2B
+  NVCOMP_TYPE_USHORT = 3,    // 2B
+  NVCOMP_TYPE_INT = 4,       // 4B
+  NVCOMP_TYPE_UINT = 5,      // 4B
+  NVCOMP_TYPE_LONGLONG = 6,  // 8B
+  NVCOMP_TYPE_ULONGLONG = 7, // 8B
+  NVCOMP_TYPE_BITS = 0xff    // 1b
+} nvcompType_t;/* Supported datatypes */
+
+
+template <typename T>
+T unpackBytes(
+    const void* data, const uint8_t numBits, const T minValue, const size_t i)
+{
+  using U = typename std::make_unsigned<T>::type;
+
+  if (numBits == 0) {
+    return minValue;
+  } else {
+    // enough space to hold 64 bits with up to 7 bit offset
+    uint8_t scratch[9] = {0,0,0,0,0,0,0,0,0};
+
+    // shifting by width of the type is UB
+    const U mask = numBits < sizeof(T)*8U ? static_cast<U>((1ULL << numBits) -
+        1) : static_cast<U>(-1);
+    const uint8_t* byte_data = reinterpret_cast<decltype(byte_data)>(data);
+
+    // Specialized
+    // Need to copy into scratch because
+    // GPU can only address n byte datatype on multiple of n address
+    // TODO: add an optimized version in case numBits aligns to word size
+    //       boundaries (1,2,4,8,16,32 and 64 bits)
+    size_t start_byte = (i * numBits) / 8;
+    // end_byte needed so we don't attempt to read from illegal memory
+    size_t end_byte = ((i + 1) * numBits - 1) / 8;
+    assert(end_byte - start_byte <= sizeof(scratch));
+
+    for (size_t j = start_byte, k = 0; j <= end_byte; ++j, ++k) {
+      scratch[k] = byte_data[j];
+    }
+
+    const int bitOffset = (i * numBits) % 8;
+    U baseValue = 0;
+    for (size_t k = 0; k <= end_byte - start_byte; ++k) {
+      U shifted;
+      if (k > 0) {
+        shifted = static_cast<U>(scratch[k]) << ((k * 8) - bitOffset);
+      } else {
+        shifted = static_cast<U>(scratch[k]) >> bitOffset;
+      }
+      baseValue |= mask & shifted;
+    }
+
+    const T value = baseValue + minValue;
+    return value;
+  }
+}
+
+size_t getReduceScratchSpaceSize(size_t const num);
+size_t requiredWorkspaceSize(size_t const num, const nvcompType_t type);
+
+// returns nano-seconds
+inline uint64_t get_time(timespec start, timespec end)
+{
+  constexpr const uint64_t BILLION = 1000000000ULL;
+  const uint64_t elapsed_time
+      = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+  return elapsed_time;
+}
+
+// size in bytes, returns GB/s
+inline double gibs(struct timespec start, struct timespec end, size_t s)
+{
+  uint64_t t = get_time(start, end);
+  return (double)s / t * 1e9 / 1024 / 1024 / 1024;
+}
+
+// size in bytes, returns GB/s
+inline double
+gbs(const std::chrono::time_point<std::chrono::steady_clock>& start,
+    const std::chrono::time_point<std::chrono::steady_clock>& end,
+    size_t s)
+{
+  return (double)s / std::chrono::nanoseconds(end - start).count();
+}
+
+template <typename T>
+T* align(T* const ptr, const size_t alignment)
+{
+  const size_t bits = reinterpret_cast<size_t>(ptr);
+  const size_t mask = alignment - 1;
+
+  return reinterpret_cast<T*>(((bits - 1) | mask) + 1);
+}
+
+template <typename T>
+size_t
+relativeEndOffset(const void* start, const T* subsection, const size_t length)
+{
+  std::ptrdiff_t diff = reinterpret_cast<const char*>(subsection)
+                        - static_cast<const char*>(start);
+  return static_cast<size_t>(diff) + length * sizeof(T);
+}
+
+template <typename T = size_t>
+T relativeEndOffset(const void* start, const void* subsection)
+{
+  std::ptrdiff_t diff = reinterpret_cast<const char*>(subsection)
+                        - static_cast<const char*>(start);
+  return static_cast<T>(diff);
+}
+
+template <typename U, typename T>
+constexpr __host__ __device__ U roundUpDiv(U const num, T const chunk)
+{
+  return (num / chunk) + (num % chunk > 0);
+}
+
+template <typename U, typename T>
+constexpr __host__ __device__ U roundDownTo(U const num, T const chunk)
+{
+  return (num / chunk) * chunk;
+}
+
+template <typename U, typename T>
+constexpr __host__ __device__ U roundUpTo(U const num, T const chunk)
+{
+  return roundUpDiv(num, chunk) * chunk;
+}
+
+template <typename T>
+inline nvcompType_t TypeOf()
+{
+  if (std::is_same<T, int8_t>::value) {
+    return NVCOMP_TYPE_CHAR;
+  } else if (std::is_same<T, uint8_t>::value) {
+    return NVCOMP_TYPE_UCHAR;
+  } else if (std::is_same<T, int16_t>::value) {
+    return NVCOMP_TYPE_SHORT;
+  } else if (std::is_same<T, uint16_t>::value) {
+    return NVCOMP_TYPE_USHORT;
+  } else if (std::is_same<T, int32_t>::value) {
+    return NVCOMP_TYPE_INT;
+  } else if (std::is_same<T, uint32_t>::value) {
+    return NVCOMP_TYPE_UINT;
+  } else if (std::is_same<T, int64_t>::value) {
+    return NVCOMP_TYPE_LONGLONG;
+  } else if (std::is_same<T, uint64_t>::value) {
+    return NVCOMP_TYPE_ULONGLONG;
+  } else {
+    return NVCOMP_TYPE_INT;
+  }
+}
+
+__inline__ size_t sizeOfnvcompType(nvcompType_t type)
+{
+  switch (type) {
+  case NVCOMP_TYPE_BITS:
+    return 1;
+  case NVCOMP_TYPE_CHAR:
+    return sizeof(int8_t);
+  case NVCOMP_TYPE_UCHAR:
+    return sizeof(uint8_t);
+  case NVCOMP_TYPE_SHORT:
+    return sizeof(int16_t);
+  case NVCOMP_TYPE_USHORT:
+    return sizeof(uint16_t);
+  case NVCOMP_TYPE_INT:
+    return sizeof(int32_t);
+  case NVCOMP_TYPE_UINT:
+    return sizeof(uint32_t);
+  case NVCOMP_TYPE_LONGLONG:
+    return sizeof(int64_t);
+  case NVCOMP_TYPE_ULONGLONG:
+    return sizeof(uint64_t);
+  default:
+    throw std::runtime_error("Unsupported type " + std::to_string(type));
+  }
+}
+
+void compress(
+    void* const workspace,
+    const size_t workspaceSize,
+    const nvcompType_t inType,
+    void* const outPtr,
+    const void* const in,
+    const size_t* const numPtr,
+    const size_t maxNum,
+    void* const minValueDevicePtr,
+    unsigned char* const numBitsDevicePtr);
+
+#endif
