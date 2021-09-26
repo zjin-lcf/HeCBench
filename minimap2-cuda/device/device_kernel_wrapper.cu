934d8ea7024ec3dd6af602918bf8dd18949fdd5a
Jin Z <5zj@oswald00.ftpn.ornl.gov>
2021-09-26 01:54:50 -0400

    remove poininters to pointers(**) by transforming arrays and add the restrict keyword

--- minimap2-cuda/device/device_kernel_wrapper.cu
+++ minimap2-cuda/device/device_kernel_wrapper.cu
@@ -11,7 +11,7 @@
 __global__
 void device_chain_tiled(
         return_dt *ret, const anchor_dt *a,
-        const control_dt *control, score_dt **max_tracker_g, parent_dt **j_tracker_g,
+        const control_dt *control, score_dt *max_tracker_g, parent_dt *j_tracker_g,
         const int max_dist_x, const int max_dist_y, const int bw);
 
 __host__
@@ -41,33 +41,17 @@ void device_chain_kernel_wrapper(
     control_dt *d_control;
     anchor_dt *d_arg;
     return_dt *d_ret;
+    score_dt *d_max_tracker;
+    parent_dt *d_j_tracker;
 
-    // presistent storage
-    score_dt *d_max_tracker[PE_NUM];
-    parent_dt *d_j_tracker[PE_NUM];
+    cudaMalloc((void**)&d_control, cont.size() * sizeof(control_dt));
+    cudaMalloc((void**)&d_arg, arg.size() * sizeof(anchor_dt));
+    cudaMalloc((void**)&d_ret, batch_count * TILE_SIZE * PE_NUM * sizeof(return_dt));
+    cudaMalloc((void**)&d_max_tracker, PE_NUM * BACK_SEARCH_COUNT_GPU * sizeof(score_dt));
+    cudaMalloc((void**)&d_j_tracker, PE_NUM * BACK_SEARCH_COUNT_GPU * sizeof(parent_dt));
 
-    score_dt **d_d_max_tracker;
-    parent_dt **d_d_j_tracker;
-
-    cudaMalloc(&d_control, cont.size() * sizeof(control_dt));
-    cudaMalloc(&d_arg, arg.size() * sizeof(anchor_dt));
-    cudaMalloc(&d_ret, batch_count * TILE_SIZE * PE_NUM * sizeof(return_dt));
-
-    for (auto pe = 0; pe < PE_NUM; pe++) {
-        cudaMalloc(&d_max_tracker[pe], BACK_SEARCH_COUNT_GPU * sizeof(score_dt));
-        cudaMalloc(&d_j_tracker[pe], BACK_SEARCH_COUNT_GPU * sizeof(parent_dt));
-    }
-    cudaMalloc(&d_d_max_tracker, PE_NUM * sizeof(score_dt *));
-    cudaMalloc(&d_d_j_tracker, PE_NUM * sizeof(parent_dt *));
-
-    cudaMemcpy(d_control, h_control,
-            cont.size() * sizeof(control_dt), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_arg, h_arg,
-            arg.size() * sizeof(anchor_dt), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_d_max_tracker, d_max_tracker,
-            PE_NUM * sizeof(score_dt *), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_d_j_tracker, d_j_tracker,
-            PE_NUM * sizeof(parent_dt *), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_control, h_control, cont.size() * sizeof(control_dt), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_arg, h_arg, arg.size() * sizeof(anchor_dt), cudaMemcpyHostToDevice);
 
 
     for (auto batch = 0; batch < batch_count; batch++) {
@@ -75,8 +59,8 @@ void device_chain_kernel_wrapper(
                     d_ret + batch * PE_NUM * TILE_SIZE,
                     d_arg + batch * PE_NUM * TILE_SIZE_ACTUAL,
                     d_control + batch * PE_NUM ,
-                    d_d_max_tracker,
-                    d_d_j_tracker,
+                    d_max_tracker,
+                    d_j_tracker,
                     max_dist_x, max_dist_y, bw);
     }
 
@@ -85,12 +69,8 @@ void device_chain_kernel_wrapper(
     cudaFree(d_control);
     cudaFree(d_arg);
     cudaFree(d_ret);
-    for (auto pe = 0; pe < PE_NUM; pe++) {
-        cudaFree(d_max_tracker[pe]);
-        cudaFree(d_j_tracker[pe]);
-    }
-    cudaFree(d_d_max_tracker);
-    cudaFree(d_d_j_tracker);
+    cudaFree(d_max_tracker);
+    cudaFree(d_j_tracker);
 
     clock_gettime(CLOCK_BOOTTIME, &end);
     printf(" ***** offloading took %f seconds for end-to-end\n",
