6d3ebcebda4df022a7728733643f9529c044d5c3
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2021-10-06 22:52:04 -0400

    add the lower bound examples

---
+++ keogh-omp/main.cpp
@@ -0,0 +1,93 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+
+#include "reference.h"
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 3) {
+    printf("Usage: ./%s <query length> <subject length>\n", argv[0]);
+    return -1;
+  }
+
+  const int M = atoi(argv[1]);
+  const int N = atoi(argv[2]);
+
+  printf("Query length = %d\n", M);
+  printf("Subject length = %d\n", N);
+
+  // host side memory
+  float *subject = (float*) malloc (sizeof(float)*N);
+  float *lower_bound = (float*) malloc (sizeof(float)*N);
+  float *upper_bound = (float*) malloc (sizeof(float)*N);
+  float *lb = (float*) malloc (sizeof(float)*(N-M+1));
+  float *lb_h = (float*) malloc (sizeof(float)*(N-M+1));
+  float *avgs = (float*) malloc (sizeof(float)*(N-M+1));
+  float *stds = (float*) malloc (sizeof(float)*(N-M+1));
+
+  srand(123);
+  for (int i = 0; i < N; ++i) subject[i] = (float)rand() / (float)RAND_MAX;
+  for (int i = 0; i < N-M+1; ++i) avgs[i] = (float)rand() / (float)RAND_MAX;
+  for (int i = 0; i < N-M+1; ++i) stds[i] = (float)rand() / (float)RAND_MAX;
+  for (int i = 0; i < M; ++i) upper_bound[i] = (float)rand() / (float)RAND_MAX;
+  for (int i = 0; i < M; ++i) lower_bound[i] = (float)rand() / (float)RAND_MAX;
+
+  const int blocks = 256;
+  const int grids = (N-M+1 + blocks - 1) / blocks;
+
+  #pragma omp target data map (to: subject[0:N], \
+                                   avgs[0:N-M+1],\
+                                   stds[0:N-M+1],\
+                                   lower_bound[0:N],\
+                                   upper_bound[0:N])\
+                          map(from: lb[0:N-M+1])
+  {
+    for (int i = 0; i < 100; i++) {
+      #pragma omp target teams distribute num_teams(grids) thread_limit(blocks)
+      for (int idx = 0; idx < N-M+1; idx++) {
+        // obtain statistics
+        float residues = 0;
+        float avg = avgs[idx];
+        float std = stds[idx];
+
+        #pragma omp parallel for reduction(+:residues)
+        for (int i = 0; i < M; ++i) {
+          // differences to envelopes
+          float value = (subject[idx+i] - avg) / std;
+          float lower = value - lower_bound[i];
+          float upper = value - upper_bound[i];
+
+          // Euclidean distance
+          residues += upper*upper*(upper > 0) + lower*lower*(lower < 0);
+        }
+
+        lb[idx] = residues;
+      }
+    }
+  }
+
+  // verify
+  reference(subject, avgs, stds, lb_h, lower_bound, upper_bound, M, N);
+  bool ok = true;
+  for (int i = 0; i < N-M+1; i++) {
+    if (fabs(lb[i] - lb_h[i]) > 1e-3) {
+      printf("%d %f %f\n", i, lb[i], lb_h[i]);
+      ok = false;
+      break;
+    }
+  }
+  printf("%s\n", ok ? "PASS" : "FAIL");
+
+  free(lb);
+  free(lb_h);
+  free(avgs);
+  free(stds);
+  free(subject);
+  free(lower_bound);
+  free(upper_bound);
+  return 0;
+}
+
+
