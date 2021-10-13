e9bff396802634d6bf9f8e73db9467ef2b0dca55
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2021-10-13 09:29:34 -0400

    the SYCL port causes hanging on a V100

--- bsw-sycl/utils.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_HPP
-#define UTILS_HPP
-#include "driver.hpp"
-
-unsigned getMaxLength (std::vector<std::string> v);
-int get_new_min_length(short* alAend, short* alBend, int blocksLaunched);
-#endif
