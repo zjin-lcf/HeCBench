fdf0247b1c08151f7df103a6e0df78707abae518
Jin Z <5zj@oswald00.ftpn.ornl.gov>
2021-10-14 12:42:39 -0400

    slightly format the file

--- extend2-sycl/read_data.cpp
+++ extend2-sycl/read_data.cpp
@@ -14,18 +14,14 @@
 /* return non-zero on success */
 static int read_check(int fd, void *buf, size_t sz)
 {
-  if(read(fd, buf, sz) != (ssize_t)sz)
-    return 0;
+  if(read(fd, buf, sz) != (ssize_t)sz) return 0;
   return 1;
 }
 
 /* return non-zero on success */
 int read_data(const char *fn, struct extend2_dat *d)
 {
-
-  if (!d)
-    return 0;
-
+  if (!d) return 0;
   int fd = open(fn, O_RDONLY);
   if (fd < 0) {
     perror("Cannot open output file\n");
@@ -36,14 +32,11 @@ int read_data(const char *fn, struct extend2_dat *d)
   assert( read_check(fd, (void*)&d->qlen, sizeof(int)) );
   d->query = (uint8_t*)malloc(d->qlen);
   assert( read_check(fd, d->query, d->qlen) );
-
   assert( read_check(fd, (void*)&d->tlen, sizeof(int)) );
   d->target = (uint8_t*)malloc(d->tlen);
   assert( read_check(fd, d->target, d->tlen) );
-
   assert( read_check(fd, (void*)&d->m, sizeof(int)) );
   assert( read_check(fd, (void*)d->mat, 25) );
-
   assert( read_check(fd, (void*)&d->o_del, sizeof(int)) );
   assert( read_check(fd, (void*)&d->e_del, sizeof(int)) );
   assert( read_check(fd, (void*)&d->o_ins, sizeof(int)) );
