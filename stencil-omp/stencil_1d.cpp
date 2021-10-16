10df2e9c6160350cd3340560b285d1702f039487
Jin Z <5zj@leconte.ftpn.ornl.gov>
2021-10-16 08:41:44 -0400

    remove my name, replace headers, and update the error checking in the 1D stencil

--- stencil-omp/stencil_1d.cpp
+++ stencil-omp/stencil_1d.cpp
@@ -3,20 +3,15 @@
    Here, the 1D stencil kernel adds all its neighboring data within a radius.
 
    The C model is added to verify the stencil result on a GPU
-
-Developer: Zheming Jin
 */
-
-#define LENGTH 134217728
-//#define LENGTH 2048
-#define THREADS_PER_BLOCK 256
-#define RADIUS 7
-#define BLOCK_SIZE THREADS_PER_BLOCK
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
+#define LENGTH 134217728
+#define THREADS_PER_BLOCK 256
+#define RADIUS 7
+#define BLOCK_SIZE THREADS_PER_BLOCK
 
 int main(void) {
   int size = LENGTH;
@@ -28,10 +23,10 @@ int main(void) {
 
   for (int i = 0; i < LENGTH+RADIUS; i++) a[i] = i;
 
-#pragma omp target teams distribute map(to: a[0:pad_size]) map(from:b[0:size]) 
+  #pragma omp target teams distribute map(to: a[0:pad_size]) map(from:b[0:size]) 
   for (int i = 0; i < LENGTH; i = i + THREADS_PER_BLOCK) {
     int temp[BLOCK_SIZE + 2 * RADIUS];
-#pragma omp parallel for schedule(static,1)
+    #pragma omp parallel for schedule(static,1)
     for (int j = 0; j < THREADS_PER_BLOCK; j++) {
       int gindex = i+j;
       temp[j+RADIUS] = a[gindex]; 
@@ -41,7 +36,7 @@ int main(void) {
       }
     }
 
-#pragma omp parallel for schedule(static,1)
+    #pragma omp parallel for schedule(static,1)
     for (int j = 0; j < THREADS_PER_BLOCK; j++) {
       int result = 0;
       for (int offset = -RADIUS ; offset <= RADIUS ; offset++)
@@ -50,36 +45,33 @@ int main(void) {
     }
   }
 
-
   // verification
-  bool error = false;
+  bool ok = true;
   for (int i = 0; i < 2*RADIUS; i++) {
     int s = 0;
-    for (int j = i; j <= i+2*RADIUS; j++) {
+    for (int j = i; j <= i+2*RADIUS; j++)
       s += j < RADIUS ? 0 : (a[j] - RADIUS);
-    }
     if (s != b[i]) {
-      printf("FAILED at %d: %d (cpu) != %d (gpu)\n", i, s, b[i]);
-      error = true;
+      printf("Error at %d: %d (host) != %d (device)\n", i, s, b[i]);
+      ok = false;
       break;
     }
   }
 
   for (int i = 2*RADIUS; i < LENGTH; i++) {
     int s = 0;
-    for (int j = i-RADIUS; j <= i+RADIUS; j++) {
+    for (int j = i-RADIUS; j <= i+RADIUS; j++)
       s += a[j];
-    }
     if (s != b[i]) {
-      printf("FAILED at %d: %d (cpu) != %d (gpu)\n", i, s, b[i]);
-      error = true;
+      printf("Error at %d: %d (host) != %d (device)\n", i, s, b[i]);
+      ok = false;
       break;
     }
   }
+  printf("%s\n", ok ? "PASS" : "FAIL");
 
   // Cleanup
   free(a);
   free(b); 
-  if (!error) printf("PASSED\n");
   return 0;
 }
