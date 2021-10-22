b0af9f3ab250d04f93c0304d8871d3035b56a162
Jin Z <5zj@oswald00.ftpn.ornl.gov>
2021-10-22 08:51:01 -0400

    minimize the d2h copy overhead with floating-point atomic max reduction in the kernel

--- lebesgue-cuda/kernels.cu
+++ lebesgue-cuda/kernels.cu
@@ -2,8 +2,22 @@
 #include <stdlib.h>
 #include <cuda.h>
 
+// double-precision atomic max
+__device__ __forceinline__
+double atomicMax(double *address, double val)
+{
+  unsigned long long ret = __double_as_longlong(*address);
+  while(val > __longlong_as_double(ret))
+  {
+    unsigned long long old = ret;
+    if((ret = atomicCAS((unsigned long long *)address, old, __double_as_longlong(val))) == old)
+      break;
+  }
+  return __longlong_as_double(ret);
+}
+
 __global__
-void kernel (double *__restrict__ lfun,
+void kernel (double *__restrict__ lmax,
              double *__restrict__ linterp,
              const double *__restrict__ xfun, 
              const double *__restrict__ x,
@@ -11,46 +25,47 @@ void kernel (double *__restrict__ lfun,
 {
   int j = blockIdx.x * blockDim.x + threadIdx.x;
   if (j >= nfun) return;
-  if ( n == 1 ) {
-    lfun[j] = 1.0;
-  } else {
-    for (int i = 0; i < n; i++ )
-      linterp[i*nfun+j] = 1.0;
+  for (int i = 0; i < n; i++ )
+    linterp[i*nfun+j] = 1.0;
 
-    for (int i1 = 0; i1 < n; i1++ )
-      for (int i2 = 0; i2 < n; i2++ )
-        if ( i1 != i2 )
-          linterp[i1*nfun+j] = linterp[i1*nfun+j] * ( xfun[j] - x[i2] ) / ( x[i1] - x[i2] );
+  for (int i1 = 0; i1 < n; i1++ )
+    for (int i2 = 0; i2 < n; i2++ )
+      if ( i1 != i2 )
+        linterp[i1*nfun+j] = linterp[i1*nfun+j] * ( xfun[j] - x[i2] ) / ( x[i1] - x[i2] );
 
-    double t = 0.0;
-    for (int i = 0; i < n; i++ )
-      t += fabs ( linterp[i*nfun+j] );
+  double t = 0.0;
+  for (int i = 0; i < n; i++ )
+    t += fabs ( linterp[i*nfun+j] );
 
-    lfun[j] = t;
-  }
+  atomicMax(lmax, t);
 }
 
-double *lebesgue_function ( int n, double x[], int nfun, double xfun[] )
+double lebesgue_function ( int n, double x[], int nfun, double xfun[] )
 {
   double *lfun = ( double * ) malloc ( nfun * sizeof ( double ) );
+  double lmax = 0.0;
 
-  double *d_fun, *d_interp, *d_xfun, *d_x;
-  cudaMalloc((void**)&d_fun, nfun * sizeof ( double ) );
+  double *d_max, *d_interp, *d_xfun, *d_x;
+  cudaMalloc((void**)&d_max, sizeof ( double ) );
+  cudaMemcpy(d_max, &lmax, sizeof ( double ), cudaMemcpyHostToDevice );
+
   cudaMalloc((void**)&d_interp, n * nfun * sizeof ( double ) );
-  cudaMalloc((void**)&d_x, n * sizeof ( double ) );
-  cudaMalloc((void**)&d_xfun, nfun * sizeof ( double ) );
 
+  cudaMalloc((void**)&d_x, n * sizeof ( double ) );
   cudaMemcpy(d_x, x, n * sizeof ( double ), cudaMemcpyHostToDevice );
+
+  cudaMalloc((void**)&d_xfun, nfun * sizeof ( double ) );
   cudaMemcpy(d_xfun, xfun, nfun * sizeof ( double ), cudaMemcpyHostToDevice );
-  
+
   dim3 grids ((nfun + 255)/256);
   dim3 blocks (256);
 
-  kernel<<<grids, blocks>>> (d_fun, d_interp, d_xfun, d_x, n, nfun);
-  cudaMemcpy(lfun, d_fun, nfun * sizeof ( double ), cudaMemcpyDeviceToHost );
-  cudaFree(d_fun);
+  kernel<<<grids, blocks>>> (d_max, d_interp, d_xfun, d_x, n, nfun);
+  cudaMemcpy(&lmax, d_max, sizeof ( double ), cudaMemcpyDeviceToHost );
+
+  cudaFree(d_max);
   cudaFree(d_interp);
   cudaFree(d_xfun);
   cudaFree(d_x);
-  return lfun;
+  return lmax;
 }
