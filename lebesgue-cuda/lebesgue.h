bef16404785992b6f85d2dcf7b54355fc085140a
Jin Z <5zj@oswald00.ftpn.ornl.gov>
2021-10-22 08:02:58 -0400

    add the driver and utilities

---
+++ lebesgue-cuda/lebesgue.h
@@ -0,0 +1,15 @@
+double *chebyshev1 ( int n );
+double *chebyshev2 ( int n );
+double *chebyshev3 ( int n );
+double *chebyshev4 ( int n );
+double *equidistant1 ( int n );
+double *equidistant2 ( int n );
+double *equidistant3 ( int n );
+double *fejer1 ( int n );
+double *fejer2 ( int n );
+double lebesgue_constant ( int n, double x[], int nfun, double xfun[] );
+double *lebesgue_function ( int n, double x[], int nfun, double xfun[] );
+double *r8vec_linspace_new ( int n, double a, double b );
+double r8vec_max ( int n, double r8vec[] );
+void r8vec_print ( int n, double a[], char *title );
+void timestamp ( );
