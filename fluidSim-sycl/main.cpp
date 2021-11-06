cbccee1af934eb99560ee87539eb144e902f2f52
Jin Z <5zj@oswald00.ftpn.ornl.gov>
2021-11-06 13:20:59 -0400

    change the order of de of the device function parameters for consistence with the host function; format the source files slightly

--- fluidSim-sycl/main.cpp
+++ fluidSim-sycl/main.cpp
@@ -5,23 +5,23 @@
 
 int main(int argc, char * argv[])
 {
-  if (argc != 4) {
-    std::cout << "Usage ./main <iterations> <width> <height>\n";
-    std::cout << "Width and height are a multiple of 256\n";
+  if (argc != 2) {
+    std::cout << "Usage ./main <iterations>\n";
     return 1;
   }
-  int iterations = atoi(argv[1]);  // Simulation iterations
-  int lbm_width = atoi(argv[2]);   // Dimension of LBM simulation area
-  int lbm_height = atoi(argv[3]);
+
+  const int iterations = atoi(argv[1]); // Simulation iterations
+  const int lbm_width = 256;            // Dimension of LBM simulation area
+  const int lbm_height = 256;
 
   int dims[2] = {lbm_width, lbm_height};
   size_t temp = dims[0] * dims[1];
 
-   // Directions
-   double e[9][2] = {{0,0}, {1,0}, {0,1}, {-1,0}, {0,-1}, {1,1}, {-1,1}, {-1,-1}, {1,-1}};
+  // Directions
+  double e[9][2] = {{0,0}, {1,0}, {0,1}, {-1,0}, {0,-1}, {1,1}, {-1,1}, {-1,-1}, {1,-1}};
    
-   // Weights
-   double w[9] = {4.0/9.0, 1.0/9.0, 1.0/9.0, 1.0/9.0, 1.0/9.0, 1.0/36.0, 1.0/36.0, 1.0/36.0, 1.0/36.0};
+  // Weights
+  double w[9] = {4.0/9.0, 1.0/9.0, 1.0/9.0, 1.0/9.0, 1.0/9.0, 1.0/36.0, 1.0/36.0, 1.0/36.0, 1.0/36.0};
 
   // Omega
   const double omega = 1.2f;
@@ -68,17 +68,18 @@ int main(int argc, char * argv[])
       double den = rand() % 10 + 1;
 
       // Initialize the velocity buffer
-      u[pos] = double2(u0[0], u0[1]);
+      u[pos].x() = u0[0];
+      u[pos].y() = u0[1];
 
-      h_if0[pos] = computefEq(w[0], e[0], den, u0);
-      h_if1234[pos * 4 + 0] = computefEq(w[1], e[1], den, u0);
-      h_if1234[pos * 4 + 1] = computefEq(w[2], e[2], den, u0);
-      h_if1234[pos * 4 + 2] = computefEq(w[3], e[3], den, u0);
-      h_if1234[pos * 4 + 3] = computefEq(w[4], e[4], den, u0);
-      h_if5678[pos * 4 + 0] = computefEq(w[5], e[5], den, u0);
-      h_if5678[pos * 4 + 1] = computefEq(w[6], e[6], den, u0);
-      h_if5678[pos * 4 + 2] = computefEq(w[7], e[7], den, u0);
-      h_if5678[pos * 4 + 3] = computefEq(w[8], e[8], den, u0);
+      h_if0[pos]            = computefEq(den, w[0], e[0], u0);
+      h_if1234[pos * 4 + 0] = computefEq(den, w[1], e[1], u0);
+      h_if1234[pos * 4 + 1] = computefEq(den, w[2], e[2], u0);
+      h_if1234[pos * 4 + 2] = computefEq(den, w[3], e[3], u0);
+      h_if1234[pos * 4 + 3] = computefEq(den, w[4], e[4], u0);
+      h_if5678[pos * 4 + 0] = computefEq(den, w[5], e[5], u0);
+      h_if5678[pos * 4 + 1] = computefEq(den, w[6], e[6], u0);
+      h_if5678[pos * 4 + 2] = computefEq(den, w[7], e[7], u0);
+      h_if5678[pos * 4 + 3] = computefEq(den, w[8], e[8], u0);
 
       // Initialize boundary cells
       if (x == 0 || x == (dims[0] - 1) || y == 0 || y == (dims[1] - 1))
