808575c178c0c08486c24d6be8d88d984be31909
Jin Z <5zj@leconte.ftpn.ornl.gov>
2021-11-12 11:36:19 -0500

    Intel LLVM #2793: Fix the buffer access mode and upgrade the examples with the number of reverse operations odd or even

--- reverse-hip/main.cu
+++ reverse-hip/main.cu
@@ -1,33 +1,69 @@
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 #include <hip/hip_runtime.h>
 
-__global__ void reverse (int* d, const int len)
+__global__ void reverse (int *d, const int len)
 {
   __shared__ int s[256];
   int t = threadIdx.x;
-  int tr = len-t-1;
   s[t] = d[t];
   __syncthreads();
-  d[t] = s[tr];
+  d[t] = s[len-t-1];
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+
+  if (argc != 2) {
+    printf("Usage: ./%s <iterations>\n", argv[0]);
+    return 1;
+  }
+
+  // specify the number of test cases
+  const int iteration = atoi(argv[1]);
+
+  // number of elements to reverse
   const int len = 256;
-  const int iteration = 1 << 20;
-  int d[len];
-  for (int i = 0; i < len; i++) d[i] = i;
+  const int elem_size = len * sizeof(int);
 
-  int *dd;
-  hipMalloc((void**)&dd, sizeof(int)*len);
-  hipMemcpy(dd, d, sizeof(int)*len, hipMemcpyHostToDevice);
-  for (int i = 0; i <= iteration; i++)
-    hipLaunchKernelGGL(reverse, dim3(1), dim3(256), 0, 0, dd, len);
-  hipMemcpy(d, dd, sizeof(int)*len, hipMemcpyDeviceToHost);
-  hipFree(dd);
+  // device result
+  int test[len];
 
-  for (int i = 0; i < len; i++) assert(d[i] == len-i-1);
-  printf("PASS\n");
+  // expected results after reverse operations even/odd times
+  int error = 0;
+  int gold_odd[len];
+  int gold_even[len];
 
+  for (int i = 0; i < len; i++) {
+    gold_odd[i] = len-i-1;
+    gold_even[i] = i;
+  }
+
+  int *d_test;
+  hipMalloc((void**)&d_test, elem_size);
+
+  srand(123);
+  for (int i = 0; i < iteration; i++) {
+
+    const int count = rand() % 10000 + 100;  // bound the reverse range
+
+    hipMemcpy(d_test, gold_even, elem_size, hipMemcpyHostToDevice);
+
+    for (int j = 0; j < count; j++)
+      hipLaunchKernelGGL(reverse, dim3(1), dim3(len), 0, 0, d_test, len);
+
+    hipMemcpy(test, d_test, elem_size, hipMemcpyDeviceToHost);
+
+    if (count % 2 == 0)
+      error = memcmp(test, gold_even, elem_size);
+    else
+      error = memcmp(test, gold_odd, elem_size);
+    
+    if (error) break;
+  }
+  
+  printf("%s\n", error ? "FAIL" : "PASS");
+
+  hipFree(d_test);
   return 0;
 }
--- reverse-omp/main.cpp
+++ reverse-omp/main.cpp
@@ -1,33 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <omp.h>
-#include <assert.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+
+  if (argc != 2) {
+    printf("Usage: ./%s <iterations>\n", argv[0]);
+    return 1;
+  }
+
+  // specify the number of test cases
+  const int iteration = atoi(argv[1]);
+
+  // number of elements to reverse
   const int len = 256;
-  const int iteration = 1 << 20;
-  int d[len];
-  for (int i = 0; i < len; i++) d[i] = i;
+  const int elem_size = len * sizeof(int);
 
-  #pragma omp target data map(tofrom: d[0:len]) 
+  // device result
+  int test[len];
+
+  // expected results after reverse operations even/odd times
+  int error = 0;
+  int gold_odd[len];
+  int gold_even[len];
+
+  for (int i = 0; i < len; i++) {
+    gold_odd[i] = len-i-1;
+    gold_even[i] = i;
+  }
+
+  #pragma omp target data map(alloc: test[0:len]) 
   {
-    for (int i = 0; i <= iteration; i++) {
-      #pragma omp target teams num_teams(1) thread_limit(len)
-      { 
-        int s[len];
-        #pragma omp parallel 
+    srand(123);
+    for (int i = 0; i < iteration; i++) {
+
+      const int count = rand() % 10000 + 100;  // bound the reverse range
+
+      memcpy(test, gold_even, elem_size);
+      #pragma omp target update to (test[0:len])
+
+      for (int j = 0; j < count; j++) {
+        #pragma omp target teams num_teams(1) thread_limit(len)
         {
-          int t = omp_get_thread_num();
-          int tr = len-t-1;
-          s[t] = d[t];
-          #pragma omp barrier
-          d[t] = s[tr];
+          int s[len];
+          #pragma omp parallel 
+          {
+            int t = omp_get_thread_num();
+            s[t] = test[t];
+            #pragma omp barrier
+            test[t] = s[len-t-1];
+          }
         }
       }
+
+      #pragma omp target update from (test[0:len])
+
+      if (count % 2 == 0)
+        error = memcmp(test, gold_even, elem_size);
+      else
+        error = memcmp(test, gold_odd, elem_size);
+      
+      if (error) break;
     }
   }
 
-  for (int i = 0; i < len; i++) assert(d[i] == len-i-1);
-  printf("PASS\n");
+  printf("%s\n", error ? "FAIL" : "PASS");
 
   return 0;
 }
