eccf701a730d6a4c3283a96012e1ac27870250c8
Jin Z <5zj@leconte.ftpn.ornl.gov>
2021-11-12 11:36:19 -0500

    Intel LLVM #2793: Fix the buffer access mode and upgrade the examples with the number of reverse operations odd or even

--- reverse-cuda/main.cu
+++ reverse-cuda/main.cu
@@ -1,33 +1,69 @@
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 #include <cuda.h>
 
-__global__ void reverse (int* d, const int len)
+__global__ void reverse (int *d, const int len)
 {
   __shared__ int s[256];
   int t = threadIdx.x;
-  int tr = len-t-1;
   s[t] = d[t];
   __syncthreads();
-  d[t] = s[tr];
+  d[t] = s[len-t-1];
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+
+  if (argc != 2) {
+    printf("Usage: ./%s <iterations>\n", argv[0]);
+    return 1;
+  }
+
+  // specify the number of test cases
+  const int iteration = atoi(argv[1]);
+
+  // number of elements to reverse
   const int len = 256;
-  const int iteration = 1 << 20;
-  int d[len];
-  for (int i = 0; i < len; i++) d[i] = i;
+  const int elem_size = len * sizeof(int);
 
-  int *dd;
-  cudaMalloc((void**)&dd, sizeof(int)*len);
-  cudaMemcpy(dd, d, sizeof(int)*len, cudaMemcpyHostToDevice);
-  for (int i = 0; i <= iteration; i++)
-    reverse<<<1, 256>>> (dd, len);
-  cudaMemcpy(d, dd, sizeof(int)*len, cudaMemcpyDeviceToHost);
-  cudaFree(dd);
+  // device result
+  int test[len];
 
-  for (int i = 0; i < len; i++) assert(d[i] == len-i-1);
-  printf("PASS\n");
+  // expected results after reverse operations even/odd times
+  int error = 0;
+  int gold_odd[len];
+  int gold_even[len];
 
+  for (int i = 0; i < len; i++) {
+    gold_odd[i] = len-i-1;
+    gold_even[i] = i;
+  }
+
+  int *d_test;
+  cudaMalloc((void**)&d_test, elem_size);
+
+  srand(123);
+  for (int i = 0; i < iteration; i++) {
+
+    const int count = rand() % 10000 + 100;  // bound the reverse range
+
+    cudaMemcpy(d_test, gold_even, elem_size, cudaMemcpyHostToDevice);
+
+    for (int j = 0; j < count; j++)
+      reverse<<<1, len>>> (d_test, len);
+
+    cudaMemcpy(test, d_test, elem_size, cudaMemcpyDeviceToHost);
+
+    if (count % 2 == 0)
+      error = memcmp(test, gold_even, elem_size);
+    else
+      error = memcmp(test, gold_odd, elem_size);
+    
+    if (error) break;
+  }
+  
+  printf("%s\n", error ? "FAIL" : "PASS");
+
+  cudaFree(d_test);
   return 0;
 }
--- reverse-sycl/main.cpp
+++ reverse-sycl/main.cpp
@@ -1,39 +1,82 @@
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 #include "common.h"
 
-int main() {
-  const int len = 256;
-  const int iteration = 1; //1 << 20;
-  int d[len];
-  for (int i = 0; i < len; i++) d[i] = i;
+int main(int argc, char* argv[]) {
 
-  {
+  if (argc != 2) {
+    printf("Usage: ./%s <iterations>\n", argv[0]);
+    return 1;
+  }
+
+  // specify the number of test cases
+  const int iteration = atoi(argv[1]);
+
+  // number of elements to reverse
+  const int len = 256;
+
+  // device result
+  int test[len];
+
+  // expected results after reverse operations even/odd times
+  int error = 0;
+  int gold_odd[len];
+  int gold_even[len];
+
+  for (int i = 0; i < len; i++) {
+    gold_odd[i] = len-i-1;
+    gold_even[i] = i;
+  }
+
 #ifdef USE_GPU
   gpu_selector dev_sel;
 #else
   cpu_selector dev_sel;
 #endif
-  queue q(dev_sel); //, {property::queue::in_order()});
 
-  buffer<int, 1> dd (d, len);
-  for (int i = 0; i <= iteration; i++) {
-    q.submit([&](handler &h) {
-      accessor <int, 1, sycl_read_write, access::target::local> s (len, h);
-      auto d = dd.get_access<sycl_discard_read_write>(h);
-      h.parallel_for<class scan>(nd_range<1>(range<1>(len), range<1>(len)), [=](nd_item<1> item) {
-        int t = item.get_local_id(0);
-        int tr = len-t-1;
-        s[t] = d[t];
-        item.barrier(access::fence_space::local_space);
-        d[t] = s[tr];
-      });
+  queue q(dev_sel);
+  
+  buffer<int, 1> d_test (len);
+  range<1> gws (len);
+  range<1> lws (len);
+
+  srand(123);
+  for (int i = 0; i < iteration; i++) {
+
+    const int count = rand() % 10000 + 100;  // bound the reverse range
+
+    q.submit([&](handler &cgh) {
+      auto acc = d_test.get_access<sycl_discard_write>(cgh);
+      cgh.copy(gold_even, acc);
     });
+      
+    for (int j = 0; j < count; j++)
+      q.submit([&](handler &cgh) {
+        accessor <int, 1, sycl_read_write, access::target::local> s (len, cgh);
+        auto acc = d_test.get_access<sycl_read_write>(cgh);
+        cgh.parallel_for<class blockReverse>(nd_range<1>(gws, lws), [=](nd_item<1> item) {
+          int t = item.get_local_id(0);
+          s[t] = acc[t];
+          item.barrier(access::fence_space::local_space);
+          acc[t] = s[len-t-1];
+        });
+      });
+
+    q.submit([&](handler &cgh) {
+      auto acc = d_test.get_access<sycl_read>(cgh);
+      cgh.copy(acc, test);
+    }).wait();
+
+    if (count % 2 == 0)
+      error = memcmp(test, gold_even, len*sizeof(int));
+    else
+      error = memcmp(test, gold_odd, len*sizeof(int));
+    
+    if (error) break;
   }
-  q.wait();
-  }
-  for (int i = 0; i < len; i++) assert(d[i] == len-i-1);
-  printf("PASS\n");
+  
+  printf("%s\n", error ? "FAIL" : "PASS");
 
   return 0;
 }
