cd4c1845fedd2f3b05314283b3dc2be85fc1c4bf
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2021-12-04 22:27:34 -0500

    add the cbsfil examples

---
+++ cbsfil-cuda/kernels.h
@@ -0,0 +1,95 @@
+
+#ifndef _CUBIC_BSPLINE_PREFILTER_KERNEL_H_
+#define _CUBIC_BSPLINE_PREFILTER_KERNEL_H_
+
+// The code below is based on the work of Philippe Thevenaz.
+// See <http://bigwww.epfl.ch/thevenaz/interpolation/>
+
+#define POLE (sqrtf(3.0f)-2.0f)  //pole for cubic b-spline
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+__device__ float InitialCausalCoefficient(
+    float* c,         // coefficients
+    uint DataLength,  // number of coefficients
+    int step)         // element interleave in bytes
+{
+  const uint Horizon = 12 < DataLength ? 12 : DataLength;
+
+  // this initialization corresponds to clamping boundaries
+  // accelerated loop
+  float zn = POLE;
+  float Sum = *c;
+  for (uint n = 0; n < Horizon; n++) {
+    Sum += zn * *c;
+    zn *= POLE;
+    c = (float*)((uchar*)c + step);
+  }
+  return(Sum);
+}
+
+__device__ float InitialAntiCausalCoefficient(
+    float* c,         // last coefficient
+    uint DataLength,  // number of samples or coefficients
+    int step)         // element interleave in bytes
+{
+  // this initialization corresponds to clamping boundaries
+  return((POLE / (POLE - 1.0f)) * *c);
+}
+
+__device__ void ConvertToInterpolationCoefficients(
+    float* coeffs,    // input samples --> output coefficients
+    uint DataLength,  // number of samples or coefficients
+    int step)         // element interleave in bytes
+{
+  // compute the overall gain
+  const float Lambda = (1.0f - POLE) * (1.0f - 1.0f / POLE);
+
+  // causal initialization
+  float* c = coeffs;
+  float previous_c;  //cache the previously calculated c rather than look it up again (faster!)
+  *c = previous_c = Lambda * InitialCausalCoefficient(c, DataLength, step);
+  // causal recursion
+  for (uint n = 1; n < DataLength; n++) {
+    c = (float*)((uchar*)c + step);
+    *c = previous_c = Lambda * *c + POLE * previous_c;
+  }
+  // anticausal initialization
+  *c = previous_c = InitialAntiCausalCoefficient(c, DataLength, step);
+  // anticausal recursion
+  for (int n = DataLength - 2; 0 <= n; n--) {
+    c = (float*)((uchar*)c - step);
+    *c = previous_c = POLE * (previous_c - *c);
+  }
+}
+
+__global__ void toCoef2DX(
+    float* image,
+    uint pitch,
+    uint width,
+    uint height)
+{
+  // process lines horizontally
+  const uint y = blockIdx.x * blockDim.x + threadIdx.x;
+  if (y < height) {
+    float* line = (float*)((uchar*)image + y * pitch);  //direct access
+    ConvertToInterpolationCoefficients(line, width, sizeof(float));
+  }
+}
+
+__global__ void toCoef2DY(
+    float* image,
+    uint pitch,
+    uint width,
+    uint height)
+{
+  // process lines vertically
+  const uint x = blockIdx.x * blockDim.x + threadIdx.x;
+  if (x < width) {
+    float* line = image + x;  //direct access
+    ConvertToInterpolationCoefficients(line, height, pitch);
+  }
+}
+
+#endif // _CUBIC_BSPLINE_PREFILTER_KERNEL_H_
---
+++ cbsfil-cuda/main.cu
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <cuda.h>
+#include "kernels.h"
+
+int PowTwoDivider(int n)
+{
+  if (n == 0) return 0;
+  int divider = 1;
+  while ((n & divider) == 0) divider <<= 1;
+  return divider;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 4) {
+    printf("Usage: %s <width> <height> <repeat>\n", argv[0]);
+    return 1;
+  }
+
+  const int width = atoi(argv[1]);
+  const int height = atoi(argv[2]);
+  const int repeat = atoi(argv[3]);
+
+  const int image_pitch = width * sizeof(float);
+  const int numPix = width * height;
+  const int image_size = numPix * sizeof(float);
+
+  float *image = (float*) malloc (image_size);
+
+  // image image with random values
+  srand(123);
+  for (int i = 0; i < numPix; i++) {
+    uint x = rand() % 256;
+    uint y = rand() % 256;
+    uint z = rand() % 256;
+    uint w = rand() % 256;
+    *(uint*)(&image[i]) = (w << 24) | (z << 16) | (y << 8) | x;
+  }
+
+  float *d_image;
+  cudaMalloc((void**)&d_image, image_size);
+
+  int blocks = std::min(PowTwoDivider(height), 64);
+  dim3 dimBlockX (blocks);
+  dim3 dimGridX ((height + blocks - 1) / blocks);
+
+  blocks = std::min(PowTwoDivider(width), 64);
+  dim3 dimBlockY (blocks);
+  dim3 dimGridY ((width  + blocks - 1) / blocks);
+
+  for (int i = 0; i < repeat; i++) {
+    cudaMemcpy(d_image, image, image_size, cudaMemcpyHostToDevice);
+    toCoef2DX<<<dimGridX, dimBlockX>>>(d_image, image_pitch, width, height);
+    toCoef2DY<<<dimGridY, dimBlockY>>>(d_image, image_pitch, width, height);
+  }
+
+  cudaMemcpy(image, d_image, image_size, cudaMemcpyDeviceToHost);
+
+  float sum = 0.f;
+  for (int i = 0; i < numPix; i++) {
+    const uchar *t = (const uchar*)(&image[i]);
+    sum += t[0] + t[1] + t[2] + t[3];
+  }
+  printf("Checksum: %f\n", sum);
+
+  cudaFree(d_image);
+  free(image);
+  return 0;
+}
