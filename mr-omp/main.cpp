e3e6303c0c438eaa37c298656863f0145461244b
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2021-12-11 16:58:24 -0500

    the mr examples reuse the kernel details (sprp32*.h) in mr-cuda; -Wno-narrowing disables the compiler error when converting int type to char type

---
+++ mr-omp/main.cpp
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <omp.h>
+#include "benchmark.h"
+#include "kernels.h"
+
+void run_benchmark()
+{
+  int i, j, cnt, val_ref, val_eff;
+  uint64_t time_vals[SIZES_CNT_MAX][BASES_CNT_MAX][2];
+
+  int bases32_size = sizeof(bases32) / sizeof(bases32[0]);
+
+  const uint32_t *d_bases32 = bases32;
+  uint32_t *d_n32 = (uint32_t*) malloc (sizeof(uint32_t) * BENCHMARK_ITERATIONS);
+  int val_dev;
+  int *d_val = &val_dev;
+
+  printf("Starting benchmark...\n");
+
+  #pragma omp target data map (to: d_bases32[0:bases32_size]) \
+                          map (alloc: d_n32[0:BENCHMARK_ITERATIONS], d_val[0:1])
+  {
+    for (i = 0; i < SIZES_CNT32; i++) {
+      val_ref = val_eff = 0;
+
+      memcpy(d_n32, n32[i], sizeof(uint32_t) * BENCHMARK_ITERATIONS);
+      #pragma omp target update to (d_n32[0:BENCHMARK_ITERATIONS])
+      
+      #pragma omp target 
+      d_val[0] = 0;
+
+      for (cnt = 1; cnt <= BASES_CNT32; cnt++) {
+        time_point start = get_time();
+        for (j = 0; j < BENCHMARK_ITERATIONS; j++)
+          val_eff += efficient_mr32(bases32, cnt, n32[i][j]);
+        time_vals[i][cnt - 1][0] = elapsed_time(start);
+      }
+
+      for (cnt = 1; cnt <= BASES_CNT32; cnt++) {
+        time_point start = get_time();
+        for (j = 0; j < BENCHMARK_ITERATIONS; j++)
+          val_ref += straightforward_mr32(bases32, cnt, n32[i][j]);
+        time_vals[i][cnt - 1][1] = elapsed_time(start);
+      }
+
+      // verify the results of simple and efficient versions on a host
+      if (val_ref != val_eff) {
+        fprintf(stderr, "Results mismatch: val_ref = %d, val_eff = %d\n", val_ref, val_eff);
+        break;
+      }
+
+      // the efficient version is faster than the simple version on a device
+      mr32_sf(d_bases32, d_n32, d_val, BENCHMARK_ITERATIONS);
+      #pragma omp target update from (d_val[0:1])
+
+      if (val_ref != val_dev) {
+        fprintf(stderr, "Results mismatch: val_dev = %d, val_ref = %d\n", val_dev, val_ref);
+        break;
+      }
+
+      #pragma omp target 
+      d_val[0] = 0;
+
+      mr32_eff(d_bases32, d_n32, d_val, BENCHMARK_ITERATIONS);
+      #pragma omp target update from (d_val[0:1])
+
+      if (val_ref != val_dev) {
+        fprintf(stderr, "Results mismatch: val_dev = %d, val_ref = %d\n", val_dev, val_ref);
+        break;
+      }
+    }
+  }
+
+  // device results are not included
+  print_results(bits32, SIZES_CNT32, BASES_CNT32, time_vals);
+  free(d_n32);
+}
+
+int main()
+{
+#ifdef _WIN32
+  system("mode CON: COLS=98");
+#endif
+
+  printf("Setting random primes...\n");
+  set_nprimes();
+  run_benchmark();
+
+  printf("Setting random odd integers...\n");
+  set_nintegers();
+  run_benchmark();
+
+  return 0;
+}
---
+++ mr-sycl/kernels.h
@@ -0,0 +1,44 @@
+#include "common.h"
+
+void mr32_sf(
+  nd_item<1> &item,
+  const uint32_t *__restrict__ bases,
+  const uint32_t *__restrict__ n32,
+  int *__restrict__ val,
+  int iter)
+{
+  int j = item.get_global_id(0);
+  if (j < iter) {
+    int n = n32[j];
+    for (int cnt = 1; cnt <= BASES_CNT32; cnt++) {
+      // atomicAdd(val, straightforward_mr32(bases, cnt, n));
+      auto ao = ext::oneapi::atomic_ref<int, 
+            ext::oneapi::memory_order::relaxed,
+            ext::oneapi::memory_scope::device,
+            access::address_space::global_space> (val[0]);
+      ao.fetch_add(straightforward_mr32(bases, cnt, n));
+    }
+  }
+}
+
+void mr32_eff(
+  nd_item<1> &item,
+  const uint32_t *__restrict__ bases,
+  const uint32_t *__restrict__ n32,
+  int *__restrict__ val,
+  int iter)
+{
+  int j = item.get_global_id(0);
+  if (j < iter) {
+    int n = n32[j];
+    for (int cnt = 1; cnt <= BASES_CNT32; cnt++) {
+      // atomicAdd(val, efficient_mr32(bases, cnt, n));
+      auto ao = ext::oneapi::atomic_ref<int, 
+            ext::oneapi::memory_order::relaxed,
+            ext::oneapi::memory_scope::device,
+            access::address_space::global_space> (val[0]);
+      ao.fetch_add(efficient_mr32(bases, cnt, n));
+    }
+  }
+}
+
