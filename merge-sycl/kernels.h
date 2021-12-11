9bca4b61048571ded9193b7b4b6ad09541679111
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2021-12-11 07:29:46 -0500

    add the array merge examples

---
+++ merge-sycl/kernels.h
@@ -0,0 +1,261 @@
+/* POSITIVEINFINITY
+ * Returns maximum value of a type
+ */
+float positiveInfinity(float tmp) {
+  return FLT_MAX;
+}
+
+double positiveInfinity(double tmp) {
+  return DBL_MAX;
+}
+
+uint32_t positiveInfinity(uint32_t tmp) {
+  return 0xFFFFFFFFUL;
+}
+
+uint64_t positiveInfinity(uint64_t tmp) {
+  return 0xFFFFFFFFFFFFFFFFUL;
+}
+
+template<typename vec_t>
+vec_t getPositiveInfinity() {
+  vec_t tmp = 0;
+  return positiveInfinity(tmp);
+}
+
+/* NEGATIVEINFINITY
+ * Returns minimum value of a type
+ */
+float negativeInfinity(float tmp) {
+  return FLT_MIN;
+}
+
+double negativeInfinity(double tmp) {
+  return DBL_MIN;
+}
+
+uint32_t negativeInfinity(uint32_t tmp) {
+  return 0;
+}
+
+uint64_t negativeInfinity(uint64_t tmp) {
+  return 0;
+}
+
+template<typename vec_t>
+vec_t getNegativeInfinity() {
+  vec_t tmp = 0;
+  return negativeInfinity(tmp);
+}
+
+#define MAX(X,Y) (((X) > (Y)) ? (X) : (Y))
+#define MIN(X,Y) (((X) < (Y)) ? (X) : (Y))
+#define syncthreads() item.barrier(access::fence_space::local_space);
+
+/*
+ * Performs a 32-wide binary search on one global diagonal per block to find the intersection with the path.
+ * This divides the workload into independent merges for the next step 
+ */
+template<typename vec_t>
+void workloadDiagonals(
+  nd_item<1> &item,
+  volatile int32_t *__restrict x_top, 
+  volatile int32_t *__restrict y_top,
+  volatile int32_t *__restrict x_bottom,
+  volatile int32_t *__restrict y_bottom,
+  volatile int32_t *__restrict found,
+           int32_t *__restrict oneorzero,
+  const vec_t *__restrict A, uint32_t A_length, 
+  const vec_t *__restrict B, uint32_t B_length, 
+  uint32_t *__restrict diagonal_path_intersections) {
+
+  int threadIdx_x = item.get_local_id(0);
+  int blockIdx_x = item.get_group(0);
+  int gridDim_x = item.get_group_range(0);
+  // Calculate combined index around the MergePath "matrix"
+  int32_t combinedIndex = (uint64_t)blockIdx_x * ((uint64_t)A_length + (uint64_t)B_length) / (uint64_t)gridDim_x;
+
+  int threadOffset = threadIdx_x - 16;
+
+  // Figure out the coordinates of our diagonal
+  *x_top = MIN(combinedIndex, A_length);
+  *y_top = combinedIndex > (A_length) ? combinedIndex - (A_length) : 0;
+  *x_bottom = *y_top;
+  *y_bottom = *x_top;
+
+  *found = 0;
+
+  // Search the diagonal
+  while(!(*found)) {
+    // Update our coordinates within the 32-wide section of the diagonal 
+    int32_t current_x = *x_top - ((*x_top - *x_bottom) >> 1) - threadOffset;
+    int32_t current_y = *y_top + ((*y_bottom - *y_top) >> 1) + threadOffset;
+
+    // Are we a '1' or '0' with respect to A[x] <= B[x]
+    if(current_x >= A_length || current_y < 0) {
+      oneorzero[threadIdx_x] = 0;
+    } else if(current_y >= B_length || current_x < 1) {
+      oneorzero[threadIdx_x] = 1;
+    } else {
+      oneorzero[threadIdx_x] = (A[current_x-1] <= B[current_y]) ? 1 : 0;
+    }
+
+    syncthreads();
+
+    // If we find the meeting of the '1's and '0's, we found the 
+    // intersection of the path and diagonal
+    if(threadIdx_x > 0 && (oneorzero[threadIdx_x] != oneorzero[threadIdx_x-1])) {
+      *found = 1;
+      diagonal_path_intersections[blockIdx_x] = current_x;
+      diagonal_path_intersections[blockIdx_x + gridDim_x + 1] = current_y;
+    }
+
+    syncthreads();
+
+    // Adjust the search window on the diagonal
+    if(threadIdx_x == 16) {
+      if(oneorzero[31] != 0) {
+        *x_bottom = current_x;
+        *y_bottom = current_y;
+      } else {
+        *x_top = current_x;
+        *y_top = current_y;
+      }
+    }
+    syncthreads();
+  }
+
+  // Set the boundary diagonals (through 0,0 and A_length,B_length)
+  if(threadIdx_x == 0 && blockIdx_x == 0) {
+    diagonal_path_intersections[0] = 0;
+    diagonal_path_intersections[gridDim_x + 1] = 0;
+    diagonal_path_intersections[gridDim_x] = A_length;
+    diagonal_path_intersections[gridDim_x + gridDim_x + 1] = B_length;
+  }
+}
+
+/*
+ * Performs merge windows within a thread block from that block's global diagonal 
+ * intersection to the next 
+ */
+#define K 512
+template<typename vec_t, bool timesections, bool countloops>
+void mergeSinglePath(
+    nd_item<1> &item,
+    vec_t *__restrict A_shared,
+    volatile uint32_t *__restrict x_block_top,
+    volatile uint32_t *__restrict y_block_top,
+    volatile uint32_t  *__restrict x_block_stop,
+    volatile uint32_t  *__restrict y_block_stop,
+    const vec_t * __restrict A, uint32_t A_length,
+    const vec_t * __restrict B, uint32_t B_length, 
+    const uint32_t * __restrict diagonal_path_intersections,
+    vec_t * __restrict C, uint32_t C_length)
+{
+  int threadIdx_x = item.get_local_id(0);
+  int blockIdx_x = item.get_group(0);
+  int gridDim_x = item.get_group_range(0);
+  int blockDim_x = item.get_local_range(0);
+
+  vec_t* B_shared = A_shared + K+2;
+
+  // Pre-calculate reused indices
+  uint32_t threadIdX4 = threadIdx_x + threadIdx_x;
+  threadIdX4 = threadIdX4 + threadIdX4;
+  uint32_t threadIdX4p1 = threadIdX4 + 1;
+  uint32_t threadIdX4p2 = threadIdX4p1 + 1;
+  uint32_t threadIdX4p3 = threadIdX4p2 + 1;
+
+  // Define global window and create sentinels
+  switch(threadIdx_x) {
+    case 0:
+      *x_block_top = diagonal_path_intersections[blockIdx_x];
+      A_shared[0] = getNegativeInfinity<vec_t>();
+      break;
+    case 64:
+      *y_block_top = diagonal_path_intersections[blockIdx_x + gridDim_x + 1];
+      A_shared[K+1] = getPositiveInfinity<vec_t>();
+      break;
+    case 32:
+      *x_block_stop = diagonal_path_intersections[blockIdx_x + 1];
+      B_shared[0] = getNegativeInfinity<vec_t>();
+      break;
+    case 96:
+      *y_block_stop = diagonal_path_intersections[blockIdx_x + gridDim_x + 2];
+      B_shared[K+1] = getPositiveInfinity<vec_t>();
+      break;
+    default:
+      break;
+  }
+
+  A--;
+  B--;
+
+  syncthreads();
+
+  // Construct and merge windows from diagonal_path_intersections[blockIdx_x] 
+  // to diagonal_path_intersections[blockIdx_x+1]
+  while(((*x_block_top < *x_block_stop) || (*y_block_top < *y_block_stop))) {
+
+    // Load current local window
+    const vec_t * Atemp = A + *x_block_top;
+    const vec_t * Btemp = B + *y_block_top;
+    uint32_t sharedX = threadIdx_x+1;
+
+    A_shared[sharedX] = Atemp[sharedX];
+    B_shared[sharedX] = Btemp[sharedX];
+    sharedX += blockDim_x;
+    A_shared[sharedX] = Atemp[sharedX];
+    B_shared[sharedX] = Btemp[sharedX];
+    sharedX += blockDim_x;
+    A_shared[sharedX] = Atemp[sharedX];
+    B_shared[sharedX] = Btemp[sharedX];
+    sharedX += blockDim_x;
+    A_shared[sharedX] = Atemp[sharedX];
+    B_shared[sharedX] = Btemp[sharedX];
+
+    // Make sure this is before the sync
+    vec_t *Ctemp = C + *x_block_top + *y_block_top;
+
+    syncthreads();
+
+    // Binary search diagonal in the local window for path
+    int32_t offset = threadIdX4 >> 1;
+    uint32_t Ax = offset + 1;
+    vec_t * BSm1 = B_shared + threadIdX4p2;
+    vec_t * BS = BSm1 + 1;
+    while(true) {
+      offset = ((offset+1) >> 1);
+      if(A_shared[Ax] > BSm1[~Ax]) {
+        if(A_shared[Ax-1] <= BS[~Ax]) {
+          //Found it
+          break;
+        }
+        Ax -= offset;
+      } else {
+        Ax += offset;
+      }
+    }
+
+    uint32_t Bx = threadIdX4p2 - Ax;
+
+    // Merge four elements starting at the found path intersection
+    vec_t Ai, Bi, Ci;
+    Ai = A_shared[Ax];
+    Bi = B_shared[Bx];
+    if(Ai > Bi) {Ci = Bi; Bx++; Bi = B_shared[Bx];} else {Ci = Ai; Ax++; Ai = A_shared[Ax];}
+    Ctemp[threadIdX4] = Ci;
+    if(Ai > Bi) {Ci = Bi; Bx++; Bi = B_shared[Bx];} else {Ci = Ai; Ax++; Ai = A_shared[Ax];}
+    Ctemp[threadIdX4p1] = Ci;
+    if(Ai > Bi) {Ci = Bi; Bx++; Bi = B_shared[Bx];} else {Ci = Ai; Ax++; Ai = A_shared[Ax];}
+    Ctemp[threadIdX4p2] = Ci;
+    Ctemp[threadIdX4p3] = Ai > Bi ? Bi : Ai;
+
+    // Update for next window
+    if(threadIdx_x == 127) {
+      *x_block_top += Ax - 1;
+      *y_block_top += Bx - 1;
+    }
+    syncthreads();
+  } // Go to next window
+}
---
+++ merge-sycl/main.cpp
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <float.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "common.h"
+#include "kernels.h"
+
+#define CSV 0
+#if(CSV)
+#define PS(X, S) std::cout << X << ", " << S << ", "; fflush(stdout);
+#define PV(X) std::cout << X << ", "; fflush(stdout);
+#else
+#define PS(X, S) std::cout << X << " " << S <<" :\n"; fflush(stdout);
+#define PV(X) std::cout << "\t" << #X << " \t: " << X << "\n"; fflush(stdout);
+#endif
+
+/*
+ * Produce 64-bits of pseudo-randomness
+ * Note: not very "good" or "random" 
+ */
+template<typename vec_t>
+vec_t rand64() {
+  vec_t rtn;
+  do {
+    uint32_t * rtn32 = (uint32_t *)&rtn;
+    rtn32[0] = rand();
+    if(sizeof(vec_t) > 4) rtn32[1] = rand();
+  } while(!(rtn < getPositiveInfinity<vec_t>() &&
+        rtn > getNegativeInfinity<vec_t>()));
+  return rtn;
+}
+
+template<typename vec_t>
+class divide;
+
+template<typename vec_t>
+class merge;
+
+/*
+ * Perform <runs> merges of two sorted pseudorandom <vec_t> arrays of length <size> 
+ * Checks the output of each merge for correctness
+ */
+#define PADDING 1024
+template<typename vec_t, uint32_t blocks, uint32_t threads, uint32_t runs>
+void mergeType(queue &q, uint64_t size) {
+  // Prepare host and device vectors
+  std::vector<vec_t> hA (size + PADDING);
+  std::vector<vec_t> hB (size + PADDING);
+  std::vector<vec_t> hC (2*size + PADDING);
+
+  buffer<vec_t, 1> dA (size + PADDING);
+  buffer<vec_t, 1> dB (size + PADDING);
+  buffer<vec_t, 1> dC (2*size + PADDING);
+
+  // diagonal_path_intersections;
+  buffer<uint32_t, 1> dpi (2 * (blocks + 1));
+
+  uint32_t errors = 0;
+
+  for(uint32_t i = 0; i < runs; i++) {
+
+    // Generate two sorted psuedorandom arrays
+    for (uint64_t n = 0; n < size; n++) {
+       hA[n] = rand64<vec_t>();
+       hB[n] = rand64<vec_t>();
+    }
+
+    for (uint64_t n = size; n < size + PADDING; n++) {
+      hA[n] = getPositiveInfinity<vec_t>();
+      hB[n] = getPositiveInfinity<vec_t>();
+    }
+
+    std::sort(hA.begin(), hA.end());
+    std::sort(hB.begin(), hB.end());
+
+    q.submit([&] (handler &cgh) {
+      auto acc = dA.template get_access<sycl_discard_write>(cgh);
+      cgh.copy(hA.data(), acc);
+    });
+     
+    q.submit([&] (handler &cgh) {
+      auto acc = dB.template get_access<sycl_discard_write>(cgh);
+      cgh.copy(hB.data(), acc);
+    });
+
+    // Perform the global diagonal intersection serach to divide work among SMs
+    range<1> gws (blocks * 32);
+    range<1> lws (32);
+    q.submit([&] (handler &cgh) {
+      auto a = dA.template get_access<sycl_read>(cgh);
+      auto b = dB.template get_access<sycl_read>(cgh);
+      auto d = dpi.template get_access<sycl_discard_write>(cgh);
+      accessor<int32_t, 1, sycl_read_write, access::target::local> xt(1, cgh);
+      accessor<int32_t, 1, sycl_read_write, access::target::local> yt(1, cgh);
+      accessor<int32_t, 1, sycl_read_write, access::target::local> xb(1, cgh);
+      accessor<int32_t, 1, sycl_read_write, access::target::local> yb(1, cgh);
+      accessor<int32_t, 1, sycl_read_write, access::target::local> found(1, cgh);
+      accessor<int32_t, 1, sycl_read_write, access::target::local> oneorzero(32, cgh);
+      cgh.parallel_for<class divide<vec_t>>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+        workloadDiagonals<vec_t>(
+          item, xt.get_pointer(), yt.get_pointer(), xb.get_pointer(), yb.get_pointer(),
+          found.get_pointer(), oneorzero.get_pointer(), 
+          a.get_pointer(), size, b.get_pointer(), size, d.get_pointer());
+      });
+    });
+
+    // Merge between global diagonals independently on each block
+    range<1> gws2 (blocks * threads);
+    range<1> lws2 (threads);
+    q.submit([&] (handler &cgh) {
+      auto a = dA.template get_access<sycl_read>(cgh);
+      auto b = dB.template get_access<sycl_read>(cgh);
+      auto c = dC.template get_access<sycl_discard_write>(cgh);
+      auto d = dpi.template get_access<sycl_read>(cgh);
+      accessor<vec_t, 1, sycl_read_write, access::target::local> A((K+2)<<1, cgh);
+      accessor<uint32_t, 1, sycl_read_write, access::target::local> xt(1, cgh);
+      accessor<uint32_t, 1, sycl_read_write, access::target::local> yt(1, cgh);
+      accessor<uint32_t, 1, sycl_read_write, access::target::local> xs(1, cgh);
+      accessor<uint32_t, 1, sycl_read_write, access::target::local> ys(1, cgh);
+      cgh.parallel_for<class merge<vec_t>>(nd_range<1>(gws2, lws2), [=] (nd_item<1> item) {
+        mergeSinglePath<vec_t,false,false> (
+          item, A.get_pointer(), xt.get_pointer(), yt.get_pointer(),
+          xs.get_pointer(), ys.get_pointer(),
+          a.get_pointer(), size, b.get_pointer(), size, 
+          d.get_pointer(), c.get_pointer(), size * 2);
+      });
+    });
+
+    // Test for errors
+    q.submit([&] (handler &cgh) {
+      auto acc = dC.template get_access<sycl_read>(cgh, range<1>(size));
+      cgh.copy(acc, hC.data());
+    }).wait();
+
+    for(uint32_t i = 1; i < size; i++) {
+      errors += hC[i] < hC[i-1];
+    }
+  }
+
+  // Print error info
+  PV(errors);
+}
+
+/* 
+ * Performs <runs> merge tests for each type at a given size
+ */
+template<uint32_t blocks, uint32_t threads, uint32_t runs>
+void mergeAllTypes(queue &q, uint64_t size) {
+  PS("uint32_t", size)  mergeType<uint32_t, blocks, threads, runs>(q, size); printf("\n");
+  PS("float",    size)  mergeType<float,    blocks, threads, runs>(q, size); printf("\n");
+  PS("uint64_t", size)  mergeType<uint64_t, blocks, threads, runs>(q, size); printf("\n");
+  PS("double", size)    mergeType<double,   blocks, threads, runs>(q, size); printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s <length of the arrays>\n", argv[0]);
+    return 1;
+  }
+  // length is sufficiently large; 
+  // otherwise there are invalid global reads in the kernel mergeSinglePath
+  const uint64_t length = atol(argv[1]);
+
+  const int blocks = 112;
+  const int threads = 128;  // do not change
+  const int runs = 100;
+
+#ifdef USE_GPU
+  gpu_selector dev_sel;
+#else
+  cpu_selector dev_sel;
+#endif
+  queue q(dev_sel);
+
+  mergeAllTypes<blocks, threads, runs>(q, length);
+  return 0;
+}
+
