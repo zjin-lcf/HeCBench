6a66756cec9ba2ecdf9dd11e7d8ac6992e93e9ac
Jin Z <5zj@oswald00.ftpn.ornl.gov>
2021-12-19 23:45:11 -0500

    slightly update the hotspot codes

--- hotspot-sycl/hotspot.h
+++ hotspot-sycl/hotspot.h
@@ -2,10 +2,8 @@
 #define HOTSPOT_H
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <assert.h>
 
 #ifdef RD_WG_SIZE_0_0                                                            
         #define BLOCK_SIZE RD_WG_SIZE_0_0                                        
@@ -20,35 +18,29 @@
 #define STR_SIZE 256
 # define EXPAND_RATE 2// add one iteration will extend the pyramid base by 2 per each borderline
 
-/* maximum power density possible (say 300W for a 10mm x 10mm chip)	*/
+/* maximum power density possible (say 300W for a 10mm x 10mm chip) */
 #define MAX_PD	(3.0e6)
-/* required precision in degrees	*/
+/* required precision in degrees */
 #define PRECISION	0.001
 #define SPEC_HEAT_SI 1.75e6
 #define K_SI 100
-/* capacitance fitting factor	*/
+/* capacitance fitting factor */
 #define FACTOR_CHIP	0.5
 
 #define MIN(a, b) ((a)<=(b) ? (a) : (b))
 
 #define IN_RANGE(x, min, max)   ((x)>=(min) && (x)<=(max))
 
-
-
-/* chip parameters	*/
+/* chip parameters */
 const static float t_chip = 0.0005;
 const static float chip_height = 0.016;
 const static float chip_width = 0.016;
-/* ambient temperature, assuming no package at all	*/
+/* ambient temperature, assuming no package at all */
 const static float amb_temp = 80.0;
 
-
-void writeoutput(float *, int, int, char *);
 void writeoutput(buffer<float,1>&, int, int, char *);
 void readinput(float *, int, int, char *);
 void usage(int, char **);
 void run(int, char **);
-
-
 
 #endif
