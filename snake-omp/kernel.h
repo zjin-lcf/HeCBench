ee2b3866c3d7d4ee1aa21c9e82ad2fa3f04e12e2
Jin Z <5zj@oswald00.ftpn.ornl.gov>
2021-12-30 13:16:39 -0500

    update the snake in omp and add an omp-target version of clz

---
+++ snake-omp/kernel.h
@@ -0,0 +1,250 @@
+void sneaky_snake(
+  const int nTeams,
+  const int nThreads,
+  const uint*__restrict F_ReadSeq,
+  const uint*__restrict F_RefSeq, 
+  int*__restrict Ftest_Results, 
+  const int NumReads,
+  const int F_ErrorThreshold)
+{
+  #pragma omp target teams distribute parallel for num_teams(nTeams) thread_limit(nThreads)
+  for (int tid = 0;tid < NumReads; tid++) {
+
+    uint ReadsPerThread[NBytes];
+    uint RefsPerThread[NBytes];
+  
+    #pragma unroll
+    for (int i = 0; i < NBytes; i++)
+    {
+      ReadsPerThread[i] = F_ReadSeq[tid*8 + i];
+      RefsPerThread[i] = F_RefSeq[tid*8 + i];
+    }
+  
+    /////////////////////////////////////////////////////////////////////////////
+    Ftest_Results[tid] = 1;
+  
+    uint ReadCompTmp = 0;
+    uint RefCompTmp = 0;
+    uint DiagonalResult = 0;
+  
+    uint ReadTmp1 = 0;
+    uint ReadTmp2 = 0;
+  
+    uint RefTmp1 = 0;
+    uint RefTmp2 = 0;
+  
+    uint CornerCase = 0;
+  
+    int localCounter= 0;
+    int localCounterMax=0;
+    int globalCounter = 0;
+    int Max_leading_zeros = 0;
+    int AccumulatedErrs = 0;
+  
+    int Diagonal = 0;
+    int ShiftValue = 0;
+  
+    int j = 0; //specifying the j-th int that we are reading in each read-ref comparison (can be from 0 to 7)
+  
+    while ( (j < 7) && (globalCounter < 200))
+    {
+      Diagonal = 0;
+      RefTmp1 = lsl(RefsPerThread[j], ShiftValue);
+      RefTmp2 = lsr(RefsPerThread[j + 1], 32 - ShiftValue);
+      ReadTmp1 = lsl(ReadsPerThread[j], ShiftValue);
+      ReadTmp2 = lsr(ReadsPerThread[j + 1], 32 - ShiftValue);
+  
+      ReadCompTmp = ReadTmp1 | ReadTmp2;
+      RefCompTmp = RefTmp1 | RefTmp2;
+      DiagonalResult = ReadCompTmp ^ RefCompTmp;
+      localCounterMax = __clz(DiagonalResult);
+  
+      //////////////////// Upper diagonals /////////////////////
+  
+      for(int e = 1; e <= F_ErrorThreshold; e++)
+      {
+        Diagonal += 1;
+        CornerCase = 0;
+        if ( (j == 0) && ( (ShiftValue - (2*e)) < 0 ) )
+        {
+          ReadTmp1 = lsr(ReadsPerThread[j], 2*e - ShiftValue);
+          ReadTmp2 = 0;
+  
+          ReadCompTmp = ReadTmp1 | ReadTmp2;
+          RefCompTmp = RefTmp1 | RefTmp2;
+  
+          DiagonalResult = ReadCompTmp ^ RefCompTmp;
+  
+          CornerCase = 0;
+          for(int Ci = 0; Ci < (2*e) - ShiftValue; Ci++)
+          {
+            set_bit(CornerCase, 31 - Ci);
+          }
+  
+          DiagonalResult  = DiagonalResult | CornerCase;
+          localCounter = __clz(DiagonalResult);
+  
+        }
+        else if ( (ShiftValue - (2*e) ) < 0 )
+        {
+          ReadTmp1 = lsl(ReadsPerThread[j-1], 32 - (2*e - ShiftValue));
+          ReadTmp2 = lsr(ReadsPerThread[j], 2*e - ShiftValue);
+  
+          ReadCompTmp = ReadTmp1 | ReadTmp2;
+          RefCompTmp = RefTmp1 | RefTmp2;
+  
+          DiagonalResult = ReadCompTmp ^ RefCompTmp;
+  
+          localCounter = __clz(DiagonalResult);
+        }
+        else
+        {
+          ReadTmp1 = lsl(ReadsPerThread[j], ShiftValue - 2*e);
+          ReadTmp2 = lsr(ReadsPerThread[j+1], 32 - (ShiftValue - 2*e)) ;
+  
+          ReadCompTmp = ReadTmp1 | ReadTmp2;
+          RefCompTmp = RefTmp1 | RefTmp2;
+  
+          DiagonalResult = ReadCompTmp ^ RefCompTmp;
+  
+          localCounter = __clz(DiagonalResult);
+        }
+        if (localCounter>localCounterMax)
+          localCounterMax=localCounter;
+      }
+  
+      /*
+         sh = shift
+         up = upper diagonal
+         RC = ReadCompTmp
+         FC = RefCompTmp
+         D = DiagonalResult
+         DN = diagonal
+         LC = localCounter
+         */
+  
+      //////////////////// Lower diagonals /////////////////////
+  
+      for(int e = 1; e <= F_ErrorThreshold; e++)
+      {
+        Diagonal += 1;
+        CornerCase = 0;
+        if (j<5)
+        {
+          if ((ShiftValue + 2*e) < 32)
+          {
+            ReadTmp1 = lsl(ReadsPerThread[j], ShiftValue + 2*e);
+            ReadTmp2 = lsr(ReadsPerThread[j+1], 32 - (ShiftValue + 2*e));
+  
+            ReadCompTmp = ReadTmp1 | ReadTmp2;
+            RefCompTmp = RefTmp1 | RefTmp2;
+  
+            DiagonalResult = ReadCompTmp ^ RefCompTmp;
+            localCounter = __clz(DiagonalResult);
+          }
+          else
+          {
+            ReadTmp1 = lsl(ReadsPerThread[j+1], (ShiftValue + 2*e) % 32);
+            ReadTmp2 = lsr(ReadsPerThread[j+2], 32 - (ShiftValue + 2*e) % 32);
+  
+            ReadCompTmp = ReadTmp1 | ReadTmp2;
+            RefCompTmp = RefTmp1 | RefTmp2;
+  
+            DiagonalResult = 0xffffffff;//ReadCompTmp ^ RefCompTmp;
+  
+            DiagonalResult = ReadCompTmp ^ RefCompTmp;
+  
+            localCounter = __clz(DiagonalResult);
+          }
+        }
+        else
+        {
+          ReadTmp1 = lsl(ReadsPerThread[j], ShiftValue + 2*e);
+          ReadTmp2 = lsr(ReadsPerThread[j+1], 32 - (ShiftValue + 2*e));
+  
+          ReadCompTmp = ReadTmp1 | ReadTmp2;
+          RefCompTmp = RefTmp1 | RefTmp2;
+          DiagonalResult = ReadCompTmp ^ RefCompTmp;
+  
+          CornerCase = 0;
+          if ((globalCounter+32)>200) {
+  
+            for(int Ci = globalCounter+32-200; Ci < globalCounter+32-200+2*e; Ci++)
+            {
+              set_bit(CornerCase, Ci);
+            }
+          }
+  
+          else if ((globalCounter+32)>=(200- (2*e))){
+  
+            for(int Ci = 0; Ci < (2*e); Ci++)
+            {
+              set_bit(CornerCase, Ci);
+            }
+          }
+          DiagonalResult = DiagonalResult | CornerCase;
+  
+          localCounter = __clz(DiagonalResult);
+        }
+  
+        if (localCounter>localCounterMax)
+          localCounterMax=localCounter;
+      }
+  
+      /*
+         CC = CornerCase
+         sh = shift
+         up = upper diagonal
+         RC = ReadCompTmp
+         FC = RefCompTmp
+         D = DiagonalResult
+         DN = diagonal
+         LC = localCounter
+         */
+  
+      Max_leading_zeros = 0;
+  
+      if ( (j == 6) && ( ((localCounterMax/2)*2) >= 8) )
+      {
+        Max_leading_zeros = 8;
+        break;
+      }
+      else if((localCounterMax/2*2) > Max_leading_zeros)
+      {
+        Max_leading_zeros = ((localCounterMax/2)*2);
+      }
+  
+      if (((Max_leading_zeros/2) < 16) && (j < 5))
+      {
+        AccumulatedErrs += 1;
+      }
+      else if ((j == 6) && ((Max_leading_zeros/2) < 4))
+      {
+        AccumulatedErrs += 1;
+      }
+  
+      if(AccumulatedErrs > F_ErrorThreshold)
+      {
+        Ftest_Results[tid] = 0;
+        break;
+      }
+  
+      if(ShiftValue + Max_leading_zeros + 2 >= 32)
+      {
+        j += 1;
+      }
+  
+      // ShiftValue_2Ref = (ShiftValue_2Ref + Max_leading_zeros + 2) %32;
+      if (Max_leading_zeros == 32)
+      {
+        globalCounter += Max_leading_zeros;
+      }
+      else
+      {
+        ShiftValue = ((ShiftValue + Max_leading_zeros + 2) % 32);
+        globalCounter += (Max_leading_zeros + 2);
+      }
+    }
+  }
+}
+
--- snake-omp/main.cpp
+++ snake-omp/main.cpp
@@ -39,39 +39,48 @@
 using namespace std::chrono;
 
 #define warp_size 32
-#define SharedPartDevice 64
-#define FULL_MASK 0xffffffff
-#define NBytes Nuints
-#define PRINT 0
+#define NBytes 8
 
-#define Number_of_Diagonals 9
-#define F_ReadLength 100
-#define BitVal(data,y) ( (data>>y) & 1)    // Return Data.Y value
-#define SetBit(data,y) data |= (1 << y)    // Set Data.Y   to 1
-
-// suboptimal
 #pragma omp declare target
-uint popcnt( uint x )
-{
-    x -= ((x >> 1) & 0x55555555);
-    x = (((x >> 2) & 0x33333333) + (x & 0x33333333));
-    x = (((x >> 4) + x) & 0x0f0f0f0f);
-    x += (x >> 8);
-    x += (x >> 16);
-    return x & 0x0000003f;
+inline uint lsr(uint x, int sa) {
+  if(sa > 0 && sa < 32) return (x >> sa);
+  return x;
 }
 
-uint clz( uint x )
+inline uint lsl(uint x, int sa) {
+  if (sa > 0 && sa < 32) return (x << sa);
+  return x;
+}
+
+inline uint set_bit(uint &data, int y) {
+  data |= lsl(1, y);
+  return data;
+}
+
+// clz for omp target 
+uint popcnt( uint x )
 {
-    x |= (x >> 1);
-    x |= (x >> 2);
-    x |= (x >> 4);
-    x |= (x >> 8);
-    x |= (x >> 16);
-    return 32 - popcnt(x);
+  x -= ((x >> 1) & 0x55555555);
+  x = (((x >> 2) & 0x33333333) + (x & 0x33333333));
+  x = (((x >> 4) + x) & 0x0f0f0f0f);
+  x += (x >> 8);
+  x += (x >> 16);
+  return x & 0x0000003f;
+}
+
+inline int __clz( int x )
+{
+  x |= (x >> 1);
+  x |= (x >> 2);
+  x |= (x >> 4);
+  x |= (x >> 8);
+  x |= (x >> 16);
+  return 32 - popcnt(x);
 }
 #pragma omp end declare target
 
+#include "kernel.h"
+#include "reference.h"
 
 int main(int argc, const char * const argv[])
 {
@@ -97,10 +106,10 @@ int main(int argc, const char * const argv[])
 
   int F_ErrorThreshold =0;
 
-  uint* ReadSeq = (uint * ) calloc(NumReads * 8, sizeof(uint));
-  uint* RefSeq = (uint * ) calloc(NumReads * 8, sizeof(uint));
-  int* Results = (int * ) calloc(NumReads, sizeof(int));
-
+  uint* ReadSeq = (uint *) calloc(NumReads * 8, sizeof(uint));
+  uint* RefSeq = (uint *) calloc(NumReads * 8, sizeof(uint));
+  int* DFinal_Results = (int *) calloc(NumReads, sizeof(int));
+  int* HFinal_Results = (int *) calloc(NumReads, sizeof(int));
 
   int tokenIndex=1;
   fp = fopen(argv[2], "r");
@@ -123,20 +132,18 @@ int main(int argc, const char * const argv[])
           }
           else if (p[j] == 'C')
           {
-            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
+            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = set_bit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
           }
           else if (p[j] == 'G')
           {
-            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
+            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = set_bit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
           }
           else if (p[j] == 'T')
           {
-            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
+            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = set_bit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
 
-            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
+            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = set_bit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
           }
-          //printf("%c",p[j]);
-          //printf(" %08x", ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)]);
         }
       }
       else if(tokenIndex==2)
@@ -149,20 +156,18 @@ int main(int argc, const char * const argv[])
           }
           else if (p[j] == 'C')
           {
-            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
+            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = set_bit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
           }
           else if (p[j] == 'G')
           {
-            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
+            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = set_bit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
           }
           else if (p[j] == 'T')
           {
-            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
+            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = set_bit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
 
-            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
+            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = set_bit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
           }
-          //printf("%c",p[j]);
-          //printf(" %08x", RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)]);
         }
       }
       tokenIndex=tokenIndex+1;
@@ -170,282 +175,44 @@ int main(int argc, const char * const argv[])
   }
   fclose(fp);
 
-#pragma omp target data map(to: ReadSeq[0:NumReads*8], RefSeq[0:NumReads*8]) \
-                        map(alloc: Results[0:NumReads])
-{
+  #pragma omp target data map(to: ReadSeq[0:NumReads*8], RefSeq[0:NumReads*8]) \
+                          map(alloc: DFinal_Results[0:NumReads])
+  {
 
-  for (int n = 0; n < 100; n++) {
-    for (int loopPar = 0; loopPar <= 25; loopPar++) {
-      F_ErrorThreshold = (loopPar*ReadLength)/100;
+  bool error = false;
+  for (int loopPar = 0; loopPar <= 25; loopPar++) {
 
-      high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    F_ErrorThreshold = (loopPar*ReadLength)/100;
 
-      #pragma omp target teams distribute parallel for num_teams(Number_of_blocks_inside_each_kernel) \
-                                                   thread_limit(Concurrent_threads_In_Block)
-      for (int tid = 0; tid < NumReads; tid++) {
-          // const int NBytes = 8;
-          uint ReadsPerThread[NBytes];
-          uint RefsPerThread[NBytes];
-
-          #pragma unroll
-          for (int i = 0; i < NBytes; i++)
-          {
-            ReadsPerThread[i] = ReadSeq[tid*8 + i];
-            RefsPerThread[i] = RefSeq[tid*8 + i];
-          }
-
-          /////////////////////////////////////////////////////////////////////////////
-          Results[tid] = 1;
-
-          uint ReadCompTmp = 0;
-          uint RefCompTmp = 0;
-          uint DiagonalResult = 0;
-
-          uint ReadTmp1 = 0;
-          uint ReadTmp2 = 0;
-
-          uint RefTmp1 = 0;
-          uint RefTmp2 = 0;
-
-          uint CornerCase = 0;
-
-          int localCounter= 0;
-          int localCounterMax=0;
-          int globalCounter = 0;
-          int Max_leading_zeros = 0;
-          int AccumulatedErrs = 0;
-
-          int ShiftValue = 0;
-          int Diagonal = 0;
-
-          int j = 0; //specifying the j-th uint that we are reading in each read-ref comparison (can be from 0 to 7)
-
-          while ( (j < 7) && (globalCounter < 200))
-          {
-            Diagonal = 0;
-            RefTmp1 = RefsPerThread[j] << ShiftValue;
-            RefTmp2 = RefsPerThread[j + 1] >>  32 - ShiftValue;
-            ReadTmp1 = ReadsPerThread[j] << ShiftValue;
-            ReadTmp2 = ReadsPerThread[j + 1] >>  32 - ShiftValue;
-
-            ReadCompTmp = ReadTmp1 | ReadTmp2;
-            RefCompTmp = RefTmp1 | RefTmp2;
-            DiagonalResult = ReadCompTmp ^ RefCompTmp;
-            localCounterMax = clz(DiagonalResult);
-
-            //////////////////// Upper diagonals /////////////////////
-
-            for(int e = 1; e <= F_ErrorThreshold; e++)
-            {
-              Diagonal += 1;
-              CornerCase = 0;
-              if (  (j == 0)  &&  (  (ShiftValue - (2*e))  < 0 )  )
-              {
-                ReadTmp1 = ReadsPerThread[j] >> ( (2*e) - ShiftValue );
-                ReadTmp2 = 0;
-
-                ReadCompTmp = ReadTmp1 | ReadTmp2;
-                RefCompTmp = RefTmp1 | RefTmp2;
-
-                DiagonalResult = ReadCompTmp ^ RefCompTmp;
-
-                CornerCase = 0;
-                for(int Ci = 0; Ci < (2*e) - ShiftValue; Ci++)
-                {
-                  SetBit(CornerCase, 31 - Ci);
-                }
-
-                DiagonalResult  = DiagonalResult | CornerCase;
-                localCounter = clz(DiagonalResult);
-
-              }
-              else if ( (ShiftValue - (2*e) ) < 0 )
-              {
-                ReadTmp1 = ReadsPerThread[j-1] << 32 - ( (2*e) - ShiftValue );
-                ReadTmp2 = ReadsPerThread[j] >> (2*e) - ShiftValue;
-
-                ReadCompTmp = ReadTmp1 | ReadTmp2;
-                RefCompTmp = RefTmp1 | RefTmp2;
-
-                DiagonalResult = ReadCompTmp ^ RefCompTmp;
-
-                localCounter = clz(DiagonalResult);
-              }
-              else
-              {
-                ReadTmp1 = ReadsPerThread[j] <<  ShiftValue - (2*e);
-                ReadTmp2 = ReadsPerThread[j+1] >> 32 - (ShiftValue - (2*e) ) ;
-
-                ReadCompTmp = ReadTmp1 | ReadTmp2;
-                RefCompTmp = RefTmp1 | RefTmp2;
-
-                DiagonalResult = ReadCompTmp ^ RefCompTmp;
-
-                localCounter = clz(DiagonalResult);
-
-              }
-              if (localCounter>localCounterMax)
-                localCounterMax=localCounter;
-            }
-
-
-            /*
-               sh = shift
-               up = upper diagonal
-               RC = ReadCompTmp
-               FC = RefCompTmp
-               D = DiagonalResult
-               DN = diagonal
-               LC = localCounter
-             */
-
-            //////////////////// Lower diagonals /////////////////////
-
-            for(int e = 1; e <= F_ErrorThreshold; e++)
-            {
-              Diagonal += 1;
-              CornerCase = 0;
-              if ( j<5)//  ( (globalCounter + ShiftValue + (2*e) + 32) < 200) )
-              {
-                if ( (ShiftValue + (2*e) )  < 32)
-                {
-                  ReadTmp1 = ReadsPerThread[j] << ShiftValue + (2*e);
-                  ReadTmp2 = ReadsPerThread[j+1] >> 32 - ( ShiftValue + (2*e) );
-
-                  ReadCompTmp = ReadTmp1 | ReadTmp2;
-                  RefCompTmp = RefTmp1 | RefTmp2;
-
-                  DiagonalResult = ReadCompTmp ^ RefCompTmp;
-                  localCounter = clz(DiagonalResult);
-
-                }
-                else
-                {
-                  ReadTmp1 = ReadsPerThread[j+1] << ( ShiftValue + (2*e) ) % 32;
-                  ReadTmp2 = ReadsPerThread[j+2] >>  32 - ( ( ShiftValue + (2*e) ) % 32 );
-
-                  ReadCompTmp = ReadTmp1 | ReadTmp2;
-                  RefCompTmp = RefTmp1 | RefTmp2;
-
-                  DiagonalResult = 0xffffffff;//ReadCompTmp ^ RefCompTmp;
-
-                  DiagonalResult = ReadCompTmp ^ RefCompTmp;
-
-                  localCounter = clz(DiagonalResult);
-                }
-              }
-              else
-              {
-                //printf("HI3");
-                ReadTmp1 = ReadsPerThread[j] << ShiftValue + (2*e);
-                ReadTmp2 = ReadsPerThread[j+1] >>   32 - ( ShiftValue + (2*e) );
-
-                ReadCompTmp = ReadTmp1 | ReadTmp2;
-                RefCompTmp = RefTmp1 | RefTmp2;
-                DiagonalResult = ReadCompTmp ^ RefCompTmp;
-
-                CornerCase = 0;
-                if ((globalCounter+32)>200 ) {
-
-                  for(int Ci = ((globalCounter+32)-200); Ci < (((globalCounter+32)-200)+ 2*e); Ci++)
-                  {
-                    SetBit(CornerCase, Ci);
-                  }
-                }
-
-                else if ((globalCounter+32)>=(200- (2*e))){
-
-                  for(int Ci = 0; Ci < (2*e); Ci++)
-                  {
-                    SetBit(CornerCase, Ci);
-                  }
-                }
-                DiagonalResult = DiagonalResult | CornerCase;
-
-                localCounter = clz(DiagonalResult);
-              }
-
-              if (localCounter>localCounterMax)
-                localCounterMax=localCounter;
-
-            }
-
-            /*
-               CC = CornerCase
-               sh = shift
-               up = upper diagonal
-               RC = ReadCompTmp
-               FC = RefCompTmp
-               D = DiagonalResult
-               DN = diagonal
-               LC = localCounter
-             */
-
-            Max_leading_zeros = 0;
-
-            if ( (j == 6) && ( ((localCounterMax/2)*2) >= 8)  )
-            {
-              Max_leading_zeros = 8;
-              break;
-            }
-            else if( ((localCounterMax/2)*2) > Max_leading_zeros)
-            {
-              Max_leading_zeros = ((localCounterMax/2)*2);
-            }
-
-            if ( ( (Max_leading_zeros/2) < 16) && (j < 5) )
-            {
-              AccumulatedErrs += 1;
-            }
-            else if (  (j == 6) && ( (Max_leading_zeros/2) < 4) )
-            {
-              AccumulatedErrs += 1;
-            }
-
-            if(AccumulatedErrs > F_ErrorThreshold)
-            {
-              Results[tid] = 0;
-              break;
-            }
-
-
-            if(ShiftValue + Max_leading_zeros + 2 >= 32)
-            {
-              j += 1;
-            }
-
-            // ShiftValue_2Ref = (ShiftValue_2Ref + Max_leading_zeros + 2) %32;
-            if (Max_leading_zeros == 32)
-            {
-              globalCounter += Max_leading_zeros;
-            }
-            else
-            {
-              ShiftValue = ((ShiftValue + Max_leading_zeros + 2) % 32);
-              globalCounter += (Max_leading_zeros + 2);
-            }
-          }
-       }
-      #pragma omp target update from (Results[0:NumReads])
-
-      high_resolution_clock::time_point t2 = high_resolution_clock::now();
-
-      double elapsed_time = duration_cast<microseconds>(t2 - t1).count();
-      int accepted = 0;
-      for(int i = 0; i < NumReads; i++)
-      {
-        if(Results[i] == 1)
-          accepted += 1;
-      }
-
-      printf("E: \t %d \t Snake-on-GPU: \t %5.4f \t Accepted: \t %10d \t Rejected: \t %10d\n", 
-          F_ErrorThreshold, elapsed_time, accepted, NumReads - accepted);
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    for (int n = 0; n < 100; n++) {
+      sneaky_snake(Number_of_blocks_inside_each_kernel, Concurrent_threads_In_Block,
+                   ReadSeq, RefSeq, DFinal_Results, NumReads, F_ErrorThreshold);
     }
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    double elapsed_time = duration_cast<microseconds>(t2 - t1).count();
+
+    #pragma omp target update from (DFinal_Results[0:NumReads])
+
+    // verify
+    sneaky_snake_ref(ReadSeq, RefSeq, HFinal_Results, NumReads, F_ErrorThreshold);
+    error = memcmp(DFinal_Results, HFinal_Results, NumReads * sizeof(int));
+    if (error) break;
+
+    // stats
+    int D_accepted = 0;
+    for(int i = 0; i < NumReads; i++) if(DFinal_Results[i] == 1) D_accepted++;
+
+    printf("Error threshold: \t %d \t Time (us): \t %5.4f \t Accepted: \t %10d \t Rejected: \t %10d\n", 
+          F_ErrorThreshold, elapsed_time / 100, D_accepted, NumReads - D_accepted);
   }
-}
+  printf("%s\n", error ? "FAIL" : "PASS");
+
+  }
 
   free(ReadSeq);
   free(RefSeq);
-  free(Results);
+  free(DFinal_Results);
+  free(HFinal_Results);
   return 0;
 }
