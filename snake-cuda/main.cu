ded065e1db209e2ed656082e56d81c0eb1e68656
Jin Z <5zj@oswald00.ftpn.ornl.gov>
2021-12-30 13:11:23 -0500

    wrap the shift operations with the lsl and lsr functions; verify the host and device results

--- snake-cuda/main.cu
+++ snake-cuda/main.cu
@@ -40,17 +40,28 @@
 using namespace std::chrono;
 
 #define warp_size 32
-#define SharedPartDevice 64
-#define FULL_MASK 0xffffffff
-#define NBytes Nuints
-#define PRINT 0
+#define NBytes 8
 
-#define Number_of_Diagonals 9
-#define F_ReadLength 100
-#define BitVal(data,y) ( (data>>(y)) & 1)    // Return Data.Y value
-#define SetBit(data,y) data |= (1 << (y))    // Set Data.Y   to 1
+__host__ __device__
+inline uint lsr(uint x, int sa) {
+  if(sa > 0 && sa < 32) return (x >> sa);
+  return x;
+}
+
+__host__ __device__
+inline uint lsl(uint x, int sa) {
+  if (sa > 0 && sa < 32) return (x << sa);
+  return x;
+}
+
+__host__ __device__
+inline uint set_bit(uint &data, int y) {
+  data |= lsl(1, y);
+  return data;
+}
 
 #include "kernel.h"
+#include "reference.h"
 
 int main(int argc, const char * const argv[])
 {
@@ -61,7 +72,7 @@ int main(int argc, const char * const argv[])
 
   int ReadLength = atoi(argv[1]);//in my inputs, it is always 100. Just for the generality we keep it as a variable
   int NumReads = atoi(argv[3]); // Number of reads
-  int Size_of_uint_in_Bit = 32; //in Bits 
+  int Size_of_int_in_Bit = 32; //in Bits 
 
   FILE * fp;
   char * line = NULL;
@@ -76,9 +87,10 @@ int main(int argc, const char * const argv[])
 
   int F_ErrorThreshold =0;
 
-  uint* ReadSeq = (uint * ) calloc(NumReads * 8, sizeof(uint));
-  uint* RefSeq = (uint * ) calloc(NumReads * 8, sizeof(uint));
-  int* DFinal_Results = (int * ) calloc(NumReads, sizeof(int));
+  uint* ReadSeq = (uint *) calloc(NumReads * 8, sizeof(uint));
+  uint* RefSeq = (uint *) calloc(NumReads * 8, sizeof(uint));
+  int* DFinal_Results = (int *) calloc(NumReads, sizeof(int));
+  int* HFinal_Results = (int *) calloc(NumReads, sizeof(int));
 
   int tokenIndex=1;
   fp = fopen(argv[2], "r");
@@ -101,20 +113,18 @@ int main(int argc, const char * const argv[])
           }
           else if (p[j] == 'C')
           {
-            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
+            ReadSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)] = set_bit(ReadSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_int_in_Bit/2)) * 2 + 1));
           }
           else if (p[j] == 'G')
           {
-            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
+            ReadSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)] = set_bit(ReadSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_int_in_Bit/2)) * 2));
           }
           else if (p[j] == 'T')
           {
-            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
+            ReadSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)] = set_bit(ReadSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_int_in_Bit/2)) * 2));
 
-            ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
+            ReadSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)] = set_bit(ReadSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_int_in_Bit/2)) * 2 + 1));
           }
-          //printf("%c",p[j]);
-          //printf(" %08x", ReadSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)]);
         }
       }
       else if(tokenIndex==2)
@@ -127,20 +137,18 @@ int main(int argc, const char * const argv[])
           }
           else if (p[j] == 'C')
           {
-            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
+            RefSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)] = set_bit(RefSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_int_in_Bit/2)) * 2 + 1));
           }
           else if (p[j] == 'G')
           {
-            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
+            RefSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)] = set_bit(RefSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_int_in_Bit/2)) * 2));
           }
           else if (p[j] == 'T')
           {
-            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2));
+            RefSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)] = set_bit(RefSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_int_in_Bit/2)) * 2));
 
-            RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)] = SetBit(RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_uint_in_Bit/2)) * 2 + 1));
+            RefSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)] = set_bit(RefSeq[((j*2/Size_of_int_in_Bit) + this_read * NBytes)], 31 - ((j%(Size_of_int_in_Bit/2)) * 2 + 1));
           }
-          //printf("%c",p[j]);
-          //printf(" %08x", RefSeq[((j*2/Size_of_uint_in_Bit) + this_read * NBytes)]);
         }
       }
       tokenIndex=tokenIndex+1;
@@ -158,37 +166,42 @@ int main(int argc, const char * const argv[])
   dim3 grid (Number_of_blocks_inside_each_kernel);
   dim3 block (Concurrent_threads_In_Block);
 
-  cudaMemcpy(Dev_ReadSeq, ReadSeq, sizeof(uint) * NumReads * 8, cudaMemcpyHostToDevice);
-  cudaMemcpy(Dev_RefSeq, RefSeq, sizeof(uint) * NumReads * 8, cudaMemcpyHostToDevice);
+  cudaMemcpy(Dev_ReadSeq, ReadSeq, sizeof(int) * NumReads * 8, cudaMemcpyHostToDevice);
+  cudaMemcpy(Dev_RefSeq, RefSeq, sizeof(int) * NumReads * 8, cudaMemcpyHostToDevice);
 
-  for (int n = 0; n < 100; n++) {
-    for (int loopPar = 0; loopPar <= 25; loopPar++) {
-      F_ErrorThreshold = (loopPar*ReadLength)/100;
+  bool error = false;
+  for (int loopPar = 0; loopPar <= 25; loopPar++) {
 
-      high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    F_ErrorThreshold = (loopPar*ReadLength)/100;
 
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    for (int n = 0; n < 100; n++) {
       sneaky_snake<<<grid, block>>>(Dev_ReadSeq, Dev_RefSeq, Dev_Results, NumReads, F_ErrorThreshold);
-
-      cudaMemcpy(DFinal_Results, Dev_Results, sizeof(int) * NumReads, cudaMemcpyDeviceToHost);
-
-      high_resolution_clock::time_point t2 = high_resolution_clock::now();
-
-      double elapsed_time = duration_cast<microseconds>(t2 - t1).count();
-      int accepted = 0;
-      for(int i = 0; i < NumReads; i++)
-      {
-        if(DFinal_Results[i] == 1)
-          accepted += 1;
-      }
-
-      printf("E: \t %d \t Snake-on-GPU: \t %5.4f \t Accepted: \t %10d \t Rejected: \t %10d\n", 
-          F_ErrorThreshold, elapsed_time, accepted, NumReads - accepted);
     }
-  }
+    cudaDeviceSynchronize();
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    double elapsed_time = duration_cast<microseconds>(t2 - t1).count();
+    
+    cudaMemcpy(DFinal_Results, Dev_Results, sizeof(int) * NumReads, cudaMemcpyDeviceToHost);
 
+    // verify
+    sneaky_snake_ref(ReadSeq, RefSeq, HFinal_Results, NumReads, F_ErrorThreshold);
+    error = memcmp(DFinal_Results, HFinal_Results, NumReads * sizeof(int));
+    if (error) break;
+
+    // stats
+    int D_accepted = 0;
+    for(int i = 0; i < NumReads; i++) if(DFinal_Results[i] == 1) D_accepted++;
+
+    printf("Error threshold: \t %d \t Time (us): \t %5.4f \t Accepted: \t %10d \t Rejected: \t %10d\n", 
+          F_ErrorThreshold, elapsed_time / 100, D_accepted, NumReads - D_accepted);
+  }
+  printf("%s\n", error ? "FAIL" : "PASS");
+  
   free(ReadSeq);
   free(RefSeq);
   free(DFinal_Results);
+  free(HFinal_Results);
   cudaFree(Dev_ReadSeq);
   cudaFree(Dev_RefSeq);
   cudaFree(Dev_Results);
