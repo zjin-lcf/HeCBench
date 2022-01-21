d39e088269f113709c43b8b99b4eaf60ab9c6345
Jin Z <5zj@oswald00.ftpn.ornl.gov>
2022-01-21 13:39:27 -0500

    add the laplace3d in sycl

---
+++ laplace3d-sycl/kernel.h
@@ -0,0 +1,147 @@
+//
+// Notes:
+//
+// 1) strategy: one thread per node in the 2D block;
+//    after initialisation it marches in the k-direction
+//    working with 3 planes of data at a time
+//
+// 2) each thread also loads in data for at most one halo node;
+//    assumes the number of halo nodes is not more than the
+//    number of interior nodes
+//
+// 3) corner halo nodes are included because they are needed
+//    for more general applications with cross-derivatives
+//
+// 4) could try double-buffering in the future fairly easily
+//
+
+// define kernel block size
+
+#define BLOCK_X 32
+#define BLOCK_Y 8
+
+#define IOFF  1
+#define JOFF (BLOCK_X+2)
+#define KOFF (BLOCK_X+2)*(BLOCK_Y+2)
+
+// definition to use efficient __mul24 intrinsic
+
+#define INDEX(i,j,j_off)  (i + sycl::mul24(j,j_off))
+
+#define syncthreads() item.barrier(access::fence_space::local_space)
+
+// device code
+
+void laplace3d(
+  nd_item<2> &item,
+  int NX, int NY, int NZ, int pitch, 
+  const float *__restrict d_u1,
+        float *__restrict d_u2,
+        float *__restrict u1)
+{
+  int   indg, indg_h, indg0;
+  int   i, j, k, ind, ind_h, halo, active;
+  float u2, sixth=1.0f/6.0f;
+
+  int NXM1 = NX-1;
+  int NYM1 = NY-1;
+  int NZM1 = NZ-1;
+
+  //
+  // first set up indices for halos
+  //
+  const int threadIdx_x = item.get_local_id(1);
+  const int threadIdx_y = item.get_local_id(0);
+  const int blockIdx_x = item.get_group(1);
+  const int blockIdx_y = item.get_group(0);
+
+  k    = threadIdx_x + threadIdx_y*BLOCK_X;
+  halo = k < 2*(BLOCK_X+BLOCK_Y+2);
+
+  if (halo) {
+    if (threadIdx_y<2) {               // y-halos (coalesced)
+      i = threadIdx_x;
+      j = threadIdx_y*(BLOCK_Y+1) - 1;
+    }
+    else {                             // x-halos (not coalesced)
+      i = (k%2)*(BLOCK_X+1) - 1;
+      j =  k/2 - BLOCK_X - 1;
+    }
+
+    ind_h  = INDEX(i+1,j+1,JOFF) + KOFF;
+
+    i      = INDEX(i,blockIdx_x,BLOCK_X);   // global indices
+    j      = INDEX(j,blockIdx_y,BLOCK_Y);
+    indg_h = INDEX(i,j,pitch);
+
+    halo   =  (i>=0) && (i<NX) && (j>=0) && (j<NY);
+  }
+
+  //
+  // then set up indices for main block
+  //
+
+  i    = threadIdx_x;
+  j    = threadIdx_y;
+  ind  = INDEX(i+1,j+1,JOFF) + KOFF;
+
+  i    = INDEX(i,blockIdx_x,BLOCK_X);     // global indices
+  j    = INDEX(j,blockIdx_y,BLOCK_Y);
+  indg = INDEX(i,j,pitch);
+
+  active = (i<NX) && (j<NY);
+
+  //
+  // read initial plane of u1 array
+  //
+
+  if (active) u1[ind+KOFF] = d_u1[indg];
+  if (halo) u1[ind_h+KOFF] = d_u1[indg_h];
+
+  //
+  // loop over k-planes
+  //
+
+  for (k=0; k<NZ; k++) {
+
+    // move two planes down and read in new plane k+1
+
+    if (active) {
+      indg0 = indg;
+      indg  = INDEX(indg,NY,pitch);
+      u1[ind-KOFF] = u1[ind];
+      u1[ind]      = u1[ind+KOFF];
+      if (k<NZM1)
+        u1[ind+KOFF] = d_u1[indg];
+    }
+
+    if (halo) {
+      indg_h = INDEX(indg_h,NY,pitch);
+      u1[ind_h-KOFF] = u1[ind_h];
+      u1[ind_h]      = u1[ind_h+KOFF];
+      if (k<NZM1)
+        u1[ind_h+KOFF] = d_u1[indg_h];
+    }
+
+    syncthreads();
+
+  //
+  // perform Jacobi iteration to set values in u2
+  //
+
+    if (active) {
+      if (i==0 || i==NXM1 || j==0 || j==NYM1 || k==0 || k==NZM1) {
+        u2 = u1[ind];          // Dirichlet b.c.'s
+      }
+      else {
+        u2 = ( u1[ind-IOFF] + u1[ind+IOFF]
+             + u1[ind-JOFF] + u1[ind+JOFF]
+             + u1[ind-KOFF] + u1[ind+KOFF] ) * sixth;
+      }
+      d_u2[indg0] = u2;
+    }
+
+    syncthreads();
+
+  }
+}
---
+++ laplace3d-sycl/main.cpp
@@ -0,0 +1,153 @@
+//
+// Program to solve Laplace equation on a regular 3D grid
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "common.h"
+#include <algorithm>
+#include "kernel.h"
+#include "reference.h"
+
+// declaration, forward
+
+void reference(int NX, int NY, int NZ, float* h_u1, float* h_u2);
+
+void printHelp(void);
+
+// Main program
+
+int main(int argc, char **argv){
+
+  // check command line inputs
+
+  if(argc != 6) {
+    printHelp();
+    return 1;
+  }
+
+  const int NX = atoi(argv[1]);
+  const int NY = atoi(argv[2]);
+  const int NZ = atoi(argv[3]);
+  const int REPEAT = atoi(argv[4]);
+  const int verify = atoi(argv[5]);
+
+  // NX is a multiple of 32
+  if (NX <= 0 || NX % 32 != 0 || NY <= 0 || NZ <= 0 || REPEAT <= 0) return 1;
+
+  printf("\nGrid dimensions: %d x %d x %d\n", NX, NY, NZ);
+  printf("Result verification %s \n", verify ? "enabled" : "disabled");
+ 
+  // allocate memory for arrays
+
+  const size_t grid3D_size = NX * NY * NZ ;
+  const size_t grid3D_bytes = grid3D_size * sizeof(float);
+
+  float *h_u1 = (float *) malloc (grid3D_bytes);
+  float *h_u2 = (float *) malloc (grid3D_bytes);
+  float *h_u3 = (float *) malloc (grid3D_bytes);
+
+  const int pitch = NX;
+
+  // initialise u1
+  int i, j, k;
+    
+  for (k=0; k<NZ; k++) {
+    for (j=0; j<NY; j++) {
+      for (i=0; i<NX; i++) {
+        int ind = i + j*NX + k*NX*NY;
+        if (i==0 || i==NX-1 || j==0 || j==NY-1|| k==0 || k==NZ-1)
+          h_u1[ind] = 1.0f;           // Dirichlet b.c.'s
+        else
+          h_u1[ind] = 0.0f;
+      }
+    }
+  }
+
+  { // sycl scope
+#ifdef USE_GPU
+  gpu_selector dev_sel;
+#else
+  cpu_selector dev_sel;
+#endif
+  queue q(dev_sel);
+
+  buffer<float, 1> d_u1 (h_u1, grid3D_size);
+  buffer<float, 1> d_u2 (grid3D_size);
+
+  d_u1.set_final_data(h_u2);
+
+  // Set up the execution configuration
+  const int bx = 1 + (NX-1)/BLOCK_X;
+  const int by = 1 + (NY-1)/BLOCK_Y;
+
+  range<2> lws (by, bx);
+  range<2> gws (by * BLOCK_Y, bx * BLOCK_X);
+
+  printf("\n globalSize  = %d %d %d \n", bx * BLOCK_X, by * BLOCK_Y, 1);
+  printf(" localSize = %d %d %d \n", BLOCK_X, BLOCK_Y, 1);
+
+  // Execute GPU kernel
+  for (i = 1; i <= REPEAT; ++i) {
+    q.submit([&] (handler &cgh) {
+      auto u1 = d_u1.get_access<sycl_read>(cgh);
+      auto u2 = d_u2.get_access<sycl_discard_write>(cgh);
+      accessor<float, 1, sycl_read_write, access::target::local> sm (3*KOFF, cgh);
+      cgh.parallel_for<class stencil>(nd_range<2>(gws, lws), [=] (nd_item<2> item) {
+        laplace3d(item, NX, NY, NZ, pitch,
+                  u1.get_pointer(), u2.get_pointer(), sm.get_pointer());
+      });
+    });
+    std::swap(d_u1, d_u2);
+  }
+
+  } // sycl scope
+
+  if (verify) {
+    // Reference
+    for (i = 1; i <= REPEAT; ++i) {
+      reference(NX, NY, NZ, h_u1, h_u3);
+      std::swap(h_u1, h_u3);
+    }
+
+    // verify (may take long for large grid sizes)
+    float err = 0.f;
+    for (k=0; k<NZ; k++) {
+      for (j=0; j<NY; j++) {
+        for (i=0; i<NX; i++) {
+          int ind = i + j*NX + k*NX*NY;
+          err += (h_u1[ind]-h_u2[ind])*(h_u1[ind]-h_u2[ind]);
+        }
+      }
+    }
+    printf("\n rms error = %f \n",sqrtf(err/ NX*NY*NZ));
+  }
+
+ // Release CPU memory
+  free(h_u1);
+  free(h_u2);
+  free(h_u3);
+
+  return 0;
+}
+
+
+//Print help screen
+void printHelp(void)
+{
+  printf("Usage:  laplace3d [OPTION]...\n");
+  printf("6-point stencil 3D Laplace test \n");
+  printf("\n");
+  printf("Example: run 100 iterations on a 256x128x128 grid\n");
+  printf("./main 256 128 128 100 1\n");
+
+  printf("\n");
+  printf("Options:\n");
+  printf("Grid width\n");
+  printf("Grid height\n");
+  printf("Grid depth\n");
+  printf("Number of repetitions\n");
+  printf("verify the result\n");
+}
