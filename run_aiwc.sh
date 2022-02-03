67a2b6f99d38de1dbc52b5504f24ca0cb31471bd
Beau Johnston <johnstonbe@ornl.gov>
2022-02-03 06:31:18 -0500

    added sample aiwc runner script to perform the workload characterization

---
+++ run_aiwc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#ComputeCPP installed from https://developer.codeplay.com/products/computecpp/ce/download
+export Oclgrind_DIR=/home/9bj/.oclgrind
+export ComputeCpp_DIR=/home/9bj/computecpp/ComputeCpp-CE-2.7.0-x86_64-linux-gnu
+export CPLUS_INCLUDE_PATH=/opt/rocm/opencl/include
+export VENDOR=codeplay
+export LD_LIBRARY_PATH=$ComputeCpp_DIR/lib:$Oclgrind_DIR/lib:$LD_LIBRARY_PATH
+export PATH=$ComputeCpp_DIR/bin:$PATH
+#for verbose logging with ComputeCpp:
+echo "verbose_output=true" > computecpp.conf
+
+#DPC++ installed with:
+# git clone https://github.com/intel/llvm -b sycl
+# python $HOME/Downloads/llvm/buildbot/configure.py -o build_dpc++_opencl
+# python $HOME/Downloads/llvm/buildbot/compile.py -o build_dpc++_opencl
+#export LD_LIBRARY_PATH=/home/9bj/Downloads/build_dpc++_opencl/lib:$LD_LIBRARY_PATH
+#export PATH=/home/9bj/Downloads/build_dpc++_opencl/bin:$PATH
+# For SYCL
+#export SYCL_BE=PI_OPENCL
+
+for dir in ace-sycl
+#for dir in $(find . -mindepth 1 -maxdepth 1 -type d | grep -Ev '.\.git|include|cuda|omp|hip')
+do
+  cd "${dir}"
+  echo "Benchmark: ${dir} Passes: ${NUM_PASSES}"
+  rm -f aiwc_*.csv report*.txt
+  make clean
+  make VENDOR=$VENDOR GPU=no
+  OCL_ICD_VENDORS=/home/9bj/.oclgrind/lib OCL_ICD_FILENAMES=liboclgrind-rt-icd.so OCLGRIND_WORKLOAD_CHARACTERISATION=1 COMPUTECPP_CONFIGURATION_FILE=computecpp.conf NUM_PASSES=1 make run
+  break
+  #echo "${dir} results:"
+  #for (( i = 0; i < 10; i = i + 1 ))
+  #do
+  #  #cliloader -q -h -d make run &> report${i}.txt
+  #done
+  #grep -H "Total" report*.txt
+  #make clean
+  cd ..
+done
+
