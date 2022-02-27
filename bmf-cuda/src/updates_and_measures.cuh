d0c77decc5cedd5b26c1d248b1d49592c936b67d
Jin Z <5zj@oswald00.ftpn.ornl.gov>
2022-02-27 09:43:38 -0500

    remove the unused functions

--- bmf-cuda/src/updates_and_measures.cuh
+++ bmf-cuda/src/updates_and_measures.cuh
@@ -38,34 +38,6 @@ uint32_t get_flip_mask(const uint8_t factorDim, fast_kiss_state32_t state,
   // return random_many < flipManyChance ? get_flip_mask_all() : get_flip_mask_one(state);
 }
 
-
-// float updates ---------------------------------------------------------------
-#ifdef __CUDACC__
-__inline__ __device__
-float get_float_update_many(fast_kiss_state32_t state) {
-  return 0.1f;
-}
-
-__inline__ __device__
-float get_float_update_one(const uint8_t factorDim, fast_kiss_state32_t state) {
-  const uint32_t lane = fast_kiss32(state) % factorDim;
-  return threadIdx.x % warpSize == lane ? 0.1f : 0.0f;
-}
-
-__inline__ __device__
-float get_float_update(const uint8_t factorDim, fast_kiss_state32_t state, const float flipManyChance) {
-  const float random_many = fast_kiss32(state) / (float) UINT32_MAX;
-  float update = random_many < flipManyChance ? get_float_update_many(state) : get_float_update_one(factorDim, state);
-
-  const float random_factor = fast_kiss32(state) / (float) UINT32_MAX;
-  update = random_factor < 0.5f ? 5*update : update;
-
-  const float random_sign = fast_kiss32(state) / (float) UINT32_MAX;
-  return random_sign < 0.5f ? update : -1.0*update;
-}
-#endif
-
-
 // Metropolis–Hastings algorithm
 template<typename error_t>
 __inline__ HOST_DEVICE_QUALIFIER
