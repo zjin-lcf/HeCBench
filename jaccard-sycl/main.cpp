4a184987bf7db8afc8e93f4723cfab5d4ffc5bc0
Jin Z <5zj@equinox.ftpn.ornl.gov>
2022-02-28 21:53:57 -0500

    format the codes; replace shuffle() with the group functions; factor out the prefix sum function

--- jaccard-sycl/main.cpp
+++ jaccard-sycl/main.cpp
@@ -41,77 +41,51 @@ class jw;
 template<bool weighted, typename T>
 class fill_elements;
 
-/// Atomically add the value operand to the value at the addr and assign the
-/// result to the value at addr, Float version.
-/// \param [in, out] addr The pointer to the data.
-/// \param operand The value to add to the value at \p addr.
-/// \param memoryOrder The memory ordering used.
-/// \return The value at the \p addr before the call.
-template <cl::sycl::access::address_space addressSpace =
-cl::sycl::access::address_space::global_space>
-inline float atomicAdd(
-    float *addr, float operand,
-    cl::sycl::memory_order memoryOrder = cl::sycl::memory_order::relaxed) {
-
-  cl::sycl::atomic<int, addressSpace> obj(
-      (cl::sycl::multi_ptr<int, addressSpace>(reinterpret_cast<int *>(addr))));
-
-  int old_value;
-  float old_float_value;
-
-  do {
-    old_value = obj.load(memoryOrder);
-    old_float_value = *reinterpret_cast<const float *>(&old_value);
-    const float new_float_value = old_float_value + operand;
-    const int new_value = *reinterpret_cast<const int *>(&new_float_value);
-    if (obj.compare_exchange_strong(old_value, new_value, memoryOrder))
-      break;
-  } while (true);
-
-  return old_float_value;
+template<typename T, sycl::memory_scope MemoryScope = sycl::memory_scope::device>
+static inline void atomicAdd(T& val, const T delta)
+{
+  sycl::ext::oneapi::atomic_ref<T, sycl::memory_order::relaxed, 
+     MemoryScope, sycl::access::address_space::global_space> ref(val);
+  ref.fetch_add(delta);
 }
 
-/// Atomically add the value operand to the value at the addr and assign the
-/// result to the value at addr, Double version.
-/// \param [in, out] addr The pointer to the data.
-/// \param operand The value to add to the value at \p addr
-/// \param memoryOrder The memory ordering used.
-/// \return The value at the \p addr before the call.
-template <cl::sycl::access::address_space addressSpace =
-cl::sycl::access::address_space::global_space>
-inline double atomicAdd(
-    double *addr, double operand,
-    cl::sycl::memory_order memoryOrder = cl::sycl::memory_order::relaxed) {
+template<typename T>
+T parallel_prefix_sum(nd_item<3> &item, const int n, const int *ind, global_ptr<T> w) 
+{
+  T sum = 0.0;
+  T last;
+  const int blockDim_x = item.get_local_range(2);
+  const int threadIdx = item.get_local_id(2);
+  auto sg = item.get_sub_group();
 
-  cl::sycl::atomic<unsigned long long int, addressSpace> obj(
-      (cl::sycl::multi_ptr<unsigned long long int, addressSpace>(
-                       reinterpret_cast<unsigned long long int *>(addr))));
+  //the number of nnz elements in a row is a multiple of blockDim_x.x
+  int mn =((n+blockDim_x-1)/blockDim_x*blockDim_x); 
 
-  unsigned long long int old_value;
-  double old_double_value;
+  for (int i=threadIdx; i<mn; i+=blockDim_x) {
+    bool valid = i < n;
+    last = sycl::select_from_group(sg, sum, blockDim_x-1);
+    sum = (valid) ? w[ind[i]] : 0.0;
 
-  do {
-    old_value = obj.load(memoryOrder);
-    old_double_value = *reinterpret_cast<const double *>(&old_value);
-    const double new_double_value = old_double_value + operand;
-    const unsigned long long int new_value =
-      *reinterpret_cast<const unsigned long long int *>(&new_double_value);
-
-    if (obj.compare_exchange_strong(old_value, new_value, memoryOrder))
-      break;
-  } while (true);
-
-  return old_double_value;
+    for (int j=1; j<blockDim_x; j*=2) {
+      T v = sycl::shift_group_right(sg, sum, j);
+      if (threadIdx >= j) sum += v;
+    }
+    sum += last;
+  }
+  last = sycl::select_from_group(sg, sum, blockDim_x-1);
+  return last;
 }
 
 
 // Volume of neighboors (*weight_s)
 template<bool weighted, typename T>
-void jaccard_row_sum(queue &q, const int n, 
-    buffer<int, 1> &d_csrPtr, 
-    buffer<int, 1> &d_csrInd, 
-    buffer<T, 1> &d_weight_j, 
-    buffer<T, 1> &d_work) 
+void jaccard_row_sum(
+  queue &q,
+  const int n, 
+  buffer<int, 1> &d_csrPtr, 
+  buffer<int, 1> &d_csrInd, 
+  buffer<T, 1> &d_weight_j, 
+  buffer<T, 1> &d_work) 
 {
 
   const int y = 4;
@@ -119,96 +93,60 @@ void jaccard_row_sum(queue &q, const int n,
   range<3> sum_lws (1, y, 64/y);
 
   q.submit([&] (handler &cgh) {
-      auto csrPtr = d_csrPtr.get_access<sycl_read>(cgh);
-      auto csrInd = d_csrInd.get_access<sycl_read>(cgh);
-      auto w = d_weight_j.template get_access<sycl_read>(cgh);
-      auto work = d_work.template get_access<sycl_discard_write>(cgh);
-      cgh.parallel_for<class row_sum<weighted,T>>(nd_range<3>(sum_gws, sum_lws), [=] (nd_item<3> item) {
+    auto csrPtr = d_csrPtr.get_access<sycl_read>(cgh);
+    auto csrInd = d_csrInd.get_access<sycl_read>(cgh);
+    auto w = d_weight_j.template get_access<sycl_read>(cgh);
+    auto work = d_work.template get_access<sycl_discard_write>(cgh);
+    cgh.parallel_for<class row_sum<weighted,T>>(nd_range<3>(sum_gws, sum_lws), [=] (nd_item<3> item) 
+         [[sycl::reqd_sub_group_size(32)]] {
 
-          for(int row = item.get_global_id(1); row < n; 
+      for(int row = item.get_global_id(1); row < n; 
               row += item.get_group_range(1)*item.get_local_range(1)) {
-          int start = csrPtr[row];
-          int end   = csrPtr[row+1];
-          int length= end-start;
-          //compute row sums 
-          if (weighted) {
-          //T sum = parallel_prefix_sum(length, csrInd + start, w); 
-
-          T sum = 0.0;
-          T last;
-          int blockDim = item.get_local_range(2);
-          int threadIdx = item.get_local_id(2);
-
-          //the number of nnz elements in a row is a multiple of blockDim.x
-          int mn =((length+blockDim-1)/blockDim*blockDim); 
-
-          for (int i=threadIdx; i<mn; i+=blockDim) {
-            //All threads (especially the last one) must always participate
-            //in the shfl instruction, otherwise their sum will be undefined.
-            //So, the loop stopping condition is based on multiple of n in loop increments,
-            //so that all threads enter into the loop and inside we make sure we do not
-            //read out of bounds memory checking for the actual size n.
-
-            //check if the thread is valid
-            bool valid = i < length;
-
-            //Notice that the last thread is used to propagate the prefix sum.
-            //For all the threads, in the first iteration the last is 0, in the following
-            //iterations it is the value at the last thread of the previous iterations.
-
-            //get the value of the last thread
-            last = item.get_sub_group().shuffle(sum, blockDim-1);
-
-            //if you are valid read the value from memory, otherwise set your value to 0
-            sum = (valid) ? w[csrInd[start+i]] : 0.0;
-
-            //do prefix sum (of size warpSize=blockDim =< 32)
-            for (int j=1; j<blockDim; j*=2) {
-              T v = item.get_sub_group().shuffle_up(sum, j);
-              if (threadIdx >= j) sum += v;
-            }
-            //shift by last
-            sum += last;
-          }
-          //get the value of the last thread (to all threads)
-          if (threadIdx == 0) work[row] = item.get_sub_group().shuffle(sum, blockDim-1);
-          }
-          else {
-            work[row] = (T)length;
-          }
-          }
-      });
+        int start = csrPtr[row];
+        int end   = csrPtr[row+1];
+        int length= end-start;
+        if (weighted) {
+          T sum = parallel_prefix_sum(item, length, csrInd.get_pointer() + start, w.get_pointer()); 
+          if (item.get_local_id(2) == 0) work[row] = sum;
+        } else {
+          work[row] = (T)length;
+        }
+      }
+    });
   });
 }
 
 // Volume of intersections (*weight_i) and cumulated volume of neighboors (*weight_s)
 // Note the number of columns is constrained by the number of rows
-  template<bool weighted, typename T>
-void jaccard_is(queue &q, const int n, const int e, 
-    buffer<int, 1> &d_csrPtr, 
-    buffer<int, 1> &d_csrInd, 
-    buffer<T, 1> &d_weight_j, 
-    buffer<T, 1> &d_work, 
-    buffer<T, 1> &d_weight_i, 
-    buffer<T, 1> &d_weight_s)
+template<bool weighted, typename T>
+void jaccard_is(
+  queue &q,
+  const int n,
+  const int e, 
+  buffer<int, 1> &d_csrPtr, 
+  buffer<int, 1> &d_csrInd, 
+  buffer<T, 1> &d_weight_j, 
+  buffer<T, 1> &d_work, 
+  buffer<T, 1> &d_weight_i, 
+  buffer<T, 1> &d_weight_s)
 {
   const int y = 4;
   range<3> is_gws ((n+7)/8*8, y, 32/y);
   range<3> is_lws(8, y, 32/y);
 
   q.submit([&] (handler &cgh) {
-      auto csrPtr = d_csrPtr.get_access<sycl_read>(cgh);
-      auto csrInd = d_csrInd.get_access<sycl_read>(cgh);
-      auto weight_i = d_weight_i.template get_access<sycl_read_write>(cgh);
-      auto weight_s = d_weight_s.template get_access<sycl_write>(cgh);
-      auto v = d_weight_j.template get_access<sycl_read>(cgh);
-      auto work = d_work.template get_access<sycl_read>(cgh);
-      cgh.parallel_for<class intersection<weighted,T>>(nd_range<3>(is_gws, is_lws), [=] (nd_item<3> item) {
+    auto csrPtr = d_csrPtr.get_access<sycl_read>(cgh);
+    auto csrInd = d_csrInd.get_access<sycl_read>(cgh);
+    auto weight_i = d_weight_i.template get_access<sycl_read_write>(cgh);
+    auto weight_s = d_weight_s.template get_access<sycl_write>(cgh);
+    auto v = d_weight_j.template get_access<sycl_read>(cgh);
+    auto work = d_work.template get_access<sycl_read>(cgh);
+    cgh.parallel_for<class intersection<weighted,T>>(nd_range<3>(is_gws, is_lws), [=] (nd_item<3> item) {
 
-          for(int row = item.get_global_id(0); row < n; 
+      for(int row = item.get_global_id(0); row < n; 
               row += item.get_group_range(0)*item.get_local_range(0)) {
-          for (int j = csrPtr[row]+item.get_global_id(1); j < csrPtr[row+1]; 
-              j+= item.get_local_range(1) * item.get_group_range(1)) {
+        for (int j = csrPtr[row]+item.get_global_id(1); j < csrPtr[row+1]; 
+                 j+= item.get_local_range(1) * item.get_group_range(1)) {
 
           int col = csrInd[j];
           //find which row has least elements (and call it reference row)
@@ -223,7 +161,7 @@ void jaccard_is(queue &q, const int n, const int e,
           //compute new intersection weights 
           //search for the element with the same column index in the reference row
           for (int i = csrPtr[ref]+item.get_global_id(2); i < csrPtr[ref+1]; 
-              i += item.get_local_range(2) * item.get_group_range(2)) {
+                   i += item.get_local_range(2) * item.get_group_range(2)) {
             int match  =-1;
             int ref_col = csrInd[i];
             T ref_val = weighted ? v[ref_col] : (T)1.0;
@@ -248,22 +186,24 @@ void jaccard_is(queue &q, const int n, const int e,
 
             //if the element with the same column index in the reference row has been found
             if (match != -1){
-              atomicAdd(&weight_i[j], ref_val);
+              atomicAdd(weight_i[j], ref_val);
             }
           }
-          }
-          }
-      });
+        }
+      }
+    });
   });
 }
 
-  template<bool weighted, typename T>
-void jaccard_jw(queue &q, const int e, 
-    buffer<T, 1> &d_csrVal, 
-    const T gamma, 
-    buffer<T, 1> &d_weight_i, 
-    buffer<T, 1> &d_weight_s, 
-    buffer<T, 1> &d_weight_j) 
+template<bool weighted, typename T>
+void jaccard_jw(
+  queue &q,
+  const int e, 
+  buffer<T, 1> &d_csrVal, 
+  const T gamma, 
+  buffer<T, 1> &d_weight_i, 
+  buffer<T, 1> &d_weight_s, 
+  buffer<T, 1> &d_weight_j) 
 {
   int threads = std::min(e, MAX_KERNEL_THREADS);
 
@@ -271,40 +211,37 @@ void jaccard_jw(queue &q, const int e,
   range<1> jw_lws (threads);
 
   q.submit([&] (handler &cgh) {
-      auto csrVal = d_csrVal.template get_access<sycl_read>(cgh);
-      auto weight_i = d_weight_i.template get_access<sycl_read>(cgh);
-      auto weight_s = d_weight_s.template get_access<sycl_read>(cgh);
-      auto weight_j = d_weight_j.template get_access<sycl_discard_write>(cgh);
-      cgh.parallel_for<class jw<weighted,T>>(nd_range<1>(jw_gws, jw_lws), [=] (nd_item<1> item) {
-          for (int j = item.get_global_id(0); j < e; 
-              j += item.get_group_range(0)*item.get_local_range(0)) {
-          T Wi =  weight_i[j];
-          T Ws =  weight_s[j];
-          weight_j[j] = (gamma*csrVal[j])* (Wi/(Ws-Wi));
-          }
-          });
-      });
+    auto csrVal = d_csrVal.template get_access<sycl_read>(cgh);
+    auto weight_i = d_weight_i.template get_access<sycl_read>(cgh);
+    auto weight_s = d_weight_s.template get_access<sycl_read>(cgh);
+    auto weight_j = d_weight_j.template get_access<sycl_discard_write>(cgh);
+    cgh.parallel_for<class jw<weighted,T>>(nd_range<1>(jw_gws, jw_lws), [=] (nd_item<1> item) {
+      for (int j = item.get_global_id(0); j < e; 
+               j += item.get_group_range(0)*item.get_local_range(0)) {
+        T Wi =  weight_i[j];
+        T Ws =  weight_s[j];
+        weight_j[j] = (gamma*csrVal[j])* (Wi/(Ws-Wi));
+      }
+    });
+  });
 }
 
 
-  template <bool weighted, typename T>
+template <bool weighted, typename T>
 void fill_weights(queue &q, const int e, buffer<T, 1> &d_w, const T value) 
 {
   range<1> fill_gws((e+MAX_KERNEL_THREADS-1)/MAX_KERNEL_THREADS*MAX_KERNEL_THREADS);  
   range<1> fill_lws(MAX_KERNEL_THREADS);
   q.submit([&] (handler &cgh) {
-      auto w = d_w.template get_access<sycl_discard_write>(cgh);
-      cgh.parallel_for<class fill_elements<weighted, T>>(nd_range<1>(fill_gws, fill_lws), [=] (nd_item<1> item) {
-          int j = item.get_global_id(0);
-          // e.g. w[0] is the weight of a non-zeron element when csr_ind[i] equals 0. 
-          // So multiple non-zero elements on different rows of a matrix may share 
-          // the same weight value
-          w[j] = weighted ? (T)(j+1)/e : value; 
-          });
-      });
+    auto w = d_w.template get_access<sycl_discard_write>(cgh);
+    cgh.parallel_for<class fill_elements<weighted, T>>(nd_range<1>(fill_gws, fill_lws), [=] (nd_item<1> item) {
+      for (int j = item.get_global_id(0); j<e; j+=item.get_group_range(0)*item.get_local_range(0))
+        w[j] = weighted ? (T)(j+1)/e : value; 
+    });
+  });
 }
 
-  template <bool weighted, typename T>
+template <bool weighted, typename T>
 void jaccard_weight (queue &q, const int iteration, const int n, const int e, 
     int* csr_ptr, int* csr_ind, T* csr_val)
 {
@@ -338,10 +275,9 @@ void jaccard_weight (queue &q, const int iteration, const int n, const int e,
 
 #ifdef DEBUG
       q.submit([&] (handler &cgh) {
-          auto work_acc = d_work.template get_access<sycl_read>(cgh);
-          cgh.copy(work_acc, work);
-          });
-      q.wait();
+        auto work_acc = d_work.template get_access<sycl_read>(cgh);
+        cgh.copy(work_acc, work);
+      }).wait();
       for (int i = 0; i < n; i++) printf("work: %d %f\n", i, work[i]);
 #endif
 
@@ -351,16 +287,14 @@ void jaccard_weight (queue &q, const int iteration, const int n, const int e,
 
 #ifdef DEBUG
       q.submit([&] (handler &cgh) {
-          auto weight_i_acc = d_weight_i.template get_access<sycl_read>(cgh);
-          cgh.copy(weight_i_acc, weight_i);
-          });
-      q.wait();
+        auto weight_i_acc = d_weight_i.template get_access<sycl_read>(cgh);
+        cgh.copy(weight_i_acc, weight_i);
+      }).wait();
       for (int i = 0; i < e; i++) printf("wi: %d %f\n", i, weight_i[i]);
       q.submit([&] (handler &cgh) {
-          auto weight_s_acc = d_weight_s.template get_access<sycl_read>(cgh);
-          cgh.copy(weight_s_acc, weight_s);
-          });
-      q.wait();
+        auto weight_s_acc = d_weight_s.template get_access<sycl_read>(cgh);
+        cgh.copy(weight_s_acc, weight_s);
+      }).wait();
       for (int i = 0; i < e; i++) printf("ws: %d %f\n", i, weight_s[i]);
 #endif
 
