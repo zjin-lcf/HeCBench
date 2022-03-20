81d213f01fa8563d4627d10242b4196259719c91
Jin Z <5zj@equinox.ftpn.ornl.gov>
2022-03-20 10:27:22 -0400

    add the pns example in omp

---
+++ pns-omp/main.cpp
@@ -0,0 +1,177 @@
+/***************************************************************************
+ *
+ *            (C) Copyright 2007 The Board of Trustees of the
+ *                        University of Illinois
+ *                         All Rights Reserved
+ *
+ ***************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <sys/time.h>
+#include <omp.h>
+
+
+/*
+// Place and Transition are implicitly included in the code
+// as the grid is a fixed one
+typedef struct {
+        float mark;
+} Place;
+
+typedef struct {
+        int from1, from2;
+        int to1, to2;
+} Transition;
+
+// this starts from row 0 and col 0
+P(r,c)    -> T(r,c)   -> P(r,c+1)  ->
+  |            |            |
+ \/           \/           \/
+T(r+1,c-1)-> P(r+1,c) -> T(r+1,c)  ->
+  |            |            |
+ \/           \/           \/
+P(r+2,c)  -> T(r+2,c) -> P(r+2,c+1)->
+  |            |            |
+ \/           \/           \/
+T(r+3,c-1)-> P(r+3,c) -> T(r+3,c)->
+  |            |            |
+ \/           \/           \/
+
+*/
+
+#include "rand_gen.cpp"
+#include "petri_kernel.cpp"
+
+static int N, S, T, NSQUARE2;
+uint32 host_mt[MERS_N];
+
+void PetrinetOnDevice();
+void compute_statistics();
+
+float results[4];
+float* h_vars;
+int* h_maxs;
+
+long long get_time() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return (tv.tv_sec * 1000000) + tv.tv_usec;
+}
+
+int main(int argc, char** argv) 
+{
+  if (argc<4) 
+    {
+      printf("Usage: petri n s t\n"
+	     "n: the place-transition grid is 2nX2n\n"
+	     "s: the maximum steps in a trajectory\n"
+	     "t: number of trajectories\n");
+      return -1;
+    }
+
+  N = atoi(argv[1]);
+  if (N<1)
+    return -1;
+  S = atoi(argv[2]);
+  if (S<1)
+    return -1;
+
+  T = atoi(argv[3]);
+  if (T<1)
+    return -1;
+
+  NSQUARE2 = N*(N+N);
+  
+  h_vars = (float*)malloc(T*sizeof(float));
+  h_maxs = (int*)malloc(T*sizeof(int));
+  
+  // compute the simulation on the GPU
+
+  auto start = get_time();
+
+  PetrinetOnDevice();
+
+  auto end = get_time();
+  printf("Total device execution time: %.2f s\n", (end - start) / 1e6f);
+
+  compute_statistics();
+
+  free(h_vars);
+  free(h_maxs);
+    
+  printf("petri N=%d S=%d T=%d\n", N, S, T);
+  printf("mean_vars: %f    var_vars: %f\n", results[0], results[1]);
+  printf("mean_maxs: %f    var_maxs: %f\n", results[2], results[3]);
+
+  return 0;
+}
+
+void compute_statistics() 
+{
+  float sum = 0;
+  float sum_vars = 0;
+  float sum_max = 0;
+  float sum_max_vars = 0;
+  int i;
+  for (i=0; i<T; i++) 
+    {
+      sum += h_vars[i];
+      sum_vars += h_vars[i]*h_vars[i];
+      sum_max += h_maxs[i];
+      sum_max_vars += h_maxs[i]*h_maxs[i];
+    }
+  results[0] = sum/T;
+  results[1] = sum_vars/T - results[0]*results[0];
+  results[2] = sum_max/T;
+  results[3] = sum_max_vars/T - results[2]*results[2];
+}
+
+void PetrinetOnDevice()
+{
+  // Allocate memory
+  int i;
+  int unit_size = NSQUARE2*(sizeof(int)+sizeof(char))+sizeof(float)+sizeof(int);
+  int block_num = MAX_DEVICE_MEM/unit_size;
+
+  printf("Number of thread blocks: %d\n", block_num);
+
+  const int places_size_byte = (unit_size - sizeof(float) - sizeof(int))*block_num / sizeof(int);
+  const int places_size_word =  places_size_byte / sizeof(int);
+  int* h_places = (int*) malloc (places_size_byte);
+  
+  #pragma omp target data map (alloc: h_vars[0:block_num], \
+                                      h_maxs[0:block_num], \
+                                      h_places[0:places_size_word])
+  {
+    // Launch the device computation threads!
+    for (i = 0; i<T-block_num; i+=block_num) {
+      #pragma omp target teams num_teams(block_num) thread_limit(256)
+      {
+        uint32 mt [MERS_N];
+        #pragma omp parallel 
+        {
+          PetrinetKernel(mt, h_places, h_vars, h_maxs, N, S, 5489*(i+1));
+        }
+      }
+
+      #pragma omp target update to (h_maxs[i:i+block_num])
+      #pragma omp target update to (h_vars[i:i+block_num])
+    }
+          
+    #pragma omp target teams num_teams(T-i) thread_limit(256)
+    {
+      uint32 mt [MERS_N];
+      #pragma omp parallel 
+      {
+        PetrinetKernel(mt, h_places, h_vars, h_maxs, N, S, 5489*(i+1));
+      }
+    }
+
+    #pragma omp target update to (h_maxs[i:i+T-i])
+    #pragma omp target update to (h_vars[i:i+T-i])
+  }
+
+  free(h_places);
+}
---
+++ pns-omp/rand_gen.cpp
@@ -0,0 +1,132 @@
+/***************************************************************************
+ *cr
+ *cr            (C) Copyright 2007 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ***************************************************************************/
+#include "randomc.h"
+
+#define LOWER_MASK ((1LU << MERS_R) - 1)         
+#define UPPER_MASK (0xFFFFFFFF << MERS_R)        
+
+/* 
+   The following two functions implement the Mersenne Twister random 
+   number generator.  The copyright notice/disclaimer, etc are related
+   to this code.
+
+   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+   All rights reserved.                          
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+     1. Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+
+     2. Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+
+     3. The names of its contributors may not be used to endorse or promote 
+        products derived from this software without specific prior written 
+        permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+void RandomInit(uint32 *mt, uint32 seed) 
+{
+  int i;
+  // re-seed generator
+  if(omp_get_thread_num() == 0)
+    {
+      mt[0]= seed & 0xffffffffUL;
+      for (i=1; i < MERS_N; i++) 
+	{
+	  mt[i] = (1812433253UL * (mt[i-1] ^ (mt[i-1] >> 30)) + i);
+  	}
+    }
+  #pragma omp barrier
+}
+
+void BRandom(uint32* mt) 
+{
+  // generate 32 random bits
+  uint32 y;
+  int thdx;
+  int threadIdx_x = omp_get_thread_num();
+
+  // block size is 256
+  // step 1: 0-226, MERS_N-MERS_M=227
+  if (threadIdx_x<MERS_N-MERS_M) 
+    {
+      y = (mt[threadIdx_x] & UPPER_MASK) | (mt[threadIdx_x+1] & LOWER_MASK);
+      y = mt[threadIdx_x+MERS_M] ^ (y >> 1) ^ ( (y & 1)? MERS_A: 0);
+    }
+  #pragma omp barrier
+  if (threadIdx_x<MERS_N-MERS_M) 
+    {
+      mt[threadIdx_x] = y;
+    }
+  #pragma omp barrier
+  
+  // step 2: 227-453
+  thdx = threadIdx_x + (MERS_N-MERS_M);
+  if (threadIdx_x<MERS_N-MERS_M) 
+    {
+      y = (mt[thdx] & UPPER_MASK) | (mt[thdx+1] & LOWER_MASK);
+      y = mt[threadIdx_x] ^ (y >> 1) ^ ( (y & 1)? MERS_A: 0);
+    }
+  #pragma omp barrier
+  if (threadIdx_x<MERS_N-MERS_M) 
+    {
+      mt[thdx] = y;
+    }
+  #pragma omp barrier
+  
+  // step 3: 454-622
+  thdx += (MERS_N-MERS_M);
+  if (thdx < MERS_N-1) 
+    {
+      y = (mt[thdx] & UPPER_MASK) | (mt[thdx+1] & LOWER_MASK);
+      y = mt[threadIdx_x+(MERS_N-MERS_M)] ^ (y >> 1) ^ ( (y & 1)? MERS_A: 0);
+    }
+  #pragma omp barrier
+  if (thdx < MERS_N-1) 
+    {
+      mt[thdx] = y;
+    }
+  #pragma omp barrier
+
+  // step 4: 623
+  if (threadIdx_x == 0) 
+    {
+      y = (mt[MERS_N-1] & UPPER_MASK) | (mt[0] & LOWER_MASK);
+      mt[MERS_N-1] = mt[MERS_M-1] ^ (y >> 1) ^ ( (y & 1)? MERS_A: 0);
+    }
+  #pragma omp barrier
+
+  // Tempering (May be omitted):
+  y ^=  y >> MERS_U;
+  y ^= (y << MERS_S) & MERS_B;
+  y ^= (y << MERS_T) & MERS_C;
+  y ^=  y >> MERS_L;
+
+}
+
+
+// return a random in [0, max]
+// #define Random(max_plus_1)  (BRandom()% (max_plus_1))
+
