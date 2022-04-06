b68ba4708abd80281b4867de9c59db3db17a5f3d
u58480 <u58480@s001-n182.aidevcloud>
2022-04-06 08:54:20 -0700

    add warpsort in sycl; remove DPCT headers and macros

---
+++ warpsort-sycl/cuda/Comparators.dp.hpp
@@ -0,0 +1,32 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+#pragma once
+
+#include <CL/sycl.hpp>
+
+namespace facebook { namespace cuda {
+
+/**
+   Prototype:
+
+   ~~~{.cpp}
+   template <typename T>
+   struct Comparator {
+     static __device__ __forceinline__ bool compare(const T lhs, const T rhs);
+   };
+   ~~~
+*/
+template <typename T>
+struct GreaterThan {
+  static inline bool compare(const T lhs, const T rhs) {
+    return (lhs > rhs);
+  }
+};
+
+template <typename T>
+struct LessThan {
+  static inline bool compare(const T lhs, const T rhs) {
+    return (lhs < rhs);
+  }
+};
+
+} } // namespace
---
+++ warpsort-sycl/cuda/ShuffleTypes.dp.hpp
@@ -0,0 +1,74 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+#pragma once
+
+#include <CL/sycl.hpp>
+#include "cuda/Pair.dp.hpp"
+
+namespace facebook { namespace cuda {
+
+/** @file
+
+    Templated warp shuffles that work for basic and pair types
+*/
+
+template <typename T>
+inline T shfl(const T val, int srcLane, sycl::nd_item<1> &item,
+                       int width = WARP_SIZE) {
+  return sycl::select_from_group(item.get_sub_group(), val, srcLane);
+}
+
+template <typename T>
+inline T shfl_up(const T val, int delta, sycl::nd_item<1> &item,
+                          int width = WARP_SIZE) {
+  return sycl::shift_group_right(item.get_sub_group(), val, delta);
+}
+
+template <typename T>
+inline T shfl_down(const T val, int delta, sycl::nd_item<1> &item,
+                            int width = WARP_SIZE) {
+  return sycl::shift_group_left(item.get_sub_group(), val, delta);
+}
+
+template <typename T>
+inline T shfl_xor(const T val, int laneMask, sycl::nd_item<1> &item,
+                           int width = WARP_SIZE) {
+  return sycl::permute_group_by_xor(item.get_sub_group(), val, laneMask);
+}
+
+template <typename K, typename V>
+inline Pair<K, V> shfl(const Pair<K, V> &p, int srcLane,
+                                sycl::nd_item<1> &item,
+                                int width = WARP_SIZE) {
+  return Pair<K, V>(
+      sycl::select_from_group(item.get_sub_group(), p.k, srcLane),
+      sycl::select_from_group(item.get_sub_group(), p.v, srcLane));
+}
+
+template <typename K, typename V>
+inline Pair<K, V> shfl_up(const Pair<K, V> &p, int delta,
+                                   sycl::nd_item<1> &item,
+                                   int width = WARP_SIZE) {
+  return Pair<K, V>(
+      sycl::shift_group_right(item.get_sub_group(), p.k, delta),
+      sycl::shift_group_right(item.get_sub_group(), p.v, delta));
+}
+
+template <typename K, typename V>
+inline Pair<K, V> shfl_down(const Pair<K, V> &p, int delta,
+                                     sycl::nd_item<1> &item,
+                                     int width = WARP_SIZE) {
+  return Pair<K, V>(
+      sycl::shift_group_left(item.get_sub_group(), p.k, delta),
+      sycl::shift_group_left(item.get_sub_group(), p.v, delta));
+}
+
+template <typename K, typename V>
+inline Pair<K, V> shfl_xor(const Pair<K, V> &p, int laneMask,
+                                    sycl::nd_item<1> &item,
+                                    int width = WARP_SIZE) {
+  return Pair<K, V>(
+      sycl::permute_group_by_xor(item.get_sub_group(), p.k, laneMask),
+      sycl::permute_group_by_xor(item.get_sub_group(), p.v, laneMask));
+}
+
+} } // namespace
