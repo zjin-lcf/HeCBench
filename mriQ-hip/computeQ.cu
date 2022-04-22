7e8d42d9ea39e2b4065b38cc20fddbc718cdf3fa
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-04-22 08:04:22 -0400

    add the mriQ in hip; the datasets are not included (see README)

---
+++ mriQ-hip/computeQ.cu
@@ -0,0 +1,152 @@
+/***************************************************************************
+ *
+ *            (C) Copyright 2007 The Board of Trustees of the
+ *                        University of Illinois
+ *                         All Rights Reserved
+ *
+ ***************************************************************************/
+
+#define PI   3.1415926535897932384626433832795029f
+#define PIx2 6.2831853071795864769252867665590058f
+
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+#define K_ELEMS_PER_GRID 2048
+
+#define KERNEL_PHI_MAG_THREADS_PER_BLOCK 256
+#define KERNEL_Q_THREADS_PER_BLOCK 256
+#define KERNEL_Q_K_ELEMS_PER_GRID 1024
+
+#define HIP_ERRCK							\
+  {hipError_t err;							\
+    if ((err = hipGetLastError()) != hipSuccess) {			\
+      fprintf(stderr, "HIP error on line %d: %s\n", __LINE__, hipGetErrorString(err)); \
+      exit(-1);								\
+    }									\
+  }
+
+struct kValues {
+  float Kx;
+  float Ky;
+  float Kz;
+  float PhiMag;
+};
+
+/* Values in the k-space coordinate system are stored in constant memory
+ * on the GPU */
+__constant__ __device__ kValues ck[KERNEL_Q_K_ELEMS_PER_GRID];
+
+__global__ void
+ComputePhiMag_GPU(
+  const float* __restrict__ phiR,
+  const float* __restrict__ phiI,
+        float* __restrict__ phiMag,
+  const int numK)
+{
+  int indexK = blockIdx.x*KERNEL_PHI_MAG_THREADS_PER_BLOCK + threadIdx.x;
+  if (indexK < numK) {
+    float real = phiR[indexK];
+    float imag = phiI[indexK];
+    phiMag[indexK] = real*real + imag*imag;
+  }
+}
+
+__global__ void
+ComputeQ_GPU(
+  const int numK,
+        int kGlobalIndex,
+  const float* __restrict__ x,
+  const float* __restrict__ y,
+  const float* __restrict__ z,
+        float* __restrict__ Qr,
+        float* __restrict__ Qi)
+{
+  // Determine the element of the X arrays computed by this thread
+  int xIndex = blockIdx.x*KERNEL_Q_THREADS_PER_BLOCK + threadIdx.x;
+
+  // Read block's X values from global mem to shared mem
+  float sX = x[xIndex];
+  float sY = y[xIndex];
+  float sZ = z[xIndex];
+  float sQr = Qr[xIndex];
+  float sQi = Qi[xIndex];
+
+  // Loop over all elements of K in constant mem to compute a partial value
+  // for X.
+  int kIndex = 0;
+  if (numK % 2) {
+    float expArg = PIx2 * (ck[0].Kx * sX + ck[0].Ky * sY + ck[0].Kz * sZ);
+    sQr += ck[0].PhiMag * cosf(expArg);
+    sQi += ck[0].PhiMag * sinf(expArg);
+    kIndex++;
+    kGlobalIndex++;
+  }
+
+  for (; (kIndex < KERNEL_Q_K_ELEMS_PER_GRID) && (kGlobalIndex < numK);
+       kIndex += 2, kGlobalIndex += 2) {
+    float expArg = PIx2 * (ck[kIndex].Kx * sX +
+			   ck[kIndex].Ky * sY +
+			   ck[kIndex].Kz * sZ);
+    sQr += ck[kIndex].PhiMag * cosf(expArg);
+    sQi += ck[kIndex].PhiMag * sinf(expArg);
+
+    int kIndex1 = kIndex + 1;
+    float expArg1 = PIx2 * (ck[kIndex1].Kx * sX +
+			    ck[kIndex1].Ky * sY +
+			    ck[kIndex1].Kz * sZ);
+    sQr += ck[kIndex1].PhiMag * cosf(expArg1);
+    sQi += ck[kIndex1].PhiMag * sinf(expArg1);
+  }
+
+  Qr[xIndex] = sQr;
+  Qi[xIndex] = sQi;
+}
+
+void computePhiMag_GPU(int numK, float* phiR_d, float* phiI_d, float* phiMag_d)
+{
+  int phiMagBlocks = numK / KERNEL_PHI_MAG_THREADS_PER_BLOCK;
+  if (numK % KERNEL_PHI_MAG_THREADS_PER_BLOCK)
+    phiMagBlocks++;
+
+  dim3 DimPhiMagBlock(KERNEL_PHI_MAG_THREADS_PER_BLOCK);
+  dim3 DimPhiMagGrid(phiMagBlocks);
+
+  hipLaunchKernelGGL(ComputePhiMag_GPU, DimPhiMagGrid, DimPhiMagBlock , 0, 0,
+                     phiR_d, phiI_d, phiMag_d, numK);
+}
+
+void computeQ_GPU(int numK, int numX,
+                  float* x_d, float* y_d, float* z_d,
+                  kValues* kVals,
+                  float* Qr_d, float* Qi_d)
+{
+  int QGrids = numK / KERNEL_Q_K_ELEMS_PER_GRID;
+  if (numK % KERNEL_Q_K_ELEMS_PER_GRID) QGrids++;
+
+  int QBlocks = numX / KERNEL_Q_THREADS_PER_BLOCK;
+  if (numX % KERNEL_Q_THREADS_PER_BLOCK) QBlocks++;
+
+  dim3 DimQBlock(KERNEL_Q_THREADS_PER_BLOCK);
+  dim3 DimQGrid(QBlocks);
+
+  for (int QGrid = 0; QGrid < QGrids; QGrid++) {
+    // Put the tile of K values into constant mem
+    int QGridBase = QGrid * KERNEL_Q_K_ELEMS_PER_GRID;
+    kValues* kValsTile = kVals + QGridBase;
+    int numElems = MIN(KERNEL_Q_K_ELEMS_PER_GRID, numK - QGridBase);
+
+    hipMemcpyToSymbol(HIP_SYMBOL(ck), kValsTile, numElems * sizeof(kValues), 0);
+
+    hipLaunchKernelGGL(ComputeQ_GPU, DimQGrid, DimQBlock , 0, 0,
+                       numK, QGridBase, x_d, y_d, z_d, Qr_d, Qi_d);
+  }
+  hipDeviceSynchronize();
+}
+
+void createDataStructsCPU(int numK, int numX, float** phiMag,
+	 float** Qr, float** Qi)
+{
+  *phiMag = (float* ) memalign(16, numK * sizeof(float));
+  *Qr = (float*) memalign(16, numX * sizeof (float));
+  *Qi = (float*) memalign(16, numX * sizeof (float));
+}
+
