4818dde74a86de8244f7a583af04bcb6fd243caf
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-04-22 07:15:47 -0400

    add the merkle tree in sycl; choose the approach 1 version  only and add a debug macro for dumping the results for verification

---
+++ merkle-sycl/bench_merkle_tree.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <CL/sycl.hpp>
+
+// Returns exact time of kernel execution in nanosecond
+//
+// Ensure SYCL queue has profiling enabled !
+uint64_t
+benchmark_merklize_approach_1(sycl::queue& q,
+                              const size_t leaf_count,
+                              const size_t wg_size);
