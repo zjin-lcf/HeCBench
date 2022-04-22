ab5397e02a9e8c6ed5598890f73e78e23b5b5a92
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-04-22 08:05:57 -0400

    add the mriQ in sycl; the datasets are not included (see README)

---
+++ mriQ-sycl/computeQ.cpp
@@ -0,0 +1,187 @@
+/***************************************************************************
+ *
+ *            (C) Copyright 2007 The Board of Trustees of the
+ *                        University of Illinois
+ *                         All Rights Reserved
+ *
+ ***************************************************************************/
+
+#define PI   3.1415926535897932384626433832795029f
+#define PIx2 6.2831853071795864769252867665590058f
+
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+#define K_ELEMS_PER_GRID 2048
+
+#define KERNEL_PHI_MAG_THREADS_PER_BLOCK 256
+#define KERNEL_Q_THREADS_PER_BLOCK 256
+#define KERNEL_Q_K_ELEMS_PER_GRID 1024
+
+struct kValues {
+  float Kx;
+  float Ky;
+  float Kz;
+  float PhiMag;
+};
+
+void ComputePhiMag_GPU(
+  nd_item<1> &item,
+  const int numK,
+  const float* __restrict phiR,
+  const float* __restrict phiI,
+        float* __restrict phiMag)
+{
+  int indexK = item.get_global_id(0);
+  if (indexK < numK) {
+    float real = phiR[indexK];
+    float imag = phiI[indexK];
+    phiMag[indexK] = real*real + imag*imag;
+  }
+}
+
+void ComputeQ_GPU(
+  nd_item<1> &item,
+  const int numK,
+        int kGlobalIndex,
+  const kValues* __restrict ck,
+  const float* __restrict x,
+  const float* __restrict y,
+  const float* __restrict z,
+        float* __restrict Qr,
+        float* __restrict Qi)
+{
+  // Determine the element of the X arrays computed by this thread
+  int xIndex = item.get_global_id(0);
+
+  // Read block's X values from global mem to shared mem
+  float sX = x[xIndex];
+  float sY = y[xIndex];
+  float sZ = z[xIndex];
+  float sQr = Qr[xIndex];
+  float sQi = Qi[xIndex];
+
+  // Loop over all elements of K in constant mem to compute a partial value
+  // for X.
+  int kIndex = 0;
+  if (numK % 2) {
+    float expArg = PIx2 * (ck[0].Kx * sX + ck[0].Ky * sY + ck[0].Kz * sZ);
+    sQr += ck[0].PhiMag * sycl::cos(expArg);
+    sQi += ck[0].PhiMag * sycl::sin(expArg);
+    kIndex++;
+    kGlobalIndex++;
+  }
+
+  for (; (kIndex < KERNEL_Q_K_ELEMS_PER_GRID) && (kGlobalIndex < numK);
+       kIndex += 2, kGlobalIndex += 2) {
+    float expArg = PIx2 * (ck[kIndex].Kx * sX +
+			   ck[kIndex].Ky * sY +
+			   ck[kIndex].Kz * sZ);
+    sQr += ck[kIndex].PhiMag * sycl::cos(expArg);
+    sQi += ck[kIndex].PhiMag * sycl::sin(expArg);
+
+    int kIndex1 = kIndex + 1;
+    float expArg1 = PIx2 * (ck[kIndex1].Kx * sX +
+			    ck[kIndex1].Ky * sY +
+			    ck[kIndex1].Kz * sZ);
+    sQr += ck[kIndex1].PhiMag * sycl::cos(expArg1);
+    sQi += ck[kIndex1].PhiMag * sycl::sin(expArg1);
+  }
+
+  Qr[xIndex] = sQr;
+  Qi[xIndex] = sQi;
+}
+
+void computePhiMag_GPU(
+  queue &q,
+  int numK,
+  buffer<float, 1> &phiR_d,
+  buffer<float, 1> &phiI_d,
+  buffer<float, 1> &phiMag_d)
+{
+  int phiMagBlocks = numK / KERNEL_PHI_MAG_THREADS_PER_BLOCK;
+  if (numK % KERNEL_PHI_MAG_THREADS_PER_BLOCK)
+    phiMagBlocks++;
+
+  range<1> lws (KERNEL_PHI_MAG_THREADS_PER_BLOCK);
+  range<1> gws (KERNEL_PHI_MAG_THREADS_PER_BLOCK * phiMagBlocks);
+
+  q.submit([&] (handler &cgh) {
+    auto r = phiR_d.get_access<sycl_read>(cgh);
+    auto i = phiI_d.get_access<sycl_read>(cgh);
+    auto m = phiMag_d.get_access<sycl_write>(cgh);
+    cgh.parallel_for<class compute_phi_mag>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+      ComputePhiMag_GPU (
+        item,
+        numK,
+        r.get_pointer(),
+        i.get_pointer(),
+        m.get_pointer());
+    });
+  });
+}
+
+void computeQ_GPU(
+  queue &q,
+  int numK, int numX,
+  buffer<float, 1> &x_d,
+  buffer<float, 1> &y_d,
+  buffer<float, 1> &z_d,
+  kValues          *kVals,
+  buffer<float, 1> &Qr_d,
+  buffer<float, 1> &Qi_d)
+{
+  int QGrids = numK / KERNEL_Q_K_ELEMS_PER_GRID;
+  if (numK % KERNEL_Q_K_ELEMS_PER_GRID) QGrids++;
+
+  int QBlocks = numX / KERNEL_Q_THREADS_PER_BLOCK;
+  if (numX % KERNEL_Q_THREADS_PER_BLOCK) QBlocks++;
+
+  range<1> lws (KERNEL_Q_THREADS_PER_BLOCK);
+  range<1> gws (KERNEL_Q_THREADS_PER_BLOCK * QBlocks);
+
+/* Values in the k-space coordinate system are stored in constant memory
+ * on the GPU */
+   buffer<kValues, 1> ck (KERNEL_Q_K_ELEMS_PER_GRID);
+
+  for (int QGrid = 0; QGrid < QGrids; QGrid++) {
+    // Put the tile of K values into constant mem
+    int QGridBase = QGrid * KERNEL_Q_K_ELEMS_PER_GRID;
+    kValues* kValsTile = kVals + QGridBase;
+    int numElems = MIN(KERNEL_Q_K_ELEMS_PER_GRID, numK - QGridBase);
+
+    q.submit([&] (handler &cgh) {
+      auto acc = ck.get_access<sycl_write>(cgh, range<1>(numElems));
+      cgh.copy(kValsTile, acc);
+    });
+
+    q.submit([&] (handler &cgh) {
+      auto k = ck.get_access<sycl_read, sycl_cmem>(cgh);
+      auto x = x_d.get_access<sycl_read>(cgh);
+      auto y = y_d.get_access<sycl_read>(cgh);
+      auto z = z_d.get_access<sycl_read>(cgh);
+      auto Qr = Qr_d.get_access<sycl_read_write>(cgh);
+      auto Qi = Qi_d.get_access<sycl_read_write>(cgh);
+      cgh.parallel_for<class compute_q>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+        ComputeQ_GPU (
+          item,
+          numK,
+          QGridBase, 
+          k.get_pointer(),
+          x.get_pointer(),
+          y.get_pointer(),
+          z.get_pointer(),
+          Qr.get_pointer(),
+          Qi.get_pointer());
+      });
+    });
+  }
+  q.wait();
+}
+
+void createDataStructsCPU(int numK, int numX, float** phiMag,
+	 float** Qr, float** Qi)
+{
+  *phiMag = (float* ) memalign(16, numK * sizeof(float));
+  *Qr = (float*) memalign(16, numX * sizeof (float));
+  *Qi = (float*) memalign(16, numX * sizeof (float));
+}
+
---
+++ mriQ-sycl/main.cpp
@@ -0,0 +1,134 @@
+/***************************************************************************
+ *
+ *            (C) Copyright 2007 The Board of Trustees of the
+ *                        University of Illinois
+ *                         All Rights Reserved
+ *
+ ***************************************************************************/
+
+/* 
+ * C code for creating the Q data structure for fast convolution-based 
+ * Hessian multiplication for arbitrary k-space trajectories.
+ *
+ * Inputs:
+ * kx - VECTOR of kx values, same length as ky and kz
+ * ky - VECTOR of ky values, same length as kx and kz
+ * kz - VECTOR of kz values, same length as kx and ky
+ * x  - VECTOR of x values, same length as y and z
+ * y  - VECTOR of y values, same length as x and z
+ * z  - VECTOR of z values, same length as x and y
+ * phi - VECTOR of the Fourier transform of the spatial basis 
+ *      function, evaluated at [kx, ky, kz].  Same length as kx, ky, and kz.
+ *
+ * recommended g++ options:
+ *  -O3 -lm -ffast-math -funroll-all-loops
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <malloc.h>
+#include "common.h"
+#include "file.h"
+#include "computeQ.cpp"
+
+long long get_time() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return (tv.tv_sec * 1000000) + tv.tv_usec;
+}
+
+int main (int argc, char *argv[]) {
+  char* inputFileName = argv[1];
+  char* outputFileName = argv[2];
+
+  int numX, numK;		/* Number of X and K values */
+  float *kx, *ky, *kz;		/* K trajectory (3D vectors) */
+  float *x, *y, *z;		/* X coordinates (3D vectors) */
+  float *phiR, *phiI;		/* Phi values (complex) */
+  float *phiMag;		/* Magnitude of Phi */
+  float *Qr, *Qi;		/* Q signal (complex) */
+
+  struct kValues* kVals;
+
+  /* Read in data */
+  inputData(inputFileName,
+	    &numK, &numX,
+	    &kx, &ky, &kz,
+	    &x, &y, &z,
+	    &phiR, &phiI);
+
+  printf("%d pixels in output; %d samples in trajectory\n", numX, numK);
+
+  /* Create CPU data structures */
+  createDataStructsCPU(numK, numX, &phiMag, &Qr, &Qi);
+
+#ifdef USE_GPU
+  gpu_selector dev_sel;
+#else
+  cpu_selector dev_sel;
+#endif
+
+  queue q(dev_sel);
+
+  /* GPU section 1 (precompute PhiMag) */
+  {
+    /* Mirror several data structures on the device */
+    buffer<float, 1> phiR_d (phiR, numK);
+    buffer<float, 1> phiI_d (phiI, numK);
+    buffer<float, 1> phiMag_d (phiMag, numK);
+
+    computePhiMag_GPU(q, numK, phiR_d, phiI_d, phiMag_d);
+  }
+
+  kVals = (struct kValues*)calloc(numK, sizeof (struct kValues));
+  for (int k = 0; k < numK; k++) {
+    kVals[k].Kx = kx[k];
+    kVals[k].Ky = ky[k];
+    kVals[k].Kz = kz[k];
+    kVals[k].PhiMag = phiMag[k];
+  }
+
+  /* GPU section 2 */
+  {
+    auto start = get_time();
+
+    buffer<float, 1> x_d (x, numX);
+    buffer<float, 1> y_d (y, numX);
+    buffer<float, 1> z_d (z, numX);
+    buffer<float, 1> Qr_d (Qr, numX);
+    buffer<float, 1> Qi_d (Qi, numX);
+
+    q.submit([&] (handler &cgh) {
+      auto acc = Qr_d.get_access<sycl_discard_write>(cgh);
+      cgh.fill(acc, 0.f);
+    });
+
+    q.submit([&] (handler &cgh) {
+      auto acc = Qi_d.get_access<sycl_discard_write>(cgh);
+      cgh.fill(acc, 0.f);
+    });
+
+    computeQ_GPU(q, numK, numX, x_d, y_d, z_d, kVals, Qr_d, Qi_d);
+
+    auto end = get_time();
+    printf("computeQ execution time: %.2f s\n", (end - start) / 1e6f);
+  }
+
+  outputData(outputFileName, Qr, Qi, numX);
+
+  free(phiMag);
+  free (kx);
+  free (ky);
+  free (kz);
+  free (x);
+  free (y);
+  free (z);
+  free (phiR);
+  free (phiI);
+  free (kVals);
+  free (Qr);
+  free (Qi);
+  return 0;
+}
