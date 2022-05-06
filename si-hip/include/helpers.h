7755b669a6f9d2265b6fe9895a052827ac9996f1
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2022-05-06 07:56:28 -0400

    add the si example in hip

---
+++ si-hip/include/helpers.h
@@ -0,0 +1,16 @@
+#ifndef HELPERS_CUH
+#define HELPERS_CUH
+
+#pragma once
+
+#define errorCheck(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(hipError_t code, const char *file, int line, bool abort=true)
+{
+    if (code != hipSuccess)
+    {
+        fprintf(stderr,"GPUassert: %s %s %d\n", hipGetErrorString(code), file, line);
+        if (abort) exit(code);
+    }
+}
+
+#endif //HELPERS_CUH
