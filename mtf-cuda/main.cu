31c038175848b16cbb9c76263426edf45232b903
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-05-14 22:08:11 -0400

    add the thrust-based mtf in cuda

---
+++ mtf-cuda/main.cu
@@ -0,0 +1,86 @@
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/device_ptr.h>
+#include <thrust/copy.h>
+#include <thrust/sequence.h>
+#include <thrust/find.h>
+
+#include <chrono>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+void mtf(vector<char> word, bool output)
+{
+  //Parallel initialisation of character set  
+  thrust::device_vector<char> d_list(256);
+  thrust::sequence(d_list.begin(), d_list.end());
+  thrust::host_vector<char> list(256);
+  thrust::device_vector<char> d_word(word.size());
+
+  size_t counter;
+  thrust::device_vector<char>::iterator iter, count;
+  thrust::host_vector<char> h_word(word.size());
+  h_word = word;
+  d_word = h_word;
+
+  for (counter = 0; counter < word.size(); counter++)
+  {
+    thrust::copy(list.begin(), list.end(), d_list.begin());
+
+    h_word[0] = d_word[counter];
+    iter = thrust::find(d_list.begin(), d_list.end(), d_word[counter]);
+
+    if (d_list[0] != h_word[0])
+    {
+      thrust::copy(d_list.begin(), iter, list.begin()+1);
+      list[0] = h_word[0];
+    }
+  }
+
+  thrust::copy(list.begin(), list.end(), d_list.begin());
+  thrust::copy(word.begin(), word.end(), d_word.begin());
+  for (counter = 0; counter < list.size(); counter++)
+  {
+    iter = thrust::find(d_word.begin(), d_word.end(), d_list[counter]);
+    while (iter != d_word.end())
+    {
+      *iter = counter;
+      iter = thrust::find(d_word.begin(), d_word.end(), d_list[counter]);
+    }
+  }
+  thrust::copy(d_word.begin(), d_word.end(), h_word.begin());
+
+  if (output) {
+    for (counter = 0; counter < word.size(); counter++)
+      printf("%d ", h_word[counter]);
+    printf("\n");
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    printf("Usage: %s <string_input>\n", argv[0]);
+    exit(1);
+  }
+
+  int len = strlen(argv[1]);
+  vector<char> word(argv[1], argv[1] + len);
+
+  // output MTF result
+  mtf(word, true);
+
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < 100; i++)
+    mtf(word, false);
+
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<float> time = end - start;
+  std::cout << "Total execution time: " << time.count() << std::endl;
+  return 0;
+}
