06707fffc8f29a1846b9636103dea07e02d78681
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2022-05-16 12:33:03 -0400

    add the depixel examples and their reference

---
+++ depixel-cuda/main.cu
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <random>
+#include <cuda.h>
+#include "kernels.h"
+
+#define nthreads 256
+
+int main(int argc, char** argv) {
+
+  if (argc != 4) {
+    printf("Usage: %s <width> <height> <repeat>\n", argv[0]);
+    return 1;
+  }
+
+  int width = atoi(argv[1]);
+  int height = atoi(argv[2]);
+  int repeat = atoi(argv[3]);
+
+  size_t size = width * height;
+  size_t size_output_bytes = size * sizeof(uint);
+  size_t size_image_bytes = size * sizeof(float3);
+
+  std::mt19937 gen(19937);
+  // reduce the upper bound can increase the kernel execution time of eliminate_crosses
+  std::uniform_real_distribution<float> dis(0.f, 0.4f); 
+
+  float3 *h_img = (float3*) malloc(size_image_bytes);
+
+  uint *h_out = (uint*) malloc(size_output_bytes);
+
+  float3 *d_img;
+  cudaMalloc((void**)&d_img, size_image_bytes);
+
+  uint *d_tmp, *d_out;
+  cudaMalloc((void**)&d_tmp, size_output_bytes);
+  cudaMalloc((void**)&d_out, size_output_bytes);
+
+  // assume that size is a multiple of nthreads
+  dim3 grids (size / nthreads); 
+  dim3 blocks (nthreads);
+
+  float sum = 0;
+  float total_time = 0;
+
+  for (int n = 0; n < repeat; n++) {
+
+    for (size_t i = 0; i < size; i++) {
+      h_img[i].x = dis(gen);
+      h_img[i].y = dis(gen);
+      h_img[i].z = dis(gen);
+    }
+
+    auto start = std::chrono::steady_clock::now();
+
+    cudaMemcpy(d_img, h_img, size_image_bytes, cudaMemcpyHostToDevice);
+    check_connect<<<grids, blocks>>>(d_img, d_tmp, width, height);
+    eliminate_crosses<<<grids, blocks>>>(d_tmp, d_out, width, height);
+    cudaMemcpy(h_out, d_out, size_output_bytes, cudaMemcpyDeviceToHost);
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<float> time = end - start;
+    total_time += time.count();
+
+    float lsum = 0;
+    for (size_t i = 0; i < size; i++)
+      lsum += (h_out[i] & 0xff) / 256.f + 
+             ((h_out[i] >> 8) & 0xff) / 256.f + 
+             ((h_out[i] >> 16) & 0xff) / 256.f + 
+             ((h_out[i] >> 24) & 0xff) / 256.f;
+
+    sum += lsum / size;
+  }
+
+  printf("Image size: %d (width) x %d (height)\ncheckSum: %f\n",
+         width, height, sum);
+  printf("Average device time over %d iterations: %f (s)\n",
+         repeat, total_time / repeat);
+
+  cudaFree(d_out);
+  cudaFree(d_img);
+  cudaFree(d_tmp);
+  free(h_out);
+  free(h_img);
+
+  return 0;
+}
---
+++ depixel-omp/main.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <chrono>
+#include <random>
+#include <omp.h>
+#include "kernels.h"
+
+int main(int argc, char** argv) {
+
+  if (argc != 4) {
+    printf("Usage: %s <width> <height> <repeat>\n", argv[0]);
+    return 1;
+  }
+
+  int width = atoi(argv[1]);
+  int height = atoi(argv[2]);
+  int repeat = atoi(argv[3]);
+
+  size_t size = width * height;
+  size_t size_output_bytes = size * sizeof(uint);
+  size_t size_image_bytes = size * sizeof(float3);
+
+  std::mt19937 gen(19937);
+  // reduce the upper bound can increase the kernel execution time of eliminate_crosses
+  std::uniform_real_distribution<float> dis(0.f, 0.4f); 
+
+  float3 *img = (float3*) malloc(size_image_bytes);
+
+  uint *out = (uint*) malloc(size_output_bytes);
+  uint *tmp = (uint*) malloc(size_output_bytes);
+
+  float sum = 0;
+  float total_time = 0;
+
+  #pragma omp target data map (alloc: img[0:size], tmp[0:size]) \
+                          map (from: out[0:size])
+  {
+    for (int n = 0; n < repeat; n++) {
+
+      for (size_t i = 0; i < size; i++) {
+        img[i].x = dis(gen);
+        img[i].y = dis(gen);
+        img[i].z = dis(gen);
+      }
+
+      auto start = std::chrono::steady_clock::now();
+
+      #pragma omp target update to (img[0:size])
+      check_connect(img, tmp, width, height);
+      eliminate_crosses(tmp, out, width, height);
+      #pragma omp target update from (out[0:size])
+
+      auto end = std::chrono::steady_clock::now();
+      std::chrono::duration<float> time = end - start;
+      total_time += time.count();
+
+      float lsum = 0;
+      for (size_t i = 0; i < size; i++)
+        lsum += (out[i] & 0xff) / 256.f + 
+               ((out[i] >> 8) & 0xff) / 256.f + 
+               ((out[i] >> 16) & 0xff) / 256.f + 
+               ((out[i] >> 24) & 0xff) / 256.f;
+
+      sum += lsum / size;
+    }
+  }
+
+  printf("Image size: %d (width) x %d (height)\ncheckSum: %f\n",
+         width, height, sum);
+  printf("Average device time over %d iterations: %f (s)\n",
+         repeat, total_time / repeat);
+
+  free(out);
+  free(img);
+  free(tmp);
+
+  return 0;
+}
