984f0dc7208bec84bc43fbb38d21c56178df7100
Jin Z <5zj@leconte.ftpn.ornl.gov>
2022-05-17 18:58:17 -0400

    update the affine example in omp

--- affine-omp/main.cpp
+++ affine-omp/main.cpp
@@ -35,105 +35,15 @@
 #include <math.h>
 #include <cstring>
 #include <cmath>
+#include <chrono>
 #include <iostream>
-
-#define X_SIZE 512
-#define Y_SIZE 512
-#define PI     3.14159265359f
-#define WHITE  (short)(1)
-#define BLACK  (short)(0)
-
-// reference implementation for verification
-void affine_reference(const unsigned short *src, unsigned short *dst) 
-{
-  for (int y = 0; y < 512; y++) {
-    for (int x = 0; x < 512; x++) {
-      const float    lx_rot   = 30.0f;
-      const float    ly_rot   = 0.0f; 
-      const float    lx_expan = 0.5f;
-      const float    ly_expan = 0.5f; 
-      int      lx_move  = 0;
-      int      ly_move  = 0;
-      float    affine[2][2];   // coefficients
-      float    i_affine[2][2];
-      float    beta[2];
-      float    i_beta[2];
-      float    det;
-      float    x_new, y_new;
-      float    x_frac, y_frac;
-      float    gray_new;
-      int      m, n;
-
-      unsigned short    output_buffer[X_SIZE];
-
-
-      // forward affine transformation 
-      affine[0][0] = lx_expan * std::cos((float)(lx_rot*PI/180.0f));
-      affine[0][1] = ly_expan * std::sin((float)(ly_rot*PI/180.0f));
-      affine[1][0] = lx_expan * std::sin((float)(lx_rot*PI/180.0f));
-      affine[1][1] = ly_expan * std::cos((float)(ly_rot*PI/180.0f));
-      beta[0]      = lx_move;
-      beta[1]      = ly_move;
-
-      // determination of inverse affine transformation
-      det = (affine[0][0] * affine[1][1]) - (affine[0][1] * affine[1][0]);
-      if (det == 0.0f)
-      {
-        i_affine[0][0]   = 1.0f;
-        i_affine[0][1]   = 0.0f;
-        i_affine[1][0]   = 0.0f;
-        i_affine[1][1]   = 1.0f;
-        i_beta[0]        = -beta[0];
-        i_beta[1]        = -beta[1];
-      } 
-      else 
-      {
-        i_affine[0][0]   =  affine[1][1]/det;
-        i_affine[0][1]   = -affine[0][1]/det;
-        i_affine[1][0]   = -affine[1][0]/det;
-        i_affine[1][1]   =  affine[0][0]/det;
-        i_beta[0]        = -i_affine[0][0]*beta[0]-i_affine[0][1]*beta[1];
-        i_beta[1]        = -i_affine[1][0]*beta[0]-i_affine[1][1]*beta[1];
-      }
-
-      // Output image generation by inverse affine transformation and bilinear transformation
-
-      x_new    = i_beta[0] + i_affine[0][0]*(x-X_SIZE/2.0f) + i_affine[0][1]*(y-Y_SIZE/2.0f) + X_SIZE/2.0f;
-      y_new    = i_beta[1] + i_affine[1][0]*(x-X_SIZE/2.0f) + i_affine[1][1]*(y-Y_SIZE/2.0f) + Y_SIZE/2.0f;
-
-      m        = (int)std::floor(x_new);
-      n        = (int)std::floor(y_new);
-
-      x_frac   = x_new - m;
-      y_frac   = y_new - n;
-
-      if ((m >= 0) && (m + 1 < X_SIZE) && (n >= 0) && (n+1 < Y_SIZE))
-      {
-        gray_new = (1.0f - y_frac) * ((1.0f - x_frac) * (src[(n * X_SIZE) + m])       + x_frac * (src[(n * X_SIZE) + m + 1])) + 
-          y_frac  * ((1.0f - x_frac) * (src[((n + 1) * X_SIZE) + m]) + x_frac * (src[((n + 1) * X_SIZE) + m + 1]));
-
-        output_buffer[x] = (unsigned short)gray_new;
-      } 
-      else if (((m + 1 == X_SIZE) && (n >= 0) && (n < Y_SIZE)) || ((n + 1 == Y_SIZE) && (m >= 0) && (m < X_SIZE))) 
-      {
-        output_buffer[x] = src[(n * X_SIZE) + m];
-      } 
-      else 
-      {
-        output_buffer[x] = WHITE;
-      }
-
-      dst[(y * X_SIZE)+x] = output_buffer[x];
-    }
-  }
-}
+#include "reference.h"
 
 int main(int argc, char** argv)
 {
-
-  if (argc != 3)
+  if (argc != 4)
   {
-    printf("Usage: %s <input image> <output image>\n", argv[0]) ;
+    printf("Usage: %s <input image> <output image> <iterations>\n", argv[0]) ;
     return -1 ;
   }
 
@@ -157,39 +67,41 @@ int main(int argc, char** argv)
   printf("   Reading RAW Image\n");
   size_t items_read = fread(input_image, sizeof(input_image), 1, input_file);
   printf("   Bytes read = %d\n\n", (int)(items_read * sizeof(input_image)));
+  fclose(input_file);
 
+  const int iterations = atoi(argv[3]);
 
   #pragma omp target data map(to: input_image[0:X_SIZE*Y_SIZE]) map(from:output_image[0:X_SIZE*Y_SIZE])
   {
-    for (int i = 0; i < 100; i++) {
+    auto start = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < iterations; i++) {
       #pragma omp target teams distribute parallel for collapse(2) thread_limit(256)
       for (int y = 0; y < Y_SIZE; y++)
         for (int x = 0; x < X_SIZE; x++) {
 
-          const float    lx_rot   = 30.0f;
-          const float    ly_rot   = 0.0f; 
-          const float    lx_expan = 0.5f;
-          const float    ly_expan = 0.5f; 
-          int      lx_move  = 0;
-          int      ly_move  = 0;
-          float    affine[2][2];   // coefficients
-          float    i_affine[2][2];
-          float    beta[2];
-          float    i_beta[2];
-          float    det;
-          float    x_new, y_new;
-          float    x_frac, y_frac;
-          float    gray_new;
-          int      m, n;
-
-          unsigned short    output_buffer[X_SIZE];
-
+          const float lx_rot   = 30.0f;
+          const float ly_rot   = 0.0f; 
+          const float lx_expan = 0.5f;
+          const float ly_expan = 0.5f; 
+          int   lx_move  = 0;
+          int   ly_move  = 0;
+          float affine[2][2];   // coefficients
+          float i_affine[2][2];
+          float beta[2];
+          float i_beta[2];
+          float det;
+          float x_new, y_new;
+          float x_frac, y_frac;
+          float gray_new;
+          int   m, n;
+          unsigned short output_buffer;
 
           // forward affine transformation 
-          affine[0][0] = lx_expan * cosf((float)(lx_rot*PI/180.0f));
-          affine[0][1] = ly_expan * sinf((float)(ly_rot*PI/180.0f));
-          affine[1][0] = lx_expan * sinf((float)(lx_rot*PI/180.0f));
-          affine[1][1] = ly_expan * cosf((float)(ly_rot*PI/180.0f));
+          affine[0][0] = lx_expan * cosf(lx_rot*PI/180.0f);
+          affine[0][1] = ly_expan * sinf(ly_rot*PI/180.0f);
+          affine[1][0] = lx_expan * sinf(lx_rot*PI/180.0f);
+          affine[1][1] = ly_expan * cosf(ly_rot*PI/180.0f);
           beta[0]      = lx_move;
           beta[1]      = ly_move;
 
@@ -197,33 +109,33 @@ int main(int argc, char** argv)
           det = (affine[0][0] * affine[1][1]) - (affine[0][1] * affine[1][0]);
           if (det == 0.0f)
           {
-            i_affine[0][0]   = 1.0f;
-            i_affine[0][1]   = 0.0f;
-            i_affine[1][0]   = 0.0f;
-            i_affine[1][1]   = 1.0f;
-            i_beta[0]        = -beta[0];
-            i_beta[1]        = -beta[1];
+            i_affine[0][0] = 1.0f;
+            i_affine[0][1] = 0.0f;
+            i_affine[1][0] = 0.0f;
+            i_affine[1][1] = 1.0f;
+            i_beta[0]      = -beta[0];
+            i_beta[1]      = -beta[1];
           } 
           else 
           {
-            i_affine[0][0]   =  affine[1][1]/det;
-            i_affine[0][1]   = -affine[0][1]/det;
-            i_affine[1][0]   = -affine[1][0]/det;
-            i_affine[1][1]   =  affine[0][0]/det;
-            i_beta[0]        = -i_affine[0][0]*beta[0]-i_affine[0][1]*beta[1];
-            i_beta[1]        = -i_affine[1][0]*beta[0]-i_affine[1][1]*beta[1];
+            i_affine[0][0] =  affine[1][1]/det;
+            i_affine[0][1] = -affine[0][1]/det;
+            i_affine[1][0] = -affine[1][0]/det;
+            i_affine[1][1] =  affine[0][0]/det;
+            i_beta[0]      = -i_affine[0][0]*beta[0]-i_affine[0][1]*beta[1];
+            i_beta[1]      = -i_affine[1][0]*beta[0]-i_affine[1][1]*beta[1];
           }
 
           // Output image generation by inverse affine transformation and bilinear transformation
 
-          x_new    = i_beta[0] + i_affine[0][0]*(x-X_SIZE/2.0f) + i_affine[0][1]*(y-Y_SIZE/2.0f) + X_SIZE/2.0f;
-          y_new    = i_beta[1] + i_affine[1][0]*(x-X_SIZE/2.0f) + i_affine[1][1]*(y-Y_SIZE/2.0f) + Y_SIZE/2.0f;
+          x_new  = i_beta[0] + i_affine[0][0]*(x-X_SIZE/2.0f) + i_affine[0][1]*(y-Y_SIZE/2.0f) + X_SIZE/2.0f;
+          y_new  = i_beta[1] + i_affine[1][0]*(x-X_SIZE/2.0f) + i_affine[1][1]*(y-Y_SIZE/2.0f) + Y_SIZE/2.0f;
 
-          m        = (int)floorf(x_new);
-          n        = (int)floorf(y_new);
+          m      = (int)floorf(x_new);
+          n      = (int)floorf(y_new);
 
-          x_frac   = x_new - m;
-          y_frac   = y_new - n;
+          x_frac = x_new - m;
+          y_frac = y_new - n;
 
           if ((m >= 0) && (m + 1 < X_SIZE) && (n >= 0) && (n+1 < Y_SIZE))
           {
@@ -232,20 +144,23 @@ int main(int argc, char** argv)
               y_frac  * ((1.0f - x_frac) * (input_image[((n + 1) * X_SIZE) + m]) + 
                   x_frac * (input_image[((n + 1) * X_SIZE) + m + 1]));
 
-            output_buffer[x] = (unsigned short)gray_new;
+            output_buffer = (unsigned short)gray_new;
           } 
           else if (((m + 1 == X_SIZE) && (n >= 0) && (n < Y_SIZE)) || ((n + 1 == Y_SIZE) && (m >= 0) && (m < X_SIZE))) 
           {
-            output_buffer[x] = input_image[(n * X_SIZE) + m];
+            output_buffer = input_image[(n * X_SIZE) + m];
           } 
           else 
           {
-            output_buffer[x] = WHITE;
+            output_buffer = WHITE;
           }
 
-          output_image[(y * X_SIZE)+x] = output_buffer[x];
+          output_image[(y * X_SIZE)+x] = output_buffer;
         }
     }
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    std::cout << "   Average kernel execution time " << (time * 1e-9f) / iterations << " (s)\n";
   }
 
   // verify
@@ -258,7 +173,6 @@ int main(int argc, char** argv)
   }
   printf("   Max output error is %d\n\n", max_error);
 
-
   printf("   Writing RAW Image\n");
   const char *outputImageFilename = argv[2];
   FILE *output_file = fopen(outputImageFilename, "wb");
@@ -269,5 +183,7 @@ int main(int argc, char** argv)
   }
   size_t items_written = fwrite(output_image, sizeof(output_image), 1, output_file);
   printf("   Bytes written = %d\n\n", (int)(items_written * sizeof(output_image)));
+  fclose(output_file);
+
   return 0 ;
 }
