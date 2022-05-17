de93f0968cc31ca58563b55036571aa77253cfd6
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-05-17 09:19:51 -0400

    add the suffix array examples in cuda and hip

---
+++ sa-cuda/kernels.h
@@ -0,0 +1,196 @@
+//  dc3 algorithm on GPU
+//
+//  Created by gangliao on 12/22/14.
+//  Copyright (c) 2014 gangliao. All rights reserved.
+
+__device__ int leq(int a1, int a2, int b1, int b2) {
+  return (a1 < b1 || (a1 == b1 && a2 <= b2));
+}
+
+__device__ int leq2(int a1, int a2, int a3, int b1, int b2, int b3) {
+  return (a1 < b1 || (a1 == b1 && leq(a2, a3, b2, b3)));
+}
+
+__global__ void Init_d_s12(int* s12, int n)
+{
+  int index = blockIdx.x*blockDim.x + threadIdx.x;
+  if (index >= n) return;
+  s12[index] = index + index / 2 + 1;
+}
+
+//d_SA12, d_s12, n02
+__global__
+void keybits(      int*__restrict__ SA12,
+             const int*__restrict__ s12,
+             const int*__restrict__ s,
+             int n, int i)
+{
+  int index = blockIdx.x*blockDim.x + threadIdx.x;
+  if (index >= n) return;
+  SA12[index] = s[s12[index] + i];
+}
+
+__global__
+void InitScan(const int*__restrict__ s,
+              const int*__restrict__ SA12,
+                    int*__restrict__ scan,
+              int n)
+{
+  int index = blockIdx.x*blockDim.x + threadIdx.x;
+  if (index >= n) return;
+  if ((s[SA12[index]] == s[SA12[index + 1]]) && 
+      (s[SA12[index] + 1] == s[SA12[index + 1] + 1]) &&
+      (s[SA12[index] + 2] == s[SA12[index + 1] + 2]))
+  {
+    scan[index] = 0;
+  }
+  else
+    scan[index] = 1;
+}
+
+__global__
+void Set_suffix_rank(      int*__restrict__ s12,
+                     const int*__restrict__ SA12,
+                     const int*__restrict__ scan,
+                     int n02, int n0)
+{
+  int index = blockIdx.x*blockDim.x + threadIdx.x;
+  if (index >= n02) return;
+  s12[SA12[index] / 3 + ((SA12[index] % 3) - 1) * n0] = scan[index] + 1;
+}
+
+__global__
+void Store_unique_ranks(      int*__restrict__ s12,
+                        const int*__restrict__ SA12,
+                        int n)
+{
+  int index = blockIdx.x*blockDim.x + threadIdx.x;
+  if (index >= n) return;
+  s12[SA12[index]] = index + 1;
+}
+
+__global__
+void Compute_SA_From_UniqueRank(const int*__restrict__ s12,
+                                      int*__restrict__ SA12,
+                                int n)
+{
+  int index = blockIdx.x*blockDim.x + threadIdx.x;
+  if (index >= n) return;
+  SA12[s12[index] - 1] = index;
+}
+
+__global__
+void InitScan2(int* SA12, int* scan, int n0, int n02)
+{
+  int index = blockIdx.x*blockDim.x + threadIdx.x;
+  if (index >= n02)
+    return;
+  if (SA12[index] < n0)
+    scan[index] = 1;
+  else
+    scan[index] = 0;
+}
+
+__global__
+void Set_S0(      int*__restrict__ s0,
+            const int*__restrict__ SA12,
+            const int*__restrict__ scan,
+            int n0, int n02)
+{
+  int index = blockIdx.x*blockDim.x + threadIdx.x;
+  if (index >= n02) return;
+  if (SA12[index] < n0)
+    s0[scan[index]] = 3 * SA12[index];
+}
+
+__global__
+void merge_suffixes(const int*__restrict__ SA0,
+                    const int*__restrict__ SA12,
+                          int*__restrict__ SA,
+                    const int*__restrict__ s,
+                    const int*__restrict__ s12,
+                    int n0, int n02, int n)
+{
+  int index = blockIdx.x*blockDim.x + threadIdx.x;
+  int left, right, mid;
+  int flag = 0;
+  if (index >= n0 + n02) return;
+
+  if (n != n0 + n02)
+  {
+    flag = 1;
+    if (index == n0) return;  
+  }
+
+  int i, j;
+  if (index < n0)
+  {
+    i = SA0[index];
+    left = n0;
+    right = n0 + n02;
+
+    while (left < right)
+    {
+      mid = (left + right) / 2;
+      if (SA12[mid - n0] < n0)
+      {
+        j = SA12[mid - n0] * 3 + 1;
+
+        if (leq(s[j], s12[(j + 1) / 3 + ((j + 1) % 3 - 1)*n0],
+                s[i], s12[i / 3]))
+          left = mid + 1;
+        else
+          right = mid;
+      }
+      else
+      {
+        j = (SA12[mid - n0] - n0) * 3 + 2;
+
+        if (leq2(s[j], s[j + 1], s12[(j + 2) / 3 + ((j + 2) % 3 - 1)*n0],
+                 s[i], s[i + 1], s12[i / 3 + n0]))
+          left = mid + 1;
+        else
+          right = mid;
+      }
+
+    }
+    SA[index + left - n0 - flag] = i;
+  }
+  else
+  {
+    if (SA12[index - n0] < n0)
+    {
+      i = SA12[index - n0] * 3 + 1;
+      left = 0;
+      right = n0;
+      while (left < right)
+      {
+        mid = (left + right) / 2;
+
+        if (leq(s[SA0[mid]], s12[SA0[mid] / 3], s[i],
+                s12[(i + 1) / 3 + ((i + 1) % 3 - 1)*n0]))
+          left = mid + 1;
+        else
+          right = mid;
+      }
+      SA[index - n0 + left - flag] = i;
+    }
+    else
+    {
+      i = (SA12[index - n0] - n0) * 3 + 2;
+      left = 0;
+      right = n0;
+      while (left < right)
+      {
+        mid = (left + right) / 2;
+
+        if (leq2(s[SA0[mid]], s[SA0[mid] + 1], s12[SA0[mid] / 3 + n0],
+                 s[i], s[i + 1], s12[(i + 2) / 3 + ((i + 2) % 3 - 1)*n0]))
+          left = mid + 1;
+        else
+          right = mid;
+      }
+      SA[index - n0 + left - flag] = i;
+    }
+  }
+}
---
+++ sa-hip/main.cu
@@ -0,0 +1,201 @@
+//  Created by gangliao on 12/22/14.
+//  Copyright (c) 2014 gangliao. All rights reserved.
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <hip/hip_runtime.h>
+#include <chrono>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/sort.h>
+#include <thrust/copy.h>
+#include <thrust/scan.h>
+#include "kernels.h"
+
+#define MAX_ALPHA 26
+
+// number of threads in a block
+#define BLOCK_SIZE 256
+
+void suffixArray(thrust::device_vector<int>&, thrust::device_vector<int>&, int, int);
+
+#ifdef DEBUG
+void print_suffix(const char *cc, int i)
+{
+  printf("%d: ", i);
+  for (unsigned j = i; j < strlen(cc); j++)
+    printf("%c", cc[j]);
+  printf("\n");
+}
+#endif
+
+bool read_data(char *filename, char *buffer, int num) {
+  FILE *fh;
+  fh = fopen(filename, "r");
+  if (fh == NULL) {
+    printf("Failed to open file %s\n", filename);
+    return true;
+  }
+  fread(buffer, 1, num, fh);
+  buffer[num] = '\0';
+  fclose(fh);
+  return false;
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 4) {
+    printf("Usage: %s <dataset> <dataset size> <repeat>\n", argv[0]);
+    printf("size of dataset to evaluate (10 - 1000000)\n");
+    return 1;
+  }
+
+  char* filename = argv[1];
+
+#ifdef DEBUG
+  const int n = 100;
+#else
+  const int n = atoi(argv[2]);
+#endif
+
+  const int repeat = atoi(argv[3]);
+
+  //load data buffer with a genome file
+  char *data = (char *) malloc ((n + 1)*sizeof(char));
+  bool fail = read_data(filename, data, n);
+  if (data == NULL || fail) {
+    if (data) free(data);
+    return 1;
+  }
+
+  thrust::host_vector<int> h_inp (n + 3);
+  thrust::host_vector<int> h_SA (n + 3, 0);
+  thrust::device_vector<int> d_inp;
+  thrust::device_vector<int> d_SA;
+
+  int i;
+  for (i = 0; i < n; i++) h_inp[i] = (int)data[i];
+  h_inp[i] = 0; h_inp[i + 1] = 0; h_inp[i + 2] = 0; //prepare for triples
+
+  auto start = std::chrono::steady_clock::now();
+
+  for (i = 0; i < repeat; i++) {
+    d_inp = h_inp;
+    d_SA = h_SA;
+
+    suffixArray(d_inp, d_SA, n, MAX_ALPHA);
+
+    h_SA = d_SA;
+  }
+
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<float> time = end - start;
+  printf("Average suffix array construct time (input size = %d): %f (s)\n", 
+         n, time.count() / repeat);
+
+  long sum = 0;
+  for (i = 0; i < n/2; i++)
+    sum += (h_SA[2*i] + h_SA[2*i+1]) / abs(h_SA[2*i] - h_SA[2*i+1]);
+  if (n % 2) sum -= h_SA[n-1];
+  printf("checksum = 0x%lx\n", sum);
+  
+#ifdef DEBUG
+  //peek sorted suffixes from data set
+  for(i = 0; i < 8; i++) {
+    printf("No.%d Index.", i); 
+    print_suffix(data, h_SA[i]);
+  }
+
+  printf("...\n...\n");
+
+  for(i = n-8; i < n; i++) {
+    printf("No.%d Index.", i); 
+    print_suffix(data, h_SA[i]);
+  }
+#endif
+
+  free(data);
+  return 0;
+}
+
+void suffixArray(thrust::device_vector<int>& s,
+                 thrust::device_vector<int>& SA,
+                 int n, int K)
+{
+  int n0 = (n + 2) / 3,
+      n2 = n / 3,
+      n02 = n0 + n2;
+
+  thrust::device_vector<int> d_s12 (n02 + 3, 0);
+  int *pd_s12 = thrust::raw_pointer_cast(&d_s12[0]);
+
+  thrust::device_vector<int> d_SA12 (n02 + 3, 0);
+  int *pd_SA12 = thrust::raw_pointer_cast(&d_SA12[0]);
+
+  thrust::device_vector<int> d_s0 (n0, 0);
+  int *pd_s0 = thrust::raw_pointer_cast(&d_s0[0]);
+
+  thrust::device_vector<int> d_SA0 (n0, 0);
+  int *pd_SA0 = thrust::raw_pointer_cast(&d_SA0[0]);
+
+  thrust::device_vector<int> d_scan (n02 + 3);
+  int *pd_scan = thrust::raw_pointer_cast(&d_scan[0]);
+
+  int *pd_s = thrust::raw_pointer_cast(&s[0]);
+  int *pd_SA = thrust::raw_pointer_cast(&SA[0]);
+
+  dim3 numThreads(BLOCK_SIZE);
+  dim3 numBlocks((n02 - 1) / BLOCK_SIZE + 1);
+
+  // S12 initialization:
+  hipLaunchKernelGGL(Init_d_s12, numBlocks, numThreads, 0, 0, pd_s12, n02);
+
+  // radix sort - using SA12 to store keys
+  hipLaunchKernelGGL(keybits, numBlocks, numThreads, 0, 0, pd_SA12, pd_s12, pd_s, n02, 2);
+
+  thrust::sort_by_key(d_SA12.begin(), d_SA12.begin() + n02, d_s12.begin());
+
+  hipLaunchKernelGGL(keybits, numBlocks, numThreads, 0, 0, pd_SA12, pd_s12, pd_s, n02, 1);
+  thrust::sort_by_key(d_SA12.begin(), d_SA12.begin() + n02, d_s12.begin());
+
+  hipLaunchKernelGGL(keybits, numBlocks, numThreads, 0, 0, pd_SA12, pd_s12, pd_s, n02, 0);
+  thrust::sort_by_key(d_SA12.begin(), d_SA12.begin() + n02, d_s12.begin());
+
+  d_SA12 = d_s12;
+
+  // stably sort the mod 0 suffixes from SA12 by their first character
+  // find lexicographic names of triples
+
+  hipLaunchKernelGGL(InitScan, numBlocks, numThreads, 0, 0, pd_s, pd_SA12, pd_scan, n02);
+
+  thrust::exclusive_scan(d_scan.begin(), d_scan.begin() + n02 + 1, d_scan.begin());
+
+  hipLaunchKernelGGL(Set_suffix_rank, numBlocks, numThreads, 0, 0, pd_s12, pd_SA12, pd_scan, n02, n0);
+
+  int max_rank = d_scan[n02];
+
+  // if max_rank is less than the size of s12, we have a repeat. repeat dc3.
+  // else generate the suffix array of s12 directly
+  if (max_rank < n02) {
+    suffixArray(d_s12, d_SA12, n02, max_rank);
+    hipLaunchKernelGGL(Store_unique_ranks, numBlocks, numThreads, 0, 0, pd_s12, pd_SA12, n02);
+  }
+  else {
+    hipLaunchKernelGGL(Compute_SA_From_UniqueRank, numBlocks, numThreads, 0, 0, pd_s12, pd_SA12, n02);
+  }
+
+  hipLaunchKernelGGL(InitScan2, numBlocks, numThreads, 0, 0, pd_SA12, pd_scan, n0, n02);
+  thrust::exclusive_scan(d_scan.begin(), d_scan.begin() + n02, d_scan.begin()); 
+  hipLaunchKernelGGL(Set_S0, numBlocks, numThreads, 0, 0, pd_s0, pd_SA12, pd_scan, n0, n02);
+
+  dim3 numBlocks2((n0 - 1) / BLOCK_SIZE + 1);
+  hipLaunchKernelGGL(keybits, numBlocks2, numThreads, 0, 0, pd_SA0, pd_s0, pd_s, n0, 0);
+  thrust::sort_by_key(d_SA0.begin(), d_SA0.begin() + n0, d_s0.begin());
+  d_SA0 = d_s0;
+
+  // merge sorted SA0 suffixes and sorted SA12 suffixes
+  dim3 numBlocks3((n0 + n02 - 1) / BLOCK_SIZE + 1);
+  hipLaunchKernelGGL(merge_suffixes, numBlocks3, numThreads, 0, 0, pd_SA0, pd_SA12, pd_SA, pd_s, pd_s12, n0, n02, n);
+}
