de7e9844b418289052ba9a604178bc4d67e04b5c
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-05-17 18:31:16 -0400

    eliminate the local memory usage of the affine kernel in CUDA; split the main source file for reuse

--- affine-cuda/main.cu
+++ affine-cuda/main.cu
@@ -35,195 +35,22 @@
 #include <cstring>
 #include <cmath>
 #include <iostream>
+#include <chrono>
 #include <cuda.h>
-
-#define X_SIZE 512
-#define Y_SIZE 512
-#define PI     3.14159265359f
-#define WHITE  (short)(1)
-#define BLACK  (short)(0)
-
-__global__ void affine (const unsigned short *src, unsigned short *dst) 
-{
-
-  int x = blockIdx.x*blockDim.x+threadIdx.x;
-  int y = blockIdx.y*blockDim.y+threadIdx.y;
-
-  const float    lx_rot   = 30.0f;
-  const float    ly_rot   = 0.0f; 
-  const float    lx_expan = 0.5f;
-  const float    ly_expan = 0.5f; 
-  int      lx_move  = 0;
-  int      ly_move  = 0;
-  float    affine[2][2];   // coefficients
-  float    i_affine[2][2];
-  float    beta[2];
-  float    i_beta[2];
-  float    det;
-  float    x_new, y_new;
-  float    x_frac, y_frac;
-  float    gray_new;
-  int      m, n;
-
-  unsigned short    output_buffer[X_SIZE];
-
-
-  // forward affine transformation 
-  affine[0][0] = lx_expan * cos((float)(lx_rot*PI/180.0f));
-  affine[0][1] = ly_expan * sin((float)(ly_rot*PI/180.0f));
-  affine[1][0] = lx_expan * sin((float)(lx_rot*PI/180.0f));
-  affine[1][1] = ly_expan * cos((float)(ly_rot*PI/180.0f));
-  beta[0]      = lx_move;
-  beta[1]      = ly_move;
-
-  // determination of inverse affine transformation
-  det = (affine[0][0] * affine[1][1]) - (affine[0][1] * affine[1][0]);
-  if (det == 0.0f)
-  {
-    i_affine[0][0]   = 1.0f;
-    i_affine[0][1]   = 0.0f;
-    i_affine[1][0]   = 0.0f;
-    i_affine[1][1]   = 1.0f;
-    i_beta[0]        = -beta[0];
-    i_beta[1]        = -beta[1];
-  } 
-  else 
-  {
-    i_affine[0][0]   =  affine[1][1]/det;
-    i_affine[0][1]   = -affine[0][1]/det;
-    i_affine[1][0]   = -affine[1][0]/det;
-    i_affine[1][1]   =  affine[0][0]/det;
-    i_beta[0]        = -i_affine[0][0]*beta[0]-i_affine[0][1]*beta[1];
-    i_beta[1]        = -i_affine[1][0]*beta[0]-i_affine[1][1]*beta[1];
-  }
-
-  // Output image generation by inverse affine transformation and bilinear transformation
-
-  x_new    = i_beta[0] + i_affine[0][0]*(x-X_SIZE/2.0f) + i_affine[0][1]*(y-Y_SIZE/2.0f) + X_SIZE/2.0f;
-  y_new    = i_beta[1] + i_affine[1][0]*(x-X_SIZE/2.0f) + i_affine[1][1]*(y-Y_SIZE/2.0f) + Y_SIZE/2.0f;
-
-  m        = (int)floor(x_new);
-  n        = (int)floor(y_new);
-
-  x_frac   = x_new - m;
-  y_frac   = y_new - n;
-
-  if ((m >= 0) && (m + 1 < X_SIZE) && (n >= 0) && (n+1 < Y_SIZE))
-  {
-    gray_new = (1.0f - y_frac) * ((1.0f - x_frac) * (src[(n * X_SIZE) + m])       + x_frac * (src[(n * X_SIZE) + m + 1])) + 
-      y_frac  * ((1.0f - x_frac) * (src[((n + 1) * X_SIZE) + m]) + x_frac * (src[((n + 1) * X_SIZE) + m + 1]));
-
-    output_buffer[x] = (unsigned short)gray_new;
-  } 
-  else if (((m + 1 == X_SIZE) && (n >= 0) && (n < Y_SIZE)) || ((n + 1 == Y_SIZE) && (m >= 0) && (m < X_SIZE))) 
-  {
-    output_buffer[x] = src[(n * X_SIZE) + m];
-  } 
-  else 
-  {
-    output_buffer[x] = WHITE;
-  }
-
-  dst[(y * X_SIZE)+x] = output_buffer[x];
-}
-
-// reference implementation for verification
-void affine_reference(const unsigned short *src, unsigned short *dst) 
-{
-  for (int y = 0; y < 512; y++) {
-    for (int x = 0; x < 512; x++) {
-      const float    lx_rot   = 30.0f;
-      const float    ly_rot   = 0.0f; 
-      const float    lx_expan = 0.5f;
-      const float    ly_expan = 0.5f; 
-      int      lx_move  = 0;
-      int      ly_move  = 0;
-      float    affine[2][2];   // coefficients
-      float    i_affine[2][2];
-      float    beta[2];
-      float    i_beta[2];
-      float    det;
-      float    x_new, y_new;
-      float    x_frac, y_frac;
-      float    gray_new;
-      int      m, n;
-
-      unsigned short    output_buffer[X_SIZE];
-
-
-      // forward affine transformation 
-      affine[0][0] = lx_expan * std::cos((float)(lx_rot*PI/180.0f));
-      affine[0][1] = ly_expan * std::sin((float)(ly_rot*PI/180.0f));
-      affine[1][0] = lx_expan * std::sin((float)(lx_rot*PI/180.0f));
-      affine[1][1] = ly_expan * std::cos((float)(ly_rot*PI/180.0f));
-      beta[0]      = lx_move;
-      beta[1]      = ly_move;
-
-      // determination of inverse affine transformation
-      det = (affine[0][0] * affine[1][1]) - (affine[0][1] * affine[1][0]);
-      if (det == 0.0f)
-      {
-        i_affine[0][0]   = 1.0f;
-        i_affine[0][1]   = 0.0f;
-        i_affine[1][0]   = 0.0f;
-        i_affine[1][1]   = 1.0f;
-        i_beta[0]        = -beta[0];
-        i_beta[1]        = -beta[1];
-      } 
-      else 
-      {
-        i_affine[0][0]   =  affine[1][1]/det;
-        i_affine[0][1]   = -affine[0][1]/det;
-        i_affine[1][0]   = -affine[1][0]/det;
-        i_affine[1][1]   =  affine[0][0]/det;
-        i_beta[0]        = -i_affine[0][0]*beta[0]-i_affine[0][1]*beta[1];
-        i_beta[1]        = -i_affine[1][0]*beta[0]-i_affine[1][1]*beta[1];
-      }
-
-      // Output image generation by inverse affine transformation and bilinear transformation
-
-      x_new    = i_beta[0] + i_affine[0][0]*(x-X_SIZE/2.0f) + i_affine[0][1]*(y-Y_SIZE/2.0f) + X_SIZE/2.0f;
-      y_new    = i_beta[1] + i_affine[1][0]*(x-X_SIZE/2.0f) + i_affine[1][1]*(y-Y_SIZE/2.0f) + Y_SIZE/2.0f;
-
-      m        = (int)std::floor(x_new);
-      n        = (int)std::floor(y_new);
-
-      x_frac   = x_new - m;
-      y_frac   = y_new - n;
-
-      if ((m >= 0) && (m + 1 < X_SIZE) && (n >= 0) && (n+1 < Y_SIZE))
-      {
-        gray_new = (1.0f - y_frac) * ((1.0f - x_frac) * (src[(n * X_SIZE) + m])       + x_frac * (src[(n * X_SIZE) + m + 1])) + 
-          y_frac  * ((1.0f - x_frac) * (src[((n + 1) * X_SIZE) + m]) + x_frac * (src[((n + 1) * X_SIZE) + m + 1]));
-
-        output_buffer[x] = (unsigned short)gray_new;
-      } 
-      else if (((m + 1 == X_SIZE) && (n >= 0) && (n < Y_SIZE)) || ((n + 1 == Y_SIZE) && (m >= 0) && (m < X_SIZE))) 
-      {
-        output_buffer[x] = src[(n * X_SIZE) + m];
-      } 
-      else 
-      {
-        output_buffer[x] = WHITE;
-      }
-
-      dst[(y * X_SIZE)+x] = output_buffer[x];
-    }
-  }
-}
+#include "reference.h"
+#include "kernel.h"
 
 int main(int argc, char** argv)
 {
-
-  if (argc != 3)
+  if (argc != 4)
   {
-    printf("Usage: %s <input image> <output image>\n", argv[0]) ;
+    printf("Usage: %s <input image> <output image> <iterations>\n", argv[0]) ;
     return -1 ;
   }
 
-  unsigned short    input_image[Y_SIZE*X_SIZE] __attribute__((aligned(1024)));
-  unsigned short    output_image[Y_SIZE*X_SIZE] __attribute__((aligned(1024)));
-  unsigned short    output_image_ref[Y_SIZE*X_SIZE] __attribute__((aligned(1024)));
+  unsigned short input_image[Y_SIZE*X_SIZE] __attribute__((aligned(1024)));
+  unsigned short output_image[Y_SIZE*X_SIZE] __attribute__((aligned(1024)));
+  unsigned short output_image_ref[Y_SIZE*X_SIZE] __attribute__((aligned(1024)));
 
   // Read the bit map file into memory and allocate memory for the final image
   std::cout << "Reading input image...\n";
@@ -242,6 +69,7 @@ int main(int argc, char** argv)
   size_t items_read = fread(input_image, sizeof(input_image), 1, input_file);
   printf("   Bytes read = %d\n\n", (int)(items_read * sizeof(input_image)));
 
+  const int iterations = atoi(argv[3]);
 
   unsigned short *d_input_image;
   cudaMalloc((void**)&d_input_image, sizeof(unsigned short)*X_SIZE*Y_SIZE);
@@ -253,9 +81,17 @@ int main(int argc, char** argv)
   dim3 grids (32,32);
   dim3 threads (16,16);
 
-  for (int i = 0; i < 100; i++) {
+  cudaDeviceSynchronize();
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < iterations; i++) {
     affine<<<grids, threads>>>(d_input_image, d_output_image);
   }
+
+  cudaDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  std::cout << "   Average kernel execution time " << (time * 1e-9f) / iterations << " (s)\n";
 
   cudaMemcpy(output_image, d_output_image, sizeof(unsigned short)*X_SIZE*Y_SIZE, cudaMemcpyDeviceToHost);
   cudaFree(d_input_image);
