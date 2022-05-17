8373ed091f953d0c488a1cad58b0038bf78125ad
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-05-17 18:31:16 -0400

    eliminate the local memory usage of the affine kernel in CUDA; split the main source file for reuse

---
+++ affine-cuda/kernel.h
@@ -0,0 +1,83 @@
+__global__ void affine (
+  const unsigned short *__restrict__ src,
+        unsigned short *__restrict__ dst) 
+{
+  int x = blockIdx.x*blockDim.x+threadIdx.x;
+  int y = blockIdx.y*blockDim.y+threadIdx.y;
+
+  const float lx_rot   = 30.0f;
+  const float ly_rot   = 0.0f; 
+  const float lx_expan = 0.5f;
+  const float ly_expan = 0.5f; 
+  int   lx_move  = 0;
+  int   ly_move  = 0;
+  float affine[2][2];   // coefficients
+  float i_affine[2][2];
+  float beta[2];
+  float i_beta[2];
+  float det;
+  float x_new, y_new;
+  float x_frac, y_frac;
+  float gray_new;
+  int   m, n;
+  unsigned short output_buffer;
+
+  // forward affine transformation 
+  affine[0][0] = lx_expan * cosf(lx_rot*PI/180.0f);
+  affine[0][1] = ly_expan * sinf(ly_rot*PI/180.0f);
+  affine[1][0] = lx_expan * sinf(lx_rot*PI/180.0f);
+  affine[1][1] = ly_expan * cosf(ly_rot*PI/180.0f);
+  beta[0]      = lx_move;
+  beta[1]      = ly_move;
+
+  // determination of inverse affine transformation
+  det = (affine[0][0] * affine[1][1]) - (affine[0][1] * affine[1][0]);
+  if (det == 0.0f)
+  {
+    i_affine[0][0] = 1.0f;
+    i_affine[0][1] = 0.0f;
+    i_affine[1][0] = 0.0f;
+    i_affine[1][1] = 1.0f;
+    i_beta[0]      = -beta[0];
+    i_beta[1]      = -beta[1];
+  } 
+  else 
+  {
+    i_affine[0][0] =  affine[1][1]/det;
+    i_affine[0][1] = -affine[0][1]/det;
+    i_affine[1][0] = -affine[1][0]/det;
+    i_affine[1][1] =  affine[0][0]/det;
+    i_beta[0]      = -i_affine[0][0]*beta[0]-i_affine[0][1]*beta[1];
+    i_beta[1]      = -i_affine[1][0]*beta[0]-i_affine[1][1]*beta[1];
+  }
+
+  // Output image generation by inverse affine transformation and bilinear transformation
+
+  x_new  = i_beta[0] + i_affine[0][0]*(x-X_SIZE/2.0f) + i_affine[0][1]*(y-Y_SIZE/2.0f) + X_SIZE/2.0f;
+  y_new  = i_beta[1] + i_affine[1][0]*(x-X_SIZE/2.0f) + i_affine[1][1]*(y-Y_SIZE/2.0f) + Y_SIZE/2.0f;
+
+  m      = (int)floorf(x_new);
+  n      = (int)floorf(y_new);
+
+  x_frac = x_new - m;
+  y_frac = y_new - n;
+
+  if ((m >= 0) && (m + 1 < X_SIZE) && (n >= 0) && (n+1 < Y_SIZE))
+  {
+    gray_new = (1.0f - y_frac) * ((1.0f - x_frac) * (src[(n * X_SIZE) + m]) + x_frac * (src[(n * X_SIZE) + m + 1])) + 
+      y_frac  * ((1.0f - x_frac) * (src[((n + 1) * X_SIZE) + m]) + x_frac * (src[((n + 1) * X_SIZE) + m + 1]));
+
+    output_buffer = (unsigned short)gray_new;
+  } 
+  else if (((m + 1 == X_SIZE) && (n >= 0) && (n < Y_SIZE)) || ((n + 1 == Y_SIZE) && (m >= 0) && (m < X_SIZE))) 
+  {
+    output_buffer = src[(n * X_SIZE) + m];
+  } 
+  else 
+  {
+    output_buffer = WHITE;
+  }
+
+  dst[(y * X_SIZE)+x] = output_buffer;
+}
+
