c5dcf1f0e08c7ffb51a7e7543a26d28288b4c2d9
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2022-05-18 12:14:10 -0400

    add the sa example in hip

--- sa-hip/main.cu
+++ sa-hip/main.cu
@@ -153,15 +153,10 @@ void suffixArray(thrust::device_vector<int>& s,
   hipLaunchKernelGGL(Init_d_s12, numBlocks, numThreads, 0, 0, pd_s12, n02);
 
   // radix sort - using SA12 to store keys
-  hipLaunchKernelGGL(keybits, numBlocks, numThreads, 0, 0, pd_SA12, pd_s12, pd_s, n02, 2);
-
-  thrust::sort_by_key(d_SA12.begin(), d_SA12.begin() + n02, d_s12.begin());
-
-  hipLaunchKernelGGL(keybits, numBlocks, numThreads, 0, 0, pd_SA12, pd_s12, pd_s, n02, 1);
-  thrust::sort_by_key(d_SA12.begin(), d_SA12.begin() + n02, d_s12.begin());
-
-  hipLaunchKernelGGL(keybits, numBlocks, numThreads, 0, 0, pd_SA12, pd_s12, pd_s, n02, 0);
-  thrust::sort_by_key(d_SA12.begin(), d_SA12.begin() + n02, d_s12.begin());
+  for (int i = 2; i >= 0; i--) {
+    hipLaunchKernelGGL(keybits, numBlocks, numThreads, 0, 0, pd_SA12, pd_s12, pd_s, n02, i);
+    thrust::sort_by_key(d_SA12.begin(), d_SA12.begin() + n02, d_s12.begin());
+  }
 
   d_SA12 = d_s12;
 
