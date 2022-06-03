dbc2fdc5a7b43916f0f7b49fa859688e9829cf72
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-06-03 17:10:40 -0400

    [debayer] display the average kernel execution time; fix checksum mismatch by zeroing the device output for each kernel iteration

--- debayer-cuda/main.cu
+++ debayer-cuda/main.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <chrono>
 #include <cuda.h>
 #include "util.h"
 #include "image.h"
@@ -44,13 +45,23 @@ int main(int argc, char* argv[])
   dim3 grids ((width + tile_cols - 1) / tile_cols, (height + tile_rows - 1) / tile_rows);
   dim3 blocks (tile_cols, tile_rows);
 
+  cudaDeviceSynchronize();
+  auto start = std::chrono::steady_clock::now();
+
   //this version takes a tile (z=1) and each tile job does 4 line median sorts
-  for (int i = 0; i < repeat; i++) 
+  for (int i = 0; i < repeat; i++) {
+    cudaMemset(d_output, 0, output_image_size);
     malvar_he_cutler_demosaic <<< grids, blocks >>> (
       height, width, 
       d_input, input_image_pitch,
       d_output, output_image_pitch,
       bayer_pattern );
+  }
+
+  cudaDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average kernel execution time %f (s)\n", time * 1e-9f / repeat);
 
   cudaMemcpy(output, d_output, output_image_size, cudaMemcpyDeviceToHost);
 
