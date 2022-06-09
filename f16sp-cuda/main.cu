58b873cbc2c6b625fa5e18905c23bcf0e4bd3139
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-06-09 12:36:34 -0400

    [f16sp] add an option for kernel execution count; display the average kernel execution time

--- f16sp-cuda/main.cu
+++ f16sp-cuda/main.cu
@@ -11,6 +11,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
+#include <cuda.h>
 #include <cuda_fp16.h>
 
 #define NUM_OF_BLOCKS 1024
@@ -19,175 +21,193 @@
 __forceinline__ __device__ 
 void reduceInShared_intrinsics(half2 * const v)
 {
-    int lid = threadIdx.x;	
-    if(lid<64) v[lid] = __hadd2( v[lid], v[lid+64]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+32]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+16]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+8]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+4]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+2]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+1]);
-    __syncthreads();
+  int lid = threadIdx.x;  
+  if(lid<64) v[lid] = __hadd2( v[lid], v[lid+64]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+32]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+16]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+8]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+4]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+2]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+1]);
+  __syncthreads();
 }
 
 __forceinline__ __device__
 void reduceInShared_native(half2 * const v)
 {
-    int lid = threadIdx.x;	
-    if(lid<64) v[lid] = v[lid] + v[lid+64];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+32];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+16];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+8];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+4];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+2];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+1];
-    __syncthreads();
+  int lid = threadIdx.x;  
+  if(lid<64) v[lid] = v[lid] + v[lid+64];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+32];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+16];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+8];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+4];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+2];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+1];
+  __syncthreads();
 }
 
 __global__
 void scalarProductKernel_intrinsics(
-        half2 const * const a,
-        half2 const * const b,
-        float * const results,
-        size_t const size
-        )
+    half2 const *__restrict__ const a,
+    half2 const *__restrict__ const b,
+    float *__restrict__ const results,
+    size_t const size)
 {
-    const int stride = gridDim.x*blockDim.x;
-    __shared__ half2 shArray[NUM_OF_THREADS];
+  const int stride = gridDim.x*blockDim.x;
+  __shared__ half2 shArray[NUM_OF_THREADS];
 
-    half2 value = __float2half2_rn(0.f);
+  half2 value = __float2half2_rn(0.f);
 
-    for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < size; i+=stride)
-    {
-        value = __hfma2(a[i], b[i], value);
-    }
+  for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < size; i+=stride)
+  {
+    value = __hfma2(a[i], b[i], value);
+  }
 
-    shArray[threadIdx.x] = value;
-    __syncthreads();
-    reduceInShared_intrinsics(shArray);
+  shArray[threadIdx.x] = value;
+  __syncthreads();
+  reduceInShared_intrinsics(shArray);
 
-    if (threadIdx.x == 0)
-    {
-        half2 result = shArray[0];
-        float f_result = __low2float(result) + __high2float(result);
-        results[blockIdx.x] = f_result;
-    }
+  if (threadIdx.x == 0)
+  {
+    half2 result = shArray[0];
+    float f_result = __low2float(result) + __high2float(result);
+    results[blockIdx.x] = f_result;
+  }
 }
 
 __global__
 void scalarProductKernel_native(
-        half2 const * const a,
-        half2 const * const b,
-        float * const results,
-        size_t const size
-        )
+    half2 const *__restrict__ const a,
+    half2 const *__restrict__ const b,
+    float *__restrict__ const results,
+    size_t const size)
 {
-    const int stride = gridDim.x*blockDim.x;
-    __shared__ half2 shArray[NUM_OF_THREADS];
+  const int stride = gridDim.x*blockDim.x;
+  __shared__ half2 shArray[NUM_OF_THREADS];
 
-    half2 value(0.f, 0.f);
-    shArray[threadIdx.x] = value;
+  half2 value(0.f, 0.f);
+  shArray[threadIdx.x] = value;
 
-    for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < size; i+=stride)
-    {
-        value = a[i] * b[i] + value;
-    }
+  for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < size; i+=stride)
+  {
+    value += a[i] * b[i];
+  }
 
-    shArray[threadIdx.x] = value;
-    __syncthreads();
-    reduceInShared_native(shArray);
+  shArray[threadIdx.x] = value;
+  __syncthreads();
+  reduceInShared_native(shArray);
 
-    if (threadIdx.x == 0)
-    {
-        half2 result = shArray[0];
-        float f_result = (float)result.y + (float)result.x;
-        results[blockIdx.x] = f_result;
-    }
+  if (threadIdx.x == 0)
+  {
+    half2 result = shArray[0];
+    float f_result = (float)result.y + (float)result.x;
+    results[blockIdx.x] = f_result;
+  }
 }
 
 void generateInput(half2 * a, size_t size)
 {
-    for (size_t i = 0; i < size; ++i)
-    {
-        half2 temp;
-        temp.x = static_cast<float>(rand() % 4);
-        temp.y = static_cast<float>(rand() % 2);
-        a[i] = temp;
-    }
+  for (size_t i = 0; i < size; ++i)
+  {
+    half2 temp;
+    temp.x = static_cast<float>(rand() % 4);
+    temp.y = static_cast<float>(rand() % 2);
+    a[i] = temp;
+  }
 }
 
 int main(int argc, char *argv[])
 {
-    size_t size = NUM_OF_BLOCKS*NUM_OF_THREADS*16;
+  if (argc != 2) {
+    printf("Usage: %s <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int repeat = atoi(argv[1]);
 
-    half2 * a, *b;
-    half2 * d_a, *d_b;
+  size_t size = NUM_OF_BLOCKS*NUM_OF_THREADS*16;
 
-    float * r;  // result
-    float * d_r;
+  half2 * a, *b;
+  half2 * d_a, *d_b;
 
-    a = (half2*) malloc (size*sizeof(half2));
-    b = (half2*) malloc (size*sizeof(half2));
-    cudaMalloc((void**)&d_a, size*sizeof(half2));
-    cudaMalloc((void**)&d_b, size*sizeof(half2));
+  float * r;  // result
+  float * d_r;
 
-    r = (float*) malloc (NUM_OF_BLOCKS*sizeof(float));
-    cudaMalloc((void**)&d_r, NUM_OF_BLOCKS*sizeof(float));
+  a = (half2*) malloc (size*sizeof(half2));
+  b = (half2*) malloc (size*sizeof(half2));
+  cudaMalloc((void**)&d_a, size*sizeof(half2));
+  cudaMalloc((void**)&d_b, size*sizeof(half2));
 
-    srand(123); 
-    generateInput(a, size);
-    cudaMemcpy(d_a, a, size*sizeof(half2), cudaMemcpyHostToDevice);
+  r = (float*) malloc (NUM_OF_BLOCKS*sizeof(float));
+  cudaMalloc((void**)&d_r, NUM_OF_BLOCKS*sizeof(float));
 
-    generateInput(b, size);
-    cudaMemcpy(d_b, b, size*sizeof(half2), cudaMemcpyHostToDevice);
+  srand(123); 
+  generateInput(a, size);
+  cudaMemcpy(d_a, a, size*sizeof(half2), cudaMemcpyHostToDevice);
 
-    for (int i = 0; i < 10000; i++)
-      scalarProductKernel_intrinsics<<<NUM_OF_BLOCKS, NUM_OF_THREADS>>>(d_a, d_b, d_r, size);
+  generateInput(b, size);
+  cudaMemcpy(d_b, b, size*sizeof(half2), cudaMemcpyHostToDevice);
 
-    cudaMemcpy(r, d_r, NUM_OF_BLOCKS*sizeof(float), cudaMemcpyDeviceToHost);
+  cudaDeviceSynchronize();
+  auto start = std::chrono::steady_clock::now();
 
+  for (int i = 0; i < repeat; i++)
+    scalarProductKernel_intrinsics<<<NUM_OF_BLOCKS, NUM_OF_THREADS>>>(d_a, d_b, d_r, size);
 
-    float result_intrinsics = 0;
-    for (int i = 0; i < NUM_OF_BLOCKS; ++i)
-    {
-        result_intrinsics += r[i];
-    }
-    printf("Result intrinsics\t: %f \n", result_intrinsics);
+  cudaDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average kernel execution time %f (s)\n", (time * 1e-9f) / repeat);
 
-    for (int i = 0; i < 10000; i++)
-      scalarProductKernel_native<<<NUM_OF_BLOCKS, NUM_OF_THREADS>>>(d_a, d_b, d_r, size);
+  cudaMemcpy(r, d_r, NUM_OF_BLOCKS*sizeof(float), cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(r, d_r, NUM_OF_BLOCKS*sizeof(float), cudaMemcpyDeviceToHost);
+  float result_intrinsics = 0;
+  for (int i = 0; i < NUM_OF_BLOCKS; ++i)
+  {
+    result_intrinsics += r[i];
+  }
+  printf("Result intrinsics\t: %f \n", result_intrinsics);
 
-    float result_native = 0;
-    for (int i = 0; i < NUM_OF_BLOCKS; ++i)
-    {
-        result_native += r[i];
-    }
-    printf("Result native operators\t: %f \n", result_native);
+  cudaDeviceSynchronize();
+  start = std::chrono::steady_clock::now();
 
+  for (int i = 0; i < repeat; i++)
+    scalarProductKernel_native<<<NUM_OF_BLOCKS, NUM_OF_THREADS>>>(d_a, d_b, d_r, size);
 
-    printf("fp16ScalarProduct %s\n", (fabs(result_intrinsics - result_native) < 0.00001) ? 
-                                     "PASSED" : "FAILED");
+  cudaDeviceSynchronize();
+  end = std::chrono::steady_clock::now();
+  time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average kernel execution time %f (s)\n", (time * 1e-9f) / repeat);
 
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_r);
-    free(a);
-    free(b);
-    free(r);
+  cudaMemcpy(r, d_r, NUM_OF_BLOCKS*sizeof(float), cudaMemcpyDeviceToHost);
 
-    return EXIT_SUCCESS;
+  float result_native = 0;
+  for (int i = 0; i < NUM_OF_BLOCKS; ++i)
+  {
+    result_native += r[i];
+  }
+  printf("Result native operators\t: %f \n", result_native);
+
+  printf("fp16ScalarProduct %s\n", (fabs(result_intrinsics - result_native) < 0.00001) ? 
+         "PASS" : "FAIL");
+
+  cudaFree(d_a);
+  cudaFree(d_b);
+  cudaFree(d_r);
+  free(a);
+  free(b);
+  free(r);
+
+  return EXIT_SUCCESS;
 }
--- f16sp-hip/main.cu
+++ f16sp-hip/main.cu
@@ -11,6 +11,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
+#include <hip/hip_runtime.h>
 #include <hip/hip_fp16.h>
 
 #define NUM_OF_BLOCKS 1024
@@ -19,176 +21,193 @@
 __forceinline__ __device__ 
 void reduceInShared_intrinsics(half2 * const v)
 {
-    int lid = threadIdx.x;	
-    if(lid<64) v[lid] = __hadd2( v[lid], v[lid+64]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+32]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+16]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+8]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+4]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+2]);
-    __syncthreads();
-    if(lid<32) v[lid] = __hadd2( v[lid], v[lid+1]);
-    __syncthreads();
+  int lid = threadIdx.x;  
+  if(lid<64) v[lid] = __hadd2( v[lid], v[lid+64]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+32]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+16]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+8]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+4]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+2]);
+  __syncthreads();
+  if(lid<32) v[lid] = __hadd2( v[lid], v[lid+1]);
+  __syncthreads();
 }
 
 __forceinline__ __device__
 void reduceInShared_native(half2 * const v)
 {
-    int lid = threadIdx.x;	
-    if(lid<64) v[lid] = v[lid] + v[lid+64];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+32];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+16];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+8];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+4];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+2];
-    __syncthreads();
-    if(lid<32) v[lid] = v[lid] + v[lid+1];
-    __syncthreads();
+  int lid = threadIdx.x;  
+  if(lid<64) v[lid] = v[lid] + v[lid+64];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+32];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+16];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+8];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+4];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+2];
+  __syncthreads();
+  if(lid<32) v[lid] = v[lid] + v[lid+1];
+  __syncthreads();
 }
 
 __global__
 void scalarProductKernel_intrinsics(
-        half2 const * const a,
-        half2 const * const b,
-        float * const results,
-        size_t const size
-        )
+    half2 const *__restrict__ const a,
+    half2 const *__restrict__ const b,
+    float *__restrict__ const results,
+    size_t const size)
 {
-    const int stride = gridDim.x*blockDim.x;
-    __shared__ half2 shArray[NUM_OF_THREADS];
+  const int stride = gridDim.x*blockDim.x;
+  __shared__ half2 shArray[NUM_OF_THREADS];
 
-    half2 value = __float2half2_rn(0.f);
+  half2 value = __float2half2_rn(0.f);
 
-    for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < size; i+=stride)
-    {
-        value = __hfma2(a[i], b[i], value);
-    }
+  for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < size; i+=stride)
+  {
+    value = __hfma2(a[i], b[i], value);
+  }
 
-    shArray[threadIdx.x] = value;
-    __syncthreads();
-    reduceInShared_intrinsics(shArray);
+  shArray[threadIdx.x] = value;
+  __syncthreads();
+  reduceInShared_intrinsics(shArray);
 
-    if (threadIdx.x == 0)
-    {
-        half2 result = shArray[0];
-        float f_result = __low2float(result) + __high2float(result);
-        results[blockIdx.x] = f_result;
-    }
+  if (threadIdx.x == 0)
+  {
+    half2 result = shArray[0];
+    float f_result = __low2float(result) + __high2float(result);
+    results[blockIdx.x] = f_result;
+  }
 }
 
 __global__
 void scalarProductKernel_native(
-        half2 const * const a,
-        half2 const * const b,
-        float * const results,
-        size_t const size
-        )
+    half2 const *__restrict__ const a,
+    half2 const *__restrict__ const b,
+    float *__restrict__ const results,
+    size_t const size)
 {
-    const int stride = gridDim.x*blockDim.x;
-    __shared__ half2 shArray[NUM_OF_THREADS];
+  const int stride = gridDim.x*blockDim.x;
+  __shared__ half2 shArray[NUM_OF_THREADS];
 
-    half2 value(0.f, 0.f);
+  half2 value(0.f, 0.f);
+  shArray[threadIdx.x] = value;
 
-    for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < size; i+=stride)
-    {
-        value = a[i] * b[i] + value;
-    }
+  for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < size; i+=stride)
+  {
+    value += a[i] * b[i];
+  }
 
-    shArray[threadIdx.x] = value;
-    __syncthreads();
-    reduceInShared_native(shArray);
+  shArray[threadIdx.x] = value;
+  __syncthreads();
+  reduceInShared_native(shArray);
 
-    if (threadIdx.x == 0)
-    {
-        half2 result = shArray[0];
-	// x and y are protected members
-        //float f_result = (float)result.y + (float)result.x;
-        float f_result = (float)__half2_raw(result).data.x +
-                         (float)__half2_raw(result).data.y;
-        results[blockIdx.x] = f_result;
-    }
+  if (threadIdx.x == 0)
+  {
+    half2 result = shArray[0];
+    float f_result = (float)result.y + (float)result.x;
+    results[blockIdx.x] = f_result;
+  }
 }
 
-// temp.x and temp.y are protected members
 void generateInput(half2 * a, size_t size)
 {
-    for (size_t i = 0; i < size; ++i)
-    {
-        a[i] = half2( static_cast<float>(rand() % 4),
-	              static_cast<float>(rand() % 2) );
-    }
+  for (size_t i = 0; i < size; ++i)
+  {
+    half2 temp;
+    temp.x = static_cast<float>(rand() % 4);
+    temp.y = static_cast<float>(rand() % 2);
+    a[i] = temp;
+  }
 }
 
 int main(int argc, char *argv[])
 {
-    size_t size = NUM_OF_BLOCKS*NUM_OF_THREADS*16;
+  if (argc != 2) {
+    printf("Usage: %s <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int repeat = atoi(argv[1]);
 
-    half2 * a, *b;
-    half2 * d_a, *d_b;
+  size_t size = NUM_OF_BLOCKS*NUM_OF_THREADS*16;
 
-    float * r;  // result
-    float * d_r;
+  half2 * a, *b;
+  half2 * d_a, *d_b;
 
-    a = (half2*) malloc (size*sizeof(half2));
-    b = (half2*) malloc (size*sizeof(half2));
-    hipMalloc((void**)&d_a, size*sizeof(half2));
-    hipMalloc((void**)&d_b, size*sizeof(half2));
+  float * r;  // result
+  float * d_r;
 
-    r = (float*) malloc (NUM_OF_BLOCKS*sizeof(float));
-    hipMalloc((void**)&d_r, NUM_OF_BLOCKS*sizeof(float));
+  a = (half2*) malloc (size*sizeof(half2));
+  b = (half2*) malloc (size*sizeof(half2));
+  hipMalloc((void**)&d_a, size*sizeof(half2));
+  hipMalloc((void**)&d_b, size*sizeof(half2));
 
-    srand(123); 
-    generateInput(a, size);
-    hipMemcpy(d_a, a, size*sizeof(half2), hipMemcpyHostToDevice);
+  r = (float*) malloc (NUM_OF_BLOCKS*sizeof(float));
+  hipMalloc((void**)&d_r, NUM_OF_BLOCKS*sizeof(float));
 
-    generateInput(b, size);
-    hipMemcpy(d_b, b, size*sizeof(half2), hipMemcpyHostToDevice);
+  srand(123); 
+  generateInput(a, size);
+  hipMemcpy(d_a, a, size*sizeof(half2), hipMemcpyHostToDevice);
 
-    for (int i = 0; i < 10000; i++)
-      hipLaunchKernelGGL(scalarProductKernel_intrinsics, dim3(NUM_OF_BLOCKS), dim3(NUM_OF_THREADS), 0, 0, d_a, d_b, d_r, size);
+  generateInput(b, size);
+  hipMemcpy(d_b, b, size*sizeof(half2), hipMemcpyHostToDevice);
 
-    hipMemcpy(r, d_r, NUM_OF_BLOCKS*sizeof(float), hipMemcpyDeviceToHost);
+  hipDeviceSynchronize();
+  auto start = std::chrono::steady_clock::now();
 
+  for (int i = 0; i < repeat; i++)
+    hipLaunchKernelGGL(scalarProductKernel_intrinsics, NUM_OF_BLOCKS, NUM_OF_THREADS, 0, 0, d_a, d_b, d_r, size);
 
-    float result_intrinsics = 0;
-    for (int i = 0; i < NUM_OF_BLOCKS; ++i)
-    {
-        result_intrinsics += r[i];
-    }
-    printf("Result intrinsics\t: %f \n", result_intrinsics);
+  hipDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average kernel execution time %f (s)\n", (time * 1e-9f) / repeat);
 
-    for (int i = 0; i < 10000; i++)
-      hipLaunchKernelGGL(scalarProductKernel_native, dim3(NUM_OF_BLOCKS), dim3(NUM_OF_THREADS), 0, 0, d_a, d_b, d_r, size);
+  hipMemcpy(r, d_r, NUM_OF_BLOCKS*sizeof(float), hipMemcpyDeviceToHost);
 
-    hipMemcpy(r, d_r, NUM_OF_BLOCKS*sizeof(float), hipMemcpyDeviceToHost);
+  float result_intrinsics = 0;
+  for (int i = 0; i < NUM_OF_BLOCKS; ++i)
+  {
+    result_intrinsics += r[i];
+  }
+  printf("Result intrinsics\t: %f \n", result_intrinsics);
 
-    float result_native = 0;
-    for (int i = 0; i < NUM_OF_BLOCKS; ++i)
-    {
-        result_native += r[i];
-    }
-    printf("Result native operators\t: %f \n", result_native);
+  hipDeviceSynchronize();
+  start = std::chrono::steady_clock::now();
 
+  for (int i = 0; i < repeat; i++)
+    hipLaunchKernelGGL(scalarProductKernel_native, NUM_OF_BLOCKS, NUM_OF_THREADS, 0, 0, d_a, d_b, d_r, size);
 
-    printf("fp16ScalarProduct %s\n", (fabs(result_intrinsics - result_native) < 0.00001) ? 
-                                     "PASSED" : "FAILED");
+  hipDeviceSynchronize();
+  end = std::chrono::steady_clock::now();
+  time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average kernel execution time %f (s)\n", (time * 1e-9f) / repeat);
 
-    hipFree(d_a);
-    hipFree(d_b);
-    hipFree(d_r);
-    free(a);
-    free(b);
-    free(r);
+  hipMemcpy(r, d_r, NUM_OF_BLOCKS*sizeof(float), hipMemcpyDeviceToHost);
 
-    return EXIT_SUCCESS;
+  float result_native = 0;
+  for (int i = 0; i < NUM_OF_BLOCKS; ++i)
+  {
+    result_native += r[i];
+  }
+  printf("Result native operators\t: %f \n", result_native);
+
+  printf("fp16ScalarProduct %s\n", (fabs(result_intrinsics - result_native) < 0.00001) ? 
+         "PASS" : "FAIL");
+
+  hipFree(d_a);
+  hipFree(d_b);
+  hipFree(d_r);
+  free(a);
+  free(b);
+  free(r);
+
+  return EXIT_SUCCESS;
 }
