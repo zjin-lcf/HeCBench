61ddaf91cb5649080efed2d92fb0a8c659db7ab0
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-06-11 22:38:34 -0400

    [gamma-correction] print basic usage message; display average kernel execution time; format the sources

--- gamma-correction-hip/main.cu
+++ gamma-correction-hip/main.cu
@@ -6,92 +6,105 @@
 
 #include <iomanip>
 #include <iostream>
+#include <chrono>
 #include <hip/hip_runtime.h>
 #include "utils.hpp"
 
-#ifndef BLOCK_SIZE
-#define BLOCK_SIZE 256
-#endif
-
-using namespace std;
-
 __global__ 
 void gamma_correction(ImgPixel* pixel) {
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-	// Lambda to process image with gamma = 2
-	const float v = (0.3f * pixel[i].r + 0.59f * pixel[i].g + 0.11f * pixel[i].b) / 255.0;
+  // Lambda to process image with gamma = 2
+  const float v = (0.3f * pixel[i].r + 0.59f * pixel[i].g + 0.11f * pixel[i].b) / 255.0;
 
-	std::uint8_t gamma_pixel = static_cast<std::uint8_t>(255 * v * v);
-	if (gamma_pixel > 255) gamma_pixel = 255;
-	pixel[i].set(gamma_pixel, gamma_pixel, gamma_pixel, gamma_pixel);
+  std::uint8_t gamma_pixel = static_cast<std::uint8_t>(255 * v * v);
+  if (gamma_pixel > 255) gamma_pixel = 255;
+  pixel[i].set(gamma_pixel, gamma_pixel, gamma_pixel, gamma_pixel);
 }
 
+int main(int argc, char* argv[]) {
+  if (argc != 5) {
+    printf("Usage: %s <image width> <image height> <block size> <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int width = atoi(argv[1]);
+  const int height = atoi(argv[2]);
+  const int block_size = atoi(argv[3]);
+  const int repeat = atoi(argv[4]);
 
-int main() {
-	// Image size is width x height
-	int width = 7680; //2560;
-	int height = 4320; //1600;
+  Img<ImgFormat::BMP> image{width, height};
+  ImgFractal fractal{width, height};
 
-	Img<ImgFormat::BMP> image{width, height};
-	ImgFractal fractal{width, height};
+  // Lambda to process image with gamma = 2
+  auto gamma_f = [](ImgPixel& pixel) {
+    float v = (0.3f * pixel.r + 0.59f * pixel.g + 0.11f * pixel.b) / 255.0;
 
-	// Lambda to process image with gamma = 2
-	auto gamma_f = [](ImgPixel& pixel) {
-		float v = (0.3f * pixel.r + 0.59f * pixel.g + 0.11f * pixel.b) / 255.0;
+    std::uint8_t gamma_pixel = static_cast<std::uint8_t>(255 * v * v);
+    if (gamma_pixel > 255) gamma_pixel = 255;
+    pixel.set(gamma_pixel, gamma_pixel, gamma_pixel, gamma_pixel);
+  };
 
-		std::uint8_t gamma_pixel = static_cast<std::uint8_t>(255 * v * v);
-		if (gamma_pixel > 255) gamma_pixel = 255;
-		pixel.set(gamma_pixel, gamma_pixel, gamma_pixel, gamma_pixel);
-	};
+  // fill image with created fractal
+  int index = 0;
+  image.fill([&index, width, &fractal](ImgPixel& pixel) {
+      int x = index % width;
+      int y = index / width;
 
-	// fill image with created fractal
-	int index = 0;
-	image.fill([&index, width, &fractal](ImgPixel& pixel) {
-			int x = index % width;
-			int y = index / width;
+      auto fractal_pixel = fractal(x, y);
+      if (fractal_pixel < 0) fractal_pixel = 0;
+      if (fractal_pixel > 255) fractal_pixel = 255;
+      pixel.set(fractal_pixel, fractal_pixel, fractal_pixel, fractal_pixel);
 
-			auto fractal_pixel = fractal(x, y);
-			if (fractal_pixel < 0) fractal_pixel = 0;
-			if (fractal_pixel > 255) fractal_pixel = 255;
-			pixel.set(fractal_pixel, fractal_pixel, fractal_pixel, fractal_pixel);
+      ++index;
+  });
 
-			++index;
-			});
-
-	Img<ImgFormat::BMP> image2 = image;
+  Img<ImgFormat::BMP> image2 = image;
 #ifdef DEBUG
-	image.write("fractal_original.bmp");
+  image.write("fractal_original.bmp");
 #endif
 
-	// call standard serial function for correctness check
-	image.fill(gamma_f);
+  // call standard serial function for correctness check
+  image.fill(gamma_f);
 #ifdef DEBUG
-	image.write("fractal_gamma_serial.bmp");
+  image.write("fractal_gamma_serial.bmp");
 #endif
 
-	ImgPixel* pixel;
-	hipMalloc((void**)&pixel, sizeof(ImgPixel) * image2.width() * image2.height());
-	hipMemcpy(pixel, image2.data(), sizeof(ImgPixel) * image2.width() * image2.height(), hipMemcpyHostToDevice);
-	hipLaunchKernelGGL(gamma_correction, width * height / BLOCK_SIZE, BLOCK_SIZE, 0, 0, pixel); 
-	hipMemcpy(image2.data(), pixel, sizeof(ImgPixel) * image2.width() * image2.height(), hipMemcpyDeviceToHost);
+  ImgPixel* pixel;
+  hipMalloc((void**)&pixel, sizeof(ImgPixel) * image2.width() * image2.height());
+  
+  dim3 grids (width * height / block_size);
+  dim3 blocks (block_size); 
 
-	// check correctness
-	if (check(image.begin(), image.end(), image2.begin())) {
-		std::cout << "Pass";
-	} else {
-		std::cout << "Fail";
-	}
+  float total_time = 0.f;
 
-	hipDeviceProp_t props;
-	hipGetDeviceProperties(&props, 0);
+  for (int i = 0; i < repeat; i++) {
+    hipMemcpy(pixel, image2.data(), sizeof(ImgPixel) * image2.width() * image2.height(), hipMemcpyHostToDevice);
 
-	std::cout << ". Run on " << props.name << std::endl;
+    hipDeviceSynchronize();
+    auto start = std::chrono::steady_clock::now();
+
+    hipLaunchKernelGGL(gamma_correction, grids, blocks, 0, 0, pixel); 
+
+    hipDeviceSynchronize();
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    total_time += time;
+  }
+  printf("Average kernel execution time %f (s)\n", (total_time * 1e-9f) / repeat);
+
+  hipMemcpy(image2.data(), pixel, sizeof(ImgPixel) * image2.width() * image2.height(), hipMemcpyDeviceToHost);
+
+  // check correctness
+  if (check(image.begin(), image.end(), image2.begin())) {
+    std::cout << "PASS\n";
+  } else {
+    std::cout << "FAIL\n";
+  }
 
 #ifdef DEBUG
-	image.write("fractal_gamma_parallel.bmp");
+  image.write("fractal_gamma_parallel.bmp");
 #endif
-	hipFree(pixel); 
+  hipFree(pixel); 
 
-	return 0;
+  return 0;
 }
--- gamma-correction-omp/main.cpp
+++ gamma-correction-omp/main.cpp
@@ -6,76 +6,92 @@
 
 #include <iomanip>
 #include <iostream>
+#include <chrono>
+#include <omp.h>
 #include "utils.hpp"
 
-#ifndef BLOCK_SIZE
-#define BLOCK_SIZE 256
-#endif
+int main(int argc, char* argv[]) {
+  if (argc != 5) {
+    printf("Usage: %s <image width> <image height> <block size> <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int width = atoi(argv[1]);
+  const int height = atoi(argv[2]);
+  const int block_size = atoi(argv[3]);
+  const int repeat = atoi(argv[4]);
 
-int main() {
-	// Image size is width x height
-	int width = 7680; //2560;
-	int height = 4320; //1600;
+  Img<ImgFormat::BMP> image{width, height};
+  ImgFractal fractal{width, height};
 
-	Img<ImgFormat::BMP> image{width, height};
-	ImgFractal fractal{width, height};
+  // Lambda to process image with gamma = 2
+  auto gamma_f = [](ImgPixel& pixel) {
+    float v = (0.3f * pixel.r + 0.59f * pixel.g + 0.11f * pixel.b) / 255.0;
+    std::uint8_t gamma_pixel = static_cast<std::uint8_t>(255 * v * v);
+    if (gamma_pixel > 255) gamma_pixel = 255;
+    pixel.set(gamma_pixel, gamma_pixel, gamma_pixel, gamma_pixel);
+  };
 
-	// Lambda to process image with gamma = 2
-	auto gamma_f = [](ImgPixel& pixel) {
-		float v = (0.3f * pixel.r + 0.59f * pixel.g + 0.11f * pixel.b) / 255.0;
-		std::uint8_t gamma_pixel = static_cast<std::uint8_t>(255 * v * v);
-		if (gamma_pixel > 255) gamma_pixel = 255;
-		pixel.set(gamma_pixel, gamma_pixel, gamma_pixel, gamma_pixel);
-	};
+  // fill image with created fractal
+  int index = 0;
+  image.fill([&index, width, &fractal](ImgPixel& pixel) {
+      int x = index % width;
+      int y = index / width;
 
-	// fill image with created fractal
-	int index = 0;
-	image.fill([&index, width, &fractal](ImgPixel& pixel) {
-          int x = index % width;
-          int y = index / width;
-          
-          auto fractal_pixel = fractal(x, y);
-          if (fractal_pixel < 0) fractal_pixel = 0;
-          if (fractal_pixel > 255) fractal_pixel = 255;
-          pixel.set(fractal_pixel, fractal_pixel, fractal_pixel, fractal_pixel);
-          
-          ++index;
-        });
+      auto fractal_pixel = fractal(x, y);
+      if (fractal_pixel < 0) fractal_pixel = 0;
+      if (fractal_pixel > 255) fractal_pixel = 255;
+      pixel.set(fractal_pixel, fractal_pixel, fractal_pixel, fractal_pixel);
 
-	Img<ImgFormat::BMP> image2 = image;
+      ++index;
+  });
+
+  Img<ImgFormat::BMP> image2 = image;
 #ifdef DEBUG
-	image.write("fractal_original.bmp");
+  image.write("fractal_original.bmp");
 #endif
 
-	// call standard serial function for correctness check
-	image.fill(gamma_f);
+  // call standard serial function for correctness check
+  image.fill(gamma_f);
 #ifdef DEBUG
-	image.write("fractal_gamma_serial.bmp");
+  image.write("fractal_gamma_serial.bmp");
 #endif
-	auto pixel = image2.data();
-        int image_size = image2.width() * image2.height(); 
+  auto pixel = image2.data();
+  int image_size = image2.width() * image2.height(); 
 
-        #pragma omp target map(pixel[0:image_size])
-	{
-          #pragma omp teams distribute parallel for thread_limit(BLOCK_SIZE)
-          for (int i = 0; i < image_size; i++) {
-              // Lambda to process image with gamma = 2
-              const float v = (0.3f * pixel[i].r + 0.59f * pixel[i].g + 0.11f * pixel[i].b) / 255.0;
-              std::uint8_t gamma_pixel = static_cast<std::uint8_t>(255 * v * v);
-              if (gamma_pixel > 255) gamma_pixel = 255;
-              pixel[i].set(gamma_pixel, gamma_pixel, gamma_pixel, gamma_pixel);
-	  }
-	}
+  #pragma omp target data map(from: pixel[0:image_size])
+  {
+    float total_time = 0.f;
 
-	// check correctness
-	if (check(image.begin(), image.end(), image2.begin())) {
-		std::cout << "Pass\n";
-	} else {
-		std::cout << "Fail\n";
-	}
+    for (int i = 0; i < repeat; i++) {
+      #pragma omp target update to (pixel[0:image_size])
+
+      auto start = std::chrono::steady_clock::now();
+
+      #pragma omp target teams distribute parallel for thread_limit(block_size)
+      for (int i = 0; i < image_size; i++) {
+        // Lambda to process image with gamma = 2
+        const float v = (0.3f * pixel[i].r + 0.59f * pixel[i].g + 0.11f * pixel[i].b) / 255.0;
+        std::uint8_t gamma_pixel = static_cast<std::uint8_t>(255 * v * v);
+        if (gamma_pixel > 255) gamma_pixel = 255;
+        pixel[i].set(gamma_pixel, gamma_pixel, gamma_pixel, gamma_pixel);
+      }
+
+      auto end = std::chrono::steady_clock::now();
+      auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+      total_time += time;
+    }
+    printf("Average kernel execution time %f (s)\n", (total_time * 1e-9f) / repeat);
+  }
+
+  // check correctness
+  if (check(image.begin(), image.end(), image2.begin())) {
+    std::cout << "PASS\n";
+  } else {
+    std::cout << "FAIL\n";
+  }
 
 #ifdef DEBUG
-	image.write("fractal_gamma_parallel.bmp");
+  image.write("fractal_gamma_parallel.bmp");
 #endif
-	return 0;
+  return 0;
 }
