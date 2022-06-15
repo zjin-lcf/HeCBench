92e6fba7eb0a86f2bff2e4be16d8c806f3247e68
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-06-15 11:37:24 -0400

    [hmm] display the total execution time of the loop iterations

--- hmm-cuda/ViterbiGPU.cu
+++ hmm-cuda/ViterbiGPU.cu
@@ -9,17 +9,18 @@
  *
  */
 
+#include <chrono>
 #include <cstdlib>
 #include <cstdio>
 #include <cuda.h>
 
 __global__ void
-viterbi (const float* maxProbOld, 
-         const float* mtState, 
-         const float* mtEmit, 
-         int *obs, 
-         float* maxProbNew, 
-         int* path, 
+viterbi (const float*__restrict__ maxProbOld, 
+         const float*__restrict__ mtState, 
+         const float*__restrict__ mtEmit, 
+                 int*__restrict__ obs, 
+               float*__restrict__ maxProbNew, 
+                 int*__restrict__ path, 
          const int nState,
          const int t)
 {
@@ -42,19 +43,17 @@ viterbi (const float* maxProbOld,
   }
 }
 
-///////////////////////////////////////////////////////////////////////////////
 // Using Viterbi algorithm to search for a Hidden Markov Model for the most
 // probable state path given the observation sequence.
-///////////////////////////////////////////////////////////////////////////////
 int ViterbiGPU(float &viterbiProb,
-    int   *__restrict__ viterbiPath,
-    int   *__restrict__ obs, 
+    int   *viterbiPath,
+    int   *obs, 
     const int nObs, 
-    float *__restrict__ initProb,
-    float *__restrict__ mtState, 
+    float *initProb,
+    float *mtState, 
     const int nState,
     const int nEmit,
-    float *__restrict__ mtEmit)
+    float *mtEmit)
 {
 
   float maxProbNew[nState];
@@ -80,12 +79,21 @@ int ViterbiGPU(float &viterbiProb,
   // initial probability
   cudaMemcpy(d_maxProbOld, initProb, sizeof(float)*nState, cudaMemcpyHostToDevice);
 
+  cudaDeviceSynchronize();
+  auto start = std::chrono::steady_clock::now();
+
   // main iteration of Viterbi algorithm
   for (int t = 1; t < nObs; t++) // for every input observation
   { 
-    viterbi<<<gridDim, blockDim>>> ( d_maxProbOld, d_mtState, d_mtEmit, d_obs, d_maxProbNew, d_path, nState, t);
+    viterbi<<<gridDim, blockDim>>> (d_maxProbOld, d_mtState, d_mtEmit, d_obs, d_maxProbNew, d_path, nState, t);
     cudaMemcpy(d_maxProbOld, d_maxProbNew, sizeof(float)*nState, cudaMemcpyDeviceToDevice);
   }
+
+  cudaDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Device execution time of Viterbi iterations %f (s)\n", time * 1e-9f);
+
   cudaMemcpy(maxProbNew, d_maxProbNew, sizeof(float)*nState, cudaMemcpyDeviceToHost);
   cudaMemcpy(path, d_path, sizeof(int)*(nObs-1)*nState, cudaMemcpyDeviceToHost);
 
