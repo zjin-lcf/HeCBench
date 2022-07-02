110154f51b03eebd8c689564b19018aa0ab16fb8
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-07-02 17:32:48 -0400

    [matrix-rotate] print basic usage message; add an option for kernel execution count; print average kernel execution time

--- matrix-rotate-cuda/main.cu
+++ matrix-rotate-cuda/main.cu
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <cstdio>
+#include <chrono>
 #include <cuda.h>
 
 __global__ void rotate_matrix_parallel (float *matrix, const int n) {
@@ -51,8 +52,13 @@ void rotate_matrix_serial(float *matrix, int n) {
 }
 
 int main(int argc, char** argv) {
-
+  if (argc != 3) {
+    printf("Usage: %s <matrix size> <repeat>\n", argv[0]);
+    return 1;
+  }
   const int n = atoi(argv[1]);
+  const int repeat = atoi(argv[2]);
+
   float *serial_res = (float*) aligned_alloc(1024, n*n*sizeof(float));
   float *parallel_res = (float*) aligned_alloc(1024, n*n*sizeof(float));
 
@@ -60,33 +66,42 @@ int main(int argc, char** argv) {
     for (int j = 0; j < n; j++)
       serial_res[i*n+j] = parallel_res[i*n+j] = i*n+j;
 
+  for (int i = 0; i < repeat; i++) {
+    rotate_matrix_serial(serial_res, n);
+  }
+
   float *d_parallel_res;
   cudaMalloc((void**)&d_parallel_res, n*n*sizeof(float));
   cudaMemcpy(d_parallel_res, parallel_res, n*n*sizeof(float), cudaMemcpyHostToDevice);
 
-  for (int i = 0; i < 100; i++) {
-    rotate_matrix_serial(serial_res, n);
+  cudaDeviceSynchronize();
+  auto start = std::chrono::steady_clock::now();
+  
+  for (int i = 0; i < repeat; i++) {
     rotate_matrix_parallel<<<(n/2+255)/256, 256>>>(d_parallel_res, n);
   }
+
+  cudaDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average kernel execution time: %f (s)\n", (time * 1e-9f) / repeat);
+
   cudaMemcpy(parallel_res, d_parallel_res, n*n*sizeof(float), cudaMemcpyDeviceToHost);
 
-  int errors = 0;
+  bool ok = true;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       if (serial_res[i*n+j] != parallel_res[i*n+j]) {
-        errors++; 
+        ok = false;
         break;
       }
     }
   }
-  if (errors) 
-    printf("fail\n");
-  else 
-    printf("success\n");
+
+  printf("%s\n", ok ? "PASS" : "FAIL");
 
   free(serial_res);
   free(parallel_res);
   cudaFree(d_parallel_res);
   return 0;
 }
-
