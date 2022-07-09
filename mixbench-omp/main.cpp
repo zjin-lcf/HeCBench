d9a88f885849303f33b70428232efb2356e857ad
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-07-08 22:25:09 -0400

    [mixbench] [mixbench] add an option for kernel execution count; add warmup run; print total kernel execution time; factor out the kernel code as a function in sycl and omp programs

--- mixbench-omp/main.cpp
+++ mixbench-omp/main.cpp
@@ -1,5 +1,5 @@
 /**
- * main-omp.cpp: This file is the modified read-only mixbench GPU micro-benchmark suite.
+ * This file is the modified read-only mixbench GPU micro-benchmark suite.
  *
  **/
 
@@ -7,14 +7,51 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <math.h>
+#include <chrono>
 #include <omp.h>
 
 #define VECTOR_SIZE (8*1024*1024)
 #define granularity (8)
 #define fusion_degree (4)
-#define seed 0.1
+#define seed 0.1f
 
-void mixbenchGPU(long size){
+void benchmark_func(float *cd, int grid_dim, int block_dim, int compute_iterations) { 
+      
+  #pragma omp target teams num_teams(grid_dim) thread_limit(block_dim)
+  { 
+    #pragma omp parallel 
+    {
+      const unsigned int blockSize = block_dim;
+      const int stride = blockSize;
+      int idx = omp_get_team_num()*blockSize*granularity + omp_get_thread_num();
+      const int big_stride = omp_get_num_teams()*blockSize*granularity;
+      float tmps[granularity];
+      for(int k=0; k<fusion_degree; k++) {
+        #pragma unroll
+        for(int j=0; j<granularity; j++) {
+          // Load elements (memory intensive part)
+          tmps[j] = cd[idx+j*stride+k*big_stride];
+
+          // Perform computations (compute intensive part)
+          for(int i=0; i<compute_iterations; i++)
+            tmps[j] = tmps[j]*tmps[j]+(float)seed;
+        }
+
+        // Multiply add reduction
+        float sum = 0;
+        #pragma unroll
+        for(int j=0; j<granularity; j+=2)
+          sum += tmps[j]*tmps[j+1];
+
+        #pragma unroll
+        for(int j=0; j<granularity; j++)
+          cd[idx+k*big_stride] = sum;
+      }
+    }
+  }
+}
+
+void mixbenchGPU(long size, int repeat) {
   const char *benchtype = "compute with global memory (block strided)";
   printf("Trade-off type:%s\n", benchtype);
   float *cd = (float*) malloc (size*sizeof(float));
@@ -24,59 +61,52 @@ void mixbenchGPU(long size){
   const int block_dim = 256;
   const int grid_dim = reduced_grid_size/block_dim;
 
-#pragma omp target data map(tofrom: cd[0:size]) 
+  #pragma omp target data map(tofrom: cd[0:size]) 
   {
-    for (int compute_iterations = 0; compute_iterations < 2048; compute_iterations++) {
-#pragma omp target teams num_teams(grid_dim) thread_limit(block_dim)
-      { 
-#pragma omp parallel 
-        {
-          const unsigned int blockSize = block_dim;
-          const int stride = blockSize;
-          int idx = omp_get_team_num()*blockSize*granularity + omp_get_thread_num();
-          const int big_stride = omp_get_num_teams()*blockSize*granularity;
-          float tmps[granularity];
-          for(int k=0; k<fusion_degree; k++){
-#pragma unroll
-            for(int j=0; j<granularity; j++){
-              // Load elements (memory intensive part)
-              tmps[j] = cd[idx+j*stride+k*big_stride];
-              // Perform computations (compute intensive part)
-              for(int i=0; i<compute_iterations; i++){
-                tmps[j] = tmps[j]*tmps[j]+(float)seed;
-              }
-            }
-            // Multiply add reduction
-            float sum = 0;
-#pragma unroll
-            for(int j=0; j<granularity; j+=2)
-              sum += tmps[j]*tmps[j+1];
-#pragma unroll
-            for(int j=0; j<granularity; j++)
-              cd[idx+k*big_stride] = sum;
-          }
-        }
-      }
+    // warmup
+    for (int i = 0; i < repeat; i++) {
+      benchmark_func(cd, grid_dim, block_dim, i);
     }
+
+    auto start = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < repeat; i++) {
+      benchmark_func(cd, grid_dim, block_dim, i);
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Total kernel execution time: %f (s)\n", time * 1e-9f);
   }
 
   // verification
-  for (int i = 0; i < size; i++) 
+  bool ok = true;
+  for (int i = 0; i < size; i++) {
     if (cd[i] != 0) {
-      if (fabsf(cd[i] - 0.050807f) > 1e-6f)
+      if (fabsf(cd[i] - 0.050807f) > 1e-6f) {
+        ok = false;
         printf("Verification failed at index %d: %f\n", i, cd[i]);
+        break;
+      }
     }
+  }
+  printf("%s\n", ok ? "PASS" : "FAIL");
+
   free(cd);
 }
 
-
 int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int repeat = atoi(argv[1]);
 
   unsigned int datasize = VECTOR_SIZE*sizeof(float);
 
   printf("Buffer size: %dMB\n", datasize/(1024*1024));
 
-  mixbenchGPU(VECTOR_SIZE);
+  mixbenchGPU(VECTOR_SIZE, repeat);
 
   return 0;
 }
