3eb2bdc6cad6ae269e24d9b287501a50e912b59a
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-07-08 22:25:09 -0400

    [mixbench] [mixbench] add an option for kernel execution count; add warmup run; print total kernel execution time; factor out the kernel code as a function in sycl and omp programs

--- mixbench-cuda/main.cu
+++ mixbench-cuda/main.cu
@@ -1,5 +1,5 @@
 /**
- * main-omp.cpp: This file is the modified read-only mixbench GPU micro-benchmark suite.
+ * This file is the modified read-only mixbench GPU micro-benchmark suite.
  *
  **/
 
@@ -7,80 +7,108 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <math.h>
+#include <chrono>
 #include <cuda.h>
 
 #define VECTOR_SIZE (8*1024*1024)
 #define granularity (8)
 #define fusion_degree (4)
-#define seed 0.1
+#define seed 0.1f
 
-__global__ void benchmark_func(float *g_data, const int blockdim, const int compute_iterations) {
-	const unsigned int blockSize = blockdim;
-	const int stride = blockSize;
-	int idx = blockIdx.x*blockSize*granularity + threadIdx.x;
-	const int big_stride = gridDim.x*blockSize*granularity;
+__global__ void benchmark_func(float *g_data, const int blockdim,
+                               const int compute_iterations)
+{
+  const unsigned int blockSize = blockdim;
+  const int stride = blockSize;
+  int idx = blockIdx.x*blockSize*granularity + threadIdx.x;
+  const int big_stride = gridDim.x*blockSize*granularity;
 
-	float tmps[granularity];
-	for(int k=0; k<fusion_degree; k++){
-		#pragma unroll
-		for(int j=0; j<granularity; j++){
-			// Load elements (memory intensive part)
-			tmps[j] = g_data[idx+j*stride+k*big_stride];
-			// Perform computations (compute intensive part)
-			for(int i=0; i<compute_iterations; i++){
-				tmps[j] = tmps[j]*tmps[j]+(float)seed;
-			}
-		}
-		// Multiply add reduction
-		float sum = 0;
-		#pragma unroll
-		for(int j=0; j<granularity; j+=2)
-			sum += tmps[j]*tmps[j+1];
-	  #pragma unroll
-		for(int j=0; j<granularity; j++)
-			g_data[idx+k*big_stride] = sum;
-	}
+  float tmps[granularity];
+  for(int k=0; k<fusion_degree; k++) {
+    #pragma unroll
+    for(int j=0; j<granularity; j++) {
+      // Load elements (memory intensive part)
+      tmps[j] = g_data[idx+j*stride+k*big_stride];
+
+      // Perform computations (compute intensive part)
+      for(int i=0; i<compute_iterations; i++)
+        tmps[j] = tmps[j]*tmps[j]+seed;
+    }
+
+    // Multiply add reduction
+    float sum = 0.f;
+    #pragma unroll
+    for(int j=0; j<granularity; j+=2)
+      sum += tmps[j]*tmps[j+1];
+
+    #pragma unroll
+    for(int j=0; j<granularity; j++)
+      g_data[idx+k*big_stride] = sum;
+  }
 }
 
-
-void mixbenchGPU(long size){
-	const char *benchtype = "compute with global memory (block strided)";
-	printf("Trade-off type:%s\n", benchtype);
-	float *cd = (float*) malloc (size*sizeof(float));
+void mixbenchGPU(long size, int repeat) {
+  const char *benchtype = "compute with global memory (block strided)";
+  printf("Trade-off type:%s\n", benchtype);
+  float *cd = (float*) malloc (size*sizeof(float));
   for (int i = 0; i < size; i++) cd[i] = 0;
 
-	const long reduced_grid_size = size/granularity/128;
-	const int block_dim = 256;
-	const int grid_dim = reduced_grid_size/block_dim;
+  const long reduced_grid_size = size/granularity/128;
+  const int block_dim = 256;
+  const int grid_dim = reduced_grid_size/block_dim;
 
   float *d_cd;
-	cudaMalloc((void**)&d_cd, size*sizeof(float));
+  cudaMalloc((void**)&d_cd, size*sizeof(float));
   cudaMemcpy(d_cd, cd,  size*sizeof(float), cudaMemcpyHostToDevice);
 
-  for (int compute_iterations = 0; compute_iterations < 2048; compute_iterations++) {
-    benchmark_func<<<grid_dim, block_dim>>>(d_cd, block_dim, compute_iterations);
+  // warmup
+  for (int i = 0; i < repeat; i++) {
+    benchmark_func<<<grid_dim, block_dim>>>(d_cd, block_dim, i);
   }
+
+  cudaDeviceSynchronize();
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    benchmark_func<<<grid_dim, block_dim>>>(d_cd, block_dim, i);
+  }
+
+  cudaDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Total kernel execution time: %f (s)\n", time * 1e-9f);
+
   cudaMemcpy(cd, d_cd, size*sizeof(float), cudaMemcpyDeviceToHost);
 
   // verification
-  for (int i = 0; i < size; i++) 
+  bool ok = true;
+  for (int i = 0; i < size; i++) {
     if (cd[i] != 0) {
-      if (fabsf(cd[i] - 0.050807f) > 1e-6f)
+      if (fabsf(cd[i] - 0.050807f) > 1e-6f) {
+        ok = false;
         printf("Verification failed at index %d: %f\n", i, cd[i]);
+        break;
+      }
     }
+  }
+  printf("%s\n", ok ? "PASS" : "FAIL");
 
   free(cd);
   cudaFree(d_cd);
 }
 
-
 int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int repeat = atoi(argv[1]);
 
-	unsigned int datasize = VECTOR_SIZE*sizeof(float);
+  unsigned int datasize = VECTOR_SIZE*sizeof(float);
 
-	printf("Buffer size: %dMB\n", datasize/(1024*1024));
-	
-	mixbenchGPU(VECTOR_SIZE);
+  printf("Buffer size: %dMB\n", datasize/(1024*1024));
 
-	return 0;
+  mixbenchGPU(VECTOR_SIZE, repeat);
+
+  return 0;
 }
--- mixbench-sycl/main.cpp
+++ mixbench-sycl/main.cpp
@@ -12,10 +12,39 @@
 #define VECTOR_SIZE (8*1024*1024)
 #define granularity (8)
 #define fusion_degree (4)
-#define seed 0.1
+#define seed 0.1f
 
+void benchmark_func(nd_item<1> &item,
+                    float *g_data, const int block_dim,
+                    const int compute_iterations)
+{
+  const unsigned int blockSize = block_dim;
+  const int stride = blockSize;
+  int idx = item.get_group(0)*blockSize*granularity + item.get_local_id(0);
+  const int big_stride = item.get_group_range(0)*blockSize*granularity;
 
-void mixbenchGPU(long size){
+  float tmps[granularity];
+  for(int k=0; k<fusion_degree; k++){
+    #pragma unroll
+    for(int j=0; j<granularity; j++){
+      // Load elements (memory intensive part)
+      tmps[j] = g_data[idx+j*stride+k*big_stride];
+      // Perform computations (compute intensive part)
+      for(int i=0; i<compute_iterations; i++)
+        tmps[j] = tmps[j]*tmps[j]+(float)seed;
+    }
+    // Multiply add reduction
+    float sum = 0;
+    #pragma unroll
+    for(int j=0; j<granularity; j+=2)
+      sum += tmps[j]*tmps[j+1];
+    #pragma unroll
+    for(int j=0; j<granularity; j++)
+      g_data[idx+k*big_stride] = sum;
+  }
+}
+
+void mixbenchGPU(long size, int repeat) {
   const char *benchtype = "compute with global memory (block strided)";
   printf("Trade-off type:%s\n", benchtype);
   float *cd = (float*) malloc (size*sizeof(float));
@@ -38,43 +67,36 @@ void mixbenchGPU(long size){
     range<1> gws (grid_dim);
     range<1> lws (block_dim);
 
-    for (int compute_iterations = 0; compute_iterations < 2048; compute_iterations++) {
+    for (int i = 0; i < repeat; i++) {
       q.submit([&](handler &h) {
         auto g_data = d_cd.get_access<sycl_read_write>(h);
-        h.parallel_for<class mixbench>(nd_range<1>(gws, lws), [=](nd_item<1> item) {
-          const unsigned int blockSize = block_dim;
-          const int stride = blockSize;
-          int idx = item.get_group(0)*blockSize*granularity + item.get_local_id(0);
-          const int big_stride = item.get_group_range(0)*blockSize*granularity;
-
-          float tmps[granularity];
-          for(int k=0; k<fusion_degree; k++){
-            #pragma unroll
-            for(int j=0; j<granularity; j++){
-              // Load elements (memory intensive part)
-              tmps[j] = g_data[idx+j*stride+k*big_stride];
-              // Perform computations (compute intensive part)
-              for(int i=0; i<compute_iterations; i++)
-                tmps[j] = tmps[j]*tmps[j]+(float)seed;
-            }
-            // Multiply add reduction
-            float sum = 0;
-            #pragma unroll
-            for(int j=0; j<granularity; j+=2)
-              sum += tmps[j]*tmps[j+1];
-            #pragma unroll
-            for(int j=0; j<granularity; j++)
-              g_data[idx+k*big_stride] = sum;
-          }
+        h.parallel_for<class mixbench_warmup>(nd_range<1>(gws, lws), [=](nd_item<1> item) {
+          benchmark_func(item, g_data.get_pointer(), block_dim, i);
         });
       });
     }
     q.wait();
+
+    auto start = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < repeat; i++) {
+      q.submit([&](handler &h) {
+        auto g_data = d_cd.get_access<sycl_read_write>(h);
+        h.parallel_for<class mixbench_timing>(nd_range<1>(gws, lws), [=](nd_item<1> item) {
+          benchmark_func(item, g_data.get_pointer(), block_dim, i);
+        });
+      });
+    }
+
+    q.wait();
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Total kernel execution time: %f (s)\n", time * 1e-9f);
   }
 
   // verification
   bool ok = true;
-  for (int i = 0; i < size; i++) 
+  for (int i = 0; i < size; i++) {
     if (cd[i] != 0) {
       if (fabsf(cd[i] - 0.050807f) > 1e-6f) {
         ok = false;
@@ -82,18 +104,25 @@ void mixbenchGPU(long size){
         break;
       }
     }
+  }
+
   free(cd);
   printf("%s\n", ok ? "PASS" : "FAIL");
 }
 
 
 int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int repeat = atoi(argv[1]);
 
   unsigned int datasize = VECTOR_SIZE*sizeof(float);
 
   printf("Buffer size: %dMB\n", datasize/(1024*1024));
 
-  mixbenchGPU(VECTOR_SIZE);
+  mixbenchGPU(VECTOR_SIZE, repeat);
 
   return 0;
 }
