bc2e84d5883b9199581e1fa6e731613c8373dee5
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-07-15 15:03:05 -0400

    [qtclustering] print the total execution time of three major kernels

--- qtclustering-hip/QTC.cu
+++ qtclustering-hip/QTC.cu
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <chrono>
 #include <sstream>
 #include <iostream>
 #include <fstream>
@@ -67,8 +68,6 @@ void RunBenchmark(OptionParser &op){
   runTest("QTC", op);
 }
 
-
-
 // ****************************************************************************
 // Function: calculate_participants
 //
@@ -91,7 +90,10 @@ void RunBenchmark(OptionParser &op){
 // Creation: May 25, 2011
 //
 // ****************************************************************************
-void calculate_participants(int point_count, int node_count, int cwrank, int *thread_block_count, int *total_thread_block_count, int *active_node_count){
+void calculate_participants(int point_count, int node_count, int cwrank,
+                            int *thread_block_count,
+                            int *total_thread_block_count,
+                            int *active_node_count){
 
   int ac_nd_cnt, thr_blc_cnt, total_thr_blc_cnt;
 
@@ -152,8 +154,6 @@ void runTest(const string& name, OptionParser& op)
 
 }
 
-////////////////////////////////////////////////////////////////////////////////
-//
 void QTC(const string& name, OptionParser& op, int matrix_type){
   ofstream debug_out, seeds_out;
   void *Ai_mask, *cardnl, *ungrpd_pnts_indr, *clustered_pnts_mask, *result, *dist_to_clust;
@@ -162,9 +162,10 @@ void QTC(const string& name, OptionParser& op, int matrix_type){
   bool save_clusters = false;
   bool be_verbose = false;
   void *distance_matrix_gmem, *distance_matrix;
-  float *dist_source, *pnts;
+  float *dist_source, *pnts = NULL;
   float threshold = 1.0f;
-  int i, max_degree, thread_block_count, total_thread_block_count, active_node_count;
+  unsigned long int i;
+  int max_degree, thread_block_count, total_thread_block_count, active_node_count;
   int cwrank=0, node_count=1, tpb, max_card, iter=0;
   unsigned long int dst_matrix_elems, point_count, max_point_count;
 
@@ -232,7 +233,7 @@ void QTC(const string& name, OptionParser& op, int matrix_type){
   calculate_participants(point_count, node_count, cwrank, &thread_block_count, &total_thread_block_count, &active_node_count);
 
   ungrpd_pnts_indr_host = (int*) malloc (sizeof(int)*point_count);
-  for(int i=0; i<point_count; i++){
+  for(i=0; i<point_count; i++){
     ungrpd_pnts_indr_host[i] = i;
   }
 
@@ -264,7 +265,7 @@ void QTC(const string& name, OptionParser& op, int matrix_type){
   hipMemset(dist_to_clust, 0, max_degree*thread_block_count*sizeof(float));
 
   tpb = ( point_count > THREADSPERBLOCK )? THREADSPERBLOCK : point_count;
-  hipLaunchKernelGGL(compute_degrees, dim3(thread_block_count), dim3(tpb), 0, 0, (int *)indr_mtrx, (int *)degrees, point_count, max_degree);
+  hipLaunchKernelGGL(compute_degrees, thread_block_count, tpb, 0, 0, (int *)indr_mtrx, (int *)degrees, point_count, max_degree);
   hipDeviceSynchronize();
   CHECK_CUDA_ERROR();
 
@@ -291,10 +292,8 @@ void QTC(const string& name, OptionParser& op, int matrix_type){
 
   distance_matrix = distance_matrix_gmem;
 
-  //////////////////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////////////////
-  //
   // Kernel execution
+  double qtc_time = 0.0, trim_time = 0.0, update_time = 0.0;
 
   do{
     stringstream ss;
@@ -315,18 +314,20 @@ void QTC(const string& name, OptionParser& op, int matrix_type){
       break;
     cwrank = comm_get_rank();
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    ///////// -----------------               Main kernel                ----------------- /////////
-    hipLaunchKernelGGL(QTC_device, dim3(thread_block_count), dim3(tpb), 0, 0, (float*)distance_matrix, (char *)Ai_mask, (char *)clustered_pnts_mask,
+    auto start = std::chrono::steady_clock::now();
+
+    // Main kernel
+    hipLaunchKernelGGL(QTC_device, thread_block_count, tpb, 0, 0, (float*)distance_matrix, (char *)Ai_mask, (char *)clustered_pnts_mask,
         (int *)indr_mtrx, (int *)cardnl, (int *)ungrpd_pnts_indr,
         (float *)dist_to_clust, (int *)degrees, point_count, max_point_count,
         max_degree, threshold, cwrank, active_node_count,
         total_thread_block_count);
-    ///////// -----------------               Main kernel                ----------------- /////////
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    hipDeviceSynchronize();
-    CHECK_CUDA_ERROR();
 
+    hipDeviceSynchronize();
+    auto end = std::chrono::steady_clock::now();
+    qtc_time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+    CHECK_CUDA_ERROR();
 
 #ifdef DEBUG
     printf("cardinalities\n");
@@ -335,10 +336,9 @@ void QTC(const string& name, OptionParser& op, int matrix_type){
      printf("%d %d\n", i, cardinalities_debug[i]);
 #endif
 
-
     if( thread_block_count > 1 ){
       // We are reducing 128 numbers or less, so one thread should be sufficient.
-      hipLaunchKernelGGL(reduce_card_device, dim3(1), dim3(1), 0, 0, (int *)cardnl, thread_block_count);
+      hipLaunchKernelGGL(reduce_card_device, 1, 1, 0, 0, (int *)cardnl, thread_block_count);
       hipDeviceSynchronize();
       CHECK_CUDA_ERROR();
     }
@@ -355,12 +355,17 @@ void QTC(const string& name, OptionParser& op, int matrix_type){
       cout << "[" << cwrank << "] Cluster Cardinality: " << max_card << " (Node: " << cwrank << ", index: " << winner_index << ")" << endl;
     }
 
-    hipLaunchKernelGGL(trim_ungrouped_pnts_indr_array, dim3(1), dim3(tpb), 0, 0, winner_index, (int*)ungrpd_pnts_indr, (float*)distance_matrix,
+    start = std::chrono::steady_clock::now();
+
+    hipLaunchKernelGGL(trim_ungrouped_pnts_indr_array, 1, tpb, 0, 0, winner_index, (int*)ungrpd_pnts_indr, (float*)distance_matrix,
         (int *)result, (char *)Ai_mask, (char *)clustered_pnts_mask,
         (int *)indr_mtrx, (int *)cardnl, (float *)dist_to_clust, (int *)degrees,
         point_count, max_point_count, max_degree, threshold);
     hipDeviceSynchronize();
     CHECK_CUDA_ERROR();
+
+    end = std::chrono::steady_clock::now();
+    trim_time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
 
     if( cwrank == winner_node){ // for non-parallel cases, these should both be zero.
       if( save_clusters ){
@@ -379,12 +384,16 @@ void QTC(const string& name, OptionParser& op, int matrix_type){
       }
     }
 
-    hipLaunchKernelGGL(update_clustered_pnts_mask, dim3(1), dim3(tpb), 0, 0, (char *)clustered_pnts_mask, (char *)Ai_mask, max_point_count);
+    start = std::chrono::steady_clock::now();
+
+    hipLaunchKernelGGL(update_clustered_pnts_mask, 1, tpb, 0, 0, (char *)clustered_pnts_mask, (char *)Ai_mask, max_point_count);
     hipDeviceSynchronize();
     CHECK_CUDA_ERROR();
 
+    end = std::chrono::steady_clock::now();
+    update_time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
     point_count -= max_card;
-   //break;
 
   }while( max_card > 1 && point_count );
 
@@ -392,11 +401,14 @@ void QTC(const string& name, OptionParser& op, int matrix_type){
   if( save_clusters ){
     seeds_out.close();
   }
-  //
-  ////////////////////////////////////////////////////////////////////////////////
 
   if( cwrank == 0){
     cout << "QTC is complete. Clustering iteration count: " << iter << endl;
+    cout << "\nKernel execution time\n";
+    cout << "qtc: " << qtc_time * 1e-9f << " (s)\n";
+    cout << "trim: " << trim_time * 1e-9f << " (s)\n";
+    cout << "update: " << update_time * 1e-9f << " (s)\n";
+    cout << "total: " << (qtc_time + trim_time + update_time) * 1e-9f << " (s)\n";
     cout.flush();
   }
 
@@ -417,37 +429,27 @@ void QTC(const string& name, OptionParser& op, int matrix_type){
   return;
 }
 
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-  void
-allocDeviceBuffer(void** bufferp, unsigned long bytes)
+void allocDeviceBuffer(void** bufferp, unsigned long bytes)
 {
   hipMalloc(bufferp, bytes);
   CHECK_CUDA_ERROR();
 }
 
-  void
-freeDeviceBuffer(void* buffer)
+void freeDeviceBuffer(void* buffer)
 {
   hipFree(buffer);
 }
 
-  void
-copyToDevice(void* to_device, void* from_host, unsigned long bytes)
+void copyToDevice(void* to_device, void* from_host, unsigned long bytes)
 {
   hipMemcpy(to_device, from_host, bytes, hipMemcpyHostToDevice);
   CHECK_CUDA_ERROR();
 }
 
-
-  void
-copyFromDevice(void* to_host, void* from_device, unsigned long bytes)
+void copyFromDevice(void* to_host, void* from_device, unsigned long bytes)
 {
   hipMemcpy(to_host, from_device, bytes, hipMemcpyDeviceToHost);
   CHECK_CUDA_ERROR();
 }
-
