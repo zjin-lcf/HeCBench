8409710b5aed6ba309caf253f329e98221bfe69e
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-07-16 13:03:19 -0400

    [reaction] print basic usage message; update the kernel arguments

--- reaction-omp/main.cpp
+++ reaction-omp/main.cpp
@@ -6,8 +6,12 @@
 #include "kernels.cpp"
 
 int main(int argc, char* argv[]) {
-
+  if (argc != 2) {
+    printf("Usage: %s <timesteps>\n", argv[0]);
+    return 1;
+  }
   unsigned int timesteps = atoi(argv[1]);
+
   unsigned int mx = 128;
   unsigned int my = 128;
   unsigned int mz = 128;
