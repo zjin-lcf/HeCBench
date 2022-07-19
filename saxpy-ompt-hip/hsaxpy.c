7a6e0badd34482a4bc2da296f5024a7d740993d0
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2022-07-19 11:14:07 -0400

    [saxpy-ompt] format the sources slightly

--- saxpy-ompt-hip/hsaxpy.c
+++ saxpy-ompt-hip/hsaxpy.c
@@ -34,13 +34,13 @@ void hsaxpy(const int n,
 /*
  * - naive implementation
  */
-clock_gettime(CLOCK_REALTIME, rt + 0);
-#pragma omp parallel for simd schedule(simd:static) \
+  clock_gettime(CLOCK_REALTIME, rt + 0);
+  #pragma omp parallel for simd schedule(simd:static) \
   default(none) shared(a, n, x, y)
   for (int i = 0; i < n; i++) {
     y[i] = a * x[i] + y[i];
   }
-clock_gettime(CLOCK_REALTIME, rt + 1);
+  clock_gettime(CLOCK_REALTIME, rt + 1);
   if (wtcalc >= 0.0) {
     wtcalc += (rt[1].tv_sec - rt[0].tv_sec) + 1.0e-9 * (rt[1].tv_nsec - rt[0].tv_nsec);
   }
