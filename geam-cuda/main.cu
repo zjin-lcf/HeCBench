8772fb70fd987518c85f9ac1e2daa0595230e818
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2022-09-03 10:19:00 -0400

    [geam] remove the matrix row and column swap for each repeat; measure the execution time of the single- and double-precision matrix transpose blas extension functions on a device using the chrono library

--- geam-cuda/main.cu
+++ geam-cuda/main.cu
@@ -1,97 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
 #include <string.h>
-#include <utility>
+#include <chrono>
 #include <cublas_v2.h>
 
-#define uS_PER_SEC 1000000
-#define uS_PER_mS 1000
-
-int main(int argc, char* argv[]) {
-  if (argc != 4) {
-    printf("Usage %s <matrix col> <matrix row> <repeat>\n", argv[0]);
-    return 1;
-  }
-
-  int M = atoi(argv[1]); 
-  int N = atoi(argv[2]); 
-  const int repeat = atoi(argv[3]); 
-  if (M <= 0 || N <= 0 || repeat < 0) {
-    printf("Error: invalid inputs\n");
-    return 1;
-  }
-
+template <typename T>
+void transpose_f64(int nrow, int ncol, int repeat) {
   int error = 0;
-  const size_t size = N * M;
-  const size_t size_byte = size * sizeof(float);
+  const size_t size = nrow * ncol;
+  const size_t size_byte = size * sizeof(T);
 
-  // N rows and M columns
-  float *matrix = (float *) malloc (size_byte);
+  T *matrix = (T *) malloc (size_byte);
   for (size_t i = 0; i < size; i++) matrix[i] = rand() % 13; 
 
-  timeval t1, t2;
-  gettimeofday(&t1, NULL);
+  T *matrixT = (T *) malloc (size_byte);
 
-  // M rows and N columns
-  float *matrixT = (float *) malloc (size_byte);
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < M; j++)
-      matrixT[(j*N)+i] = matrix[(i*M)+j];
+  auto start = std::chrono::steady_clock::now();
 
-  gettimeofday(&t2, NULL);
-  float et1 = (((t2.tv_sec*uS_PER_SEC)+t2.tv_usec) - ((t1.tv_sec*uS_PER_SEC)+t1.tv_usec))/(float)uS_PER_mS;
-  printf("CPU time = %fms\n", et1);
+  for (int i = 0; i < nrow; i++)
+    for (int j = 0; j < ncol; j++)
+      matrixT[(j*nrow)+i] = matrix[(i*ncol)+j];
 
+  auto end = std::chrono::steady_clock::now();
+  double time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("serial transpose time = %lf (ms)\n", time * 1e-6f);
+
+  cublasStatus_t stat;
   cublasHandle_t handle;
   cublasCreate(&handle);
 
-  const float alpha = 1.f;
-  const float beta  = 0.f;
+  const T alpha = (T)1;
+  const T beta  = (T)0;
 
   // store host and device results
-  float *h_matrixT , *d_matrixT , *d_matrix;
-  h_matrixT = (float *) malloc (size_byte);
-  cudaMalloc((void**)&d_matrixT , size_byte);
-  cudaMalloc((void**)&d_matrix , size_byte);
+  T *h_matrixT, *d_matrixT, *d_matrix;
+  h_matrixT = (T *) malloc (size_byte);
 
-  cudaMemcpy(d_matrix , matrix , size_byte, cudaMemcpyHostToDevice);
+  cudaMalloc((void**)&d_matrix, size_byte);
+  cudaMemcpy(d_matrix, matrix, size_byte, cudaMemcpyHostToDevice);
 
-  // start the device timing
-  gettimeofday(&t1, NULL);
+  cudaMalloc((void**)&d_matrixT, size_byte);
+
+  time = 0.0;
 
   for (int i = 0; i < repeat; i++) {
-    auto status = cublasSgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, N, M, &alpha, 
-                              d_matrix, M, &beta, d_matrix, N, d_matrixT, N);
 
-    if (status != CUBLAS_STATUS_SUCCESS) {
+    start = std::chrono::steady_clock::now();
+    stat = cublasDgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N,
+                       nrow, ncol,
+                       &alpha, d_matrix, ncol,
+                       &beta, d_matrix, nrow,
+                       d_matrixT, nrow);
+    cudaDeviceSynchronize(); // required for timing correctness
+    end = std::chrono::steady_clock::now();
+    time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+    if (stat != CUBLAS_STATUS_SUCCESS) {
       error = 1;
-      printf("Error: cublasSgeam failed to complete\n"):
+      printf("Error: cublasDgeam failed to complete\n");
       break;
     }
-
-    std::swap(d_matrix, d_matrixT);
-    std::swap(N, M);
   }
 
-  gettimeofday(&t2, NULL);
-  float et2 = (((t2.tv_sec*uS_PER_SEC)+t2.tv_usec) - 
-               ((t1.tv_sec*uS_PER_SEC)+t1.tv_usec)) / (float)uS_PER_mS;
+  printf("Average device transpose time = %lf (us)\n", (time * 1e-3f) / repeat);
 
-  printf("Average device execution time = %fms\n", et2 / repeat);
-
-  cudaMemcpy(h_matrixT , d_matrix , size_byte , cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_matrixT, d_matrixT, size_byte, cudaMemcpyDeviceToHost);
 
   cudaFree(d_matrix);
   cudaFree(d_matrixT);
 
-  if (error == 0) {
-
-    // check host and device results
-    if (repeat % 2)
-      error = memcmp(h_matrixT, matrixT, size_byte);
-    else
-      error = memcmp(h_matrixT, matrix, size_byte);
+  if (error == 0) { // check host and device results
+    error = memcmp(h_matrixT, matrixT, size_byte);
   }
 
   printf("%s\n", error ? "FAIL" : "PASS");
@@ -99,5 +78,103 @@ int main(int argc, char* argv[]) {
   cublasDestroy(handle);
   free(matrixT);
   free(matrix);
+}
+
+template <typename T>
+void transpose_f32(int nrow, int ncol, int repeat) {
+  int error = 0;
+  const size_t size = nrow * ncol;
+  const size_t size_byte = size * sizeof(T);
+
+  T *matrix = (T *) malloc (size_byte);
+  for (size_t i = 0; i < size; i++) matrix[i] = rand() % 13; 
+
+  T *matrixT = (T *) malloc (size_byte);
+
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < nrow; i++)
+    for (int j = 0; j < ncol; j++)
+      matrixT[(j*nrow)+i] = matrix[(i*ncol)+j];
+
+  auto end = std::chrono::steady_clock::now();
+  double time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("serial transpose time = %lf (ms)\n", time * 1e-6f);
+
+  cublasStatus_t stat;
+  cublasHandle_t handle;
+  cublasCreate(&handle);
+
+  const T alpha = (T)1;
+  const T beta  = (T)0;
+
+  // store host and device results
+  T *h_matrixT, *d_matrixT, *d_matrix;
+  h_matrixT = (T *) malloc (size_byte);
+  cudaMalloc((void**)&d_matrix, size_byte);
+  cudaMemcpy(d_matrix, matrix, size_byte, cudaMemcpyHostToDevice);
+
+  cudaMalloc((void**)&d_matrixT, size_byte);
+
+  time = 0.0;
+
+  for (int i = 0; i < repeat; i++) {
+    start = std::chrono::steady_clock::now();
+    stat = cublasSgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N,
+                       nrow, ncol,
+                       &alpha, d_matrix, ncol,
+                       &beta, d_matrix, nrow,
+                       d_matrixT, nrow);
+    cudaDeviceSynchronize(); // required for timing correctness
+    end = std::chrono::steady_clock::now();
+    time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+    if (stat != CUBLAS_STATUS_SUCCESS) {
+      error = 1;
+      printf("Error: cublasSgeam failed to complete\n");
+      break;
+    }
+  }
+
+  printf("Average device transpose time = %lf (us)\n", (time * 1e-3f) / repeat);
+
+  cudaMemcpy(h_matrixT, d_matrixT, size_byte, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_matrix);
+  cudaFree(d_matrixT);
+
+  if (error == 0) { // check host and device results
+    error = memcmp(h_matrixT, matrixT, size_byte);
+  }
+
+  printf("%s\n", error ? "FAIL" : "PASS");
+
+  cublasDestroy(handle);
+  free(matrixT);
+  free(matrix);
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 4) {
+    printf("Usage %s <matrix row> <matrix col> <repeat>\n", argv[0]);
+    return 1;
+  }
+
+  int nrow = atoi(argv[1]); 
+  int ncol = atoi(argv[2]); 
+  const int repeat = atoi(argv[3]); 
+  if (nrow <= 0 || ncol <= 0 || repeat < 0) {
+    printf("Error: invalid inputs\n");
+    return 1;
+  }
+
+  printf("----------------\nFP32 transpose matrix (%d x %d)\n----------------\n",
+         nrow, ncol);
+  transpose_f32<float>(nrow, ncol, repeat);
+
+  printf("----------------\nFP64 transpose matrix (%d x %d)\n----------------\n",
+         nrow, ncol);
+  transpose_f64<double>(nrow, ncol, repeat);
+
   return 0;
 }
