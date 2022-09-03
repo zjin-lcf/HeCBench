fc1efc3ff9d6eabbdd03b510cb1e4d4ed263a475
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2022-09-03 10:19:00 -0400

    [geam] remove the matrix row and column swap for each repeat; measure the execution time of the single- and double-precision matrix transpose blas extension functions on a device using the chrono library

--- geam-hip/main.cu
+++ geam-hip/main.cu
@@ -1,100 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
 #include <string.h>
-#include <utility>
+#include <chrono>
 #include <hipblas.h>
 
-#define uS_PER_SEC 1000000
-#define uS_PER_mS 1000
-
-int main(int argc, char* argv[]) {
-  if (argc != 4) {
-    printf("Usage %s <matrix col> <matrix row> <repeat>\n", argv[0]);
-    return 1;
-  }
-
-  int M = atoi(argv[1]); 
-  int N = atoi(argv[2]); 
-  const int repeat = atoi(argv[3]); 
-  if (M <= 0 || N <= 0 || repeat < 0) {
-    printf("Error: invalid inputs\n");
-    return 1;
-  }
-
+template <typename T>
+void transpose_f64(int nrow, int ncol, int repeat) {
   int error = 0;
-  const size_t size = N * M;
-  const size_t size_byte = size * sizeof(float);
+  const size_t size = nrow * ncol;
+  const size_t size_byte = size * sizeof(T);
 
-  // N rows and M columns
-  float *matrix = (float *) malloc (size_byte);
+  T *matrix = (T *) malloc (size_byte);
   for (size_t i = 0; i < size; i++) matrix[i] = rand() % 13; 
 
-  timeval t1, t2;
-  gettimeofday(&t1, NULL);
+  T *matrixT = (T *) malloc (size_byte);
 
-  // M rows and N columns
-  float *matrixT = (float *) malloc (size_byte);
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < M; j++)
-      matrixT[(j*N)+i] = matrix[(i*M)+j];
+  auto start = std::chrono::steady_clock::now();
 
-  gettimeofday(&t2, NULL);
-  float et1 = (((t2.tv_sec*uS_PER_SEC)+t2.tv_usec) - ((t1.tv_sec*uS_PER_SEC)+t1.tv_usec))/(float)uS_PER_mS;
-  printf("CPU time = %fms\n", et1);
+  for (int i = 0; i < nrow; i++)
+    for (int j = 0; j < ncol; j++)
+      matrixT[(j*nrow)+i] = matrix[(i*ncol)+j];
 
+  auto end = std::chrono::steady_clock::now();
+  double time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("serial transpose time = %lf (ms)\n", time * 1e-6f);
+
+  hipblasStatus_t stat;
   hipblasHandle_t handle;
   hipblasCreate(&handle);
 
-  const float alpha = 1.f;
-  const float beta  = 0.f;
+  const T alpha = (T)1;
+  const T beta  = (T)0;
 
   // store host and device results
-  float *h_matrixT , *d_matrixT , *d_matrix;
-  h_matrixT = (float *) malloc (size_byte);
-  hipMalloc((void**)&d_matrixT , size_byte);
-  hipMalloc((void**)&d_matrix , size_byte);
+  T *h_matrixT, *d_matrixT, *d_matrix;
+  h_matrixT = (T *) malloc (size_byte);
 
-  hipMemcpy(d_matrix , matrix , size_byte, hipMemcpyHostToDevice);
+  hipMalloc((void**)&d_matrix, size_byte);
+  hipMemcpy(d_matrix, matrix, size_byte, hipMemcpyHostToDevice);
 
-  // start the device timing
-  gettimeofday(&t1, NULL);
+  hipMalloc((void**)&d_matrixT, size_byte);
+
+  time = 0.0;
 
   for (int i = 0; i < repeat; i++) {
-    auto status = hipblasSgeam(handle, HIPBLAS_OP_T, HIPBLAS_OP_N, N, M, &alpha, 
-                              d_matrix, M, &beta, d_matrix, N, d_matrixT, N);
 
-    if (status != HIPBLAS_STATUS_SUCCESS) {
+    start = std::chrono::steady_clock::now();
+    stat = hipblasDgeam(handle, HIPBLAS_OP_T, HIPBLAS_OP_N,
+                       nrow, ncol,
+                       &alpha, d_matrix, ncol,
+                       &beta, d_matrix, nrow,
+                       d_matrixT, nrow);
+    hipDeviceSynchronize(); // required for timing correctness
+    end = std::chrono::steady_clock::now();
+    time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+    if (stat != HIPBLAS_STATUS_SUCCESS) {
       error = 1;
-      printf("Error: hipblasSgeam failed to complete\n"):
+      printf("Error: hipblasDgeam failed to complete\n");
       break;
     }
-
-    std::swap(d_matrix, d_matrixT);
-    std::swap(N, M);
   }
 
-  gettimeofday(&t2, NULL);
+  printf("Average device transpose time = %lf (us)\n", (time * 1e-3f) / repeat);
 
-  float et2 = (((t2.tv_sec*uS_PER_SEC)+t2.tv_usec) - 
-               ((t1.tv_sec*uS_PER_SEC)+t1.tv_usec)) / (float)uS_PER_mS;
-
-  printf("Average device execution time = %fms\n", et2 / repeat);
-
-  hipMemcpy(h_matrixT , d_matrix , size_byte , hipMemcpyDeviceToHost);
+  hipMemcpy(h_matrixT, d_matrixT, size_byte, hipMemcpyDeviceToHost);
 
   hipFree(d_matrix);
   hipFree(d_matrixT);
 
-  if (error == 0) {
-
-
-    // check host and device results
-    int error;
-    if (repeat % 2)
-      error = memcmp(h_matrixT, matrixT, size_byte);
-    else
-      error = memcmp(h_matrixT, matrix, size_byte);
+  if (error == 0) { // check host and device results
+    error = memcmp(h_matrixT, matrixT, size_byte);
   }
 
   printf("%s\n", error ? "FAIL" : "PASS");
@@ -102,5 +78,103 @@ int main(int argc, char* argv[]) {
   hipblasDestroy(handle);
   free(matrixT);
   free(matrix);
+}
+
+template <typename T>
+void transpose_f32(int nrow, int ncol, int repeat) {
+  int error = 0;
+  const size_t size = nrow * ncol;
+  const size_t size_byte = size * sizeof(T);
+
+  T *matrix = (T *) malloc (size_byte);
+  for (size_t i = 0; i < size; i++) matrix[i] = rand() % 13; 
+
+  T *matrixT = (T *) malloc (size_byte);
+
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < nrow; i++)
+    for (int j = 0; j < ncol; j++)
+      matrixT[(j*nrow)+i] = matrix[(i*ncol)+j];
+
+  auto end = std::chrono::steady_clock::now();
+  double time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("serial transpose time = %lf (ms)\n", time * 1e-6f);
+
+  hipblasStatus_t stat;
+  hipblasHandle_t handle;
+  hipblasCreate(&handle);
+
+  const T alpha = (T)1;
+  const T beta  = (T)0;
+
+  // store host and device results
+  T *h_matrixT, *d_matrixT, *d_matrix;
+  h_matrixT = (T *) malloc (size_byte);
+  hipMalloc((void**)&d_matrix, size_byte);
+  hipMemcpy(d_matrix, matrix, size_byte, hipMemcpyHostToDevice);
+
+  hipMalloc((void**)&d_matrixT, size_byte);
+
+  time = 0.0;
+
+  for (int i = 0; i < repeat; i++) {
+    start = std::chrono::steady_clock::now();
+    stat = hipblasSgeam(handle, HIPBLAS_OP_T, HIPBLAS_OP_N,
+                       nrow, ncol,
+                       &alpha, d_matrix, ncol,
+                       &beta, d_matrix, nrow,
+                       d_matrixT, nrow);
+    hipDeviceSynchronize(); // required for timing correctness
+    end = std::chrono::steady_clock::now();
+    time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+    if (stat != HIPBLAS_STATUS_SUCCESS) {
+      error = 1;
+      printf("Error: hipblasSgeam failed to complete\n");
+      break;
+    }
+  }
+
+  printf("Average device transpose time = %lf (us)\n", (time * 1e-3f) / repeat);
+
+  hipMemcpy(h_matrixT, d_matrixT, size_byte, hipMemcpyDeviceToHost);
+
+  hipFree(d_matrix);
+  hipFree(d_matrixT);
+
+  if (error == 0) { // check host and device results
+    error = memcmp(h_matrixT, matrixT, size_byte);
+  }
+
+  printf("%s\n", error ? "FAIL" : "PASS");
+
+  hipblasDestroy(handle);
+  free(matrixT);
+  free(matrix);
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 4) {
+    printf("Usage %s <matrix row> <matrix col> <repeat>\n", argv[0]);
+    return 1;
+  }
+
+  int nrow = atoi(argv[1]); 
+  int ncol = atoi(argv[2]); 
+  const int repeat = atoi(argv[3]); 
+  if (nrow <= 0 || ncol <= 0 || repeat < 0) {
+    printf("Error: invalid inputs\n");
+    return 1;
+  }
+
+  printf("----------------\nFP32 transpose matrix (%d x %d)\n----------------\n",
+         nrow, ncol);
+  transpose_f32<float>(nrow, ncol, repeat);
+
+  printf("----------------\nFP64 transpose matrix (%d x %d)\n----------------\n",
+         nrow, ncol);
+  transpose_f64<double>(nrow, ncol, repeat);
+
   return 0;
 }
--- geam-sycl/main.cpp
+++ geam-sycl/main.cpp
@@ -1,29 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
 #include <string.h>
-#include <utility>
+#include <chrono>
+#include <CL/sycl.hpp>
 #include <oneapi/mkl.hpp>
-#include "common.h"
 
-#define uS_PER_SEC 1000000
-#define uS_PER_mS 1000
+template <typename T>
+void transpose(sycl::queue &q, int nrow, int ncol, int repeat) {
+  int error = 0;
+  const size_t size = nrow * ncol;
+  const size_t size_byte = size * sizeof(T);
 
-int main(int argc, char *argv[]) {
+  T *matrix = (T *) malloc (size_byte);
+  for (size_t i = 0; i < size; i++) matrix[i] = rand() % 13; 
+
+  T *matrixT = (T *) malloc (size_byte);
+
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < nrow; i++)
+    for (int j = 0; j < ncol; j++)
+      matrixT[(j*nrow)+i] = matrix[(i*ncol)+j];
+
+  auto end = std::chrono::steady_clock::now();
+  double time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("serial transpose time = %lf (ms)\n", time * 1e-6f);
+
+  const T alpha = (T)1;
+  const T beta  = (T)0;
+
+  // store host and device results
+  T *h_matrixT, *d_matrixT, *d_matrix;
+  h_matrixT = (T *) malloc (size_byte);
+
+  d_matrixT = (T *)sycl::malloc_device(size_byte, q);
+  d_matrix = (T *)sycl::malloc_device(size_byte, q);
+  q.memcpy(d_matrix, matrix, size_byte).wait();
+
+  time = 0.0;
+
+  sycl::event status;
+  for (int i = 0; i < repeat; i++) {
+    start = std::chrono::steady_clock::now();
+    try {
+      status = oneapi::mkl::blas::omatadd_batch(
+        q,
+        oneapi::mkl::transpose::trans,
+        oneapi::mkl::transpose::nontrans,
+        nrow,
+        ncol,
+        alpha,
+        d_matrix,
+        ncol,
+        size,
+        beta,
+        d_matrix,
+        nrow,
+        size,
+        d_matrixT,
+        nrow,
+        size,
+        1);
+    } catch(sycl::exception const& e) {
+      std::cout << "\t\tCaught SYCL exception during omatadd_batch:\n"
+                << e.what() << std::endl;
+      error = 1;
+      break;
+    }
+    status.wait();
+    end = std::chrono::steady_clock::now();
+    time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  }
+
+  printf("Average device transpose time = %lf (us)\n", (time * 1e-3f) / repeat);
+
+  q.memcpy(h_matrixT, d_matrixT, size_byte).wait();
+
+  sycl::free(d_matrix, q);
+  sycl::free(d_matrixT, q);
+
+  if (error == 0) { // check host and device results
+    error = memcmp(h_matrixT, matrixT, size_byte);
+  }
+
+  printf("%s\n", error ? "FAIL" : "PASS");
+
+  free(matrixT);
+  free(matrix);
+}
+
+int main(int argc, char* argv[]) {
   if (argc != 4) {
-    printf("Usage %s <matrix col> <matrix row> <repeat times>\n", argv[0]);
+    printf("Usage %s <matrix row> <matrix col> <repeat>\n", argv[0]);
     return 1;
   }
 
-  int M = atoi(argv[1]); 
-  int N = atoi(argv[2]); 
+  int nrow = atoi(argv[1]); 
+  int ncol = atoi(argv[2]); 
   const int repeat = atoi(argv[3]); 
-  if (M <= 0 || N <= 0 || repeat < 0) {
+  if (nrow <= 0 || ncol <= 0 || repeat < 0) {
     printf("Error: invalid inputs\n");
     return 1;
   }
-
-  int error = 0;
 
 #ifdef USE_GPU
   sycl::gpu_selector dev_sel;
@@ -32,99 +110,13 @@ int main(int argc, char *argv[]) {
 #endif
   sycl::queue q(dev_sel);
 
-  const size_t size = N * M;
-  const size_t size_byte = size * sizeof(float);
+  printf("----------------\nFP32 transpose matrix (%d x %d)\n----------------\n",
+         nrow, ncol);
+  transpose<float>(q, nrow, ncol, repeat);
 
-  // N rows and M columns
-  float *matrix = (float *) malloc (size_byte);
-  for (size_t i = 0; i < size; i++) matrix[i] = rand() % 13; 
+  printf("----------------\nFP64 transpose matrix (%d x %d)\n----------------\n",
+         nrow, ncol);
+  transpose<double>(q, nrow, ncol, repeat);
 
-  timeval t1, t2;
-  gettimeofday(&t1, NULL);
-
-  // M rows and N columns
-  float *matrixT = (float *) malloc (size_byte);
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < M; j++)
-      matrixT[(j*N)+i] = matrix[(i*M)+j];
-
-  gettimeofday(&t2, NULL);
-  float et1 = (((t2.tv_sec*uS_PER_SEC)+t2.tv_usec) - ((t1.tv_sec*uS_PER_SEC)+t1.tv_usec))/(float)uS_PER_mS;
-  printf("CPU time = %fms\n", et1);
-
-  const float alpha = 1.f;
-  const float beta  = 0.f;
-
-  // store host and device results
-  float *h_matrixT , *d_matrixT , *d_matrix;
-  h_matrixT = (float *) malloc (size_byte);
-  d_matrixT = (float *)sycl::malloc_device(size_byte, q);
-  d_matrix = (float *)sycl::malloc_device(size_byte, q);
-
-  q.memcpy(d_matrix, matrix, size_byte).wait();
-
-  std::vector<event> deps;
-
-  // start the device timing
-  gettimeofday(&t1, NULL);
-
-  for (int i = 0; i < repeat; i++) {
-    event status;
-    try {
-      status = oneapi::mkl::blas::omatadd_batch(
-        q,
-        oneapi::mkl::transpose::trans,
-        oneapi::mkl::transpose::nontrans,
-        N,
-        M,
-        alpha,
-        d_matrix,
-        M,
-        size,
-        beta,
-        d_matrix,
-        N,
-        size,
-        d_matrixT,
-        N,
-        size,
-        1,
-        deps);
-    } catch(sycl::exception const& e) {
-      std::cout << "\t\tCaught SYCL exception during omatadd_batch:\n"
-                << e.what() << std::endl;
-      error = 1;
-      break;
-    }
-    status.wait();
-    std::swap(d_matrix, d_matrixT);
-    std::swap(N, M);
-  }
-
-  gettimeofday(&t2, NULL);
-
-  float et2 = (((t2.tv_sec*uS_PER_SEC)+t2.tv_usec) - 
-               ((t1.tv_sec*uS_PER_SEC)+t1.tv_usec)) / (float)uS_PER_mS;
-
-  printf("Average device execution time = %fms\n", et2 / repeat);
-
-  q.memcpy(h_matrixT, d_matrix, size_byte).wait();
-
-  sycl::free(d_matrix, q);
-  sycl::free(d_matrixT, q);
-
-  if (error == 0) {
-
-    // check host and device results
-    if (repeat % 2)
-      error = memcmp(h_matrixT, matrixT, size_byte);
-    else
-      error = memcmp(h_matrixT, matrix, size_byte);
-  }
-
-  printf("%s\n", error ? "FAIL" : "PASS");
-
-  free(matrixT);
-  free(matrix);
   return 0;
 }
