fbb96d3cb228779cf3d1091467da3e3464818f30
Jin Z <5zj@equinox.ftpn.ornl.gov>
2022-09-24 14:17:13 -0400

    [scan] extend the block scan with bank conflict aware optimization  and more data types; reorganize the runTest functions

--- scan-cuda/main.cu
+++ scan-cuda/main.cu
@@ -1,16 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <chrono>
 #include <cuda.h>
 
+// scan over N elements
 #define N 512
 
-template<typename dataType>
-__global__ void prescan(
-        dataType *__restrict__ g_odata,
-  const dataType *__restrict__ g_idata,
+template<typename T>
+void verify(const T* cpu_out, const T* gpu_out, int n)
+{
+  int error = memcmp(cpu_out, gpu_out, n * sizeof(T));
+  printf("%s\n", error ? "FAIL" : "PASS");
+}
+
+// bank conflict aware optimization
+
+#define LOG_MEM_BANKS 5
+#define OFFSET(n) ((n) >> LOG_MEM_BANKS)
+
+template<typename T>
+__global__ void prescan_bcao (
+        T *__restrict__ g_odata,
+  const T *__restrict__ g_idata,
   const int n)
 {
-  __shared__ dataType temp[N];
+  __shared__ T temp[2*N];
+  int thid = threadIdx.x; 
+  int a = thid;
+  int b = a + (n/2);
+  int oa = OFFSET(a);
+  int ob = OFFSET(b);
+
+  temp[a + oa] = g_idata[a];
+  temp[b + ob] = g_idata[b];
+
+  int offset = 1;
+  for (int d = n >> 1; d > 0; d >>= 1) 
+  {
+    __syncthreads();
+    if (thid < d) 
+    {
+      int ai = offset*(2*thid+1)-1;
+      int bi = offset*(2*thid+2)-1;
+      ai += OFFSET(ai);
+      bi += OFFSET(bi);
+      temp[bi] += temp[ai];
+    }
+    offset *= 2;
+  }
+
+  if (thid == 0) temp[n-1+OFFSET(n-1)] = 0; // clear the last elem
+  for (int d = 1; d < n; d *= 2) // traverse down
+  {
+    offset >>= 1;     
+    __syncthreads();      
+    if (thid < d)
+    {
+      int ai = offset*(2*thid+1)-1;
+      int bi = offset*(2*thid+2)-1;
+      ai += OFFSET(ai);
+      bi += OFFSET(bi);
+      T t = temp[ai];
+      temp[ai] = temp[bi];
+      temp[bi] += t;
+    }
+  }
+  __syncthreads(); // required
+
+  g_odata[a] = temp[a + oa];
+  g_odata[b] = temp[b + ob];
+}
+
+template<typename T>
+__global__ void prescan(
+        T *__restrict__ g_odata,
+  const T *__restrict__ g_idata,
+  const int n)
+{
+  __shared__ T temp[N];
   int thid = threadIdx.x; 
   int offset = 1;
   temp[2*thid]   = g_idata[2*thid];
@@ -45,14 +113,25 @@ __global__ void prescan(
   g_odata[2*thid+1] = temp[2*thid+1];
 }
 
-template <typename dataType>
-void runTest (const dataType *in, dataType *out, const int n, const int repeat) 
+template <typename T>
+void runTest (const int repeat, bool timing = false) 
 {
-  dataType *d_in;
-  dataType *d_out;
-  cudaMalloc((void**)&d_in, n*sizeof(dataType));
-  cudaMalloc((void**)&d_out, n*sizeof(dataType));
-  cudaMemcpy(d_in, in, n*sizeof(dataType), cudaMemcpyHostToDevice); 
+  T in[N];
+  T cpu_out[N];
+  T gpu_out[N];
+
+  int n = N;
+
+  for (int i = 0; i < n; i++) in[i] = (i % 5)+1;
+  cpu_out[0] = 0;
+  for (int i = 1; i < n; i++) cpu_out[i] = cpu_out[i-1] + in[i-1];
+
+  T *d_in, *d_out;
+
+  cudaMalloc((void**)&d_in, n*sizeof(T));
+  cudaMemcpy(d_in, in, n*sizeof(T), cudaMemcpyHostToDevice); 
+
+  cudaMalloc((void**)&d_out, n*sizeof(T));
 
   dim3 grids (1);
   dim3 blocks (n/2);
@@ -67,9 +146,31 @@ void runTest (const dataType *in, dataType *out, const int n, const int repeat)
   cudaDeviceSynchronize();
   auto end = std::chrono::steady_clock::now();
   auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  printf("Average execution time of block scan: %f (us)\n", (time * 1e-3f) / repeat);
+  if (timing) {
+    printf("Element size in bytes is %zu. Average execution time of block scan (w/  bank conflicts): %f (us)\n",
+           sizeof(T), (time * 1e-3f) / repeat);
+  }
+  cudaMemcpy(gpu_out, d_out, n*sizeof(T), cudaMemcpyDeviceToHost);
+  if (!timing) verify(cpu_out, gpu_out, n);
 
-  cudaMemcpy(out, d_out, n*sizeof(dataType), cudaMemcpyDeviceToHost);
+  // bcao
+  start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    prescan_bcao<<<grids, blocks>>>(d_out, d_in, n);
+  }
+
+  cudaDeviceSynchronize();
+  end = std::chrono::steady_clock::now();
+  auto bcao_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  if (timing) {
+    printf("Element size in bytes is %zu. Average execution time of block scan (w/o bank conflicts): %f (us). ",
+           sizeof(T), (bcao_time * 1e-3f) / repeat);
+    printf("Reduce the time by %.1f%%\n", (time - bcao_time) * 1.0 / time * 100);
+  }
+  cudaMemcpy(gpu_out, d_out, n*sizeof(T), cudaMemcpyDeviceToHost);
+  if (!timing) verify(cpu_out, gpu_out, n);
+
   cudaFree(d_in);
   cudaFree(d_out);
 }
@@ -82,27 +183,13 @@ int main(int argc, char* argv[])
   }
   const int repeat = atoi(argv[1]);
     
-  float in[N];
-  float cpu_out[N];
-  float gpu_out[N];
-  for (int i = 0; i < N; i++) in[i] = (i % 5)+1;
-
-  runTest(in, gpu_out, N, repeat);
-
-  bool ok = true;
-  if (gpu_out[0] != 0) {
-    ok = false;
+  for (int i = 0; i < 2; i++) {
+    bool timing = i > 0;
+    runTest<char>(repeat, timing);
+    runTest<short>(repeat, timing);
+    runTest<int>(repeat, timing);
+    runTest<long>(repeat, timing);
   }
 
-  cpu_out[0] = 0;
-  for (int i = 1; i < N; i++) 
-  {
-    cpu_out[i] = cpu_out[i-1] + in[i-1];
-    if (cpu_out[i] != gpu_out[i]) { 
-      ok = false;
-      break;
-    }
-  }
-  printf("%s\n", ok ? "PASS" : "FAIL");
   return 0; 
 }
--- scan-sycl/main.cpp
+++ scan-sycl/main.cpp
@@ -1,21 +1,145 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <chrono>
 #include "common.h"
 
+// scan over N elements
 #define N 512
 
-template <typename dataType>
-void runTest (const dataType *in, dataType *out, const int n, const int repeat) 
+template<typename T>
+void verify(const T* cpu_out, const T* gpu_out, int n)
 {
-#ifdef USE_GPU
-  gpu_selector dev_sel;
-#else
-  cpu_selector dev_sel;
-#endif
-  queue q(dev_sel);
+  int error = memcmp(cpu_out, gpu_out, n * sizeof(T));
+  printf("%s\n", error ? "FAIL" : "PASS");
+}
 
-  buffer<dataType,1> d_in(in, n);
-  buffer<dataType,1> d_out(out, n);
+#define LOG_MEM_BANKS 5
+#define OFFSET(n) ((n) >> LOG_MEM_BANKS)
+#define __syncthreads() item.barrier(access::fence_space::local_space)
+
+template<typename T>
+class opt_block_scan;
+
+template <typename T>
+class block_scan;
+
+// bank conflict aware optimization
+template<typename T>
+__attribute__((always_inline))
+void prescan_bcao (
+        nd_item<1> &item,
+        local_ptr<T> temp,
+        T *__restrict g_odata,
+  const T *__restrict g_idata,
+  const int n)
+{
+  int thid = item.get_local_id(0);
+  int a = thid;
+  int b = a + (n/2);
+  int oa = OFFSET(a);
+  int ob = OFFSET(b);
+
+  temp[a + oa] = g_idata[a];
+  temp[b + ob] = g_idata[b];
+
+  int offset = 1;
+  for (int d = n >> 1; d > 0; d >>= 1) 
+  {
+    __syncthreads();
+    if (thid < d) 
+    {
+      int ai = offset*(2*thid+1)-1;
+      int bi = offset*(2*thid+2)-1;
+      ai += OFFSET(ai);
+      bi += OFFSET(bi);
+      temp[bi] += temp[ai];
+    }
+    offset *= 2;
+  }
+
+  if (thid == 0) temp[n-1+OFFSET(n-1)] = 0; // clear the last elem
+  for (int d = 1; d < n; d *= 2) // traverse down
+  {
+    offset >>= 1;     
+    __syncthreads();      
+    if (thid < d)
+    {
+      int ai = offset*(2*thid+1)-1;
+      int bi = offset*(2*thid+2)-1;
+      ai += OFFSET(ai);
+      bi += OFFSET(bi);
+      T t = temp[ai];
+      temp[ai] = temp[bi];
+      temp[bi] += t;
+    }
+  }
+  __syncthreads(); // required
+
+  g_odata[a] = temp[a + oa];
+  g_odata[b] = temp[b + ob];
+}
+
+template<typename T>
+__attribute__((always_inline))
+void prescan (
+        nd_item<1> &item,
+        local_ptr<T> temp,
+        T *__restrict g_odata,
+  const T *__restrict g_idata,
+  const int n)
+{
+  int thid = item.get_local_id(0);
+  int offset = 1;
+  temp[2*thid]   = g_idata[2*thid];
+  temp[2*thid+1] = g_idata[2*thid+1];
+  for (int d = n >> 1; d > 0; d >>= 1) 
+  {
+    __syncthreads();
+    if (thid < d) 
+    {
+      int ai = offset*(2*thid+1)-1;
+      int bi = offset*(2*thid+2)-1;
+      temp[bi] += temp[ai];
+    }
+    offset *= 2;
+  }
+
+  if (thid == 0) temp[n-1] = 0; // clear the last elem
+  for (int d = 1; d < n; d *= 2) // traverse down
+  {
+    offset >>= 1;     
+    __syncthreads();      
+    if (thid < d)
+    {
+      int ai = offset*(2*thid+1)-1;
+      int bi = offset*(2*thid+2)-1;
+      float t = temp[ai];
+      temp[ai] = temp[bi];
+      temp[bi] += t;
+    }
+  }
+  g_odata[2*thid] = temp[2*thid];
+  g_odata[2*thid+1] = temp[2*thid+1];
+}
+
+template <typename T>
+void runTest (queue &q, const int repeat, bool timing = false) 
+{
+  T in[N];
+  T cpu_out[N];
+  T gpu_out[N];
+
+  int n = N;
+
+  for (int i = 0; i < n; i++) in[i] = (i % 5)+1;
+  cpu_out[0] = 0;
+  for (int i = 1; i < n; i++) cpu_out[i] = cpu_out[i-1] + in[i-1];
+
+  T *d_in = malloc_device<T>(n, q);
+  q.memcpy(d_in, in, n*sizeof(T));
+
+  T *d_out = malloc_device<T>(n, q);
 
   range<1> lws (n/2);
   range<1> gws (n/2);
@@ -25,42 +149,9 @@ void runTest (const dataType *in, dataType *out, const int n, const int repeat)
 
   for (int i = 0; i < repeat; i++) {
     q.submit([&] (handler &cgh) {
-      auto g_odata = d_out.template get_access<sycl_discard_write>(cgh);
-      auto g_idata = d_in.template get_access<sycl_read>(cgh);
-      accessor<dataType, 1, sycl_read_write, access::target::local> temp(N, cgh);
-      cgh.parallel_for<class scan_block>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
-        int thid = item.get_local_id(0);
-        int offset = 1;
-        temp[2*thid]   = g_idata[2*thid];
-        temp[2*thid+1] = g_idata[2*thid+1];
-        for (int d = n >> 1; d > 0; d >>= 1) 
-        {
-          item.barrier(access::fence_space::local_space);
-          if (thid < d) 
-          {
-            int ai = offset*(2*thid+1)-1;
-            int bi = offset*(2*thid+2)-1;
-            temp[bi] += temp[ai];
-          }
-          offset *= 2;
-        }
-        
-        if (thid == 0) temp[n-1] = 0; // clear the last elem
-        for (int d = 1; d < n; d *= 2) // traverse down
-        {
-          offset >>= 1;     
-          item.barrier(access::fence_space::local_space);
-          if (thid < d)
-          {
-            int ai = offset*(2*thid+1)-1;
-            int bi = offset*(2*thid+2)-1;
-            float t = temp[ai];
-            temp[ai] = temp[bi];
-            temp[bi] += t;
-          }
-        }
-        g_odata[2*thid] = temp[2*thid];
-        g_odata[2*thid+1] = temp[2*thid+1];
+      accessor<T, 1, sycl_read_write, access::target::local> temp (N, cgh);
+      cgh.parallel_for<class block_scan<T>>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+        prescan(item, temp.get_pointer(), d_out, d_in, n);
       });
     });
   }
@@ -68,7 +159,38 @@ void runTest (const dataType *in, dataType *out, const int n, const int repeat)
   q.wait();
   auto end = std::chrono::steady_clock::now();
   auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  printf("Average execution time of block scan: %f (us)\n", (time * 1e-3f) / repeat);
+  if (timing) {
+    printf("Element size in bytes is %zu. Average execution time of block scan (w/  bank conflicts): %f (us)\n",
+           sizeof(T), (time * 1e-3f) / repeat);
+  }
+  q.memcpy(gpu_out, d_out, n*sizeof(T)).wait();
+  if (!timing) verify(cpu_out, gpu_out, n);
+
+  // bcao
+  start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    q.submit([&] (handler &cgh) {
+      accessor<T, 1, sycl_read_write, access::target::local> temp (N*2, cgh);
+      cgh.parallel_for<class opt_block_scan<T>>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+        prescan_bcao(item, temp.get_pointer(), d_out, d_in, n);
+      });
+    });
+  }
+
+  q.wait();
+  end = std::chrono::steady_clock::now();
+  auto bcao_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  if (timing) {
+    printf("Element size in bytes is %zu. Average execution time of block scan (w/o bank conflicts): %f (us). ",
+           sizeof(T), (bcao_time * 1e-3f) / repeat);
+    printf("Reduce the time by %.1f%%\n", (time - bcao_time) * 1.0 / time * 100);
+  }
+  q.memcpy(gpu_out, d_out, n*sizeof(T)).wait();
+  if (!timing) verify(cpu_out, gpu_out, n);
+
+  free(d_in, q);
+  free(d_out, q);
 }
 
 int main(int argc, char* argv[])
@@ -78,28 +200,21 @@ int main(int argc, char* argv[])
     return 1;
   }
   const int repeat = atoi(argv[1]);
+
+#ifdef USE_GPU
+  gpu_selector dev_sel;
+#else
+  cpu_selector dev_sel;
+#endif
+  queue q(dev_sel);
     
-  float in[N];
-  float cpu_out[N];
-  float gpu_out[N];
-  for (int i = 0; i < N; i++) in[i] = (i % 5)+1;
-
-  runTest(in, gpu_out, N, repeat);
-
-  bool ok = true;
-  if (gpu_out[0] != 0) {
-    ok = false;
+  for (int i = 0; i < 2; i++) {
+    bool timing = i > 0;
+    runTest<char>(q, repeat, timing);
+    runTest<short>(q, repeat, timing);
+    runTest<int>(q, repeat, timing);
+    runTest<long>(q, repeat, timing);
   }
 
-  cpu_out[0] = 0;
-  for (int i = 1; i < N; i++) 
-  {
-    cpu_out[i] = cpu_out[i-1] + in[i-1];
-    if (cpu_out[i] != gpu_out[i]) { 
-      ok = false;
-      break;
-    }
-  }
-  printf("%s\n", ok ? "PASS" : "FAIL");
   return 0; 
 }
