7ebc48401bfecc94d1e71c155810464352c141bb
Jin Z <5zj@oswald03.ftpn.ornl.gov>
2022-10-04 14:18:02 -0400

    [crossEntropy] the function LossNLL_BWD is called twice; memory allocation and data transfer time is significant on the first call in the HIP implementation; the SYCL program is more concise than the CUDA program for FP16

---
+++ crossEntropy-cuda/main.cu
@@ -0,0 +1,318 @@
+/*  Copyright (c) 2021-2022 Intel Corporation
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include "reference.h"
+
+__inline__ __device__
+float exponent (float x) { return __expf(x); }
+
+__inline__ __device__
+double exponent (double x) { return exp(x); }
+
+template <typename scalar_t, typename gscalar_t>
+__global__ void loss_bwd (
+    const scalar_t* __restrict__ log_softmax,
+    const gscalar_t* __restrict__ grad_output,
+    const gscalar_t* __restrict__ grad_output_neg,
+    const int64_t* __restrict__ target,
+    const scalar_t* __restrict__ weight,
+    const int64_t* __restrict__ mask,
+          gscalar_t* __restrict__ grad_predict)
+{
+  int local_id_x = threadIdx.x;
+  int group_id_bs = blockIdx.y;
+  int group_id_x = blockIdx.x;
+
+  int linear_x_id = group_id_x * threadX + local_id_x;
+
+  if (linear_x_id >= H) return;
+
+  int offset2d = group_id_bs * H + linear_x_id;
+  int idx = target[offset2d];
+  int sum_offset = group_id_bs * W * H + idx * H + linear_x_id;
+
+  gscalar_t tmp_grad;
+  if (mask[offset2d])
+    tmp_grad = -(grad_output[offset2d] + grad_output_neg[offset2d]);
+  else
+    tmp_grad = -grad_output[offset2d];
+
+  tmp_grad = tmp_grad * weight[offset2d];
+
+  float sum_value = tmp_grad * log_softmax[sum_offset];
+
+  #pragma unroll
+  for (int i = 0; i < W; ++i) {
+    int in_offset = group_id_bs * W * H + i * H + linear_x_id;
+    float tmp_sfm = exponent(log_softmax[in_offset]) * sum_value;
+    float res = 0.f;
+    if (i == idx) {
+      res = (float)tmp_grad - tmp_sfm;
+    }
+    else {
+      res = -tmp_sfm;
+    }
+    grad_predict[in_offset] = res;
+  }
+}
+
+template <>
+__global__ void loss_bwd<__half, __half> (
+    const __half* __restrict__ log_softmax,
+    const __half* __restrict__ grad_output,
+    const __half* __restrict__ grad_output_neg,
+    const int64_t* __restrict__ target,
+    const __half* __restrict__ weight,
+    const int64_t* __restrict__ mask,
+          __half* __restrict__ grad_predict)
+{
+  int local_id_x = threadIdx.x;
+  int group_id_bs = blockIdx.y;
+  int group_id_x = blockIdx.x;
+
+  int linear_x_id = group_id_x * threadX + local_id_x;
+
+  if (linear_x_id >= H) return;
+
+  int offset2d = group_id_bs * H + linear_x_id;
+  int idx = target[offset2d];
+  int sum_offset = group_id_bs * W * H + idx * H + linear_x_id;
+
+  __half tmp_grad;
+  if (mask[offset2d])
+    tmp_grad = __hneg(__hadd(grad_output[offset2d] , grad_output_neg[offset2d]));
+  else
+    tmp_grad = __hneg(grad_output[offset2d]);
+
+  tmp_grad = __hmul(tmp_grad , weight[offset2d]);
+
+  float sum_value = h2f(__hmul(tmp_grad , log_softmax[sum_offset]));
+
+  #pragma unroll
+  for (int i = 0; i < W; ++i) {
+    int in_offset = group_id_bs * W * H + i * H + linear_x_id;
+    float tmp_sfm = h2f(hexp(log_softmax[in_offset])) * sum_value;
+    float res = 0.f;
+    if (i == idx) {
+      res = h2f(tmp_grad) - tmp_sfm;
+    }
+    else {
+      res = -tmp_sfm;
+    }
+    grad_predict[in_offset] = res;
+  }
+}
+
+template <typename scalar_t, typename gscalar_t>
+float loss_bwd_kernel(
+  scalar_t* log_softmax,
+  gscalar_t* grad_output,
+  gscalar_t* grad_output_neg,
+  int64_t* target, scalar_t* weight,
+  int64_t* mask, gscalar_t* grad_predict)
+{
+  dim3 blocks ( threadX, threadBS );
+  dim3 grids ( H, bs );
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  loss_bwd<<<grids, blocks>>>(log_softmax, grad_output, grad_output_neg,
+                              target, weight, mask, grad_predict);
+  cudaDeviceSynchronize();
+
+  auto end = std::chrono::high_resolution_clock::now();
+  float time = std::chrono::duration<float, std::milli>(end - start).count(); // ms
+  return time;
+}
+
+// compute cross entropy in the backward phase
+template <typename scalar_t, typename gscalar_t>
+void LossNLL_BWD() {
+
+  vector<double> durations(3, 0.0); // timing
+
+  const size_t grad_predict_size = sizeof(gscalar_t) * PredictShape;
+  const size_t log_softmax_size = sizeof(scalar_t) * PredictShape;
+  const size_t weight_size = sizeof(scalar_t) * OutputShape;
+  const size_t target_size = sizeof(int64_t) * TargetShape;
+  const size_t mask_size = sizeof(int64_t) * TargetShape;
+  const size_t grad_output_size = sizeof(gscalar_t) * OutputShape;
+
+  gscalar_t* grad_predict;
+  cudaMallocHost((void**)&grad_predict, grad_predict_size);
+
+  gscalar_t* grad_predict_device_host;
+  cudaMallocHost((void**)&grad_predict_device_host, grad_predict_size);
+
+  scalar_t* weight;
+  cudaMallocHost((void**)&weight, weight_size);
+
+  int64_t* target;
+  cudaMallocHost((void**)&target, target_size);
+  
+  int64_t* mask;
+  cudaMallocHost((void**)&mask, mask_size);
+
+  scalar_t* log_softmax;
+  cudaMallocHost((void**)&log_softmax, log_softmax_size);
+
+  gscalar_t* grad_output;
+  cudaMallocHost((void**)&grad_output, grad_output_size);
+
+  gscalar_t* grad_output_neg;
+  cudaMallocHost((void**)&grad_output_neg, grad_output_size);
+
+  for (int i = 0; i < PredictShape; ++i)
+    log_softmax[i] = static_cast<scalar_t>(random_float());
+  for (int i = 0; i < OutputShape; ++i)
+    grad_output[i] = static_cast<gscalar_t>(random_float());
+  for (int i = 0; i < OutputShape; ++i)
+    grad_output_neg[i] = static_cast<gscalar_t>(random_float());
+  for (int i = 0; i < OutputShape; ++i)
+    weight[i] = static_cast<scalar_t>(random_float());
+  for (int i = 0; i < TargetShape; ++i)
+    target[i] = static_cast<int64_t>(random_int(0, W - 1));
+  for (int i = 0; i < TargetShape; ++i)
+    mask[i] = static_cast<int64_t>(random_int(0, 1));
+
+  // malloc device memory
+  auto start = std::chrono::high_resolution_clock::now();
+
+  gscalar_t* grad_predict_device;
+  cudaMalloc((void**)&grad_predict_device, grad_predict_size);
+
+  scalar_t* weight_device;
+  cudaMalloc((void**)&weight_device, weight_size);
+
+  int64_t* target_device;
+  cudaMalloc((void**)&target_device, target_size);
+  
+  int64_t* mask_device;
+  cudaMalloc((void**)&mask_device, mask_size);
+
+  scalar_t* log_softmax_device;
+  cudaMalloc((void**)&log_softmax_device, log_softmax_size);
+
+  gscalar_t* grad_output_device;
+  cudaMalloc((void**)&grad_output_device, grad_output_size);
+
+  gscalar_t* grad_output_neg_device;
+  cudaMalloc((void**)&grad_output_neg_device, grad_output_size);
+
+  cudaMemcpy(grad_predict_device, grad_predict, grad_predict_size, cudaMemcpyHostToDevice);
+  cudaMemcpy(target_device, target, target_size, cudaMemcpyHostToDevice);
+  cudaMemcpy(mask_device, mask, mask_size, cudaMemcpyHostToDevice);
+  cudaMemcpy(weight_device, weight, weight_size, cudaMemcpyHostToDevice);
+  cudaMemcpy(grad_output_device, grad_output, grad_output_size, cudaMemcpyHostToDevice);
+  cudaMemcpy(grad_output_neg_device, grad_output_neg, grad_output_size, cudaMemcpyHostToDevice);
+  cudaMemcpy(log_softmax_device, log_softmax, log_softmax_size, cudaMemcpyHostToDevice);
+
+  auto end = std::chrono::high_resolution_clock::now();
+  float time = std::chrono::duration<float, std::milli>(end - start).count(); // ms
+  durations[0] += time;
+
+  int warmup = 10;
+  for (int k = 0; k < warmup + iterations; ++k) {
+
+    float duration_gpu = loss_bwd_kernel<scalar_t, gscalar_t>(
+      log_softmax_device, grad_output_device, grad_output_neg_device,
+      target_device, weight_device, mask_device, grad_predict_device);
+
+    if (k >= warmup) {
+      durations[1] += duration_gpu;
+    }
+  }
+
+  start = std::chrono::high_resolution_clock::now();
+  cudaMemcpy(grad_predict_device_host, grad_predict_device, grad_predict_size, cudaMemcpyDeviceToHost);
+  end = std::chrono::high_resolution_clock::now();
+  time = std::chrono::duration<float, std::milli>(end - start).count(); // ms
+  durations[0] += time;
+
+  start = std::chrono::high_resolution_clock::now();
+
+  loss_bwd_cpu<scalar_t, gscalar_t>(log_softmax, target, weight, mask, grad_output, grad_output_neg, grad_predict);
+
+  end = std::chrono::high_resolution_clock::now();
+  time = std::chrono::duration<float, std::milli>(end - start).count(); // ms
+  durations[2] = time;
+
+  verify<gscalar_t>(grad_predict, grad_predict_device_host, bs * W * H);
+
+  std::cout << "GPU device memory allocation and data transfer time (ms) : "
+            << (durations[0] / iterations) << std::endl;
+  std::cout << "Average GPU kernel time (ms) : "
+            << (durations[1] / iterations) << std::endl;
+  std::cout << "CPU serial time (ms) : "
+            << (durations[2]) << std::endl;
+
+  double allBytes = static_cast<double>(sizeof(scalar_t)) * static_cast<double>(PredictShape * 2.0 + OutputShape * 3.0)
+                  + static_cast<double>(sizeof(int64_t)) * static_cast<double>(TargetShape * 2.0);
+
+  printf("BandWidth = %lf (GB / s)\n", allBytes / (durations[1] / iterations / 1000) / 1e9);
+
+  cudaFree(grad_predict_device);
+  cudaFree(weight_device);
+  cudaFree(target_device);
+  cudaFree(mask_device);
+  cudaFree(log_softmax_device);
+  cudaFree(grad_output_device);
+  cudaFree(grad_output_neg_device);
+
+  cudaFreeHost(grad_predict);
+  cudaFreeHost(grad_predict_device_host);
+  cudaFreeHost(weight);
+  cudaFreeHost(target);
+  cudaFreeHost(mask);
+  cudaFreeHost(log_softmax);
+  cudaFreeHost(grad_output);
+  cudaFreeHost(grad_output_neg);
+}
+
+int main(int argc, char** argv) {
+
+  printf("Tensor size (BatchSize * Width * Height) = %d * %d * %d \n", bs, W, H);
+
+  printf("=========== Data type is FP16 ==========\n");
+
+  LossNLL_BWD<__half, __half>();
+  LossNLL_BWD<__half, __half>();
+
+  printf("%s\n", (errors == 0) ? "PASS" : "FAIL");
+
+  printf("=========== Data type is FP32 ==========\n");
+
+  LossNLL_BWD<float, float>();
+  LossNLL_BWD<float, float>();
+
+  printf("%s\n", (errors == 0) ? "PASS" : "FAIL");
+
+  printf("=========== Data type is FP64 ==========\n");
+
+  LossNLL_BWD<double, double>();
+  LossNLL_BWD<double, double>();
+
+  printf("%s\n", (errors == 0) ? "PASS" : "FAIL");
+
+  return 0;
+}
---
+++ crossEntropy-cuda/reference.h
@@ -0,0 +1,154 @@
+using namespace std;
+
+// generate rand int64_t [a, b]
+#define random_int(a, b) ( rand() % (b - a) + a )
+
+// generate rand float [0, 1]
+#define random_float() (rand() / double(RAND_MAX))
+
+// This version fused the log_softmax
+#define tolerance 4e-3
+#define iterations 1
+
+// Param for computaiton, tunable
+#define threadX  64
+#define threadBS 1
+
+int64_t errors(0);
+constexpr int bs = 128;
+constexpr int W = 81;
+constexpr int H = 8732;
+constexpr int PredictShape = bs * W * H;
+constexpr int TargetShape = bs * H;
+constexpr int OutputShape = bs * H;
+
+__host__ __half f2h (float x) { return __float2half(x); }
+
+__inline__ __host__ __device__
+float h2f (__half x) { return __half2float(x); }
+
+template <typename scalar_t, typename gscalar_t>
+void loss_bwd_cpu(scalar_t* predict, int64_t* target, scalar_t* weight, int64_t* mask,
+                  gscalar_t* grad_output, gscalar_t* grad_output_neg, gscalar_t* grad_predict)
+{
+  for (int i = 0; i < bs; ++i) {
+    for (int k = 0; k < W; ++k) {
+      for (int j = 0; j < H; ++j) {
+        int64_t offset = i * H + j;
+        int64_t idx = target[offset];
+        int64_t predict_offset = i * W * H + k * H + j;
+
+        if (idx == int64_t(k)) {
+          grad_predict[predict_offset] = (-grad_output[offset] * weight[offset]) +
+            (mask[offset] ? -grad_output_neg[offset] * weight[offset] : 0);
+        }
+        else {
+          grad_predict[predict_offset] = 0;
+        }
+      }
+    }
+  }
+
+  vector<vector<float>> sum_value;
+  for (int i = 0; i < bs; ++i) {
+    vector<float> bs_sum_value;
+    for (int j = 0; j < H; ++j) {
+      float sum = 0.0;
+      for (int k = 0; k < W; ++k) {
+        int64_t offset = i * W * H + k * H + j;
+        sum += grad_predict[offset] * predict[offset];
+      }
+      bs_sum_value.push_back(sum);
+    }
+    sum_value.push_back(bs_sum_value);
+  }
+
+  for (int i = 0; i < bs; ++i) {
+    for (int k = 0; k < W; ++k) {
+      for (int j = 0; j < H; ++j) {
+        int64_t offset = i * W * H + k * H + j;
+        grad_predict[offset] = grad_predict[offset] - std::exp(predict[offset]) * sum_value[i][j];
+      }
+    }
+  }
+}
+
+template <>
+void loss_bwd_cpu<__half, __half>(__half* predict, int64_t* target, __half* weight, int64_t* mask,
+                  __half* grad_output, __half* grad_output_neg, __half* grad_predict)
+{
+  for (int i = 0; i < bs; ++i) {
+    for (int k = 0; k < W; ++k) {
+      for (int j = 0; j < H; ++j) {
+        int64_t offset = i * H + j;
+        int64_t idx = target[offset];
+        int64_t predict_offset = i * W * H + k * H + j;
+
+        if (idx == int64_t(k)) {
+          grad_predict[predict_offset] = f2h((-h2f(grad_output[offset]) * h2f(weight[offset])) +
+            (mask[offset] ? -h2f(grad_output_neg[offset]) * h2f(weight[offset]) : 0));
+        }
+        else {
+          grad_predict[predict_offset] = 0;
+        }
+      }
+    }
+  }
+
+  vector<vector<float>> sum_value;
+  for (int i = 0; i < bs; ++i) {
+    vector<float> bs_sum_value;
+    for (int j = 0; j < H; ++j) {
+      float sum = 0.0;
+      for (int k = 0; k < W; ++k) {
+        int64_t offset = i * W * H + k * H + j;
+        sum += h2f(grad_predict[offset]) * h2f(predict[offset]);
+      }
+      bs_sum_value.push_back(sum);
+    }
+    sum_value.push_back(bs_sum_value);
+  }
+
+  for (int i = 0; i < bs; ++i) {
+    for (int k = 0; k < W; ++k) {
+      for (int j = 0; j < H; ++j) {
+        int64_t offset = i * W * H + k * H + j;
+        grad_predict[offset] = f2h(h2f(grad_predict[offset]) - std::exp(h2f(predict[offset])) * sum_value[i][j]);
+      }
+    }
+  }
+}
+
+template <typename scalar_t>
+void verify(scalar_t* output, scalar_t* output_device, size_t sz) {
+  int count = 0;
+  for (size_t i = 0; i < sz; ++i) {
+    int64_t offset = i;
+    if (std::abs(output[offset] - output_device[offset]) > tolerance) {
+      count++;
+      if (count < 10) 
+        std::cout << "Error, output not equal, i="
+                  << i << ", cpu_result = " << output[offset] << ", device_result = "
+                  << output_device[offset] << ", gap = "
+                  << output[offset] - output_device[offset] << std::endl;
+    }
+  }
+  errors += count;
+}
+
+template <>
+void verify<__half>(__half* output, __half* output_device, size_t sz) {
+  int count = 0;
+  for (size_t i = 0; i < sz; ++i) {
+    int64_t offset = i;
+    if (std::abs(h2f(output[offset]) - h2f(output_device[offset])) > tolerance) {
+      count++;
+      if (count < 10) 
+        std::cout << "Error, output not equal, i="
+                  << i << ", cpu_result = " << h2f(output[offset]) << ", device_result = "
+                  << h2f(output_device[offset]) << ", gap = "
+                  << h2f(output[offset]) - h2f(output_device[offset]) << std::endl;
+    }
+  }
+  errors += count;
+}
