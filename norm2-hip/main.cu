5140608f21958d783f614f1738e31e3372d613ad
Jin Z <5zj@equinox.ftpn.ornl.gov>
2022-10-31 11:57:18 -0400

    [norm2] remove the offload time for clarity; add a simple metric for performance evaluation

--- norm2-hip/main.cu
+++ norm2-hip/main.cu
@@ -52,8 +52,6 @@ int main (int argc, char* argv[]){
     }
     gold = sqrt(gold);
 
-    auto start = std::chrono::steady_clock::now();
-
     hipStat = hipMalloc((void**)&d_a, size);
     if (hipStat != hipSuccess) {
       printf ("device memory allocation failed");
@@ -84,17 +82,13 @@ int main (int argc, char* argv[]){
 
     auto kend = std::chrono::steady_clock::now();
     auto ktime = std::chrono::duration_cast<std::chrono::nanoseconds>(kend - kstart).count();
-    printf("Average hipblasSnrm2 execution time: %f (us)\n", (ktime * 1e-3f) / repeat);
+    printf("#elements = %.2f M: average hipblasSnrm2 execution time = %f (us), performance = %f (Gop/s)\n",
+           n / (1024.f*1024.f), (ktime * 1e-3f) / repeat, 1.f * (2*n+1) * repeat / ktime);
 
     hipStat = hipFree(d_a);
     if (hipStat != hipSuccess) {
       printf ("device memory deallocation failed");
     }
-
-    auto end = std::chrono::steady_clock::now();
-    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-
-    printf("#elements = %.2f M, measured time = %.3f s\n", n / (1024.f*1024.f), time * 1e-9f);
 
     // snrm2 results match across all iterations
     for (j = 0; j < repeat; j++) 
