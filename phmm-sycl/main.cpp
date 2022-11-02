0b035bc37a75b3a05cc542fe55bbefd18d81f93a
Jin Z <5zj@equinox.ftpn.ornl.gov>
2022-11-02 17:31:05 -0400

    [phmm] work around the limit that SYCL range can only be a 1d, 2d, or 3d vector; replace SYCL buffers with USM

--- phmm-sycl/main.cpp
+++ phmm-sycl/main.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include "common.h"
 #include "constants_types.h"
+#include "kernel.h"
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -74,21 +75,24 @@ int main(int argc, char* argv[]) {
 #else
   cpu_selector dev_sel;
 #endif
-  queue q(dev_sel);
+  queue q(dev_sel, property::queue::in_order());
 
-  buffer<double, 1> d_cur_forward ((double*)h_cur_forward, forward_matrix_elem);
-  buffer<double, 1> d_next_forward (forward_matrix_elem);
-  buffer<double, 1> d_emis ((double*)h_emis, emissions_elem);
-  buffer<double, 1> d_trans ((double*)h_trans, transitions_elem);
-  buffer<double, 1> d_like ((double*)h_like, likelihood_elem);
-  buffer<double, 1> d_start ((double*)h_start, start_transitions_elem);
+  fArray *d_cur_forward = malloc_device<fArray>(forward_matrix_elem, q);
+  q.memcpy(d_cur_forward, h_cur_forward, forward_matrix_size);
 
-  auto d_cur_fw_re = d_cur_forward.reinterpret<fArray>(range<1>(x_dim+1));
-  auto d_nxt_fw_re = d_next_forward.reinterpret<fArray>(range<1>(x_dim+1));
-  auto d_emis_re = d_emis.reinterpret<fArray>(range<1>(x_dim+1));
-  auto d_trans_re = d_trans.reinterpret<tArray>(range<1>(x_dim+1));
-  auto d_like_re = d_like.reinterpret<lArray>(range<1>(2));
-  auto d_start_re = d_start.reinterpret<sArray>(range<1>(batch));
+  fArray *d_next_forward = malloc_device<fArray>(forward_matrix_elem, q);
+
+  fArray *d_emis = malloc_device<fArray>(emissions_elem, q);
+  q.memcpy(d_emis, h_emis, emissions_size);
+
+  tArray *d_trans = malloc_device<tArray>(transitions_elem, q);
+  q.memcpy(d_trans, h_trans, transitions_size);
+
+  lArray *d_like = malloc_device<lArray>(likelihood_elem, q);
+  q.memcpy(d_like, h_like, likelihood_size);
+
+  sArray *d_start = malloc_device<sArray>(start_transitions_elem, q);
+  q.memcpy(d_start, h_start, start_transitions_size);
 
   range<1> gws (batch * (states-1));
   range<1> lws (batch);
@@ -97,26 +101,17 @@ int main(int argc, char* argv[]) {
   auto t1 = std::chrono::high_resolution_clock::now();
 
   for(int count = 0; count < repeat; count++) {
-    for (int cur_i = 1; cur_i < x_dim + 1; cur_i++) {
-      for (int cur_j = 1; cur_j < y_dim + 1; cur_j++) {
+    for (int i = 1; i < x_dim + 1; i++) {
+      for (int j = 1; j < y_dim + 1; j++) {
         q.submit([&] (handler &cgh) {
-          auto forward_matrix_in = d_cur_fw_re.get_access<sycl_read>(cgh);
-          auto forward_matrix_out = d_nxt_fw_re.get_access<sycl_write>(cgh);
-          auto emissions = d_emis_re.get_access<sycl_read>(cgh);
-          auto transitions = d_trans_re.get_access<sycl_read>(cgh);
-          auto likelihood = d_like_re.get_access<sycl_read>(cgh);
-          auto start_transitions = d_start_re.get_access<sycl_read>(cgh);
-          accessor<double, 2, sycl_read_write, access::target::local> e({batch, states-1}, cgh);
-          accessor<double, 3, sycl_read_write, access::target::local> f01({1, batch, 2}, cgh);
-          accessor<double, 3, sycl_read_write, access::target::local> mul_3d({1, batch, 2}, cgh);
-          accessor<double, 4, sycl_read_write, access::target::local> mul_4d({4, batch, 1, 2}, cgh);
           cgh.parallel_for<class pair_hmm_forward>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
-            #include "kernel.h"
+            pair_HMM_forward(item, i, j, d_cur_forward, d_trans, d_emis,
+                             d_like, d_start, d_next_forward);
           });
         });
-        auto t = std::move(d_cur_fw_re);
-        d_cur_fw_re = std::move(d_nxt_fw_re);
-        d_nxt_fw_re = std::move(t);
+        auto t = d_cur_forward;
+        d_cur_forward = d_next_forward;
+        d_next_forward = t;
       }
     }
   }
@@ -126,10 +121,7 @@ int main(int argc, char* argv[]) {
   std::chrono::duration<double, std::milli> milli = (t2 - t1);
   std::cout << "Total execution time " <<  milli.count() << " milliseconds\n" ;
 
-  q.submit([&] (handler &cgh) {
-    auto acc = d_cur_forward.get_access<sycl_read>(cgh);
-    cgh.copy(acc, h_cur_forward);
-  }).wait();
+  q.memcpy(h_cur_forward, d_cur_forward, forward_matrix_size).wait();
 
   double checkSum = 0.0;
   for (int i = 0; i < x_dim+1; i++) {
@@ -146,6 +138,12 @@ int main(int argc, char* argv[]) {
   }
   std::cout << "Checksum " << checkSum << std::endl;
 
+  free(d_cur_forward, q);
+  free(d_next_forward, q);
+  free(d_emis, q);
+  free(d_trans, q);
+  free(d_like, q);
+  free(d_start, q);
   free(h_cur_forward);
   free(h_emis);
   free(h_trans);
