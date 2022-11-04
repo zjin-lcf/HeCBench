10db85d9b2c772ebe85136ec466ea70107f966c0
Jin Z <5zj@equinox.ftpn.ornl.gov>
2022-11-04 16:03:43 -0400

    [split] replace SYCL buffers with USM for more accurate measurement of kernel timing

--- split-hip/main.cu
+++ split-hip/main.cu
@@ -196,6 +196,8 @@ int main(int argc, char* argv[])
   printf("Average kernel execution time: %f (us)\n", (time * 1e-3f) / repeat);
 
   hipMemcpy(out, d_tempKeys, N*sizeof(unsigned int), hipMemcpyDeviceToHost);
+  hipFree(d_keys);
+  hipFree(d_tempKeys);
 
   bool check = verify(out, keys, threads, N);
   if (check)
@@ -203,8 +205,6 @@ int main(int argc, char* argv[])
   else 
     printf("FAIL\n");
 
-  hipFree(d_keys);
-  hipFree(d_tempKeys);
   free(keys);
   free(out);
 
