2e5c6f5cbb9574dcec8314e6e4553efd759ce21d
Jin Z <5zj@equinox.ftpn.ornl.gov>
2022-11-09 12:53:50 -0500

    [tsa] replace SYCL buffers with USM for more accurate measurement of kernel execution time

--- tsa-sycl/kernels.h
+++ tsa-sycl/kernels.h
@@ -4,10 +4,8 @@ inline void trotter_vert_pair(
     int width, int height,
     T &cell_r, T &cell_i, 
     int kx, int ky, int py, 
-    //T rl[BLOCK_HEIGHT][BLOCK_WIDTH],
-    const accessor<T, 2, sycl_read_write, access::target::local> &rl,
-    //T im[BLOCK_HEIGHT][BLOCK_WIDTH]) 
-    const accessor<T, 2, sycl_read_write, access::target::local> &im)
+    T rl[BLOCK_HEIGHT][BLOCK_WIDTH],
+    T im[BLOCK_HEIGHT][BLOCK_WIDTH]) 
 {
   T peer_r;
   T peer_i;
@@ -29,10 +27,8 @@ inline void trotter_horz_pair(
     int width, int height,
     T &cell_r, T &cell_i, 
     int kx, int ky, int px, 
-    //T rl[BLOCK_HEIGHT][BLOCK_WIDTH],
-    const accessor<T, 2, sycl_read_write, access::target::local> &rl,
-    //T im[BLOCK_HEIGHT][BLOCK_WIDTH]) 
-    const accessor<T, 2, sycl_read_write, access::target::local> &im)
+    T rl[BLOCK_HEIGHT][BLOCK_WIDTH],
+    T im[BLOCK_HEIGHT][BLOCK_WIDTH]) 
 {
   T peer_r;
   T peer_i;
@@ -45,5 +41,154 @@ inline void trotter_horz_pair(
     im[ky][kx_peer] = a * peer_i + b * cell_r;
     cell_r = a * cell_r - b * peer_i;
     cell_i = a * cell_i + b * peer_r;
+  }
+}
+
+template<typename T, int STEPS, int BLOCK_X, int BLOCK_Y, int MARGIN_X, int MARGIN_Y, int STRIDE_Y>
+void tsa_kernel(
+    nd_item<2> &item,
+    T a, T b, int width, int height, 
+    const T * __restrict p_real,
+    const T * __restrict p_imag,
+          T * __restrict p2_real,
+          T * __restrict p2_imag) 
+{
+  multi_ptr<T[BLOCK_Y][BLOCK_X], access::address_space::local_space> rl_ptr =
+    ext::oneapi::group_local_memory_for_overwrite<T[BLOCK_Y][BLOCK_X]>(item.get_group());
+  T (*rl)[BLOCK_X] = *rl_ptr;
+
+  multi_ptr<T[BLOCK_Y][BLOCK_X], access::address_space::local_space> im_ptr =
+    ext::oneapi::group_local_memory_for_overwrite<T[BLOCK_Y][BLOCK_X]>(item.get_group());
+  T (*im)[BLOCK_X] = *im_ptr;
+
+  int blockIdx_x = item.get_group(1);
+  int blockIdx_y = item.get_group(0);
+  int threadIdx_x = item.get_local_id(1);
+  int threadIdx_y = item.get_local_id(0);
+
+  int px = blockIdx_x * (BLOCK_X - 2 * STEPS * MARGIN_X) + threadIdx_x - STEPS * MARGIN_X;
+  int py = blockIdx_y * (BLOCK_Y - 2 * STEPS * MARGIN_Y) + threadIdx_y - STEPS * MARGIN_Y;
+
+  // Read block from global into shared memory
+  if (px >= 0 && px < width) {
+    #pragma unroll
+    for (int i = 0, pidx = py * width + px; i < BLOCK_Y / STRIDE_Y; ++i, pidx += STRIDE_Y * width) {
+      if (py + i * STRIDE_Y >= 0 && py + i * STRIDE_Y < height) {
+        rl[threadIdx_y + i * STRIDE_Y][threadIdx_x] = p_real[pidx];
+        im[threadIdx_y + i * STRIDE_Y][threadIdx_x] = p_imag[pidx];
+      }
+    }
+  }
+
+  item.barrier(access::fence_space::local_space);
+
+  // Place threads along the black cells of a checkerboard pattern
+  int sx = threadIdx_x;
+  int sy;
+  if ((STEPS * MARGIN_X) % 2 == (STEPS * MARGIN_Y) % 2) {
+    sy = 2 * threadIdx_y + threadIdx_x % 2;
+  } else {
+    sy = 2 * threadIdx_y + 1 - threadIdx_x % 2;
+  }
+
+  // global y coordinate of the thread on the checkerboard (px remains the same)
+  // used for range checks
+  int checkerboard_py = blockIdx_y * (BLOCK_Y - 2 * STEPS * MARGIN_Y) + sy - STEPS * MARGIN_Y;
+
+  // keep the fixed black cells on registers, reds are updated in shared memory
+  T cell_r[BLOCK_Y / (STRIDE_Y * 2)];
+  T cell_i[BLOCK_Y / (STRIDE_Y * 2)];
+
+  // read black cells to registers
+  #pragma unroll
+  for (int part = 0; part < BLOCK_Y / (STRIDE_Y * 2); ++part) {
+    cell_r[part] = rl[sy + part * 2 * STRIDE_Y][sx];
+    cell_i[part] = im[sy + part * 2 * STRIDE_Y][sx];
+  }
+
+  // update cells STEPS full steps
+  #pragma unroll
+  for (int i = 0; i < STEPS; i++) {
+    // 12344321
+    #pragma unroll
+    for (int part = 0; part < BLOCK_Y / (STRIDE_Y * 2); ++part) {
+      trotter_vert_pair<T, BLOCK_X, BLOCK_Y, STEPS * MARGIN_X, STEPS * MARGIN_Y, 0>(
+          a, b, width, height, cell_r[part], cell_i[part], 
+          sx, sy + part * 2 * STRIDE_Y, checkerboard_py + part * 2 * STRIDE_Y, rl, im);
+    }
+    item.barrier(access::fence_space::local_space);
+    #pragma unroll
+    for (int part = 0; part < BLOCK_Y / (STRIDE_Y * 2); ++part) {
+      trotter_horz_pair<T, BLOCK_X, BLOCK_Y, STEPS * MARGIN_X, STEPS * MARGIN_Y, 0>(
+          a, b, width, height, cell_r[part], cell_i[part],
+          sx, sy + part * 2 * STRIDE_Y, px, rl, im);
+    }
+    item.barrier(access::fence_space::local_space);
+    #pragma unroll
+    for (int part = 0; part < BLOCK_Y / (STRIDE_Y * 2); ++part) {
+      trotter_vert_pair<T, BLOCK_X, BLOCK_Y, STEPS * MARGIN_X, STEPS * MARGIN_Y, 1>(
+          a, b, width, height, cell_r[part], cell_i[part], 
+          sx, sy + part * 2 * STRIDE_Y, checkerboard_py + part * 2 * STRIDE_Y, rl, im);
+    }
+    item.barrier(access::fence_space::local_space);
+    #pragma unroll
+    for (int part = 0; part < BLOCK_Y / (STRIDE_Y * 2); ++part) {
+      trotter_horz_pair<T, BLOCK_X, BLOCK_Y, STEPS * MARGIN_X, STEPS * MARGIN_Y, 1>(
+          a, b, width, height, cell_r[part], cell_i[part],
+          sx, sy + part * 2 * STRIDE_Y, px, rl, im);
+    }
+    item.barrier(access::fence_space::local_space);
+
+    #pragma unroll
+    for (int part = 0; part < BLOCK_Y / (STRIDE_Y * 2); ++part) {
+      trotter_horz_pair<T, BLOCK_X, BLOCK_Y, STEPS * MARGIN_X, STEPS * MARGIN_Y, 1>(
+          a, b, width, height, cell_r[part], cell_i[part],
+          sx, sy + part * 2 * STRIDE_Y, px, rl, im);
+    }
+    item.barrier(access::fence_space::local_space);
+    #pragma unroll
+    for (int part = 0; part < BLOCK_Y / (STRIDE_Y * 2); ++part) {
+      trotter_vert_pair<T, BLOCK_X, BLOCK_Y, STEPS * MARGIN_X, STEPS * MARGIN_Y, 1>(
+          a, b, width, height, cell_r[part], cell_i[part],
+          sx, sy + part * 2 * STRIDE_Y, checkerboard_py + part * 2 * STRIDE_Y, rl, im);
+    }
+    item.barrier(access::fence_space::local_space);
+    #pragma unroll
+    for (int part = 0; part < BLOCK_Y / (STRIDE_Y * 2); ++part) {
+      trotter_horz_pair<T, BLOCK_X, BLOCK_Y, STEPS * MARGIN_X, STEPS * MARGIN_Y, 0>(
+          a, b, width, height, cell_r[part], cell_i[part],
+          sx, sy + part * 2 * STRIDE_Y, px, rl, im);
+    }
+    item.barrier(access::fence_space::local_space);
+    #pragma unroll
+    for (int part = 0; part < BLOCK_Y / (STRIDE_Y * 2); ++part) {
+      trotter_vert_pair<T, BLOCK_X, BLOCK_Y, STEPS * MARGIN_X, STEPS * MARGIN_Y, 0>
+        (a, b, width, height, cell_r[part], cell_i[part], 
+         sx, sy + part * 2 * STRIDE_Y, checkerboard_py + part * 2 * STRIDE_Y, rl, im);
+    }
+    item.barrier(access::fence_space::local_space);
+  }
+
+  // write black cells in registers to shared memory
+  #pragma unroll
+  for (int part = 0; part < BLOCK_Y / (STRIDE_Y * 2); ++part) {
+    rl[sy + part * 2 * STRIDE_Y][sx] = cell_r[part];
+    im[sy + part * 2 * STRIDE_Y][sx] = cell_i[part];
+  }
+  item.barrier(access::fence_space::local_space);
+
+  // discard the halo and copy results from shared to global memory
+  sx = threadIdx_x + STEPS * MARGIN_X;
+  sy = threadIdx_y + STEPS * MARGIN_Y;
+  px += STEPS * MARGIN_X;
+  py += STEPS * MARGIN_Y;
+  if (sx < BLOCK_X - STEPS * MARGIN_X && px < width) {
+    #pragma unroll
+    for (int i = 0, pidx = py * width + px; i < BLOCK_Y / STRIDE_Y; ++i, pidx += STRIDE_Y * width) {
+      if (sy + i * STRIDE_Y < BLOCK_Y - STEPS * MARGIN_Y && py + i * STRIDE_Y < height) {
+        p2_real[pidx] = rl[sy + i * STRIDE_Y][sx];
+        p2_imag[pidx] = im[sy + i * STRIDE_Y][sx];
+      }
+    }
   }
 }
