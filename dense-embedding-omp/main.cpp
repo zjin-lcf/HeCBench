ed494ac52cd763f9165d940fcecd1253d4490af0
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-02-07 13:01:03 -0500

    [dense-embedding] add the dense embedding operations examples

---
+++ dense-embedding-omp/main.cpp
@@ -0,0 +1,158 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <random>
+#include <omp.h>
+
+template <typename T>
+void reference(
+    const T* input,
+    const T* dense,
+    T* output,
+    int embedding_dim,
+    int batch_size,
+    const int* offset)
+{
+  for (int batch_idx = 0; batch_idx < batch_size; batch_idx++) {
+    const int range = offset[batch_idx + 1] - offset[batch_idx];
+    for (int idx = 0; idx < embedding_dim; idx++) {
+      const T dense_elem = dense[batch_idx * embedding_dim + idx];
+      for (int nested_idx = idx; nested_idx < range; nested_idx += embedding_dim) {
+        output[offset[batch_idx] + nested_idx] = 
+          input[offset[batch_idx] + nested_idx] + dense_elem;
+      }
+    }
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 4) {
+    printf("Usage: %s <number of rows> <batch size> <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int nrows = atoi(argv[1]);
+  const int batch_size = atoi(argv[2]);
+  const int repeat = atoi(argv[3]);
+  assert(nrows > batch_size * batch_size);
+
+  printf("Number of rows in the embedding table: %d\n", nrows);
+  printf("Batch size: %d\n", batch_size);
+
+  for (int ncols = 64; ncols <= 2048; ncols = ncols * 2) {
+
+    printf("\nEmbedding dimension: %d\n", ncols);
+
+    int input_size = nrows * ncols;  // same as output size
+    size_t input_size_bytes = input_size * sizeof(float);
+
+    int dense_size = batch_size * ncols ;
+    int dense_size_bytes = dense_size * sizeof(float);
+
+    int batch_size_bytes = (batch_size + 1) * sizeof(float);
+
+    float *input, *dense, *output, *output_ref;
+    input = (float*) malloc (input_size_bytes); // [sum(*) x embedding_dim]
+    dense = (float*) malloc (dense_size_bytes); // [batch_size x embedding_dim]
+    output = (float*) malloc (input_size_bytes); // [sum(*) x embedding_dim]
+    output_ref = (float*) malloc (input_size_bytes); // [sum(*) x embedding_dim]
+    int *offset = (int*) malloc (batch_size_bytes);  // [batch_size]
+
+    // meet a few conditions:
+    // range > ncols
+    // range + offset[i] < input_size
+    // offset[i] > offset[i-1]
+    srand(123);
+    offset[0] = 0;
+    for (int i = 1; i <= batch_size; i++)
+      offset[i] = offset[i-1] + (rand() % batch_size + 1) * ncols;
+
+    std::default_random_engine g (123);
+    std::uniform_real_distribution<float> distr (-1.f, 1.f);
+    for (int i = 0; i < dense_size; i++) {
+      dense[i] = distr(g);
+    }
+
+    for (int i = 0; i < input_size; i++) {
+      input[i] = distr(g);
+      output[i] = output_ref[i] = 0;
+    }
+
+    reference(input, dense, output_ref, ncols, batch_size, offset);
+
+    #pragma omp target data map(to: input[0:input_size], \
+                                    dense[0:dense_size], \
+                                    offset[0:batch_size+1], \
+                                    output[0:input_size])
+    {
+      auto start = std::chrono::steady_clock::now();
+
+      for (int i = 0; i < repeat; i++) {
+        #pragma omp target teams num_teams(batch_size) thread_limit(256)
+        {
+          #pragma omp parallel 
+          {
+            const int batch_idx  = omp_get_team_num(); // each batch is handled by a block
+            const int grain_size = omp_get_num_threads();
+            const int tid = omp_get_thread_num();
+            const int range = offset[batch_idx + 1] - offset[batch_idx];
+            for (int idx = tid; idx < ncols; idx += grain_size) {
+              const auto dense_elem = dense[batch_idx * ncols + idx];
+              for (int nested_idx = idx; nested_idx < range; nested_idx += ncols) {
+                output[offset[batch_idx] + nested_idx] = input[offset[batch_idx] + nested_idx] + dense_elem;
+              }
+            }
+          }
+        }
+      }
+
+      auto end = std::chrono::steady_clock::now();
+      auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+      printf("Average execution time of dense embedding kernel (k1): %f (us)\n", (time * 1e-3f) / repeat);
+
+      start = std::chrono::steady_clock::now();
+
+      for (int i = 0; i < repeat; i++) {
+        #pragma omp target teams num_teams(batch_size) thread_limit(256)
+        {
+          #pragma omp parallel 
+          {
+            const int batch_idx  = omp_get_team_num(); // each batch is handled by a block
+            const int start = offset[batch_idx];
+            const int range = offset[batch_idx + 1] - start;
+            for (int idx = omp_get_thread_num(); idx < ncols; idx += omp_get_num_threads()) {
+              const auto dense_elem = dense[batch_idx * ncols + idx];
+              for (int nested_idx = idx; nested_idx < range; nested_idx += ncols) {
+                output[start + nested_idx] = input[start + nested_idx] + dense_elem;
+              }
+            }
+          }
+        }  
+      }  
+
+      end = std::chrono::steady_clock::now();
+      time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+      printf("Average execution time of dense embedding kernel (k2): %f (us)\n", (time * 1e-3f) / repeat);
+
+      #pragma omp target update from (output[0:input_size])
+
+      bool ok = true;
+      for (int i = 0; i < input_size; i++) {
+        if (fabsf(output[i] - output_ref[i]) > 1e-3f) {
+          ok = false;
+          break;
+        }
+      }
+      printf("%s\n", ok ? "PASS" : "FAIL");
+    }
+
+    free(input);
+    free(dense);
+    free(output);
+    free(output_ref);
+    free(offset);
+  }
+
+  return 0;
+}
---
+++ dense-embedding-sycl/main.cpp
@@ -0,0 +1,207 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <random>
+#include "common.h"
+
+template <typename T>
+void reference(
+    const T* input,
+    const T* dense,
+    T* output,
+    int embedding_dim,
+    int batch_size,
+    const int* offset)
+{
+  for (int batch_idx = 0; batch_idx < batch_size; batch_idx++) {
+    const int range = offset[batch_idx + 1] - offset[batch_idx];
+    for (int idx = 0; idx < embedding_dim; idx++) {
+      const T dense_elem = dense[batch_idx * embedding_dim + idx];
+      for (int nested_idx = idx; nested_idx < range; nested_idx += embedding_dim) {
+        output[offset[batch_idx] + nested_idx] = 
+          input[offset[batch_idx] + nested_idx] + dense_elem;
+      }
+    }
+  }
+}
+
+template <typename T>
+void dense_esuhm(
+    nd_item<1> &item,
+    const T* input,
+    const T* dense,
+          T* output,
+    int embedding_dim,
+    const int* offset)
+{
+  const int batch_idx  = item.get_group(0); // each batch is handled by a block
+  const int grain_size = item.get_local_range(0);
+  const int tid = item.get_local_id(0);
+  const int range = offset[batch_idx + 1] - offset[batch_idx];
+  for (int idx = tid; idx < embedding_dim; idx += grain_size) {
+    const T dense_elem = dense[batch_idx * embedding_dim + idx];
+    for (int nested_idx = idx; nested_idx < range; nested_idx += embedding_dim) {
+      output[offset[batch_idx] + nested_idx] =
+        input[offset[batch_idx] + nested_idx] + dense_elem;
+    }
+  }
+}
+
+template <typename T>
+void dense_esuhm2(
+    nd_item<1> &item,
+    const T* input,
+    const T* dense,
+          T* output,
+    int embedding_dim,
+    const int* offset)
+{
+  const int batch_idx = item.get_group(0);
+  const int start = offset[batch_idx];
+  const int range = offset[batch_idx + 1] - start;
+  for (int idx = item.get_local_id(0); idx < embedding_dim;
+           idx += item.get_local_range(0)) {
+    const T dense_elem = dense[batch_idx * embedding_dim + idx];
+    for (int nested_idx = idx; nested_idx < range; nested_idx += embedding_dim) {
+      output[start + nested_idx] = input[start + nested_idx] + dense_elem;
+    }
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 4) {
+    printf("Usage: %s <number of rows> <batch size> <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int nrows = atoi(argv[1]);
+  const int batch_size = atoi(argv[2]);
+  const int repeat = atoi(argv[3]);
+  assert(nrows > batch_size * batch_size);
+
+  printf("Number of rows in the embedding table: %d\n", nrows);
+  printf("Batch size: %d\n", batch_size);
+
+#ifdef USE_GPU
+  gpu_selector dev_sel;
+#else
+  cpu_selector dev_sel;
+#endif
+  queue q(dev_sel);
+
+  for (int ncols = 64; ncols <= 2048; ncols = ncols * 2) {
+
+    printf("\nEmbedding dimension: %d\n", ncols);
+
+    int input_size = nrows * ncols;  // same as output size
+    size_t input_size_bytes = input_size * sizeof(float);
+
+    int dense_size = batch_size * ncols ;
+    int dense_size_bytes = dense_size * sizeof(float);
+
+    int batch_size_bytes = (batch_size + 1) * sizeof(float);
+
+    float *input, *dense, *output, *output_ref;
+    input = (float*) malloc (input_size_bytes); // [sum(*) x embedding_dim]
+    dense = (float*) malloc (dense_size_bytes); // [batch_size x embedding_dim]
+    output = (float*) malloc (input_size_bytes); // [sum(*) x embedding_dim]
+    output_ref = (float*) malloc (input_size_bytes); // [sum(*) x embedding_dim]
+    int *input_offset = (int*) malloc (batch_size_bytes);  // [batch_size]
+
+    // meet a few conditions:
+    // range > ncols
+    // range + offset[i] < input_size
+    // offset[i] > offset[i-1]
+    srand(123);
+    input_offset[0] = 0;
+    for (int i = 1; i <= batch_size; i++)
+      input_offset[i] = input_offset[i-1] + (rand() % batch_size + 1) * ncols;
+
+    std::default_random_engine g (123);
+    std::uniform_real_distribution<float> distr (-1.f, 1.f);
+    for (int i = 0; i < dense_size; i++) {
+      dense[i] = distr(g);
+    }
+
+    for (int i = 0; i < input_size; i++) {
+      input[i] = distr(g);
+      output_ref[i] = 0;
+    }
+
+    reference(input, dense, output_ref, ncols, batch_size, input_offset);
+
+    float *d_input, *d_dense, *d_output;
+    d_input = malloc_device<float>(input_size, q);
+    q.memcpy(d_input, input, input_size_bytes);
+    
+    d_dense = malloc_device<float>(dense_size, q);
+    q.memcpy(d_dense, dense, dense_size_bytes);
+
+    d_output = malloc_device<float>(input_size, q);
+    q.memset(d_output, 0, input_size_bytes);
+
+    int* d_input_offset;
+    d_input_offset = malloc_device<int>(batch_size+1, q);
+    q.memcpy(d_input_offset, input_offset, batch_size_bytes);
+    
+    range<1> gws (batch_size * 256);
+    range<1> lws (256);
+
+    q.wait();
+    auto start = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < repeat; i++) {
+      q.submit([&] (handler &cgh) {
+        cgh.parallel_for<class de1>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+          dense_esuhm(item, d_input, d_dense, d_output, ncols, d_input_offset);
+        });
+      });
+    }
+
+    q.wait();
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Average execution time of dense embedding kernel (k1): %f (us)\n", (time * 1e-3f) / repeat);
+
+    q.wait();
+    start = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < repeat; i++) {
+      q.submit([&] (handler &cgh) {
+        cgh.parallel_for<class de2>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+          dense_esuhm2(item, d_input, d_dense, d_output, ncols, d_input_offset);
+        });
+      });
+    }
+
+    q.wait();
+    end = std::chrono::steady_clock::now();
+    time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Average execution time of dense embedding kernel (k2): %f (us)\n", (time * 1e-3f) / repeat);
+
+    q.memcpy(output, d_output, input_size_bytes).wait();
+
+    bool ok = true;
+    for (int i = 0; i < input_size; i++) {
+      if (fabsf(output[i] - output_ref[i]) > 1e-3f) {
+        ok = false;
+        break;
+      }
+    }
+    printf("%s\n", ok ? "PASS" : "FAIL");
+
+    free(d_input, q);
+    free(d_dense, q);
+    free(d_output, q);
+    free(d_input_offset, q);
+
+    free(input);
+    free(dense);
+    free(output);
+    free(output_ref);
+    free(input_offset);
+  }
+
+  return 0;
+}
