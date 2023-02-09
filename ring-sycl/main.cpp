2aa60a415e0770e4a9ac983be9a7e01587592799
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2023-02-09 12:19:03 -0500

    [ring] add the examples for data transfers in a ring (non-P2P)

---
+++ ring-sycl/main.cpp
@@ -0,0 +1,93 @@
+#include <chrono>
+#include <cstdio>
+#include <cstring>
+#include <numeric>  // iota
+#include <vector>
+#include <sycl/sycl.hpp>
+
+// Send data in a circular manner in all GPU devices (non-P2P)
+// Original author: Thomas Applencourt
+
+
+int main(int argc, char* argv[])
+{
+  if (argc != 4) {
+    printf("Usage: %s <minimum copy length> <maximum copy length> <repeat>\n", argv[0]);
+    return 1;
+  }
+  const long min_len = atol(argv[1]);
+  const long max_len = atol(argv[2]);
+  const int repeat = atoi(argv[3]);
+
+  auto const& gpu_devices = sycl::device::get_devices(sycl::info::device_type::gpu);
+  for(const auto& d : gpu_devices) {
+    printf("Device name: %s\n", d.get_info<sycl::info::device::name>().c_str());
+  }
+
+  const int num_devices = gpu_devices.size();
+  if (num_devices <= 1) {
+    printf("Warning, only one device is detected."
+           "This program is supposed to execute with multiple devices.\n");
+  }
+
+  // Can't add devices across platforms to a single context
+  // sycl::context same_ctxt = sycl::context(gpu_devices);
+  std::vector<sycl::queue> queues;
+
+  // Alocate memory to each device. Each queue share the same context
+  for (int i = 0; i < num_devices; i++) {
+    //queues.push_back(sycl::queue(same_ctxt, gpu_devices[i]) );
+    queues.push_back(sycl::queue(gpu_devices[i]));
+  }
+
+  for (long len = min_len; len <= max_len; len = len * 4) {
+    std::vector<int *> device_ptr;
+
+    for (int i = 0; i < num_devices; i++) {
+      int *ptr = sycl::malloc_device<int>(len, queues[i]);
+      device_ptr.push_back(ptr);
+    }
+
+    // Allocate host data, and set the value
+    std::vector<int> host_ptr(len);
+    std::iota(host_ptr.begin(), host_ptr.end(), 0);
+
+    // Copy the data to the first device, and 0ed the host data
+    const size_t data_size_bytes = len * sizeof(int);
+    queues[0].memcpy(device_ptr[0], host_ptr.data(), data_size_bytes).wait();
+    std::memset(host_ptr.data(), 0, data_size_bytes);
+
+    auto start = std::chrono::steady_clock::now();
+
+    // The circular exchange
+    for (int n = 0; n < repeat; n++) {
+      for (int i = 0; i < num_devices; i++) {
+        int *src = device_ptr[i];
+        int *dst = device_ptr[ ( i+1 ) % num_devices ];
+        queues[i].memcpy(dst, src, data_size_bytes).wait();
+      }
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Total circular exchange time (length = %ld): %f (us)\n",
+           len, time * 1e-3f / repeat);
+
+    // Copy back data and check for correctness
+    queues[0].memcpy(host_ptr.data(), device_ptr[0], data_size_bytes).wait();
+
+    bool ok = true;
+    for(int i = 0; i < len; i++) {
+      if (host_ptr[i] != i) {
+        ok = false;
+        break;
+      }
+    }
+    printf("%s\n", ok ? "PASS" : "FAIL");
+
+    for (int i = 0; i < num_devices; i++) {
+      free(device_ptr[i], queues[i]);
+    }
+  }
+  return 0;
+}
