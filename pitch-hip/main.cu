8a14e073e3c56eff6691f4754b98d1a700c4731a
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-02-10 17:44:13 -0500

    [pitch] add the examples to evaluate pitched memory allocation

---
+++ pitch-hip/main.cu
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <hip/hip_runtime.h>
+
+__device__
+float sigmoid (float x) {
+  return (1.f / (1.f + expf(-x)));
+}
+
+__global__
+void parallelPitched2DAccess (float* devPtr, size_t pitch, int width, int height)
+{
+  int r = blockIdx.y * blockDim.y + threadIdx.y;
+  int c = blockIdx.x * blockDim.x + threadIdx.x;
+  if (r < height && c < width) {
+    float* row = (float*)((char*)devPtr + r * pitch);
+    row[c] = sigmoid(row[c]);
+  }
+}
+
+__global__
+void parallelSimple2DAccess (float* elem, int width, int height)
+{
+  int r = blockIdx.y * blockDim.y + threadIdx.y;
+  int c = blockIdx.x * blockDim.x + threadIdx.x;
+  if (r < height && c < width) {
+    elem[r * width + c] = sigmoid(elem[r * width + c]);
+  }
+}
+
+__global__
+void parallelPitched3DAccess (hipPitchedPtr devPitchedPtr, int width, int height, int depth)
+{
+  int z = blockIdx.z * blockDim.z + threadIdx.z;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  if (z < depth && y < height && x < width) {
+    char* devPtr = (char*)devPitchedPtr.ptr;
+    size_t pitch = devPitchedPtr.pitch;
+    size_t slicePitch = pitch * height;
+    char* slice = devPtr + z * slicePitch;
+    float* row = (float*)(slice + y * pitch);
+    row[x] = sigmoid(row[x]);
+  }
+}
+
+__global__
+void parallelSimple3DAccess (float* elem, int width, int height, int depth)
+{
+  int z = blockIdx.z * blockDim.z + threadIdx.z;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  if (z < depth && y < height && x < width) {
+    float element = elem[z * height * width + y * width + x];
+    elem[z * height * width + y * width + x] = sigmoid(element);
+  }
+}
+
+// Host code
+void malloc2D (int repeat, int width, int height) {
+  printf("Dimension: (%d %d)\n", width, height);
+
+  dim3 grid ((width + 15)/16, (height + 15)/16);
+  dim3 block (16, 16);
+
+  float* devPtr;
+  size_t pitch;
+  hipMallocPitch((void**)&devPtr, &pitch, width * sizeof(float), height);
+
+  parallelPitched2DAccess<<<grid, block>>>(devPtr, pitch, width, height);
+  hipDeviceSynchronize();
+
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++)
+    parallelPitched2DAccess<<<grid, block>>>(devPtr, pitch, width, height);
+
+  hipDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+  hipFree(devPtr);
+
+  hipMalloc((void**)&devPtr, width * height * sizeof(float));
+
+  parallelSimple2DAccess<<<grid, block>>>(devPtr, width, height);
+  hipDeviceSynchronize();
+
+  start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++)
+    parallelSimple2DAccess<<<grid, block>>>(devPtr, width, height);
+
+  hipDeviceSynchronize();
+  end = std::chrono::steady_clock::now();
+  auto time2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time (pitched vs simple): %f %f (us)\n",
+          (time * 1e-3f) / repeat, (time2 * 1e-3f) / repeat);
+
+  hipFree(devPtr);
+}
+
+
+// Host code
+void malloc3D (int repeat, int width, int height, int depth) {
+  printf("Dimension: (%d %d %d)\n", width, height, depth);
+  dim3 grid ((width + 15)/16, (height + 7)/8, (depth + 3)/4);
+  dim3 block (16, 8, 4);
+
+  hipExtent extent = make_hipExtent(width * sizeof(float), height, depth);
+  hipPitchedPtr devPitchedPtr;
+  hipMalloc3D(&devPitchedPtr, extent);
+
+  parallelPitched3DAccess<<<grid, block>>>(devPitchedPtr, width, height, depth);
+  hipDeviceSynchronize();
+
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++)
+    parallelPitched3DAccess<<<grid, block>>>(devPitchedPtr, width, height, depth);
+
+  hipDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+  hipFree(devPitchedPtr.ptr);
+
+  float* devPtr;
+  hipMalloc(&devPtr, width * height * depth * sizeof(float));
+
+  parallelSimple3DAccess<<<grid, block>>>(devPtr, width, height, depth);
+  hipDeviceSynchronize();
+
+  start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++)
+    parallelSimple3DAccess<<<grid, block>>>(devPtr, width, height, depth);
+
+  hipDeviceSynchronize();
+  end = std::chrono::steady_clock::now();
+  auto time2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time (pitched vs simple): %f %f (us)\n",
+          (time * 1e-3f) / repeat, (time2 * 1e-3f) / repeat);
+
+  hipFree(devPtr);
+}
+
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2) {
+    printf("Usage: %s <repeat>\n", argv[0]);
+    return 1;
+  }
+
+  const int repeat = atoi(argv[1]);
+
+  // width, height and depth
+  const int w[] = {227, 256, 720, 768, 854, 1280, 1440, 1920, 2048, 3840, 4096};
+  const int h[] = {227, 256, 480, 576, 480, 720, 1080, 1080, 1080, 2160, 2160};
+  const int d[] = {1, 3};
+
+  for (int i = 0; i < 11; i++)
+    malloc2D(repeat, w[i], h[i]);
+
+  for (int i = 0; i < 11; i++)
+    for (int j = 0; j < 2; j++)
+      malloc3D(repeat, w[i], h[i], d[j]);
+
+  return 0;
+}
