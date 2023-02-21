d9a70a64d0575974e828fb203579b112d2c61a79
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-02-21 12:25:44 -0500

    [alp] replace shared memory allocation; always check host and device results; only measure kernel time; add the OpenMP example

--- alp-hip/main.cu
+++ alp-hip/main.cu
@@ -5,43 +5,62 @@
 #include "reference.h"
 #include "kernels.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+  if (argc != 2) {
+    printf("Usage: %s <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int repeat = atoi(argv[1]);
+
   double costheta = 0.3;
   int lmax = LMAX;
-  #ifdef CHECK
   double h_plm[NDLM];
-  #endif
+  double r_plm[NDLM]; // reference
 
-  auto start = std::chrono::steady_clock::now();
 
   double *d_plm;
-  hipMallocManaged(&d_plm, NDLM * sizeof(double));
+  hipMalloc(&d_plm, NDLM * sizeof(double));
 
+  // warmup up and check results
+  bool ok = true;
   for (int l = 0; l <= lmax; l++) { 
-    hipLaunchKernelGGL(associatedLegendre, 1, LMAX, 0, 0, costheta,l,d_plm);
+    associatedLegendre<<<1, LMAX>>>(costheta,l,d_plm);
 
-    hipDeviceSynchronize();
-    
-    #ifdef CHECK
     // compute on host
-    associatedLegendreFunctionNormalized<double>(costheta,l,h_plm);
+    associatedLegendreFunctionNormalized<double>(costheta,l,r_plm);
+
+    hipMemcpy(h_plm, d_plm, NDLM * sizeof(double), hipMemcpyDeviceToHost);  
 
     for(int i = 0; i <= l; i++)
     {
-      if(fabs(h_plm[i] - d_plm[i]) > 1e-6)
+      if(fabs(h_plm[i] - r_plm[i]) > 1e-6)
       {
-        fprintf(stderr, "%d: %lf != %lf\n", i, h_plm[i], d_plm[i]);
+        fprintf(stderr, "%d: %lf != %lf\n", i, h_plm[i], r_plm[i]);
+        ok = false;
         break;
       }
     }
-    #endif
   }
-  hipFree(d_plm);
+
+  printf("%s\n", ok ? "PASS" : "FAIL");
+
+  // performance measurement
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    for (int l = 0; l <= lmax; l++)
+      associatedLegendre<<<1, LMAX>>>(costheta,l,d_plm);
+  }
+
+  hipDeviceSynchronize();
 
   auto end = std::chrono::steady_clock::now();
   auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  printf("Total execution time %f (s)\n", time * 1e-9f);
+  printf("Average execution time of associatedLegendre kernels %f (us)\n",
+         time * 1e-3f / repeat);
+
+  hipFree(d_plm);
 
   return 0;
 }
