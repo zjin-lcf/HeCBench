7e21dd0fc26d7a4022d4a5695925786a585fb36b
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-03-10 22:50:07 -0500

    [unfold] add the unfold examples in which the kernel is implemented as a lambda function; unlike the Pytorch doc on unfold, the number of elements in the input and output 1D arrays are the same when size = 2 and step = 1

---
+++ unfold-hip/main.cu
@@ -0,0 +1,152 @@
+#include <cstdio>
+#include <cstdlib>
+#include <chrono>
+#include <hip/hip_runtime.h>
+
+template <int n_threads, int n_elems_per_thread, typename func_t>
+__global__
+void unfold_backward_elementwise_kernel(int total_n_elems, func_t f) {
+  constexpr int total_work_block = n_threads * n_elems_per_thread;
+  int idx = total_work_block * blockIdx.x + threadIdx.x;
+  #pragma unroll
+  for (int i = 0; i < n_elems_per_thread; ++i) {
+    if (idx < total_n_elems) {
+      f(idx);
+      idx += n_threads;
+    }
+  }
+}
+
+template <int n_threads, int n_elems_per_thread, typename func_t>
+static void launch_unfold_backward_kernel(int total_n_elems, func_t f) {
+  const int total_work_block = n_threads * n_elems_per_thread;
+  dim3 grid((total_n_elems + total_work_block - 1) / total_work_block);
+  dim3 block(n_threads);
+
+  unfold_backward_elementwise_kernel<n_threads, n_elems_per_thread, func_t>
+    <<<grid, block>>>(total_n_elems, f);
+}
+
+template <typename scalar_t>
+void unfold_backward_internal_kernel(
+        scalar_t* __restrict__ grad_out_ptr,
+  const scalar_t* __restrict__ grad_in_ptr,
+  const int64_t* __restrict__ idx_dim_ptr,
+  int64_t size,
+  int64_t step,
+  int64_t grad_in_dim_stride,
+  int64_t grad_in_last_dim_stride,
+  int64_t grad_in_dim_size,
+  int64_t grad_out_dim_stride) 
+{
+  // For each index in grad_out find
+  // the elements contributing to it and sum them up.
+  // Note: the algorithm does not require any synchronization.
+  auto loop = [=] __device__ (int i) {
+    int64_t offsets[] = {i, i, 0};
+
+    auto* __restrict__ grad_out_data = grad_out_ptr + offsets[0];
+    auto* __restrict__ grad_in_data = grad_in_ptr + offsets[1];
+    auto idx_dim = *(idx_dim_ptr + offsets[2]);
+
+    // left_fold potentially intersecting with idx_dim
+    // is either (idx_dim - size) / step or the next integer.
+    int64_t left_fold_idx = (idx_dim > size) ? (idx_dim - size) / step : 0;
+    if (!(left_fold_idx * step <= idx_dim && idx_dim < left_fold_idx * step + size)) {
+      ++left_fold_idx;
+    }
+
+    auto right_fold_idx = idx_dim / step;
+    right_fold_idx = (right_fold_idx >= grad_in_dim_size) ?
+                     (grad_in_dim_size - 1) : right_fold_idx;
+
+    for (auto fold_idx = left_fold_idx; fold_idx <= right_fold_idx; ++fold_idx) {
+      auto idx_last_dim = idx_dim - fold_idx * step;
+      *grad_out_data += grad_in_data[fold_idx * grad_in_dim_stride +
+                                     idx_last_dim * grad_in_last_dim_stride];
+    }
+  };
+
+  launch_unfold_backward_kernel<64, 4>(grad_in_dim_size, loop);
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 3) {
+    printf("Usage: %s <number of elements> <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int nelem = atoi(argv[1]);
+  const int repeat = atoi(argv[2]);
+
+  const int64_t size = 2;
+  const int64_t step = 1;
+  const int64_t grad_in_dim_stride = 1;
+  const int64_t grad_in_last_dim_stride = 1;
+  const int64_t grad_in_dim_size = nelem;
+  const int64_t grad_out_dim_stride = 1;
+
+  using scalar_t = int;
+
+  size_t input_size_bytes = grad_in_dim_size * sizeof(scalar_t);
+  size_t output_size_bytes = grad_in_dim_size * sizeof(scalar_t);
+
+  scalar_t *h_grad_in = (scalar_t*) malloc (input_size_bytes);
+  scalar_t *h_grad_out = (scalar_t*) malloc (output_size_bytes);
+
+  srand(123);
+  for (int i = 0; i < grad_in_dim_size; i++) {
+    h_grad_in[i] = rand() % 256;
+  }
+
+  const int64_t h_idx_dim = 0; // one dimension
+
+  scalar_t *d_grad_in, *d_grad_out;
+  hipMalloc((void**)&d_grad_in, input_size_bytes);
+  hipMemcpy(d_grad_in, h_grad_in, input_size_bytes, hipMemcpyHostToDevice); 
+
+  int64_t *d_idx_dim;
+  hipMalloc((void**)&d_idx_dim, sizeof(int64_t));
+  hipMemcpy(d_idx_dim, &h_idx_dim, sizeof(int64_t), hipMemcpyHostToDevice); 
+
+  hipMalloc((void**)&d_grad_out, output_size_bytes);
+
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    unfold_backward_internal_kernel(d_grad_out,
+                                    d_grad_in,
+                                    d_idx_dim,
+                                    size,
+                                    step,
+                                    grad_in_dim_stride,
+                                    grad_in_last_dim_stride,
+                                    grad_in_dim_size,
+                                    grad_out_dim_stride);
+  }
+  hipDeviceSynchronize();
+
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time of unfold backward kernel: %f (us)\n", (time * 1e-3f) / repeat);
+
+  hipMemcpy(h_grad_out, d_grad_out, output_size_bytes, hipMemcpyDeviceToHost); 
+
+  bool ok = true;
+  for (int i = 0; i < grad_in_dim_size; i++) {
+    if (repeat * h_grad_in[i] != h_grad_out[i]) {
+      ok = false;
+      break;
+    }
+  }
+  printf("%s\n", ok ? "PASS" : "FAIL");
+ 
+  hipFree(d_grad_in);
+  hipFree(d_grad_out);
+  hipFree(d_idx_dim);
+
+  free(h_grad_in);
+  free(h_grad_out);
+
+  return 0;
+}
---
+++ unfold-sycl/main.cpp
@@ -0,0 +1,167 @@
+#include <cstdio>
+#include <cstdlib>
+#include <chrono>
+#include "common.h"
+
+template <int n_threads, int n_elems_per_thread, typename func_t>
+void unfold_backward_elementwise_kernel(int total_n_elems, func_t f,
+                                        const nd_item<1> &item) {
+  constexpr int total_work_block = n_threads * n_elems_per_thread;
+  int idx = total_work_block * item.get_group(0) + item.get_local_id(0);
+  #pragma unroll
+  for (int i = 0; i < n_elems_per_thread; ++i) {
+    if (idx < total_n_elems) {
+      f(idx);
+      idx += n_threads;
+    }
+  }
+}
+
+template <int n_threads, int n_elems_per_thread, typename func_t>
+static void launch_unfold_backward_kernel(queue &q, int total_n_elems, func_t f) {
+  const int total_work_block = n_threads * n_elems_per_thread;
+
+  range<1> lws (n_threads);
+  range<1> gws ((total_n_elems + total_work_block - 1) /
+                total_work_block * n_threads);
+
+  q.submit([&] (handler &cgh) {
+    cgh.parallel_for(nd_range<1>(gws, lws), [=](nd_item<1> item) {
+      unfold_backward_elementwise_kernel
+        <n_threads, n_elems_per_thread, func_t>(total_n_elems, f, item);
+    });
+  });
+}
+
+template <typename scalar_t>
+void unfold_backward_internal_kernel(
+  queue &q,
+  scalar_t* __restrict__ grad_out_ptr,
+  const scalar_t* __restrict__ grad_in_ptr,
+  const int64_t* __restrict__ idx_dim_ptr,
+  int64_t size,
+  int64_t step,
+  int64_t grad_in_dim_stride,
+  int64_t grad_in_last_dim_stride,
+  int64_t grad_in_dim_size,
+  int64_t grad_out_dim_stride) 
+{
+  // For each index in grad_out find
+  // the elements contributing to it and sum them up.
+  // Note: the algorithm does not require any synchronization.
+  auto loop = [=] (int i) {
+    int64_t offsets[] = {i, i, 0};
+
+    auto* __restrict__ grad_out_data = grad_out_ptr + offsets[0];
+    auto* __restrict__ grad_in_data = grad_in_ptr + offsets[1];
+    auto idx_dim = *(idx_dim_ptr + offsets[2]);
+
+    // left_fold potentially intersecting with idx_dim
+    // is either (idx_dim - size) / step or the next integer.
+    int64_t left_fold_idx = (idx_dim > size) ? (idx_dim - size) / step : 0;
+    if (!(left_fold_idx * step <= idx_dim && idx_dim < left_fold_idx * step + size)) {
+      ++left_fold_idx;
+    }
+
+    auto right_fold_idx = idx_dim / step;
+    right_fold_idx = (right_fold_idx >= grad_in_dim_size) ?
+                     (grad_in_dim_size - 1) : right_fold_idx;
+
+    for (auto fold_idx = left_fold_idx; fold_idx <= right_fold_idx; ++fold_idx) {
+      auto idx_last_dim = idx_dim - fold_idx * step;
+      *grad_out_data += grad_in_data[fold_idx * grad_in_dim_stride +
+                                     idx_last_dim * grad_in_last_dim_stride];
+    }
+  };
+
+  launch_unfold_backward_kernel<64, 4>(q, grad_in_dim_size, loop);
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 3) {
+    printf("Usage: %s <number of elements> <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int nelem = atoi(argv[1]);
+  const int repeat = atoi(argv[2]);
+
+  const int64_t size = 2;
+  const int64_t step = 1;
+  const int64_t grad_in_dim_stride = 1;
+  const int64_t grad_in_last_dim_stride = 1;
+  const int64_t grad_in_dim_size = nelem;
+  const int64_t grad_out_dim_stride = 1;
+
+  using scalar_t = int;
+
+  size_t input_size_bytes = grad_in_dim_size * sizeof(scalar_t);
+  size_t output_size_bytes = grad_in_dim_size * sizeof(scalar_t);
+
+  scalar_t *h_grad_in = (scalar_t*) malloc (input_size_bytes);
+  scalar_t *h_grad_out = (scalar_t*) malloc (output_size_bytes);
+
+  srand(123);
+  for (int i = 0; i < grad_in_dim_size; i++) {
+    h_grad_in[i] = rand() % 256;
+  }
+
+  const int64_t h_idx_dim = 0; // one dimension
+
+#ifdef USE_GPU
+  gpu_selector dev_sel;
+#else
+  cpu_selector dev_sel;
+#endif
+  queue q(dev_sel, property::queue::in_order());
+
+  scalar_t *d_grad_in, *d_grad_out;
+  d_grad_in = (scalar_t *)sycl::malloc_device(input_size_bytes, q);
+  q.memcpy(d_grad_in, h_grad_in, input_size_bytes);
+
+  int64_t *d_idx_dim = malloc_device<int64_t>(1, q);
+  q.memcpy(d_idx_dim, &h_idx_dim, sizeof(int64_t));
+
+  d_grad_out = (scalar_t *)sycl::malloc_device(output_size_bytes, q);
+
+  q.wait();
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    unfold_backward_internal_kernel(q,
+                                    d_grad_out,
+                                    d_grad_in,
+                                    d_idx_dim,
+                                    size,
+                                    step,
+                                    grad_in_dim_stride,
+                                    grad_in_last_dim_stride,
+                                    grad_in_dim_size,
+                                    grad_out_dim_stride);
+  }
+  q.wait();
+
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time of unfold backward kernel: %f (us)\n", (time * 1e-3f) / repeat);
+
+  q.memcpy(h_grad_out, d_grad_out, output_size_bytes).wait();
+
+  bool ok = true;
+  for (int i = 0; i < grad_in_dim_size; i++) {
+    if (repeat * h_grad_in[i] != h_grad_out[i]) {
+      ok = false;
+      break;
+    }
+  }
+  printf("%s\n", ok ? "PASS" : "FAIL");
+
+  free(d_grad_in, q);
+  free(d_grad_out, q);
+  free(d_idx_dim, q);
+
+  free(h_grad_in);
+  free(h_grad_out);
+
+  return 0;
+}
