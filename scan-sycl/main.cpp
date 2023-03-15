9b20f8bd2d5aed49a89a4b484f8b65085fe4adb7
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-03-15 14:29:13 -0400

    [scan] change from a block scan to a segmented scan where the number of elements to scan in each segment is N (512); the total number of elements to scan is specified as a command-line argument; the change is based on the comments in https://github.com/intel/llvm/issues/7643

--- scan-sycl/main.cpp
+++ scan-sycl/main.cpp
@@ -4,11 +4,11 @@
 #include <chrono>
 #include "common.h"
 
-// scan over N elements
+// N is the number of elements to scan in a thread block
 #define N 512
 
 template<typename T>
-void verify(const T* cpu_out, const T* gpu_out, int n)
+void verify(const T* cpu_out, const T* gpu_out, size_t n)
 {
   int error = memcmp(cpu_out, gpu_out, n * sizeof(T));
   printf("%s\n", error ? "FAIL" : "PASS");
@@ -26,17 +26,19 @@ class block_scan;
 
 // bank conflict aware optimization
 template<typename T>
-__attribute__((always_inline))
-void prescan_bcao (
+void scan_bcao (
         nd_item<1> &item,
         local_ptr<T> temp,
         T *__restrict g_odata,
-  const T *__restrict g_idata,
-  const int n)
+  const T *__restrict g_idata)
 {
+  int bid = item.get_group(0);
+  g_idata += bid * N;
+  g_odata += bid * N;
+
   int thid = item.get_local_id(0);
   int a = thid;
-  int b = a + (n/2);
+  int b = a + (N/2);
   int oa = OFFSET(a);
   int ob = OFFSET(b);
 
@@ -44,10 +46,10 @@ void prescan_bcao (
   temp[b + ob] = g_idata[b];
 
   int offset = 1;
-  for (int d = n >> 1; d > 0; d >>= 1) 
+  for (int d = N >> 1; d > 0; d >>= 1) 
   {
     __syncthreads();
-    if (thid < d) 
+    if (thid < d)
     {
       int ai = offset*(2*thid+1)-1;
       int bi = offset*(2*thid+2)-1;
@@ -58,8 +60,8 @@ void prescan_bcao (
     offset *= 2;
   }
 
-  if (thid == 0) temp[n-1+OFFSET(n-1)] = 0; // clear the last elem
-  for (int d = 1; d < n; d *= 2) // traverse down
+  if (thid == 0) temp[N-1+OFFSET(N-1)] = 0; // clear the last elem
+  for (int d = 1; d < N; d *= 2) // traverse down
   {
     offset >>= 1;     
     __syncthreads();      
@@ -81,19 +83,21 @@ void prescan_bcao (
 }
 
 template<typename T>
-__attribute__((always_inline))
-void prescan (
+void scan (
         nd_item<1> &item,
         local_ptr<T> temp,
         T *__restrict g_odata,
-  const T *__restrict g_idata,
-  const int n)
+  const T *__restrict g_idata)
 {
+  int bid = item.get_group(0);
+  g_idata += bid * N;
+  g_odata += bid * N;
+
   int thid = item.get_local_id(0);
   int offset = 1;
   temp[2*thid]   = g_idata[2*thid];
   temp[2*thid+1] = g_idata[2*thid+1];
-  for (int d = n >> 1; d > 0; d >>= 1) 
+  for (int d = N >> 1; d > 0; d >>= 1) 
   {
     __syncthreads();
     if (thid < d) 
@@ -105,8 +109,8 @@ void prescan (
     offset *= 2;
   }
 
-  if (thid == 0) temp[n-1] = 0; // clear the last elem
-  for (int d = 1; d < n; d *= 2) // traverse down
+  if (thid == 0) temp[N-1] = 0; // clear the last elem
+  for (int d = 1; d < N; d *= 2) // traverse down
   {
     offset >>= 1;     
     __syncthreads();      
@@ -123,26 +127,40 @@ void prescan (
   g_odata[2*thid+1] = temp[2*thid+1];
 }
 
+
 template <typename T>
-void runTest (queue &q, const int repeat, bool timing = false) 
+void runTest (queue &q, const size_t n, const int repeat, bool timing = false) 
 {
-  T in[N];
-  T cpu_out[N];
-  T gpu_out[N];
+  const size_t num_blocks = (n + N - 1) / N;
 
-  int n = N;
+  const size_t nelems = num_blocks * N; // actual total number of elements
 
-  for (int i = 0; i < n; i++) in[i] = (i % 5)+1;
-  cpu_out[0] = 0;
-  for (int i = 1; i < n; i++) cpu_out[i] = cpu_out[i-1] + in[i-1];
+  size_t bytes = nelems * sizeof(T);
 
-  T *d_in = malloc_device<T>(n, q);
-  q.memcpy(d_in, in, n*sizeof(T));
+  T *in = (T*) malloc (bytes);
+  T *cpu_out = (T*) malloc (bytes);
+  T *gpu_out = (T*) malloc (bytes);
 
-  T *d_out = malloc_device<T>(n, q);
+  srand(123);
+  for (size_t n = 0; n < nelems; n++) in[n] = rand() % 5 + 1;
 
-  range<1> lws (n/2);
-  range<1> gws (n/2);
+  T *t_in = in;
+  T *t_out = cpu_out;
+  for (size_t n = 0; n < num_blocks; n++) { 
+    t_out[0] = 0;
+    for (int i = 1; i < N; i++) 
+      t_out[i] = t_out[i-1] + t_in[i-1];
+    t_out += N;
+    t_in += N;
+  }
+
+  T *d_in = malloc_device<T>(nelems, q);
+  q.memcpy(d_in, in, bytes);
+
+  T *d_out = malloc_device<T>(nelems, q);
+
+  range<1> gws (num_blocks * N/2);
+  range<1> lws (N/2);
 
   q.wait();
   auto start = std::chrono::steady_clock::now();
@@ -151,7 +169,7 @@ void runTest (queue &q, const int repeat, bool timing = false)
     q.submit([&] (handler &cgh) {
       accessor<T, 1, sycl_read_write, access::target::local> temp (N, cgh);
       cgh.parallel_for<class block_scan<T>>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
-        prescan(item, temp.get_pointer(), d_out, d_in, n);
+        scan(item, temp.get_pointer(), d_out, d_in);
       });
     });
   }
@@ -160,11 +178,11 @@ void runTest (queue &q, const int repeat, bool timing = false)
   auto end = std::chrono::steady_clock::now();
   auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
   if (timing) {
-    printf("Element size in bytes is %zu. Average execution time of block scan (w/  bank conflicts): %f (us)\n",
+    printf("Element size in bytes is %zu. Average execution time of scan (w/  bank conflicts): %f (us)\n",
            sizeof(T), (time * 1e-3f) / repeat);
   }
-  q.memcpy(gpu_out, d_out, n*sizeof(T)).wait();
-  if (!timing) verify(cpu_out, gpu_out, n);
+  q.memcpy(gpu_out, d_out, bytes).wait();
+  if (!timing) verify(cpu_out, gpu_out, nelems);
 
   // bcao
   start = std::chrono::steady_clock::now();
@@ -173,7 +191,7 @@ void runTest (queue &q, const int repeat, bool timing = false)
     q.submit([&] (handler &cgh) {
       accessor<T, 1, sycl_read_write, access::target::local> temp (N*2, cgh);
       cgh.parallel_for<class opt_block_scan<T>>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
-        prescan_bcao(item, temp.get_pointer(), d_out, d_in, n);
+        scan_bcao(item, temp.get_pointer(), d_out, d_in);
       });
     });
   }
@@ -182,24 +200,28 @@ void runTest (queue &q, const int repeat, bool timing = false)
   end = std::chrono::steady_clock::now();
   auto bcao_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
   if (timing) {
-    printf("Element size in bytes is %zu. Average execution time of block scan (w/o bank conflicts): %f (us). ",
+    printf("Element size in bytes is %zu. Average execution time of scan (w/o bank conflicts): %f (us). ",
            sizeof(T), (bcao_time * 1e-3f) / repeat);
     printf("Reduce the time by %.1f%%\n", (time - bcao_time) * 1.0 / time * 100);
   }
-  q.memcpy(gpu_out, d_out, n*sizeof(T)).wait();
-  if (!timing) verify(cpu_out, gpu_out, n);
+  q.memcpy(gpu_out, d_out, bytes).wait();
+  if (!timing) verify(cpu_out, gpu_out, nelems);
 
   free(d_in, q);
   free(d_out, q);
+  free(in);
+  free(cpu_out);
+  free(gpu_out);
 }
 
 int main(int argc, char* argv[])
 {
-  if (argc != 2) {
-    printf("Usage: %s <repeat>\n", argv[0]);
+  if (argc != 3) {
+    printf("Usage: %s <number of elements> <repeat>\n", argv[0]);
     return 1;
   }
-  const int repeat = atoi(argv[1]);
+  const int n = atoi(argv[1]);
+  const int repeat = atoi(argv[2]);
 
 #ifdef USE_GPU
   gpu_selector dev_sel;
@@ -210,10 +232,10 @@ int main(int argc, char* argv[])
     
   for (int i = 0; i < 2; i++) {
     bool timing = i > 0;
-    runTest<char>(q, repeat, timing);
-    runTest<short>(q, repeat, timing);
-    runTest<int>(q, repeat, timing);
-    runTest<long>(q, repeat, timing);
+    runTest<char>(q, n, repeat, timing);
+    runTest<short>(q, n, repeat, timing);
+    runTest<int>(q, n, repeat, timing);
+    runTest<long>(q, n, repeat, timing);
   }
 
   return 0; 
