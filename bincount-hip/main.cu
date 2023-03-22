42f74534df07df99bba40a86996c276c1c00e3d5
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-03-22 11:25:28 -0400

    [bincount] count the number of values that fall into each bin; the number of bins ranges from 768 to 768*32; the atomic operations are limited to int32 operations

---
+++ bincount-hip/main.cu
@@ -0,0 +1,221 @@
+#include <algorithm>
+#include <chrono>
+#include <cstdio>
+#include <cstdlib>
+#include <random>
+#include <hip/hip_runtime.h>
+
+#define threadsPerBlock  256
+
+static inline __device__
+void gpuAtomicAddNoReturn(int32_t *address, int32_t val) {
+  atomicAdd(address, val);
+}
+
+#define FOR_KERNEL_LOOP(i, lim)                                      \
+  for (IndexType i = blockIdx.x * blockDim.x + threadIdx.x; i < lim; \
+                 i += gridDim.x * blockDim.x)
+
+// Memory types used for the bincount implementations
+enum class DeviceMemoryType { SHARED, GLOBAL };
+
+template <typename input_t, typename IndexType>
+__device__ static IndexType
+getBin(input_t v, input_t minvalue, input_t maxvalue, IndexType nbins)
+{
+  IndexType bin = (v - minvalue) * nbins / (maxvalue - minvalue);
+  // (only applicable for histc)
+  // while each bin is inclusive at the lower end and exclusive at the higher,
+  // i.e. [start, end) the last bin is inclusive at both, i.e. [start, end], in
+  // order to include maxvalue if exists therefore when bin == nbins, adjust bin
+  // to the last bin
+  if (bin == nbins) bin--;
+  return bin;
+}
+
+// Kernel for computing the histogram of the input
+template <typename output_t,
+          typename input_t,
+          typename IndexType,
+          DeviceMemoryType MemoryType>
+__global__ void bincount (
+       output_t *output,
+  const input_t *input,
+  IndexType nbins,
+  input_t minvalue,
+  input_t maxvalue,
+  IndexType input_size,
+  IndexType output_size)
+{
+  extern __shared__ unsigned char my_smem[];
+  output_t* smem = nullptr;
+
+  if (MemoryType == DeviceMemoryType::SHARED) {
+    // atomically add to block specific shared memory
+    // then atomically add to the global output tensor
+    smem = reinterpret_cast<output_t*>(my_smem);
+    for (IndexType i = threadIdx.x; i < nbins; i += blockDim.x) {
+      smem[i] = 0;
+    }
+    __syncthreads();
+
+    FOR_KERNEL_LOOP(linearIndex, input_size) {
+      const auto v = input[linearIndex];
+
+      if (v >= minvalue && v <= maxvalue) {
+        const IndexType bin = getBin<input_t, IndexType>(
+                              v, minvalue, maxvalue, nbins);
+        gpuAtomicAddNoReturn(&smem[bin], 1);
+      }
+    }
+    __syncthreads();
+
+    // Atomically update output bin count.
+    for (IndexType i = threadIdx.x; i < nbins; i += blockDim.x) {
+      gpuAtomicAddNoReturn(&output[i], smem[i]);
+    }
+
+  } else {
+    ////////////////////////// Global memory //////////////////////////
+    // atomically add to the output tensor
+    // compute histogram for the block
+    FOR_KERNEL_LOOP(linearIndex, input_size) {
+      const auto v = input[linearIndex];
+      if (v >= minvalue && v <= maxvalue) {
+        const IndexType bin = getBin<input_t, IndexType>(
+                              v, minvalue, maxvalue, nbins);
+        gpuAtomicAddNoReturn(&output[bin], 1);
+      }
+    }
+  }
+}
+
+#define HANDLE_CASE(MEMORY_TYPE, SHARED_MEM)                 \
+  auto start = std::chrono::steady_clock::now();             \
+  for (int i = 0; i < repeat; i++)                           \
+  bincount<                                                  \
+      output_t,                                              \
+      input_t,                                               \
+      IndexType,                                             \
+      MEMORY_TYPE><<<grid, block, SHARED_MEM>>>(             \
+      d_output,                                              \
+      d_input,                                               \
+      nbins,                                                 \
+      minvalue,                                              \
+      maxvalue,                                              \
+      input_size,                                            \
+      output_size);                                          \
+  hipDeviceSynchronize();                                   \
+  auto end = std::chrono::steady_clock::now();               \
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); \
+  printf("Average execution time of bincount kernel: %f (us)\n", \
+         (time * 1e-3f) / repeat)
+
+#define HANDLE_SWITCH_CASE(mType)                           \
+  switch (mType) {                                          \
+    case DeviceMemoryType::SHARED: {                        \
+      HANDLE_CASE(DeviceMemoryType::SHARED, sharedMem);     \
+      break;                                                \
+    }                                                       \
+    default: {                                              \
+      HANDLE_CASE(DeviceMemoryType::GLOBAL, 0);             \
+    }                                                       \
+  }
+
+/*
+  Calculate the frequency of the input values.
+  3 implementations based of input size and memory usage:
+    case: sufficient shared mem
+        SHARED: Each block atomically adds to it's own **shared** hist copy,
+        then atomically updates the global tensor.
+    case: insufficient shared memory
+        GLOBAL: all threads atomically update to a single **global** hist copy.
+ */
+template <typename output_t, typename input_t, typename IndexType>
+void eval(IndexType input_size, int repeat)
+{
+  size_t input_size_bytes = sizeof(input_t) * input_size;
+
+  input_t *input = (input_t*) malloc (input_size_bytes); 
+
+  std::default_random_engine generator (123);
+  std::normal_distribution<input_t> distribution(5.0,2.0);
+  for (int i = 0; i < input_size; i++) {
+    input[i] = distribution(generator);
+  }
+
+  auto min_iter = std::min_element(input, input+input_size);
+  auto max_iter = std::max_element(input, input+input_size);
+  
+  input_t minvalue = *min_iter;
+  input_t maxvalue = *max_iter;
+  printf("Input min, max values: (%f %f)\n", (float)minvalue, (float)maxvalue);
+
+  input_t *d_input;
+  hipMalloc((void**)&d_input, input_size_bytes);
+  hipMemcpy(d_input, input, input_size_bytes, hipMemcpyHostToDevice);
+
+  int maxSharedMemory;
+  hipDeviceGetAttribute(&maxSharedMemory, hipDeviceAttributeMaxSharedMemoryPerBlock, 0);
+  int th = maxSharedMemory / sizeof(output_t);
+
+  printf("Max number of bins allocated in shared local memory: %d\n", th);
+
+  for (IndexType nbins = th / 16; nbins <= 2 * th; nbins = nbins * 2) {
+
+    IndexType output_size = nbins;
+    size_t output_size_bytes = sizeof(output_t) * output_size;
+    output_t *output = (output_t*) malloc (output_size_bytes); 
+
+    output_t *d_output;
+    hipMalloc((void**)&d_output, output_size_bytes);
+    hipMemset(d_output, 0, output_size_bytes);
+
+    printf("\nNumber of bins: %d\n", nbins);
+    IndexType sharedMem = nbins * sizeof(output_t);
+
+    dim3 grid ((input_size + threadsPerBlock - 1) / threadsPerBlock);
+    dim3 block (threadsPerBlock);
+
+    // determine memory type to use in the kernel
+    DeviceMemoryType memType = DeviceMemoryType::GLOBAL;
+    printf("bincount using global atomics\n");
+    HANDLE_SWITCH_CASE(memType)
+
+    if (sharedMem <= maxSharedMemory) {
+      printf("bincount using global and local atomics\n");
+      memType = DeviceMemoryType::SHARED;
+      HANDLE_SWITCH_CASE(memType)
+    }
+
+    hipMemcpy(output, d_output, output_size_bytes, hipMemcpyDeviceToHost);
+    auto min_iter = std::min_element(output, output+output_size);
+    auto max_iter = std::max_element(output, output+output_size);
+    output_t minvalue = *min_iter;
+    output_t maxvalue = *max_iter;
+    printf("Output min, median, max values: (%ld %ld %ld)\n",
+           (int64_t)minvalue / repeat,
+           (int64_t)output[output_size/2] / repeat,
+           (int64_t)maxvalue / repeat);
+
+    hipFree(d_output);
+    free(output);
+  }
+
+  hipFree(d_input);
+  free(input);
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 3) {
+    printf("Usage: %s <number of elements> <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int n = atoi(argv[1]);
+  const int repeat = atoi(argv[2]);
+
+  eval<int32_t, float, int32_t>(n, repeat);
+
+  return 0; 
+}
