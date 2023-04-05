e0b23ed477deec89bc018a015272656c3c00bf55
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-04-05 19:42:56 -0400

    [vanGenuchten] reuse the result of the arithmetic expression for multiply and update the comment style in the kernels

--- vanGenuchten-hip/main.cu
+++ vanGenuchten-hip/main.cu
@@ -14,7 +14,7 @@ void vanGenuchten(
         double *__restrict__ K,
   const int size)
 {
-  double Se, _theta, _psi, lambda, m;
+  double Se, _theta, _psi, lambda, m, t;
 
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   if (i < size)
@@ -34,8 +34,9 @@ void vanGenuchten(
     // Compute the effective saturation [eqn 2]
     Se = (_theta - theta_R)/(theta_S - theta_R);
 
-    /* . . .Compute the hydraulic conductivity [eqn 8] . . .*/
-    K[i] = Ksat[i] * sqrt(Se) * (1.0 - pow( 1.0-pow(Se,1.0/m), m) ) * (1.0 - pow( 1.0-pow( Se, 1.0/m), m ));
+    // Compute the hydraulic conductivity [eqn 8]
+    t = 1.0 - pow(1.0-pow(Se,1.0/m), m);
+    K[i] = Ksat[i] * sqrt(Se) * t * t;
 
     // Compute the specific moisture storage derivative of eqn (21).
     // So we have to calculate C = d(theta)/dh. Then the unit is converted into [1/m].
