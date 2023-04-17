e3eece046c282e68ca3fbfef20ec5e4774038cbf
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2023-04-17 16:25:23 +0100

    Explicitly use the sycl namespace and the sycl::local_accessor, replace cl::sycl:: with sycl::, and replace sycl::atomic with sycl::atomic_ref.

--- testSNAP-sycl/main.cpp
+++ testSNAP-sycl/main.cpp
@@ -20,7 +20,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include "common.h"
+#include <sycl/sycl.hpp>
 #include "snap.h"
 #include "utils.cpp"
 
@@ -48,14 +48,14 @@ int nsteps = 1; // num of force evaluations
 /// \returns The value at the \p addr before the call.
 inline double atomicAdd( double *addr, double operand )
 {
-  atomic<long, access::address_space::global_space> obj(
-    (multi_ptr<long, access::address_space::global_space>(reinterpret_cast<long *>(addr))));
+  sycl::atomic_ref<long, sycl::memory_order::relaxed, sycl::memory_scope::device, sycl::access::address_space::global_space> obj(
+    (*reinterpret_cast<long *>(addr)));
 
   long old_value;
   double old_double_value;
 
   do {
-    old_value = obj.load(memory_order::relaxed);
+    old_value = obj.load(sycl::memory_order::relaxed);
     old_double_value = *reinterpret_cast<const double *>(&old_value);
     const double new_double_value = old_double_value + operand;
     const long new_value = *reinterpret_cast<const long *>(&new_double_value);
@@ -74,8 +74,8 @@ int main(int argc, char* argv[])
   const int switch_flag = 1;     // SNAP parameter
 
   // record timings of individual routines
-  double elapsed_ui = 0.0, 
-         elapsed_yi = 0.0, 
+  double elapsed_ui = 0.0,
+         elapsed_yi = 0.0,
          elapsed_duidrj = 0.0,
          elapsed_deidrj = 0.0;
 
@@ -88,8 +88,8 @@ int main(int argc, char* argv[])
   const double rcutfac = refdata.rcutfac;
 
   const double wself = 1.0;
-  const int num_atoms = nlocal; 
-  const int num_nbor = ninside; 
+  const int num_atoms = nlocal;
+  const int num_nbor = ninside;
 
   //coeffi = memory->grow(coeffi, ncoeff + 1, "coeffi");
   double* coeffi = (double*) malloc (sizeof(double) * (ncoeff+1));
@@ -305,9 +305,9 @@ int main(int argc, char* argv[])
   const int jdimpq = twojmax + 2;
   double* rootpqarray = (double*) malloc(sizeof(double) * jdimpq * jdimpq);
   double* cglist = (double*) malloc (sizeof(double) * idxcg_max);
-  double* dedr = (double*) malloc (sizeof(double) * num_atoms * num_nbor * 3); 
+  double* dedr = (double*) malloc (sizeof(double) * num_atoms * num_nbor * 3);
 
-  COMPLEX* ulist = (COMPLEX*) malloc (sizeof(COMPLEX) * num_atoms * num_nbor * idxu_max); 
+  COMPLEX* ulist = (COMPLEX*) malloc (sizeof(COMPLEX) * num_atoms * num_nbor * idxu_max);
   COMPLEX* ylist = (COMPLEX*) malloc (sizeof(COMPLEX) * num_atoms * idxdu_max);
   COMPLEX* ulisttot = (COMPLEX*) malloc (sizeof(COMPLEX) * num_atoms * idxu_max);
   COMPLEX* dulist = (COMPLEX*) malloc (sizeof(COMPLEX) * num_atoms * num_nbor * 3 * idxdu_max);
@@ -375,35 +375,34 @@ int main(int argc, char* argv[])
   double sumsqferr = 0.0;
 
 #ifdef USE_GPU
-  gpu_selector dev_sel;
+  sycl::queue q(sycl::gpu_selector_v);
 #else
-  cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v);
 #endif
-  queue q(dev_sel);
 
-  buffer<int, 1> d_idxu_block(idxu_block, jdim);
-  buffer<int, 1> d_ulist_parity(ulist_parity, idxu_max);
-  buffer<double, 1> d_rootpqarray(rootpqarray, jdimpq * jdimpq);
-  buffer<int, 1> d_idxz(idxz, idxz_max * 9);
-  buffer<double, 1> d_idxzbeta(idxzbeta, idxz_max);
-  buffer<int, 1> d_idxcg_block(idxcg_block, jdim * jdim * jdim);
-  buffer<int, 1> d_idxdu_block(idxdu_block, jdim);
-  buffer<double, 1> d_cglist(cglist, idxcg_max);
+  sycl::buffer<int, 1> d_idxu_block(idxu_block, jdim);
+  sycl::buffer<int, 1> d_ulist_parity(ulist_parity, idxu_max);
+  sycl::buffer<double, 1> d_rootpqarray(rootpqarray, jdimpq * jdimpq);
+  sycl::buffer<int, 1> d_idxz(idxz, idxz_max * 9);
+  sycl::buffer<double, 1> d_idxzbeta(idxzbeta, idxz_max);
+  sycl::buffer<int, 1> d_idxcg_block(idxcg_block, jdim * jdim * jdim);
+  sycl::buffer<int, 1> d_idxdu_block(idxdu_block, jdim);
+  sycl::buffer<double, 1> d_cglist(cglist, idxcg_max);
 
-  buffer<COMPLEX, 1> d_dulist(dulist, num_atoms * num_nbor * 3 * idxdu_max);
-  buffer<COMPLEX, 1> d_ulist(ulist, num_atoms * num_nbor * idxu_max);
-  buffer<double, 1> d_dedr(dedr, num_atoms * num_nbor * 3);
+  sycl::buffer<COMPLEX, 1> d_dulist(dulist, num_atoms * num_nbor * 3 * idxdu_max);
+  sycl::buffer<COMPLEX, 1> d_ulist(ulist, num_atoms * num_nbor * idxu_max);
+  sycl::buffer<double, 1> d_dedr(dedr, num_atoms * num_nbor * 3);
 
   d_ulist.set_final_data(nullptr);
   d_dulist.set_final_data(nullptr);
   d_dedr.set_final_data(nullptr);
 
-  buffer<COMPLEX, 1> d_ulisttot(num_atoms * idxu_max);
-  buffer<COMPLEX, 1> d_ylist(num_atoms * idxdu_max);
+  sycl::buffer<COMPLEX, 1> d_ulisttot(num_atoms * idxu_max);
+  sycl::buffer<COMPLEX, 1> d_ylist(num_atoms * idxdu_max);
 
-  buffer<double, 1> d_rij(num_atoms*num_nbor*3);
-  buffer<double, 1> d_rcutij(num_atoms*num_nbor);
-  buffer<double, 1> d_wj(num_atoms*num_nbor);
+  sycl::buffer<double, 1> d_rij(num_atoms*num_nbor*3);
+  sycl::buffer<double, 1> d_rcutij(num_atoms*num_nbor);
+  sycl::buffer<double, 1> d_wj(num_atoms*num_nbor);
 
   // loop over steps
 
@@ -429,18 +428,18 @@ int main(int argc, char* argv[])
       }
     }
 
-    q.submit([&] (handler &cgh) {
-      auto acc = d_rij.get_access<sycl_discard_write>(cgh);
+    q.submit([&] (sycl::handler &cgh) {
+      auto acc = d_rij.get_access<sycl::access::mode::discard_write>(cgh);
       cgh.copy(rij, acc);
     });
 
-    q.submit([&] (handler &cgh) {
-      auto acc = d_rcutij.get_access<sycl_discard_write>(cgh);
+    q.submit([&] (sycl::handler &cgh) {
+      auto acc = d_rcutij.get_access<sycl::access::mode::discard_write>(cgh);
       cgh.copy(rcutij, acc);
     });
 
-    q.submit([&] (handler &cgh) {
-      auto acc = d_wj.get_access<sycl_discard_write>(cgh);
+    q.submit([&] (sycl::handler &cgh) {
+      auto acc = d_wj.get_access<sycl::access::mode::discard_write>(cgh);
       cgh.copy(wj, acc);
     });
 
@@ -453,24 +452,24 @@ int main(int argc, char* argv[])
     //   compute r0 = (x,y,z,z0)
     //   utot(j,ma,mb) += u(r0;j,ma,mb) for all j,ma,mb
 
-    range<1> gws_k1 ((num_atoms*idxu_max+255)/256*256);
-    range<1> lws_k1 (256);
-    q.submit([&] (handler &cgh) {
-      auto acc = d_ulisttot.get_access<sycl_write>(cgh);
-      cgh.parallel_for<class reset_ulisttot>(nd_range<1>(gws_k1, lws_k1), [=] (nd_item<1> item) {
+    sycl::range<1> gws_k1 ((num_atoms*idxu_max+255)/256*256);
+    sycl::range<1> lws_k1 (256);
+    q.submit([&] (sycl::handler &cgh) {
+      auto acc = d_ulisttot.get_access<sycl::access::mode::write>(cgh);
+      cgh.parallel_for<class reset_ulisttot>(sycl::nd_range<1>(gws_k1, lws_k1), [=] (sycl::nd_item<1> item) {
         int i = item.get_global_id(0);
         if (i < num_atoms*idxu_max) acc[i] = {0.0, 0.0};
       });
     });
 
-    range<1> gws_k2 ((num_atoms+255)/256*256);
-    range<1> lws_k2 (256);
-    q.submit([&] (handler &cgh) {
-      auto ulisttot = d_ulisttot.get_access<sycl_write>(cgh);
-      auto idxu_block = d_idxu_block.get_access<sycl_read>(cgh);
-      cgh.parallel_for<class set_ulisttot>(nd_range<1>(gws_k2, lws_k2), [=] (nd_item<1> item) {
+    sycl::range<1> gws_k2 ((num_atoms+255)/256*256);
+    sycl::range<1> lws_k2 (256);
+    q.submit([&] (sycl::handler &cgh) {
+      auto ulisttot = d_ulisttot.get_access<sycl::access::mode::write>(cgh);
+      auto idxu_block = d_idxu_block.get_access<sycl::access::mode::read>(cgh);
+      cgh.parallel_for<class set_ulisttot>(sycl::nd_range<1>(gws_k2, lws_k2), [=] (sycl::nd_item<1> item) {
         int natom = item.get_global_id(0);
-        if (natom < num_atoms) 
+        if (natom < num_atoms)
           for (int j = 0; j <= twojmax; j++) {
             int jju = idxu_block[j];
             for (int ma = 0; ma <= j; ma++) {
@@ -481,20 +480,20 @@ int main(int argc, char* argv[])
       });
     });
 
-    range<2> gws_k3 ((num_nbor+15)/16*16, (num_atoms+15)/16*16);
-    range<2> lws_k3 (16, 16);
+    sycl::range<2> gws_k3 ((num_nbor+15)/16*16, (num_atoms+15)/16*16);
+    sycl::range<2> lws_k3 (16, 16);
 
-    q.submit([&] (handler &cgh) {
-      auto wj = d_wj.get_access<sycl_read>(cgh);
-      auto rij = d_rij.get_access<sycl_read>(cgh);
-      auto rcutij = d_rcutij.get_access<sycl_read>(cgh);
-      auto ulist_parity = d_ulist_parity.get_access<sycl_read>(cgh);
-      auto idxu_block = d_idxu_block.get_access<sycl_read>(cgh);
-      auto rootpqarray = d_rootpqarray.get_access<sycl_read>(cgh);
-      auto ulist = d_ulist.get_access<sycl_read_write>(cgh);
-      auto ulisttot = d_ulisttot.get_access<sycl_read_write>(cgh);
+    q.submit([&] (sycl::handler &cgh) {
+      auto wj = d_wj.get_access<sycl::access::mode::read>(cgh);
+      auto rij = d_rij.get_access<sycl::access::mode::read>(cgh);
+      auto rcutij = d_rcutij.get_access<sycl::access::mode::read>(cgh);
+      auto ulist_parity = d_ulist_parity.get_access<sycl::access::mode::read>(cgh);
+      auto idxu_block = d_idxu_block.get_access<sycl::access::mode::read>(cgh);
+      auto rootpqarray = d_rootpqarray.get_access<sycl::access::mode::read>(cgh);
+      auto ulist = d_ulist.get_access<sycl::access::mode::read_write>(cgh);
+      auto ulisttot = d_ulisttot.get_access<sycl::access::mode::read_write>(cgh);
 
-      cgh.parallel_for<class update_ulisttot>(nd_range<2>(gws_k3, lws_k3), [=] (nd_item<2> item) {
+      cgh.parallel_for<class update_ulisttot>(sycl::nd_range<2>(gws_k3, lws_k3), [=] (sycl::nd_item<2> item) {
 	int nbor = item.get_global_id(0);
 	int natom = item.get_global_id(1);
         if (natom < num_atoms && nbor < num_nbor) {
@@ -502,17 +501,17 @@ int main(int argc, char* argv[])
           double y = rij[ULIST_INDEX(natom, nbor, 1)];
           double z = rij[ULIST_INDEX(natom, nbor, 2)];
           double rsq = x * x + y * y + z * z;
-          double r = cl::sycl::sqrt(rsq);
+          double r = sycl::sqrt(rsq);
 
           double theta0 = (r - rmin0) * rfac0 * MY_PI / (rcutij[INDEX_2D(natom, nbor)] - rmin0);
-          double z0 = r / cl::sycl::tan(theta0);
+          double z0 = r / sycl::tan(theta0);
 
           double rootpq;
           int jju, jjup;
 
           // compute Cayley-Klein parameters for unit quaternion
 
-          double r0inv = 1.0 / cl::sycl::sqrt(r * r + z0 * z0);
+          double r0inv = 1.0 / sycl::sqrt(r * r + z0 * z0);
           double a_r = r0inv * z0;
           double a_i = -r0inv * z;
           double b_r = r0inv * y;
@@ -644,30 +643,30 @@ int main(int argc, char* argv[])
     //compute_yi(beta);
 
     // Initialize ylist elements to zeros
-    range<1> gws_k4 ((num_atoms*idxdu_max+255)/256*256);
-    range<1> lws_k4 (256);
-    q.submit([&] (handler &cgh) {
-      auto acc = d_ylist.get_access<sycl_write>(cgh);
-      cgh.parallel_for<class reset_ylist>(nd_range<1>(gws_k4, lws_k4), [=] (nd_item<1> item) {
+    sycl::range<1> gws_k4 ((num_atoms*idxdu_max+255)/256*256);
+    sycl::range<1> lws_k4 (256);
+    q.submit([&] (sycl::handler &cgh) {
+      auto acc = d_ylist.get_access<sycl::access::mode::write>(cgh);
+      cgh.parallel_for<class reset_ylist>(sycl::nd_range<1>(gws_k4, lws_k4), [=] (sycl::nd_item<1> item) {
         int i = item.get_global_id(0);
-        if (i < num_atoms*idxdu_max) acc[i] = {0.0, 0.0}; 
+        if (i < num_atoms*idxdu_max) acc[i] = {0.0, 0.0};
       });
     });
 
-    range<2> gws_k5 ((idxz_max+15)/16*16, (num_atoms+15)/16*16);
-    range<2> lws_k5 (16, 16);
+    sycl::range<2> gws_k5 ((idxz_max+15)/16*16, (num_atoms+15)/16*16);
+    sycl::range<2> lws_k5 (16, 16);
 
-    q.submit([&] (handler &cgh) {
-      auto idxz = d_idxz.get_access<sycl_read>(cgh);
-      auto idxzbeta = d_idxzbeta.get_access<sycl_read>(cgh);
-      auto idxcg_block = d_idxcg_block.get_access<sycl_read>(cgh);
-      auto cglist = d_cglist.get_access<sycl_read>(cgh);
-      auto idxu_block = d_idxu_block.get_access<sycl_read>(cgh);
-      auto idxdu_block = d_idxdu_block.get_access<sycl_read>(cgh);
-      auto ulisttot = d_ulisttot.get_access<sycl_read>(cgh);
-      auto ylist = d_ylist.get_access<sycl_read_write>(cgh);
+    q.submit([&] (sycl::handler &cgh) {
+      auto idxz = d_idxz.get_access<sycl::access::mode::read>(cgh);
+      auto idxzbeta = d_idxzbeta.get_access<sycl::access::mode::read>(cgh);
+      auto idxcg_block = d_idxcg_block.get_access<sycl::access::mode::read>(cgh);
+      auto cglist = d_cglist.get_access<sycl::access::mode::read>(cgh);
+      auto idxu_block = d_idxu_block.get_access<sycl::access::mode::read>(cgh);
+      auto idxdu_block = d_idxdu_block.get_access<sycl::access::mode::read>(cgh);
+      auto ulisttot = d_ulisttot.get_access<sycl::access::mode::read>(cgh);
+      auto ylist = d_ylist.get_access<sycl::access::mode::read_write>(cgh);
 
-      cgh.parallel_for<class compute_yi>(nd_range<2>(gws_k5, lws_k5), [=] (nd_item<2> item) {
+      cgh.parallel_for<class compute_yi>(sycl::nd_range<2>(gws_k5, lws_k5), [=] (sycl::nd_item<2> item) {
         int jjz = item.get_global_id(0);
         int natom = item.get_global_id(1);
         if (jjz < idxz_max && natom < num_atoms) {
@@ -752,18 +751,18 @@ int main(int argc, char* argv[])
     // compute_duidrj
     start = system_clock::now();
 
-    range<2> gws_k6 ((num_nbor+15)/16*16, (num_atoms+15)/16*16);
-    range<2> lws_k6 (16, 16);
+    sycl::range<2> gws_k6 ((num_nbor+15)/16*16, (num_atoms+15)/16*16);
+    sycl::range<2> lws_k6 (16, 16);
 
-    q.submit([&] (handler &cgh) {
-      auto wj = d_wj.get_access<sycl_read>(cgh);
-      auto rij = d_rij.get_access<sycl_read>(cgh);
-      auto rcutij = d_rcutij.get_access<sycl_read>(cgh);
-      auto rootpqarray = d_rootpqarray.get_access<sycl_read>(cgh);
-      auto ulist = d_ulist.get_access<sycl_read>(cgh);
-      auto dulist = d_dulist.get_access<sycl_read_write>(cgh);
+    q.submit([&] (sycl::handler &cgh) {
+      auto wj = d_wj.get_access<sycl::access::mode::read>(cgh);
+      auto rij = d_rij.get_access<sycl::access::mode::read>(cgh);
+      auto rcutij = d_rcutij.get_access<sycl::access::mode::read>(cgh);
+      auto rootpqarray = d_rootpqarray.get_access<sycl::access::mode::read>(cgh);
+      auto ulist = d_ulist.get_access<sycl::access::mode::read>(cgh);
+      auto dulist = d_dulist.get_access<sycl::access::mode::read_write>(cgh);
 
-      cgh.parallel_for<class compute_duidrj>(nd_range<2>(gws_k6, lws_k6), [=] (nd_item<2> item) {
+      cgh.parallel_for<class compute_duidrj>(sycl::nd_range<2>(gws_k6, lws_k6), [=] (sycl::nd_item<2> item) {
 	int nbor = item.get_global_id(0);
 	int natom = item.get_global_id(1);
         if (natom < num_atoms && nbor < num_nbor) {
@@ -774,19 +773,19 @@ int main(int argc, char* argv[])
           double y = rij[ULIST_INDEX(natom, nbor, 1)];
           double z = rij[ULIST_INDEX(natom, nbor, 2)];
           double rsq = x * x + y * y + z * z;
-          double r = cl::sycl::sqrt(rsq);
+          double r = sycl::sqrt(rsq);
           double rscale0 = rfac0 * MY_PI / (rcut - rmin0);
           double theta0 = (r - rmin0) * rscale0;
-          double cs = cl::sycl::cos(theta0);
-          double sn = cl::sycl::sin(theta0);
+          double cs = sycl::cos(theta0);
+          double sn = sycl::sin(theta0);
           double z0 = r * cs / sn;
           double dz0dr = z0 / r - (r * rscale0) * (rsq + z0 * z0) / rsq;
 
-          compute_duarray(natom, nbor, num_atoms, num_nbor, twojmax, 
+          compute_duarray(natom, nbor, num_atoms, num_nbor, twojmax,
                           idxdu_max, jdimpq, switch_flag,
                           x, y, z, z0, r, dz0dr, wj_in, rcut,
-                          rootpqarray.get_pointer(), 
-                          ulist.get_pointer(), 
+                          rootpqarray.get_pointer(),
+                          ulist.get_pointer(),
                           dulist.get_pointer());
          }
       });
@@ -799,16 +798,16 @@ int main(int argc, char* argv[])
 
     start = system_clock::now();
     // compute_deidrj();
-    range<2> gws_k7 ((num_nbor+15)/16*16, (num_atoms+15)/16*16);
-    range<2> lws_k7 (16, 16);
+    sycl::range<2> gws_k7 ((num_nbor+15)/16*16, (num_atoms+15)/16*16);
+    sycl::range<2> lws_k7 (16, 16);
 
-    q.submit([&] (handler &cgh) {
-      auto idxdu_block = d_idxdu_block.get_access<sycl_read>(cgh);
-      auto dulist = d_dulist.get_access<sycl_read>(cgh);
-      auto ylist = d_ylist.get_access<sycl_read>(cgh);
-      auto dedr = d_dedr.get_access<sycl_read_write>(cgh);
+    q.submit([&] (sycl::handler &cgh) {
+      auto idxdu_block = d_idxdu_block.get_access<sycl::access::mode::read>(cgh);
+      auto dulist = d_dulist.get_access<sycl::access::mode::read>(cgh);
+      auto ylist = d_ylist.get_access<sycl::access::mode::read>(cgh);
+      auto dedr = d_dedr.get_access<sycl::access::mode::read_write>(cgh);
 
-      cgh.parallel_for<class compute_deidrj>(nd_range<2>(gws_k7, lws_k7), [=] (nd_item<2> item) {
+      cgh.parallel_for<class compute_deidrj>(sycl::nd_range<2>(gws_k7, lws_k7), [=] (sycl::nd_item<2> item) {
 	int nbor = item.get_global_id(0);
 	int natom = item.get_global_id(1);
         if (natom < num_atoms && nbor < num_nbor) {
@@ -872,8 +871,8 @@ int main(int argc, char* argv[])
     elapsed = end - start;
     elapsed_deidrj += elapsed.count();
 
-    q.submit([&] (handler &cgh) {
-      auto acc = d_dedr.get_access<sycl_read>(cgh);
+    q.submit([&] (sycl::handler &cgh) {
+      auto acc = d_dedr.get_access<sycl::access::mode::read>(cgh);
       cgh.copy(acc, dedr);
     });
     q.wait();
@@ -903,7 +902,7 @@ int main(int argc, char* argv[])
   }
   auto stop = myclock::now();
   myduration elapsed = stop - begin;
-  double duration = elapsed.count(); 
+  double duration = elapsed.count();
 
   printf("-----------------------\n");
   printf("Summary of TestSNAP run\n");
