d4876a91462c0ce3addc60bdf55ca5c7e76970bb
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-04-19 12:51:20 -0400

    [atomicCAS-sycl] fix compiler warnings for newer compilers; simplify the implemenetations with templated min/max/add functions; exclude h2d copy from the timing measurement

--- atomicCAS-sycl/main.cpp
+++ atomicCAS-sycl/main.cpp
@@ -10,201 +10,60 @@
 #include <stdio.h>
 #include <limits.h>
 #include <float.h>
-#include "common.h"
+#include <sycl/sycl.hpp>
 
 #define NUM_BLOCKS 1024
 #define BLOCK_SIZE 256
 
-// int64 atomic_min
-long long atomic_min(long long *address, long long val)
+// templated atomic_min
+template<typename T>
+inline T atomic_min(T *address, T val)
 {
-  sycl::atomic<unsigned long long, access::address_space::global_space> obj (
-    sycl::multi_ptr<unsigned long long, access::address_space::global_space>(
-      reinterpret_cast<unsigned long long*>(address)));
-
-  long long old_val = *address;
-  const unsigned long long new_val = *reinterpret_cast<const unsigned long long*>(&val);
-
-  while(val < old_val) 
-  {
-    if(obj.compare_exchange_strong(
-       *reinterpret_cast<unsigned long long*>(&old_val), new_val))
-      break;
-  }
-  return old_val;
+  sycl::atomic_ref<T, 
+                   sycl::memory_order::relaxed,
+                   sycl::memory_scope::device,
+                   sycl::access::address_space::global_space> ao (*address);
+  return ao.fetch_min(val);
 }
 
-// uint64 atomic_min
-inline
-unsigned long long atomic_min(unsigned long long *address, unsigned long long val)
+// templated atomic_min
+template<typename T>
+inline T atomic_max(T *address, T val)
 {
-  sycl::atomic<unsigned long long, access::address_space::global_space> obj (
-    (sycl::multi_ptr<unsigned long long, access::address_space::global_space>(address)));
-
-  unsigned long long old_val = *address;
-  while(val < old_val) 
-  {
-    if(obj.compare_exchange_strong(old_val, val))
-      break;
-  }
-  return old_val;
+  sycl::atomic_ref<T, 
+                   sycl::memory_order::relaxed,
+                   sycl::memory_scope::device,
+                   sycl::access::address_space::global_space> ao (*address);
+  return ao.fetch_max(val);
 }
 
 // uint64 atomic add
-inline
-unsigned long long atomic_add(unsigned long long *address, unsigned long long val)
+template<typename T>
+inline T atomic_add(T *address, T val)
 {
-  sycl::atomic<unsigned long long, access::address_space::global_space> obj ((
-    sycl::multi_ptr<unsigned long long, access::address_space::global_space>(
-      address)));
-
-  unsigned long long old_val = *address;
-  while(true)
-  {
-    const unsigned long long new_val = old_val + val;
-    if(obj.compare_exchange_strong(old_val, new_val))
-      break;
-  }
-  return old_val;
-}
-
-// int64 atomic_max
-long long atomic_max(long long *address, long long val)
-{
-  sycl::atomic<unsigned long long, access::address_space::global_space> obj (
-    sycl::multi_ptr<unsigned long long, access::address_space::global_space>(
-      reinterpret_cast<unsigned long long*>(address)));
-
-  long long old_val = *address;
-  const unsigned long long new_val = *reinterpret_cast<const unsigned long long*>(&val);
-
-  while(val > old_val) 
-  {
-    if(obj.compare_exchange_strong(
-       *reinterpret_cast<unsigned long long*>(&old_val), new_val))
-      break;
-  }
-  return old_val;
-}
-
-// uint64 atomic_max
-inline
-unsigned long long atomic_max(unsigned long long *address, unsigned long long val)
-{
-  sycl::atomic<unsigned long long, access::address_space::global_space> obj (
-    (sycl::multi_ptr<unsigned long long, access::address_space::global_space>(address)));
-
-  unsigned long long old_val = *address;
-  while(val > old_val) 
-  {
-    if(obj.compare_exchange_strong(old_val, val))
-      break;
-  }
-  return old_val;
-}
-
-// For all double atomics:
-//      Must do the compare with integers, not floating point,
-//      since NaN is never equal to any other NaN
-
-// double atomic_min
-double atomic_min(double *address, double val)
-{
-  sycl::atomic<unsigned long long, access::address_space::global_space> obj (
-    sycl::multi_ptr<unsigned long long, access::address_space::global_space>(
-      reinterpret_cast<unsigned long long*>(address)));
-
-  double old_val = *address;
-  const unsigned long long new_val = *reinterpret_cast<const unsigned long long*>(&val);
-
-  while(val < old_val) 
-  {
-    if(obj.compare_exchange_strong(
-       *reinterpret_cast<unsigned long long*>(&old_val), new_val))
-      break;
-  }
-  return old_val;
-}
-
-// int64 atomic add
-inline
-long long atomic_add(long long *address, long long val)
-{
-  sycl::atomic<unsigned long long, access::address_space::global_space> obj (
-    sycl::multi_ptr<unsigned long long, access::address_space::global_space>(
-      reinterpret_cast<unsigned long long*>(address)));
-
-  long long old_val = *address;
-  while(true)
-  {
-    const long long new_val = old_val + val;
-    const unsigned long long new_val_u64 = 
-      *reinterpret_cast<const unsigned long long*>(&new_val);
-    if(obj.compare_exchange_strong(
-       *reinterpret_cast<unsigned long long*>(&old_val), new_val_u64))
-      break;
-  }
-  return old_val;
-}
-
-
-// double atomic_max
-inline
-double atomic_max(double *address, double val)
-{
-  sycl::atomic<unsigned long long, access::address_space::global_space> obj (
-    sycl::multi_ptr<unsigned long long, access::address_space::global_space>(
-      reinterpret_cast<unsigned long long*>(address)));
-
-  double old_val = *address;
-  const unsigned long long new_val = *reinterpret_cast<const unsigned long long*>(&val);
-
-  while(val > old_val) 
-  {
-    if(obj.compare_exchange_strong(
-       *reinterpret_cast<unsigned long long*>(&old_val), new_val))
-      break;
-  }
-  return old_val;
-}
-
-// Double-precision floating point atomic add
-inline
-double atomic_add(double *address, double val)
-{
-  sycl::atomic<unsigned long long, access::address_space::global_space> obj (
-    sycl::multi_ptr<unsigned long long, access::address_space::global_space>(
-      reinterpret_cast<unsigned long long*>(address)));
-
-  double old_val = *address;
-  while(true)
-  {
-    const double new_val = old_val + val;
-    const unsigned long long new_val_u64 = 
-      *reinterpret_cast<const unsigned long long*>(&new_val);
-    if(obj.compare_exchange_strong(
-       *reinterpret_cast<unsigned long long*>(&old_val), new_val_u64))
-      break;
-  }
-  return old_val;
+  sycl::atomic_ref<T, 
+                   sycl::memory_order::relaxed,
+                   sycl::memory_scope::device,
+                   sycl::access::address_space::global_space> ao (*address);
+  return ao.fetch_add(val);
 }
 
 template <typename T>
-void atomicMinDerived (nd_item<1> &item, T *__restrict res)
+void atomicMinDerived (sycl::nd_item<1> &item, T *__restrict res)
 {
   int i = item.get_global_id(0) + 1;
   atomic_min(res, (T)i);
 }
 
 template <typename T>
-void atomicMaxDerived (nd_item<1> &item, T *__restrict res)
+void atomicMaxDerived (sycl::nd_item<1> &item, T *__restrict res)
 {
   int i = item.get_global_id(0) + 1;
   atomic_max(res, (T)i);
 }
 
 template <typename T>
-void atomicAddDerived (nd_item<1> &item, T *__restrict res)
+void atomicAddDerived (sycl::nd_item<1> &item, T *__restrict res)
 {
   int i = item.get_global_id(0) + 1;
   atomic_add(res, (T)i);
@@ -220,16 +79,18 @@ template <typename T>
 class k_atomic_add;
 
 template <typename T>
-void testMin (queue &q, T *h_ptr, T *d_ptr, const int repeat, const char* name) {
-  range<1> gws (NUM_BLOCKS * BLOCK_SIZE);
-  range<1> lws (BLOCK_SIZE);
+void testMin (sycl::queue &q, T *h_ptr, T *d_ptr, const int repeat, const char* name) {
+  sycl::range<1> gws (NUM_BLOCKS * BLOCK_SIZE);
+  sycl::range<1> lws (BLOCK_SIZE);
+
+  q.memcpy(d_ptr, h_ptr, sizeof(T));
 
   auto start = std::chrono::steady_clock::now();
 
   for (int n = 0; n < repeat; n++) {
-    q.memcpy(d_ptr, h_ptr, sizeof(T));
-    q.submit([&] (handler &cgh) {
-      cgh.parallel_for<class k_atomic_min<T>>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class k_atomic_min<T>>(
+        sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
         atomicMinDerived<T>(item, d_ptr);
       });
     });
@@ -245,16 +106,18 @@ void testMin (queue &q, T *h_ptr, T *d_ptr, const int repeat, const char* name) 
 }
 
 template <typename T>
-void testMax (queue &q, T *h_ptr, T *d_ptr, const int repeat, const char* name) {
-  range<1> gws (NUM_BLOCKS * BLOCK_SIZE);
-  range<1> lws (BLOCK_SIZE);
+void testMax (sycl::queue &q, T *h_ptr, T *d_ptr, const int repeat, const char* name) {
+  sycl::range<1> gws (NUM_BLOCKS * BLOCK_SIZE);
+  sycl::range<1> lws (BLOCK_SIZE);
+
+  q.memcpy(d_ptr, h_ptr, sizeof(T));
 
   auto start = std::chrono::steady_clock::now();
 
   for (int n = 0; n < repeat; n++) {
-    q.memcpy(d_ptr, h_ptr, sizeof(T));
-    q.submit([&] (handler &cgh) {
-      cgh.parallel_for<class k_atomic_max<T>>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class k_atomic_max<T>>(
+        sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
         atomicMaxDerived<T>(item, d_ptr);
       });
     });
@@ -270,16 +133,18 @@ void testMax (queue &q, T *h_ptr, T *d_ptr, const int repeat, const char* name) 
 }
 
 template <typename T>
-void testAdd (queue &q, T *h_ptr, T *d_ptr, const int repeat, const char* name) {
-  range<1> gws (NUM_BLOCKS * BLOCK_SIZE);
-  range<1> lws (BLOCK_SIZE);
+void testAdd (sycl::queue &q, T *h_ptr, T *d_ptr, const int repeat, const char* name) {
+  sycl::range<1> gws (NUM_BLOCKS * BLOCK_SIZE);
+  sycl::range<1> lws (BLOCK_SIZE);
+
+  q.memcpy(d_ptr, h_ptr, sizeof(T));
 
   auto start = std::chrono::steady_clock::now();
 
   for (int n = 0; n < repeat; n++) {
-    q.memcpy(d_ptr, h_ptr, sizeof(T));
-    q.submit([&] (handler &cgh) {
-      cgh.parallel_for<class k_atomic_add<T>>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class k_atomic_add<T>>(
+        sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
         atomicAddDerived<T>(item, d_ptr);
       });
     });
@@ -308,11 +173,10 @@ int main(int argc, char** argv) {
   double res_f64[3] = {DBL_MAX,DBL_MIN,0};
 
 #ifdef USE_GPU
-  gpu_selector dev_sel;
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-  cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-  queue q(dev_sel, property::queue::in_order());
 
   unsigned long long *d_res_u64 = sycl::malloc_device<unsigned long long>(3, q);
   long long *d_res_s64 = sycl::malloc_device<long long>(3, q);
