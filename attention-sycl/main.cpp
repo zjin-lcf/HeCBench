4e84a2a427f539f5a452f8904d5dd3a483d9ec2f
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-04-19 15:02:22 -0400

    [attention-sycl] fix compiler warnings for newer compilers

--- attention-sycl/main.cpp
+++ attention-sycl/main.cpp
@@ -2,42 +2,41 @@
 #include <stdio.h>
 #include <math.h>
 #include <chrono>
-#include "common.h"
+#include <sycl/sycl.hpp>
 #include "reference.h"
 
 float* attention_device(const float* key, const float* value, const float* query,
                         const int n, const int d, const int repeat) 
 {
 #ifdef USE_GPU
-  gpu_selector dev_sel;
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-  cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-  queue q(dev_sel, property::queue::in_order());
 
   // input
-  float *d_key = malloc_device<float>(n*d, q);
+  float *d_key = sycl::malloc_device<float>(n*d, q);
   q.memcpy(d_key, key, sizeof(float) * n * d);
 
-  float *d_value = malloc_device<float>(n*d, q);
+  float *d_value = sycl::malloc_device<float>(n*d, q);
   q.memcpy(d_value, value, sizeof(float) * n * d);
 
-  float *d_query = malloc_device<float>(d, q);
+  float *d_query = sycl::malloc_device<float>(d, q);
   q.memcpy(d_query, query, sizeof(float) * d);
 
   // intermediate
-  float *d_dot_product = malloc_device<float>(n, q);
-  float *d_score = malloc_device<float>(n, q);
-  float *d_exp_sum = malloc_device<float>(1, q);
+  float *d_dot_product = sycl::malloc_device<float>(n, q);
+  float *d_score = sycl::malloc_device<float>(n, q);
+  float *d_exp_sum = sycl::malloc_device<float>(1, q);
 
   // result
   float *output = (float*) malloc (d * sizeof(float));
-  float *d_output = malloc_device<float>(d, q);
+  float *d_output = sycl::malloc_device<float>(d, q);
 
-  range<1> n_gws ((n+255)/256*256);
-  range<1> n_lws (256);
-  range<1> d_gws ((d+255)/256*256);
-  range<1> d_lws (256);
+  sycl::range<1> n_gws ((n+255)/256*256);
+  sycl::range<1> n_lws (256);
+  sycl::range<1> d_gws ((d+255)/256*256);
+  sycl::range<1> d_lws (256);
 
   q.wait();
   auto start = std::chrono::steady_clock::now();
@@ -45,33 +44,33 @@ float* attention_device(const float* key, const float* value, const float* query
   for (int k = 0; k < repeat; k++) {
     q.memset(d_exp_sum, 0, sizeof(float));
 
-    q.submit([&] (handler &cgh) {
-      cgh.parallel_for<class k1>(nd_range<1>(n_gws, n_lws), [=] (nd_item<1> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class k1>(sycl::nd_range<1>(n_gws, n_lws), [=] (sycl::nd_item<1> item) {
         int i = item.get_global_id(0);
         if (i < n) {
           float sum = 0;
           for (int j = 0; j < d; j++)
             sum += d_key[i * d + j] * d_query[j];
           d_dot_product[i] = sum;
-          auto atomic_obj_ref = ext::oneapi::atomic_ref<float, 
-            ext::oneapi::memory_order::relaxed,
-            ext::oneapi::memory_scope::device,
-            access::address_space::global_space> (d_exp_sum[0]);
-          atomic_obj_ref.fetch_add(sycl::exp(sum));
+          auto ao = sycl::atomic_ref<float, 
+            sycl::memory_order::relaxed,
+            sycl::memory_scope::device,
+            sycl::access::address_space::global_space> (d_exp_sum[0]);
+          ao.fetch_add(sycl::exp(sum));
         }
       });
     });
 
-    q.submit([&] (handler &cgh) {
-      cgh.parallel_for<class k2>(nd_range<1>(n_gws, n_lws), [=] (nd_item<1> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class k2>(sycl::nd_range<1>(n_gws, n_lws), [=] (sycl::nd_item<1> item) {
         int i = item.get_global_id(0);
         if (i < n)
           d_score[i] = sycl::exp(d_dot_product[i]) / d_exp_sum[0];
       });
     });
 
-    q.submit([&] (handler &cgh) {
-      cgh.parallel_for<class k3>(nd_range<1>(d_gws, d_lws), [=] (nd_item<1> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class k3>(sycl::nd_range<1>(d_gws, d_lws), [=] (sycl::nd_item<1> item) {
         int j = item.get_global_id(0);
         if (j < d) {
           float sum = 0;
@@ -90,13 +89,13 @@ float* attention_device(const float* key, const float* value, const float* query
 
   q.memcpy(output, d_output, sizeof(float) * d).wait();
 
-  free(d_key, q);
-  free(d_value, q);
-  free(d_query, q);
-  free(d_dot_product, q);
-  free(d_score, q);
-  free(d_exp_sum, q);
-  free(d_output, q);
+  sycl::free(d_key, q);
+  sycl::free(d_value, q);
+  sycl::free(d_query, q);
+  sycl::free(d_dot_product, q);
+  sycl::free(d_score, q);
+  sycl::free(d_exp_sum, q);
+  sycl::free(d_output, q);
 
   return output;
 }
