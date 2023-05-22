ba39c7ac1bb6917a48209c312540e8304dd55688
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-05-22 11:26:40 -0400

    [hellinger-sycl] switch to a pointer-based program; fix compiler warnings for newer compilers

--- hellinger-sycl/main.cpp
+++ hellinger-sycl/main.cpp
@@ -41,91 +41,95 @@ int main(int argc, char** argv)
 
   int i, j;
 
+  std::cout << "Problem size: c(" << M << "," << P << ") = a(" << M << "," << N
+            << ") * b(" << N << "," << P << ")\n";
+
   // 2D arrays on host side.
-  FP(*a_host)[N] = new FP[M][N];
-  FP(*b_host)[P] = new FP[N][P];
+  FP(*h_a)[N] = new FP[M][N];
+  FP(*h_b)[P] = new FP[N][P];
   // host-side cpu result
-  FP(*c_host)[P] = new FP[M][P];
+  FP(*h_c)[P] = new FP[M][P];
   // host-side gpu result
   FP(*c_back)[P] = new FP[M][P];
 
   for (i = 0; i < M; i++)
     for (j = 0; j < N; j++)
-      a_host[i][j] = (FP)1.0 / N;
+      h_a[i][j] = (FP)1.0 / N;
 
   srand(123);
   for (i = 0; i < N; i++)
     for (j = 0; j < P; j++)
-      b_host[i][j] = rand() % 256;
+      h_b[i][j] = rand() % 256;
 
   for (j = 0; j < P; j++) { 
     FP sum = 0;
     for (i = 0; i < N; i++)
-      sum += b_host[i][j];
+      sum += h_b[i][j];
     for (i = 0; i < N; i++)
-      b_host[i][j] /= sum;
+      h_b[i][j] /= sum;
   }
 
   // Initialize the device queue with the default selector. The device queue is
   // used to enqueue kernels. It encapsulates all states needed for execution.
-  {
 #ifdef USE_GPU
-    gpu_selector dev_sel;
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-    cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-    sycl::queue q(dev_sel);
 
-    // Create buffers for matrices, buffer c is bound with host memory c_back
-    buffer<FP, 1> a(reinterpret_cast<FP*>(a_host), range<1>(M*N));
-    buffer<FP, 1> b(reinterpret_cast<FP*>(b_host), range<1>(N*P));
-    buffer<FP, 1> c(reinterpret_cast<FP*>(c_back), range<1>(M*P));
+  // Create buffers for matrices, buffer c is bound with host memory c_back
+  FP *d_a = sycl::malloc_device<FP>(M*N, q);
+  q.memcpy(d_a, h_a, sizeof(FP)*M*N);
 
-    std::cout << "Problem size: c(" << M << "," << P << ") = a(" << M << "," << N
-              << ") * b(" << N << "," << P << ")\n";
+  FP *d_b = sycl::malloc_device<FP>(N*P, q);
+  q.memcpy(d_b, h_b, sizeof(FP)*N*P);
 
-    auto grid_rows = (M + BLOCK_SIZE - 1) / BLOCK_SIZE * BLOCK_SIZE;
-    auto grid_cols = (P + BLOCK_SIZE - 1) / BLOCK_SIZE * BLOCK_SIZE;
-    range<2> gws (grid_rows, grid_cols);
-    range<2> lws (BLOCK_SIZE, BLOCK_SIZE);
+  FP *d_c = sycl::malloc_device<FP>(M*P, q);
 
-    q.wait();
-    auto start = std::chrono::steady_clock::now();
+  auto grid_rows = (M + BLOCK_SIZE - 1) / BLOCK_SIZE * BLOCK_SIZE;
+  auto grid_cols = (P + BLOCK_SIZE - 1) / BLOCK_SIZE * BLOCK_SIZE;
+  sycl::range<2> gws (grid_rows, grid_cols);
+  sycl::range<2> lws (BLOCK_SIZE, BLOCK_SIZE);
 
-    for (int i = 0; i < repeat; i++) {
-      q.submit([&](handler &h) {
-        auto A = a.get_access<sycl_read>(h);
-        auto B = b.get_access<sycl_read>(h);
-        auto C = c.get_access<sycl_discard_write>(h);
-        h.parallel_for<class hellinger>(nd_range<2>(gws, lws), [=](nd_item<2> index) {
-          int row = index.get_global_id(0);
-          int col = index.get_global_id(1);
-          if( col < P && row < M) {
-            FP sum = (FP)0.0;
-            for (int i = 0; i < N; i++) {
-              sum += sycl::sqrt(A[row * N + i] * B[i * P + col]);
-            }
-            const FP value = (FP)1.0 - sum;
-            const FP gate = (!sycl::signbit(value));
-            C[row * P + col] = sycl::sqrt(gate * value);
+  q.wait();
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    q.submit([&](sycl::handler &h) {
+      h.parallel_for<class hellinger>(
+        sycl::nd_range<2>(gws, lws), [=](sycl::nd_item<2> index) {
+        int row = index.get_global_id(0);
+        int col = index.get_global_id(1);
+        if( col < P && row < M) {
+          FP sum = (FP)0.0;
+          for (int i = 0; i < N; i++) {
+            sum += sycl::sqrt(d_a[row * N + i] * d_b[i * P + col]);
           }
-        });
+          const FP value = (FP)1.0 - sum;
+          const FP gate = (!sycl::signbit(value));
+          d_c[row * P + col] = sycl::sqrt(gate * value);
+        }
       });
-    }
-
-    q.wait();
-    auto end = std::chrono::steady_clock::now();
-    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-    std::cout << "Average kernel execution time " << (time * 1e-9f) / repeat << " (s)\n";
+    });
   }
 
+  q.wait();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  std::cout << "Average kernel execution time " << (time * 1e-9f) / repeat << " (s)\n";
+  
+  q.memcpy(c_back, d_c, sizeof(int)*M*P).wait();
+
 #ifdef VERIFY
-  VerifyResult(a_host, b_host, c_host, c_back);
+  VerifyResult(h_a, h_b, h_c, c_back);
 #endif
 
-  delete[] a_host;
-  delete[] b_host;
-  delete[] c_host;
+  delete[] h_a;
+  delete[] h_b;
+  delete[] h_c;
   delete[] c_back;
+  sycl::free(d_a, q);
+  sycl::free(d_b, q);
+  sycl::free(d_c, q);
   return 0;
 }
