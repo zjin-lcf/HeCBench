174e827776cd99ed35fcb095404a111174d36773
Jin Z <5zj@cousteau.ftpn.ornl.gov>
2023-05-22 10:10:17 -0400

    [heartwall-sycl] switch to a pointer-based program; fix compiler warnings for newer compilers

--- heartwall-sycl/kernel/kernel.cpp
+++ heartwall-sycl/kernel/kernel.cpp
@@ -1,17 +1,12 @@
 #include "./../main.h"                // (in main directory)            needed to recognized input parameters
 #include "./../util/avi/avilib.h"          // (in directory)              needed by avi functions
 #include "./../util/avi/avimod.h"          // (in directory)              needed by avi functions
-#include <CL/sycl.hpp>
+#include <sycl/sycl.hpp>
 #include <iostream>
 
-using namespace cl::sycl;
-constexpr access::mode sycl_read       = access::mode::read;
-constexpr access::mode sycl_write      = access::mode::write;
-constexpr access::mode sycl_read_write = access::mode::read_write;
-//constexpr access::mode sycl_atomic     = access::mode::atomic;
-
-  void 
-kernel_gpu_wrapper(  params_common common,
+void 
+kernel_gpu_wrapper(
+    params_common common,
     int* endoRow,
     int* endoCol,
     int* tEndoRowLoc,
@@ -24,35 +19,10 @@ kernel_gpu_wrapper(  params_common common,
 {
 
 #ifdef USE_GPU
-  gpu_selector dev_sel;
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-  cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-
-#ifdef DEBUG
-  auto exception_handler = [] (cl::sycl::exception_list exceptions) {
-    for (std::exception_ptr const& e : exceptions) {
-      try {
-        std::rethrow_exception(e);
-      } catch(cl::sycl::exception const& e) {
-        std::cout << "Caught asynchronous SYCL exception:\n"
-          << e.what() << std::endl;
-      }
-    }
-  };
-  cl::sycl::queue q(dev_sel, exception_handler);
-  auto device = q.get_device();
-
-  auto deviceName = device.get_info<info::device::name>();
-  std::cout << " Device Name: " << deviceName << std::endl;
-  auto platformName =
-    device.get_platform().get_info<info::platform::name>();
-  std::cout << " Platform Name " << platformName << std::endl;
-#else
-  cl::sycl::queue q(dev_sel);
-#endif
-
-  const property_list props = property::buffer::use_host_ptr();
 
   // common
   //printf("tSize is %d, sSize is %d\n", common.tSize, common.sSize);
@@ -65,14 +35,14 @@ kernel_gpu_wrapper(  params_common common,
   // endo points templates
   //==================================================50
 
-  buffer<FP,1> d_endoT(common.in_elem * common.endoPoints);
+  FP *d_endoT = sycl::malloc_device<FP>(common.in_elem * common.endoPoints, q);
   //printf("%d\n", common.in_elem * common.endoPoints);
 
   //==================================================50
   // epi points templates
   //==================================================50
 
-  buffer<FP,1> d_epiT(common.in_elem * common.epiPoints);
+  FP *d_epiT = sycl::malloc_device<FP>(common.in_elem * common.epiPoints, q);
   //printf("%d\n", common.in_elem * common.epiPoints);
 
   //====================================================================================================100
@@ -86,7 +56,7 @@ kernel_gpu_wrapper(  params_common common,
   common.in2_mem = sizeof(FP) * common.in2_elem;
 
   // unique
-  buffer<FP,1> d_in2(common.in2_elem * common.allPoints);
+  FP *d_in2 = sycl::malloc_device<FP>(common.in2_elem * common.allPoints, q);
   //printf("%d\n", common.in2_elem * common.allPoints);
 
   //====================================================================================================100
@@ -102,7 +72,7 @@ kernel_gpu_wrapper(  params_common common,
   common.joffset = 0;
 
   // unique
-  buffer<FP,1> d_conv(common.conv_elem * common.allPoints);
+  FP *d_conv = sycl::malloc_device<FP>(common.conv_elem * common.allPoints, q);
   //printf("%d\n", common.conv_elem * common.allPoints);
 
   //====================================================================================================100
@@ -123,7 +93,7 @@ kernel_gpu_wrapper(  params_common common,
   common.in2_pad_cumv_mem = sizeof(FP) * common.in2_pad_cumv_elem;
 
   // unique
-  buffer<FP,1> d_in2_pad_cumv(common.in2_pad_cumv_elem * common.allPoints);
+  FP *d_in2_pad_cumv = sycl::malloc_device<FP>(common.in2_pad_cumv_elem * common.allPoints, q);
   //printf("%d\n", common.in2_pad_cumv_elem * common.allPoints);
 
   //==================================================50
@@ -141,7 +111,7 @@ kernel_gpu_wrapper(  params_common common,
   common.in2_pad_cumv_sel_mem = sizeof(FP) * common.in2_pad_cumv_sel_elem;
 
   // unique
-  buffer<FP,1> d_in2_pad_cumv_sel(common.in2_pad_cumv_sel_elem * common.allPoints);
+  FP *d_in2_pad_cumv_sel = sycl::malloc_device<FP>(common.in2_pad_cumv_sel_elem * common.allPoints, q);
   //printf("%d\n", common.in2_pad_cumv_sel_elem * common.allPoints);
 
   //==================================================50
@@ -159,7 +129,7 @@ kernel_gpu_wrapper(  params_common common,
   common.in2_sub_cumh_mem = sizeof(FP) * common.in2_sub_cumh_elem;
 
   // unique
-  buffer<FP,1> d_in2_sub_cumh(common.in2_sub_cumh_elem * common.allPoints);
+  FP *d_in2_sub_cumh = sycl::malloc_device<FP>(common.in2_sub_cumh_elem * common.allPoints, q);
   //printf("%d\n", common.in2_sub_cumh_elem * common.allPoints);
 
   //==================================================50
@@ -177,7 +147,7 @@ kernel_gpu_wrapper(  params_common common,
   common.in2_sub_cumh_sel_mem = sizeof(FP) * common.in2_sub_cumh_sel_elem;
 
   // unique
-  buffer<FP,1> d_in2_sub_cumh_sel(common.in2_sub_cumh_sel_elem * common.allPoints);
+  FP *d_in2_sub_cumh_sel = sycl::malloc_device<FP>(common.in2_sub_cumh_sel_elem * common.allPoints, q);
   //printf("%d\n", common.in2_sub_cumh_sel_elem * common.allPoints);
 
   //==================================================50
@@ -195,7 +165,7 @@ kernel_gpu_wrapper(  params_common common,
   common.in2_sub2_mem = sizeof(FP) * common.in2_sub2_elem;
 
   // unique
-  buffer<FP,1> d_in2_sub2(common.in2_sub2_elem * common.allPoints);
+  FP *d_in2_sub2 = sycl::malloc_device<FP>(common.in2_sub2_elem * common.allPoints, q);
   //printf("%d\n", common.in2_sub2_elem * common.allPoints);
 
   //====================================================================================================100
@@ -213,7 +183,7 @@ kernel_gpu_wrapper(  params_common common,
   common.in2_sqr_mem = common.in2_mem;
 
   // unique
-  buffer<FP,1> d_in2_sqr(common.in2_elem * common.allPoints);
+  FP *d_in2_sqr = sycl::malloc_device<FP>(common.in2_elem * common.allPoints, q);
   //printf("%d\n", common.in2_elem * common.allPoints);
 
   //==================================================50
@@ -227,7 +197,7 @@ kernel_gpu_wrapper(  params_common common,
   common.in2_sqr_sub2_mem = common.in2_sub2_mem;
 
   // unique
-  buffer<FP,1> d_in2_sqr_sub2(common.in2_sub2_elem * common.allPoints);
+  FP *d_in2_sqr_sub2 = sycl::malloc_device<FP>(common.in2_sub2_elem * common.allPoints, q);
   //printf("%d\n", common.in2_sub2_elem * common.allPoints);
 
   //====================================================================================================100
@@ -241,7 +211,7 @@ kernel_gpu_wrapper(  params_common common,
   common.in_sqr_mem = common.in_mem;
 
   // unique
-  buffer<FP,1> d_in_sqr(common.in_elem * common.allPoints);
+  FP *d_in_sqr = sycl::malloc_device<FP>(common.in_elem * common.allPoints, q);
   //printf("%d\n", common.in_elem * common.allPoints);
 
   //====================================================================================================100
@@ -255,7 +225,7 @@ kernel_gpu_wrapper(  params_common common,
   common.tMask_mem = sizeof(FP) * common.tMask_elem;
 
   // unique
-  buffer<FP,1> d_tMask(common.tMask_elem * common.allPoints);
+  FP *d_tMask = sycl::malloc_device<FP>(common.tMask_elem * common.allPoints, q);
   //printf("%d\n", common.tMask_elem * common.allPoints);
 
   //====================================================================================================100
@@ -293,38 +263,48 @@ kernel_gpu_wrapper(  params_common common,
   //printf("common.endo_mem=%d\n", common.endo_mem); // 80
   //printf("common.epi_mem=%d\n", common.epi_mem); // 124
   //
-  //buffer<params_common,1> d_common(&common, 1, props); // range is 1 ?
-  buffer<int,1> d_endoRow(endoRow, common.endoPoints, props);
-  d_endoRow.set_final_data(nullptr);
-  buffer<int,1> d_endoCol(endoCol, common.endoPoints, props);
-  d_endoCol.set_final_data(nullptr);
-  buffer<int,1> d_tEndoRowLoc(tEndoRowLoc, common.endoPoints * common.no_frames, props);
-  buffer<int,1> d_tEndoColLoc(tEndoColLoc, common.endoPoints * common.no_frames, props);
-  buffer<int,1> d_epiRow(epiRow, common.epiPoints, props);
-  d_epiRow.set_final_data(nullptr);
-  buffer<int,1> d_epiCol(epiCol, common.epiPoints, props);
-  d_epiCol.set_final_data(nullptr);
-  buffer<int,1> d_tEpiRowLoc(tEpiRowLoc, common.epiPoints * common.no_frames, props);
-  buffer<int,1> d_tEpiColLoc(tEpiColLoc, common.epiPoints * common.no_frames, props);
+  int *d_endoRow = sycl::malloc_device<int>(common.endoPoints, q);
+  q.memcpy(d_endoRow, endoRow, common.endo_mem);
 
+  int *d_endoCol = sycl::malloc_device<int>(common.endoPoints, q);
+  q.memcpy(d_endoCol, endoCol, common.endo_mem);
 
-  buffer<FP,1> d_mask_conv(common.mask_conv_elem * common.allPoints);
+  int *d_tEndoRowLoc = sycl::malloc_device<int>(common.endoPoints * common.no_frames, q);
+  q.memcpy(d_tEndoRowLoc, tEndoRowLoc, common.endo_mem * common.no_frames);
+
+  int *d_tEndoColLoc = sycl::malloc_device<int>(common.endoPoints * common.no_frames, q);
+  q.memcpy(d_tEndoColLoc, tEndoColLoc, common.endo_mem * common.no_frames);
+
+  int *d_epiRow = sycl::malloc_device<int>(common.epiPoints, q);
+  q.memcpy(d_epiRow, epiRow, common.epi_mem);
+
+  int *d_epiCol = sycl::malloc_device<int>(common.epiPoints, q);
+  q.memcpy(d_epiCol, epiCol, common.epi_mem);
+
+  int *d_tEpiRowLoc = sycl::malloc_device<int>(common.epiPoints * common.no_frames, q);
+  q.memcpy(d_tEpiRowLoc, tEpiRowLoc, common.epi_mem * common.no_frames);
+
+  int *d_tEpiColLoc = sycl::malloc_device<int>(common.epiPoints * common.no_frames, q);
+  q.memcpy(d_tEpiColLoc, tEpiColLoc, common.epi_mem * common.no_frames);
+
+  FP *d_mask_conv = sycl::malloc_device<FP>(common.mask_conv_elem * common.allPoints, q);
   //printf("%d\n", common.mask_conv_elem * common.allPoints);
-  buffer<FP,1> d_in_mod_temp(common.in_elem * common.allPoints);
+  FP *d_in_mod_temp = sycl::malloc_device<FP>(common.in_elem * common.allPoints, q);
   //printf("%d\n", common.in_elem * common.allPoints);
-  buffer<FP,1> d_in_partial_sum(common.in_cols * common.allPoints);
+  FP *d_in_partial_sum = sycl::malloc_device<FP>(common.in_cols * common.allPoints, q);
   //printf("%d\n", common.in_cols * common.allPoints);
-  buffer<FP,1> d_in_sqr_partial_sum(common.in_sqr_rows * common.allPoints);
+  FP *d_in_sqr_partial_sum = sycl::malloc_device<FP>(common.in_sqr_rows * common.allPoints, q);
   //printf("%d\n", common.in_sqr_rows * common.allPoints);
-  buffer<FP,1> d_par_max_val(common.mask_conv_rows * common.allPoints);
+  FP *d_par_max_val = sycl::malloc_device<FP>(common.mask_conv_rows * common.allPoints, q);
   //printf("%d\n", common.mask_conv_rows * common.allPoints);
-  buffer<int,1> d_par_max_coo( common.mask_conv_rows * common.allPoints);
-  buffer<FP,1> d_in_final_sum(common.allPoints);
-  buffer<FP,1> d_in_sqr_final_sum(common.allPoints);
-  buffer<FP,1> d_denomT(common.allPoints);
+  int *d_par_max_coo = sycl::malloc_device<int>(common.mask_conv_rows * common.allPoints, q);
+  FP *d_in_final_sum = sycl::malloc_device<FP>(common.allPoints, q);
+  FP *d_in_sqr_final_sum = sycl::malloc_device<FP>(common.allPoints, q);
+  FP *d_denomT = sycl::malloc_device<FP>(common.allPoints, q);
 
 #ifdef TEST_CHECKSUM
-  buffer<FP,1> d_checksum(CHECK);
+  FP* checksum = (FP*) malloc (sizeof(FP)*CHECK);
+  FP *d_checksum = sycl::malloc_device<FP>(CHECK, q);
   //printf("%d\n", CHECK);
 #endif
 
@@ -333,11 +313,12 @@ kernel_gpu_wrapper(  params_common common,
   //====================================================================================================100
 
   // All kernels operations within kernel use same max size of threads. Size of block size is set to the size appropriate for max size operation (on padded matrix). Other use subsets of that.
-  size_t local_work_size = NUMBER_THREADS;
-  size_t global_work_size = common.allPoints * local_work_size;
+  size_t lws = NUMBER_THREADS;
+  size_t gws = common.allPoints * lws;
 
 #ifdef DEBUG
-  printf("# of workgroups = %d, # of threads/workgroup = %d (ensure that device can handle)\n",(int)(global_work_size/local_work_size), (int)local_work_size);
+  printf("# of workgroups = %d, # of threads/workgroup = %d (ensure that device can handle)\n",
+         (int)(gws/lws), (int)lws);
 #endif
 
 
@@ -352,7 +333,7 @@ kernel_gpu_wrapper(  params_common common,
   FP* frame;
   int frame_no;
 
-  buffer<FP,1> d_frame(common.frame_elem);
+  FP *d_frame = sycl::malloc_device<FP>(common.frame_elem, q);
 
   for(frame_no=0; frame_no<common.frames_processed; frame_no++){
 
@@ -361,67 +342,24 @@ kernel_gpu_wrapper(  params_common common,
     //==================================================50
 
     // Extract a cropped version of the first frame from the video file
-    frame = get_frame(  frames,                // pointer to video file
-        frame_no,              // number of frame that needs to be returned
+    frame = get_frame(frames, // pointer to video file
+        frame_no,             // number of frame that needs to be returned
         0,                  // cropped?
         0,                  // scaled?
         1);                  // converted
 
     // copy frame to GPU memory
-    q.submit ([&](handler &cgh) {
-        auto d_frame_acc = d_frame.get_access<sycl_write>(cgh);
-        cgh.copy(frame, d_frame_acc);
-        });
+    q.memcpy(d_frame, frame, sizeof(FP) * common.frame_elem);
 
     //==================================================50
     //  launch kernel
     //==================================================50
-    q.submit ([&](handler &cgh) {
-
-        // read access
-        //auto d_common_acc = d_common.get_access<sycl_read>(cgh);
-        auto d_frame_acc = d_frame.get_access<sycl_read>(cgh);
-
-        auto d_endoRow_acc = d_endoRow.get_access<sycl_read_write>(cgh);
-        auto d_endoCol_acc = d_endoCol.get_access<sycl_read_write>(cgh);
-        auto d_tEndoRowLoc_acc = d_tEndoRowLoc.get_access<sycl_read_write>(cgh);
-        auto d_tEndoColLoc_acc = d_tEndoColLoc.get_access<sycl_read_write>(cgh);
-        auto d_epiRow_acc = d_epiRow.get_access<sycl_read_write>(cgh);
-        auto d_epiCol_acc = d_epiCol.get_access<sycl_read_write>(cgh);
-        auto d_tEpiRowLoc_acc = d_tEpiRowLoc.get_access<sycl_read_write>(cgh);
-        auto d_tEpiColLoc_acc = d_tEpiColLoc.get_access<sycl_read_write>(cgh);
-
-        auto d_endoT_acc = d_endoT.get_access<sycl_read_write>(cgh);
-        auto d_epiT_acc = d_epiT.get_access<sycl_read_write>(cgh);
-        auto d_in2_all_acc = d_in2.get_access<sycl_read_write>(cgh);
-        auto d_conv_all_acc = d_conv.get_access<sycl_read_write>(cgh);
-        auto d_in2_pad_cumv_all_acc = d_in2_pad_cumv.get_access<sycl_read_write>(cgh);
-        auto d_in2_pad_cumv_sel_all_acc = d_in2_pad_cumv_sel.get_access<sycl_read_write>(cgh);
-        auto d_in2_sub_cumh_all_acc = d_in2_sub_cumh.get_access<sycl_read_write>(cgh);
-        auto d_in2_sub_cumh_sel_all_acc = d_in2_sub_cumh_sel.get_access<sycl_read_write>(cgh);
-        auto d_in2_sub2_all_acc = d_in2_sub2.get_access<sycl_read_write>(cgh);
-        auto d_in2_sqr_all_acc = d_in2_sqr.get_access<sycl_read_write>(cgh);
-        auto d_in2_sqr_sub2_all_acc = d_in2_sqr_sub2.get_access<sycl_read_write>(cgh);
-        auto d_in_sqr_all_acc = d_in_sqr.get_access<sycl_read_write>(cgh);
-        auto d_tMask_all_acc = d_tMask.get_access<sycl_read_write>(cgh);
-        auto d_mask_conv_all_acc = d_mask_conv.get_access<sycl_read_write>(cgh);
-        auto d_in_mod_temp_all_acc = d_in_mod_temp.get_access<sycl_read_write>(cgh);
-        auto d_in_partial_sum_all_acc = d_in_partial_sum.get_access<sycl_read_write>(cgh);
-        auto d_in_sqr_partial_sum_all_acc = d_in_sqr_partial_sum.get_access<sycl_read_write>(cgh);
-        auto par_max_val_all_acc = d_par_max_val.get_access<sycl_read_write>(cgh);
-        auto par_max_coo_all_acc = d_par_max_coo.get_access<sycl_read_write>(cgh);
-        auto d_in_final_sum_all_acc = d_in_final_sum.get_access<sycl_read_write>(cgh);
-        auto d_in_sqr_final_sum_all_acc = d_in_sqr_final_sum.get_access<sycl_read_write>(cgh);
-        auto denomT_all_acc = d_denomT.get_access<sycl_read_write>(cgh);
-
-#ifdef TEST_CHECKSUM
-        auto checksum_acc = d_checksum.get_access<sycl_read_write>(cgh);
-#endif
-
-        cgh.parallel_for<class heartwall>(
-            nd_range<1>(range<1>(global_work_size), range<1>(local_work_size)), [=] (nd_item<1> item) {
-#include "kernel.sycl"
-            });
+    q.submit ([&](sycl::handler &cgh) {
+      cgh.parallel_for<class heartwall>(
+        sycl::nd_range<1>(sycl::range<1>(gws), sycl::range<1>(lws)),
+        [=] (sycl::nd_item<1> item) {
+        #include "kernel.sycl"
+      });
     });
 
 #ifdef DEBUG
@@ -452,7 +390,7 @@ kernel_gpu_wrapper(  params_common common,
     //==================================================50
 
 #ifdef TEST_CHECKSUM
-    auto checksum_host = d_checksum.get_access<sycl_read>();
+    q.memcpy(checksum, d_checksum, sizeof(FP)*CHECK).wait();
     printf("CHECKSUM:\n");
     for(int i=0; i<CHECK; i++){
       printf("i=%d checksum=%f\n", i, checksum_host[i]);
@@ -466,11 +404,51 @@ kernel_gpu_wrapper(  params_common common,
 
   }
 
+  q.memcpy(tEndoRowLoc, d_tEndoRowLoc, common.endo_mem * common.no_frames);
+  q.memcpy(tEndoColLoc, d_tEndoColLoc, common.endo_mem * common.no_frames);
+  q.memcpy(tEpiRowLoc, d_tEpiRowLoc, common.epi_mem * common.no_frames);
+  q.memcpy(tEpiColLoc, d_tEpiColLoc, common.epi_mem * common.no_frames);
+  q.wait();
+
   //====================================================================================================100
   //  PRINT FRAME PROGRESS END
   //====================================================================================================100
+#ifdef TEST_CHECKSUM
+  free(checksum);
+  sycl::free(d_checksum, q);
+#endif
+  sycl::free(d_epiT, q);
+  sycl::free(d_endoT, q);
+  sycl::free(d_in2, q);
+  sycl::free(d_conv, q);
+  sycl::free(d_in2_pad_cumv, q);
+  sycl::free(d_in2_pad_cumv_sel, q);
+  sycl::free(d_in2_sub_cumh, q);
+  sycl::free(d_in2_sub_cumh_sel, q);
+  sycl::free(d_in2_sub2, q);
+  sycl::free(d_in2_sqr, q);
+  sycl::free(d_in2_sqr_sub2, q);
+  sycl::free(d_in_sqr, q);
+  sycl::free(d_tMask, q);
+  sycl::free(d_endoRow, q);
+  sycl::free(d_endoCol, q);
+  sycl::free(d_tEndoRowLoc, q);
+  sycl::free(d_tEndoColLoc, q);
+  sycl::free(d_epiRow, q);
+  sycl::free(d_epiCol, q);
+  sycl::free(d_tEpiRowLoc, q);
+  sycl::free(d_tEpiColLoc, q);
+  sycl::free(d_mask_conv, q);
+  sycl::free(d_in_mod_temp, q);
+  sycl::free(d_in_partial_sum, q);
+  sycl::free(d_in_sqr_partial_sum, q);
+  sycl::free(d_par_max_val, q);
+  sycl::free(d_par_max_coo, q);
+  sycl::free(d_in_final_sum, q);
+  sycl::free(d_in_sqr_final_sum, q);
+  sycl::free(d_denomT, q);
+  sycl::free(d_frame, q);
 
   printf("\n");
   fflush(NULL);
-
 }
