12f3dc9edebe3bd969288b46cda00c0c1bcbf275
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-05-23 14:31:39 -0400

    [hypterm-sycl] fix compiler warnings for newer compilers

--- hypterm-sycl/kernels.cpp
+++ hypterm-sycl/kernels.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <chrono>
-#include "common.h"
+#include <sycl/sycl.hpp>
 
 #define max(x,y)  ((x) > (y)? (x) : (y))
 #define min(x,y)  ((x) < (y)? (x) : (y))
@@ -13,7 +13,7 @@ void check_error (double *ptr, const char* message) {
 }
 
 __attribute__ ((always_inline))
-void hypterm_1 (nd_item<3> &item,
+void hypterm_1 (sycl::nd_item<3> &item,
                 double * __restrict flux_0,
                 double * __restrict flux_1,
                 double * __restrict flux_2,
@@ -51,7 +51,7 @@ void hypterm_1 (nd_item<3> &item,
 }
 
 __attribute__ ((always_inline))
-void hypterm_2 (nd_item<3> &item,
+void hypterm_2 (sycl::nd_item<3> &item,
                 double * __restrict flux_0,
                 double * __restrict flux_1,
                 double * __restrict flux_2,
@@ -89,7 +89,7 @@ void hypterm_2 (nd_item<3> &item,
 }
 
 __attribute__ ((always_inline))
-void hypterm_3 (nd_item<3> &item,
+void hypterm_3 (sycl::nd_item<3> &item,
                 double * __restrict flux_0,
                 double * __restrict flux_1,
                 double * __restrict flux_2,
@@ -129,64 +129,63 @@ void hypterm_3 (nd_item<3> &item,
 extern "C" void offload (double *h_flux_0, double *h_flux_1, double *h_flux_2, double *h_flux_3, double *h_flux_4, double *h_cons_1, double *h_cons_2, double *h_cons_3, double *h_cons_4, double *h_q_1, double *h_q_2, double *h_q_3, double *h_q_4, double dxinv0, double dxinv1, double dxinv2, int L, int M, int N, int repeat) {
 
 #ifdef USE_GPU
-  gpu_selector dev_sel;
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-  cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-  queue q(dev_sel);
 
   size_t vol = L*M*N;
   size_t vol_size = sizeof(double) * vol;
 
-  double *flux_0 = malloc_device<double>(vol, q);
+  double *flux_0 = sycl::malloc_device<double>(vol, q);
   check_error (flux_0, "Failed to allocate device memory for flux_0\n");
 
-  double *flux_1 = malloc_device<double>(vol, q);
+  double *flux_1 = sycl::malloc_device<double>(vol, q);
   check_error (flux_1, "Failed to allocate device memory for flux_1\n");
 
-  double *flux_2 = malloc_device<double>(vol, q);
+  double *flux_2 = sycl::malloc_device<double>(vol, q);
   check_error (flux_2, "Failed to allocate device memory for flux_2\n");
 
-  double *flux_3 = malloc_device<double>(vol, q);
+  double *flux_3 = sycl::malloc_device<double>(vol, q);
   check_error (flux_3, "Failed to allocate device memory for flux_3\n");
 
-  double *flux_4 = malloc_device<double>(vol, q);
+  double *flux_4 = sycl::malloc_device<double>(vol, q);
   check_error (flux_4, "Failed to allocate device memory for flux_4\n");
 
-  double *cons_1 = malloc_device<double>(vol, q);
+  double *cons_1 = sycl::malloc_device<double>(vol, q);
   check_error (cons_1, "Failed to allocate device memory for cons_1\n");
   q.memcpy (cons_1, h_cons_1, vol_size);
 
-  double *cons_2 = malloc_device<double>(vol, q);
+  double *cons_2 = sycl::malloc_device<double>(vol, q);
   check_error (cons_2, "Failed to allocate device memory for cons_2\n");
   q.memcpy (cons_2, h_cons_2, vol_size);
 
-  double *cons_3 = malloc_device<double>(vol, q);
+  double *cons_3 = sycl::malloc_device<double>(vol, q);
   check_error (cons_3, "Failed to allocate device memory for cons_3\n");
   q.memcpy (cons_3, h_cons_3, vol_size);
 
-  double *cons_4 = malloc_device<double>(vol, q);
+  double *cons_4 = sycl::malloc_device<double>(vol, q);
   check_error (cons_4, "Failed to allocate device memory for cons_4\n");
   q.memcpy (cons_4, h_cons_4, vol_size);
 
-  double *q_1 = malloc_device<double>(vol, q);
+  double *q_1 = sycl::malloc_device<double>(vol, q);
   check_error (q_1, "Failed to allocate device memory for q_1\n");
   q.memcpy (q_1, h_q_1, vol_size);
 
-  double *q_2 = malloc_device<double>(vol, q);
+  double *q_2 = sycl::malloc_device<double>(vol, q);
   check_error (q_2, "Failed to allocate device memory for q_2\n");
   q.memcpy (q_2, h_q_2, vol_size);
 
-  double *q_3 = malloc_device<double>(vol, q);
+  double *q_3 = sycl::malloc_device<double>(vol, q);
   check_error (q_3, "Failed to allocate device memory for q_3\n");
   q.memcpy (q_3, h_q_3, vol_size);
 
-  double *q_4 = malloc_device<double>(vol, q);
+  double *q_4 = sycl::malloc_device<double>(vol, q);
   check_error (q_4, "Failed to allocate device memory for q_3\n");
   q.memcpy (q_4, h_q_4, vol_size);
 
-  range<3> lws (4, 4, 16);
-  range<3> gws (ceil(L, 4)*4, ceil(M, 4)*4, ceil(N, 16)*16);
+  sycl::range<3> lws (4, 4, 16);
+  sycl::range<3> gws (ceil(L, 4)*4, ceil(M, 4)*4, ceil(N, 16)*16);
 
   long t1 = 0, t2 = 0, t3 = 0;
 
@@ -199,8 +198,8 @@ extern "C" void offload (double *h_flux_0, double *h_flux_1, double *h_flux_2, d
     q.wait();
 
     auto start = std::chrono::steady_clock::now();
-    q.submit([&] (handler &cgh) {
-      cgh.parallel_for<class k1>(nd_range<3>(gws, lws), [=] (nd_item<3> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class k1>(sycl::nd_range<3>(gws, lws), [=] (sycl::nd_item<3> item) {
         hypterm_1(item, flux_0, flux_1, flux_2, flux_3, flux_4,
                   cons_1, cons_2, cons_3, cons_4,
                   q_1, q_2, q_3, q_4,
@@ -211,8 +210,8 @@ extern "C" void offload (double *h_flux_0, double *h_flux_1, double *h_flux_2, d
     t1 += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
 
     start = std::chrono::steady_clock::now();
-    q.submit([&] (handler &cgh) {
-      cgh.parallel_for<class k2>(nd_range<3>(gws, lws), [=] (nd_item<3> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class k2>(sycl::nd_range<3>(gws, lws), [=] (sycl::nd_item<3> item) {
         hypterm_2(item, flux_0, flux_1, flux_2, flux_3, flux_4,
                   cons_1, cons_2, cons_3, cons_4,
                   q_1, q_2, q_3, q_4,
@@ -223,8 +222,8 @@ extern "C" void offload (double *h_flux_0, double *h_flux_1, double *h_flux_2, d
     t2 += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
 
     start = std::chrono::steady_clock::now();
-    q.submit([&] (handler &cgh) {
-      cgh.parallel_for<class k3>(nd_range<3>(gws, lws), [=] (nd_item<3> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class k3>(sycl::nd_range<3>(gws, lws), [=] (sycl::nd_item<3> item) {
         hypterm_3(item, flux_0, flux_1, flux_2, flux_3, flux_4,
                   cons_1, cons_2, cons_3, cons_4,
                   q_1, q_2, q_3, q_4,
@@ -246,17 +245,17 @@ extern "C" void offload (double *h_flux_0, double *h_flux_1, double *h_flux_2, d
   q.memcpy (h_flux_4, flux_4, vol_size);
   q.wait();
 
-  free(cons_1, q);
-  free(cons_2, q);
-  free(cons_3, q);
-  free(cons_4, q);
-  free(q_1, q);
-  free(q_2, q);
-  free(q_3, q);
-  free(q_4, q);
-  free(flux_0, q);
-  free(flux_1, q);
-  free(flux_2, q);
-  free(flux_3, q);
-  free(flux_4, q);
+  sycl::free(cons_1, q);
+  sycl::free(cons_2, q);
+  sycl::free(cons_3, q);
+  sycl::free(cons_4, q);
+  sycl::free(q_1, q);
+  sycl::free(q_2, q);
+  sycl::free(q_3, q);
+  sycl::free(q_4, q);
+  sycl::free(flux_0, q);
+  sycl::free(flux_1, q);
+  sycl::free(flux_2, q);
+  sycl::free(flux_3, q);
+  sycl::free(flux_4, q);
 }
