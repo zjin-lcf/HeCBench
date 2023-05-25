7d12a0222cf4b8b3ec57b3b0ccb2f03a6fb7df13
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-05-25 14:34:23 -0400

    [lebesgue-sycl] switch to a pointer-based program for more accurate timing; fix compiler warnings for newer compilers

--- lebesgue-sycl/lebesgue.h
+++ lebesgue-sycl/lebesgue.h
@@ -1,4 +1,4 @@
-#include "common.h"
+#include <sycl/sycl.hpp>
 
 double *chebyshev1 ( int n );
 double *chebyshev2 ( int n );
@@ -9,8 +9,8 @@ double *equidistant2 ( int n );
 double *equidistant3 ( int n );
 double *fejer1 ( int n );
 double *fejer2 ( int n );
-double lebesgue_constant ( queue &q, int n, double x[], int nfun, double xfun[] );
-double lebesgue_function ( queue &q, int n, double x[], int nfun, double xfun[] );
+double lebesgue_constant ( sycl::queue &q, int n, double x[], int nfun, double xfun[] );
+double lebesgue_function ( sycl::queue &q, int n, double x[], int nfun, double xfun[] );
 double *r8vec_linspace_new ( int n, double a, double b );
 double r8vec_max ( int n, double r8vec[] );
 void r8vec_print ( int n, double a[], const char *title );
