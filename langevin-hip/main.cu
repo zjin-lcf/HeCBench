aeaa5f82a12d4cf419ce339aff24af0f7aa19fa7
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-05-24 21:35:16 -0400

    [langevin] add missing Makefile scripts; remove three kernels from evaluation for they are slower and less accurate; add a reference function for error statistics

--- langevin-hip/main.cu
+++ langevin-hip/main.cu
@@ -15,63 +15,39 @@ __global__
 void k1 (const float *__restrict__ a, float *__restrict__ o) {
   int t = blockIdx.x * blockDim.x + threadIdx.x;
   float x = a[t];
-  o[t] = (expf(x) + expf(-x)) / (expf(x) - expf(-x)) - 1.f/x;
-}
-
-__global__
-void k2 (const float *__restrict__ a, float *__restrict__ o) {
-  int t = blockIdx.x * blockDim.x + threadIdx.x;
-  float x = a[t];
-  o[t] = (expf(2*x) + 1.f) / (expf(2*x) - 1.f) - 1.f/x;
-}
-
-__global__
-void k3 (const float *__restrict__ a, float *__restrict__ o) {
-  int t = blockIdx.x * blockDim.x + threadIdx.x;
-  float x = a[t];
   o[t] = 1.f / tanhf(x) - 1.f/x;
-}
-
-__global__
-void k4 (const float *__restrict__ a, float *__restrict__ o) {
-  int t = blockIdx.x * blockDim.x + threadIdx.x;
-  float x = a[t];
-  float x2 = x * x;
-  float x4 = x2 * x2;
-  float x6 = x4 * x2;
-  o[t] = x * (1.f/3.f - 1.f/45.f * x2 + 2.f/945.f * x4 - 1.f/4725.f * x6);
 }
 
 /*
 Copyright (c) 2018-2021, Norbert Juffa
   All rights reserved.
 
-  Redistribution and use in source and binary forms, with or without 
+  Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions
   are met:
 
-  1. Redistributions of source code must retain the above copyright 
+  1. Redistributions of source code must retain the above copyright
      notice, this list of conditions and the following disclaimer.
 
   2. Redistributions in binary form must reproduce the above copyright
      notice, this list of conditions and the following disclaimer in the
      documentation and/or other materials provided with the distribution.
 
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 __global__
-void k5 (const float *__restrict__ a, float *__restrict__ o) {
+void k2 (const float *__restrict__ a, float *__restrict__ o) {
   int t = blockIdx.x * blockDim.x + threadIdx.x;
   float x = a[t];
   float s, r;
@@ -93,13 +69,14 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  const int n = atoi(argv[1]); 
-  const int repeat = atoi(argv[2]); 
+  const int n = atoi(argv[1]);
+  const int repeat = atoi(argv[2]);
   const size_t size = sizeof(float) * n;
 
-  float *a, *o0, *o1, *o2, *o3, *o4, *o5;
+  float *a, *o, *o0, *o1, *o2;
 
   a = (float*) malloc (size);
+  o = (float*) malloc (size);
   // the range [-1.8, -0.00001)
   for (int i = 0; i < n; i++) {
     a[i] = -1.8f + i * (1.79999f / n);
@@ -108,25 +85,19 @@ int main(int argc, char* argv[]) {
   o0 = (float*) malloc (size);
   o1 = (float*) malloc (size);
   o2 = (float*) malloc (size);
-  o3 = (float*) malloc (size);
-  o4 = (float*) malloc (size);
-  o5 = (float*) malloc (size);
 
-  float *d_a, *d_o0, *d_o1, *d_o2, *d_o3, *d_o4, *d_o5;
+  float *d_a, *d_o0, *d_o1, *d_o2;
   hipMalloc((void**)&d_a, size);
   hipMalloc((void**)&d_o0, size);
   hipMalloc((void**)&d_o1, size);
   hipMalloc((void**)&d_o2, size);
-  hipMalloc((void**)&d_o3, size);
-  hipMalloc((void**)&d_o4, size);
-  hipMalloc((void**)&d_o5, size);
 
   hipMemcpy(d_a, a, size, hipMemcpyHostToDevice);
 
   hipDeviceSynchronize();
   auto start = std::chrono::steady_clock::now();
   for (int i = 0; i < repeat; i++) {
-    hipLaunchKernelGGL(k0, n/256, 256, 0, 0, d_a, d_o0);
+    k0<<<n/256, 256>>>(d_a, d_o0);
   }
   hipDeviceSynchronize();
   auto end = std::chrono::steady_clock::now();
@@ -135,7 +106,7 @@ int main(int argc, char* argv[]) {
 
   start = std::chrono::steady_clock::now();
   for (int i = 0; i < repeat; i++) {
-    hipLaunchKernelGGL(k1, n/256, 256, 0, 0, d_a, d_o1);
+    k1<<<n/256, 256>>>(d_a, d_o1);
   }
   hipDeviceSynchronize();
   end = std::chrono::steady_clock::now();
@@ -144,97 +115,48 @@ int main(int argc, char* argv[]) {
 
   start = std::chrono::steady_clock::now();
   for (int i = 0; i < repeat; i++) {
-    hipLaunchKernelGGL(k2, n/256, 256, 0, 0, d_a, d_o2);
+    k2<<<n/256, 256>>>(d_a, d_o2);
   }
   hipDeviceSynchronize();
   end = std::chrono::steady_clock::now();
   time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
   printf("Average execution time of k2: %f (s)\n", (time * 1e-9f) / repeat);
 
-  start = std::chrono::steady_clock::now();
-  for (int i = 0; i < repeat; i++) {
-    hipLaunchKernelGGL(k3, n/256, 256, 0, 0, d_a, d_o3);
-  }
-  hipDeviceSynchronize();
-  end = std::chrono::steady_clock::now();
-  time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  printf("Average execution time of k3: %f (s)\n", (time * 1e-9f) / repeat);
-
-  start = std::chrono::steady_clock::now();
-  for (int i = 0; i < repeat; i++) {
-    hipLaunchKernelGGL(k4, n/256, 256, 0, 0, d_a, d_o4);
-  }
-  hipDeviceSynchronize();
-  end = std::chrono::steady_clock::now();
-  time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  printf("Average execution time of k4: %f (s)\n", (time * 1e-9f) / repeat);
-
-  start = std::chrono::steady_clock::now();
-  for (int i = 0; i < repeat; i++) {
-    hipLaunchKernelGGL(k5, n/256, 256, 0, 0, d_a, d_o5);
-  }
-  hipDeviceSynchronize();
-  end = std::chrono::steady_clock::now();
-  time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  printf("Average execution time of k5: %f (s)\n", (time * 1e-9f) / repeat);
-
   hipMemcpy(o0, d_o0, size, hipMemcpyDeviceToHost);
   hipMemcpy(o1, d_o1, size, hipMemcpyDeviceToHost);
   hipMemcpy(o2, d_o2, size, hipMemcpyDeviceToHost);
-  hipMemcpy(o3, d_o3, size, hipMemcpyDeviceToHost);
-  hipMemcpy(o4, d_o4, size, hipMemcpyDeviceToHost);
-  hipMemcpy(o5, d_o5, size, hipMemcpyDeviceToHost);
-    
-  float s01 = 0, s02 = 0, s03 = 0, s04 = 0, s05 = 0;
-  float s12 = 0, s13 = 0, s14 = 0, s15 = 0;
-  float s23 = 0, s24 = 0, s25 = 0;
-  float s34 = 0, s35 = 0;
-  float s45 = 0;
 
-  for (int j = 0; j < n; j++) {
-    s01 += (o0[j] - o1[j]) * (o0[j] - o1[j]);
-    s02 += (o0[j] - o2[j]) * (o0[j] - o2[j]);
-    s03 += (o0[j] - o3[j]) * (o0[j] - o3[j]);
-    s04 += (o0[j] - o4[j]) * (o0[j] - o4[j]);
-    s05 += (o0[j] - o5[j]) * (o0[j] - o5[j]);
-
-    s12 += (o1[j] - o2[j]) * (o1[j] - o2[j]);
-    s13 += (o1[j] - o3[j]) * (o1[j] - o3[j]);
-    s14 += (o1[j] - o4[j]) * (o1[j] - o4[j]);
-    s15 += (o1[j] - o5[j]) * (o1[j] - o5[j]);
-
-    s23 += (o2[j] - o3[j]) * (o2[j] - o3[j]);
-    s24 += (o2[j] - o4[j]) * (o2[j] - o4[j]);
-    s25 += (o2[j] - o5[j]) * (o2[j] - o5[j]);
-
-    s34 += (o3[j] - o4[j]) * (o3[j] - o4[j]);
-    s35 += (o3[j] - o5[j]) * (o3[j] - o5[j]);
-
-    s45 += (o4[j] - o5[j]) * (o4[j] - o5[j]);
+  // https://en.wikipedia.org/wiki/Brillouin_and_Langevin_functions
+  for (int i = 0; i < n; i++) {
+    float x = a[i];
+    float x2 = x * x;
+    float x4 = x2 * x2;
+    float x6 = x4 * x2;
+    o[i] = x * (1.f/3.f - 1.f/45.f * x2 + 2.f/945.f * x4 - 1.f/4725.f * x6);
   }
 
-  printf("\nSquared error statistics for six kernels :\n");
-  printf("%f (k0-k1) %f (k0-k2) %f (k0-k3) %f (k0-k4) %f (k0-k5)\n",
-         s01, s02, s03, s04, s05);
-  printf("%f (k1-k2) %f (k1-k3) %f (k1-k4) %f (k1-k5)\n",
-         s12, s13, s14, s15);
-  printf("%f (k2-k3) %f (k2-k4) %f (k2-k5)\n", s23, s24, s25);
-  printf("%f (k3-k4) %f (k3-k5) \n", s34, s35);
-  printf("%f (k4-k5) \n", s45);
+  float e[3] = {0,0,0};
+
+  for (int i = 0; i < n; i++) {
+    e[0] += (o[i] - o0[i]) * (o[i] - o0[i]);
+    e[1] += (o[i] - o1[i]) * (o[i] - o1[i]);
+    e[2] += (o[i] - o2[i]) * (o[i] - o2[i]);
+  }
+
+  printf("\nError statistics for the kernels:\n");
+  for (int i = 0; i < 3; i++) {
+    printf("%f ", sqrt(e[i]));
+  }
+  printf("\n");
 
   free(a);
+  free(o);
   free(o0);
   free(o1);
   free(o2);
-  free(o3);
-  free(o4);
-  free(o5);
   hipFree(d_a);
   hipFree(d_o0);
   hipFree(d_o1);
   hipFree(d_o2);
-  hipFree(d_o3);
-  hipFree(d_o4);
-  hipFree(d_o5);
   return 0;
 }
