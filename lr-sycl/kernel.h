059bdef267245b9b4b91f1e12fde52de51ce44ad
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-05-26 12:07:49 -0400

    [lr-sycl] fix compiler warnings for newer compilers

--- lr-sycl/kernel.h
+++ lr-sycl/kernel.h
@@ -1,11 +1,11 @@
 void linear_regression(
-  nd_item<1> &item,
-  const float2 *__restrict dataset,
-  float4 *__restrict interns,
-  float4 *__restrict result)
+  sycl::nd_item<1> &item,
+  const sycl::float2 *__restrict dataset,
+  sycl::float4 *__restrict interns,
+  sycl::float4 *__restrict result)
 {
   size_t loc_id   = item.get_local_id(0);
-  size_t loc_size = item.get_local_range(0); 
+  size_t loc_size = item.get_local_range(0);
   size_t glob_id  = item.get_group(0) * loc_size + loc_id;
 
   /* Initialize local buffer */
@@ -13,8 +13,8 @@ void linear_regression(
   interns[loc_id].y() = dataset[glob_id].y();
   interns[loc_id].z() = (dataset[glob_id].x() * dataset[glob_id].y());
   interns[loc_id].w() = (dataset[glob_id].x() * dataset[glob_id].x());
-  
-  item.barrier(access::fence_space::local_space);
+
+  item.barrier(sycl::access::fence_space::local_space);
 
   for (size_t i = (loc_size / 2), old_i = loc_size; i > 0; old_i = i, i /= 2)
   {
@@ -26,22 +26,22 @@ void linear_regression(
         interns[loc_id] += interns[old_i - 1];
       }
     }
-    item.barrier(access::fence_space::local_space);
+    item.barrier(sycl::access::fence_space::local_space);
   }
 
   if (loc_id == 0) result[item.get_group(0)] = interns[0];
 }
 
 void rsquared(
-  nd_item<1> &item,  
-  const float2 *__restrict dataset,
+  sycl::nd_item<1> &item,
+  const sycl::float2 *__restrict dataset,
   const float mean,
-  const float2 equation, // [a0,a1]
-  float2 *__restrict dist,
-  float2 *__restrict result)
+  const sycl::float2 equation, // [a0,a1]
+  sycl::float2 *__restrict dist,
+  sycl::float2 *__restrict result)
 {
   size_t loc_id   = item.get_local_id(0);
-  size_t loc_size = item.get_local_range(0); 
+  size_t loc_size = item.get_local_range(0);
   size_t glob_id  = item.get_group(0) * loc_size + loc_id;
 
   dist[loc_id].x() = sycl::pow((dataset[glob_id].y() - mean), 2.f);
@@ -49,7 +49,7 @@ void rsquared(
   float y_estimated = dataset[glob_id].x() * equation.y() + equation.x();
   dist[loc_id].y() = sycl::pow((y_estimated - mean), 2.f);
 
-  item.barrier(access::fence_space::local_space);
+  item.barrier(sycl::access::fence_space::local_space);
 
   for (size_t i = (loc_size / 2), old_i = loc_size; i > 0; old_i = i, i /= 2)
   {
@@ -61,7 +61,7 @@ void rsquared(
         dist[loc_id] += dist[old_i - 1];
       }
     }
-    item.barrier(access::fence_space::local_space);
+    item.barrier(sycl::access::fence_space::local_space);
   }
 
   if (loc_id == 0) result[item.get_group(0)] = dist[0];
