f16156aec27417d189409a036bd2c4ebd85258ce
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-05-31 09:48:11 -0400

    [mt-sycl] switch to a pointer-based program; fix compiler warnings for newer compilers

--- mt-sycl/MT.cpp
+++ mt-sycl/MT.cpp
@@ -17,12 +17,12 @@
 // standard utilities and systems includes
 #include <stdio.h>
 #include "MT.h"
-#include "common.h"
+#include <sycl/sycl.hpp>
 
 // comment the below line if not doing Box-Muller transformation
 #define DO_BOXMULLER
 
-// Reference CPU MT and Box-Muller transformation 
+// Reference CPU MT and Box-Muller transformation
 extern "C" void initMTRef(const char *fname);
 extern "C" void RandomRef(float *h_Rand, int nPerRng, unsigned int seed);
 #ifdef DO_BOXMULLER
@@ -35,14 +35,14 @@ using namespace std::chrono;
 ///////////////////////////////////////////////////////////////////////////////
 //Load twister configurations
 ///////////////////////////////////////////////////////////////////////////////
-void loadMTGPU(const char *fname, 
-	       const unsigned int seed, 
+void loadMTGPU(const char *fname,
+	       const unsigned int seed,
 	       mt_struct_stripped *h_MT,
 	       const size_t size)
 {
   // open the file for binary read
   FILE* fd = fopen(fname, "rb");
-  if (fd == NULL) 
+  if (fd == NULL)
   {
     printf("Failed to open file %s\n", fname);
     exit(-1);
@@ -65,7 +65,7 @@ void BoxMullerTrans(float *u1, float *u2)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// Main function 
+// Main function
 ///////////////////////////////////////////////////////////////////////////////
 int main(int argc, const char **argv)
 {
@@ -76,7 +76,10 @@ int main(int argc, const char **argv)
   int numIterations = atoi(argv[1]);
 
   size_t globalWorkSize = {MT_RNG_COUNT};  // 1D var for Total # of work items
-  size_t localWorkSize = {128};            // 1D var for # of work items in the work group	
+  size_t localWorkSize = {128};            // 1D var for # of work items in the work group
+  sycl::range<1> gws (globalWorkSize);
+  sycl::range<1> lws (localWorkSize);
+
   const int seed = 777;
   const int nPerRng = 5860;                // # of recurrence steps, must be even if do Box-Muller transformation
   const int nRand = MT_RNG_COUNT * nPerRng;// Output size
@@ -94,42 +97,40 @@ int main(int argc, const char **argv)
   float *h_RandGPU = (float*)malloc(sizeof(float)*nRand); // Host buffers for GPU output
   float *h_RandCPU = (float*)malloc(sizeof(float)*nRand); // Host buffers for CPU test
 
-  printf("Allocate memory...\n"); 
-  
-  buffer<mt_struct_stripped, 1> d_MT (h_MT, MT_RNG_COUNT); 
-  buffer<float, 1> d_Rand (nRand);
+  printf("Allocate memory...\n");
 
 #ifdef USE_GPU
-  gpu_selector dev_sel;
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-  cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-  queue q(dev_sel);
 
-  printf("Call Mersenne Twister kernel... (%d iterations)\n\n", numIterations); 
+  mt_struct_stripped *d_MT = sycl::malloc_device<mt_struct_stripped>(MT_RNG_COUNT, q);
+  q.memcpy(d_MT, h_MT, sizeof(mt_struct_stripped)*MT_RNG_COUNT).wait();
+
+  float *d_Rand = sycl::malloc_device<float>(nRand, q);
+
+  printf("Call Mersenne Twister kernel... (%d iterations)\n\n", numIterations);
   high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
-  range<1> gws (globalWorkSize);
-  range<1> lws (localWorkSize);
   for (int i = 0; i < numIterations; i++)
   {
-    q.submit([&] (handler &cgh) {
-      auto Rand = d_Rand.get_access<sycl_discard_write>(cgh); 
-      auto MT = d_MT.get_access<sycl_read>(cgh); 
-      cgh.parallel_for<class mt>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class mt>(
+        sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
         int globalID = item.get_global_id(0);
 
         int iState, iState1, iStateM, iOut;
         unsigned int mti, mti1, mtiM, x;
-        unsigned int mt[MT_NN], matrix_a, mask_b, mask_c; 
+        unsigned int mt[MT_NN], matrix_a, mask_b, mask_c;
 
         //Load bit-vector Mersenne Twister parameters
-        matrix_a = MT[globalID].matrix_a;
-        mask_b   = MT[globalID].mask_b;
-        mask_c   = MT[globalID].mask_c;
-            
+        matrix_a = d_MT[globalID].matrix_a;
+        mask_b   = d_MT[globalID].mask_b;
+        mask_c   = d_MT[globalID].mask_c;
+
         //Initialize current state
-        mt[0] = MT[globalID].seed;
+        mt[0] = d_MT[globalID].seed;
         for (iState = 1; iState < MT_NN; iState++)
           mt[iState] = (1812433253U * (mt[iState - 1] ^ (mt[iState - 1] >> 30)) + iState) & MT_WMASK;
 
@@ -158,19 +159,19 @@ int main(int argc, const char **argv)
           x ^= (x >> MT_SHIFT1);
 
           //Convert to (0, 1] float and write to global memory
-          Rand[globalID + iOut * MT_RNG_COUNT] = ((float)x + 1.0f) / 4294967296.0f;
+          d_Rand[globalID + iOut * MT_RNG_COUNT] = ((float)x + 1.0f) / 4294967296.0f;
         }
       });
     });
-  	
-    #ifdef DO_BOXMULLER 
-    q.submit([&] (handler &cgh) {
-      auto Rand = d_Rand.get_access<sycl_read_write>(cgh); 
-      cgh.parallel_for<class boxmuller>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+
+    #ifdef DO_BOXMULLER
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class boxmuller>(
+        sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
         int globalID = item.get_global_id(0);
         for (int iOut = 0; iOut < nPerRng; iOut += 2) {
-           BoxMullerTrans(&Rand[globalID + (iOut + 0) * MT_RNG_COUNT],
-             &Rand[globalID + (iOut + 1) * MT_RNG_COUNT]);
+           BoxMullerTrans(&d_Rand[globalID + (iOut + 0) * MT_RNG_COUNT],
+                          &d_Rand[globalID + (iOut + 1) * MT_RNG_COUNT]);
         }
       });
     });
@@ -182,14 +183,11 @@ int main(int argc, const char **argv)
   duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
   double gpuTime = time_span.count() / (double)numIterations;
   printf("MersenneTwister, Throughput = %.4f GNumbers/s, "
-         "Time = %.5f s, Size = %u Numbers, Workgroup = %lu\n", 
-         ((double)nRand * 1.0E-9 / gpuTime), gpuTime, nRand, localWorkSize);    
+         "Time = %.5f s, Size = %u Numbers, Workgroup = %lu\n",
+         ((double)nRand * 1.0E-9 / gpuTime), gpuTime, nRand, localWorkSize);
 
-  printf("\nRead back results...\n"); 
-  q.submit([&] (handler &cgh) {
-    auto acc = d_Rand.get_access<sycl_read>(cgh); 
-    cgh.copy(acc, h_RandGPU);
-  }).wait();
+  printf("\nRead back results...\n");
+  q.memcpy(h_RandGPU, d_Rand, sizeof(float)*nRand).wait();
 
   printf("Compute CPU reference solution...\n");
   RandomRef(h_RandCPU, nPerRng, seed);
@@ -215,6 +213,8 @@ int main(int argc, const char **argv)
   free(h_MT);
   free(h_RandGPU);
   free(h_RandCPU);
+  sycl::free(d_MT, q);
+  sycl::free(d_Rand, q);
 
   // finish
   printf("%s\n", (L1norm < 1e-6) ? "PASS" : "FAIL");
