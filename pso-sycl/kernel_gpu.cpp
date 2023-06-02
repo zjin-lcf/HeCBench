0d79fd394566281a299bce9ab2968e8172e0a043
Jin Z <5zj@milan0.ftpn.ornl.gov>
2023-06-02 17:10:50 -0400

    [pso-sycl] switch to a pointer-based program; fix compiler warnings for newer compilers

--- pso-sycl/kernel_gpu.cpp
+++ pso-sycl/kernel_gpu.cpp
@@ -1,5 +1,5 @@
 #include <chrono>
-#include "common.h"
+#include <sycl/sycl.hpp>
 #include "kernel.h"
 
 float fitness_function(float x[])
@@ -20,7 +20,7 @@ float fitness_function(float x[])
   return res;
 }
 
-void kernelUpdateParticle(nd_item<1> &item,
+void kernelUpdateParticle(sycl::nd_item<1> &item,
                           float *__restrict positions,
                           float *__restrict velocities,
                           const float *__restrict pBests,
@@ -38,7 +38,7 @@ void kernelUpdateParticle(nd_item<1> &item,
   positions[i]+=velocities[i];
 }
 
-void kernelUpdatePBest(nd_item<1> &item,
+void kernelUpdatePBest(sycl::nd_item<1> &item,
                        const float *__restrict positions,
                              float *__restrict pBests,
                              float *__restrict gBest,
@@ -65,10 +65,10 @@ void kernelUpdatePBest(nd_item<1> &item,
     if(fitness_function(tempParticle1)<130.f) //fitness_function(gBest))
     {
       for(int j=0;j<DIM;j++) {
-        auto ao = ext::oneapi::atomic_ref<float, 
-            ext::oneapi::memory_order::relaxed,
-            ext::oneapi::memory_scope::device,
-            access::address_space::global_space> (gBest[j]);
+        auto ao = sycl::atomic_ref<float,
+            sycl::memory_order::relaxed,
+            sycl::memory_scope::device,
+            sycl::access::address_space::global_space> (gBest[j]);
         ao.fetch_add(tempParticle1[j]);
       }
     }
@@ -83,21 +83,20 @@ extern "C" void gpu_pso(int p, int r,
   size_t res_size_byte = sizeof(float) * DIM;
 
 #ifdef USE_GPU
-  gpu_selector dev_sel;
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-  cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-  queue q(dev_sel, property::queue::in_order());
 
-  float *devPos = malloc_device<float>(size, q);
-  float *devVel = malloc_device<float>(size, q);
-  float *devPBest = malloc_device<float>(size, q);
-  float *devGBest = malloc_device<float>(DIM, q);
+  float *devPos = sycl::malloc_device<float>(size, q);
+  float *devVel = sycl::malloc_device<float>(size, q);
+  float *devPBest = sycl::malloc_device<float>(size, q);
+  float *devGBest = sycl::malloc_device<float>(DIM, q);
 
   const int threadNum = 256;
-  range<1> lws (threadNum);
-  range<1> gws1 ((size+threadNum-1)/threadNum*threadNum);
-  range<1> gws2 ((p+threadNum-1)/threadNum*threadNum);
+  sycl::range<1> lws (threadNum);
+  sycl::range<1> gws1 ((size+threadNum-1)/threadNum*threadNum);
+  sycl::range<1> gws2 ((p+threadNum-1)/threadNum*threadNum);
 
   q.memcpy(devPos,positions,size_byte);
   q.memcpy(devVel,velocities,size_byte);
@@ -111,15 +110,15 @@ extern "C" void gpu_pso(int p, int r,
   {
     float rp=getRandomClamped(iter);
     float rg=getRandomClamped(r-iter);
-    q.submit([&] (handler &cgh) {
-      cgh.parallel_for<class k1>(nd_range<1>(gws1, lws), [=] (nd_item<1> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class k1>(sycl::nd_range<1>(gws1, lws), [=] (sycl::nd_item<1> item) {
         kernelUpdateParticle(item, devPos,devVel,devPBest,devGBest,
                              p,rp,rg);
       });
     });
 
-    q.submit([&] (handler &cgh) {
-      cgh.parallel_for<class k2>(nd_range<1>(gws2, lws), [=] (nd_item<1> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      cgh.parallel_for<class k2>(sycl::nd_range<1>(gws2, lws), [=] (sycl::nd_item<1> item) {
         kernelUpdatePBest(item, devPos,devPBest,devGBest,p);
       });
     });
@@ -129,13 +128,13 @@ extern "C" void gpu_pso(int p, int r,
   auto end = std::chrono::steady_clock::now();
   auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
   printf("Average kernel execution time %f (us)\n", time * 1e-3f / r);
-  
+
   q.memcpy(gBest,devGBest,res_size_byte);
   q.memcpy(pBests,devPBest,size_byte);
   q.wait();
 
-  free(devPos, q);
-  free(devVel, q);
-  free(devPBest, q);
-  free(devGBest, q);
+  sycl::free(devPos, q);
+  sycl::free(devVel, q);
+  sycl::free(devPBest, q);
+  sycl::free(devGBest, q);
 }
