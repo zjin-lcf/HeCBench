6f46a6c01f3e05f42187e69d26e563309a8027b3
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-06-06 19:25:07 -0400

    [s3d-sycl] switch to a pointer-based SYCL program; fix compiler warnings for newer compilers

--- s3d-sycl/S3D.h
+++ s3d-sycl/S3D.h
@@ -40,13 +40,13 @@ inline T POW (T in, T in2);
 template<>
 inline double POW<double>(double in, double in2)
 {
-    return cl::sycl::pow(in, in2);
+    return sycl::pow(in, in2);
 }
 
 template<>
 inline float POW<float>(float in, float in2)
 {
-    return cl::sycl::pow(in, in2);
+    return sycl::pow(in, in2);
 }
 // EXP
 template<class T>
@@ -55,13 +55,13 @@ inline T EXP(T in);
 template<>
 inline double EXP<double>(double in)
 {
-    return cl::sycl::exp(in);
+    return sycl::exp(in);
 }
 
 template<>
 inline float EXP<float>(float in)
 {
-    return cl::sycl::exp(in);
+    return sycl::exp(in);
 }
 
 // EXP10
@@ -71,13 +71,13 @@ inline T EXP10(T in);
 template<>
 inline double EXP10<double>(double in)
 {
-    return cl::sycl::exp10(in);
+    return sycl::exp10(in);
 }
 
 template<>
 inline float EXP10<float>(float in)
 {
-    return cl::sycl::exp10(in);
+    return sycl::exp10(in);
 }
 
 // EXP2
@@ -87,13 +87,13 @@ inline T EXP2(T in);
 template<>
 inline double EXP2<double>(double in)
 {
-    return cl::sycl::exp2(in);
+    return sycl::exp2(in);
 }
 
 template<>
 inline float EXP2<float>(float in)
 {
-    return cl::sycl::exp2(in);
+    return sycl::exp2(in);
 }
 
 // FMAX
@@ -103,13 +103,13 @@ inline T MAX(T in, T in2);
 template<>
 inline double MAX<double>(double in, double in2)
 {
-    return cl::sycl::fmax(in, in2);
+    return sycl::fmax(in, in2);
 }
 
 template<>
 inline float MAX<float>(float in, float in2)
 {
-    return cl::sycl::fmax(in, in2);
+    return sycl::fmax(in, in2);
 }
 
 // FMIN
@@ -119,13 +119,13 @@ inline T MIN(T in, T in2);
 template<>
 inline double MIN<double>(double in, double in2)
 {
-    return cl::sycl::fmin(in, in2);
+    return sycl::fmin(in, in2);
 }
 
 template<>
 inline float MIN<float>(float in, float in2)
 {
-    return cl::sycl::fmin(in, in2);
+    return sycl::fmin(in, in2);
 }
 
 // LOG
@@ -135,13 +135,13 @@ inline T LOG(T in);
 template<>
 inline double LOG<double>(double in)
 {
-    return cl::sycl::log(in);
+    return sycl::log(in);
 }
 
 template<>
 inline float LOG<float>(float in)
 {
-    return cl::sycl::log(in);
+    return sycl::log(in);
 }
 
 // LOG10
@@ -151,13 +151,13 @@ inline T LOG10(T in);
 template<>
 inline double LOG10<double>(double in)
 {
-    return cl::sycl::log10(in);
+    return sycl::log10(in);
 }
 
 template<>
 inline float LOG10<float>(float in)
 {
-    return cl::sycl::log10(in);
+    return sycl::log10(in);
 }
 
 template <class t1, class t2, class t3, class t4, class t5>
@@ -171,18 +171,18 @@ t1 polyx(t1 x, t2 c0, t3 c1, t4 c2, t5 c3)
 #define idx2(p,z) (p[(((z)-1)*(N_GP)) + item.get_global_id(0)])
 #define idx(x, y) ((x)[(y)-1])
 
-#define C(q)     idx2(C, q)
-#define Y(q)     idx2(Y, q)
-#define RF(q)    idx2(RF, q)
-#define EG(q)    idx2(EG, q)
-#define RB(q)    idx2(RB, q)
-#define RKLOW(q) idx2(RKLOW, q)
+#define C(q)     idx2(d_c, q)
+#define Y(q)     idx2(d_y, q)
+#define RF(q)    idx2(d_rf, q)
+#define EG(q)    idx2(d_eg, q)
+#define RB(q)    idx2(d_rb, q)
+#define RKLOW(q) idx2(d_rklow, q)
 #define ROP(q)   idx(ROP, q)
-#define WDOT(q)  idx2(WDOT, q)
-#define RKF(q)   idx2(RKF, q)
-#define RKR(q)   idx2(RKR, q)
+#define WDOT(q)  idx2(d_wdot, q)
+#define RKF(q)   idx2(d_rf, q)
+#define RKR(q)   idx2(d_rb, q)
 #define A_DIM    (11)
-#define A(b, c)  idx2(A, (((b)*A_DIM)+c) )
+#define A(b, c)  idx2(d_a, (((b)*A_DIM)+c) )
 
 // Size macros
 // This is the number of floats/doubles per thread for each var
