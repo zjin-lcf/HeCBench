db34d283185c7b0f106c7ec38179ad747a0f0c9b
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-06-10 14:47:20 -0400

    [softmax-sycl] switch to a pointer-based program; fix compiler warnings for newer compilers

--- softmax-sycl/main.cpp
+++ softmax-sycl/main.cpp
@@ -1,7 +1,7 @@
 #include <chrono>
 #include <cstdlib>
 #include <cstdio>
-#include "common.h"
+#include <sycl/sycl.hpp>
 
 #define BLOCK_SIZE 256
 
@@ -44,41 +44,39 @@ int main(int argc, char* argv[]) {
     for (int j = 0; j < sliceSize; j++)
       input[i*sliceSize+j] = rand() % 13; 
 
-  {
-
-#ifdef USE_GPU 
-  gpu_selector dev_sel;
+#ifdef USE_GPU
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-  cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-  queue q(dev_sel);
 
-  buffer<float, 1> d_input (input, numElem);
-  buffer<float, 1> d_output (output_gpu, numElem);
+  float *d_input = sycl::malloc_device<float>(numElem, q);
+  q.memcpy(d_input, input, sizeof(float) * numElem);
 
-  range<1> global_work_size ((numSlice+BLOCK_SIZE-1)/BLOCK_SIZE*BLOCK_SIZE);
-  range<1> local_work_size (BLOCK_SIZE);
+  float *d_output = sycl::malloc_device<float>(numElem, q);
+
+  sycl::range<1> gws ((numSlice+BLOCK_SIZE-1)/BLOCK_SIZE*BLOCK_SIZE);
+  sycl::range<1> lws (BLOCK_SIZE);
 
   q.wait();
   auto start = std::chrono::steady_clock::now();
 
   for (int n = 0; n < repeat; n++) {
-    q.submit([&](handler &h) {
-      auto src = d_input.get_access<sycl_read>(h);
-      auto dest = d_output.get_access<sycl_discard_write>(h);
-      h.parallel_for<class sm>(nd_range<1>(global_work_size, local_work_size), [=](nd_item<1> item) {
+    q.submit([&](sycl::handler &h) {
+      h.parallel_for<class sm>(
+        sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item) {
         int i = item.get_global_id(0); 
         if (i >= numSlice) return;
-        float max_ = src[i * sliceSize];
+        float max_ = d_input[i * sliceSize];
         for (int j = 0; j < sliceSize; j++) {
-          max_ = sycl::max(max_, src[i * sliceSize + j]);
+          max_ = sycl::max(max_, d_input[i * sliceSize + j]);
         }
         float sum = 0;
         for (int j = 0; j < sliceSize; j++) {
-          sum += sycl::exp(src[i * sliceSize + j] - max_);
+          sum += sycl::exp(d_input[i * sliceSize + j] - max_);
         }
         for (int j = 0; j < sliceSize; j++) {
-          dest[i * sliceSize + j] = sycl::exp(src[i * sliceSize + j] - max_) / sum;
+          d_output[i * sliceSize + j] = sycl::exp(d_input[i * sliceSize + j] - max_) / sum;
         }
       });
     });
@@ -89,7 +87,7 @@ int main(int argc, char* argv[]) {
   auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
   printf("Average kernel execution time: %f (s)\n", (time * 1e-9f) / repeat);
 
-  }
+  q.memcpy(output_gpu, d_output, sizeof(float) * numElem).wait();
 
   // verification
   bool ok = true;
@@ -106,5 +104,7 @@ int main(int argc, char* argv[]) {
   free(input);
   free(output_cpu);
   free(output_gpu);
+  sycl::free(d_input, q);
+  sycl::free(d_output, q);
   return 0;
 }
