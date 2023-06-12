67e4f7fa5495846a7ab882290a43e0140e918349
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-06-12 12:22:54 -0400

    [split-sycl] fix compiler warnings for newer compilers

--- split-sycl/main.cpp
+++ split-sycl/main.cpp
@@ -12,8 +12,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <chrono>
-#include "common.h"
+#include <sycl/sycl.hpp>
 #include "verify.cpp"
+
+using uint4 = sycl::uint4;
 
 //----------------------------------------------------------------------------
 // Scans each warp in parallel ("warp-scan"), one element per thread.
@@ -21,15 +23,15 @@
 //----------------------------------------------------------------------------
 #define WARP_SIZE 32
 
-uint scanwarp(nd_item<1> &item, uint val, volatile uint* sData, const int maxlevel)
+uint scanwarp(sycl::nd_item<1> &item, uint val, volatile uint* sData, const int maxlevel)
 {
-  // The following is the same as 2 * RadixSort::WARP_SIZE * warpId + threadInWarp = 
+  // The following is the same as 2 * RadixSort::WARP_SIZE * warpId + threadInWarp =
   // 64*(threadIdx.x >> 5) + (threadIdx.x & (RadixSort::WARP_SIZE - 1))
   int localId = item.get_local_id(0);
   int idx = 2 * localId - (localId & (WARP_SIZE - 1));
   sData[idx] = 0;
   idx += WARP_SIZE;
-  sData[idx] = val;     
+  sData[idx] = val;
 
   if (0 <= maxlevel) { sData[idx] += sData[idx - 1]; }
   if (1 <= maxlevel) { sData[idx] += sData[idx - 2]; }
@@ -41,15 +43,15 @@ uint scanwarp(nd_item<1> &item, uint val, volatile uint* sData, const int maxlev
 }
 
 //----------------------------------------------------------------------------
-// scan4 scans 4*RadixSort::CTA_SIZE numElements in a block (4 per thread), using 
+// scan4 scans 4*RadixSort::CTA_SIZE numElements in a block (4 per thread), using
 // a warp-scan algorithm
 //----------------------------------------------------------------------------
-sycl::uint4 scan4(nd_item<1> item, const sycl::uint4 idata, uint* ptr)
-{    
+uint4 scan4(sycl::nd_item<1> item, const uint4 idata, uint* ptr)
+{
 
   uint idx = item.get_local_id(0);
 
-  sycl::uint4 val4 = idata;
+  uint4 val4 = idata;
   uint sum[3];
   sum[0] = val4.x();
   sum[1] = val4.y() + sum[0];
@@ -58,18 +60,18 @@ sycl::uint4 scan4(nd_item<1> item, const sycl::uint4 idata, uint* ptr)
   uint val = val4.w() + sum[2];
 
   val = scanwarp(item, val, ptr, 4);
-  item.barrier(access::fence_space::local_space);
+  item.barrier(sycl::access::fence_space::local_space);
 
   if ((idx & (WARP_SIZE - 1)) == WARP_SIZE - 1)
   {
     ptr[idx >> 5] = val + val4.w() + sum[2];
   }
-  item.barrier(access::fence_space::local_space);
+  item.barrier(sycl::access::fence_space::local_space);
 
   if (idx < WARP_SIZE)
     ptr[idx] = scanwarp(item, ptr[idx], ptr, 2);
 
-  item.barrier(access::fence_space::local_space);
+  item.barrier(sycl::access::fence_space::local_space);
 
   val += ptr[idx >> 5];
 
@@ -81,37 +83,37 @@ sycl::uint4 scan4(nd_item<1> item, const sycl::uint4 idata, uint* ptr)
   return val4;
 }
 
-sycl::uint4 rank4(nd_item<1> &item, const sycl::uint4 preds, uint* sMem, local_ptr<uint> numtrue)
+uint4 rank4(sycl::nd_item<1> &item, const uint4 preds, uint* sMem, uint &numtrue)
 {
   int localId = item.get_local_id(0);
   int localSize = item.get_local_range(0);
 
-  sycl::uint4 address = scan4(item, preds, sMem);
+  uint4 address = scan4(item, preds, sMem);
 
-  if (localId == localSize - 1) 
+  if (localId == localSize - 1)
   {
-    numtrue[0] = address.w() + preds.w();
+    numtrue = address.w() + preds.w();
   }
-  item.barrier(access::fence_space::local_space);
+  item.barrier(sycl::access::fence_space::local_space);
 
-  sycl::uint4 rank;
+  uint4 rank;
   int idx = localId*4;
-  rank.x() = (preds.x()) ? address.x() : numtrue[0] + idx - address.x();
-  rank.y() = (preds.y()) ? address.y() : numtrue[0] + idx + 1 - address.y();
-  rank.z() = (preds.z()) ? address.z() : numtrue[0] + idx + 2 - address.z();
-  rank.w() = (preds.w()) ? address.w() : numtrue[0] + idx + 3 - address.w();
+  rank.x() = (preds.x()) ? address.x() : numtrue + idx - address.x();
+  rank.y() = (preds.y()) ? address.y() : numtrue + idx + 1 - address.y();
+  rank.z() = (preds.z()) ? address.z() : numtrue + idx + 2 - address.z();
+  rank.w() = (preds.w()) ? address.w() : numtrue + idx + 3 - address.w();
 
   return rank;
 }
 
 void radixSortBlocksKeysK(
-    nd_item<1> &item,
-    uint*__restrict keysIn, 
+    sycl::nd_item<1> &item,
+    uint*__restrict keysIn,
     uint*__restrict keysOut,
     const uint nbits,
     const uint startbit,
     uint*__restrict sMem,
-    uint*__restrict numtrue)
+    uint &numtrue)
 {
   int globalId = item.get_global_id(0);
   int localId = item.get_local_id(0);
@@ -119,17 +121,17 @@ void radixSortBlocksKeysK(
 
   uint4 key = reinterpret_cast<uint4*>(keysIn)[globalId];
 
-  item.barrier(access::fence_space::local_space);
+  item.barrier(sycl::access::fence_space::local_space);
 
   for(uint shift = startbit; shift < (startbit + nbits); ++shift)
   {
-    sycl::uint4 lsb;
+    uint4 lsb;
     lsb.x() = !((key.x() >> shift) & 0x1);
     lsb.y() = !((key.y() >> shift) & 0x1);
     lsb.z() = !((key.z() >> shift) & 0x1);
     lsb.w() = !((key.w() >> shift) & 0x1);
 
-    sycl::uint4 r;
+    uint4 r;
 
     r = rank4(item, lsb, sMem, numtrue);
 
@@ -138,7 +140,7 @@ void radixSortBlocksKeysK(
     sMem[(r.y() & 3) * localSize + (r.y() >> 2)] = key.y();
     sMem[(r.z() & 3) * localSize + (r.z() >> 2)] = key.z();
     sMem[(r.w() & 3) * localSize + (r.w() >> 2)] = key.w();
-    item.barrier(access::fence_space::local_space);
+    item.barrier(sycl::access::fence_space::local_space);
 
     // The above allows us to read without 4-way bank conflicts:
     key.x() = sMem[localId];
@@ -146,10 +148,10 @@ void radixSortBlocksKeysK(
     key.z() = sMem[localId + 2 * localSize];
     key.w() = sMem[localId + 3 * localSize];
 
-    item.barrier(access::fence_space::local_space);
+    item.barrier(sycl::access::fence_space::local_space);
   }
 
-  reinterpret_cast<uint4*>(keysOut)[globalId] = key;  
+  reinterpret_cast<uint4*>(keysOut)[globalId] = key;
 }
 
 int main(int argc, char** argv) {
@@ -161,40 +163,40 @@ int main(int argc, char** argv) {
   const int repeat = atoi(argv[2]);
 
   srand(512);
-  unsigned int *keys = (unsigned int*) malloc (N * sizeof(unsigned int));
-  unsigned int *out = (unsigned int*) malloc (N * sizeof(unsigned int));
+  uint *keys = (uint*) malloc (N * sizeof(uint));
+  uint *out = (uint*) malloc (N * sizeof(uint));
   for (int i = 0; i < N; i++)  keys[i] = rand() % 16;
 
-  const unsigned int startbit = 0;
-  const unsigned int nbits = 4;
+  const uint startbit = 0;
+  const uint nbits = 4;
   const unsigned threads = 128;
   const unsigned teams = N/4/threads;
 
 #ifdef USE_GPU
-  gpu_selector dev_sel;
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-  cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-  queue q(dev_sel, property::queue::in_order());
 
-  uint *d_keys = malloc_device<uint>(N, q);
+  uint *d_keys = sycl::malloc_device<uint>(N, q);
   q.memcpy(d_keys, keys, sizeof(uint) * N);
 
-  uint *d_tempKeys = malloc_device<uint>(N, q);
+  uint *d_tempKeys = sycl::malloc_device<uint>(N, q);
 
-  range<1> gws (teams * threads);
-  range<1> lws (threads);
+  sycl::range<1> gws (teams * threads);
+  sycl::range<1> lws (threads);
 
   q.wait();
   auto start = std::chrono::steady_clock::now();
 
   for (int i = 0; i < repeat; i++)
-    q.submit([&] (handler &cgh) {
-      accessor<unsigned int, 1, sycl_read_write, access::target::local> sMem(4*threads, cgh);
-      accessor<unsigned int, 1, sycl_read_write, access::target::local> numtrue(1, cgh);
-      cgh.parallel_for<class radixSort_blocksKeys>(nd_range<1>(gws, lws), [=] (nd_item<1> item) {
+    q.submit([&] (sycl::handler &cgh) {
+      sycl::local_accessor<uint, 1> sMem(sycl::range<1>(4*threads), cgh);
+      sycl::local_accessor<uint, 0> numtrue(cgh);
+      cgh.parallel_for<class radixSort_blocksKeys>(
+        sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
         radixSortBlocksKeysK(item, d_keys, d_tempKeys,
-                             nbits, startbit, sMem.get_pointer(), numtrue.get_pointer());
+                             nbits, startbit, sMem.get_pointer(), numtrue);
       });
     });
 
@@ -204,13 +206,13 @@ int main(int argc, char** argv) {
   printf("Average kernel execution time: %f (us)\n", (time * 1e-3f) / repeat);
 
   q.memcpy(out, d_tempKeys, sizeof(uint) * N).wait();
-  free(d_tempKeys, q);
-  free(d_keys, q);
- 
+  sycl::free(d_tempKeys, q);
+  sycl::free(d_keys, q);
+
   bool check = verify(out, keys, threads, N);
   if (check)
     printf("PASS\n");
-  else 
+  else
     printf("FAIL\n");
 
   free(keys);
