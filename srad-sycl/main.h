93f85c7ab4ac847affe0e9464d2d0b05d7841079
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-06-12 16:18:15 -0400

    [srad-sycl] switch to a pointer-based program; fix compiler warnings for newer compilers

--- srad-sycl/main.h
+++ srad-sycl/main.h
@@ -1,11 +1,4 @@
-#include <CL/sycl.hpp>
-
-using namespace cl::sycl;
-constexpr access::mode sycl_read       = access::mode::read;
-constexpr access::mode sycl_write      = access::mode::write;
-constexpr access::mode sycl_read_write = access::mode::read_write;
-constexpr access::mode sycl_discard_read_write = access::mode::discard_read_write;
-constexpr access::mode sycl_discard_write = access::mode::discard_write;
+#include <sycl/sycl.hpp>
 
 #define FP float
 
