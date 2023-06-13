8b6c601ea0aa28326160007334c34bb90ecf2afa
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-06-13 14:45:45 -0400

    [su3-sycl] switch to a pointer-based program and fix compiler warnings for newer compilers

--- su3-sycl/mat_nn_sycl.hpp
+++ su3-sycl/mat_nn_sycl.hpp
@@ -1,18 +1,20 @@
-#include "common.h"
+#include <sycl/sycl.hpp>
 #define THREADS_PER_SITE 36
 
-// Sycl requires that kernels be named
 class k_mat_nn;
 
-double su3_mat_nn(const std::vector<site> &a, const std::vector<su3_matrix> &b, std::vector<site> &c, 
+double su3_mat_nn(const std::vector<site> &a, const std::vector<su3_matrix> &b, std::vector<site> &c,
                   const size_t total_sites, const size_t iterations, size_t wgsize, const int target)
-{ 
+{
 #ifdef USE_GPU
-  gpu_selector dev_sel;
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-  cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-  queue q(dev_sel, property::queue::in_order());
+
+  int size_a = sizeof(site) * total_sites;
+  int size_b = sizeof(su3_matrix) * 4;
+  int size_c = sizeof(site) * total_sites;
 
   // check to make sure the workgroup size is sufficient for the algorithm
   if (wgsize == 0)
@@ -26,30 +28,27 @@ double su3_mat_nn(const std::vector<site> &a, const std::vector<su3_matrix> &b, 
   }
   std::cout << std::flush;
 
-  // wrap arrays in SYCL buffers, suppling global memory pointer implicitly copies the data to the device when needed
-  buffer<site, 1>       a_buf {a.data(), range<1> {total_sites}};
-  buffer<su3_matrix, 1> b_buf {b.data(), range<1> {4}};
-  buffer<site, 1>       c_buf {range<1> {total_sites}};
-  // The copy of c from device -> host will occur when the destructor is called (at the end of the scope)
-  c_buf.set_final_data(c.data());
+  site *d_a = sycl::malloc_device<site>(total_sites, q);
+  q.memcpy(d_a, a.data(), size_a);
+
+  su3_matrix *d_b = sycl::malloc_device<su3_matrix>(4, q);
+  q.memcpy(d_b, b.data(), size_b);
+
+  site *d_c = sycl::malloc_device<site>(total_sites, q);
 
   // benchmark loop
   auto tstart = Clock::now();
   for (int iters=0; iters<iterations+warmups; ++iters) {
     if (iters == warmups) {
-      q.wait(); 
+      q.wait();
       tstart = Clock::now();
     }
 
     // create a command_group to issue commands
-    q.submit([&](handler& cgh) {
-      // request access to the device buffers
-      auto a = a_buf.get_access<sycl_read>(cgh);
-      auto b = b_buf.get_access<sycl_read>(cgh);
-      auto c = c_buf.get_access<sycl_write>(cgh);
-
+    q.submit([&](sycl::handler& cgh) {
       // Lambda function defines the kernel scope
-      cgh.parallel_for<class k_mat_nn>(nd_range<1> {total_wi, wgsize}, [=](nd_item<1> item) {
+      cgh.parallel_for<class k_mat_nn>(
+        sycl::nd_range<1> {total_wi, wgsize}, [=](sycl::nd_item<1> item) {
         size_t id = item.get_global_id(0);
         size_t i = id/36;
         if (i < total_sites) {
@@ -59,12 +58,12 @@ double su3_mat_nn(const std::vector<site> &a, const std::vector<su3_matrix> &b, 
           Complx cc = {0.0, 0.0};
           for (int m=0;m<3;m++) {
             #ifdef MILC_COMPLEX
-            CMULSUM(a[i].link[j].e[k][m], b[j].e[m][l], cc);
+            CMULSUM(d_a[i].link[j].e[k][m], d_b[j].e[m][l], cc);
             #else
-            cc += a[i].link[j].e[k][m] * b[j].e[m][l];
+            cc += d_a[i].link[j].e[k][m] * d_b[j].e[m][l];
             #endif
           }
-          c[i].link[j].e[k][l] = cc;
+          d_c[i].link[j].e[k][l] = cc;
         }
       }); // end of the kernel lambda function
     });   // end of command group
@@ -73,6 +72,13 @@ double su3_mat_nn(const std::vector<site> &a, const std::vector<su3_matrix> &b, 
 
   double ttotal = std::chrono::duration_cast<std::chrono::microseconds>(Clock::now()-tstart).count();
 
+  // copy data back from device
+  q.memcpy(c.data(), d_c, size_c).wait();
+
+  // Deallocate
+  sycl::free(d_a, q);
+  sycl::free(d_b, q);
+  sycl::free(d_c, q);
+
   return (ttotal /= 1.0e6);
 } // end of SYCL block
-
