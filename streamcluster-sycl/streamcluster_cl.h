0a64f49168d0b9669ee17f443bdc32cd208cdb6a
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-06-13 14:24:53 -0400

    [streamcluster-sycl] switch to a pointer-based program and fix compiler warnings for newer compilers

--- streamcluster-sycl/streamcluster_cl.h
+++ streamcluster-sycl/streamcluster_cl.h
@@ -30,15 +30,16 @@ float *gl_lower;
 Point_Struct *p_h;
 
 
-std::optional<buffer<float, 1>> work_mem_d;
-std::optional<buffer<int,  1>> center_table_d;
-std::optional<buffer<char, 1>> switch_membership_d;
-std::optional<buffer<Point_Struct,1>> p_d;
-std::optional<buffer<float,1>> coord_d;
+// device memory
+float *work_mem_d;
+float *coord_d;
+int   *center_table_d;
+char  *switch_membership_d;
+Point_Struct *p_d;
 
 static int c;      // counters
 
-float pgain( long x, Points *points, float z, long int *numcenters, 
+float pgain( long x, Points *points, float z, long int *numcenters,
              int kmax, bool *is_center, int *center_table, char *switch_membership,
              double *serial, double *cpu_gpu_memcpy, double *memcpy_back,
              double *gpu_malloc, double *kernel_time) {
@@ -80,28 +81,25 @@ float pgain( long x, Points *points, float z, long int *numcenters,
         for(int j=0; j<num; j++)
           coord_h[ (num*i)+j ] = points->p[j].coord[i];
       }
-#ifdef PROFILE_TMP    
+#ifdef PROFILE_TMP
       double t4 = gettime();
       *serial += t4 - t3;
 #endif
 
-      work_mem_d = buffer<float,1>((kmax+1) * num);
-      center_table_d = buffer<int,  1> (num);
-      switch_membership_d = buffer<char, 1>(num);
-      p_d = buffer<Point_Struct,1>(num);
-      coord_d = buffer<float,1> (dim * num);
+      work_mem_d = sycl::malloc_device<float>((kmax+1) * num, q);
+      center_table_d = sycl::malloc_device<int> (num, q);
+      switch_membership_d = sycl::malloc_device<char>(num, q);
+      p_d = sycl::malloc_device<Point_Struct>(num, q);
+      coord_d = sycl::malloc_device<float> (dim * num, q);
 
 #ifdef PROFILE_TMP
       double t5 = gettime();
       *gpu_malloc += t5 - t4;
 #endif
 
-      // copy coordinate to device memory  
-      q.submit([&] (handler& cgh) {
-        accessor<float,1,sycl_write,access::target::global_buffer>  \
-        coord_d_acc (coord_d.value(), cgh, range<1>(num*dim), id<1>(0)); 
-        cgh.copy(coord_h, coord_d_acc);
-      });
+      // copy coordinate to device memory
+      q.memcpy(coord_d, coord_h, sizeof(float)*num*dim);
+
 #ifdef PROFILE_TMP
       q.wait();
       double t6 = gettime();
@@ -116,7 +114,7 @@ float pgain( long x, Points *points, float z, long int *numcenters,
     for(int i=0; i<num; i++){
       p_h[i].weight = ((points->p)[i]).weight;
       p_h[i].assign = ((points->p)[i]).assign;
-      p_h[i].cost = ((points->p)[i]).cost;  
+      p_h[i].cost = ((points->p)[i]).cost;
     }
 
 #ifdef PROFILE_TMP
@@ -127,17 +125,9 @@ float pgain( long x, Points *points, float z, long int *numcenters,
     double t7 = gettime();
 #endif
     /***** memory transfer from host to device *****/
-    /* copy to device memory */
-    q.submit([&] (handler& cgh) {
-      accessor<int,1,sycl_write,access::target::global_buffer>  \
-      center_table_d_acc (center_table_d.value(), cgh, range<1>(num), id<1>(0)); 
-      cgh.copy(center_table, center_table_d_acc);
-    });
-    q.submit([&] (handler& cgh) {
-      accessor<Point_Struct,1,sycl_write,access::target::global_buffer>  \
-      p_d_acc (p_d.value(), cgh, range<1>(num), id<1>(0)); 
-      cgh.copy(p_h, p_d_acc);
-    });
+    q.memcpy(center_table_d, center_table, sizeof(int)*num);
+    q.memcpy(p_d, p_h, sizeof(Point_Struct)*num);
+
 #ifdef PROFILE_TMP
     q.wait();
     double t8 = gettime();
@@ -152,33 +142,26 @@ float pgain( long x, Points *points, float z, long int *numcenters,
     double t9 = gettime();
 #endif
 
-    q.submit([&] (handler& cgh) {
-      accessor<char,1,sycl_write,access::target::global_buffer>  \
-      switch_membership_d_acc (switch_membership_d.value(), cgh, range<1>(num), id<1>(0)); // add workgroup size
-      cgh.fill(switch_membership_d_acc, (char)0);
-    });
-
-    q.submit([&] (handler& cgh) {
-      accessor<float,1,sycl_write,access::target::global_buffer>  \
-      work_mem_d_acc (work_mem_d.value(), cgh, range<1>(num*(K+1)), id<1>(0)); // add workgroup size
-      cgh.fill(work_mem_d_acc, 0.0f);
-    });
+    q.memset(switch_membership_d, 0, num * sizeof(char));
+    q.memset(work_mem_d, 0, num*(K+1)*sizeof(float));
 
     int work_group_size = THREADS_PER_BLOCK;
     int work_items = num;
-    if(work_items%work_group_size != 0)  //process situations that work_items cannot be divided by work_group_size
+
+    //process situations that work_items cannot be divided by work_group_size
+    if(work_items%work_group_size != 0)
       work_items = work_items + (work_group_size-(work_items%work_group_size));
 
-    q.submit([&](handler& cgh) {
-      auto p_d_acc = p_d.value().get_access<sycl_read>(cgh);
-      auto coord_d_acc = coord_d.value().get_access<sycl_read>(cgh);
-      auto work_mem_d_acc = work_mem_d.value().get_access<sycl_write>(cgh);
-      auto center_table_d_acc = center_table_d.value().get_access<sycl_read>(cgh);
-      auto switch_membership_d_acc = switch_membership_d.value().get_access<sycl_write>(cgh);
-      accessor <float, 1, sycl_read_write, access::target::local> coord_s_acc (smSize, cgh);
-
+    q.submit([&](sycl::handler& cgh) {
+      auto work_mem_d_acc = work_mem_d;
+      auto coord_d_acc = coord_d;
+      auto center_table_d_acc = center_table_d;
+      auto switch_membership_d_acc = switch_membership_d;
+      auto p_d_acc = p_d;
+      sycl::local_accessor <float, 1> coord_s_acc (sycl::range<1>(smSize), cgh);
       cgh.parallel_for<class kernel_pgain>(
-          nd_range<1>(range<1>(work_items), range<1>(work_group_size)), [=] (nd_item<1> item) {
+        sycl::nd_range<1>(sycl::range<1>(work_items),
+                          sycl::range<1>(work_group_size)), [=] (sycl::nd_item<1> item) {
           #include "kernel.sycl"
       });
     });
@@ -190,20 +173,10 @@ float pgain( long x, Points *points, float z, long int *numcenters,
 #endif
 
     /***** copy back to host for CPU side work *****/
-    q.submit([&] (handler& cgh) {
-      accessor<char,1,sycl_read,access::target::global_buffer>  \
-      switch_membership_d_acc (switch_membership_d.value(), cgh, range<1>(num), id<1>(0)); // add workgroup size
-      cgh.copy(switch_membership_d_acc, switch_membership);
-    });
-
-    // reset on the host already
-    q.submit([&] (handler& cgh) {
-      accessor<float,1,sycl_read,access::target::global_buffer>  \
-      work_mem_d_acc (work_mem_d.value(), cgh, range<1>(num*(K+1)), id<1>(0)); // add workgroup size
-      cgh.copy(work_mem_d_acc, work_mem_h);
-    });
-
+    q.memcpy(switch_membership, switch_membership_d, num * sizeof(char));
+    q.memcpy(work_mem_h, work_mem_d, num*(K+1) * sizeof(float));
     q.wait();
+
 #ifdef PROFILE_TMP
     double t11 = gettime();
     *memcpy_back += t11 - t10;
@@ -220,11 +193,11 @@ float pgain( long x, Points *points, float z, long int *numcenters,
         //printf("i=%d  ", i);
         for( int j = 0; j < num; j++ )
           low += work_mem_h[ j*(K+1) + center_table[i] ];
-        //printf("low=%f\n", low);    
+        //printf("low=%f\n", low);
         gl_lower[center_table[i]] = low;
 
         if ( low > 0 ) {
-          numclose++;        
+          numclose++;
           work_mem_h[i*(K+1)+K] -= low;
         }
       }
@@ -262,7 +235,7 @@ float pgain( long x, Points *points, float z, long int *numcenters,
   }
   catch(string msg){
     printf("--cambine:%s\n", msg.c_str());
-    exit(-1);    
+    exit(-1);
   }
   catch(...){
     printf("--cambine: unknow reasons in pgain\n");
