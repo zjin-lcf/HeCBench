296dbe83aa7f8405950b1e8688358e780158a70f
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-09-19 11:22:49 -0400

    [spgemm] add the library-based examples in CUDA and HIP

---
+++ spgemm-cuda/utils.h
@@ -0,0 +1,80 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void init_matrix(float *matrix, int num_rows, int num_cols, int nnz)
+{
+  int n = (int)num_rows * num_cols;
+
+  float *d = (float *) malloc(n * sizeof(float));
+
+  srand(123);
+  for (int i = 0; i < n; i++) d[i] = (float)i;
+  for (int i = n; i > 0; i--) {
+    int a = i-1;
+    int b = rand() % i;
+    if (a != b) {
+      auto t = d[a];
+      d[a] = d[b];
+      d[b] = t;
+    }
+  }
+
+#ifdef DEBUG
+  for (int i = 0; i < num_rows; i++) {
+    for (int j = 0; j < num_cols; j++) {
+      matrix[i*num_cols+j] = (d[i*num_cols+j] >= nnz) ? 0 : (float)(drand48()+1);
+      printf("%f ", matrix[i*num_cols+j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+#endif
+
+  free(d);
+}
+
+void init_csr(int *row_indices, float *values,
+              int *col_indices, float *matrix,
+              int num_rows, int num_cols, int nnz)
+{
+  row_indices[num_rows] = nnz;
+  row_indices[0] = 0;
+  int *non_zero_elements = (int*) malloc (num_rows * sizeof(int));
+
+  int tmp = 0;
+  for (int i = 0; i < num_rows; i++) {
+    int nnz_per_row = 0; // nnz per row
+    for (int j = 0; j < num_cols; j++) {
+      if(matrix[i*num_cols+j] != 0) {
+        values[tmp] = matrix[i*num_rows+j];
+        col_indices[tmp] = j;
+        tmp++;
+        nnz_per_row++;
+      }
+    }
+    non_zero_elements[i] = nnz_per_row;
+  }
+
+  for (int i = 1; i < num_rows; i++) {
+    row_indices[i] = row_indices[i-1] + non_zero_elements[i-1];
+  }
+
+  free(non_zero_elements);
+}
+
+
+void gemm (float *A, float *B, float *C,
+           int A_num_cols, int C_num_rows, int C_num_cols)
+{
+  for(int i = 0; i < C_num_rows; i++) {
+    for (int j = 0; j < C_num_cols; j++) {
+      double s = 0;
+      for (int k = 0; k < A_num_cols; k++) { // B_num_rows
+        s += A[i*A_num_cols+k] * B[k*C_num_cols+j];
+      }
+      C[i*C_num_cols+j] = s;
+    }
+  }
+}
+
+#endif
---
+++ spgemm-hip/main.cu
@@ -0,0 +1,275 @@
+/*
+ * Copyright 1993-2022 NVIDIA Corporation.  All rights reserved.
+ *
+ * NOTICE TO LICENSEE:
+ *
+ * This source code and/or documentation ("Licensed Deliverables") are
+ * subject to NVIDIA intellectual property rights under U.S. and
+ * international Copyright laws.
+ *
+ * These Licensed Deliverables contained herein is PROPRIETARY and
+ * CONFIDENTIAL to NVIDIA and is being provided under the terms and
+ * conditions of a form of NVIDIA software license agreement by and
+ * between NVIDIA and Licensee ("License Agreement") or electronically
+ * accepted by Licensee.  Notwithstanding any terms or conditions to
+ * the contrary in the License Agreement, reproduction or disclosure
+ * of the Licensed Deliverables to any third party without the express
+ * written consent of NVIDIA is prohibited.
+ *
+ * NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+ * LICENSE AGREEMENT, NVIDIA MAKES NO REPRESENTATION ABOUT THE
+ * SUITABILITY OF THESE LICENSED DELIVERABLES FOR ANY PURPOSE.  IT IS
+ * PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY OF ANY KIND.
+ * NVIDIA DISCLAIMS ALL WARRANTIES WITH REGARD TO THESE LICENSED
+ * DELIVERABLES, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+ * NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+ * LICENSE AGREEMENT, IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THESE LICENSED DELIVERABLES.
+ *
+ * U.S. Government End Users.  These Licensed Deliverables are a
+ * "commercial item" as that term is defined at 48 C.F.R. 2.101 (OCT
+ * 1995), consisting of "commercial computer software" and "commercial
+ * computer software documentation" as such terms are used in 48
+ * C.F.R. 12.212 (SEPT 1995) and is provided to the U.S. Government
+ * only as a commercial end item.  Consistent with 48 C.F.R.12.212 and
+ * 48 C.F.R. 227.7202-1 through 227.7202-4 (JUNE 1995), all
+ * U.S. Government End Users acquire the Licensed Deliverables with
+ * only those rights set forth herein.
+ *
+ * Any use of the Licensed Deliverables in individual and commercial
+ * software must include, in the user documentation and internal
+ * comments to the code, the above Disclaimer and U.S. Government End
+ * Users Notice.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <chrono>
+#include <hip/hip_runtime_api.h>
+#include <hipsparse/hipsparse.h>
+#include "utils.h"
+
+#define CHECK_HIP(func)                                                  \
+{                                                                        \
+    hipError_t status = (func);                                          \
+    if (status != hipSuccess) {                                          \
+        printf("HIP API failed at line %d with error: %s (%d)\n",        \
+               __LINE__, hipGetErrorString(status), status);             \
+        return EXIT_FAILURE;                                             \
+    }                                                                    \
+}
+
+#define CHECK_HIPSPARSE_ERROR_CASE__(token_) \
+    case token_:                             \
+        fprintf(stderr, "HIPSPARSE API failed at line %d with error: %s\n",         \
+               __LINE__, #token_); \
+        break
+
+#define CHECK_HIPSPARSE(error)                                                      \
+    {                                                                                     \
+        auto local_error = (error);                                                       \
+        if(local_error != HIPSPARSE_STATUS_SUCCESS)                                       \
+        {                                                                                 \
+            fprintf(stderr, "hipSPARSE error: ");                                         \
+            switch(local_error)                                                           \
+            {                                                                             \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_SUCCESS);                   \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_NOT_INITIALIZED);           \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_ALLOC_FAILED);              \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_INVALID_VALUE);             \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_ARCH_MISMATCH);             \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_MAPPING_ERROR);             \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_EXECUTION_FAILED);          \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_INTERNAL_ERROR);            \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_MATRIX_TYPE_NOT_SUPPORTED); \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_ZERO_PIVOT);                \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_NOT_SUPPORTED);             \
+                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_INSUFFICIENT_RESOURCES);    \
+            }                                                                             \
+            fprintf(stderr, "\n");                                                        \
+            return local_error;                                                           \
+        }                                                                                 \
+    }                                                                                     
+
+int main(int argc, char *argv[])
+{
+  int repeat = 1;
+
+  if (argc != 7) {
+    printf("Single-precision sparse matrix-dense matrix multiplication into dense matrix,\n");
+    printf("where the sparse matrix is represented in CSR (Compressed Sparse Row) storage format\n");
+    printf("Usage %s <M> <K> <N> <A_nnz> <repeat> <verify>\n", argv[0]);
+    printf("SPMM (A, B, C) where (A: M * K, B: K * N, C: M * N)\n");
+    return 1;
+  }
+
+  int m, k, n, a_nnz, verify;
+
+  m = atoi(argv[1]);
+  k = atoi(argv[2]);
+  n = atoi(argv[3]);
+  a_nnz = atoi(argv[4]);
+  repeat = atoi(argv[5]);
+  verify = atoi(argv[6]);
+
+  // Host problem definition
+  const int A_num_rows = m;
+  const int A_num_cols = k;
+  const int A_nnz      = a_nnz;
+  const int B_num_rows = A_num_cols;
+  const int B_num_cols = n;
+  const int lda        = A_num_cols;
+  const int ldb        = B_num_cols;
+  const int ldc        = B_num_cols;
+  const int A_size     = lda * A_num_rows;
+  const int B_size     = ldb * B_num_rows;
+  const int C_size     = ldc * A_num_rows;
+
+  float *hA = (float*) malloc (A_size * sizeof(float));
+  float *hB = (float*) malloc (B_size * sizeof(float));
+
+  const size_t A_value_size_bytes  = A_nnz * sizeof(float);
+  const size_t A_colidx_size_bytes = A_nnz * sizeof(int);
+  const size_t A_rowidx_size_bytes = (A_num_rows + 1) * sizeof(size_t);
+
+  float *hA_values = (float*) malloc (A_value_size_bytes);
+  int *hA_columns = (int*) malloc (A_colidx_size_bytes);
+  int *hA_offsets = (int*) malloc (A_rowidx_size_bytes);
+
+  init_matrix(hA, A_num_rows, A_num_cols, A_nnz);
+  init_csr(hA_offsets, hA_values, hA_columns, hA,
+           A_num_rows, A_num_cols, A_nnz);
+
+  init_matrix(hB, B_num_rows, B_num_cols, B_size);
+
+  float               alpha       = 1.0f;
+  float               beta        = 0.0f;
+  hipsparseOperation_t opA        = HIPSPARSE_OPERATION_NON_TRANSPOSE;
+  hipsparseOperation_t opB        = HIPSPARSE_OPERATION_NON_TRANSPOSE;
+
+  //--------------------------------------------------------------------------
+  // Device memory management: Allocate and copy A, B
+  int   *dA_offsets, *dA_columns;
+  float *dA_values, *dB, *dC;
+  // allocate A
+  CHECK_HIP( hipMalloc((void**) &dA_offsets,
+                         (A_num_rows + 1) * sizeof(int)) )
+  CHECK_HIP( hipMalloc((void**) &dA_columns, A_nnz * sizeof(int))   )
+  CHECK_HIP( hipMalloc((void**) &dA_values,  A_nnz * sizeof(float)) )
+  // allocate B
+  CHECK_HIP( hipMalloc((void**) &dB, B_size * sizeof(float)) )
+  // allocate C
+  CHECK_HIP( hipMalloc((void**) &dC, C_size * sizeof(float)) )
+
+  // copy A
+  CHECK_HIP( hipMemcpy(dA_offsets, hA_offsets, (A_num_rows + 1) * sizeof(int),
+                       hipMemcpyHostToDevice) )
+  CHECK_HIP( hipMemcpy(dA_columns, hA_columns, A_nnz * sizeof(int),
+                       hipMemcpyHostToDevice) )
+  CHECK_HIP( hipMemcpy(dA_values, hA_values,
+                       A_nnz * sizeof(float), hipMemcpyHostToDevice) )
+  // copy B
+  CHECK_HIP( hipMemcpy(dB, hB, B_size * sizeof(float), hipMemcpyHostToDevice) )
+
+  //--------------------------------------------------------------------------
+  // HIPSPARSE APIs
+  hipsparseHandle_t     handle = NULL;
+  hipsparseSpMatDescr_t matA;
+  hipsparseDnMatDescr_t matB, matC;
+  void*  dBuffer    = NULL;
+  size_t bufferSize = 0;
+  CHECK_HIPSPARSE( hipsparseCreate(&handle) )
+  // Create sparse matrix A in CSR format
+  CHECK_HIPSPARSE( hipsparseCreateCsr(&matA, A_num_rows, A_num_cols, A_nnz,
+                                      dA_offsets, dA_columns, dA_values,
+                                      HIPSPARSE_INDEX_32I, HIPSPARSE_INDEX_32I,
+                                      HIPSPARSE_INDEX_BASE_ZERO, HIP_R_32F) )
+  CHECK_HIPSPARSE( hipsparseCreateDnMat(&matB, B_num_rows, B_num_cols, ldb,
+                                        dB, HIP_R_32F, HIPSPARSE_ORDER_ROW) )
+  CHECK_HIPSPARSE( hipsparseCreateDnMat(&matC, A_num_rows, B_num_cols, ldc,
+                                        dC, HIP_R_32F, HIPSPARSE_ORDER_ROW) )
+
+  // allocate an external buffer if needed
+  CHECK_HIPSPARSE( hipsparseSpMM_bufferSize(
+                               handle,
+                               opA,
+                               opB,
+                               &alpha, matA, matB, &beta, matC, HIP_R_32F,
+                               HIPSPARSE_SPMM_ALG_DEFAULT, &bufferSize) )
+  CHECK_HIP( hipMalloc(&dBuffer, bufferSize) )
+
+  hipDeviceSynchronize();
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+
+    // compute the product of A * B
+    CHECK_HIPSPARSE( hipsparseSpMM(handle,
+                                   opA,
+                                   opB,
+                                   &alpha, matA, matB, &beta, matC, HIP_R_32F,
+                                   HIPSPARSE_SPMM_ALG_DEFAULT, dBuffer) )
+
+  }
+  hipDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time of SPGEMM compute: %f (us)\n", (time * 1e-3f) / repeat);
+
+  // destroy matrix/vector descriptors
+  CHECK_HIPSPARSE( hipsparseDestroySpMat(matA) )
+  CHECK_HIPSPARSE( hipsparseDestroyDnMat(matB) )
+  CHECK_HIPSPARSE( hipsparseDestroyDnMat(matC) )
+  CHECK_HIPSPARSE( hipsparseDestroy(handle) )
+
+  //--------------------------------------------------------------------------
+  // device result check
+  
+  if (verify) {
+    printf("Computing the reference SPGEMM results..\n");
+    float *hC = (float*) malloc (C_size * sizeof(float));
+    gemm (hA, hB, hC, A_num_cols, A_num_rows, B_num_cols);
+
+    float *hC_tmp = (float*) malloc (C_size * sizeof(float));
+    CHECK_HIP( hipMemcpy(hC_tmp, dC, C_size * sizeof(float),
+                         hipMemcpyDeviceToHost) )
+    int correct = 1;
+    for (int i = 0; i < C_size; i++) {
+      if (fabsf(hC_tmp[i] - hC[i]) > 1e-2f) {
+        printf("@%d %f != %f\n", i, hC_tmp[i], hC[i]);
+        correct = 0;                       
+        break;
+      }
+    }
+
+    free(hC_tmp);
+    free(hC);
+
+    if (correct)
+        printf("spgemm_example test PASSED\n");
+    else {
+        printf("spgemm_example test FAILED: wrong result\n");
+        return EXIT_FAILURE;
+    }
+  }
+
+  //--------------------------------------------------------------------------
+  // device memory deallocation
+  CHECK_HIP( hipFree(dBuffer) )
+  CHECK_HIP( hipFree(dA_offsets) )
+  CHECK_HIP( hipFree(dA_columns) )
+  CHECK_HIP( hipFree(dA_values) )
+  CHECK_HIP( hipFree(dB) )
+  CHECK_HIP( hipFree(dC) )
+  free(hA);
+  free(hB);
+  free(hA_values);
+  free(hA_columns);
+  free(hA_offsets);
+  return EXIT_SUCCESS;
+}
