7fa99f1a6ba9e44390e16e8756111193b81c1e5b
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-09-27 12:00:34 -0400

    [chi2] address the issue (https://github.com/zjin-lcf/HeCBench/issues/59): remove READMEs that contain dead links to the input data file; instead generate the random data in the programs; update the sources to fix data initialization and accesses issues

--- src/chi2-cuda/chi2.cu
+++ src/chi2-cuda/chi2.cu
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <iostream>
 #include <chrono>
+#include <random>
 #include <cuda.h>
 #include "reference.h"
 
@@ -18,8 +19,6 @@ __global__ void kernel(
   unsigned char y;
   int m, n;
   unsigned int p = 0;
-  int cases[3];
-  int controls[3];
   int tot_cases = 1;
   int tot_controls= 1;
   int total = 1;
@@ -33,12 +32,12 @@ __global__ void kernel(
   int tid = threadIdx.x + blockIdx.x * blockDim.x;
   if (tid >= cols) return;
 
-  cases[0]=1;cases[1]=1;cases[2]=1;
-  controls[0]=1;controls[1]=1;controls[2]=1;
+  int cases[3] = {1,1,1};
+  int controls[3] = {1,1,1};
 
   // read cases: each thread reads a column of snpdata matrix
   for ( m = 0 ; m < cRows ; m++ ) {
-    y = snpdata[m * cols + tid];
+    y = snpdata[(size_t)m * (size_t)cols + tid];
     if ( y == '0') { cases[0]++; }
     else if ( y == '1') { cases[1]++; }
     else if ( y == '2') { cases[2]++; }
@@ -46,48 +45,44 @@ __global__ void kernel(
 
   // read controls: each thread reads a column of snpdata matrix
   for ( n = cRows ; n < cRows + contRows ; n++ ) {
-    y = snpdata[n * cols + tid];
+    y = snpdata[(size_t)n * (size_t)cols + tid];
     if ( y == '0' ) { controls[0]++; }
     else if ( y == '1') { controls[1]++; }
     else if ( y == '2') { controls[2]++; }
   }
 
-  tot_cases = cases[0]+cases[1]+cases[2];
-  tot_controls = controls[0]+controls[1]+controls[2];
+  for( p = 0 ; p < 3; p++ ) {
+    tot_cases += cases[p];
+    tot_controls += controls[p];
+  }
   total = tot_cases + tot_controls;
 
-  for( p = 0 ; p < 3; p++) {
+  for( p = 0 ; p < 3; p++ ) {
     exp[p] = (float)cases[p] + controls[p]; 
     Cexpected[p] = tot_cases * exp[p] / total;
     Conexpected[p] = tot_controls * exp[p] / total;
     numerator1 = (float)cases[p] - Cexpected[p];
     numerator2 = (float)controls[p] - Conexpected[p];
-    chisquare += numerator1 * numerator1 / Cexpected[p] +  numerator2 * numerator2 / Conexpected[p];
+    chisquare += numerator1 * numerator1 / Cexpected[p] +
+                 numerator2 * numerator2 / Conexpected[p];
   }
   results[tid] = chisquare;
 }
 
 int main(int argc, char* argv[]) {
 
-  if (argc != 8) {
-    printf("Usage: %s <filename> <rows> <cols> <cases> <controls> "
+  if (argc != 7) {
+    printf("Usage: %s <rows> <cols> <cases> <controls> "
            "<threads> <repeat>\n", argv[0]);
     return 1;
   }
 
-  // check if the data file is readable
-  FILE *fp = fopen(argv[1], "r");
-  if (fp == NULL) {
-    printf("Cannot open the file %s\n", argv[1]);
-    return 1;
-  }
-
-  unsigned int rows = atoi(argv[2]);
-  unsigned int cols = atoi(argv[3]);
-  int ncases = atoi(argv[4]);
-  int ncontrols = atoi(argv[5]);
-  int nthreads = atoi(argv[6]);
-  int repeat = atoi(argv[7]);
+  unsigned int rows = atoi(argv[1]);
+  unsigned int cols = atoi(argv[2]);
+  int ncases = atoi(argv[3]);
+  int ncontrols = atoi(argv[4]);
+  int nthreads = atoi(argv[5]);
+  int repeat = atoi(argv[6]);
 
   printf("Individuals=%d SNPs=%d cases=%d controls=%d nthreads=%d\n",
          rows,cols,ncases,ncontrols,nthreads);
@@ -110,20 +105,11 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  // transfer the SNP Data from the file to CPU Memory
-  unsigned long i=0;
-  char *line = NULL; size_t len = 0;
-  char *token, *saveptr;
-  while (getline(&line, &len, fp) != -1) {
-    token = strtok_r(line, " ", &saveptr);
-    while (token != NULL) {
-      dataT[i] = *token;
-      i++;
-      token = strtok_r(NULL, " ", &saveptr);
-    }
+  std::mt19937 gen(19937); // mersenne_twister_engin
+  std::uniform_int_distribution<> distrib(0, 2);
+  for (size_t i = 0; i < snpdata_size; i++) {
+    dataT[i] = distrib(gen) + '0';
   }
-  fclose(fp);
-  fflush(stdout);
 
   // allocate SNP device data
   unsigned char *d_data;
--- src/chi2-omp/chi2.cpp
+++ src/chi2-omp/chi2.cpp
@@ -4,30 +4,24 @@
 #include <math.h>
 #include <iostream>
 #include <chrono>
+#include <random>
 #include <omp.h>
 #include "reference.h"
 
 int main(int argc, char* argv[]) {
 
-  if (argc != 8) {
-    printf("Usage: %s <filename> <rows> <cols> <cases> <controls> "
+  if (argc != 7) {
+    printf("Usage: %s <rows> <cols> <cases> <controls> "
            "<threads> <repeat>\n", argv[0]);
     return 1;
   }
 
-  // check if the data file is readable
-  FILE *fp = fopen(argv[1], "r");
-  if (fp == NULL) {
-    printf("Cannot open the file %s\n", argv[1]);
-    return 1;
-  }
-
-  unsigned int rows = atoi(argv[2]);
-  unsigned int cols = atoi(argv[3]);
-  int ncases = atoi(argv[4]);
-  int ncontrols = atoi(argv[5]);
-  int nthreads = atoi(argv[6]);
-  int repeat = atoi(argv[7]);
+  unsigned int rows = atoi(argv[1]);
+  unsigned int cols = atoi(argv[2]);
+  int ncases = atoi(argv[3]);
+  int ncontrols = atoi(argv[4]);
+  int nthreads = atoi(argv[5]);
+  int repeat = atoi(argv[6]);
 
   printf("Individuals=%d SNPs=%d cases=%d controls=%d nthreads=%d\n",
          rows,cols,ncases,ncontrols,nthreads);
@@ -47,6 +41,12 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
+  std::mt19937 gen(19937); // mersenne_twister_engin
+  std::uniform_int_distribution<> distrib(0, 2);
+  for (size_t i = 0; i < size; i++) {
+    dataT[i] = distrib(gen) + '0';
+  }
+
   #pragma omp target data map(to: dataT[0:size]) map(from: h_results[0:cols])
   {
     auto start = std::chrono::high_resolution_clock::now();
@@ -56,9 +56,9 @@ int main(int argc, char* argv[]) {
       for (int i = 0; i < cols; i++) {
         unsigned char y;
         int m, n;
-        unsigned int p = 0 ;
-        int cases[3];
-        int controls[3];
+        unsigned int p = 0;
+        int cases[3] = {1,1,1};
+        int controls[3] = {1,1,1};
         int tot_cases = 1;
         int tot_controls= 1;
         int total = 1;
@@ -69,12 +69,9 @@ int main(int argc, char* argv[]) {
         float numerator1;
         float numerator2;
 
-        cases[0]=1;cases[1]=1;cases[2]=1;
-        controls[0]=1;controls[1]=1;controls[2]=1;
-
         // read cases: each thread reads a column of snpdata matrix
         for ( m = 0 ; m < ncases ; m++ ) {
-          y = dataT[m * cols + i];
+          y = dataT[(size_t)m * (size_t)cols + i];
           if ( y == '0') { cases[0]++; }
           else if ( y == '1') { cases[1]++; }
           else if ( y == '2') { cases[2]++; }
@@ -82,17 +79,19 @@ int main(int argc, char* argv[]) {
 
         // read controls: each thread reads a column of snpdata matrix
         for ( n = ncases ; n < ncases + ncontrols ; n++ ) {
-          y = dataT[n * cols + i];
+          y = dataT[(size_t)n * (size_t)cols + i];
           if ( y == '0' ) { controls[0]++; }
           else if ( y == '1') { controls[1]++; }
           else if ( y == '2') { controls[2]++; }
         }
 
-        tot_cases = cases[0]+cases[1]+cases[2];
-        tot_controls = controls[0]+controls[1]+controls[2];
+        for( p = 0 ; p < 3; p++ ) {
+          tot_cases += cases[p];
+          tot_controls += controls[p];
+        }
         total = tot_cases + tot_controls;
 
-        for( p = 0 ; p < 3; p++) {
+        for( p = 0 ; p < 3; p++ ) {
           exp[p] = (float)cases[p] + controls[p]; 
           Cexpected[p] = tot_cases * exp[p] / total;
           Conexpected[p] = tot_controls * exp[p] / total;
