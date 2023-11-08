1e2f2d32f65cc6c5bf02dd1a2057406be4a5db74
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-11-08 16:41:33 -0500

    [rodrigues] the axis of rotation is a unit vector

--- src/rodrigues-cuda/main.cu
+++ src/rodrigues-cuda/main.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <chrono>
 #include <cuda.h>
 
@@ -74,8 +75,12 @@ int main(int argc, char* argv[])
   const int n = atoi(argv[1]);
   const int repeat = atoi(argv[2]);
     
-  float3 w = make_float3(-0.33f, -0.66, 0.66);
-  float angle = 0.5f;
+  // axis of rotation
+  const float wx = -0.3, wy = -0.6, wz = 0.15;
+  const float norm = 1.f / sqrtf(wx*wx + wy*wy + wz*wz);
+  const float3 w = make_float3(wx*norm, wy*norm, wz*norm);
+
+  const float angle = 0.5f;
 
   float3 *h = (float3*) malloc (sizeof(float3) * n);
   float4 *h2 = (float4*) malloc (sizeof(float4) * n);
