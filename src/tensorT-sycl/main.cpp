d5fb15a3a48f3c21cfc362459ae3d6b43a84ef11
Jin Z <5zj@equinox.ftpn.ornl.gov>
2023-11-22 09:17:57 -0500

    [tensorT] add the usage message

--- src/tensorT-sycl/main.cpp
+++ src/tensorT-sycl/main.cpp
@@ -35,9 +35,11 @@ void verify(double *input, double *output) {
 }
 
 int main(int argv, char **argc) {
-  if (argv > 1) {
-    repeat = atoi(argc[1]);
+  if (argc != 2) {
+    printf("Usage: %s <repeat>\n", argv[0]);
+    return 1;
   }
+  repeat = atoi(argv[1]);
 
   double *input = new double[data_size]();
   double *output = new double[data_size]();
