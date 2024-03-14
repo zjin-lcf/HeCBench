835c0c4c1d6dfcea06abb76c60fec14295f6f9f3
Jin Z <5zj@zenith.ftpn.ornl.gov>
2024-03-14 15:57:52 -0400

    [blockAccess] add the block access examples in CUDA, HIP, and SYCL

---
+++ src/blockAccess-sycl/main.cpp
@@ -0,0 +1,158 @@
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <random>
+#include <sycl/sycl.hpp>
+#include "utils.h"
+#include "block_load.h"
+#include "block_store.h"
+
+#define NUM 4
+
+void reference (const float * __restrict__ A,
+                unsigned char *out, const unsigned int n,
+                const sycl::nd_item<3> &item)
+{
+  unsigned int idx = item.get_global_id(2);
+  if (idx < n) {
+    out[idx] = int(A[idx]);
+  }
+}
+
+template<int TH, int NUM_BLOCK>
+void kernel(const float * __restrict__ A,
+            unsigned char *out, const int n,
+            const sycl::nd_item<3> &item)
+{
+  const int bid = item.get_group(2);
+  const int n_full = (NUM_BLOCK*(n/NUM_BLOCK)) + (n % NUM_BLOCK == 0 ? 0 : NUM_BLOCK);
+  const int base_idx = (bid * NUM_BLOCK);
+
+  float vals[NUM];
+  unsigned char qvals[NUM];
+
+  typedef BlockLoad<float, TH, NUM> LoadFloat;
+  typedef BlockStore<unsigned char, TH, NUM> StoreChar;
+
+  sycl::multi_ptr<typename LoadFloat::TempStorage, sycl::access::address_space::local_space> p1 =
+      sycl::ext::oneapi::group_local_memory_for_overwrite<typename LoadFloat::TempStorage>(item.get_group());
+  typename LoadFloat::TempStorage loadf = *p1;
+
+  sycl::multi_ptr<typename StoreChar::TempStorage, sycl::access::address_space::local_space> p2 =
+      sycl::ext::oneapi::group_local_memory_for_overwrite<typename StoreChar::TempStorage>(item.get_group());
+  typename StoreChar::TempStorage storec = *p2;
+
+  for (unsigned int i = base_idx; i < n_full; i += item.get_group_range(2)*NUM_BLOCK)
+  {
+      unsigned int valid_items = n - i > NUM_BLOCK ? NUM_BLOCK : n - i;
+
+      LoadFloat(loadf, item).Load(&(A[i]), vals, valid_items);
+
+      //item.barrier(sycl::access::fence_space::local_space);
+
+      #pragma unroll
+      for(int j = 0; j < NUM; j++)
+          qvals[j] = (int)vals[j];
+
+      StoreChar(storec, item).Store(&(out[i]), qvals, valid_items);
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 4) {
+    printf("Usage: %s <number of rows> <number of columns> <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int nrows = atoi(argv[1]);
+  const int ncols = atoi(argv[2]);
+  const int repeat = atoi(argv[3]);
+
+  const size_t n = (size_t)nrows * ncols;
+  const size_t A_size = n * sizeof(float);
+  const size_t out_size = n * sizeof(unsigned char);
+
+  float *A = (float*) malloc (A_size);
+  unsigned char *out = (unsigned char*) malloc (out_size);
+
+  std::mt19937 gen{19937};
+ 
+  std::normal_distribution<float> d{128.0, 127.0};
+
+  for (size_t i = 0; i < n; i++) {
+    A[i] = d(gen); 
+  }
+
+#ifdef USE_GPU
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
+#else
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
+#endif
+
+  float *d_A;
+  d_A = (float *)sycl::malloc_device(A_size, q);
+  q.memcpy(d_A, A, A_size).wait();
+
+  unsigned char *d_out;
+  d_out = (unsigned char *)sycl::malloc_device(out_size, q);
+
+  const int block_size = 256;
+
+  sycl::range<3> grid(1, 1, (n + block_size - 1) / block_size);
+  sycl::range<3> block(1, 1, block_size);
+
+  q.wait();
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    q.submit([&](sycl::handler &cgh) {
+      cgh.parallel_for(
+          sycl::nd_range<3>(grid * block, block),
+          [=](sycl::nd_item<3> item) {
+            reference(d_A, d_out, n, item);
+          });
+    });
+  }
+  q.wait();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time of the reference kernel: %f (us)\n", (time * 1e-3f) / repeat);
+
+  start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    q.submit([&](sycl::handler &cgh) {
+      cgh.parallel_for(
+          sycl::nd_range<3>(grid * block, block),
+          [=](sycl::nd_item<3> item) {
+            kernel<block_size, block_size * NUM>(
+                d_A, d_out, n, item);
+          });
+    });
+  }
+  q.wait();
+  end = std::chrono::steady_clock::now();
+  time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time of the blockAccess kernel: %f (us)\n", (time * 1e-3f) / repeat);
+
+  q.memcpy(out, d_out, out_size).wait();
+
+  bool error = false;
+  for (unsigned int i = 0; i < n; i++) {
+    unsigned char t = int(A[i]);
+    if (out[i] != t) {
+      printf("@%u: %u != %u\n", i, out[i], t);
+      error = true;
+      break;
+    }
+  }
+  printf("%s\n", error ? "FAIL" : "PASS");
+  
+  sycl::free(d_A, q);
+  sycl::free(d_out, q);
+  free(A);
+  free(out);
+  return 0;
+}
---
+++ src/blockAccess-sycl/utils.h
@@ -0,0 +1,147 @@
+#pragma once
+
+#define LOG_WARP_THREADS(unused) (5)
+#define WARP_THREADS(unused) (1 << LOG_WARP_THREADS(0))
+#define PTX_WARP_THREADS        WARP_THREADS(0)
+#define PTX_LOG_WARP_THREADS    LOG_WARP_THREADS(0)
+
+struct NullType
+{
+    //using value_type = NullType;
+
+    template <typename T>
+    inline NullType &operator=(const T &) {
+        return *this;
+    }
+
+    inline bool operator==(const NullType &) { return true; }
+
+    inline bool operator!=(const NullType &) { return false; }
+};
+
+
+/// Structure alignment
+template <typename T>
+struct AlignBytes
+{
+    struct Pad
+    {
+        T       val;
+        char    byte;
+    };
+
+    enum
+    {
+        /// The "true CUDA" alignment of T in bytes
+        ALIGN_BYTES = sizeof(Pad) - sizeof(T)
+    };
+
+    /// The "truly aligned" type
+    typedef T Type;
+};
+
+#define __ALIGN_BYTES(t, b)                                                    \
+    template <> struct AlignBytes<t>                                           \
+    { enum { ALIGN_BYTES = b }; typedef __attribute__((aligned(b))) t Type; };
+
+__ALIGN_BYTES(sycl::short4, 8)
+__ALIGN_BYTES(sycl::ushort4, 8)
+__ALIGN_BYTES(sycl::int2, 8)
+__ALIGN_BYTES(sycl::uint2, 8)
+__ALIGN_BYTES(long long, 8)
+__ALIGN_BYTES(unsigned long long, 8)
+__ALIGN_BYTES(sycl::float2, 8)
+__ALIGN_BYTES(double, 8)
+__ALIGN_BYTES(sycl::long2, 16)
+__ALIGN_BYTES(sycl::ulong2, 16)
+__ALIGN_BYTES(sycl::int4, 16)
+__ALIGN_BYTES(sycl::uint4, 16)
+__ALIGN_BYTES(sycl::float4, 16)
+__ALIGN_BYTES(sycl::long4, 16)
+__ALIGN_BYTES(sycl::ulong4, 16)
+//__ALIGN_BYTES(sycl::long2, 16)
+//__ALIGN_BYTES(sycl::ulong2, 16)
+__ALIGN_BYTES(sycl::double2, 16)
+//__ALIGN_BYTES(sycl::long4, 16)
+//__ALIGN_BYTES(sycl::ulong4, 16)
+__ALIGN_BYTES(sycl::double4, 16)
+
+template <typename T> struct AlignBytes<volatile T> : AlignBytes<T> {};
+template <typename T> struct AlignBytes<const T> : AlignBytes<T> {};
+template <typename T> struct AlignBytes<const volatile T> : AlignBytes<T> {};
+
+template <bool Test, class T1, class T2>
+using conditional_t = typename std::conditional<Test, T1, T2>::type;
+
+/// Unit-words of data movement
+template <typename T>
+struct UnitWord
+{
+    enum {
+        ALIGN_BYTES = AlignBytes<T>::ALIGN_BYTES
+    };
+
+    template <typename Unit>
+    struct IsMultiple
+    {
+        enum {
+            UNIT_ALIGN_BYTES    = AlignBytes<Unit>::ALIGN_BYTES,
+            IS_MULTIPLE         = (sizeof(T) % sizeof(Unit) == 0) && (int(ALIGN_BYTES) % int(UNIT_ALIGN_BYTES) == 0)
+        };
+    };
+
+    /// Biggest shuffle word that T is a whole multiple of and is not larger than
+    /// the alignment of T
+    using ShuffleWord = conditional_t<
+      IsMultiple<int>::IS_MULTIPLE,
+      unsigned int,
+      conditional_t<IsMultiple<short>::IS_MULTIPLE,
+                                 unsigned short,
+                                 unsigned char>>;
+
+    /// Biggest volatile word that T is a whole multiple of and is not larger than
+    /// the alignment of T
+    using VolatileWord =
+      conditional_t<IsMultiple<long long>::IS_MULTIPLE,
+                                 unsigned long long,
+                                 ShuffleWord>;
+
+    /// Biggest memory-access word that T is a whole multiple of and is not larger
+    /// than the alignment of T
+    using DeviceWord = conditional_t<IsMultiple<sycl::long2>::IS_MULTIPLE,
+                                     sycl::ulong2, VolatileWord>;
+};
+
+template <typename T>
+struct Uninitialized
+{
+    /// Biggest memory-access word that T is a whole multiple of and is not larger than the alignment of T
+    typedef typename UnitWord<T>::DeviceWord DeviceWord;
+
+    static constexpr std::size_t DATA_SIZE = sizeof(T);
+    static constexpr std::size_t WORD_SIZE = sizeof(DeviceWord);
+    static constexpr std::size_t WORDS = DATA_SIZE / WORD_SIZE;
+
+    /// Backing storage
+    DeviceWord storage[WORDS];
+
+    /// Alias
+    inline T &Alias()
+    {
+        return reinterpret_cast<T&>(*this);
+    }
+};
+
+/**
+ * \brief Returns the row-major linear thread identifier for a multidimensional thread block
+ */
+inline int RowMajorTid(int block_dim_x, int block_dim_y,
+                                int block_dim_z,
+                                const sycl::nd_item<3> &item)
+{
+    return ((block_dim_z == 1)
+                ? 0
+                : (item.get_local_id(0) * block_dim_x * block_dim_y)) +
+           ((block_dim_y == 1) ? 0 : (item.get_local_id(1) * block_dim_x)) +
+           item.get_local_id(2);
+}
