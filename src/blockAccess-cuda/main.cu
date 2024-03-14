a4a450fb69dbfc6c032a35624397925203b3d336
Jin Z <5zj@zenith.ftpn.ornl.gov>
2024-03-14 15:57:52 -0400

    [blockAccess] add the block access examples in CUDA, HIP, and SYCL

---
+++ src/blockAccess-cuda/main.cu
@@ -0,0 +1,132 @@
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <random>
+#include <cuda.h>
+#include "block_load.h"
+#include "block_store.h"
+
+#define NUM 4
+
+__global__ void reference (const float * __restrict__ A,
+                           unsigned char *out, const unsigned int n)
+{
+  unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < n) {
+    out[idx] = int(A[idx]);
+  }
+}
+
+template<int TH, int NUM_BLOCK>
+__global__ void kernel (const float * __restrict__ A,
+                        unsigned char *out, const unsigned int n)
+{
+  const int bid = blockIdx.x;
+  const int n_full = (NUM_BLOCK*(n/NUM_BLOCK)) + (n % NUM_BLOCK == 0 ? 0 : NUM_BLOCK);
+  const int base_idx = (bid * NUM_BLOCK);
+
+  float vals[NUM];
+  unsigned char qvals[NUM];
+
+  typedef BlockLoad<float, TH, NUM> LoadFloat;
+  typedef BlockStore<unsigned char, TH, NUM> StoreChar;
+
+  __shared__ typename LoadFloat::TempStorage loadf;
+  __shared__ typename StoreChar::TempStorage storec;
+
+  for (unsigned int i = base_idx; i < n_full; i += gridDim.x*NUM_BLOCK)
+  {
+      unsigned int valid_items = n - i > NUM_BLOCK ? NUM_BLOCK : n - i;
+
+      LoadFloat(loadf).Load(&(A[i]), vals, valid_items);
+
+      //__syncthreads();
+
+      #pragma unroll
+      for(int j = 0; j < NUM; j++)
+          qvals[j] = int(vals[j]);
+
+      StoreChar(storec).Store(&(out[i]), qvals, valid_items);
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 4) {
+    printf("Usage: %s <number of rows> <number of columns> <repeat>\n", argv[0]);
+    return 1;
+  }
+  const int nrows = atoi(argv[1]);
+  const int ncols = atoi(argv[2]);
+  const int repeat = atoi(argv[3]);
+
+  const unsigned int n = nrows * ncols;
+  const size_t A_size = n * sizeof(float);
+  const size_t out_size = n * sizeof(unsigned char);
+
+  float *A = (float*) malloc (A_size);
+  unsigned char *out = (unsigned char*) malloc (out_size);
+
+  std::mt19937 gen{19937};
+ 
+  std::normal_distribution<float> d{128.0, 127.0};
+
+  for (unsigned int i = 0; i < n; i++) {
+    A[i] = d(gen); 
+  }
+
+  float *d_A;
+  cudaMalloc((void**)&d_A, A_size);
+  cudaMemcpy(d_A, A, A_size, cudaMemcpyHostToDevice);
+
+  unsigned char *d_out;
+  cudaMalloc((void**)&d_out, out_size);
+  
+  const int block_size = 256;
+
+  dim3 grid ((n+block_size-1)/block_size);
+  dim3 block (block_size);
+
+  cudaDeviceSynchronize();
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    reference<<<grid, block>>>(d_A, d_out, n);
+  }
+
+  cudaDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time of the reference kernel: %f (us)\n", (time * 1e-3f) / repeat);
+
+  start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    kernel<block_size, block_size*NUM><<<grid, block>>>(d_A, d_out, n);
+  }
+
+  cudaDeviceSynchronize();
+  end = std::chrono::steady_clock::now();
+  time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time of the blockAccess kernel: %f (us)\n", (time * 1e-3f) / repeat);
+  
+  cudaMemcpy(out, d_out, out_size, cudaMemcpyDeviceToHost);
+
+  bool error = false;
+  for (unsigned int i = 0; i < n; i++) {
+    unsigned char t = int(A[i]);
+    if (out[i] != t) {
+      printf("@%u: %u != %u\n", i, out[i], t);
+      error = true;
+      break;
+    }
+  }
+  printf("%s\n", error ? "FAIL" : "PASS");
+  
+  cudaFree(d_A);
+  cudaFree(d_out);
+  free(A);
+  free(out);
+  return 0;
+}
---
+++ src/blockAccess-sycl/block_load.h
@@ -0,0 +1,275 @@
+/******************************************************************************
+ * Copyright (c) 2011, Duane Merrill.  All rights reserved.
+ * Copyright (c) 2011-2016, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the NVIDIA CORPORATION nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************/
+
+/**
+ * \file
+ * Operations for reading linear tiles of data into the GPU thread block.
+ */
+
+#pragma once
+
+#include <iterator>
+#include <type_traits>
+#include "utils.h"
+
+template <typename InputT, int ITEMS_PER_THREAD, typename InputIteratorT>
+inline void LoadDirectBlocked(int linear_tid, InputIteratorT block_itr,
+                                       InputT (&items)[ITEMS_PER_THREAD])
+{
+    #pragma unroll
+    for (int ITEM = 0; ITEM < ITEMS_PER_THREAD; ITEM++)
+    {
+        items[ITEM] = block_itr[(linear_tid * ITEMS_PER_THREAD) + ITEM];
+    }
+}
+
+template <typename InputT, int ITEMS_PER_THREAD, typename InputIteratorT>
+inline void
+LoadDirectBlocked(int linear_tid, InputIteratorT block_itr,
+                  InputT (&items)[ITEMS_PER_THREAD],
+                  int valid_items) ///< [in] Number of valid items to load
+{
+
+    #pragma unroll
+    for (int ITEM = 0; ITEM < ITEMS_PER_THREAD; ITEM++)
+    {
+        if ((linear_tid * ITEMS_PER_THREAD) + ITEM < valid_items)
+        {
+            items[ITEM] = block_itr[(linear_tid * ITEMS_PER_THREAD) + ITEM];
+        }
+    }
+}
+
+template <typename InputT, typename DefaultT, int ITEMS_PER_THREAD,
+          typename InputIteratorT>
+inline void LoadDirectBlocked(int linear_tid, InputIteratorT block_itr,
+                                       InputT (&items)[ITEMS_PER_THREAD],
+                                       int valid_items, DefaultT oob_default)
+{
+    #pragma unroll
+    for (int ITEM = 0; ITEM < ITEMS_PER_THREAD; ITEM++)
+        items[ITEM] = oob_default;
+
+    LoadDirectBlocked(linear_tid, block_itr, items, valid_items);
+}
+
+
+//-----------------------------------------------------------------------------
+// Generic BlockLoad abstraction
+//-----------------------------------------------------------------------------
+
+enum BlockLoadAlgorithm
+{
+    BLOCK_LOAD_DIRECT
+};
+
+
+template <
+    typename            InputT,
+    int                 BLOCK_DIM_X,
+    int                 ITEMS_PER_THREAD,
+    BlockLoadAlgorithm  ALGORITHM           = BLOCK_LOAD_DIRECT,
+    int                 BLOCK_DIM_Y         = 1,
+    int                 BLOCK_DIM_Z         = 1>
+class BlockLoad
+{
+private:
+
+    /******************************************************************************
+     * Constants and typed definitions
+     ******************************************************************************/
+
+    enum
+    {
+        /// The thread block size in threads
+        BLOCK_THREADS = BLOCK_DIM_X * BLOCK_DIM_Y * BLOCK_DIM_Z,
+    };
+
+
+    /******************************************************************************
+     * Algorithmic variants
+     ******************************************************************************/
+
+    /// Load helper
+    template <BlockLoadAlgorithm _POLICY, int DUMMY>
+    struct LoadInternal;
+
+    /**
+     * BLOCK_LOAD_DIRECT specialization of load helper
+     */
+    template <int DUMMY>
+    struct LoadInternal<BLOCK_LOAD_DIRECT, DUMMY>
+    {
+        /// Shared memory storage layout type
+        typedef NullType TempStorage;
+
+        /// Linear thread-id
+        int linear_tid;
+
+        /// Constructor
+        inline LoadInternal(TempStorage & /*temp_storage*/,
+                                     int linear_tid)
+            : linear_tid(linear_tid)
+        {}
+
+        /// Load a linear segment of items from memory
+        template <typename InputIteratorT>
+        inline void Load(InputIteratorT block_itr,
+                                  InputT (&items)[ITEMS_PER_THREAD])
+        {
+            LoadDirectBlocked(linear_tid, block_itr, items);
+        }
+
+        /// Load a linear segment of items from memory, guarded by range
+        template <typename InputIteratorT>
+        inline void Load(InputIteratorT block_itr,
+                                  InputT (&items)[ITEMS_PER_THREAD],
+                                  int valid_items)
+        {
+            LoadDirectBlocked(linear_tid, block_itr, items, valid_items);
+        }
+
+        /// Load a linear segment of items from memory, guarded by range, with a fall-back assignment of out-of-bound elements
+        template <typename InputIteratorT, typename DefaultT>
+        inline void Load(InputIteratorT block_itr,
+                                  InputT (&items)[ITEMS_PER_THREAD],
+                                  int valid_items, DefaultT oob_default)
+        {
+            LoadDirectBlocked(linear_tid, block_itr, items, valid_items, oob_default);
+        }
+
+    };
+
+
+    /******************************************************************************
+     * Type definitions
+     ******************************************************************************/
+
+    /// Internal load implementation to use
+    typedef LoadInternal<ALGORITHM, 0> InternalLoad;
+
+
+    /// Shared memory storage layout type
+    typedef typename InternalLoad::TempStorage _TempStorage;
+
+
+    /******************************************************************************
+     * Utility methods
+     ******************************************************************************/
+
+    /// Internal storage allocator
+    inline _TempStorage &PrivateStorage(_TempStorage &private_storage)
+    {
+
+        return private_storage;
+    }
+
+
+    /******************************************************************************
+     * Thread fields
+     ******************************************************************************/
+
+    /// Thread reference to shared storage
+    _TempStorage &temp_storage;
+
+    /// Linear thread-id
+    int linear_tid;
+
+public:
+
+    struct TempStorage : Uninitialized<_TempStorage> {};
+
+
+    /******************************************************************//**
+     * \name Collective constructors
+     *********************************************************************/
+
+    /**
+     * \brief Collective constructor using a private static allocation of shared memory as temporary storage.
+     */
+    inline BlockLoad(const sycl::nd_item<3> &item,
+                              _TempStorage &private_storage)
+        : temp_storage(PrivateStorage(private_storage)),
+          linear_tid(
+              RowMajorTid(BLOCK_DIM_X, BLOCK_DIM_Y, BLOCK_DIM_Z, item))
+    {}
+
+
+    /**
+     * \brief Collective constructor using the specified memory allocation as temporary storage.
+     */
+    inline
+    BlockLoad(TempStorage &temp_storage,
+              const sycl::nd_item<3>
+                  &item) ///< [in] Reference to memory allocation having
+                             ///< layout type TempStorage
+        : temp_storage(temp_storage.Alias()),
+          linear_tid(
+              RowMajorTid(BLOCK_DIM_X, BLOCK_DIM_Y, BLOCK_DIM_Z, item))
+    {}
+
+
+
+
+    /******************************************************************//**
+     * \name Data movement
+     *********************************************************************/
+
+    template <typename InputIteratorT>
+    inline void
+    Load(InputIteratorT block_itr, ///< [in] The thread block's base input
+                                   ///< iterator for loading from
+         InputT (&items)[ITEMS_PER_THREAD]) ///< [out] Data to load
+    {
+        InternalLoad(temp_storage, linear_tid).Load(block_itr, items);
+    }
+
+    template <typename InputIteratorT>
+    inline void
+    Load(InputIteratorT block_itr, ///< [in] The thread block's base input
+                                   ///< iterator for loading from
+         InputT (&items)[ITEMS_PER_THREAD], ///< [out] Data to load
+         int valid_items) ///< [in] Number of valid items to load
+    {
+        InternalLoad(temp_storage, linear_tid).Load(block_itr, items, valid_items);
+    }
+
+    template <typename InputIteratorT, typename DefaultT>
+    inline void
+    Load(InputIteratorT block_itr, ///< [in] The thread block's base input
+                                   ///< iterator for loading from
+         InputT (&items)[ITEMS_PER_THREAD], ///< [out] Data to load
+         int valid_items, ///< [in] Number of valid items to load
+         DefaultT
+             oob_default) ///< [in] Default value to assign out-of-bound items
+    {
+        InternalLoad(temp_storage, linear_tid).Load(block_itr, items, valid_items, oob_default);
+    }
+
+};
+
