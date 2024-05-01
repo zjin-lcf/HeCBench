d044b2fb944d61e6ffbd7835fe234e18932a5861
Jin Z <5zj@equinox.ftpn.ornl.gov>
2024-05-01 10:48:18 -0400

    [streamUM] delete StreamAttachMemAsync calls and capability checks in the CUDA and HIP programs; replace the destructor with a function to deallocate device memory in the Task structure; add a SYCL example; increase the number of tasks for verification in the Makefiles

--- src/streamUM-hip/main.cu
+++ src/streamUM-hip/main.cu
@@ -38,7 +38,9 @@
 #include <omp.h>
 #include <stdlib.h>
 
+// hipBLAS
 #include <hipblas/hipblas.h>
+#include <hip/hip_runtime.h>
 
 // utilities
 #include "reference.h"
@@ -52,10 +54,10 @@ hipError_t checkHipErrors(hipError_t result) {
   return result;
 }
 
-hipblasStatus_t checkCublasErrors(hipblasStatus_t result) {
+hipblasStatus_t checkHipblasErrors(hipblasStatus_t result) {
 #if defined(DEBUG) || defined(_DEBUG)
   if (result != HIPBLAS_STATUS_SUCCESS) {
-    fprintf(stderr, "HIPBLAS Runtime Error: %s\n", hipblasGetStatusString(result));
+    fprintf(stderr, "HIPBLAS Runtime Error: %s\n", hipblasStatusToString(result));
   }
 #endif
   return result;
@@ -77,18 +79,8 @@ struct Task {
   T *vector;
 
   Task() : size(0), id(0), data(NULL), result(NULL), vector(NULL){};
-  Task(unsigned int s) : size(s), id(0), data(NULL), result(NULL) {
-    // allocate unified memory -- the operation performed in this example will
-    // be a DGEMV
-    checkHipErrors(hipMallocManaged(&data, sizeof(T) * size * size));
-    checkHipErrors(hipMallocManaged(&result, sizeof(T) * size));
-    checkHipErrors(hipMallocManaged(&vector, sizeof(T) * size));
-    checkHipErrors(hipDeviceSynchronize());
-  }
 
-  ~Task() {
-    // ensure all memory is deallocated
-    checkHipErrors(hipDeviceSynchronize());
+  void deallocate() {
     checkHipErrors(hipFree(data));
     checkHipErrors(hipFree(result));
     checkHipErrors(hipFree(vector));
@@ -101,7 +93,6 @@ struct Task {
     checkHipErrors(hipMallocManaged(&data, sizeof(T) * size * size));
     checkHipErrors(hipMallocManaged(&result, sizeof(T) * size));
     checkHipErrors(hipMallocManaged(&vector, sizeof(T) * size));
-    checkHipErrors(hipDeviceSynchronize());
 
     // populate data with random elements
     for (unsigned int i = 0; i < size * size; i++) {
@@ -115,7 +106,6 @@ struct Task {
   }
 };
 
-
 // execute a single task on either host or device depending on size
 template <typename T>
 void execute(Task<T> &t, hipblasHandle_t *handle, hipStream_t *stream,
@@ -124,34 +114,18 @@ void execute(Task<T> &t, hipblasHandle_t *handle, hipStream_t *stream,
     // perform on host
     //printf("Task [%d], thread [%d] executing on host (%d)\n", t.id, tid, t.size);
 
-    // attach managed memory to a (dummy) stream to allow host access while the
-    // device is running
-    checkHipErrors(
-        hipStreamAttachMemAsync(stream[0], t.data, 0, hipMemAttachHost));
-    checkHipErrors(
-        hipStreamAttachMemAsync(stream[0], t.vector, 0, hipMemAttachHost));
-    checkHipErrors(
-        hipStreamAttachMemAsync(stream[0], t.result, 0, hipMemAttachHost));
-    // necessary to ensure Async hipStreamAttachMemAsync calls have finished
-    checkHipErrors(hipStreamSynchronize(stream[0]));
     // call the host operation
-    gemv(t.size, t.size, 1.0, t.data, t.vector, 0.0, t.result);
+    gemv(t.size, t.size, (T)1.0, t.data, t.vector, (T)0.0, t.result);
   } else {
     // perform on device
     //printf("Task [%d], thread [%d] executing on device (%d)\n", t.id, tid, t.size);
-    double one = 1.0;
-    double zero = 0.0;
+    T one = 1.0;
+    T zero = 0.0;
 
     // attach managed memory to my stream
-    checkCublasErrors(hipblasSetStream(handle[tid + 1], stream[tid + 1]));
-    checkHipErrors(hipStreamAttachMemAsync(stream[tid + 1], t.data, 0,
-                                             hipMemAttachSingle));
-    checkHipErrors(hipStreamAttachMemAsync(stream[tid + 1], t.vector, 0,
-                                             hipMemAttachSingle));
-    checkHipErrors(hipStreamAttachMemAsync(stream[tid + 1], t.result, 0,
-                                             hipMemAttachSingle));
+    checkHipblasErrors(hipblasSetStream(handle[tid + 1], stream[tid + 1]));
     // call the device operation
-    checkCublasErrors(hipblasDgemv(handle[tid + 1], HIPBLAS_OP_N, t.size, t.size,
+    checkHipblasErrors(hipblasDgemv(handle[tid + 1], HIPBLAS_OP_N, t.size, t.size,
                                 &one, t.data, t.size, t.vector, 1, &zero,
                                 t.result, 1));
   }
@@ -162,8 +136,7 @@ template <typename T>
 void initialise_tasks(std::vector<Task<T> > &TaskList) {
   for (unsigned int i = 0; i < TaskList.size(); i++) {
     // generate random size
-    int size;
-    size = std::max((int)(drand48() * 1000.0), 64);
+    int size = std::max((int)(drand48() * 1000.0), 64);
     TaskList[i].allocate(size, i);
   }
 }
@@ -177,9 +150,10 @@ int main(int argc, char **argv) {
   const unsigned int N = atoi(argv[2]);
   const int verify = atoi(argv[3]);
 
+  /*
   // set device
   hipDeviceProp_t device_prop;
-  int dev_id = 0;
+  const int dev_id = 0;
   checkHipErrors(hipGetDeviceProperties(&device_prop, dev_id));
 
   if (!device_prop.managedMemory) {
@@ -197,6 +171,7 @@ int main(int argc, char **argv) {
 
     exit(0);
   }
+  */
 
   // randomise task sizes
   srand48(48);
@@ -207,7 +182,7 @@ int main(int argc, char **argv) {
 
   for (int i = 0; i < nthreads + 1; i++) {
     checkHipErrors(hipStreamCreate(&streams[i]));
-    checkCublasErrors(hipblasCreate(&handles[i]));
+    checkHipblasErrors(hipblasCreate(&handles[i]));
   }
 
   // create list of N tasks
@@ -216,13 +191,15 @@ int main(int argc, char **argv) {
 
   printf("Executing tasks on host / device\n");
 
-  auto start = std::chrono::steady_clock::now();
-
 // run through all tasks using threads and streams
   omp_set_num_threads(nthreads);
-#pragma omp parallel for schedule(dynamic)
-  for (int i = 0; i < TaskList.size(); i++) {
-    checkHipErrors(hipSetDevice(dev_id));
+
+  //checkHipErrors(hipSetDevice(dev_id));
+
+  auto start = std::chrono::steady_clock::now();
+
+  #pragma omp parallel for schedule(dynamic)
+  for (size_t i = 0; i < TaskList.size(); i++) {
     int tid = omp_get_thread_num();
     execute(TaskList[i], handles, streams, tid);
   }
@@ -238,14 +215,16 @@ int main(int argc, char **argv) {
     check(TaskList);
   }
 
-  // Destroy streams and handles
+  for (size_t i = 0; i < TaskList.size(); i++) {
+    TaskList[i].deallocate();
+  }
+
   for (int i = 0; i < nthreads + 1; i++) {
     hipStreamDestroy(streams[i]);
     hipblasDestroy(handles[i]);
   }
-
-  // Free TaskList
-  std::vector<Task<double> >().swap(TaskList);
+  delete[] streams;
+  delete[] handles;
 
   return 0;
 }
