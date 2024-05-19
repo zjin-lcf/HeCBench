193da8c830f4643b480e9c7812059be62ed3f08f
Jin Z <5zj@equinox.ftpn.ornl.gov>
2024-05-19 19:53:55 -0400

    [spgemm] add the COO matrix storage format for evaluation

--- src/spgemm-hip/main.cu
+++ src/spgemm-hip/main.cu
@@ -54,69 +54,27 @@
 #include <hipsparse/hipsparse.h>
 #include "utils.h"
 
-#define CHECK_HIP(func)                                                  \
-{                                                                        \
-    hipError_t status = (func);                                          \
-    if (status != hipSuccess) {                                          \
-        printf("HIP API failed at line %d with error: %s (%d)\n",        \
-               __LINE__, hipGetErrorString(status), status);             \
-        return EXIT_FAILURE;                                             \
-    }                                                                    \
+#define CHECK_HIP(func)                                                      \
+{                                                                            \
+    hipError_t status = (func);                                              \
+    if (status != hipSuccess) {                                              \
+        printf("HIP API failed at line %d with error: %s (%d)\n",            \
+               __LINE__, hipGetErrorString(status), status);                 \
+        return EXIT_FAILURE;                                                 \
+    }                                                                        \
 }
 
-#define CHECK_HIPSPARSE_ERROR_CASE__(token_) \
-    case token_:                             \
-        fprintf(stderr, "HIPSPARSE API failed at line %d with error: %s\n",         \
-               __LINE__, #token_); \
-        break
+#define CHECK_HIPSPARSE(func)                                                \
+{                                                                            \
+    hipsparseStatus_t status = (func);                                       \
+    if (status != HIPSPARSE_STATUS_SUCCESS) {                                \
+        printf("HIPSPARSE API failed at line %d with error: %s (%d)\n",      \
+               __LINE__, hipsparseGetErrorString(status), status);           \
+        return EXIT_FAILURE;                                                 \
+    }                                                                        \
+}
 
-#define CHECK_HIPSPARSE(error)                                                      \
-    {                                                                                     \
-        auto local_error = (error);                                                       \
-        if(local_error != HIPSPARSE_STATUS_SUCCESS)                                       \
-        {                                                                                 \
-            fprintf(stderr, "hipSPARSE error: ");                                         \
-            switch(local_error)                                                           \
-            {                                                                             \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_SUCCESS);                   \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_NOT_INITIALIZED);           \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_ALLOC_FAILED);              \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_INVALID_VALUE);             \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_ARCH_MISMATCH);             \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_MAPPING_ERROR);             \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_EXECUTION_FAILED);          \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_INTERNAL_ERROR);            \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_MATRIX_TYPE_NOT_SUPPORTED); \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_ZERO_PIVOT);                \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_NOT_SUPPORTED);             \
-                CHECK_HIPSPARSE_ERROR_CASE__(HIPSPARSE_STATUS_INSUFFICIENT_RESOURCES);    \
-            }                                                                             \
-            fprintf(stderr, "\n");                                                        \
-            return local_error;                                                           \
-        }                                                                                 \
-    }                                                                                     
-
-int main(int argc, char *argv[])
-{
-  int repeat = 1;
-
-  if (argc != 7) {
-    printf("Single-precision sparse matrix-dense matrix multiplication into dense matrix,\n");
-    printf("where the sparse matrix is represented in CSR (Compressed Sparse Row) storage format\n");
-    printf("Usage %s <M> <K> <N> <A_nnz> <repeat> <verify>\n", argv[0]);
-    printf("SPMM (A, B, C) where (A: M * K, B: K * N, C: M * N)\n");
-    return 1;
-  }
-
-  int m, k, n, a_nnz, verify;
-
-  m = atoi(argv[1]);
-  k = atoi(argv[2]);
-  n = atoi(argv[3]);
-  a_nnz = atoi(argv[4]);
-  repeat = atoi(argv[5]);
-  verify = atoi(argv[6]);
-
+int COO(int m, int k, int n, int a_nnz, int repeat, int verify) {
   // Host problem definition
   const int A_num_rows = m;
   const int A_num_cols = k;
@@ -135,7 +93,162 @@ int main(int argc, char *argv[])
 
   const size_t A_value_size_bytes  = A_nnz * sizeof(float);
   const size_t A_colidx_size_bytes = A_nnz * sizeof(int);
-  const size_t A_rowidx_size_bytes = (A_num_rows + 1) * sizeof(size_t);
+  const size_t A_rowidx_size_bytes = A_nnz * sizeof(int);
+
+  float *hA_values = (float*) malloc (A_value_size_bytes);
+  int *hA_columns = (int*) malloc (A_colidx_size_bytes);
+  int *hA_rows = (int*) malloc (A_rowidx_size_bytes);
+
+  init_matrix(hA, A_num_rows, A_num_cols, A_nnz);
+  init_coo(hA_rows, hA_values, hA_columns, hA,
+           A_num_rows, A_num_cols, A_nnz);
+
+  init_matrix(hB, B_num_rows, B_num_cols, B_size);
+
+  float               alpha       = 1.0f;
+  float               beta        = 0.0f;
+  hipsparseOperation_t opA        = HIPSPARSE_OPERATION_NON_TRANSPOSE;
+  hipsparseOperation_t opB        = HIPSPARSE_OPERATION_NON_TRANSPOSE;
+
+  //--------------------------------------------------------------------------
+  // Device memory management: Allocate and copy A, B
+  int   *dA_rows, *dA_columns;
+  float *dA_values, *dB, *dC;
+  // allocate A
+  CHECK_HIP( hipMalloc((void**) &dA_rows, A_nnz * sizeof(int)) )
+  CHECK_HIP( hipMalloc((void**) &dA_columns, A_nnz * sizeof(int))   )
+  CHECK_HIP( hipMalloc((void**) &dA_values,  A_nnz * sizeof(float)) )
+  // allocate B
+  CHECK_HIP( hipMalloc((void**) &dB, B_size * sizeof(float)) )
+  // allocate C
+  CHECK_HIP( hipMalloc((void**) &dC, C_size * sizeof(float)) )
+
+  // copy A
+  CHECK_HIP( hipMemcpy(dA_rows, hA_rows, A_nnz * sizeof(int),
+                       hipMemcpyHostToDevice) )
+  CHECK_HIP( hipMemcpy(dA_columns, hA_columns, A_nnz * sizeof(int),
+                       hipMemcpyHostToDevice) )
+  CHECK_HIP( hipMemcpy(dA_values, hA_values,
+                       A_nnz * sizeof(float), hipMemcpyHostToDevice) )
+  // copy B
+  CHECK_HIP( hipMemcpy(dB, hB, B_size * sizeof(float), hipMemcpyHostToDevice) )
+
+  //--------------------------------------------------------------------------
+  // HIPSPARSE APIs
+  hipsparseHandle_t     handle = NULL;
+  hipsparseSpMatDescr_t matA;
+  hipsparseDnMatDescr_t matB, matC;
+  void*  dBuffer    = NULL;
+  size_t bufferSize = 0;
+  CHECK_HIPSPARSE( hipsparseCreate(&handle) )
+  // Create sparse matrix A in COO format
+  CHECK_HIPSPARSE( hipsparseCreateCoo(&matA, A_num_rows, A_num_cols, A_nnz,
+                                      dA_rows, dA_columns, dA_values,
+                                      HIPSPARSE_INDEX_32I,
+                                      HIPSPARSE_INDEX_BASE_ZERO, HIP_R_32F) )
+  CHECK_HIPSPARSE( hipsparseCreateDnMat(&matB, B_num_rows, B_num_cols, ldb,
+                                        dB, HIP_R_32F, HIPSPARSE_ORDER_ROW) )
+  CHECK_HIPSPARSE( hipsparseCreateDnMat(&matC, A_num_rows, B_num_cols, ldc,
+                                        dC, HIP_R_32F, HIPSPARSE_ORDER_ROW) )
+
+  // allocate an external buffer if needed
+  CHECK_HIPSPARSE( hipsparseSpMM_bufferSize(
+                               handle,
+                               opA,
+                               opB,
+                               &alpha, matA, matB, &beta, matC, HIP_R_32F,
+                               HIPSPARSE_SPMM_ALG_DEFAULT, &bufferSize) )
+  CHECK_HIP( hipMalloc(&dBuffer, bufferSize) )
+
+  hipDeviceSynchronize();
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+
+    // compute the product of A * B
+    CHECK_HIPSPARSE( hipsparseSpMM(handle,
+                                   opA,
+                                   opB,
+                                   &alpha, matA, matB, &beta, matC, HIP_R_32F,
+                                   HIPSPARSE_SPMM_ALG_DEFAULT, dBuffer) )
+
+  }
+  hipDeviceSynchronize();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time of SPGEMM (COO) compute: %f (us)\n", (time * 1e-3f) / repeat);
+
+  // destroy matrix/vector descriptors
+  CHECK_HIPSPARSE( hipsparseDestroySpMat(matA) )
+  CHECK_HIPSPARSE( hipsparseDestroyDnMat(matB) )
+  CHECK_HIPSPARSE( hipsparseDestroyDnMat(matC) )
+  CHECK_HIPSPARSE( hipsparseDestroy(handle) )
+
+  //--------------------------------------------------------------------------
+  // device result check
+  
+  if (verify) {
+    printf("Computing the reference SPGEMM results..\n");
+    float *hC = (float*) malloc (C_size * sizeof(float));
+    gemm (hA, hB, hC, A_num_cols, A_num_rows, B_num_cols);
+
+    float *hC_tmp = (float*) malloc (C_size * sizeof(float));
+    CHECK_HIP( hipMemcpy(hC_tmp, dC, C_size * sizeof(float),
+                           hipMemcpyDeviceToHost) )
+    int correct = 1;
+    for (int i = 0; i < C_size; i++) {
+      if (fabsf(hC_tmp[i] - hC[i]) > 1e-2f) {
+        printf("@%d %f != %f\n", i, hC_tmp[i], hC[i]);
+        correct = 0;                       
+        break;
+      }
+    }
+
+    free(hC_tmp);
+    free(hC);
+
+    if (correct)
+      printf("spgemm_example test PASSED\n");
+    else
+      printf("spgemm_example test FAILED: wrong result\n");
+  }
+
+  //--------------------------------------------------------------------------
+  // device memory deallocation
+  CHECK_HIP( hipFree(dBuffer) )
+  CHECK_HIP( hipFree(dA_rows) )
+  CHECK_HIP( hipFree(dA_columns) )
+  CHECK_HIP( hipFree(dA_values) )
+  CHECK_HIP( hipFree(dB) )
+  CHECK_HIP( hipFree(dC) )
+  free(hA);
+  free(hB);
+  free(hA_values);
+  free(hA_columns);
+  free(hA_rows);
+  return EXIT_SUCCESS;
+}
+
+int CSR(int m, int k, int n, int a_nnz, int repeat, int verify) {
+  // Host problem definition
+  const int A_num_rows = m;
+  const int A_num_cols = k;
+  const int A_nnz      = a_nnz;
+  const int B_num_rows = A_num_cols;
+  const int B_num_cols = n;
+  const int lda        = A_num_cols;
+  const int ldb        = B_num_cols;
+  const int ldc        = B_num_cols;
+  const int A_size     = lda * A_num_rows;
+  const int B_size     = ldb * B_num_rows;
+  const int C_size     = ldc * A_num_rows;
+
+  float *hA = (float*) malloc (A_size * sizeof(float));
+  float *hB = (float*) malloc (B_size * sizeof(float));
+
+  const size_t A_value_size_bytes  = A_nnz * sizeof(float);
+  const size_t A_colidx_size_bytes = A_nnz * sizeof(int);
+  const size_t A_rowidx_size_bytes = (A_num_rows + 1) * sizeof(int);
 
   float *hA_values = (float*) malloc (A_value_size_bytes);
   int *hA_columns = (int*) malloc (A_colidx_size_bytes);
@@ -167,7 +280,8 @@ int main(int argc, char *argv[])
   CHECK_HIP( hipMalloc((void**) &dC, C_size * sizeof(float)) )
 
   // copy A
-  CHECK_HIP( hipMemcpy(dA_offsets, hA_offsets, (A_num_rows + 1) * sizeof(int),
+  CHECK_HIP( hipMemcpy(dA_offsets, hA_offsets,
+                       (A_num_rows + 1) * sizeof(int),
                        hipMemcpyHostToDevice) )
   CHECK_HIP( hipMemcpy(dA_columns, hA_columns, A_nnz * sizeof(int),
                        hipMemcpyHostToDevice) )
@@ -219,7 +333,7 @@ int main(int argc, char *argv[])
   hipDeviceSynchronize();
   auto end = std::chrono::steady_clock::now();
   auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  printf("Average execution time of SPGEMM compute: %f (us)\n", (time * 1e-3f) / repeat);
+  printf("Average execution time of SPGEMM (CSR) compute: %f (us)\n", (time * 1e-3f) / repeat);
 
   // destroy matrix/vector descriptors
   CHECK_HIPSPARSE( hipsparseDestroySpMat(matA) )
@@ -237,7 +351,7 @@ int main(int argc, char *argv[])
 
     float *hC_tmp = (float*) malloc (C_size * sizeof(float));
     CHECK_HIP( hipMemcpy(hC_tmp, dC, C_size * sizeof(float),
-                         hipMemcpyDeviceToHost) )
+                           hipMemcpyDeviceToHost) )
     int correct = 1;
     for (int i = 0; i < C_size; i++) {
       if (fabsf(hC_tmp[i] - hC[i]) > 1e-2f) {
@@ -251,11 +365,9 @@ int main(int argc, char *argv[])
     free(hC);
 
     if (correct)
-        printf("spgemm_example test PASSED\n");
-    else {
-        printf("spgemm_example test FAILED: wrong result\n");
-        return EXIT_FAILURE;
-    }
+      printf("spgemm_example test PASSED\n");
+    else
+      printf("spgemm_example test FAILED: wrong result\n");
   }
 
   //--------------------------------------------------------------------------
@@ -271,5 +383,32 @@ int main(int argc, char *argv[])
   free(hA_values);
   free(hA_columns);
   free(hA_offsets);
+  return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[])
+{
+  int repeat = 1;
+
+  if (argc != 7) {
+    printf("Single-precision sparse matrix-dense matrix multiplication into dense matrix,\n");
+    printf("where the sparse matrix is represented in COO and CSR storage format\n");
+    printf("Usage %s <M> <K> <N> <A_nnz> <repeat> <verify>\n", argv[0]);
+    printf("SPMM (A, B, C) where (A: M * K, B: K * N, C: M * N)\n");
+    return 1;
+  }
+
+  int m, k, n, a_nnz, verify;
+
+  m = atoi(argv[1]);
+  k = atoi(argv[2]);
+  n = atoi(argv[3]);
+  a_nnz = atoi(argv[4]);
+  repeat = atoi(argv[5]);
+  verify = atoi(argv[6]);
+
+  COO(m, k, n, a_nnz, repeat, verify);
+  CSR(m, k, n, a_nnz, repeat, verify);
+
   return EXIT_SUCCESS;
 }
--- src/spgemm-sycl/main.cpp
+++ src/spgemm-sycl/main.cpp
@@ -2,30 +2,143 @@
 #include <stdlib.h>
 #include <math.h>
 #include <chrono>
-#include <oneapi/mkl.hpp>
+#include <oneapi/mkl/spblas.hpp>
 #include "utils.h"
 
-int main(int argc, char *argv[])
-{
-  int repeat = 1;
+int COO(int m, int k, int n, int a_nnz, int repeat, int verify) {
+  // Host problem definition
+  const int A_num_rows = m;
+  const int A_num_cols = k;
+  const int A_nnz      = a_nnz;
+  const int B_num_rows = A_num_cols;
+  const int B_num_cols = n;
+  const int lda        = A_num_cols;
+  const int ldb        = B_num_cols;
+  const int ldc        = B_num_cols;
+  const int A_size     = lda * A_num_rows;
+  const int B_size     = ldb * B_num_rows;
+  const int C_num_rows = A_num_rows;
+  const int C_num_cols = B_num_cols;
+  const int C_size     = C_num_rows * C_num_cols;
 
-  if (argc != 7) {
-    printf("Single-precision sparse matrix-dense matrix multiplication into dense matrix,\n");
-    printf("where the sparse matrix is represented in CSR (Compressed Sparse Row) storage format\n");
-    printf("Usage %s <M> <K> <N> <A_nnz> <repeat> <verify>\n", argv[0]);
-    printf("SPMM (A, B, C) where (A: M * K, B: K * N, C: M * N)\n");
-    return 1;
+  float *hA = (float*) malloc (A_size * sizeof(float));
+  float *hB = (float*) malloc (B_size * sizeof(float));
+
+  const size_t A_value_size_bytes  = A_nnz * sizeof(float);
+  const size_t A_colidx_size_bytes = A_nnz * sizeof(int);
+  const size_t A_rowidx_size_bytes = A_nnz * sizeof(int);
+
+  float *hA_values = (float*) malloc (A_value_size_bytes);
+  int *hA_columns = (int*) malloc (A_colidx_size_bytes);
+  int *hA_rows = (int*) malloc (A_rowidx_size_bytes);
+
+  init_matrix(hA, A_num_rows, A_num_cols, A_nnz);
+  init_coo(hA_rows, hA_values, hA_columns, hA,
+           A_num_rows, A_num_cols, A_nnz);
+
+  init_matrix(hB, B_num_rows, B_num_cols, B_size);
+
+  //--------------------------------------------------------------------------
+  // Device memory management: Allocate and copy A, B
+  int   *dA_rows, *dA_columns;
+  float *dA_values, *dB, *dC;
+
+#ifdef USE_GPU
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
+#else
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
+#endif
+
+  // allocate A
+  dA_rows = sycl::malloc_device<int>(A_num_rows + 1, q);
+  dA_columns = sycl::malloc_device<int>(A_nnz, q);
+  dA_values = sycl::malloc_device<float>(A_nnz, q);
+
+  // allocate B
+  dB = sycl::malloc_device<float>(B_size, q);
+
+  // allocate C
+  dC = sycl::malloc_device<float>(C_size, q);
+
+  // copy A
+  q.memcpy(dA_rows, hA_rows, (A_num_rows + 1) * sizeof(int));
+  q.memcpy(dA_columns, hA_columns, A_nnz * sizeof(int));
+  q.memcpy(dA_values, hA_values, A_nnz * sizeof(float));
+  // copy B
+  q.memcpy(dB, hB, B_size * sizeof(float));
+  //--------------------------------------------------------------------------
+
+  oneapi::mkl::sparse::matrix_handle_t handle = nullptr;
+  oneapi::mkl::sparse::init_matrix_handle(&handle);
+
+  float alpha = 1.f;
+  float beta  = 0.f;
+  oneapi::mkl::transpose opA = oneapi::mkl::transpose::nontrans;
+  oneapi::mkl::transpose opB = oneapi::mkl::transpose::nontrans;
+  oneapi::mkl::layout layout = oneapi::mkl::layout::row_major;
+  oneapi::mkl::index_base A_index = oneapi::mkl::index_base::zero;
+
+  oneapi::mkl::sparse::set_coo_data(q, handle, A_num_rows, A_num_cols, A_nnz, 
+                                    A_index, dA_rows, dA_columns, dA_values, {}).wait();
+
+  auto start = std::chrono::steady_clock::now();
+
+  for (int i = 0; i < repeat; i++) {
+    oneapi::mkl::sparse::gemm(q, layout, opA, opB, alpha, handle,
+                              dB, C_num_cols, ldb, beta, dC, ldc, {});
+  }
+  q.wait();
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time of SPGEMM (COO) compute: %f (us)\n", (time * 1e-3f) / repeat);
+
+  oneapi::mkl::sparse::release_matrix_handle(q, &handle, {}).wait();
+
+  //--------------------------------------------------------------------------
+  // device result check
+  
+  if (verify) {
+    printf("Computing the reference SPGEMM results..\n");
+    float *hC = (float*) malloc (C_size * sizeof(float));
+    gemm (hA, hB, hC, A_num_cols, A_num_rows, B_num_cols);
+
+    float *hC_tmp = (float*) malloc (C_size * sizeof(float));
+    q.memcpy(hC_tmp, dC, C_size * sizeof(float)).wait();
+
+    int correct = 1;
+    for (int i = 0; i < C_size; i++) {
+      if (fabsf(hC_tmp[i] - hC[i]) > 1e-2f) {
+        printf("@%d %f != %f\n", i, hC_tmp[i], hC[i]);
+        correct = 0;                       
+        break;
+      }
+    }
+
+    free(hC_tmp);
+    free(hC);
+
+    if (correct)
+      printf("spgemm_example test PASSED\n");
+    else
+      printf("spgemm_example test FAILED: wrong result\n");
   }
 
-  int m, k, n, a_nnz, verify;
+  //--------------------------------------------------------------------------
+  // device memory deallocation
+  sycl::free(dA_rows, q);
+  sycl::free(dA_columns, q);
+  sycl::free(dA_values, q);
+  sycl::free(dB, q);
+  sycl::free(dC, q);
+  free(hA);
+  free(hB);
+  free(hA_values);
+  free(hA_columns);
+  free(hA_rows);
+  return EXIT_SUCCESS;
+}
 
-  m = atoi(argv[1]);
-  k = atoi(argv[2]);
-  n = atoi(argv[3]);
-  a_nnz = atoi(argv[4]);
-  repeat = atoi(argv[5]);
-  verify = atoi(argv[6]);
-
+int CSR(int m, int k, int n, int a_nnz, int repeat, int verify) {
   // Host problem definition
   const int A_num_rows = m;
   const int A_num_cols = k;
@@ -110,7 +223,7 @@ int main(int argc, char *argv[])
   q.wait();
   auto end = std::chrono::steady_clock::now();
   auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  printf("Average execution time of SPGEMM compute: %f (us)\n", (time * 1e-3f) / repeat);
+  printf("Average execution time of SPGEMM (CSR) compute: %f (us)\n", (time * 1e-3f) / repeat);
 
   oneapi::mkl::sparse::release_matrix_handle(q, &handle, {}).wait();
 
@@ -138,11 +251,9 @@ int main(int argc, char *argv[])
     free(hC);
 
     if (correct)
-        printf("spgemm_example test PASSED\n");
-    else {
-        printf("spgemm_example test FAILED: wrong result\n");
-        return EXIT_FAILURE;
-    }
+      printf("spgemm_example test PASSED\n");
+    else
+      printf("spgemm_example test FAILED: wrong result\n");
   }
 
   //--------------------------------------------------------------------------
@@ -157,5 +268,33 @@ int main(int argc, char *argv[])
   free(hA_values);
   free(hA_columns);
   free(hA_offsets);
+  return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[])
+{
+  int repeat = 1;
+
+  if (argc != 7) {
+    printf("Single-precision sparse matrix-dense matrix multiplication into dense matrix,\n");
+    printf("where the sparse matrix is represented in COO and CSR storage format\n");
+    printf("Usage %s <M> <K> <N> <A_nnz> <repeat> <verify>\n", argv[0]);
+    printf("SPMM (A, B, C) where (A: M * K, B: K * N, C: M * N)\n");
+    return 1;
+  }
+
+  int m, k, n, a_nnz, verify;
+
+  m = atoi(argv[1]);
+  k = atoi(argv[2]);
+  n = atoi(argv[3]);
+  a_nnz = atoi(argv[4]);
+  repeat = atoi(argv[5]);
+  verify = atoi(argv[6]);
+
+  // The supported cases for <sparse_matrix_type> are csr on CPU and GPU devices, and coo only on CPU device.
+  COO(m, k, n, a_nnz, repeat, verify);
+  CSR(m, k, n, a_nnz, repeat, verify);
+
   return EXIT_SUCCESS;
 }
