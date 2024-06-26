8b7575379ffe38b4fcec071e0a9a0affbae07648
Jin Z <5zj@equinox.ftpn.ornl.gov>
2024-06-26 16:50:59 -0400

    [autohecbench.py] add the options for logging, a prompt, a summary of the benchmark execution, and re-generating the benchmark results when they are already in the output file; maintain a language-agnostic benchmark list in subset.json

--- src/scripts/autohecbench.py
+++ src/scripts/autohecbench.py
@@ -2,10 +2,17 @@
 #
 # Script to run HeCBench benchmarks and gather results
 
-import re, time, sys, subprocess, multiprocessing, os
+import re, time, datetime, sys, subprocess, multiprocessing, os
 import argparse
 import json
+import logging
 
+def await_input(prompt: str, is_valid_input) -> str:
+    """ Wait the user for input until it is valid. """
+    r = input(prompt)
+    while not is_valid_input(r):
+        r = input(prompt)
+    return r
 
 class Benchmark:
     def __init__(self, args, name, res_regex, run_args = [], binary = "main", invert = False):
@@ -26,7 +33,7 @@ class Benchmark:
                 self.MAKE_ARGS.append('HIP=no')
                 self.MAKE_ARGS.append('GPU=no')
         elif name.endswith('cuda'):
-            self.MAKE_ARGS = ['CUDA_ARCH=sm_{}'.format(args.nvidia_sm)]
+            self.MAKE_ARGS = ['ARCH=sm_{}'.format(args.nvidia_sm)]
         else:
             self.MAKE_ARGS = []
 
@@ -47,7 +54,7 @@ class Benchmark:
         self.clean = args.clean
         self.verbose = args.verbose
 
-    def compile(self):
+    def compile(self, shared_data):
         if self.clean:
             subprocess.run(["make", "clean"], cwd=self.path).check_returncode()
             time.sleep(1) # required to make sure clean is done before building, despite run waiting on the invoked executable
@@ -58,55 +65,69 @@ class Benchmark:
 
         proc = subprocess.run(["make"] + self.MAKE_ARGS, cwd=self.path,
                               stdout=out, stderr=subprocess.STDOUT, encoding="utf-8")
+
         try:
             proc.check_returncode()
+            shared_data[self.name] = "success"
         except subprocess.CalledProcessError as e:
             print(f'Failed compilation in {self.path}.\n{e}')
             if e.stderr:
                 print(e.stderr, file=sys.stderr)
 
-            print("-----------------------------------------------------------------------------------------")
+            print("*****************************************************************************************")
             print("Description of the compilation error:")
             cause = subprocess.run(["make"] + self.MAKE_ARGS, cwd=self.path,
                                    check=False, capture_output=True, encoding="utf-8")
             print(cause.stdout)
             print(cause.stderr)
-            print("-----------------------------------------------------------------------------------------")
-            raise(e)
+            print("*****************************************************************************************")
+            shared_data[self.name] = "failed"
+            #raise(e)
 
         if self.verbose:
             print(proc.stdout)
 
     def run(self):
         cmd = ["./" + self.binary] + self.args
-        proc = subprocess.run(cmd, cwd=self.path,
+        proc = subprocess.run(cmd, cwd=self.path, timeout=600,
                               stdout=subprocess.PIPE, encoding="utf-8")
         out = proc.stdout
         if self.verbose:
             print(" ".join(cmd))
             print(out)
-        res = re.findall(self.res_regex, out)
+        try:
+             res = re.findall(self.res_regex, out)
+        except re.error as e:
+             print("Regular expression error occurred:", e.msg)
+             print("Pattern:", e.pattern)
+             print("Position:", e.pos)
+        logging.debug(f'Results of re.findall:\n {res}')
         if not res:
             raise Exception(self.path + ":\nno regex match for " + self.res_regex + " in\n" + out)
-        res = sum([float(i) for i in res]) #in case of multiple outputs sum them
+        res = sum([float(i) for i in res]) #in case of multiple outputs sum them (e.g. total time)
         if self.invert:
             res = 1/res
         return res
 
 
-def comp(b):
-    print("compiling: {}".format(b.name))
-    b.compile()
+def comp(b, d):
+    b.compile(d)
+
 
 def main():
     parser = argparse.ArgumentParser(description='HeCBench runner')
+    parser.add_argument("--log", choices=["INFO", "DEBUG", "WARNING", "ERROR", "CRITICAL"],
+                        default="INFO", type=str.upper, help="logging level")
     parser.add_argument('--output', '-o',
                         help='Output file for csv results')
+    parser.add_argument('--summary', '-s',
+                        help='Output file for a summary of the execution of selected benchmarks')
+    parser.add_argument("--yes-prompt", action="store_true", help="If provided, automatically answer yes to the prompt.")
     parser.add_argument('--repeat', '-r', type=int, default=1,
                         help='Repeat benchmark run')
     parser.add_argument('--warmup', '-w', type=bool, default=True,
                         help='Run a warmup iteration')
-    parser.add_argument('--sycl-type', '-s', choices=['cuda', 'hip', 'opencl', 'cpu'], default='cuda',
+    parser.add_argument('--sycl-type', '-t', choices=['cuda', 'hip', 'opencl', 'cpu'], default='cuda',
                         help='Type of SYCL device to use (default is cuda)')
     parser.add_argument('--nvidia-sm', type=int, default=60,
                         help='NVIDIA SM version (default is 60)')
@@ -118,6 +139,9 @@ def main():
                         help='GCC toolchain location (e.g. /path/to/gcc/x86_64/gcc-9.1.0)')
     parser.add_argument('--extra-compile-flags', '-e', default='',
                         help='Additional compilation flags (inserted before the predefined CFLAGS)')
+    parser.add_argument("--overwrite", action="store_true",
+                        help="If benchmark results are already available in the output file , \
+                              then overwrite them. Default behavior is to skip existing results.")
     parser.add_argument('--clean', '-c', action='store_true',
                         help='Clean the builds')
     parser.add_argument('--verbose', '-v', action='store_true',
@@ -133,6 +157,23 @@ def main():
 
     args = parser.parse_args()
 
+    # setup logging
+    numeric_level = getattr(logging, args.log.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError("Invalid log level: {}".format(args.log))
+    logging.basicConfig(format="%(asctime)s [%(levelname)s] -- %(message)s", level=numeric_level)
+
+    # warn user before continuing
+    logging.warning("This script will compile and run selected benchmarks in HeCBench and gather results. " +
+        "It is recommended that before you run this script, the dataset are available for certain benchmarks " +
+        "and the compilers are in the PATH environment.") 
+
+    if not args.yes_prompt:
+        response = await_input("Continue? [y/n] ", lambda r: r.lower() in ["y", "n", "yes", "no"])
+        if response.lower() in ["n", "no"]:
+            logging.info("Exiting.")
+            return
+
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Load benchmark data
@@ -144,6 +185,8 @@ def main():
     with open(bench_data) as f:
         benchmarks = json.load(f)
 
+    logging.info(f"Loaded benchmark names and command-line arguments from {bench_data}.")
+
     # Load fail file
     if args.bench_fails:
         bench_fails = os.path.abspath(args.bench_fails)
@@ -153,50 +196,80 @@ def main():
     with open(bench_fails) as f:
         fails = f.read().splitlines()
 
-    # Build benchmark list
+    logging.info(f"Loaded benchmark skip file from {bench_fails}.")
+
+    # Build benchmark list from the benchmark data file (e.g. subset.json)
     benches = []
     for b in args.bench:
         if b in ['sycl', 'cuda', 'hip']:
-            benches.extend([Benchmark(args, k, *v)
+            benches.extend([Benchmark(args, k+'-'+b, *v)
                             for k, v in benchmarks.items()
-                            if k.endswith(b) and k not in fails])
+                            if k+'-'+b not in fails])  # e.g. nbody-sycl
             continue
+        # b is a specific benchmark instead
+        ch_index = b.find('-') # find specific character '-'
+        b_sub = b[:ch_index]
+        benches.append(Benchmark(args, b, *benchmarks[b_sub]))
 
-        benches.append(Benchmark(args, b, *benchmarks[b]))
-
+    summary = {}
     t0 = time.time()
     try:
-        with multiprocessing.Pool() as p:
-            p.map(comp, benches)
+        #with multiprocessing.Pool() as p:
+        #    p.map(comp, benches)
+        procs = []
+        with multiprocessing.Manager() as m:
+           d = m.dict() # a shared dictionary (nested dictionary not supported)
+           for b in benches: 
+             p = multiprocessing.Process(target = comp, args = (b, d)) 
+             procs.append(p)
+             p.start()
+
+           for p in procs:
+               p.join()
+
+           for k, v in d.items():
+               summary[k] = {}
+               summary[k]["compile"] = v;
+           
     except Exception as e:
-        print("Compilation failed, exiting")
+        print("Error compiling the benchmarks:")
         print(e)
-        sys.exit(1)
 
     t_compiled = time.time()
 
     outfile = sys.stdout
-    recorded_benchmarks = set()
-    if args.output:
-        if os.path.isfile(args.output):
-            outfile = open(args.output, 'r+t')
-            for line in outfile:
-                bench, *rest = line.split(',')
-                recorded_benchmarks.add(bench)
-            outfile.seek(0, 2) # seek to end of the file.
-        else:
+    if args.overwrite:
+        filtered_benches = benches
+        if args.output:
             outfile = open(args.output, 'w+t')
+        logging.info(f"Overwrite the output file {args.output}.")
+    else:
+        recorded_benchmarks = set()
+        if args.output:
+            if os.path.isfile(args.output):
+                outfile = open(args.output, 'r+t')
+                for line in outfile:
+                    bench, *rest = line.split(',')
+                    recorded_benchmarks.add(bench)
+                    # record the status only when it is in the input benchmark list
+                    ch_index = bench.find('-')
+                    if bench[:ch_index] in benchmarks.keys():
+                        summary[bench]["run"] = "skipped"
+                outfile.seek(0, 2) # seek to end of the file.
+            else:
+                outfile = open(args.output, 'w+t')
 
-    filtered_benches = [b for b in benches if b.name not in recorded_benchmarks]
-    num_filtered_benches = len(benches) - len(filtered_benches)
-    if num_filtered_benches:
-        print(f"Filtered out {num_filtered_benches} benchmarks."
-              " Results already exists in the output file.", flush=True)
-    benches = filtered_benches
+        filtered_benches = [b for b in benches if b.name not in recorded_benchmarks]
+        num_filtered_benches = len(benches) - len(filtered_benches)
+        if num_filtered_benches:
+            print(f"Filtered out {num_filtered_benches} benchmarks."
+                  " Results already exists in the output file.", flush=True)
 
-    for i, b in enumerate(benches, 1):
+    for i, b in enumerate(filtered_benches, 1):
+        if b.name not in summary.keys():
+            summary[b.name] = {}
         try:
-            print(f"running {i}/{len(benches)}: {b.name}", flush=True)
+            print(f"running {i}/{len(filtered_benches)}: {b.name}", flush=True)
 
             if args.warmup:
                 b.run()
@@ -206,15 +279,31 @@ def main():
                 res.append(str(b.run()))
 
             print(b.name + "," + ", ".join(res), file=outfile)
-        except Exception as err:
+            summary[b.name]["run"] = "success"
+        except Exception as e:
             print("Error running: ", b.name)
-            print(err)
+            print(e)
+            summary[b.name]["run"] = "failed"
 
     if args.output:
         outfile.close()
 
     t_done = time.time()
-    print("compilation took {} s, running took {} s.".format(t_compiled-t0, t_done-t_compiled))
+
+    print("*****************************************************************************************")
+    print(datetime.datetime.now())
+    print("Summary of the benchmark execution:\n")
+    print("Compilation took {} s, running took {} s.".format(t_compiled-t0, t_done-t_compiled))
+    if args.summary:
+        with open(args.summary, "w") as f:
+            json.dump(summary, f, indent=4, sort_keys=True)
+        logging.info(f"Wrote the summary to {args.summary}.")
+    else:
+        print(json.dumps(summary, indent=4, sort_keys=True))
+    res = sum((x['compile'] == 'failed' or x['run'] == 'failed') for x in summary.values())
+    print(f'Number of benchmark compile or run failures: {res}');
+    print("*****************************************************************************************")
+
 
 if __name__ == "__main__":
     main()
