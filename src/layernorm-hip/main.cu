b654f82fcea698a0fd2d0610170143ba7bbc1a45
Zheming Jin <zjin-lcf@users.noreply.github.com>
2024-09-29 19:16:16 -0700

    [layernorm] add the layernorm forward pass in CUDA, HIP, and SYCL

---
+++ src/layernorm-hip/main.cu
@@ -0,0 +1,244 @@
+/*
+Kernels for layernorm forward pass.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "common.h"
+#include "reference.h"
+
+__global__ void layernorm_forward_kernel1(float* __restrict__ out, float* __restrict__ mean, float* __restrict__ rstd,
+                                    const float*  __restrict__ inp, const float*  __restrict__ weight,
+                                    const float* __restrict__ bias, int N, int C) {
+    cg::thread_block block = cg::this_thread_block();
+    cg::thread_block_tile<32> warp = cg::tiled_partition<32>(block);
+    // meta_group_size is the number of warps in a block, and meta_group_rank is the warp index
+    int idx = blockIdx.x * warp.meta_group_size() + warp.meta_group_rank();
+    if(idx >= N) {
+        return;
+    }
+
+    // the row of input that this group of threads is responsible for
+    const float* x = inp + idx * C;
+
+    // mean
+    float sum = 0.0f;
+    for (int i = warp.thread_rank(); i < C; i += warp.size()) {
+        sum += x[i];
+    }
+    sum = warpReduceSum(warp, sum);
+
+    float m = sum / C;
+    if(warp.thread_rank() == 0 && mean != nullptr) {
+        __stcs(mean + idx, m);
+    }
+
+    // rstd
+    sum = 0.0f;
+    for (int i = warp.thread_rank(); i < C; i += warp.size()) {
+        float diff = x[i] - m;
+        sum += diff * diff;
+    }
+    sum = warpReduceSum(warp, sum);
+    float s = rsqrtf(sum / C + 1e-5f);
+    if(warp.thread_rank() == 0 && rstd != nullptr) {
+        __stcs(rstd + idx, s);
+    }
+
+    // final normalization and scaling by weight/bias
+    float* o = out + idx * C;
+    for (int c = warp.thread_rank(); c < C; c += warp.size()) {
+        // load and store using the .cs "streaming" hint to the compiler,
+        // indicating that this data will not be reused soon, and can be streamed through the caches
+        // this allows the threads to get more cache-hits for the (shared) weight and bias parameters
+        float n = s * (__ldcs(x+c) - m);
+        __stcs(o+c, n * weight[c] + bias[c]);
+    }
+}
+
+
+// same as kernel 1 but uses var(x) == mean(x**2) - mean(x)**2
+__global__ void layernorm_forward_kernel2(float* __restrict__ out, float* __restrict__ mean, float* __restrict__ rstd,
+                                    const float*  __restrict__ inp, const float*  __restrict__ weight,
+                                    const float* __restrict__ bias, int N, int C) {
+    namespace cg = cooperative_groups;
+    cg::thread_block block = cg::this_thread_block();
+    cg::thread_block_tile<32> warp = cg::tiled_partition<32>(block);
+    int idx = blockIdx.x * warp.meta_group_size() + warp.meta_group_rank();
+    if(idx >= N) {
+        return;
+    }
+
+    // the row of input that this group of threads is responsible for
+    const float* x = inp + idx * C;
+
+    // thread coarsening through the row, reduce the sum in series
+    float sum = 0.0; // stores sum(x)
+    float sum2 = 0.0; // stores sum(x**2)
+    for (int i = warp.thread_rank(); i < C; i += warp.size()) {
+        float xi = x[i];
+        sum += xi;
+        sum2 += xi * xi;
+    }
+    // warp-level reduction at the end
+    sum = warpReduceSum(warp, sum);
+    sum2 = warpReduceSum(warp, sum2);
+    sum /= C; // mean(x)
+    sum2 /= C; // mean(x**2)
+
+    // mean, var, rstd
+    float m = sum;
+    float var = sum2 - sum * sum;
+    float s = rsqrtf(var + 1e-5f);
+
+    // store the mean, no need to cache it
+    if(warp.thread_rank() == 0 && mean != nullptr) {
+        __stcs(mean + idx, m);
+    }
+    // store the rstd, no need to cache it
+    if(warp.thread_rank() == 0 && rstd != nullptr) {
+        __stcs(rstd + idx, s);
+    }
+    // final normalization and scaling by weight/bias
+    float* o = out + idx * C;
+    for (int c = warp.thread_rank(); c < C; c += warp.size()) {
+        float n = s * (__ldcs(x+c) - m);
+        __stcs(o+c, n * weight[c] + bias[c]);
+    }
+}
+
+void layernorm_forward1(float* out, float* mean, float* rstd,
+                       const float* inp, const float* weight, const float* bias,
+                       int B, int T, int C,
+                       const int block_size) {
+    assert(block_size % 32 == 0);
+    const int N = B * T;
+    const int grid_size = ceil_div(N * 32, block_size);
+    layernorm_forward_kernel1<<<grid_size, block_size>>>(out, mean, rstd, inp, weight, bias, N, C);
+    hipCheck(hipDeviceSynchronize());
+}
+
+void layernorm_forward2(float* out, float* mean, float* rstd,
+                       const float* inp, const float* weight, const float* bias,
+                       int B, int T, int C,
+                       const int block_size) {
+    assert(block_size % 32 == 0);
+    const int N = B * T;
+    const int grid_size = ceil_div(N * 32, block_size);
+    layernorm_forward_kernel2<<<grid_size, block_size>>>(out, mean, rstd, inp, weight, bias, N, C);
+    hipCheck(hipDeviceSynchronize());
+}
+
+
+// kernel version dispatch
+void layernorm_forward(int kernel_num,
+                    float* out, float* mean, float* rstd,
+                    const float* inp, const float* weight, const float* bias,
+                    int B, int T, int C,
+                    const int block_size) {
+    switch (kernel_num) {
+        case 1:
+            layernorm_forward1(out, mean, rstd, inp, weight, bias, B, T, C, block_size);
+            break;
+        case 2:
+            layernorm_forward2(out, mean, rstd, inp, weight, bias, B, T, C, block_size);
+            break;
+        default:
+            printf("Invalid kernel number\n");
+            exit(1);
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+int main(int argc, char **argv) {
+    srand(0);
+
+    int B = 8;
+    int T = 1024;
+    int C = 768;
+
+    // create host memory of random numbers
+    float* out = (float*)malloc(B * T * C * sizeof(float));
+    float* mean = (float*)malloc(B * T * sizeof(float));
+    float* rstd = (float*)malloc(B * T * sizeof(float));
+    float* inp = make_random_float(B * T * C);
+    float* weight = make_random_float(C);
+    float* bias = make_random_float(C);
+
+    // move to GPU
+    float* d_out;
+    float* d_mean;
+    float* d_rstd;
+    float* d_inp;
+    float* d_weight;
+    float* d_bias;
+    hipCheck(hipMalloc(&d_out, B * T * C * sizeof(float)));
+    hipCheck(hipMalloc(&d_mean, B * T * sizeof(float)));
+    hipCheck(hipMalloc(&d_rstd, B * T * sizeof(float)));
+    hipCheck(hipMalloc(&d_inp, B * T * C * sizeof(float)));
+    hipCheck(hipMalloc(&d_weight, C * sizeof(float)));
+    hipCheck(hipMalloc(&d_bias, C * sizeof(float)));
+    hipCheck(hipMemcpy(d_inp, inp, B * T * C * sizeof(float), hipMemcpyHostToDevice));
+    hipCheck(hipMemcpy(d_weight, weight, C * sizeof(float), hipMemcpyHostToDevice));
+    hipCheck(hipMemcpy(d_bias, bias, C * sizeof(float), hipMemcpyHostToDevice));
+
+    // read kernel_num from command line
+    int kernel_num = 2;
+    if (argc > 1) {
+        kernel_num = atoi(argv[1]);
+    }
+    printf("Using kernel %d\n", kernel_num);
+
+    int block_sizes[] = {32, 64, 128, 256, 512, 1024};
+
+    layernorm_forward_cpu(out, mean, rstd, inp, weight, bias, B, T, C);
+
+    // check the correctness of the kernel at all block sizes
+    for (int j = 0; j < sizeof(block_sizes) / sizeof(int); j++) {
+        int block_size = block_sizes[j];
+        printf("Checking block size %d.\n", block_size);
+
+        layernorm_forward(kernel_num, d_out, d_mean, d_rstd, d_inp, d_weight, d_bias, B, T, C, block_size);
+
+        validate_result(d_out, out, "out", B * T * C, 1e-5f);
+        validate_result(d_mean, mean, "mean", B * T, 1e-5f);
+        validate_result(d_rstd, rstd, "rstd", B * T, 1e-5f);
+    }
+
+    printf("All results match. Starting benchmarks.\n\n");
+
+    // time the kernel at different block sizes
+    for (int j = 0; j < sizeof(block_sizes) / sizeof(int); j++) {
+        int block_size = block_sizes[j];
+
+        int repeat_times = 2000;
+        float elapsed_time = benchmark_kernel(repeat_times, layernorm_forward,
+                                              kernel_num, d_out, d_mean, d_rstd, d_inp, d_weight, d_bias,
+                                              B, T, C, block_size);
+
+        // estimate the memory bandwidth achieved
+        // e.g. A100 40GB PCIe is advertised at 1,555GB/s
+        long memory_ops = (2 * B * T * C) * 4; // *4 for float
+        float memory_bandwidth = memory_ops / elapsed_time / 1e6;
+
+        printf("block_size %4d | time %.4f ms | bandwidth %.2f GB/s\n", block_size, elapsed_time, memory_bandwidth);
+    }
+
+    // free memory
+    free(out);
+    free(mean);
+    free(rstd);
+    free(inp);
+    free(weight);
+    free(bias);
+    hipCheck(hipFree(d_out));
+    hipCheck(hipFree(d_mean));
+    hipCheck(hipFree(d_rstd));
+    hipCheck(hipFree(d_inp));
+    hipCheck(hipFree(d_weight));
+    hipCheck(hipFree(d_bias));
+
+    return 0;
+}
