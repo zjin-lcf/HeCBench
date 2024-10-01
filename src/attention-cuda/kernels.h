8d676cc8198d79c189198919bf2dc0cdebf5f334
Zheming Jin <zjin-lcf@users.noreply.github.com>
2024-10-01 15:18:28 -0700

    [attention] add optimized implementations and testcase

---
+++ src/attention-cuda/kernels.h
@@ -0,0 +1,125 @@
+#include <cub/cub.cuh>
+#include <cooperative_groups.h>
+#include <cooperative_groups/reduce.h>
+
+__global__
+void kernel1 (
+    const float*__restrict__ key,
+    const float*__restrict__ query,
+    float*__restrict__ dot_product,
+    float*__restrict__ exp_sum,
+    const int n,
+    const int d)
+{
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < n) {
+    float sum = 0;
+    for (int j = 0; j < d; j++)
+      sum += key[i * d + j] * query[j];
+    dot_product[i] = sum;
+    atomicAdd(exp_sum, expf(sum));
+  }
+}
+
+__global__
+void kernel2 (
+    const float*__restrict__ exp_sum,
+    const float*__restrict__ dot_product,
+    float*__restrict__ score,
+    const int n)
+{
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < n)
+    score[i] = expf(dot_product[i]) / exp_sum[0];
+}
+
+__global__
+void kernel3 (
+    const float*__restrict__ score,
+    const float*__restrict__ value,
+    float*__restrict__ output,
+    const int n,
+    const int d)
+{
+  int j = blockIdx.x * blockDim.x + threadIdx.x;
+  if (j < d) {
+    float sum = 0;
+    for (int i = 0; i < n; i++)
+      sum += score[i] * value[i * d + j];
+    output[j] = sum;
+  }
+}
+
+__global__
+void kernel1_blockReduce (
+    const float*__restrict__ key,
+    const float*__restrict__ query,
+    float*__restrict__ dot_product,
+    float*__restrict__ exp_sum,
+    const int n,
+    const int d)
+{
+  // each i iteration is assigned to a block
+  int i = blockIdx.x;
+  float sum = 0;
+  for (int j = threadIdx.x; j < d; j += blockDim.x) {
+    sum += key[i * d + j] * query[j];
+  }
+  using BlockReduce = cub::BlockReduce<float, 256>;
+  __shared__ typename BlockReduce::TempStorage temp_storage;
+  sum = BlockReduce(temp_storage).Sum(sum);
+  if (threadIdx.x == 0) {
+    dot_product[i] = sum;
+    atomicAdd(exp_sum, expf(sum));
+  }
+}
+
+__global__
+void kernel1_warpReduce (
+    const float*__restrict__ key,
+    const float*__restrict__ query,
+    float*__restrict__ dot_product,
+    float*__restrict__ exp_sum,
+    const int n,
+    const int d)
+{
+  namespace cg = cooperative_groups;
+  cg::thread_block block = cg::this_thread_block();
+  cg::thread_block_tile<32> warp = cg::tiled_partition<32>(block);
+  // each i iteration is assigned to a warp
+  // meta_group_size is the number of warps in a block, and meta_group_rank is the warp index
+  int i = blockIdx.x * warp.meta_group_size() + warp.meta_group_rank();
+  if (i < n) {
+    float sum = 0;
+    for (int j = warp.thread_rank(); j < d; j += warp.size()) {
+      sum += key[i * d + j] * query[j];
+    }
+    sum = cg::reduce(warp, sum, cg::plus<float>{});
+    if (warp.thread_rank() == 0) {
+      dot_product[i] = sum;
+      atomicAdd(exp_sum, expf(sum));
+    }
+  }
+}
+
+__global__
+void kernel2_blockReduce (
+    const float*__restrict__ exp_sum,
+    const float*__restrict__ dot_product,
+    const float*__restrict__ value,
+    float*__restrict__ output,
+    const int n,
+    const int d)
+{
+  int j = blockIdx.x;
+  float sum = 0;
+  for (int i = threadIdx.x; i < n; i += blockDim.x) {
+    float score = expf(dot_product[i]) / exp_sum[0];
+    sum += score * value[i * d + j];
+  }
+  using BlockReduce = cub::BlockReduce<float, 256>;
+  __shared__ typename BlockReduce::TempStorage temp_storage;
+  sum = BlockReduce(temp_storage).Sum(sum);
+  if (threadIdx.x == 0)
+    output[j] = sum;
+}
--- src/attention-cuda/main.cu
+++ src/attention-cuda/main.cu
@@ -4,78 +4,28 @@
 #include <chrono>
 #include <random>
 #include <cuda.h>
+#include "kernels.h"
 #include "reference.h"
 
-__global__ 
-void kernel1 (
-    const float*__restrict__ key, 
-    const float*__restrict__ query, 
-    float*__restrict__ dot_product, 
-    float*__restrict__ exp_sum, 
-    const int n,
-    const int d) 
-{
-  int i = blockIdx.x * blockDim.x + threadIdx.x;  
-  if (i < n) {
-    float sum = 0;
-    for (int j = 0; j < d; j++)
-      sum += key[i * d + j] * query[j];
-    dot_product[i] = sum;
-    atomicAdd(exp_sum, expf(sum));
-  }
-}
-
-__global__ 
-void kernel2 (
-    const float*__restrict__ exp_sum, 
-    const float*__restrict__ dot_product, 
-    float*__restrict__ score, 
-    const int n)
-{
-  int i = blockIdx.x * blockDim.x + threadIdx.x;  
-  if (i < n)
-    score[i] = expf(dot_product[i]) / exp_sum[0];
-}
-
-__global__ 
-void kernel3 (
-    const float*__restrict__ score, 
-    const float*__restrict__ value, 
-    float*__restrict__ output, 
-    const int n,
-    const int d) 
-{
-  int j = blockIdx.x * blockDim.x + threadIdx.x;  
-  if (j < d) {
-    float sum = 0;
-    for (int i = 0; i < n; i++)
-      sum += score[i] * value[i * d + j];
-    output[j] = sum;
-  }
-}
-
 float* attention_device(const float* key, const float* value, const float* query,
-                        const int n, const int d, const int repeat) 
+                        const int n, const int d, const int impl_num, const int repeat)
 {
   // input
   float *d_key;
-  cudaMalloc((void**)&d_key, n * d * sizeof(float)); 
-  cudaMemcpy(d_key, key, n * d * sizeof(float), cudaMemcpyHostToDevice); 
+  cudaMalloc((void**)&d_key, n * d * sizeof(float));
+  cudaMemcpy(d_key, key, n * d * sizeof(float), cudaMemcpyHostToDevice);
 
   float *d_value;
-  cudaMalloc((void**)&d_value, n * d * sizeof(float)); 
-  cudaMemcpy(d_value, value, n * d * sizeof(float), cudaMemcpyHostToDevice); 
+  cudaMalloc((void**)&d_value, n * d * sizeof(float));
+  cudaMemcpy(d_value, value, n * d * sizeof(float), cudaMemcpyHostToDevice);
 
   float *d_query;
-  cudaMalloc((void**)&d_query, d * sizeof(float)); 
-  cudaMemcpy(d_query, query, d * sizeof(float), cudaMemcpyHostToDevice); 
+  cudaMalloc((void**)&d_query, d * sizeof(float));
+  cudaMemcpy(d_query, query, d * sizeof(float), cudaMemcpyHostToDevice);
 
   // intermediate
   float *d_dot_product;
   cudaMalloc((void**)&d_dot_product, n * sizeof(float));
-
-  float *d_score;
-  cudaMalloc((void**)&d_score, n * sizeof(float));
 
   float *d_exp_sum;
   cudaMalloc((void**)&d_exp_sum, sizeof(float));
@@ -85,31 +35,61 @@ float* attention_device(const float* key, const float* value, const float* query
   float *d_output;
   cudaMalloc((void**)&d_output, d * sizeof(float));
 
-  dim3 n_grid((n+255)/256);
-  dim3 n_block(256);
-  dim3 d_grid((d+255)/256);
-  dim3 d_block(256);
-
   cudaDeviceSynchronize();
-  auto start = std::chrono::steady_clock::now();
 
-  for (int k = 0; k < repeat; k++) {
-    cudaMemset(d_exp_sum, 0, 4);
+  if (impl_num == 2) {
 
-    kernel1<<<n_grid, n_block>>>(d_key, d_query, d_dot_product, d_exp_sum, n, d);
+    auto start = std::chrono::steady_clock::now();
 
-    kernel2<<<n_grid, n_block>>>(d_exp_sum, d_dot_product, d_score, n);
+    for (int k = 0; k < repeat; k++) {
+      cudaMemset(d_exp_sum, 0, 4);
+      kernel1_warpReduce<<<(n+7)/8, 256>>>(d_key, d_query, d_dot_product, d_exp_sum, n, d);
+      kernel2_blockReduce<<<d, 256>>>(d_exp_sum, d_dot_product, d_value, d_output, n, d);
+    }
 
-    kernel3<<<d_grid, d_block>>>(d_score, d_value, d_output, n, d);
+    cudaDeviceSynchronize();
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Average execution time of kernels %f (ms)\n", time * 1e-6f / repeat);
   }
 
-  cudaDeviceSynchronize();
-  auto end = std::chrono::steady_clock::now();
-  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  printf("Average execution time of kernels %f (ms)\n", time * 1e-6f / repeat);
+  else if (impl_num == 1) {
+
+    auto start = std::chrono::steady_clock::now();
+
+    for (int k = 0; k < repeat; k++) {
+      cudaMemset(d_exp_sum, 0, 4);
+      kernel1_blockReduce<<<n, 256>>>(d_key, d_query, d_dot_product, d_exp_sum, n, d);
+      kernel2_blockReduce<<<d, 256>>>(d_exp_sum, d_dot_product, d_value, d_output, n, d);
+    }
+
+    cudaDeviceSynchronize();
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Average execution time of kernels %f (ms)\n", time * 1e-6f / repeat);
+  }
+
+  else {
+    float *d_score;
+    cudaMalloc((void**)&d_score, n * sizeof(float));
+
+    auto start = std::chrono::steady_clock::now();
+
+    for (int k = 0; k < repeat; k++) {
+      cudaMemset(d_exp_sum, 0, 4);
+      kernel1<<<(n+255)/256, 256>>>(d_key, d_query, d_dot_product, d_exp_sum, n, d);
+      kernel2<<<(n+255)/256, 256>>>(d_exp_sum, d_dot_product, d_score, n);
+      kernel3<<<(d+255)/256, 256>>>(d_score, d_value, d_output, n, d);
+    }
+
+    cudaDeviceSynchronize();
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Average execution time of kernels %f (ms)\n", time * 1e-6f / repeat);
+    cudaFree(d_score);
+  }
 
   cudaMemcpy(output, d_output, d * sizeof(float), cudaMemcpyDeviceToHost);
-  cudaFree(d_score);
   cudaFree(d_value);
   cudaFree(d_output);
   cudaFree(d_key);
@@ -119,13 +99,17 @@ float* attention_device(const float* key, const float* value, const float* query
 }
 
 int main(int argc, char* argv[]) {
-  if (argc != 4) {
-    printf("Usage: %s <rows> <columns> <repeat>\n", argv[0]);
+  if (argc != 5) {
+    printf("Usage: %s <rows> <columns> <implementation> <repeat>\n", argv[0]);
+    printf("implementation 0: naive\n");
+    printf("implementation 1: fused kernels with warp reduce\n");
+    printf("implementation 2: fused kernels with block reduce\n");
     return 1;
   }
   const int n = atoi(argv[1]);
   const int d = atoi(argv[2]);
-  const int r = atoi(argv[3]);
+  const int k = atoi(argv[3]);
+  const int r = atoi(argv[4]);
 
   // input
   float* key = (float*) malloc (n * d * sizeof(float));
@@ -143,11 +127,12 @@ int main(int argc, char* argv[]) {
 
   float* hout = attention_host(key, value, query, n, d);
 
-  float* dout = attention_device(key, value, query, n, d, r);
+  float* dout = attention_device(key, value, query, n, d, k, r);
 
   float rmse = 0;
-  for (int i = 0; i < d; i++) 
+  for (int i = 0; i < d; i++) {
     rmse += (hout[i] - dout[i]) * (hout[i] - dout[i]);
+  }
   printf("RMSE = %f\n", sqrtf(rmse / d));
 
   free(key);
