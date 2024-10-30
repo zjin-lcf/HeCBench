35ee975b030e5a1493698deb2facbdc7e2f857c7
Zheming Jin <zjin-lcf@users.noreply.github.com>
2024-10-30 08:17:27 -0700

    [f16sp] the compute type of kernel1 is FP16 and the compute type of kernel2 and kernel3 is FP32; add the library-based FP16 dot products as references

--- src/f16sp-cuda/kernels.h
+++ src/f16sp-cuda/kernels.h
@@ -1,6 +1,7 @@
 #define NUM_OF_BLOCKS (1024 * 1024)
 #define NUM_OF_THREADS 128
 
+// fp16 reduction
 __forceinline__ __device__
 void reduceInShared_intrinsics(half2 * const v)
 {
@@ -12,16 +13,21 @@ void reduceInShared_intrinsics(half2 * const v)
   }
 }
 
+// fp32 reduction
 __forceinline__ __device__
-void reduceInShared_native(half2 * const v)
+void reduceInShared_native(float2 * const v)
 {
   int lid = threadIdx.x;
   #pragma unroll
   for (int i = NUM_OF_THREADS/2; i >= 1; i = i / 2) {
-    if(lid<i) v[lid] = v[lid] + v[lid+i];
+    if(lid<i) {
+      v[lid].x += v[lid+i].x;
+      v[lid].y += v[lid+i].y;
+    }
     __syncthreads();
   }
 }
+
 
 __global__
 void scalarProductKernel_intrinsics(
@@ -52,22 +58,23 @@ void scalarProductKernel_intrinsics(
   }
 }
 
+// compute type is float for high precision
 __global__
-void scalarProductKernel_native(
+void scalarProductKernel_native_fp32(
     half2 const *__restrict__ const a,
     half2 const *__restrict__ const b,
     float *__restrict__ const results,
     size_t const size)
 {
   const int stride = gridDim.x*blockDim.x;
-  __shared__ half2 shArray[NUM_OF_THREADS];
+  __shared__ float2 shArray[NUM_OF_THREADS];
 
-  half2 value(0.f, 0.f);
-  shArray[threadIdx.x] = value;
+  float2 value = {0.f, 0.f};
 
   for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < size; i+=stride)
   {
-    value += a[i] * b[i];
+    value.x += (float)a[i].x * (float)b[i].x;
+    value.y += (float)a[i].y * (float)b[i].y;
   }
 
   shArray[threadIdx.x] = value;
@@ -76,70 +83,37 @@ void scalarProductKernel_native(
 
   if (threadIdx.x == 0)
   {
-    half2 result = shArray[0];
-    float f_result = (float)result.y + (float)result.x;
-    atomicAdd(results, f_result);
+    float2 result = shArray[0];
+    atomicAdd(results, result.x + result.y);
   }
 }
 
+
+// CUB reduction with float2 seems unsupported 
 __global__
-void scalarProductKernel_intrinsics2(
+void scalarProductKernel_native2_fp32(
     half2 const *__restrict__ const a,
     half2 const *__restrict__ const b,
     float *__restrict__ const results,
     size_t const size)
 {
-  typedef cub::BlockReduce<half2, NUM_OF_THREADS> BlockReduce;
-  __shared__ typename BlockReduce::TempStorage temp_storage;
-
   const int stride = gridDim.x*blockDim.x;
 
-  half2 value = __float2half2_rn(0.f);
+  typedef cub::BlockReduce<float, NUM_OF_THREADS> BlockReduce;
+  __shared__ typename BlockReduce::TempStorage temp_storage;
+
+  float2 value = {0.f, 0.f};
 
   for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < size; i+=stride)
   {
-    value = __hfma2(a[i], b[i], value);
+    value.x += (float)a[i].x * (float)b[i].x;
+    value.y += (float)a[i].y * (float)b[i].y;
   }
 
-  value = BlockReduce(temp_storage).Sum(value);
+  value.x = BlockReduce(temp_storage).Sum(value.x + value.y);
 
   if (threadIdx.x == 0)
   {
-    float f_result = __low2float(value) + __high2float(value);
-    atomicAdd(results, f_result);
+    atomicAdd(results, value.x);
   }
 }
-
-__global__
-void scalarProductKernel_intrinsics3(
-    float4 const *__restrict__ const a,
-    float4 const *__restrict__ const b,
-    float *__restrict__ const results,
-    size_t const size)
-{
-  typedef cub::BlockReduce<half2, NUM_OF_THREADS> BlockReduce;
-  __shared__ typename BlockReduce::TempStorage temp_storage;
-
-  const int stride = gridDim.x*blockDim.x;
-
-  half2 value = __float2half2_rn(0.f);
-
-  for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < size/4; i+=stride)
-  {
-    const half2 *a_v = reinterpret_cast<const half2*>(&a[i]);
-    const half2 *b_v = reinterpret_cast<const half2*>(&b[i]);
-    value = __hfma2(a_v[0], b_v[0], value);
-    value = __hfma2(a_v[1], b_v[1], value);
-    value = __hfma2(a_v[2], b_v[2], value);
-    value = __hfma2(a_v[3], b_v[3], value);
-  }
-
-  value = BlockReduce(temp_storage).Sum(value);
-
-  if (threadIdx.x == 0)
-  {
-    float f_result = __low2float(value) + __high2float(value);
-    atomicAdd(results, f_result);
-  }
-}
-
--- src/f16sp-sycl/kernels.h
+++ src/f16sp-sycl/kernels.h
@@ -12,6 +12,18 @@ void reduceInShared_native(sycl::half2 *const v, sycl::nd_item<1> &item)
   }
 }
 
+inline
+void reduceInShared_native(sycl::float2 *const v, sycl::nd_item<1> &item)
+{
+  int lid = item.get_local_id(0);
+  #pragma unroll
+  for (int i = NUM_OF_THREADS/2; i >= 1; i = i / 2) {
+    if(lid<i) v[lid] = v[lid] + v[lid+i];
+    item.barrier(sycl::access::fence_space::local_space);
+  }
+}
+
+
 void scalarProductKernel_native(const sycl::half2 *a,
                                 const sycl::half2 *b,
                                 float *results,
@@ -20,7 +32,6 @@ void scalarProductKernel_native(const sycl::half2 *a,
                                 sycl::nd_item<1> &item)
 {
   int lid = item.get_local_id(0);
-  int gid = item.get_group(0);
 
   const int stride = item.get_group_range(0) * item.get_local_range(0);
 
@@ -47,69 +58,66 @@ void scalarProductKernel_native(const sycl::half2 *a,
   }
 }
 
-void scalarProductKernel_native2(const sycl::half2 *a,
-                                 const sycl::half2 *b,
-                                 float *results,
-                                 const size_t size,
-                                 sycl::nd_item<1> &item)
+void scalarProductKernel_native_fp32(const sycl::half2 *a,
+                                     const sycl::half2 *b,
+                                     float *results,
+                                           sycl::float2 *shArray,
+                                     const size_t size,
+                                     sycl::nd_item<1> &item)
 {
   int lid = item.get_local_id(0);
-  int gid = item.get_group(0);
 
   const int stride = item.get_group_range(0) * item.get_local_range(0);
 
-  sycl::half2 value(0.f, 0.f);
+  sycl::float2 value(0.f, 0.f);
+  shArray[lid] = value;
 
   for (int i = item.get_global_id(0); i < size; i += stride)
   {
-    value += a[i] * b[i];
+    value.x() += (float)a[i].x() * (float)b[i].x();
+    value.y() += (float)a[i].y() * (float)b[i].y();
   }
 
-  value = sycl::reduce_over_group(item.get_group(), value, sycl::plus<sycl::half2>{});
+  shArray[lid] = value;
+  item.barrier(sycl::access::fence_space::local_space);
+  reduceInShared_native(shArray, item);
 
   if (lid == 0)
   {
-    float f_result = (float)value.y() + (float)value.x();
+    sycl::float2 result = shArray[0];
     auto ao = sycl::atomic_ref<float, sycl::memory_order::relaxed, \
                      sycl::memory_scope::device,\
                      sycl::access::address_space::global_space>(results[0]);
-    ao.fetch_add(f_result);
+    ao.fetch_add(result.x() + result.y());
   }
 }
 
-void scalarProductKernel_native3(const sycl::float4 *a,
-                                 const sycl::float4 *b,
-                                 float *results,
-                                 const size_t size,
-                                 sycl::nd_item<1> &item)
+void scalarProductKernel_native2_fp32(const sycl::half2 *a,
+                                      const sycl::half2 *b,
+                                      float *results,
+                                      const size_t size,
+                                      sycl::nd_item<1> &item)
 {
   int lid = item.get_local_id(0);
-  int gid = item.get_group(0);
 
   const int stride = item.get_group_range(0) * item.get_local_range(0);
 
-  sycl::half2 value(0.f, 0.f);
+  sycl::float2 value(0.f, 0.f);
 
-  for (int i = item.get_global_id(0); i < size/4; i += stride)
+  for (int i = item.get_global_id(0); i < size; i += stride)
   {
-    const sycl::half2 *a_v = reinterpret_cast<const sycl::half2*>(&a[i]);
-    const sycl::half2 *b_v = reinterpret_cast<const sycl::half2*>(&b[i]);
-    value += a_v[0] * b_v[0];
-    value += a_v[1] * b_v[1];
-    value += a_v[2] * b_v[2];
-    value += a_v[3] * b_v[3];
+    value.x() += (float)a[i].x() * (float)b[i].x();
+    value.y() += (float)a[i].y() * (float)b[i].y();
   }
 
-  value = sycl::reduce_over_group(item.get_group(), value, sycl::plus<sycl::half2>{});
+  value = sycl::reduce_over_group(item.get_group(), value, sycl::plus<sycl::float2>{});
 
   if (lid == 0)
   {
-    float f_result = (float)value.y() + (float)value.x();
     auto ao = sycl::atomic_ref<float, sycl::memory_order::relaxed, \
                      sycl::memory_scope::device,\
                      sycl::access::address_space::global_space>(results[0]);
-    ao.fetch_add(f_result);
+    ao.fetch_add(value.x() + value.y());
   }
 }
-
 
