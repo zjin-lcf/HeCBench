de3a4550957410c5abb1d7a02fc43861f47b7f6a
Zheming Jin <zjin-lcf@users.noreply.github.com>
2024-11-29 08:38:04 -0800

    [addBiasQKV] add the CUDA and HIP examples using the fp8 for data storage and bfloat16 for compute

---
+++ src/addBiasQKV-hip/main.cu
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2019-2023, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <chrono>
+#include <cstdio>
+#include <hip/hip_runtime.h>
+#include <hip/hip_fp16.h>
+#include <hip/hip_fp8.h>
+
+// scale = AMAX(tensor) / FP8_MAX
+// A (original) = A_scaled (fp8) * "scale of A"
+
+template<typename T1, typename T2>
+struct FP8TrtAddQKVBiasParam {
+    T1*          qkv_tgt;
+    const T1*    qkv_src;
+    const T2*    qkv_bias;
+    const float* input_scale;
+    const float* output_scale;
+    const int    valid_word_num;
+    const int    head_num;
+    const int    size_per_head;
+    const int    hidden_unit;
+};
+
+inline __device__ __hip_bfloat162 hmul2(__hip_bfloat162 x, __hip_bfloat162 y) {
+  return __hmul2(x, y);
+}
+inline __device__ __hip_bfloat162 hadd2(__hip_bfloat162 x, __hip_bfloat162 y) {
+  return __hadd2(x, y);
+}
+
+// not necessarily defined in HIP
+inline __device__ __hip_bfloat162 __float2bfloat162_rn(const float a) {
+  return __hip_bfloat162{__float2bfloat16(a), __float2bfloat16(a)};
+}
+
+inline __device__ __hip_bfloat162 float_to_bfloat2(float val) {
+  return __float2bfloat162_rn(val);
+}
+
+inline __device__ void fp8x4_e4m3_fnuz_to_bfloat2(__hip_bfloat162* out1, __hip_bfloat162* out2, const __hip_fp8x4_e4m3_fnuz* in)
+{
+  const char4 tmp_val = reinterpret_cast<const char4*>(in)[0];
+  *out1 = __hip_bfloat162((float)reinterpret_cast<const __hip_fp8_e4m3_fnuz*>(&tmp_val.x)[0],
+                          (float)reinterpret_cast<const __hip_fp8_e4m3_fnuz*>(&tmp_val.y)[0]);
+  *out2 = __hip_bfloat162((float)reinterpret_cast<const __hip_fp8_e4m3_fnuz*>(&tmp_val.z)[0],
+                          (float)reinterpret_cast<const __hip_fp8_e4m3_fnuz*>(&tmp_val.w)[0]);
+}
+
+
+__global__ void FP8TrtAddQKVBiasKernel(FP8TrtAddQKVBiasParam<__hip_fp8_e4m3_fnuz, __hip_bfloat16> param)
+{
+    // Add bias ([3, head, size]), and then transpose from
+    // [valid_word_num, 3, head, size] -> [valid_word_num, head, 3, size]
+
+    using T1_4 = __hip_fp8x4_e4m3_fnuz;
+    using T2_2 = __hip_bfloat162;
+
+    const T1_4* qkv_src_ptr = (T1_4*)(param.qkv_src + blockIdx.x * 3 * param.hidden_unit);
+    const T2_2* bias_ptr    = (T2_2*)param.qkv_bias;
+    T1_4*       qkv_tgt_ptr = (T1_4*)(param.qkv_tgt + blockIdx.x * 3 * param.hidden_unit);
+
+    const int size_div_4   = param.size_per_head / 4;
+    const int hidden_div_4 = param.hidden_unit / 4;
+    const int src_id       = threadIdx.z * hidden_div_4 + threadIdx.y * size_div_4 + threadIdx.x;
+
+    T2_2 val1, val2;
+    fp8x4_e4m3_fnuz_to_bfloat2(&val1, &val2, &qkv_src_ptr[src_id]);
+    T2_2      input_scale_2  = float_to_bfloat2(__ldg(param.input_scale)); 
+    T2_2      output_scale_2 = float_to_bfloat2(__ldg(param.output_scale));
+    const int bias_id_0      = src_id * 2;
+    val1                     = hmul2(hadd2(hmul2(val1, input_scale_2), bias_ptr[bias_id_0]), output_scale_2);
+    val2                     = hmul2(hadd2(hmul2(val2, input_scale_2), bias_ptr[bias_id_0 + 1]), output_scale_2);
+
+    qkv_tgt_ptr[(threadIdx.y * 3 * size_div_4 + threadIdx.z * size_div_4) + threadIdx.x] = __hip_fp8x4_e4m3_fnuz(val1, val2);
+}
+
+template<typename T1, typename T2>
+void addBias(int batch_size, int seq_len, int hidden_units, int head_num, int repeat) {
+
+  int m = batch_size * seq_len;
+
+  T1 *qkv_buf, *q_buf;
+  hipMalloc(&qkv_buf, sizeof(T1) * m * 3 * hidden_units);
+  hipMalloc(&q_buf, sizeof(T1) * m * 3 * hidden_units);
+
+  int size_per_head = hidden_units / head_num;
+  int bias_size = 3 * hidden_units;
+
+  T2 *bias;
+  float *oscale, *oscale_inv;
+
+  hipMalloc(&bias, sizeof(T2) * bias_size);
+  hipMalloc(&oscale, sizeof(float));
+  hipMalloc(&oscale_inv, sizeof(float));
+ 
+  FP8TrtAddQKVBiasParam<T1, T2> param{q_buf,
+                                      qkv_buf,
+                                      bias,   //att_query_weight_bias,
+                                      oscale, //att_query_weight_output_scale,
+                                      oscale_inv, //att_query_weight_output_scale_inv,
+                                      m,
+                                      head_num,
+                                      size_per_head,
+                                      head_num * size_per_head};
+
+  dim3 grid(param.valid_word_num);
+  dim3 block(param.size_per_head / 4, param.head_num, 3);
+
+  // warmup
+  for (int i = 0; i < repeat; i++)
+    FP8TrtAddQKVBiasKernel<<<grid, block>>>(param);
+
+  hipDeviceSynchronize();
+  auto start = std::chrono::high_resolution_clock::now();
+
+  for (int i = 0; i < repeat; i++)
+    FP8TrtAddQKVBiasKernel<<<grid, block>>>(param);
+
+  hipDeviceSynchronize();
+  auto end = std::chrono::high_resolution_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  printf("Average execution time of the kernel: %f (us)\n", (time * 1e-3f) / repeat);
+
+  hipFree(qkv_buf);
+  hipFree(q_buf);
+  hipFree(bias);
+  hipFree(oscale);
+  hipFree(oscale_inv);
+}
+
+int main(int argc, char **argv) {
+  int batch_size = 8;
+  int seq_len = 1024;
+  int hidden_units = 768;
+  int head_num = 12;
+  int repeat = 1000;
+  addBias<__hip_fp8_e4m3_fnuz, __hip_bfloat16>(batch_size, seq_len, hidden_units, head_num, repeat);
+  return 0;
+}
