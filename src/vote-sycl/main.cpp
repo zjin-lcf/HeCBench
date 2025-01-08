bb77db6ff1f0b09ae7bb67211e548cf97942b661
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-01-08 15:12:21 -0800

    [vote-sycl] make the program portable

--- src/vote-sycl/main.cpp
+++ src/vote-sycl/main.cpp
@@ -5,20 +5,13 @@
 #include "reference.h"
 #include "kernels.h"
 
-// WARP_SIZE is defined in Makefile
-
 int main(int argc, char **argv) {
-  if (argc != 3) {
-    printf("Usage: %s <warp_size> <repeat>\n", argv[0]);
+  if (argc != 2) {
+    printf("Usage: %s <repeat>\n", argv[0]);
     return 1;
   }
 
-  const int warp_size = atoi(argv[1]);
-  if (warp_size != WARP_SIZE) {
-    printf("The warp size should be %d\n", WARP_SIZE);
-    return 1;
-  }
-  const int repeat = atoi(argv[2]);
+  const int repeat = atoi(argv[1]);
 
   bool *dinfo = NULL, *hinfo = NULL;
   int error_count[3] = {0, 0, 0};
@@ -26,17 +19,20 @@ int main(int argc, char **argv) {
   unsigned int *h_input, *h_result;
   unsigned int *d_input, *d_result;
 
-  h_input = (unsigned int *)malloc(VOTE_DATA_GROUP * warp_size *
-                                   sizeof(unsigned int));
-  h_result = (unsigned int *)malloc(VOTE_DATA_GROUP * warp_size *
-                                    sizeof(unsigned int));
-  genVoteTestPattern(h_input, VOTE_DATA_GROUP * warp_size);
-
 #ifdef USE_GPU
   sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
   sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
+  auto sg_sizes = q.get_device().get_info<sycl::info::device::sub_group_sizes>();
+  auto r = std::max_element(sg_sizes.begin(), sg_sizes.end());
+  int warp_size = *r;
+
+  h_input = (unsigned int *)malloc(VOTE_DATA_GROUP * warp_size *
+                                   sizeof(unsigned int));
+  h_result = (unsigned int *)malloc(VOTE_DATA_GROUP * warp_size *
+                                    sizeof(unsigned int));
+  genVoteTestPattern(h_input, VOTE_DATA_GROUP * warp_size);
 
   d_input = sycl::malloc_device<unsigned int>(
       VOTE_DATA_GROUP * warp_size, q);
@@ -52,21 +48,18 @@ int main(int argc, char **argv) {
 
   // Start of Vote Any Test Kernel #1
   printf("\tRunning <<Vote.Any>> kernel1 ...\n");
+  auto kernel1 = [&](sycl::handler& cgh) {
+    cgh.parallel_for(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item) {
+      VoteAnyKernel1(d_input, d_result, repeat, item);
+    });
+  };
 
   // Warmup
-
-  // reqd_sub_group_size requires a constant
-  q.parallel_for<class any_test_warmup>(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item)
-      [[sycl::reqd_sub_group_size(WARP_SIZE)]] {
-        VoteAnyKernel1(d_input, d_result, repeat, item);
-  }).wait();
+  q.submit(kernel1).wait();
 
   auto start = std::chrono::steady_clock::now();
 
-  q.parallel_for<class any_test>(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item)
-      [[sycl::reqd_sub_group_size(WARP_SIZE)]] {
-        VoteAnyKernel1(d_input, d_result, repeat, item);
-  }).wait();
+  q.submit(kernel1).wait();
 
   auto end = std::chrono::steady_clock::now();
   auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
@@ -80,19 +73,18 @@ int main(int argc, char **argv) {
 
   // Start of Vote All Test Kernel #2
   printf("\tRunning <<Vote.All>> kernel2 ...\n");
+  auto kernel2 = [&](sycl::handler& cgh) {
+    cgh.parallel_for(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item) {
+      VoteAllKernel2(d_input, d_result, repeat, item);
+    });
+  };
 
   // Warmup
-  q.parallel_for<class all_test_warmup>(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item)
-      [[sycl::reqd_sub_group_size(WARP_SIZE)]] {
-        VoteAllKernel2(d_input, d_result, repeat, item);
-  }).wait();
+  q.submit(kernel2).wait();
 
   start = std::chrono::steady_clock::now();
 
-  q.parallel_for<class all_test>(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item)
-      [[sycl::reqd_sub_group_size(WARP_SIZE)]] {
-        VoteAllKernel2(d_input, d_result, repeat, item);
-  }).wait();
+  q.submit(kernel2).wait();
 
   end = std::chrono::steady_clock::now();
   time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
@@ -108,24 +100,22 @@ int main(int argc, char **argv) {
   dinfo = (bool *)sycl::malloc_device(warp_size * 3 * 3 * sizeof(bool), q);
 
   // Warmup
-  q.parallel_for<class vote_any_warmup> (
-    sycl::nd_range<1>(sycl::range<1>(warp_size * 3),
-                      sycl::range<1>(warp_size * 3)),
-      [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(WARP_SIZE)]] {
+  auto kernel3 = [&](sycl::handler& cgh) {
+    cgh.parallel_for(sycl::nd_range<1>(sycl::range<1>(warp_size * 3),
+                                       sycl::range<1>(warp_size * 3)),
+      [=](sycl::nd_item<1> item) {
         VoteAnyKernel3(dinfo, warp_size, repeat, item);
-  }).wait();
+    });
+  };
 
+  q.submit(kernel3).wait();
   q.memset(dinfo, 0, warp_size * 3 * 3 * sizeof(bool)).wait();
 
   printf("\tRunning <<Vote.Any>> kernel3 ...\n");
 
   start = std::chrono::steady_clock::now();
 
-  q.parallel_for<class vote_any> (sycl::nd_range<1>(sycl::range<1>(warp_size * 3),
-                                                    sycl::range<1>(warp_size * 3)),
-      [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(WARP_SIZE)]] {
-        VoteAnyKernel3(dinfo, warp_size, repeat, item);
-  }).wait();
+  q.submit(kernel3).wait();
 
   end = std::chrono::steady_clock::now();
   time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
