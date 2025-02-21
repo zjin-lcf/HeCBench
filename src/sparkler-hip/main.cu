597d9f51b0dfaf8a1874ba72908d2b49a482c36a
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-02-20 18:28:50 -0800

    Add include paths to hipBlas and hipSparse libraries (https://github.com/zjin-lcf/HeCBench/pull/100)

--- src/sparkler-hip/main.cu
+++ src/sparkler-hip/main.cu
@@ -9,7 +9,7 @@
 
 #include <mpi.h>
 #include <hip/hip_runtime.h>
-#include <hipblas.h>
+#include <hipblas/hipblas.h>
 
 //=============================================================================
 
