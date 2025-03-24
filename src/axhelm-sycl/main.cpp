0e67ff1496928580d466e850c5b1d5617bee1222
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-03-24 11:17:48 -0700

    [axhelm-sycl] upgrade the SYCL program from SYCL buffers to USM

--- src/axhelm-sycl/main.cpp
+++ src/axhelm-sycl/main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <iostream>
 #include <chrono>
-#include "common.h"
+#include <cmath>
+#include <sycl/sycl.hpp>
 
 #define POLYNOMIAL_DEGREE  7
 #define p_Nggeo 7
@@ -18,9 +19,10 @@
 
 #include "meshBasis.hpp"
 
-// cpu reference 
+// cpu reference
 #include "axhelmReference.cpp"
 
+#include "axhelmKernel.h"
 
 dfloat *drandAlloc(int Nelem){
 
@@ -72,242 +74,50 @@ int main(int argc, char **argv){
 
   for(int n=0;n<Ndim;++n){
     dfloat *x = q + n*offset;
-    dfloat *Ax = Aq + n*offset; 
+    dfloat *Ax = Aq + n*offset;
     axhelmReference(Nq, Nelements, lambda1, ggeo, DrV, x, Ax);
   }
 
   auto start = std::chrono::high_resolution_clock::now();
 
-  {  // sycl scope
-
 #ifdef USE_GPU
-    gpu_selector dev_sel;
+  sycl::queue s (sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-    cpu_selector dev_sel;
+  sycl::queue s (sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-    queue Q(dev_sel);
 
-    buffer<dfloat, 1> o_ggeo (ggeo, Np*Nelements*p_Nggeo);
-    buffer<dfloat, 1> o_q    (q, Ndim*Np*Nelements);
-    buffer<dfloat, 1> o_Aq   (Ndim*Np*Nelements);
-    buffer<dfloat, 1> o_DrV  (DrV, Nq*Nq);
-    buffer<dfloat, 1> o_lambda (lambda, 2*offset);
-    o_Aq.set_final_data(q); // store the device result in the 'q' array
+  dfloat *o_ggeo, *o_q, *o_Aq, *o_DrV, *o_lambda;
+  o_ggeo = sycl::malloc_device<dfloat>(Np*Nelements*p_Nggeo, s);
+  o_q    = sycl::malloc_device<dfloat>(Ndim*Np*Nelements, s);
+  o_Aq   = sycl::malloc_device<dfloat>(Ndim*Np*Nelements, s);
+  o_DrV  = sycl::malloc_device<dfloat>(Nq*Nq, s);
+  o_lambda  = sycl::malloc_device<dfloat>(2*offset, s);
 
-    range<2> global_work_size(8, Nelements*8);
-    range<2> local_work_size(8, 8);
+  s.memcpy(o_ggeo, ggeo, Np*Nelements*p_Nggeo*sizeof(dfloat));
+  s.memcpy(o_q, q, (Ndim*Np)*Nelements*sizeof(dfloat));
+  s.memcpy(o_DrV, DrV, Nq*Nq*sizeof(dfloat));
+  s.memcpy(o_lambda, lambda, 2*offset*sizeof(dfloat));
 
-    for(int test = 0; test < Ntests; ++test) {
-      if (Ndim > 1)
-        Q.submit([&] (handler &cgh) {
-            auto ggeo = o_ggeo.get_access<sycl_read>(cgh);
-            auto D = o_DrV.get_access<sycl_read>(cgh);
-            auto lambda = o_lambda.get_access<sycl_read>(cgh);
-            auto q = o_q.get_access<sycl_read>(cgh);
-            auto Aq = o_Aq.get_access<sycl_discard_write>(cgh);
+  sycl::range<3> gws (1, 8, Nelements*8);
+  sycl::range<3> lws (1, 8, 8);
 
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_D(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_U(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_V(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_W(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_GUr(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_GUs(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_GVr(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_GVs(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_GWr(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_GWs(64, cgh);
-
-            cgh.parallel_for<class axhelm_ndim3>(nd_range<2>(global_work_size, local_work_size), [=] (nd_item<2> item) {
-                dfloat r_Ut, r_Vt, r_Wt;
-                dfloat r_U[8], r_V[8], r_W[8];
-                dfloat r_AU[8], r_AV[8], r_AW[8];
-                dfloat r_G00, r_G01, r_G02, r_G11, r_G12, r_G22, r_GwJ;
-                dfloat r_lam0, r_lam1;
-
-                int e = item.get_group(1); 
-                int j = item.get_local_id(0);
-                int i = item.get_local_id(1);
-
-                s_D[j*8+i] = D[j*8+i];
-                const int base = i + j * 8 + e * 512;
-                for (int k = 0; k < 8; k++) {
-                r_U[k] = q[base + k * 8 * 8 + 0 * offset];
-                r_V[k] = q[base + k * 8 * 8 + 1 * offset];
-                r_W[k] = q[base + k * 8 * 8 + 2 * offset];
-                r_AU[k] = 0;
-                r_AV[k] = 0;
-                r_AW[k] = 0;
-                }
-#pragma unroll 8
-                for (int k = 0; k < 8; ++k) {
-                const int id = e * 512 + k * 8 * 8 + j * 8 + i;
-                const int gbase = e * p_Nggeo * 512 + k * 8 * 8 + j * 8 + i;
-                r_G00 = ggeo[gbase + p_G00ID * 512];
-                r_G01 = ggeo[gbase + p_G01ID * 512];
-                r_G02 = ggeo[gbase + p_G02ID * 512];
-                r_G11 = ggeo[gbase + p_G11ID * 512];
-                r_G12 = ggeo[gbase + p_G12ID * 512];
-                r_G22 = ggeo[gbase + p_G22ID * 512];
-                r_GwJ = ggeo[gbase + p_GWJID * 512];
-                r_lam0 = lambda[id + 0 * offset];
-                r_lam1 = lambda[id + 1 * offset];
-                item.barrier(access::fence_space::local_space);
-                s_U[j*8+i] = r_U[k];
-                s_V[j*8+i] = r_V[k];
-                s_W[j*8+i] = r_W[k];
-                r_Ut = 0;
-                r_Vt = 0;
-                r_Wt = 0;
-#pragma unroll 8
-                for (int m = 0; m < 8; m++) {
-                  dfloat Dkm = s_D[k*8+m];
-                  r_Ut += Dkm * r_U[m];
-                  r_Vt += Dkm * r_V[m];
-                  r_Wt += Dkm * r_W[m];
-                }
-                item.barrier(access::fence_space::local_space);
-                dfloat Ur = 0, Us = 0;
-                dfloat Vr = 0, Vs = 0;
-                dfloat Wr = 0, Ws = 0;
-#pragma unroll 8
-                for (int m = 0; m < 8; m++) {
-                  dfloat Dim = s_D[i*8+m];
-                  dfloat Djm = s_D[j*8+m];
-                  Ur += Dim * s_U[j*8+m];
-                  Us += Djm * s_U[m*8+i];
-                  Vr += Dim * s_V[j*8+m];
-                  Vs += Djm * s_V[m*8+i];
-                  Wr += Dim * s_W[j*8+m];
-                  Ws += Djm * s_W[m*8+i];
-                }
-                s_GUr[j*8+i] = r_lam0 * (r_G00 * Ur + r_G01 * Us + r_G02 * r_Ut);
-                s_GVr[j*8+i] = r_lam0 * (r_G00 * Vr + r_G01 * Vs + r_G02 * r_Vt);
-                s_GWr[j*8+i] = r_lam0 * (r_G00 * Wr + r_G01 * Ws + r_G02 * r_Wt);
-                s_GUs[j*8+i] = r_lam0 * (r_G01 * Ur + r_G11 * Us + r_G12 * r_Ut);
-                s_GVs[j*8+i] = r_lam0 * (r_G01 * Vr + r_G11 * Vs + r_G12 * r_Vt);
-                s_GWs[j*8+i] = r_lam0 * (r_G01 * Wr + r_G11 * Ws + r_G12 * r_Wt);
-                r_Ut = r_lam0 * (r_G02 * Ur + r_G12 * Us + r_G22 * r_Ut);
-                r_Vt = r_lam0 * (r_G02 * Vr + r_G12 * Vs + r_G22 * r_Vt);
-                r_Wt = r_lam0 * (r_G02 * Wr + r_G12 * Ws + r_G22 * r_Wt);
-                r_AU[k] += r_GwJ * r_lam1 * r_U[k];
-                r_AV[k] += r_GwJ * r_lam1 * r_V[k];
-                r_AW[k] += r_GwJ * r_lam1 * r_W[k];
-                item.barrier(access::fence_space::local_space);
-                dfloat AUtmp = 0, AVtmp = 0, AWtmp = 0;
-#pragma unroll 8
-                for (int m = 0; m < 8; m++) {
-                  dfloat Dmi = s_D[m*8+i];
-                  dfloat Dmj = s_D[m*8+j];
-                  dfloat Dkm = s_D[k*8+m];
-                  AUtmp += Dmi * s_GUr[j*8+m];
-                  AUtmp += Dmj * s_GUs[m*8+i];
-                  AVtmp += Dmi * s_GVr[j*8+m];
-                  AVtmp += Dmj * s_GVs[m*8+i];
-                  AWtmp += Dmi * s_GWr[j*8+m];
-                  AWtmp += Dmj * s_GWs[m*8+i];
-                  r_AU[m] += Dkm * r_Ut;
-                  r_AV[m] += Dkm * r_Vt;
-                  r_AW[m] += Dkm * r_Wt;
-                }
-                r_AU[k] += AUtmp;
-                r_AV[k] += AVtmp;
-                r_AW[k] += AWtmp;
-                }
-#pragma unroll 8
-                for (int k = 0; k < 8; k++) {
-                  const int id = e * 512 + k * 8 * 8 + j * 8 + i;
-                  Aq[id + 0 * offset] = r_AU[k];
-                  Aq[id + 1 * offset] = r_AV[k];
-                  Aq[id + 2 * offset] = r_AW[k];
-                }
-            });
-        });
-      else
-        Q.submit([&] (handler &cgh) {
-            auto ggeo = o_ggeo.get_access<sycl_read>(cgh);
-            auto D = o_DrV.get_access<sycl_read>(cgh);
-            auto lambda = o_lambda.get_access<sycl_read>(cgh);
-            auto q = o_q.get_access<sycl_read>(cgh);
-            auto Aq = o_Aq.get_access<sycl_discard_write>(cgh);
-
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_D(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_q(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_Gqr(64, cgh);
-            accessor<dfloat, 1, sycl_read_write, access::target::local> s_Gqs(64, cgh);
-
-            cgh.parallel_for<class axhelm_ndim1>(nd_range<2>(global_work_size, local_work_size), [=] (nd_item<2> item) {
-                dfloat r_qt, r_Gqt, r_Auk;
-                dfloat r_q[8];
-                dfloat r_Aq[8];
-                dfloat r_G00, r_G01, r_G02, r_G11, r_G12, r_G22, r_GwJ;
-                dfloat r_lam0, r_lam1;
-
-                int e = item.get_group(1); 
-                int j = item.get_local_id(0);
-                int i = item.get_local_id(1);
-
-                s_D[j*8+i] = D[j*8+i];
-                const int base = i + j * 8 + e * 512;
-                for (int k = 0; k < 8; ++k) {
-                r_q[k] = q[base + k * 8 * 8];
-                r_Aq[k] = 0;
-                }
-#pragma unroll 8
-                for (int k = 0; k < 8; ++k) {
-                const int id = e * 512 + k * 8 * 8 + j * 8 + i;
-                const int gbase = e * p_Nggeo * 512 + k * 8 * 8 + j * 8 + i;
-                r_G00 = ggeo[gbase + p_G00ID * 512];
-                r_G01 = ggeo[gbase + p_G01ID * 512];
-                r_G02 = ggeo[gbase + p_G02ID * 512];
-                r_G11 = ggeo[gbase + p_G11ID * 512];
-                r_G12 = ggeo[gbase + p_G12ID * 512];
-                r_G22 = ggeo[gbase + p_G22ID * 512];
-                r_GwJ = ggeo[gbase + p_GWJID * 512];
-                r_lam0 = lambda[id + 0 * offset];
-                r_lam1 = lambda[id + 1 * offset];
-                item.barrier(access::fence_space::local_space);
-                s_q[j*8+i] = r_q[k];
-                r_qt = 0;
-#pragma unroll 8
-                for (int m = 0; m < 8; ++m) {
-                  r_qt += s_D[k*8+m] * r_q[m];
-                }
-                item.barrier(access::fence_space::local_space);
-                dfloat qr = 0;
-                dfloat qs = 0;
-#pragma unroll 8
-                for (int m = 0; m < 8; ++m) {
-                  qr += s_D[i*8+m] * s_q[j*8+m];
-                  qs += s_D[j*8+m] * s_q[m*8+i];
-                }
-                s_Gqs[j*8+i] = r_lam0 * (r_G01 * qr + r_G11 * qs + r_G12 * r_qt);
-                s_Gqr[j*8+i] = r_lam0 * (r_G00 * qr + r_G01 * qs + r_G02 * r_qt);
-                r_Gqt = r_lam0 * (r_G02 * qr + r_G12 * qs + r_G22 * r_qt);
-                r_Auk = r_GwJ * r_lam1 * r_q[k];
-                item.barrier(access::fence_space::local_space);
-#pragma unroll 8
-                for (int m = 0; m < 8; ++m) {
-                  r_Auk += s_D[m*8+j] * s_Gqs[m*8+i];
-                  r_Aq[m] += s_D[k*8+m] * r_Gqt;
-                  r_Auk += s_D[m*8+i] * s_Gqr[j*8+m];
-                }
-                r_Aq[k] += r_Auk;
-                item.barrier(access::fence_space::local_space);
-                }
-#pragma unroll 8
-                for (int k = 0; k < 8; ++k) {
-                  const int id = e * 512 + k * 8 * 8 + j * 8 + i;
-                  Aq[id] = r_Aq[k];
-                }
-            });
-        });
-    }
-    Q.wait();
+  for(int test = 0; test < Ntests; ++test) {
+    if (Ndim > 1)
+      axhelm_n3(s, gws, lws, 0, Nelements, offset, o_ggeo, o_DrV, o_lambda, o_q, o_Aq);
+    else
+      axhelm(s, gws, lws, 0, Nelements, offset, o_ggeo, o_DrV, o_lambda, o_q, o_Aq);
   }
+
+  s.memcpy(q, o_Aq, Ndim*Np*Nelements*sizeof(dfloat)).wait();
+
+  sycl::free(o_ggeo, s);
+  sycl::free(o_q, s);
+  sycl::free(o_Aq, s);
+  sycl::free(o_DrV, s);
+  sycl::free(o_lambda, s);
 
   auto end = std::chrono::high_resolution_clock::now();
   const double elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / Ntests;
-
 
   dfloat maxDiff = 0;
   for(int n=0;n<Ndim*Np*Nelements;++n){
