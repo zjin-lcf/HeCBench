39efb9fd1e702647db2afe9ea2dc9aad708e2c56
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-03-24 18:13:56 -0700

    [hotspot3D-sycl] update the program with a different kernel coding style

--- src/hotspot3D-sycl/3D.cpp
+++ src/hotspot3D-sycl/3D.cpp
@@ -35,6 +35,73 @@ void usage(int argc, char **argv)
   exit(1);
 }
 
+void hotspot3d(
+    sycl::queue &q,
+    sycl::range<3> &gws,
+    sycl::range<3> &lws,
+    const int slm_size,
+    const float*__restrict__ tIn,
+    const float*__restrict__ pIn,
+          float*__restrict__ tOut,
+    const int numCols,
+    const int numRows,
+    const int layers,
+    const float ce,
+    const float cw,
+    const float cn,
+    const float cs,
+    const float ct,
+    const float cb,
+    const float cc,
+    const float stepDivCap)
+{
+  auto cgf = [&] (sycl::handler &cgh) {
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      float amb_temp = 80.0;
+
+      int i = item.get_global_id(2);
+      int j = item.get_global_id(1);
+      int c = i + j * numCols;
+      int xy = numCols * numRows;
+
+      int W = (i == 0)        ? c : c - 1;
+      int E = (i == numCols-1)? c : c + 1;
+      int N = (j == 0)        ? c : c - numCols;
+      int S = (j == numRows-1)? c : c + numCols;
+
+      float temp1, temp2, temp3;
+      temp1 = temp2 = tIn[c];
+      temp3 = tIn[c+xy];
+      tOut[c] = cc * temp2 + cw * tIn[W] + ce * tIn[E] + cs * tIn[S]
+        + cn * tIn[N] + cb * temp1 + ct * temp3 + stepDivCap * pIn[c] + ct * amb_temp;
+      c += xy;
+      W += xy;
+      E += xy;
+      N += xy;
+      S += xy;
+
+      for (int k = 1; k < layers-1; ++k) {
+        temp1 = temp2;
+        temp2 = temp3;
+        temp3 = tIn[c+xy];
+        tOut[c] = cc * temp2 + cw * tIn[W] + ce * tIn[E] + cs * tIn[S]
+          + cn * tIn[N] + cb * temp1 + ct * temp3 + stepDivCap * pIn[c] + ct * amb_temp;
+        c += xy;
+        W += xy;
+        E += xy;
+        N += xy;
+        S += xy;
+      }
+      temp1 = temp2;
+      temp2 = temp3;
+      tOut[c] = cc * temp2 + cw * tIn[W] + ce * tIn[E] + cs * tIn[S]
+        + cn * tIn[N] + cb * temp1 + ct * temp3 + stepDivCap * pIn[c] + ct * amb_temp;
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
+}
+
 int main(int argc, char** argv)
 {
   if (argc != 7)
@@ -68,24 +135,24 @@ int main(int argc, char** argv)
 
   float ce, cw, cn, cs, ct, cb, cc;
   float stepDivCap = dt / Cap;
-  ce               = cw                                              = stepDivCap/ Rx;
-  cn               = cs                                              = stepDivCap/ Ry;
-  ct               = cb                                              = stepDivCap/ Rz;
-
-  cc               = 1.0 - (2.0*ce + 2.0*cn + 3.0*ct);
+  ce = cw = stepDivCap/ Rx;
+  cn = cs = stepDivCap/ Ry;
+  ct = cb = stepDivCap/ Rz;
+  cc = 1.0 - (2.0*ce + 2.0*cn + 3.0*ct);
 
   int size = numCols * numRows * layers;
-  float* tIn   = (float*) calloc(size,sizeof(float));
-  float* pIn   = (float*) calloc(size,sizeof(float));
-  float* tCopy = (float*) malloc(size * sizeof(float));
-  float* tOut  = (float*) calloc(size,sizeof(float));
+  size_t size_byte = size * sizeof(float);
+  float* tIn   = (float*) calloc(size, sizeof(float));
+  float* pIn   = (float*) calloc(size, sizeof(float));
+  float* tCopy = (float*) malloc(size_byte);
+  float* tOut  = (float*) calloc(size, sizeof(float));
 
   readinput(tIn,numRows, numCols, layers, tfile);
   readinput(pIn,numRows, numCols, layers, pfile);
 
-  size_t global_work_size[2];                   
+  size_t global_work_size[2];
   size_t local_work_size[2];
-  memcpy(tCopy,tIn, size * sizeof(float));
+  memcpy(tCopy, tIn, size_byte);
 
   long long start = get_time();
 
@@ -99,8 +166,8 @@ int main(int argc, char** argv)
   float *d_pIn = sycl::malloc_device<float>(size, q);
   float *d_tOut = sycl::malloc_device<float>(size, q);
 
-  q.memcpy(d_tIn, tIn, sizeof(float)*size);
-  q.memcpy(d_pIn, pIn, sizeof(float)*size);
+  q.memcpy(d_tIn, tIn, size_byte);
+  q.memcpy(d_pIn, pIn, size_byte);
 
   global_work_size[1] = numCols;
   global_work_size[0] = numRows;
@@ -108,20 +175,16 @@ int main(int argc, char** argv)
   local_work_size[1] = WG_SIZE_X;
   local_work_size[0] = WG_SIZE_Y;
 
-  sycl::range<2> gws (global_work_size[0], global_work_size[1]);
-  sycl::range<2> lws (local_work_size[0], local_work_size[1]);
+  sycl::range<3> gws (1, global_work_size[0], global_work_size[1]);
+  sycl::range<3> lws (1, local_work_size[0], local_work_size[1]);
 
   q.wait();
   auto kstart = std::chrono::steady_clock::now();
 
   for(int j = 0; j < iterations; j++)
   {
-    q.submit([&](sycl::handler& cgh) {
-      cgh.parallel_for<class hotspot>(
-        sycl::nd_range<2>(gws, lws), [=] (sycl::nd_item<2> item) {
-          #include "kernel_hotspot.sycl"
-      });
-    });
+    hotspot3d(q, gws, lws, 0, d_tIn, d_pIn, d_tOut, numCols, numRows, layers,
+              ce, cw, cn, cs, ct, cb, cc, stepDivCap);
 
     float* temp = d_tIn;
     d_tIn = d_tOut;
@@ -134,7 +197,7 @@ int main(int argc, char** argv)
   printf("Average kernel execution time %f (us)\n", (ktime * 1e-3f) / iterations);
 
   float* d_sel = (iterations & 01) ? d_tIn : d_tOut;
-  q.memcpy(tOut, d_sel, sizeof(float)*size).wait();
+  q.memcpy(tOut, d_sel, size_byte).wait();
 
   sycl::free(d_tIn, q);
   sycl::free(d_pIn, q);
