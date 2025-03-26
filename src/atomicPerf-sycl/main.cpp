27bdeb71184d89a46454ae1cb5f20a5931c410c4
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-03-26 09:01:49 -0700

    [atomicPerf-sycl] update the program with a different kernel coding style

--- src/atomicPerf-sycl/main.cpp
+++ src/atomicPerf-sycl/main.cpp
@@ -2,102 +2,146 @@
 #include <stdlib.h>
 #include <chrono>
 #include <sycl/sycl.hpp>
+#include "atomics.h"
 
 #define BLOCK_SIZE 256
 
 template <typename T>
-void BlockRangeAtomicOnGlobalMem(T* data, int n, sycl::nd_item<1> &item)
+void BlockRangeAtomicOnGlobalMem(
+    sycl::queue &q,
+    sycl::range<3> &gws,
+    sycl::range<3> &lws,
+    const int slm_size,
+    T* data, int n)
 {
-  unsigned int tid = item.get_global_id(0);
-  for (unsigned int i = tid; i < n;
-       i += item.get_local_range(0) * item.get_group_range(0)) {
-    auto ao = sycl::atomic_ref<T, 
-              sycl::memory_order::relaxed,
-              sycl::memory_scope::device,
-              sycl::access::address_space::global_space> (data[item.get_local_id(0)]);
-    ao.fetch_add((T)1); // arbitrary number to add
-  }
+  auto cgf = [&] (sycl::handler &cgh) {
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      unsigned int tid = item.get_global_id(2);
+      for (unsigned int i = tid; i < n;
+           i += item.get_local_range(2) * item.get_group_range(2)) {
+        atomicAdd(data[item.get_local_id(2)], (T)1);
+      }
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
 }
 
 template <typename T>
-void WarpRangeAtomicOnGlobalMem(T* data, int n, sycl::nd_item<1> &item)
+void WarpRangeAtomicOnGlobalMem(
+    sycl::queue &q,
+    sycl::range<3> &gws,
+    sycl::range<3> &lws,
+    const int slm_size,
+    T* data, int n)
 {
-  unsigned int tid = item.get_global_id(0);
-  for (unsigned int i = tid; i < n;
-       i += item.get_local_range(0) * item.get_group_range(0)) {
-    auto ao = sycl::atomic_ref<T, 
-              sycl::memory_order::relaxed,
-              sycl::memory_scope::device,
-              sycl::access::address_space::global_space> (data[i & 0x1F]);
-    ao.fetch_add((T)1); // arbitrary number to add
-  }
+  auto cgf = [&] (sycl::handler &cgh) {
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      unsigned int tid = item.get_global_id(2);
+      for (unsigned int i = tid; i < n;
+           i += item.get_local_range(2) * item.get_group_range(2)) {
+        atomicAdd(data[i & 0x1F], (T)1);
+      }
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
 }
 
 template <typename T>
-void SingleRangeAtomicOnGlobalMem(T* data, int offset, int n, sycl::nd_item<1> &item)
+void SingleRangeAtomicOnGlobalMem(
+    sycl::queue &q,
+    sycl::range<3> &gws,
+    sycl::range<3> &lws,
+    const int slm_size,
+    T* data, int offset, int n)
 {
-  unsigned int tid = item.get_global_id(0);
-  for (unsigned int i = tid; i < n;
-       i += item.get_local_range(0) * item.get_group_range(0)) {
-    auto ao = sycl::atomic_ref<T, 
-              sycl::memory_order::relaxed,
-              sycl::memory_scope::device,
-              sycl::access::address_space::global_space> (data[offset]);
-    ao.fetch_add((T)1); // arbitrary number to add
-  }
+  auto cgf = [&] (sycl::handler &cgh) {
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      unsigned int tid = item.get_global_id(2);
+      for (unsigned int i = tid; i < n;
+           i += item.get_local_range(2) * item.get_group_range(2)) {
+            atomicAdd(data[offset], (T)1);
+      }
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
 }
 
 template <typename T>
-void BlockRangeAtomicOnSharedMem(T* data, int n, sycl::nd_item<1> item,
-                                 T *smem_data)
+void BlockRangeAtomicOnSharedMem(
+    sycl::queue &q,
+    sycl::range<3> &gws,
+    sycl::range<3> &lws,
+    const int slm_size,
+    T* data, int n)
 {
-  unsigned int tid = item.get_global_id(0);
-  for (unsigned int i = tid; i < n;
-       i += item.get_local_range(0) * item.get_group_range(0)) {
-    auto ao = sycl::atomic_ref<T, 
-              sycl::memory_order::relaxed,
-              sycl::memory_scope::work_group,
-              sycl::access::address_space::local_space> (smem_data[item.get_local_id(0)]);
-    ao.fetch_add((T)1); // arbitrary number to add
-  }
-  if (item.get_group(0) == item.get_group_range(0))
-    data[item.get_local_id(0)] = smem_data[item.get_local_id(0)];
+  auto cgf = [&] (sycl::handler &cgh) {
+    sycl::local_accessor<T, 1> smem_data (sycl::range<1>(BLOCK_SIZE), cgh);
+
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      unsigned int tid = item.get_global_id(2);
+      for (unsigned int i = tid; i < n;
+           i += item.get_local_range(2) * item.get_group_range(2)) {
+        atomicAdd(smem_data[item.get_local_id(2)], (T)1);
+      }
+      if (item.get_group(2) == item.get_group_range(2))
+        data[item.get_local_id(2)] = smem_data[item.get_local_id(2)];
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
 }
 
 template <typename T>
-void WarpRangeAtomicOnSharedMem(T* data, int n, sycl::nd_item<1> item,
-                                T *smem_data)
+void WarpRangeAtomicOnSharedMem(
+    sycl::queue &q,
+    sycl::range<3> &gws,
+    sycl::range<3> &lws,
+    const int slm_size,
+    T* data, int n)
 {
-  unsigned int tid = item.get_global_id(0);
-  for (unsigned int i = tid; i < n;
-       i += item.get_local_range(0) * item.get_group_range(0)) {
-    auto ao = sycl::atomic_ref<T, 
-              sycl::memory_order::relaxed,
-              sycl::memory_scope::work_group,
-              sycl::access::address_space::local_space> (smem_data[i & 0x1F]);
-    ao.fetch_add((T)1); // arbitrary number to add
-  }
-  if (item.get_group(0) == item.get_group_range(0) &&
-      item.get_local_id(0) < 0x1F)
-    data[item.get_local_id(0)] = smem_data[item.get_local_id(0)];
+  auto cgf = [&] (sycl::handler &cgh) {
+    sycl::local_accessor<T, 1> smem_data (sycl::range<1>(32), cgh);
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      unsigned int tid = item.get_global_id(2);
+      for (unsigned int i = tid; i < n;
+           i += item.get_local_range(2) * item.get_group_range(2)) {
+        atomicAdd(smem_data[i & 0x1F], (T)1);
+      }
+      if (item.get_group(2) == item.get_group_range(2) &&
+          item.get_local_id(2) < 0x1F)
+        data[item.get_local_id(2)] = smem_data[item.get_local_id(2)];
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
 }
 
 template <typename T>
-void SingleRangeAtomicOnSharedMem(T* data, int offset, int n, sycl::nd_item<1> item,
-                                  T *smem_data)
+void SingleRangeAtomicOnSharedMem(
+    sycl::queue &q,
+    sycl::range<3> &gws,
+    sycl::range<3> &lws,
+    const int slm_size,
+    T* data, int offset, int n)
 {
-  unsigned int tid = item.get_global_id(0);
-  for (unsigned int i = tid; i < n;
-       i += item.get_local_range(0) * item.get_group_range(0)) {
-    auto ao = sycl::atomic_ref<T, 
-              sycl::memory_order::relaxed,
-              sycl::memory_scope::work_group,
-              sycl::access::address_space::local_space> (smem_data[offset]);
-    ao.fetch_add((T)1); // arbitrary number to add
-  }
-  if (item.get_group(0) == item.get_group_range(0) &&
-      item.get_local_id(0) == 0)
-    data[item.get_local_id(0)] = smem_data[item.get_local_id(0)];
+  auto cgf = [&] (sycl::handler &cgh) {
+    sycl::local_accessor<T, 1> smem_data (sycl::range<1>(BLOCK_SIZE), cgh);
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      unsigned int tid = item.get_global_id(2);
+      for (unsigned int i = tid; i < n;
+           i += item.get_local_range(2) * item.get_group_range(2)) {
+        atomicAdd(smem_data[offset], (T)1);
+      }
+      if (item.get_group(2) == item.get_group_range(2) &&
+          item.get_local_id(2) == 0)
+        data[item.get_local_id(2)] = smem_data[item.get_local_id(2)];
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
 }
 
 template <typename T>
@@ -108,7 +152,7 @@ void atomicPerf (int n, int t, int repeat)
 #else
   sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-  
+
   size_t data_size = sizeof(T) * t;
 
   T* data = (T*) malloc (data_size);
@@ -119,18 +163,14 @@ void atomicPerf (int n, int t, int repeat)
 
   T* d_data = (T *)sycl::malloc_device(data_size, q);
 
-  sycl::range<1> lws (BLOCK_SIZE);
-  sycl::range<1> gws (n);
+  sycl::range<3> lws (1, 1, BLOCK_SIZE);
+  sycl::range<3> gws (1, 1, n);
 
   q.memcpy(d_data, data, data_size).wait();
   auto start = std::chrono::steady_clock::now();
   for(int i=0; i<repeat; i++)
   {
-    q.submit([&] (sycl::handler &cgh) {
-      cgh.parallel_for(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item) {
-        BlockRangeAtomicOnGlobalMem<T>(d_data, n, item);
-      });
-    });
+    BlockRangeAtomicOnGlobalMem<T>(q, gws, lws, 0, d_data, n);
   }
   q.wait();
   auto end = std::chrono::steady_clock::now();
@@ -142,11 +182,7 @@ void atomicPerf (int n, int t, int repeat)
   start = std::chrono::steady_clock::now();
   for(int i=0; i<repeat; i++)
   {
-    q.submit([&] (sycl::handler &cgh) {
-      cgh.parallel_for(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item) {
-        WarpRangeAtomicOnGlobalMem<T>(d_data, n, item);
-      });
-    });
+    WarpRangeAtomicOnGlobalMem<T>(q, gws, lws, 0, d_data, n);
   }
   q.wait();
   end = std::chrono::steady_clock::now();
@@ -158,11 +194,7 @@ void atomicPerf (int n, int t, int repeat)
   start = std::chrono::steady_clock::now();
   for(int i=0; i<repeat; i++)
   {
-    q.submit([&] (sycl::handler &cgh) {
-      cgh.parallel_for(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item) {
-        SingleRangeAtomicOnGlobalMem<T>(d_data, i % BLOCK_SIZE, n, item);
-      });
-    });
+    SingleRangeAtomicOnGlobalMem<T>(q, gws, lws, 0, d_data, i % BLOCK_SIZE, n);
   }
   q.wait();
   end = std::chrono::steady_clock::now();
@@ -174,12 +206,7 @@ void atomicPerf (int n, int t, int repeat)
   start = std::chrono::steady_clock::now();
   for(int i=0; i<repeat; i++)
   {
-    q.submit([&] (sycl::handler &cgh) {
-      sycl::local_accessor<T, 1> smem (sycl::range<1>(BLOCK_SIZE), cgh);
-      cgh.parallel_for(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item) {
-        BlockRangeAtomicOnSharedMem<T>(d_data, n, item, smem.get_pointer());
-      });
-    });
+    BlockRangeAtomicOnSharedMem<T>(q, gws, lws, 0, d_data, n);
   }
   q.wait();
   end = std::chrono::steady_clock::now();
@@ -191,12 +218,7 @@ void atomicPerf (int n, int t, int repeat)
   start = std::chrono::steady_clock::now();
   for(int i=0; i<repeat; i++)
   {
-    q.submit([&] (sycl::handler &cgh) {
-      sycl::local_accessor<T, 1> smem (sycl::range<1>(32), cgh);
-      cgh.parallel_for(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item) {
-        WarpRangeAtomicOnSharedMem<T>(d_data, n, item, smem.get_pointer());
-      });
-    });
+    WarpRangeAtomicOnSharedMem<T>(q, gws, lws, 0, d_data, n);
   }
   q.wait();
   end = std::chrono::steady_clock::now();
@@ -208,13 +230,7 @@ void atomicPerf (int n, int t, int repeat)
   start = std::chrono::steady_clock::now();
   for(int i=0; i<repeat; i++)
   {
-    q.submit([&] (sycl::handler &cgh) {
-      sycl::local_accessor<T, 1> smem (sycl::range<1>(BLOCK_SIZE), cgh);
-      cgh.parallel_for(sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item) {
-        SingleRangeAtomicOnSharedMem<T>(d_data, i % BLOCK_SIZE,
-                                        n, item, smem.get_pointer());
-      });
-    });
+    SingleRangeAtomicOnSharedMem<T>(q, gws, lws, 0, d_data, i % BLOCK_SIZE, n);
   }
   q.wait();
   end = std::chrono::steady_clock::now();
@@ -236,15 +252,15 @@ int main(int argc, char* argv[])
 
   const int n = 3*4*7*8*9*256; // number of threads
   const int len = 1024; // data array length
-  
+
   printf("\nFP64 atomic add\n");
-  atomicPerf<double>(n, len, repeat); 
+  atomicPerf<double>(n, len, repeat);
 
   printf("\nINT32 atomic add\n");
-  atomicPerf<int>(n, len, repeat); 
+  atomicPerf<int>(n, len, repeat);
 
   printf("\nFP32 atomic add\n");
-  atomicPerf<float>(n, len, repeat); 
+  atomicPerf<float>(n, len, repeat);
 
   return 0;
 }
