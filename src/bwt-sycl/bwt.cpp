5d6d3488ba82361510470592dbf2f28ea5491e88
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-03-26 09:25:47 -0700

    [bwt-sycl] update the program with a different kernel coding style

--- src/bwt-sycl/bwt.cpp
+++ src/bwt-sycl/bwt.cpp
@@ -5,11 +5,21 @@
 
 const int blockSize = 256;
 
-void generate_table(sycl::nd_item<1> &item, int* table, int table_size, int n) {
-  int index = item.get_global_id(0);
-  int stride = item.get_local_range(0) * item.get_group_range(0);
-  for(int i = index; i < table_size; i+=stride)
-    table[i] = (i < n) ? i : -1;
+void generate_table(sycl::queue &q,
+                    sycl::range<3> &gws,
+                    sycl::range<3> &lws,
+                    const int slm_size,
+                    int* table, int table_size, int n) {
+  auto cgf = [&] (sycl::handler &cgh) {
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      int index = item.get_global_id(2);
+      int stride = item.get_local_range(2) * item.get_group_range(2);
+      for(int i = index; i < table_size; i+=stride)
+        table[i] = (i < n) ? i : -1;
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
 }
 
 bool compare_rotations(const int& a, const int& b, const char* genome, int n) {
@@ -23,33 +33,52 @@ bool compare_rotations(const int& a, const int& b, const char* genome, int n) {
   return false;
 }
 
-void bitonic_sort_step(sycl::nd_item<1> &item, int*__restrict table, int table_size, 
+void bitonic_sort_step(sycl::queue &q,
+                       sycl::range<3> &gws,
+                       sycl::range<3> &lws,
+                       const int slm_size,
+                       int*__restrict table, int table_size,
                        int j, int k, const char*__restrict genome, int n) {
-  int i = item.get_global_id(0);
-  int ixj = i ^ j;
-  if (i < table_size && ixj > i) {
-    bool f = (i & k) == 0;
-    int t1 = table[i];
-    int t2 = table[ixj];
-    if (compare_rotations(f ? t2 : t1, f ? t1 : t2, genome, n)) {
-      table[i] = t2;
-      table[ixj] = t1;
-    }
-  }
+  auto cgf = [&] (sycl::handler &cgh) {
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      int i = item.get_global_id(2);
+      int ixj = i ^ j;
+      if (i < table_size && ixj > i) {
+        bool f = (i & k) == 0;
+        int t1 = table[i];
+        int t2 = table[ixj];
+        if (compare_rotations(f ? t2 : t1, f ? t1 : t2, genome, n)) {
+          table[i] = t2;
+          table[ixj] = t1;
+        }
+      }
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
 }
 
-void reconstruct_sequence(sycl::nd_item<1> &item,
+void reconstruct_sequence(sycl::queue &q,
+                          sycl::range<3> &gws,
+                          sycl::range<3> &lws,
+                          const int slm_size,
                           const int*__restrict table,
-                          const char*__restrict sequence, 
+                          const char*__restrict sequence,
                           char*__restrict transformed_sequence, int n) {
-  int index = item.get_global_id(0);
-  int stride = item.get_local_range(0) * item.get_group_range(0);
-  for(int i = index; i < n; i += stride) {
-    transformed_sequence[i] = sequence[(n + table[i] - 1) % n];
-  }
+  auto cgf = [&] (sycl::handler &cgh) {
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      int index = item.get_global_id(2);
+      int stride = item.get_local_range(2) * item.get_group_range(2);
+      for(int i = index; i < n; i += stride) {
+        transformed_sequence[i] = sequence[(n + table[i] - 1) % n];
+      }
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
 }
 
-/* 
+/*
    returns a std::pair object
    the first item is the burrows wheeler transform of the input sequence in a std::string,
    the second item is the suffix array of the input sequence, represented as indicies of the given suffix, as an int*
@@ -83,48 +112,33 @@ std::pair<std::string,int*> bwt_with_suffix_array(const std::string sequence) {
   int* table = (int*) malloc(table_size_bytes);
 
   int numBlocks = (table_size + blockSize - 1) / blockSize;
-  sycl::range<1> gws (numBlocks * blockSize);
-  sycl::range<1> lws (blockSize);
+  sycl::range<3> gws (1, 1, numBlocks * blockSize);
+  sycl::range<3> lws (1, 1, blockSize);
 
-  q.submit([&] (sycl::handler &cgh) {
-    cgh.parallel_for<class gen>(
-      sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
-      generate_table(item, d_table, table_size, n);
-    });
-  });
+  generate_table(q, gws, lws, 0, d_table, table_size, n);
 
   char *d_sequence = sycl::malloc_device<char>(n, q);
   q.memcpy(d_sequence, sequence.c_str(), seq_size_bytes);
 
   for (int k = 2; k <= table_size; k <<= 1) {
     for (int j = k >> 1; j > 0; j = j >> 1) {
-      q.submit([&] (sycl::handler &cgh) {
-        cgh.parallel_for<class bsort>(
-          sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
-          bitonic_sort_step(item, d_table, table_size, j, k, d_sequence, n);
-        });
-      });
+      bitonic_sort_step(q, gws, lws, 0, d_table, table_size, j, k, d_sequence, n);
     }
   }
 
   char *d_transformed_sequence = sycl::malloc_device<char>(n, q);
   numBlocks = (n + blockSize - 1) / blockSize;
-  sycl::range<1> gws2 (numBlocks * blockSize);
+  sycl::range<3> gws2 (1, 1, numBlocks * blockSize);
 
-  q.submit([&] (sycl::handler &cgh) {
-    cgh.parallel_for<class restruct>(
-      sycl::nd_range<1>(gws2, lws), [=] (sycl::nd_item<1> item) {
-      reconstruct_sequence(item, d_table, d_sequence, d_transformed_sequence, n);
-    });
-  });
+  reconstruct_sequence(q, gws2, lws, 0, d_table, d_sequence, d_transformed_sequence, n);
 
   char* transformed_sequence_cstr = (char*) malloc(seq_size_bytes);
 
-  q.memcpy(transformed_sequence_cstr, d_transformed_sequence, seq_size_bytes).wait(); 
+  q.memcpy(transformed_sequence_cstr, d_transformed_sequence, seq_size_bytes).wait();
 
   std::string transformed_sequence(transformed_sequence_cstr, n);
 
-  q.memcpy(table, d_table, table_size_bytes).wait(); 
+  q.memcpy(table, d_table, table_size_bytes).wait();
   sycl::free(d_table, q);
   sycl::free(d_sequence, q);
 
