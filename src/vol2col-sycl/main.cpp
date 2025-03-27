63fed204a9652ba2de335dea2fde606651e3bb16
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-03-26 19:44:35 -0700

    [vol2col-sycl] update the program with a different kernel coding style

--- src/vol2col-sycl/main.cpp
+++ src/vol2col-sycl/main.cpp
@@ -12,7 +12,10 @@
 // Kernel for fast unfold+copy on volumes
 template <typename T>
 void vol2col_kernel(
-    sycl::nd_item<1> &item,
+    sycl::queue &q,
+    sycl::range<3> &gws,
+    sycl::range<3> &lws,
+    const int slm_size,
     const int64_t range,
     const T* data_vol,
     const int depth,
@@ -35,40 +38,49 @@ void vol2col_kernel(
     const int width_col,
     T* data_col)
 {
-  for (int64_t n = item.get_global_id(0); n < range;
-               n += item.get_group_range(0) * item.get_local_range(0)) {
-    int w_out = n % width_col;
-    int64_t index = n / width_col;
-    int h_out = index % height_col;
-    index /= height_col;
-    int t_out = index % depth_col;
-    int channel_in = index / depth_col;
-    int channel_out = channel_in * ksize_t * ksize_h * ksize_w;
-    int t_in = t_out * stride_t - pad_t;
-    int h_in = h_out * stride_h - pad_h;
-    int w_in = w_out * stride_w - pad_w;
-    auto v = data_vol + ((channel_in * depth + t_in) * height + h_in) * width + w_in;
-    auto c = data_col + ((channel_out * depth_col + t_out) * height_col + h_out) * width_col + w_out;
-    for (int i = 0; i < ksize_t; ++i) {
-      for (int j = 0; j < ksize_h; ++j) {
-        for (int k = 0; k < ksize_w; ++k) {
-          int t = t_in + i * dilation_t;
-          int h = h_in + j * dilation_h;
-          int w = w_in + k * dilation_w;
-          *c = (t >= 0 && h >= 0 && w >= 0 && t < depth && h < height && w < width)
-              ? v[i * dilation_t * height * width +
-                  j * dilation_h * width + k * dilation_w]
-              : static_cast<T>(0);
-          c += depth_col * height_col * width_col;
+  auto cgf = [&] (sycl::handler &cgh) {
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      for (int64_t n = item.get_global_id(2); n < range;
+                   n += item.get_group_range(2) * item.get_local_range(2)) {
+        int w_out = n % width_col;
+        int64_t index = n / width_col;
+        int h_out = index % height_col;
+        index /= height_col;
+        int t_out = index % depth_col;
+        int channel_in = index / depth_col;
+        int channel_out = channel_in * ksize_t * ksize_h * ksize_w;
+        int t_in = t_out * stride_t - pad_t;
+        int h_in = h_out * stride_h - pad_h;
+        int w_in = w_out * stride_w - pad_w;
+        auto v = data_vol + ((channel_in * depth + t_in) * height + h_in) * width + w_in;
+        auto c = data_col + ((channel_out * depth_col + t_out) * height_col + h_out) * width_col + w_out;
+        for (int i = 0; i < ksize_t; ++i) {
+          for (int j = 0; j < ksize_h; ++j) {
+            for (int k = 0; k < ksize_w; ++k) {
+              int t = t_in + i * dilation_t;
+              int h = h_in + j * dilation_h;
+              int w = w_in + k * dilation_w;
+              *c = (t >= 0 && h >= 0 && w >= 0 && t < depth && h < height && w < width)
+                  ? v[i * dilation_t * height * width +
+                      j * dilation_h * width + k * dilation_w]
+                  : static_cast<T>(0);
+              c += depth_col * height_col * width_col;
+            }
+          }
         }
       }
-    }
-  }
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
 }
 
 template <typename T, typename accT>
 void col2vol_kernel(
-    sycl::nd_item<1> &item,
+    sycl::queue &q,
+    sycl::range<3> &gws,
+    sycl::range<3> &lws,
+    const int slm_size,
     const int64_t n,
     const T* data_col,
     const int depth,
@@ -91,51 +103,57 @@ void col2vol_kernel(
     const int width_col,
     T* data_vol)
 {
-  for (int64_t index = item.get_global_id(0); index < n;
-               index += item.get_group_range(0) * item.get_local_range(0)) {
-    accT val = static_cast<accT>(0);
-    const int w_im = index % width + pad_w;
-    const int h_im = (index / width) % height + pad_h;
-    const int t_im = (index / width / height) % depth + pad_t;
-    const int c_im = index / (width * height * depth);
-    auto kernel_extent_w = (kernel_w - 1) * dilation_w + 1;
-    auto kernel_extent_h = (kernel_h - 1) * dilation_h + 1;
-    auto kernel_extent_t = (kernel_t - 1) * dilation_t + 1;
-    // compute the start and end of the output
-    const int w_col_start =
-        (w_im < kernel_extent_w) ? 0 : (w_im - kernel_extent_w) / stride_w + 1;
-    const int w_col_end = sycl::min(w_im / stride_w + 1, width_col);
-    const int h_col_start =
-        (h_im < kernel_extent_h) ? 0 : (h_im - kernel_extent_h) / stride_h + 1;
-    const int h_col_end = sycl::min(h_im / stride_h + 1, height_col);
-    const int t_col_start =
-        (t_im < kernel_extent_t) ? 0 : (t_im - kernel_extent_t) / stride_t + 1;
-    const int t_col_end = sycl::min(t_im / stride_t + 1, depth_col);
-    // TODO: use LCM of stride and dilation to avoid unnecessary loops
-    for (int t_col = t_col_start; t_col < t_col_end; t_col += 1) {
-      for (int h_col = h_col_start; h_col < h_col_end; h_col += 1) {
-        for (int w_col = w_col_start; w_col < w_col_end; w_col += 1) {
-          int64_t t_k = (t_im - t_col * stride_t);
-          int64_t h_k = (h_im - h_col * stride_h);
-          int64_t w_k = (w_im - w_col * stride_w);
-          if (t_k % dilation_t == 0 && h_k % dilation_h == 0 &&
-              w_k % dilation_w == 0) {
-            t_k /= dilation_t;
-            h_k /= dilation_h;
-            w_k /= dilation_w;
-            const int64_t idx_k =
-                ((c_im * kernel_t + t_k) * kernel_h + h_k) * kernel_w + w_k;
-            const int64_t data_col_index =
-                ((idx_k * depth_col + t_col) *
-                    height_col + h_col) *
-                  width_col + w_col;
-            val += data_col[data_col_index];
+  auto cgf = [&] (sycl::handler &cgh) {
+    auto kfn = [=] (sycl::nd_item<3> item) {
+      for (int64_t index = item.get_global_id(2); index < n;
+                   index += item.get_group_range(2) * item.get_local_range(2)) {
+        accT val = static_cast<accT>(0);
+        const int w_im = index % width + pad_w;
+        const int h_im = (index / width) % height + pad_h;
+        const int t_im = (index / width / height) % depth + pad_t;
+        const int c_im = index / (width * height * depth);
+        auto kernel_extent_w = (kernel_w - 1) * dilation_w + 1;
+        auto kernel_extent_h = (kernel_h - 1) * dilation_h + 1;
+        auto kernel_extent_t = (kernel_t - 1) * dilation_t + 1;
+        // compute the start and end of the output
+        const int w_col_start =
+            (w_im < kernel_extent_w) ? 0 : (w_im - kernel_extent_w) / stride_w + 1;
+        const int w_col_end = sycl::min(w_im / stride_w + 1, width_col);
+        const int h_col_start =
+            (h_im < kernel_extent_h) ? 0 : (h_im - kernel_extent_h) / stride_h + 1;
+        const int h_col_end = sycl::min(h_im / stride_h + 1, height_col);
+        const int t_col_start =
+            (t_im < kernel_extent_t) ? 0 : (t_im - kernel_extent_t) / stride_t + 1;
+        const int t_col_end = sycl::min(t_im / stride_t + 1, depth_col);
+        // TODO: use LCM of stride and dilation to avoid unnecessary loops
+        for (int t_col = t_col_start; t_col < t_col_end; t_col += 1) {
+          for (int h_col = h_col_start; h_col < h_col_end; h_col += 1) {
+            for (int w_col = w_col_start; w_col < w_col_end; w_col += 1) {
+              int64_t t_k = (t_im - t_col * stride_t);
+              int64_t h_k = (h_im - h_col * stride_h);
+              int64_t w_k = (w_im - w_col * stride_w);
+              if (t_k % dilation_t == 0 && h_k % dilation_h == 0 &&
+                  w_k % dilation_w == 0) {
+                t_k /= dilation_t;
+                h_k /= dilation_h;
+                w_k /= dilation_w;
+                const int64_t idx_k =
+                    ((c_im * kernel_t + t_k) * kernel_h + h_k) * kernel_w + w_k;
+                const int64_t data_col_index =
+                    ((idx_k * depth_col + t_col) *
+                        height_col + h_col) *
+                      width_col + w_col;
+                val += data_col[data_col_index];
+              }
+            }
           }
         }
+        data_vol[index] = static_cast<T>(val);
       }
-    }
-    data_vol[index] = static_cast<T>(val);
-  }
+    };
+    cgh.parallel_for(sycl::nd_range<3>(gws, lws), kfn);
+  };
+  q.submit(cgf);
 }
 
 int get_blocks (sycl::queue &q, int64_t n) {
@@ -211,18 +229,15 @@ void eval (
 
   int blocksPerGrid = get_blocks(q, n);
 
-  sycl::range<1> gws (blocksPerGrid * threadsPerBlock);
-  sycl::range<1> lws (threadsPerBlock);
+  sycl::range<3> gws (1, 1, blocksPerGrid * threadsPerBlock);
+  sycl::range<3> lws (1, 1, threadsPerBlock);
 
   q.wait();
   auto start = std::chrono::steady_clock::now();
 
   for (int i = 0; i < repeat; i++) {
-    q.submit([&] (sycl::handler &cgh) {
-      cgh.parallel_for<class v2c>(
-        sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
-        vol2col_kernel<T>(
-          item,
+    vol2col_kernel<T>(
+          q, gws, lws, 0,
           n,
           d_data_vol,
           depth, height, width,
@@ -232,8 +247,6 @@ void eval (
           dilation_t, dilation_h, dilation_w,
           depth_col, height_col, width_col,
           d_data_col);
-      });
-    });
   }
 
   q.wait();
@@ -260,11 +273,8 @@ void eval (
   start = std::chrono::steady_clock::now();
 
   for (int i = 0; i < repeat; i++) {
-    q.submit([&] (sycl::handler &cgh) {
-      cgh.parallel_for<class c2v>(
-        sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
-        col2vol_kernel<T, T>(
-          item,
+    col2vol_kernel<T, T>(
+          q, gws, lws, 0,
           n,
           d_data_col,
           depth, height, width,
@@ -274,8 +284,6 @@ void eval (
           dilation_t, dilation_h, dilation_w,
           depth_col, height_col, width_col,
           d_data_vol);
-      });
-    });
   }
 
   q.wait();
