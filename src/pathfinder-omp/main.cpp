d10fb3bedee4f7614e2b4c343748c90f44ad6fd7
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-04-15 07:34:44 -0700

    [pathfinder-omp] update the OpenMP kernel

--- src/pathfinder-omp/main.cpp
+++ src/pathfinder-omp/main.cpp
@@ -42,6 +42,124 @@ double get_time() {
   return t.tv_sec+t.tv_usec*1e-6;
 }
 
+void pathfinder (
+    const int numTeams,
+    const int numThreads,
+    const int*__restrict__ gpuWall,
+    const int*__restrict__ gpuSrc,
+          int*__restrict__ gpuResult,
+          int*__restrict__ outputBuffer,
+    const int iteration,
+    const int theHalo,
+    const int borderCols,
+    const int cols,
+    const int t)
+{
+  #pragma omp target teams num_teams(numTeams)
+  {
+    int prev[250];
+    int result[250];
+    #pragma omp parallel num_threads(numThreads) 
+    {
+      // Set the kernel arguments.
+      int BLOCK_SIZE = omp_get_num_threads();
+      int bx = omp_get_team_num();
+      int tx = omp_get_thread_num();
+
+      // Each block finally computes result for a small block
+      // after N iterations.
+      // it is the non-overlapping small blocks that cover
+      // all the input data
+
+      // calculate the small block size.
+      int small_block_cols = BLOCK_SIZE - (iteration*theHalo*2);
+
+      // calculate the boundary for the block according to
+      // the boundary of its small block
+      int blkX = (small_block_cols*bx) - borderCols;
+      int blkXmax = blkX+BLOCK_SIZE-1;
+
+      // calculate the global thread coordination
+      int xidx = blkX+tx;
+
+      // effective range within this block that falls within
+      // the valid range of the input data
+      // used to rule out computation outside the boundary.
+      int validXmin = (blkX < 0) ? -blkX : 0;
+      int validXmax = (blkXmax > cols-1) ? BLOCK_SIZE-1-(blkXmax-cols+1) : BLOCK_SIZE-1;
+
+      int W = tx-1;
+      int E = tx+1;
+
+      W = (W < validXmin) ? validXmin : W;
+      E = (E > validXmax) ? validXmax : E;
+
+      bool isValid = IN_RANGE(tx, validXmin, validXmax);
+
+      if(IN_RANGE(xidx, 0, cols-1))
+      {
+        prev[tx] = gpuSrc[xidx];
+      }
+
+      #pragma omp barrier
+
+      bool computed;
+      for (int i = 0; i < iteration; i++)
+      {
+        computed = false;
+
+        if( IN_RANGE(tx, i+1, BLOCK_SIZE-i-2) && isValid )
+        {
+          computed = true;
+          int left = prev[W];
+          int up = prev[tx];
+          int right = prev[E];
+          int shortest = MIN(left, up);
+          shortest = MIN(shortest, right);
+
+          int index = cols*(t+i)+xidx;
+          result[tx] = shortest + gpuWall[index];
+
+          // ===================================================================
+          // add debugging info to the debug output buffer...
+          if (tx==11 && i==0)
+          {
+            // set bufIndex to what value/range of values you want to know.
+            int bufIndex = gpuSrc[xidx];
+            // dont touch the line below.
+            outputBuffer[bufIndex] = 1;
+          }
+          // ===================================================================
+        }
+
+        #pragma omp barrier
+
+        if(i==iteration-1)
+        {
+          // we are on the last iteration, and thus don't need to 
+          // compute for the next step.
+          break;
+        }
+
+        if(computed)
+        {
+          //Assign the computation range
+          prev[tx] = result[tx];
+        }
+        #pragma omp barrier
+      }
+
+      // update the global memory
+      // after the last iteration, only threads coordinated within the
+      // small block perform the calculation and switch on "computed"
+      if (computed)
+      {
+        gpuResult[xidx] = result[tx];
+      }
+    }
+  } 
+}
+
 int main(int argc, char** argv)
 {
   // Program variables.
@@ -131,109 +249,8 @@ int main(int argc, char** argv)
       // Calculate this for the kernel argument...
       int iteration = MIN(pyramid_height, rows-t-1);
 
-      #pragma omp target teams num_teams(gws) thread_limit(lws)
-      {
-        int prev[lws];
-        int result[lws];
-        #pragma omp parallel 
-        {
-          // Set the kernel arguments.
-          int BLOCK_SIZE = omp_get_num_threads();
-          int bx = omp_get_team_num();
-          int tx = omp_get_thread_num();
-
-          // Each block finally computes result for a small block
-          // after N iterations.
-          // it is the non-overlapping small blocks that cover
-          // all the input data
-
-          // calculate the small block size.
-          int small_block_cols = BLOCK_SIZE - (iteration*theHalo*2);
-
-          // calculate the boundary for the block according to
-          // the boundary of its small block
-          int blkX = (small_block_cols*bx) - borderCols;
-          int blkXmax = blkX+BLOCK_SIZE-1;
-
-          // calculate the global thread coordination
-          int xidx = blkX+tx;
-
-          // effective range within this block that falls within
-          // the valid range of the input data
-          // used to rule out computation outside the boundary.
-          int validXmin = (blkX < 0) ? -blkX : 0;
-          int validXmax = (blkXmax > cols-1) ? BLOCK_SIZE-1-(blkXmax-cols+1) : BLOCK_SIZE-1;
-
-          int W = tx-1;
-          int E = tx+1;
-
-          W = (W < validXmin) ? validXmin : W;
-          E = (E > validXmax) ? validXmax : E;
-
-          bool isValid = IN_RANGE(tx, validXmin, validXmax);
-
-          if(IN_RANGE(xidx, 0, cols-1))
-          {
-            prev[tx] = gpuSrc[xidx];
-          }
-
-          #pragma omp barrier
-
-          bool computed;
-          for (int i = 0; i < iteration; i++)
-          {
-            computed = false;
-
-            if( IN_RANGE(tx, i+1, BLOCK_SIZE-i-2) && isValid )
-            {
-              computed = true;
-              int left = prev[W];
-              int up = prev[tx];
-              int right = prev[E];
-              int shortest = MIN(left, up);
-              shortest = MIN(shortest, right);
-
-              int index = cols*(t+i)+xidx;
-              result[tx] = shortest + gpuWall[index];
-
-              // ===================================================================
-              // add debugging info to the debug output buffer...
-              if (tx==11 && i==0)
-              {
-                // set bufIndex to what value/range of values you want to know.
-                int bufIndex = gpuSrc[xidx];
-                // dont touch the line below.
-                outputBuffer[bufIndex] = 1;
-              }
-              // ===================================================================
-            }
-
-            #pragma omp barrier
-
-            if(i==iteration-1)
-            {
-              // we are on the last iteration, and thus don't need to 
-              // compute for the next step.
-              break;
-            }
-
-            if(computed)
-            {
-              //Assign the computation range
-              prev[tx] = result[tx];
-            }
-            #pragma omp barrier
-          }
-
-          // update the global memory
-          // after the last iteration, only threads coordinated within the
-          // small block perform the calculation and switch on "computed"
-          if (computed)
-          {
-            gpuResult[xidx] = result[tx];
-          }
-        }
-      } 
+      pathfinder(gws, lws, gpuWall, gpuSrc, gpuResult, outputBuffer, 
+                 iteration, theHalo, borderCols, cols, t);
       int *temp = gpuResult;
       gpuResult = gpuSrc;
       gpuSrc = temp;
