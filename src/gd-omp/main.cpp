f0d0c452c13dcddb579c3f49f010abc38ef0f604
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-04-17 08:30:30 -0700

    [gd-omp] update the OpenMP kernels

--- src/gd-omp/main.cpp
+++ src/gd-omp/main.cpp
@@ -5,6 +5,80 @@
 #include <omp.h>
 #include "utils.h"
 
+void L2_norm(const int numTeams, const int numThreads,
+             const float *x, float* l2_norm, int n)
+{
+  #pragma omp target teams distribute parallel for \
+   num_teams(numTeams) num_threads(numThreads)
+  for (int i = 0; i < n; ++i) {
+    #pragma omp atomic update
+    l2_norm[0] += x[i] * x[i];
+  }
+}
+
+void compute (
+    const int numTeams,
+    const int numThreads,
+    const float * __restrict__ x, 
+          float * __restrict__ grad, 
+    const int   * __restrict__ A_row_ptr,
+    const int   * __restrict__ A_col_index,
+    const float * __restrict__ A_value, 
+    const int   * __restrict__ A_y_label,
+    float * __restrict__ total_obj_val,
+    int   * __restrict__ correct,
+    int m ) 
+{
+  #pragma omp target teams distribute parallel for \
+   num_teams(numTeams) num_threads(numThreads) 
+  for (int i = 0; i < m; ++i) {
+
+    // Simple sparse matrix multiply x' = A * x
+    float xp = 0.f;
+         
+    for( int j = A_row_ptr[i]; j < A_row_ptr[i+1]; ++j) {
+      xp += A_value[j] * x[A_col_index[j]];
+    }
+
+    // compute objective 
+    float v = logf(1.f + expf(-xp * A_y_label[i]));
+    #pragma omp atomic update
+    total_obj_val[0] += v;
+
+    // compute errors
+    float prediction = 1.f / (1.f + expf(-xp));
+    int t = (prediction >= 0.5f) ? 1 : -1;
+    if (A_y_label[i] == t) {
+      #pragma omp atomic update
+      correct[0]++;
+    }
+
+    // compute gradient at x
+    float accum = expf(-A_y_label[i] * xp);
+    accum = accum / (1.f + accum);
+    for(int j = A_row_ptr[i]; j < A_row_ptr[i+1]; ++j) {
+      float temp = -accum * A_value[j] * A_y_label[i];
+      #pragma omp atomic update
+      grad[A_col_index[j]] += temp;
+    }
+  }
+}
+
+void update(
+    const int numTeams,
+    const int numThreads,
+    float * __restrict__ x,
+    const float * __restrict__ grad,
+    int m, int n, float lambda, float alpha) 
+{
+  #pragma omp target teams distribute parallel for \
+   num_teams(numTeams) num_threads(numThreads) 
+  for (int i = 0; i < n; ++i) {
+    float g = grad[i] / (float)m + lambda * x[i]; 
+    x[i] = x[i] - alpha * g;
+  }
+}
+
 int main(int argc, const char *argv[]) {
   if (argc != 5) {
     printf("Usage: %s <path to file> <lambda> <alpha> <repeat>\n", argv[0]);
@@ -27,26 +101,32 @@ int main(int argc, const char *argv[]) {
 
   float *d_grad        = grad.data();
   float *d_x           = x.data();
-  int   *d_A_row_ptr   = A.row_ptr.data(); 
-  int   *d_A_col_index = A.col_index.data();
-  float *d_A_value     = A.values.data();
-  int   *d_A_y_label   = A.y_label.data();
+  int   *d_row_ptr   = A.row_ptr.data(); 
+  int   *d_col_index = A.col_index.data();
+  float *d_value     = A.values.data();
+  int   *d_y_label   = A.y_label.data();
 
   float total_obj_val[1];
   float l2_norm[1];
   int   correct[1];
 
   #pragma omp target data map (to: d_x[0:n], \
-                                   d_A_row_ptr[0:A.row_ptr.size()], \
-                                   d_A_value[0:A.values.size()], \
-                                   d_A_col_index[0:A.col_index.size()], \
-                                   d_A_y_label[0:A.y_label.size()]) \
+                                   d_row_ptr[0:A.row_ptr.size()], \
+                                   d_value[0:A.values.size()], \
+                                   d_col_index[0:A.col_index.size()], \
+                                   d_y_label[0:A.y_label.size()]) \
                           map (alloc: d_grad[0:n], \
                                       total_obj_val[0:1], \
                                       l2_norm[0:1], \
                                       correct[0:1])
 {
   long long train_start = get_time();
+
+  const int numTeams = ((m+255)/256);
+  const int numThreads = 256;
+
+  const int numTeams2 =  ((n+255)/256);
+  const int numThreads2 = 256;
 
   float obj_val = 0.f;
   float train_error = 0.f;
@@ -68,45 +148,22 @@ int main(int argc, const char *argv[]) {
     #pragma omp target update to (d_grad[0:n])
     
     // compute the total objective, correct rate, and gradient
-    #pragma omp target teams distribute parallel for thread_limit(256)
-    for (int i = 0; i < m; ++i) {
-
-      // Simple sparse matrix multiply x' = A * x
-      float xp = 0.f;
-           
-      for( int j = d_A_row_ptr[i]; j < d_A_row_ptr[i+1]; ++j) {
-        xp += d_A_value[j] * d_x[d_A_col_index[j]];
-      }
-
-      // compute objective 
-      float v = logf(1.f + expf(-xp * d_A_y_label[i]));
-      #pragma omp atomic update
-      total_obj_val[0] += v;
-
-      // compute errors
-      float prediction = 1.f / (1.f + expf(-xp));
-      int t = (prediction >= 0.5f) ? 1 : -1;
-      if (d_A_y_label[i] == t) {
-        #pragma omp atomic update
-        correct[0]++;
-      }
-
-      // compute gradient at x
-      float accum = expf(-d_A_y_label[i] * xp);
-      accum = accum / (1.f + accum);
-      for(int j = d_A_row_ptr[i]; j < d_A_row_ptr[i+1]; ++j) {
-        float temp = -accum * d_A_value[j] * d_A_y_label[i];
-        #pragma omp atomic update
-        d_grad[d_A_col_index[j]] += temp;
-      }
-    }
+    compute(
+        numTeams,
+        numThreads,
+        d_x, 
+        d_grad, 
+        d_row_ptr,
+        d_col_index,
+        d_value,
+        d_y_label,
+        total_obj_val,
+        correct,
+        m
+      ); 
 
     // display training status for verification
-    #pragma omp target teams distribute parallel for thread_limit(256)
-    for (int i = 0; i < n; ++i) {
-       #pragma omp atomic update
-       l2_norm[0] += d_x[i] * d_x[i];
-    }
+    L2_norm(numTeams2, numThreads2, d_x, l2_norm, n);
 
     #pragma omp target update from (total_obj_val[0:1])
     #pragma omp target update from (l2_norm[0:1])
@@ -116,11 +173,7 @@ int main(int argc, const char *argv[]) {
     train_error = 1.f - (correct[0]/(float)m); 
 
     // update x (gradient does not need to be updated)
-    #pragma omp target teams distribute parallel for thread_limit(256)
-    for (int i = 0; i < n; ++i) {
-      float g = d_grad[i] / (float)m + lambda * d_x[i]; 
-      d_x[i] = d_x[i] - alpha * g;
-    }
+    update(numTeams2, numThreads2, d_x, d_grad, m, n, lambda, alpha);
   }
 
   long long train_end = get_time();
