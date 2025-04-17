f686dcb087572e6c89716a1d89fabcc8ee635267
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-04-17 08:24:40 -0700

    [adamw-omp] add an OpenMP version

---
+++ src/adamw-omp/kernels.h
@@ -0,0 +1,234 @@
+static const uint8_t _bitmask = 15;
+static const uint8_t _right_pack_bitmask = _bitmask << 4;
+
+struct float2 {
+  float x;
+  float y;
+};
+
+static const float _exp_qmap [] = {
+                -0.8875,
+                -0.6625,
+                -0.4375,
+                -0.2125,
+                -0.0775,
+                -0.0325,
+                -0.0055,
+                0.0000,
+                0.0055,
+                0.0325,
+                0.0775,
+                0.2125,
+                0.4375,
+                0.6625,
+                0.8875,
+                1.0000,
+};
+
+static const float _exp_qmidpt [] = {
+
+            -0.775,
+            -0.55,
+            -0.325,
+            -0.145,
+            -0.055,
+            -0.019,
+            -0.00275,
+            0.00275,
+            0.019,
+            0.055,
+            0.145,
+            0.325,
+            0.55,
+            0.775,
+            0.94375,
+};
+
+static const float _sq_qmap [] = {
+                0.0625,
+                0.1250,
+                0.1875,
+                0.2500,
+                0.3125,
+                0.3750,
+                0.4375,
+                0.5000,
+                0.5625,
+                0.6250,
+                0.6875,
+                0.7500,
+                0.8125,
+                0.8750,
+                0.9375,
+                1.0000,
+};
+
+static const float _sq_qmidpt [] = {
+            0.09375,
+            0.15625,
+            0.21875,
+            0.28125,
+            0.34375,
+            0.40625,
+            0.46875,
+            0.53125,
+            0.59375,
+            0.65625,
+            0.71875,
+            0.78125,
+            0.84375,
+            0.90625,
+            0.96875,
+};
+
+// binary search for quantization
+inline float q_mapping(const float* __restrict__ qmap,
+                       const float* __restrict__ qmidpt,
+                       float x)
+{
+    // 4 bit range
+    int low = 0;
+    int high = 15;
+
+    if (x <= qmap[low]) return low;
+    if (qmap[high] <=x) return high;
+
+    // replace with for loop?
+    while (low < high) {
+        int mid = (low + high) >> 1;
+        if (qmap[mid] <= x)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid;
+        }
+    }
+    return (qmidpt[low-1] < x) ? low : low-1;
+}
+
+template <typename T, int block_size>
+void fused_4bit_kernel (
+    const int numTeams,
+    const int numThreads,
+    T* __restrict__ p,
+    const T* __restrict__ g,
+    T* __restrict__ exp_qscale,// m
+    T* __restrict__ sq_qscale, // v
+    int8_t* __restrict__ exp,
+    int8_t* __restrict__ sq,
+    const float beta1,
+    const float beta2,
+    const float lr,
+    const float weight_decay,
+    const float eps,
+    const float step,
+    const int64_t total_size,
+    const float correction1,
+    const float correction2_sqrt,
+    const float step_size,
+    const float weight_decay_update,
+    const float resid_beta1,
+    const float resid_beta2)
+{
+  #pragma omp target teams distribute num_teams(numTeams)
+  for (int block_id = 0; block_id < numTeams; block_id++) {  
+    float absmax_exp = 0;
+    float absmax_sq = 0;
+    float local_exp_left[block_size];
+    float local_sq_left[block_size];
+    float local_exp_right[block_size];
+    float local_sq_right[block_size];
+    #pragma omp parallel for reduction(max:absmax_sq, absmax_exp) num_threads(numThreads)
+    for (int thread_id = 0; thread_id < block_size; thread_id++) {
+      int64_t global_id = (int64_t)block_id * block_size + thread_id;
+      if (global_id < total_size) {
+        const int8_t exp_full = exp[global_id];
+        const int8_t sq_full = sq[global_id];
+
+        float2 p2 = reinterpret_cast<float2*>(p)[global_id];
+        const float2 g2 = reinterpret_cast<const float2*>(g)[global_id];
+
+        // left side processing -------------------------------------
+        const int8_t exp_left_index = exp_full & _bitmask;
+        const int8_t sq_left_index = sq_full & _bitmask;
+
+        //decoupled weight decay
+        p2.x = p2.x * weight_decay_update;
+
+        // left exp and sq updates
+        float exp_avg_qscale = exp_qscale[block_id];
+
+        float exp_left = _exp_qmap[exp_left_index] * exp_avg_qscale;
+        exp_left = beta1 * exp_left + resid_beta1 * g2.x;
+
+        float sq_left = _sq_qmap[sq_left_index] * sq_qscale[block_id];
+        sq_left = beta2 * sq_left + resid_beta2 * (g2.x * g2.x);
+
+        // param update
+        p[global_id*2] = p2.x - (step_size * (exp_left/(sqrtf(sq_left) / correction2_sqrt + eps)));
+
+        // right side processing -------------------------------
+
+        const int8_t exp_right_index = (exp_full >> 4) & _bitmask;
+        const int8_t sq_right_index = (sq_full >> 4) & _bitmask;
+
+        //decoupled weight decay, right side
+        p2.y = p2.y * weight_decay_update;
+
+        float exp_right = _exp_qmap[exp_right_index] * exp_avg_qscale;
+        exp_right = beta1 * exp_right + resid_beta1 * g2.y;
+
+        float sq_right = _sq_qmap[sq_right_index] * sq_qscale[block_id];
+        sq_right = beta2 * sq_right + resid_beta2 * (g2.y * g2.y);
+
+        // param update
+        p[global_id*2+1] = p2.y - (step_size * (exp_right/(sqrtf(sq_right) / correction2_sqrt + eps)));
+
+        // prepare quantization info - update absmax scales
+        float local_absmax_exp = fmaxf(exp_left, exp_right);
+        float local_absmax_sq = fmaxf(sq_left, sq_right);
+
+        local_exp_left[thread_id] = exp_left;
+        local_exp_right[thread_id] = exp_right;
+        local_sq_left[thread_id] = sq_left;
+        local_sq_right[thread_id] = sq_right;
+
+        // determine absmax for exp
+        absmax_exp = fmaxf(absmax_exp, local_absmax_exp);
+
+        // same for sq
+        absmax_sq = fmaxf(absmax_sq, local_absmax_sq);
+      }
+    }
+
+    exp_qscale[block_id] = absmax_exp;
+    sq_qscale[block_id] = absmax_sq;
+
+    #pragma omp parallel for num_threads(numThreads)
+    for (int thread_id = 0; thread_id < block_size; thread_id++) {
+      int64_t global_id = (int64_t)block_id * block_size + thread_id;
+      if (global_id < total_size) {
+
+        int8_t local_packed_exp = 0;
+        int8_t local_packed_sq = 0;
+
+        // quantize and pack
+        const int8_t q_exp_left = (int8_t)q_mapping (_exp_qmap, _exp_qmidpt, (float)local_exp_left[thread_id] / absmax_exp);
+        const int8_t q_sq_left = (int8_t)q_mapping (_sq_qmap, _sq_qmidpt, (float)local_sq_left[thread_id] / absmax_sq);
+        local_packed_exp |= (q_exp_left & _bitmask);
+        local_packed_sq |= (q_sq_left & _bitmask);
+
+        const int8_t q_exp_right = (int8_t)q_mapping (_exp_qmap, _exp_qmidpt, (float)local_exp_right[thread_id] / absmax_exp);
+        const int8_t q_sq_right = (int8_t)q_mapping (_sq_qmap, _sq_qmidpt, (float)local_sq_right[thread_id] / absmax_sq);
+        local_packed_exp |= (q_exp_right & _right_pack_bitmask);
+        local_packed_sq |= (q_sq_right & _right_pack_bitmask);
+
+        // store updated exp and sq
+        exp[global_id] = local_packed_exp;
+        sq[global_id] = local_packed_sq;
+      }
+    }
+  }
+}
---
+++ src/adamw-omp/main.cpp
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <chrono>
+#include <random>
+#include <omp.h>
+#include "kernels.h"
+#include "reference.h"
+
+int main(int argc, char* argv[])
+{
+  if (argc != 3) {
+    printf("Usage: %s <vector size> <number of time steps>\n", argv[0]);
+    return 1;
+  }
+
+  // assume each vector element contains two 4-bit quantized numbers
+  const long vector_size = atol(argv[1]);
+  const int time_step = atoi(argv[2]);
+
+  int64_t size = vector_size * 2;
+  int64_t size_bytes = size * sizeof(float);
+
+  float *g = (float*) malloc (size_bytes);
+  float *p = (float*) malloc (size_bytes);
+  float *p_ref = (float*) malloc (size_bytes);
+  float *m_qscale = (float*) malloc (size_bytes);
+  float *v_qscale = (float*) malloc (size_bytes);
+  int8_t *m = (int8_t*) malloc (vector_size);
+  int8_t *v = (int8_t*) malloc (vector_size);
+  float *r = (float*) malloc (size_bytes);
+
+  std::mt19937 gen(19937);
+  std::uniform_real_distribution<float> dist(0, 1);
+  for (int64_t i = 0; i < size; i++) {
+    m_qscale[i] = dist(gen);
+    v_qscale[i] = dist(gen);
+    g[i] = dist(gen);
+    r[i] = p[i] = p_ref[i] = dist(gen);
+  }
+
+  for (int64_t i = 0; i < vector_size; i++) {
+    m[i] = 256 * dist(gen);
+    v[i] = 256 * dist(gen);
+  }
+
+  #pragma omp target data map(to: m_qscale[0:size], v_qscale[0:size], \
+                                  g[0:size], p[0:size],\
+                                  m[0:vector_size], v[0:vector_size])
+  {
+    const int threadsPerBlock = 64; // fixed at 64
+    const int blocksPerGrid = (vector_size+threadsPerBlock-1) / threadsPerBlock;
+    const int numTeams = blocksPerGrid;
+    const int numThreads = threadsPerBlock;
+
+    // default constants
+    const float lr = 1e-3f;
+    const float weight_decay = 1e-2f;
+    const float beta1 = 0.9f;
+    const float beta2 = 0.999f;
+    const float eps = 1e-8f;
+    const float resid_beta1 = 1.0f - beta1;
+    const float resid_beta2 = 1.0f - beta2;
+    const float weight_decay_update = 1.0f - lr * weight_decay;
+
+    for (int step = 1; step <= time_step; step++) {
+
+      const float correction1 = 1.0f - powf(beta1, step);
+      const float correction2_sqrt = sqrtf(1.0f - powf(beta2, step));
+      const float step_size = lr / correction1;
+
+      fused_4bit_kernel<float, threadsPerBlock>(
+                numTeams,
+                numThreads,
+                p,
+                g,
+                m_qscale,
+                v_qscale,
+                m,
+                v,
+                beta1,
+                beta2,
+                lr,
+                weight_decay,
+                eps,
+                step,
+                vector_size,
+                correction1,
+                correction2_sqrt,
+                step_size,
+                weight_decay_update,
+                resid_beta1,
+                resid_beta2);
+
+      reference<float, threadsPerBlock>(
+                blocksPerGrid,
+                p_ref,
+                g,
+                m_qscale,
+                v_qscale,
+                m,
+                v,
+                beta1,
+                beta2,
+                lr,
+                weight_decay,
+                eps,
+                step,
+                vector_size,
+                correction1,
+                correction2_sqrt,
+                step_size,
+                weight_decay_update,
+                resid_beta1,
+                resid_beta2);
+    }
+
+    #pragma omp target update from (p[0:size]) 
+
+    float absmax_error = 0;
+    for (int64_t i = 0; i < size; i++) {
+      absmax_error = fmaxf(absmax_error, fabsf(p[i] - p_ref[i]));
+    }
+    printf("Absolute maximum error: %f\n", absmax_error);
+    printf("%s\n", absmax_error > 1e-3f ? "FAIL" : "PASS");
+
+    auto start = std::chrono::steady_clock::now();
+
+    for (int step = 1; step <= time_step; step++) {
+
+      const float correction1 = 1.0f - powf(beta1, step);
+      const float correction2_sqrt = sqrtf(1.0f - powf(beta2, step));
+      const float step_size = lr / correction1;
+
+      fused_4bit_kernel<float, threadsPerBlock>(
+                numTeams,
+                numThreads,
+                p,
+                g,
+                m_qscale,
+                v_qscale,
+                m,
+                v,
+                beta1,
+                beta2,
+                lr,
+                weight_decay,
+                eps,
+                step,
+                vector_size,
+                correction1,
+                correction2_sqrt,
+                step_size,
+                weight_decay_update,
+                resid_beta1,
+                resid_beta2);
+    }
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Average kernel execution time %f (ms)\n", time * 1e-6f / time_step);
+  }
+
+  free(p);
+  free(p_ref);
+  free(m_qscale);
+  free(v_qscale);
+  free(m);
+  free(v);
+  free(g);
+  free(r);
+  return 0;
+}
