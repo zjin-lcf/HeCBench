36b3b728250eedb1f56dbcb80ee9ea21cf33152a
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-06-10 06:50:52 -0700

    [rng-wallace] update the kernels and kernel timing slightly

--- src/rng-wallace-cuda/main.cu
+++ src/rng-wallace-cuda/main.cu
@@ -46,6 +46,7 @@ int main(int argc, char* argv[]) {
   dim3 rng_wallace_threads(WALLACE_NUM_THREADS, 1, 1);
   const unsigned seed = 1;
 
+  cudaDeviceSynchronize();
   auto start = std::chrono::steady_clock::now();
 
   for (int i = 0; i < repeat; i++) {
@@ -56,7 +57,7 @@ int main(int argc, char* argv[]) {
   cudaDeviceSynchronize();
   auto end = std::chrono::steady_clock::now();
   auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  printf("Average kernel execution time: %f (s)\n", time * 1e-9f / repeat);
+  printf("Average kernel execution time: %f (us)\n", time * 1e-3f / repeat);
 
   cudaMemcpy(randomNumbers, device_randomNumbers,
              4 * WALLACE_OUTPUT_SIZE, cudaMemcpyDeviceToHost);
