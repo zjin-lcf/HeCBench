58ba22985ac9513bb14857425225ae78916ba78c
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-10-09 10:39:51 -0700

    [gd] update the benchmarks, and add a reference to verify the device results

--- src/gd-omp/main.cpp
+++ src/gd-omp/main.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <omp.h>
 #include "utils.h"
+#include "reference.h"
 
 int main(int argc, const char *argv[]) {
   if (argc != 5) {
@@ -45,91 +46,92 @@ int main(int argc, const char *argv[]) {
                                       total_obj_val[0:1], \
                                       l2_norm[0:1], \
                                       correct[0:1])
-{
-  long long train_start = get_time();
-
-  float obj_val = 0.f;
-  float train_error = 0.f;
-
-  for (int k = 0; k < iters; k++) {
-
-    // reset the training status
-    total_obj_val[0] = 0.f;
-    l2_norm[0] = 0.f;
-    correct[0] = 0;
-    
-    #pragma omp target update to (total_obj_val[0:1])
-    #pragma omp target update to (l2_norm[0:1])
-    #pragma omp target update to (correct[0:1])
-
-    //reset gradient vector
-    std::fill(grad.begin(), grad.end(), 0.f);
-
-    #pragma omp target update to (d_grad[0:n])
-    
-    // compute the total objective, correct rate, and gradient
-    #pragma omp target teams distribute parallel for thread_limit(256)
-    for (int i = 0; i < m; ++i) {
-
-      // Simple sparse matrix multiply x' = A * x
-      float xp = 0.f;
-           
-      for( int j = d_A_row_ptr[i]; j < d_A_row_ptr[i+1]; ++j) {
-        xp += d_A_value[j] * d_x[d_A_col_index[j]];
-      }
-
-      // compute objective 
-      float v = logf(1.f + expf(-xp * d_A_y_label[i]));
-      #pragma omp atomic update
-      total_obj_val[0] += v;
-
-      // compute errors
-      float prediction = 1.f / (1.f + expf(-xp));
-      int t = (prediction >= 0.5f) ? 1 : -1;
-      if (d_A_y_label[i] == t) {
+  {
+    long long train_start = get_time();
+  
+    float obj_val = 0.f;
+    float train_error = 0.f;
+  
+    for (int k = 0; k < iters; k++) {
+  
+      // reset the training status
+      total_obj_val[0] = 0.f;
+      l2_norm[0] = 0.f;
+      correct[0] = 0;
+      
+      #pragma omp target update to (total_obj_val[0:1])
+      #pragma omp target update to (l2_norm[0:1])
+      #pragma omp target update to (correct[0:1])
+  
+      //reset gradient vector
+      std::fill(grad.begin(), grad.end(), 0.f);
+  
+      #pragma omp target update to (d_grad[0:n])
+      
+      // compute the total objective, correct rate, and gradient
+      #pragma omp target teams distribute parallel for thread_limit(256)
+      for (int i = 0; i < m; ++i) {
+  
+        // Simple sparse matrix multiply x' = A * x
+        float xp = 0.f;
+             
+        for( int j = d_A_row_ptr[i]; j < d_A_row_ptr[i+1]; ++j) {
+          xp += d_A_value[j] * d_x[d_A_col_index[j]];
+        }
+  
+        // compute objective 
+        float v = logf(1.f + expf(-xp * d_A_y_label[i]));
         #pragma omp atomic update
-        correct[0]++;
+        total_obj_val[0] += v;
+  
+        // compute errors
+        float prediction = 1.f / (1.f + expf(-xp));
+        int t = (prediction >= 0.5f) ? 1 : -1;
+        if (d_A_y_label[i] == t) {
+          #pragma omp atomic update
+          correct[0]++;
+        }
+  
+        // compute gradient at x
+        float accum = expf(-d_A_y_label[i] * xp);
+        accum = accum / (1.f + accum);
+        for(int j = d_A_row_ptr[i]; j < d_A_row_ptr[i+1]; ++j) {
+          float temp = -accum * d_A_value[j] * d_A_y_label[i];
+          #pragma omp atomic update
+          d_grad[d_A_col_index[j]] += temp;
+        }
       }
-
-      // compute gradient at x
-      float accum = expf(-d_A_y_label[i] * xp);
-      accum = accum / (1.f + accum);
-      for(int j = d_A_row_ptr[i]; j < d_A_row_ptr[i+1]; ++j) {
-        float temp = -accum * d_A_value[j] * d_A_y_label[i];
-        #pragma omp atomic update
-        d_grad[d_A_col_index[j]] += temp;
+  
+      // display training status for verification
+      #pragma omp target teams distribute parallel for thread_limit(256)
+      for (int i = 0; i < n; ++i) {
+         #pragma omp atomic update
+         l2_norm[0] += d_x[i] * d_x[i];
+      }
+  
+      #pragma omp target update from (total_obj_val[0:1])
+      #pragma omp target update from (l2_norm[0:1])
+      #pragma omp target update from (correct[0:1])
+  
+      obj_val = total_obj_val[0] / (float)m + 0.5f * lambda * l2_norm[0];
+      train_error = 1.f - (correct[0]/(float)m); 
+  
+      // update x (gradient does not need to be updated)
+      #pragma omp target teams distribute parallel for thread_limit(256)
+      for (int i = 0; i < n; ++i) {
+        float g = d_grad[i] / (float)m + lambda * d_x[i]; 
+        d_x[i] = d_x[i] - alpha * g;
       }
     }
+  
+    long long train_end = get_time();
+    printf("Training time takes %lf (s) for %d iterations\n\n",
+           (train_end - train_start) * 1e-6, iters);
+  
+    printf("object value = %f train_error = %f\n", obj_val, train_error);
 
-    // display training status for verification
-    #pragma omp target teams distribute parallel for thread_limit(256)
-    for (int i = 0; i < n; ++i) {
-       #pragma omp atomic update
-       l2_norm[0] += d_x[i] * d_x[i];
-    }
-
-    #pragma omp target update from (total_obj_val[0:1])
-    #pragma omp target update from (l2_norm[0:1])
-    #pragma omp target update from (correct[0:1])
-
-    obj_val = total_obj_val[0] / (float)m + 0.5f * lambda * l2_norm[0];
-    train_error = 1.f - (correct[0]/(float)m); 
-
-    // update x (gradient does not need to be updated)
-    #pragma omp target teams distribute parallel for thread_limit(256)
-    for (int i = 0; i < n; ++i) {
-      float g = d_grad[i] / (float)m + lambda * d_x[i]; 
-      d_x[i] = d_x[i] - alpha * g;
-    }
+    reference(A, x, grad, m, n, iters, alpha, lambda, obj_val, train_error);
   }
-
-  long long train_end = get_time();
-  printf("Training time takes %lf (s) for %d iterations\n\n",
-         (train_end - train_start) * 1e-6, iters);
-
-  // After 100 iterations, the expected obj_val and train_error are 0.3358405828 and 0.07433331013
-  printf("object value = %f train_error = %f\n", obj_val, train_error);
-}
 
   return 0; 
 }
