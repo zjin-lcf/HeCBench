8d3f4ac8ad3e833fcad0f9e01f5ea1af34e23684
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-10-27 13:55:38 -0700

    [page-rank] remove unused variables

--- src/page-rank-cuda/main.cu
+++ src/page-rank-cuda/main.cu
@@ -158,7 +158,6 @@ int main(int argc, char *argv[]) {
   int iter = max_iter;
   float thresh = threshold;
   int divisor = 2;
-  int nb_links = 0;
 
   int opt, opt_index = 0;
   while((opt = getopt_long(argc, argv, "::n:i:t:q:", size_opts, &opt_index)) != -1){
@@ -197,13 +196,6 @@ int main(int argc, char *argv[]) {
   }
   pages = random_pages(n,noutlinks,divisor);
   init_array(page_ranks, n, 1.0f / (float) n);
-
-  nb_links = 0;
-  for (i=0; i<n; ++i) {
-    for (j=0; j<n; ++j) {
-      nb_links += pages[(size_t)i*n+j];
-    }
-  }
 
   float *diffs;
   diffs = (float*) malloc(rank_size);
