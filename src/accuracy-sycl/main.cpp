342e046d8c3cee8835a83828298aa9da47f4b0e8
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-10-28 15:04:33 -0700

    [accuracy] add the restrict keyword to CUDA, HIP and SYCL kernels

--- src/accuracy-sycl/main.cpp
+++ src/accuracy-sycl/main.cpp
@@ -17,8 +17,8 @@ void accuracy_kernel(
     const int N,
     const int D,
     const int top_k,
-    const float* Xdata,
-    const int* labelData,
+    const float* __restrict Xdata,
+    const int* __restrict labelData,
     int* accuracy)
 {
   int count = 0;
