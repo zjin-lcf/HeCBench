e1dfac5db1f221a144cb6af7b81f9357f14143f1
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-11-04 15:52:28 -0800

    [moe] update the RNG, reference, and initialization of topk indices

--- src/moe-cuda/reference.h
+++ src/moe-cuda/reference.h
@@ -1,5 +1,3 @@
-#include <cmath>
-
 void moeSoftmax_reference(
     const float* input,
     const bool* finished,
@@ -60,21 +58,20 @@ kvp arg_max(const kvp &a, const kvp &b) {
 void moeTopK_reference(
     const float* inputs_after_softmax,
     const bool* finished,
-    float* output,
+    float* weights,
     int* __restrict__ indices,
     int* __restrict__ source_rows,
     const int num_tokens,
     const int num_experts,
-    const int k,
+    const int K,
     const int start_expert,
     const int end_expert)
 {
   kvp thread_kvp;
-
   for (int token = 0; token < num_tokens; token++) {
     const bool row_is_active = finished ? !finished[token] : true;
     const int thread_read_offset = token * num_experts;
-    for (int k_idx = 0; k_idx < k; ++k_idx) {
+    for (int k_idx = 0; k_idx < K; ++k_idx) {
       thread_kvp.key = 0;
       thread_kvp.value = -1.f;  // This is OK because inputs are probabilities
 
@@ -85,7 +82,7 @@ void moeTopK_reference(
         inp_kvp.value = inputs_after_softmax[idx];
 
         for (int prior_k = 0; prior_k < k_idx; ++prior_k) {
-          const int prior_winning_expert = indices[k * token + prior_k];
+          const int prior_winning_expert = indices[K * token + prior_k];
           if (prior_winning_expert == expert) {
             inp_kvp = thread_kvp;
           }
@@ -97,8 +94,8 @@ void moeTopK_reference(
       const bool node_uses_expert = expert >= start_expert && expert < end_expert;
       const bool should_process_row = row_is_active && node_uses_expert;
 
-      const int idx = k * token + k_idx;
-      output[idx] = thread_kvp.value;
+      const int idx = token * K + k_idx;
+      weights[idx] = thread_kvp.value;
       indices[idx] = should_process_row ? (expert - start_expert) : num_experts;
       assert(indices[idx] >= 0);
       source_rows[idx] = k_idx * num_tokens + token;
