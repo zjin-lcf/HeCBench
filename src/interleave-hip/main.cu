d6964f11de988112e7d2aeb016f4292e8690ca3c
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-12-17 10:34:49 -0500

    [interleave] rename the utility

--- src/interleave-hip/main.cu
+++ src/interleave-hip/main.cu
@@ -56,7 +56,7 @@ typedef struct
 } NON_INTERLEAVED_T;
 
 // data initialization and verification
-#include "util.cpp"
+#include "util.h"
 
 __global__ void add_kernel_interleaved(
     INTERLEAVED_T * const dest_ptr,
