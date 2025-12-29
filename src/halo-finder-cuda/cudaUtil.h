cb1deb5f1fdc67d4e0c8a588caed36c3f8d80570
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-12-29 13:42:27 -0500

    [halo-finder-cuda] update the shuffle API

--- src/halo-finder-cuda/cudaUtil.h
+++ src/halo-finder-cuda/cudaUtil.h
@@ -2,10 +2,10 @@
 #pragma once
 #include <stdio.h>
 
-#define cudaCheckError() {                                                       \
- cudaError_t e=cudaGetLastError();                                                \
- if(e!=cudaSuccess) {                                                            \
-   printf("CUDA failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));  \
+#define cudaCheckError() {                                                      \
+ cudaError_t e=cudaGetLastError();                                              \
+ if(e!=cudaSuccess) {                                                           \
+   printf("CUDA failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));\
    exit(0);                                                                     \
  }                                                                              \
 }
@@ -13,11 +13,11 @@
 __device__  __forceinline__
 void atomicWarpReduceAndUpdate(POSVEL_T *out, POSVEL_T val) {
   //perform shfl reduction
-  val+=__shfl_down(val, 16); 
-  val+=__shfl_down(val, 8); 
-  val+=__shfl_down(val, 4);
-  val+=__shfl_down(val, 2); 
-  val+=__shfl_down(val, 1);
+  val+=__shfl_down_sync(0xFFFFFFFF, val, 16);
+  val+=__shfl_down_sync(0xFFFFFFFF, val, 8);
+  val+=__shfl_down_sync(0xFFFFFFFF, val, 4);
+  val+=__shfl_down_sync(0xFFFFFFFF, val, 2);
+  val+=__shfl_down_sync(0xFFFFFFFF, val, 1);
 
   if(threadIdx.x%32==0)
     atomicAdd(out,val);  //atomics are unecessary but they are faster than non-atomics due to a single bus transaction
@@ -30,7 +30,7 @@ class cudaDeviceSelector {
     int local_rank = 0;
     int numDev=1;
 
-    //No MPI at this time so go by enviornment variables. 
+    //No MPI at this time so go by enviornment variables.
     //This may need to be updated to match your MPI flavor
     if((str = getenv("MV2_COMM_WORLD_LOCAL_RANK")) != NULL) {
       local_rank = atoi(str);
@@ -55,11 +55,11 @@ class cudaDeviceSelector {
     sprintf(var,"/tmp/nvidia-mps_%d",local_rank%numDev);
     setenv("CUDA_MPS_PIPE_DIRECTORY",var,1);
 #endif
-#else 
+#else
     int dev;
-    //set via local MPI rank 
+    //set via local MPI rank
     dev = local_rank % numDev;
- 
+
     //we must set this for all threads
     cudaSetDevice(dev);
 #endif
