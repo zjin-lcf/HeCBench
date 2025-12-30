7328f71f6595f1f88a9fb2ef3a80f0c20f6dd10b
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-12-30 16:58:11 -0500

    [is] add the string header

--- src/is-hip/main.cu
+++ src/is-hip/main.cu
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <chrono>
 #include <hip/hip_runtime.h>
 #include "is.h"
--- src/is-sycl/main.cpp
+++ src/is-sycl/main.cpp
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <chrono>
 #include <sycl/sycl.hpp>
 #include "is.h"
