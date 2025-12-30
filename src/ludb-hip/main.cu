048c4e4c3f8fa772e2ac6b6646e7b2c6f1bb71f7
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-12-30 17:14:12 -0500

    [ludb] add the header for memset

--- src/ludb-hip/main.cu
+++ src/ludb-hip/main.cu
@@ -43,6 +43,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <chrono>
 
 // rocm libraries and helpers
--- src/ludb-sycl/main.cpp
+++ src/ludb-sycl/main.cpp
@@ -43,6 +43,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <chrono>
 
 // oneAPI libraries and helpers
