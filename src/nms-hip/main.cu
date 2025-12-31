76a7fca93329ce802a25ba0f4ca8f889474a6365
Zheming Jin <zjin-lcf@users.noreply.github.com>
2025-12-31 11:22:04 -0500

    [nms-hip] add the header for memset

--- src/nms-hip/main.cu
+++ src/nms-hip/main.cu
@@ -19,6 +19,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <chrono>
 #include <hip/hip_runtime.h>
 
--- src/nms-sycl/main.cpp
+++ src/nms-sycl/main.cpp
@@ -19,6 +19,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <chrono>
 #include <sycl/sycl.hpp>
 
