d7f82de2ba6a6b048ffa1e62bc80cbe45f4555cb
Zheming Jin <zjin-lcf@users.noreply.github.com>
2026-01-15 12:35:50 -0500

    [linearprobing-sycl] address result correctness issues

--- src/linearprobing-sycl/linearprobing.h
+++ src/linearprobing-sycl/linearprobing.h
@@ -4,8 +4,8 @@
 
 struct KeyValue
 {
-  uint32_t key;
-  uint32_t value;
+    uint32_t key;
+    uint32_t value;
 };
 
 const uint32_t kHashTableCapacity = 64*1024*1024; //128 * 1024 * 1024;
@@ -16,12 +16,12 @@ const uint32_t kEmpty = 0xffffffff;
 
 double insert_hashtable(sycl::queue &q,
                         KeyValue *pHashTable,
-                        KeyValue *kvs,
+                        const KeyValue *kvs,
                         uint32_t num_kvs);
 
 double delete_hashtable(sycl::queue &q,
                         KeyValue *pHashTable,
-                        KeyValue *kvs,
+                        const KeyValue *kvs,
                         uint32_t num_kvs);
 
 std::vector<KeyValue> iterate_hashtable(sycl::queue &q, KeyValue *pHashTable);
