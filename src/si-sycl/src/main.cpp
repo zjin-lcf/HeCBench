7872afef12ed84f971ef2729cc10ae902714ce1d
Zheming Jin <zjin-lcf@users.noreply.github.com>
2026-01-16 18:30:00 -0500

    [si] update the examples with the latest cxxopts

--- src/si-sycl/src/main.cpp
+++ src/si-sycl/src/main.cpp
@@ -194,7 +194,7 @@ int main(int argc, char **argv) {
     delete [] hostInvInput; 
     delete [] d->offsets;
 
-  } catch (const cxxopts::OptionException& e) {
+  } catch (const cxxopts::exceptions::exception& e) {
     fmt::print("{}\n", e.what());
     return 1;
   }
