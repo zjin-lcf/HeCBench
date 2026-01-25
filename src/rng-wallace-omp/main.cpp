e54ee38f012f80922f1a532445510bbdcaf0b93e
Zheming Jin <zjin-lcf@users.noreply.github.com>
2026-01-25 09:42:28 -0500

    [rng-wallace] verify the device results with a reference

--- src/rng-wallace-omp/main.cpp
+++ src/rng-wallace-omp/main.cpp
@@ -6,28 +6,8 @@
 #include <omp.h>
 #include "rand_helpers.h"
 #include "constants.h"
-
-#define mul24(a,b) (a)*(b)
-
-#pragma omp declare target
-void Hadamard4x4a(float &p, float &q, float &r, float &s)
-{
-  float t = (p + q + r + s) / 2.f;
-  p = p - t;
-  q = q - t;
-  r = t - r;
-  s = t - s;
-}
-
-void Hadamard4x4b(float &p, float &q, float &r, float &s)
-{
-  float t = (p + q + r + s) / 2.f;
-  p = t - p;
-  q = t - q;
-  r = r - t;
-  s = s - t;
-}
-#pragma omp end declare target
+#include "wallace_kernel.h"
+#include "reference.h"
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -38,10 +18,12 @@ int main(int argc, char* argv[]) {
 
   // host buffers
   float *globalPool = (float *) malloc(4 * WALLACE_TOTAL_POOL_SIZE);
+  float *Pool_ref = (float *) malloc(4 * WALLACE_TOTAL_POOL_SIZE);
+
   for (unsigned i = 0; i < WALLACE_TOTAL_POOL_SIZE; i++)
   {
     float x = RandN();
-    globalPool[i] = x;
+    Pool_ref[i] = globalPool[i] = x;
   }
 
   float* rngChi2Corrections = (float *) malloc(4 * WALLACE_CHI2_COUNT);
@@ -50,122 +32,60 @@ int main(int argc, char* argv[]) {
     rngChi2Corrections[i] = MakeChi2Scale(WALLACE_TOTAL_POOL_SIZE);
   }
   float* randomNumbers = (float *) malloc(4 * WALLACE_OUTPUT_SIZE);
+  float* randomNumbers_ref = (float *) malloc(4 * WALLACE_OUTPUT_SIZE);
 
   float *chi2Corrections = rngChi2Corrections;
 
-  const unsigned m_seed = 1;
+  const unsigned seed = 1;
 
   #pragma omp target data map(to: globalPool[0: WALLACE_TOTAL_POOL_SIZE], \
                                   chi2Corrections[0:WALLACE_CHI2_COUNT]) \
                           map(alloc: randomNumbers[0: WALLACE_OUTPUT_SIZE])
   {
+    // warmup and quick validation
+    for (int i = 0; i < 30; i++) {
+      rng_wallace(seed, globalPool, randomNumbers, chi2Corrections);
+    }
+
+    for (int i = 0; i < 30; i++) {
+      reference(seed, Pool_ref, randomNumbers_ref, rngChi2Corrections, WALLACE_NUM_BLOCKS);
+    }
+
+    #pragma omp target update from(randomNumbers[0:WALLACE_OUTPUT_SIZE])
+    #pragma omp target update from(globalPool[0:WALLACE_TOTAL_POOL_SIZE])
+
+    bool ok = true;
+    for (unsigned int n = 0; n < WALLACE_OUTPUT_SIZE; n++) {
+      if (fabsf(randomNumbers_ref[n] - randomNumbers[n]) > 1e-3f) {
+        printf("randNumbers mismatch at index %d: %f %f\n", n, randomNumbers_ref[n], randomNumbers[n]);
+        ok = false;
+        break;
+      }
+    }
+    for (unsigned int n = 0; n < WALLACE_TOTAL_POOL_SIZE; n++) {
+      if (fabsf(Pool_ref[n] - globalPool[n]) > 1e-3f) {
+        printf("Pool mismatch at index %d: %f %f\n", n, Pool_ref[n], globalPool[n]);
+        ok = false;
+        break;
+      }
+    }
+    printf("%s\n", ok ? "PASS" : "FAIL");
+
     auto start = std::chrono::steady_clock::now();
 
     for (int i = 0; i < repeat; i++) {
-      #pragma omp target teams num_teams(WALLACE_NUM_BLOCKS) thread_limit(WALLACE_NUM_THREADS)
-      {
-        float pool[WALLACE_POOL_SIZE + WALLACE_CHI2_SHARED_SIZE];
-        #pragma omp parallel
-        {
-          const unsigned lcg_a = 241;
-          const unsigned lcg_c = 59;
-          const unsigned lcg_m = 256;
-          const unsigned mod_mask = lcg_m - 1;
-
-          const unsigned lid = omp_get_thread_num();
-          const unsigned gid = omp_get_team_num();
-          const unsigned offset = mul24(WALLACE_POOL_SIZE, gid);
-
-          #pragma unroll
-          for (unsigned i = 0; i < 8; i++)
-            pool[lid + WALLACE_NUM_THREADS * i] = globalPool[offset + lid + WALLACE_NUM_THREADS * i];
-
-          #pragma omp barrier
-
-          unsigned t_seed = m_seed;
-
-          // Loop generating generatedRandomNumberPools repeatedly
-          for (unsigned loop = 0; loop < WALLACE_NUM_OUTPUTS_PER_RUN; loop++)
-          {
-            t_seed = (1664525U * t_seed + 1013904223U) & 0xFFFFFFFF;
-
-            unsigned intermediate_address = mul24(loop, 8 * WALLACE_TOTAL_NUM_THREADS) + 
-              mul24(8 * WALLACE_NUM_THREADS, gid) + lid;
-
-            if (lid == 0)
-              pool[WALLACE_CHI2_OFFSET] = chi2Corrections[mul24(gid, WALLACE_NUM_OUTPUTS_PER_RUN) + loop];
-
-            #pragma omp barrier
-            float chi2CorrAndScale = pool[WALLACE_CHI2_OFFSET];
-            for (unsigned i = 0; i < 8; i++)
-            {
-              randomNumbers[intermediate_address + i * WALLACE_NUM_THREADS] = 
-                pool[mul24(i, WALLACE_NUM_THREADS) + lid] * chi2CorrAndScale;
-            }
-
-            float rin0_0, rin1_0, rin2_0, rin3_0, rin0_1, rin1_1, rin2_1, rin3_1;
-            for (unsigned i = 0; i < WALLACE_NUM_POOL_PASSES; i++)
-            {
-              unsigned seed = (t_seed + lid) & mod_mask;
-              #pragma omp barrier
-              seed = (mul24(seed, lcg_a) + lcg_c) & mod_mask;
-              rin0_0 = pool[((seed << 3))];
-              seed = (mul24(seed, lcg_a) + lcg_c) & mod_mask;
-              rin1_0 = pool[((seed << 3) + 1)];
-              seed = (mul24(seed, lcg_a) + lcg_c) & mod_mask;
-              rin2_0 = pool[((seed << 3) + 2)];
-              seed = (mul24(seed, lcg_a) + lcg_c) & mod_mask;
-              rin3_0 = pool[((seed << 3) + 3)];
-              seed = (mul24(seed, lcg_a) + lcg_c) & mod_mask;
-              rin0_1 = pool[((seed << 3) + 4)];
-              seed = (mul24(seed, lcg_a) + lcg_c) & mod_mask;
-              rin1_1 = pool[((seed << 3) + 5)];
-              seed = (mul24(seed, lcg_a) + lcg_c) & mod_mask;
-              rin2_1 = pool[((seed << 3) + 6)];
-              seed = (mul24(seed, lcg_a) + lcg_c) & mod_mask;
-              rin3_1 = pool[((seed << 3) + 7)];
-
-              #pragma omp barrier
-
-              Hadamard4x4a(rin0_0, rin1_0, rin2_0, rin3_0);
-              pool[0 * WALLACE_NUM_THREADS + lid] = rin0_0;
-              pool[1 * WALLACE_NUM_THREADS + lid] = rin1_0;
-              pool[2 * WALLACE_NUM_THREADS + lid] = rin2_0;
-              pool[3 * WALLACE_NUM_THREADS + lid] = rin3_0;
-
-              Hadamard4x4b(rin0_1, rin1_1, rin2_1, rin3_1);
-              pool[4 * WALLACE_NUM_THREADS + lid] = rin0_1;
-              pool[5 * WALLACE_NUM_THREADS + lid] = rin1_1;
-              pool[6 * WALLACE_NUM_THREADS + lid] = rin2_1;
-              pool[7 * WALLACE_NUM_THREADS + lid] = rin3_1;
-
-              #pragma omp barrier
-            }
-          }
-          #pragma omp barrier
-
-          #pragma unroll
-          for (unsigned i = 0; i < 8; i++)
-            globalPool[offset + lid + WALLACE_NUM_THREADS * i] = pool[lid + WALLACE_NUM_THREADS * i];
-        }
-      }
+      rng_wallace(seed, globalPool, randomNumbers, chi2Corrections);
     }
 
     auto end = std::chrono::steady_clock::now();
     auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
     printf("Average kernel execution time: %f (us)\n", time * 1e-3f / repeat);
-
-    #pragma omp target update from(randomNumbers[0:WALLACE_OUTPUT_SIZE])
-
-    #ifdef DEBUG
-      // random numbers are different for each i iteration 
-      for (unsigned int n = 0; n < WALLACE_OUTPUT_SIZE; n++) 
-        printf("%.3f\n", randomNumbers[n]);
-    #endif
   }
 
   free(rngChi2Corrections);
   free(randomNumbers);
+  free(randomNumbers_ref);
   free(globalPool);
+  free(Pool_ref);
   return 0;
 }
--- src/rng-wallace-sycl/main.cpp
+++ src/rng-wallace-sycl/main.cpp
@@ -6,24 +6,8 @@
 #include <sycl/sycl.hpp>
 #include "rand_helpers.h"
 #include "constants.h"
-
-void Hadamard4x4a(float &p, float &q, float &r, float &s)
-{
-  float t = (p + q + r + s) / 2.f;
-  p = p - t;
-  q = q - t;
-  r = t - r;
-  s = t - s;
-}
-
-void Hadamard4x4b(float &p, float &q, float &r, float &s)
-{
-  float t = (p + q + r + s) / 2.f;
-  p = t - p;
-  q = t - q;
-  r = r - t;
-  s = s - t;
-}
+#include "wallace_kernel.h"
+#include "reference.h"
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -34,18 +18,20 @@ int main(int argc, char* argv[]) {
 
   // host buffers
   float *hostPool = (float *) malloc(4 * WALLACE_TOTAL_POOL_SIZE);
+  float *Pool_ref = (float *) malloc(4 * WALLACE_TOTAL_POOL_SIZE);
   for (unsigned i = 0; i < WALLACE_TOTAL_POOL_SIZE; i++)
   {
-  	float x = RandN();
-  	hostPool[i] = x;
+    float x = RandN();
+    Pool_ref[i] = hostPool[i] = x;
   }
 
   float* rngChi2Corrections = (float *) malloc(4 * WALLACE_CHI2_COUNT);
   for (unsigned int i = 0; i < WALLACE_CHI2_COUNT; i++)
   {
-  	rngChi2Corrections[i] = MakeChi2Scale(WALLACE_TOTAL_POOL_SIZE);
+    rngChi2Corrections[i] = MakeChi2Scale(WALLACE_TOTAL_POOL_SIZE);
   }
   float* randomNumbers = (float *) malloc(4 * WALLACE_OUTPUT_SIZE);
+  float* randomNumbers_ref = (float *) malloc(4 * WALLACE_OUTPUT_SIZE);
 
 #ifdef USE_GPU
   sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
@@ -62,97 +48,54 @@ int main(int argc, char* argv[]) {
 
   sycl::range<1> gws (WALLACE_NUM_BLOCKS * WALLACE_NUM_THREADS);
   sycl::range<1> lws (WALLACE_NUM_THREADS);
-  const unsigned m_seed = 1;
+  const unsigned seed = 1;
+
+  // warmup and quick validation
+  for (int i = 0; i < 30; i++) {
+    q.submit([&](sycl::handler &cgh) {
+      sycl::local_accessor<float, 1> pool (
+        sycl::range<1>(WALLACE_POOL_SIZE + WALLACE_CHI2_SHARED_SIZE), cgh);
+      cgh.parallel_for(sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
+        rng_wallace(seed, d_Pool, d_randomNumbers, d_rngChi2Corrections,
+                    item, pool.get_multi_ptr<sycl::access::decorated::no>().get());
+      });
+    });
+  }
+
+  for (int i = 0; i < 30; i++) {
+    reference(seed, Pool_ref, randomNumbers_ref, rngChi2Corrections, WALLACE_NUM_BLOCKS);
+  }
+  q.memcpy(randomNumbers, d_randomNumbers, 4 * WALLACE_OUTPUT_SIZE);
+  q.memcpy(hostPool, d_Pool, 4 * WALLACE_TOTAL_POOL_SIZE);
+  q.wait();
+
+  bool ok = true;
+  for (unsigned int n = 0; n < WALLACE_OUTPUT_SIZE; n++) {
+    if (fabsf(randomNumbers_ref[n] - randomNumbers[n]) > 1e-3f) {
+      printf("randNumbers mismatch at index %d: %f %f\n", n, randomNumbers_ref[n], randomNumbers[n]);
+      ok = false;
+      break;
+    }
+  }
+  for (unsigned int n = 0; n < WALLACE_TOTAL_POOL_SIZE; n++) {
+    if (fabsf(Pool_ref[n] - hostPool[n]) > 1e-3f) {
+      printf("Pool mismatch at index %d: %f %f\n", n, Pool_ref[n], hostPool[n]);
+      ok = false;
+      break;
+    }
+  }
+  printf("%s\n", ok ? "PASS" : "FAIL");
 
   q.wait();
   auto start = std::chrono::steady_clock::now();
 
   for (int i = 0; i < repeat; i++) {
-    q.submit([&] (sycl::handler &h) {
+    q.submit([&] (sycl::handler &cgh) {
       sycl::local_accessor<float, 1> pool (
-        sycl::range<1>(WALLACE_POOL_SIZE + WALLACE_CHI2_SHARED_SIZE), h);
-      h.parallel_for<class wallace>(
-        sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
-        const unsigned lcg_a = 241;
-        const unsigned lcg_c = 59;
-        const unsigned lcg_m = 256;
-        const unsigned mod_mask = lcg_m - 1;
-
-        const unsigned lid = item.get_local_id(0);
-        const unsigned gid = item.get_group(0);
-        const unsigned offset = sycl::mul24(WALLACE_POOL_SIZE, gid);
-
-        #pragma unroll
-        for (unsigned i = 0; i < 8; i++)
-          pool[lid + WALLACE_NUM_THREADS * i] = d_Pool[offset + lid + WALLACE_NUM_THREADS * i];
-
-        item.barrier(sycl::access::fence_space::local_space);
-
-        unsigned t_seed = m_seed;
-
-        // Loop generating d_randomNumberss repeatedly
-        for (unsigned loop = 0; loop < WALLACE_NUM_OUTPUTS_PER_RUN; loop++)
-        {
-          t_seed = (1664525U * t_seed + 1013904223U) & 0xFFFFFFFF;
-
-          unsigned intermediate_address = sycl::mul24(loop, 8 * WALLACE_TOTAL_NUM_THREADS) +
-            sycl::mul24(8 * WALLACE_NUM_THREADS, gid) + lid;
-
-          if (lid == 0)
-            pool[WALLACE_CHI2_OFFSET] = d_rngChi2Corrections[sycl::mul24(gid, WALLACE_NUM_OUTPUTS_PER_RUN) + loop];
-          item.barrier(sycl::access::fence_space::local_space);
-          float chi2CorrAndScale = pool[WALLACE_CHI2_OFFSET];
-          for (unsigned i = 0; i < 8; i++)
-          {
-            d_randomNumbers[intermediate_address + i * WALLACE_NUM_THREADS] =
-              pool[sycl::mul24(i, WALLACE_NUM_THREADS) + lid] * chi2CorrAndScale;
-          }
-
-          float rin0_0, rin1_0, rin2_0, rin3_0, rin0_1, rin1_1, rin2_1, rin3_1;
-          for (unsigned i = 0; i < WALLACE_NUM_POOL_PASSES; i++)
-          {
-            unsigned seed = (t_seed + lid) & mod_mask;
-            item.barrier(sycl::access::fence_space::local_space);
-            seed = (sycl::mul24(seed, lcg_a) + lcg_c) & mod_mask;
-            rin0_0 = pool[((seed << 3))];
-            seed = (sycl::mul24(seed, lcg_a) + lcg_c) & mod_mask;
-            rin1_0 = pool[((seed << 3) + 1)];
-            seed = (sycl::mul24(seed, lcg_a) + lcg_c) & mod_mask;
-            rin2_0 = pool[((seed << 3) + 2)];
-            seed = (sycl::mul24(seed, lcg_a) + lcg_c) & mod_mask;
-            rin3_0 = pool[((seed << 3) + 3)];
-            seed = (sycl::mul24(seed, lcg_a) + lcg_c) & mod_mask;
-            rin0_1 = pool[((seed << 3) + 4)];
-            seed = (sycl::mul24(seed, lcg_a) + lcg_c) & mod_mask;
-            rin1_1 = pool[((seed << 3) + 5)];
-            seed = (sycl::mul24(seed, lcg_a) + lcg_c) & mod_mask;
-            rin2_1 = pool[((seed << 3) + 6)];
-            seed = (sycl::mul24(seed, lcg_a) + lcg_c) & mod_mask;
-            rin3_1 = pool[((seed << 3) + 7)];
-
-            item.barrier(sycl::access::fence_space::local_space);
-
-            Hadamard4x4a(rin0_0, rin1_0, rin2_0, rin3_0);
-            pool[0 * WALLACE_NUM_THREADS + lid] = rin0_0;
-            pool[1 * WALLACE_NUM_THREADS + lid] = rin1_0;
-            pool[2 * WALLACE_NUM_THREADS + lid] = rin2_0;
-            pool[3 * WALLACE_NUM_THREADS + lid] = rin3_0;
-
-            Hadamard4x4b(rin0_1, rin1_1, rin2_1, rin3_1);
-            pool[4 * WALLACE_NUM_THREADS + lid] = rin0_1;
-            pool[5 * WALLACE_NUM_THREADS + lid] = rin1_1;
-            pool[6 * WALLACE_NUM_THREADS + lid] = rin2_1;
-            pool[7 * WALLACE_NUM_THREADS + lid] = rin3_1;
-
-            item.barrier(sycl::access::fence_space::local_space);
-          }
-        }
-
-        item.barrier(sycl::access::fence_space::local_space);
-
-        #pragma unroll
-        for (unsigned i = 0; i < 8; i++)
-          d_Pool[offset + lid + WALLACE_NUM_THREADS * i] = pool[lid + WALLACE_NUM_THREADS * i];
+        sycl::range<1>(WALLACE_POOL_SIZE + WALLACE_CHI2_SHARED_SIZE), cgh);
+      cgh.parallel_for(sycl::nd_range<1>(gws, lws), [=] (sycl::nd_item<1> item) {
+        rng_wallace(seed, d_Pool, d_randomNumbers, d_rngChi2Corrections,
+                    item, pool.get_multi_ptr<sycl::access::decorated::no>().get());
       });
     });
   }
@@ -162,17 +105,11 @@ int main(int argc, char* argv[]) {
   auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
   printf("Average kernel execution time: %f (us)\n", time * 1e-3f / repeat);
 
-  q.memcpy(randomNumbers, d_randomNumbers, sizeof(float) * WALLACE_OUTPUT_SIZE).wait();
-
-#ifdef DEBUG
-  // random numbers are different for each i iteration
-  for (unsigned int n = 0; n < WALLACE_OUTPUT_SIZE; n++)
-    printf("%.3f\n", randomNumbers[n]);
-#endif
-
   free(rngChi2Corrections);
   free(randomNumbers);
+  free(randomNumbers_ref);
   free(hostPool);
+  free(Pool_ref);
   sycl::free(d_rngChi2Corrections, q);
   sycl::free(d_randomNumbers, q);
   sycl::free(d_Pool, q);
