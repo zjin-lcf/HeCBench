741b448443f263cf73392b33a5e9ea3d62b29824
Zheming Jin <zjin-lcf@users.noreply.github.com>
2026-02-02 07:57:44 -0800

    [convolutionDeformable-sycl] upgrade the example

--- src/convolutionDeformable-sycl/setup.py
+++ src/convolutionDeformable-sycl/setup.py
@@ -1,8 +1,9 @@
 import os
 import glob
 from setuptools import find_packages, setup
-import intel_extension_for_pytorch as ipex
-from intel_extension_for_pytorch.xpu.cpp_extension import DPCPPExtension, DpcppBuildExtension
+
+# https://docs.pytorch.org/docs/stable/cpp_extension.html
+from torch.utils.cpp_extension import BuildExtension, SyclExtension
 
 
 def get_extensions():
@@ -10,17 +11,13 @@ def get_extensions():
     extensions_dir = os.path.join(this_dir, "src")
 
     main_file = glob.glob(os.path.join(extensions_dir, "*.cpp"))
-    source_sycl = glob.glob(os.path.join(extensions_dir, "sycl", "*.cpp"))
+    source_sycl = glob.glob(os.path.join(extensions_dir, "sycl", "*.sycl"))
     sources = main_file + source_sycl
     sources = [os.path.join(extensions_dir, s) for s in sources]
 
-    extension = DPCPPExtension
-
     ext_modules = [
-        extension(
-            name='_ext',
-            sources=sources,
-            include_dirs=ipex.xpu.cpp_extension.include_paths(),
+        SyclExtension(
+            name='_ext', sources=sources,
         )
     ]
 
@@ -34,6 +31,6 @@ setup(
     description="deformable convolution",
     packages=find_packages(exclude=("configs", "tests")),
     ext_modules=get_extensions(),
-    cmdclass={"build_ext": DpcppBuildExtension},
+    cmdclass={"build_ext": BuildExtension},
 )
 
