484a9ffea0b96ed564be9a10f0438d77904bb8ab
Zheming Jin <zjin-lcf@users.noreply.github.com>
2026-04-19 13:13:21 -0400

    [snicit] rename the executables to main in Makefiles and run scripts

--- src/snicit-hip/bin/run.sh
+++ src/snicit-hip/bin/run.sh
@@ -5,25 +5,25 @@ printf "Log File - " > $log
 date >> $log
 
 echo "Running SNICIT on benchmark A..."
-info=`./beyond -k A -p ../../snicit-cuda/dataset | grep -oP '(?<=SNICIT info:).*'`
+info=`./main -k A -p ../../snicit-cuda/dataset | grep -oP '(?<=SNICIT info:).*'`
 echo == SNICIT on A ==, $info >> $log
 # ////////////////////////////////////////////////////////////////
 
 
 echo "Running SNICIT on benchmark B..."
-info=`./beyond -k B -p ../../snicit-cuda/dataset | grep -oP '(?<=SNICIT info:).*'`
+info=`./main -k B -p ../../snicit-cuda/dataset | grep -oP '(?<=SNICIT info:).*'`
 echo == SNICIT on B ==, $info >> $log
 # ////////////////////////////////////////////////////////////////////
 
 
 echo "Running SNICIT on benchmark C..."
-info=`./beyond -k C -p ../../snicit-cuda/dataset | grep -oP '(?<=SNICIT info:).*'`
+info=`./main -k C -p ../../snicit-cuda/dataset | grep -oP '(?<=SNICIT info:).*'`
 echo == SNICIT on C ==, $info >> $log
 # ///////////////////////////////////////////////////////////////////
 
 
 echo "Running SNICIT on benchmark D..."
-info=`./beyond -k D -p ../../snicit-cuda/dataset | grep -oP '(?<=SNICIT info:).*'`
+info=`./main -k D -p ../../snicit-cuda/dataset | grep -oP '(?<=SNICIT info:).*'`
 echo == SNICIT on D ==, $info >> $log
 
 cat $log
