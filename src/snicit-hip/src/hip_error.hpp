8f6c95fb46ebb87482b31c9557783dca97e1b100
Zheming Jin <zjin-lcf@users.noreply.github.com>
2026-05-05 17:24:40 -0400

    [snicit-hip] address the warning: ignoring return value of function declared with nodiscard attribute

--- src/snicit-hip/src/hip_error.hpp
+++ src/snicit-hip/src/hip_error.hpp
@@ -4,13 +4,11 @@
 
 // Convenience function for checking HIP runtime API results
 // can be wrapped around any runtime API call. No-op in release builds.
-inline
-hipError_t checkHip(hipError_t result)
+inline void checkHip(hipError_t result)
 {
   if (result != hipSuccess) {
     using namespace std::literals::string_literals;
     throw std::runtime_error("HIP Runtime Error : "s + hipGetErrorString(result));
     //assert(result == hipSuccess);
   }
-  return result;
 }
